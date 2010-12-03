cffbfbf3eb324a659937a3d4928e625954fd923d
Samir Das <samir@huia.bic.mni.mcgill.ca>
2010-12-03 17:27:13 -0500

    Added generic_includes file which includes things for scripts

---
+++ tools/generic_includes.php
@@ -0,0 +1,39 @@
+<?php
+
+// PEAR::Config
+require_once "Config.php";
+
+//allow instruments to find libraries
+//set_include_path(get_include_path().":../php/libraries:");
+
+// define which configuration file we're using for this installation
+$configFile = "../project/config.xml";
+
+// load the configuration data into a global variable $config
+$configObj = new Config;
+$root =& $configObj->parseConfig($configFile, "XML");
+if(PEAR::isError($root)) {
+    die("Config error: ".$root->getMessage());
+}
+$configObj =& $root->searchPath(array('config'));
+$config =& $configObj->toArray();
+$config = $config['config'];
+unset($configObj, $root);
+
+// require all relevant OO class libraries
+require_once "Database.class.inc";
+require_once "NDB_Config.class.inc";
+require_once "NDB_BVL_Instrument.class.inc";
+require_once "Candidate.class.inc";
+
+/*
+ * new DB Object
+ * gets connection name and user name..
+ */
+$DB =& Database::singleton($config['database']['database'], $config['database']['username'], $config['database']['password'], $config['database']['host']);
+if(PEAR::isError($DB)) {
+    print "Could not connect to database: ".$DB->getMessage()."<br>\n";
+    die();
+}
+
+?>
