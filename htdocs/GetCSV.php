93619489cb05f335ec1db380d935cd00630ea5b2
Dave MacFarlane <driusan@gmail.com>
2011-02-10 10:04:43 -0500

    Added support for choosing instrument for x and y independently

--- htdocs/GetCSV.php
+++ htdocs/GetCSV.php
@@ -13,11 +13,14 @@ $client->initialize();
 header("Content-type: text/plain");
 $DB = Database::singleton();
 $ConditionBindings = array();
-if(isset($_REQUEST['Instrument'])) {
-    $Instrument = $_REQUEST['Instrument'];
+if(isset($_REQUEST['InstrumentY'])) {
+    $Instrument = $_REQUEST['InstrumentY'];
+}
+if(isset($_REQUEST['InstrumentX'])) {
+    $InstrumentX = $_REQUEST['InstrumentX'];
 }
 if(isset($_REQUEST['FieldY']) && !empty($_REQUEST['FieldY'])) {
-    $Field = $_REQUEST['FieldY'];
+    $Field = "i." . $_REQUEST['FieldY'];
 } else {
     $Field = "Candidate_Age";
 }
@@ -26,19 +29,27 @@ if(isset($_REQUEST['FieldX']) && !empty($_REQUEST['FieldX'])) {
 } else {
     $FieldX = 'Candidate_Age';
 }
+if(isset($InstrumentX)) {
+    $FieldX = "i2." . $FieldX;
+}
+
 if($Field == 'Candidate_Age') {
     $Field = 'COALESCE(i.Candidate_Age, DATEDIFF(i.Date_taken,c.DoB) / 30)';
 }
 if($FieldX == 'Candidate_Age') {
-    $FieldX = 'COALESCE(i.Candidate_Age, DATEDIFF(i.Date_taken,c.DoB) / 30)';
+    if(isset($InstrumentX)) {
+        $FieldX = 'COALESCE(i2.Candidate_Age, DATEDIFF(i2.Date_taken,c.DoB) / 30)';
+    } else {
+        $FieldX = 'COALESCE(i.Candidate_Age, DATEDIFF(i.Date_taken,c.DoB) / 30)';
+    }
 }
-$QueryCondition = "i.$Field IS NOT NULL AND c.Active='Y' and c.Cancelled='N' and s.Active='Y' and s.Cancelled='N'";
+$QueryCondition = "$Field IS NOT NULL AND $FieldX IS NOT NULL AND c.Active='Y' and c.Cancelled='N' and s.Active='Y' and s.Cancelled='N'";
 if(isset($_REQUEST['site']) && !empty($_REQUEST['site'])) {
     $QueryCondition .= " AND c.CenterID=:Site";
     $ConditionBindings['Site'] = $_REQUEST['site'];
 
 }
-if(isset($_REQUEST['Administration'])) {
+if(isset($_REQUEST['Administration']) && !empty($_REQUEST['Administration'])) {
     $QueryCondition .= " AND f.Administration=:Administration";
     $ConditionBindings['Administration'] = $_REQUEST['Administration'];
 }
@@ -46,10 +57,15 @@ if(isset($_REQUEST['Visit_label']) && !empty($_REQUEST['Visit_label'])) {
         $QueryCondition .= " AND s.Visit_label=:Visit_label";
         $ConditionBindings['Visit_label'] = $_REQUEST['Visit_label'];
 }
-$QueryTable = "$Instrument i join flag f USING (CommentID) JOIN session s ON (s.ID=f.SessionID) JOIN candidate c USING (CandID)";
+$QueryTable = "$Instrument i join flag f ON (i.CommentID=f.CommentID) JOIN session s ON (s.ID=f.SessionID) JOIN candidate c USING (CandID)";
+if(isset($InstrumentX)) {
+    $QueryTable .= "JOIN flag f2 ON (f2.SessionID=f.SessionID AND f2.Test_name=";
+    $QueryTable .= "'$InstrumentX') JOIN $InstrumentX i2 ON (f2.CommentID=i2.CommentID) ";
+}
 
 
-$rows = $DB->pselect("SELECT c.PSCID as ID, $FieldX as X, i.$Field as Y, COALESCE(i.Window_Difference, 0) as Category FROM $QueryTable WHERE $QueryCondition", $ConditionBindings);
+$FullQuery = "SELECT c.PSCID as ID, $FieldX as X, $Field as Y, COALESCE(i.Window_Difference, 0) as Category FROM $QueryTable WHERE $QueryCondition";
+$rows = $DB->pselect($FullQuery, $ConditionBindings);
 foreach($rows as $row) {
     print "\"$row[ID]\",\"$row[X]\",\"$row[Y]\",\"$row[Category]\"\n";
 }
