49c6e6002e32ea8f9247046227352ce0b5ba5d06
Dave MacFarlane <driusan@gmail.com>
2011-12-07 13:47:18 -0500

    Added install script for Loris

---
+++ tools/install.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+# This will:
+#   1. Install PEAR libraries
+#   2. Set up 
+# This will only install the database components and Loris config file.
+# It will not affect any system
+
+CWD=`pwd`;
+
+if [ ! -f ../SQL/0000-00-00-schema.sql ]; then
+    echo "Could not find schema file";
+    exit 1;
+fi
+if [ ! -d ../project ]; then
+    mkdir ../project
+fi
+if [ -f ../project/config.xml ]; then
+    echo "Loris appears to already be installed. Aborting."
+    exit 1;
+fi
+
+
+read -p "What is the database name? " mysqldb
+read -p "Database host? " mysqlhost
+read -p "What MySQL user will Loris connect as? " mysqluser
+read -p "What MySQL is $mysqluser's password? " mysqlpass
+
+echo "exit" | mysql $mysqldb -h$mysqlhost -u$mysqluser -p$mysqlpass -A > /dev/null 2>&1
+
+MySQLError=$?;
+
+if [ $MySQLError -eq 0 ]; then
+    RootDir=`dirname $CWD`;
+    echo "Creating config file"
+    sed -e "s/%HOSTNAME%/$mysqlhost/g" -e "s/%USERNAME%/$mysqluser/g" -e "s/%PASSWORD%/$mysqlpass/g" -e "s/%DATABASE%/$mysqldb/g" -e "s#%LORISROOT%#$RootDir/#g" ../docs/config/config.xml > ../project/config.xml
+
+    echo "Fixing permissions on template_c"
+    chmod 777 $RootDir/php/smarty/templates_c/
+
+    echo "Creating database tables"
+    mysql $mysqldb -h$mysqlhost -u$mysqluser -p$mysqlpass -A >> logs/install-`date +%Y-%m-%d`.log 2>&1 < ../SQL/0000-00-00-schema.sql
+    read -p "Enter SiteMin user's password (will be echoed to screen, and prompted to reset on login): " lorispass
+    echo "Updating SiteMin password "
+    mysql $mysqldb -h$mysqlhost -u$mysqluser -p$mysqlpass -A -e "UPDATE users SET Password_MD5=CONCAT('aa', MD5('aa$lorispass')) WHERE ID=1"
+
+    while true; do
+        read -p "Would you like to install PEAR libraries (affects system files)? [yn] " yn
+        case $yn in
+            [Yy]* )
+                echo "Installing PEAR libraries (may prompt for sudo password)"
+                sudo pear upgrade-all >> logs/install-`date +%Y-%m-%d`.log 2>&1
+                sudo pear install Benchmark >> logs/install-`date +%Y-%m-%d`.log 2>&1
+                sudo pear install Config >> logs/install-`date +%Y-%m-%d`.log 2>&1
+                sudo pear install File_Archive >> logs/install-`date +%Y-%m-%d`.log 2>&1
+                sudo pear install HTML_Common >> logs/install-`date +%Y-%m-%d`.log 2>&1
+                sudo pear install HTML_QuickForm >> logs/install-`date +%Y-%m-%d`.log 2>&1
+                sudo pear config-set preferred_state beta >> logs/install-`date +%Y-%m-%d`.log 2>&1
+                sudo pear install HTML_QuickForm2 >> logs/install-`date +%Y-%m-%d`.log 2>&1
+                sudo pear install Mail >> logs/install-`date +%Y-%m-%d`.log 2>&1
+                sudo pear install Mail_Mime >> logs/install-`date +%Y-%m-%d`.log 2>&1
+                sudo pear install Net_SMTP >> logs/install-`date +%Y-%m-%d`.log 2>&1
+                sudo pear install Net_Socket >> logs/install-`date +%Y-%m-%d`.log 2>&1
+                sudo pear install OLE >> logs/install-`date +%Y-%m-%d`.log 2>&1
+                sudo pear install Pager >> logs/install-`date +%Y-%m-%d`.log 2>&1
+                sudo pear install PhpDocumentor >> logs/install-`date +%Y-%m-%d`.log 2>&1
+                sudo pear install Spreadsheet_Excel_Writer >> logs/install-`date +%Y-%m-%d`.log 2>&1
+                sudo pear install Structures_Graph >> logs/install-`date +%Y-%m-%d`.log 2>&1
+                sudo pear install XML_Parser >> logs/install-`date +%Y-%m-%d`.log 2>&1
+                break;;
+            [Nn]* ) 
+                echo "Not installing PEAR libraries"
+                break;;
+            * ) echo "Please enter y or n"
+       esac
+    done;
+
+    echo "Installation complete."
+    echo "Must updated php.ini and apache config"
+else
+    echo "Could not connect to database with user provided";
+    exit 1;
+fi;
