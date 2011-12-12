790745ec3f8cd363f88e4a10ced2d4b7a9d6fad2
Dave MacFarlane <driusan@gmail.com>
2011-12-12 12:48:45 -0500

    Added v36

--- tools/add_timepoint_instruments.php
+++ tools/add_timepoint_instruments.php
@@ -20,7 +20,8 @@ $visit_ages = array(
     'v06' => 180,
     'v12' => 365,
     'v18' => 545,
-    'v24' => 730
+    'v24' => 730,
+    'v36' => 1095
 );
 
 if($argc != 3) {
@@ -35,7 +36,7 @@ $fake_age = $visit_ages[$vl];
 $session= $DB->pselectRow("SELECT ID, SubprojectID, CenterID FROM session WHERE CandID=:CandID AND Visit_label=:vl", array(':CandID' => $CandID, 'vl' => $vl));
 $SessionID = $session['ID'];
 $Subproj = $session['SubprojectID'];
-$Site = $session['CenterID']
+$Site = $session['CenterID'];
 print_r($rows);
 $rows = $DB->pselect("SELECT tb.Test_name FROM test_battery tb WHERE ((tb.AgeMinDays < :fake_age AND tb.AgeMaxDays > :fake_age) OR tb.Visit_label=:vl) AND tb.SubprojectID=:subproj AND NOT EXISTS (SELECT 'x' FROM flag f WHERE f.Test_name=tb.Test_name AND f.SessionID=:SessionID) AND (tb.CenterID=:site OR tb.CenterID IS NULL)", array(':fake_age' => $fake_age, ':vl' => $vl, ':subproj' => $Subproj, ':SessionID' => $SessionID, ':site' => $Site));
 
