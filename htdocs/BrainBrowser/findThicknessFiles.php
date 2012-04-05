18797e6559c18dc27d117dda504c845bcc23e021
Mia <mia.petkova@gmail.com>
2012-04-05 14:19:13 -0400

    Added 3D BrainBrowser into a separate sub-directory

---
+++ htdocs/BrainBrowser/findThicknessFiles.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ if (file_exists("/data/preventAD/data/assembly" . $_FILES["file"]["name"]))
+      {
+      echo $_FILES["file"]["name"] . " already exists. ";
+      }
+*/
+/*
+if ($handle = opendir($base_dir)) {
+    echo "Directory handle: $handle\n";
+    echo "Entries:\n";
+
+    while (false !== ($entry = readdir($handle))) {
+ 	if ($entry != "." && $entry != "..") {
+        	echo "$entry\n";
+    	}
+	if($subHandle = opendir($base_dir . '/' . $entry)) {
+		echo "Dir handle: $subHandle\n";
+		echo "Sub Entries:\n";
+		while (false !==($subEntry = readdir($subHandle))) {
+			echo "$subEntry\n";
+	}
+	closedir($subHandle);
+	}
+    }	
+    closedir($handle);
+}
+*/
+
+$dccid = $_POST['dccidP'];
+
+
+//$dir = '/data/preventAD/data/assembly';
+$dir = 'mri/thickness';
+function ListFiles($dir) {
+   if($dh = opendir($dir)) {
+
+        $files = Array();
+        $inner_files = Array();
+
+        while($file = readdir($dh)) {
+            if($file != "." && $file != ".." && $file[0] != '.') {
+                if(is_dir($dir . "/" . $file)) {
+//		    echo "----DIR:" . $dir . "\n";
+//		    echo "++++FILE:" . $file . "\n";
+                    $inner_files = ListFiles($dir . "/" . $file);
+                    if(is_array($inner_files)) $files = array_merge($files, $inner_files); 
+                } else {
+		    if(preg_match("/.txt$/", $file))
+                    array_push($files, $dir . "/" . $file);
+                }
+            }
+        }
+
+        closedir($dh);
+        return $files;
+    }
+}
+
+
+foreach (ListFiles($dir) as $key=>$file){
+    echo $file ."\n";
+}  
+
+?>
