b1b2fea09a270fbb53ea9bbf4e24fa06cf7f4e7c
zia <zia.mohades@gmail.com>
2012-05-02 14:29:58 -0400

    Include the backbone/utility function to create data integrity flag module

---
+++ htdocs/get_csv.php
@@ -0,0 +1,31 @@
+<?php
+require_once "../tools/generic_includes.php";
+//fputcsv($output, array('Column 1', 'Column 2', 'Column 3'));
+header('Content-Type: text/csv; charset=utf-8');
+header('Content-Disposition: attachment; filename=data.csv');
+$output = fopen('php://output', 'w');
+
+$instrument = $_REQUEST['instrument'];
+$sourcefield =  $_REQUEST['sourcefield'];
+$completion_status = $_REQUEST['completion_status'];
+$sent_to_dcc_status =  $_REQUEST['sent_to_dcc_status'];
+/*
+$instrument = '3week_infancy';
+$sourcefield = 'data_entry_date';
+*/
+
+$query = "Select c.PSCID, s.Visit_label, $sourcefield as Field,f.data_entry from $instrument i , flag f,session s,candidate c
+where i.commentid = f.commentid and f.sessionid = s.ID and s.Candid = c.Candid";
+if (($completion_status!='any') && (!empty($completion_status))) {
+	$query .= " And f.data_entry = '$completion_status'";
+}
+if (($sent_to_dcc_status!='any') && (!empty($sent_to_dcc_status))){
+	$query .= " And s.current_stage = '$sent_to_dcc_status'";
+}
+$header = array("PSCID","Visit_lable",$sourcefield,"data_entry");
+fputcsv($output,$header);
+$DB->select($query,$results);
+foreach($results as $result){
+	fputcsv($output,$result);
+}
+?>
