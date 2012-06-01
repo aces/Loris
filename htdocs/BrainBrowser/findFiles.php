83d3441769ddec273ee92daa92e2da6f4320b479
Mia <mia.petkova@gmail.com>
2012-06-01 11:55:57 -0400

    3D viewer version 1

---
+++ htdocs/BrainBrowser/findFiles.php
@@ -0,0 +1,38 @@
+<?php
+require_once "NDB_Client.class.inc";
+$client =& new NDB_Client();
+$client->initialize("../../project/config.xml");
+
+
+$db =& Database::singleton();
+if(PEAR::isError($db)) {
+    print "Could not connect to database: ".$db->getMessage()."<br>\n"; die();
+}
+
+//If selecting obj file, grab those from the database
+if ($_POST['option'] == 'surface'){
+    $files = $db->pselectRow("SELECT file FROM files WHERE SessionID =:s AND FileType =:f", array(':s'=> $_POST['sessionID'], ':f'=> 'obj')); 
+    if (PEAR::isError($files)){
+        return PEAR::raiseError("Could not retrieve files: " . $files->getMessage());
+    }
+}
+
+//If selecting thickness file, grab those from the database
+elseif ($_POST['option'] == 'thickness'){
+    $files = $db->pselectRow("Select file from files where SessionID =:s and file like '%txt'", array(':s'=> $_POST['sessionID']));
+    if (PEAR::isError($files)){
+        return PEAR::raiseError("Could not retrieve files: " . $files->getMessage());
+    }
+}
+
+//Replace image path as specified in config file with sym link folder for file visibility
+$config =& NDB_Config::singleton();
+$imgPath = $config->getSetting('imagePath');
+$regexPath = preg_replace("/\//", "\/", $imgPath);
+
+foreach ($files as $key=>$value){
+    $v = preg_replace("/" . $regexPath . "/", "BrainBrowser/files", $value);
+    echo $v . "\n";
+}
+
+?>
