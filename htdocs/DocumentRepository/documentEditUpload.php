1cb2a0e7f47f83ccdd56a7fc43e382410a584248
Mia <mia.petkova@gmail.com>
2012-06-08 13:07:36 -0400

    Added php path

--- htdocs/DocumentRepository/documentEditUpload.php
+++ htdocs/DocumentRepository/documentEditUpload.php
@@ -1,4 +1,5 @@
 <?php
+set_include_path(get_include_path().":../../project/libraries:../../php/libraries:");
 require_once "NDB_Client.class.inc";
 $client =& new NDB_Client();
 $client->initialize("../../project/config.xml");
