d348df413a94a86deee5a51f4895256c7a38eadd
zia <zia.mohades@gmail.com>
2012-08-22 14:17:46 -0400

    Creates the table parameter_type_override and adds the permission

---
+++ SQL/2012-08-21_parameter_type_override.sql
@@ -0,0 +1,6 @@
+CREATE TABLE parameter_type_override ( 
+Name varchar(255) PRIMARY KEY, 
+Description text 
+);
+INSERT INTO permissions (code, description,categoryID) VALUES ('data_dict','Parameter Type description','2');
+
