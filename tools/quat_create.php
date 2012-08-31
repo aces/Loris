495f990c956e4046f17016686a16ee3f153de762
Dave MacFarlane <driusan@gmail.com>
2012-08-31 09:05:30 -0400

    New, more granular quat scripts (from IBIS)

---
+++ tools/quat_create.php
@@ -0,0 +1,185 @@
+<?
+set_time_limit(0);
+/**
+ * QUAT uses the parameter_type table to determine the list of
+ * parameters available for querying, nukes and reconstructs the query
+ * gui tables (the wide ones containing all parameter types)
+ *
+ * IMPORTANT NOTE:
+ *
+ * You will need entry in your config.xml:
+ *
+ *       <quatUser>username</quatUser>
+ *       <quatPassword>password</quatPassword>
+ * 
+ * @package main
+ * @subpackage query_gui
+ */
+
+/*
+mysql> describe parameter_type;
++-----------------+-----------------------------------------------+------+-----+---------+----------------+
+| Field           | Type                                          | Null | Key | Default | Extra          |
++-----------------+-----------------------------------------------+------+-----+---------+----------------+
+| ParameterTypeID | int(10) unsigned                              |      | PRI | NULL    | auto_increment |
+| Name            | varchar(255)                                  |      | MUL |         |                |
+| Type            | enum('ordinal','nominal','scalar','filePath') |      |     | ordinal |                |
+| Description     | text                                          | YES  |     | NULL    |                |
+| RangeMin        | double                                        | YES  |     | NULL    |                |
+| RangeMax        | double                                        | YES  |     | NULL    |                |
+| SourceField     | text                                          | YES  |     | NULL    |                |
+| SourceFrom      | text                                          | YES  |     | NULL    |                |
+| CurrentGUITable | varchar(255)                                  | YES  |     | NULL    |                |
+| Queryable       | tinyint(1)                                    | YES  |     | 1       |                |
++-----------------+-----------------------------------------------+------+-----+---------+----------------+
+*/
+
+
+// settings
+$columnThreshhold = 150;
+$quatTableBasename = 'quat_table_';
+$quatTableCounter = 1;
+
+// create an NDB client 
+require_once "../php/libraries/NDB_Client.class.inc";
+$client = new NDB_Client;
+$client->makeCommandLine();
+$client->initialize();
+
+// get a Database connection
+$config =& NDB_Config::singleton();
+$dbConfig = $config->getSetting('database');
+$db = new Database;
+$result = $db->connect($dbConfig['database'], $dbConfig['quatUser'], $dbConfig['quatPassword'], $dbConfig['host'], false);
+if(PEAR::isError($result)) {
+    die("Could not connect to database: ".$result->getMessage());
+}
+
+// nuke the current data query tables
+$query = "SELECT DISTINCT CurrentGUITable FROM parameter_type";
+$dataQueryTables = array();
+$db->select($query, $dataQueryTables);
+
+if(is_array($dataQueryTables) && count($dataQueryTables)) {
+    foreach($dataQueryTables AS $table) {
+        if(empty($table['CurrentGUITable'])) continue;
+
+        /*
+        $query = "DROP TABLE $table[CurrentGUITable]";
+        */
+        //$result = $db->run($query);
+        /*
+        if($db->isError($result)) {
+            die( "Failed to drop table $table: ".$result->getMessage()."\n" );
+        }
+        */
+
+        //$db->update('parameter_type', array('CurrentGUITable'=>null), array('CurrentGUITable'=>$table['CurrentGUITable']));
+        print "At: $query, memory: " . memory_get_usage() . "\n";
+    }
+    unset($table);
+}
+// Free no longer needed memory
+unset($dataQueryTables);
+unset($query);
+
+print "About to create tables: " . memory_get_usage() . "\n";
+// get a list of all parameter types
+$query = "SELECT ParameterTypeID, Name, Type, SourceField, SourceFrom, SourceCondition, CurrentGUITable FROM parameter_type WHERE Queryable=1";
+$parameterTypes = array();
+$db->select($query, $parameterTypes);
+
+// loop over the parameter types to build the data query tables
+$createSQL = "";
+$columnCount = 0;
+$nextTableName = $quatTableBasename . $quatTableCounter;
+$nextTableName_running = $nextTableName . "_running";
+$countParameterTypes = count($parameterTypes);
+
+
+$today= getdate();
+$date = sprintf("%04d-%02d-%02d", $today['year'], $today['mon'], $today['mday']);
+
+$logfp = fopen("logs/quat.$date.log", 'a');
+function log_msg($message) {
+    global $logfp;
+    $now_array = getdate();
+    $now_string = sprintf("%04d-%02d-%02d %02d:%02d:%02d", $now_array['year'], $now_array['mon'], $now_array['mday'], $now_array['hours'], $now_array['minutes'], $now_array['seconds']);
+    fwrite($logfp, "[$now_string] $message\n");
+}
+
+
+
+$createSQL = "CREATE TABLE parameter_type_running AS SELECT * FROM parameter_type WHERE Queryable=1";
+log_msg($createSQL);
+$db->run($createSQL);
+if($db->isError($result)) {
+    die("Failed to create temporary parameter_type table");
+}
+$updateSQL = "UPDATE parameter_type_running SET CurrentGUITable=null";
+$db->run($createSQL);
+if($db->isError($result)) {
+    die("Failed to update parameter_type_running table");
+}
+
+$createSQL = '';
+for($idx=0; $idx<$countParameterTypes; $idx++) {
+    $parameterType =& $parameterTypes[$idx];
+
+    // add column to the create table statement
+    $createSQL .= !empty($createSQL) ? ', ' : '';
+    $createSQL .= "`$parameterType[Name]` $parameterType[Type]";
+    $parameterTypes[$idx]['CurrentGUITable'] = $nextTableName;
+    $parameterTypesForQuat[] = $parameterType['ParameterTypeID'];
+    //$db->update('parameter_type', array('CurrentGUITable'=>$nextTableName), array('ParameterTypeID'=>$parameterType['ParameterTypeID']));
+    $columnCount++;
+
+    // if the number of columns is now greater than the threshhold
+    if($columnCount >= $columnThreshhold or $idx+1 == $countParameterTypes) {
+
+        // run the create table statement
+        $createSQL = "CREATE TABLE $nextTableName_running (SessionID int not null primary key, $createSQL)";
+        $updateQuatSQL = "UPDATE parameter_type_running SET CurrentGUITable=" . $db->quote($nextTableName) . " WHERE ParameterTypeID IN (" . join(',', $parameterTypesForQuat) . ')';
+        // be more aggressive -- insert every candidate who isn't cancelled
+        $insertSQL = "INSERT INTO $nextTableName_running (SessionID) SELECT s.ID from session s JOIN candidate c USING (CandID) WHERE c.Active='Y' AND c.Cancelled='N' AND s.Active='Y' AND s.Cancelled='N' AND c.CenterID IN (2, 3, 4, 5) AND s.Current_stage <> 'Recycling Bin' AND c.PSCID <> 'scanner'";
+        $quatTableCounter++;
+        $nextTableName = $quatTableBasename . $quatTableCounter;
+        $nextTableName_running = $nextTableName . "_running";
+        unset($parameterTypesForQuat);
+        log_msg($createSQL);
+        log_msg($updateQuatSQL);
+        log_msg($insertSQL);
+        $result = $db->run($createSQL);
+        if($db->isError($result)) {
+            die( "Failed to create table $nextTableName: ".$result->getMessage()."\n" );
+        }
+        $result = $db->run($updateQuatSQL);
+        if($db->isError($result)) {
+            die( "Failed to populate table $nextTableName: ".$result->getMessage()."\n" );
+        }
+        $result = $db->run($insertSQL);
+        if($db->isError($result)) {
+            die( "Failed to populate table $nextTableName: ".$result->getMessage()."\n" );
+        }
+
+        // reset the column counter and create table statement
+        $createSQL = "";
+        $columnCount = 0;
+        print "Finished creating $nextTableName: " . memory_get_usage() . "\n";
+    }
+}
+
+fclose($logfp);
+
+// These are no longer used below this point, so free up the memory
+unset($columnThreshhold);
+unset($quatTableBasename);
+unset($quatTableCounter);
+unset($createSQL);
+unset($columnCount);
+unset($nextTableName);
+unset($parameterType);
+unset($countParameterTypes);
+print "Finished creating tables: " . memory_get_usage() . "\n";
+
+?>
--- tools/quat_populate.php
+++ tools/quat_populate.php
@@ -35,7 +35,6 @@ mysql> describe parameter_type;
 */
 
 require_once "Utility.class.inc";
-require_once "Log.class.inc";
 // settings
 $columnThreshhold = Utility::getColumnThresholdCount();
 $quatTableBasename = 'quat_table_';
@@ -47,9 +46,6 @@ $client = new NDB_Client;
 $client->makeCommandLine();
 $client->initialize();
 
-//initialize the log file
-$log = new Log("quat");
-
 // get a Database connection
 $config =& NDB_Config::singleton();
 $dbConfig = $config->getSetting('database');
@@ -59,112 +55,38 @@ if(PEAR::isError($result)) {
     die("Could not connect to database: ".$result->getMessage());
 }
 
-// nuke the current data query tables
-$query = "SELECT DISTINCT CurrentGUITable FROM parameter_type";
-$dataQueryTables = array();
-$db->select($query, $dataQueryTables);
-
-if(is_array($dataQueryTables) && count($dataQueryTables)) {
-    foreach($dataQueryTables AS $table) {
-        if(empty($table['CurrentGUITable'])) continue;
-
-        $query = "DROP TABLE $table[CurrentGUITable]";
-        $result = $db->run($query);
-        if($db->isError($result)) {
-            $log->addLog("Failed to drop table $table: ".$result->getMessage()."\n");
-            die( "Failed to drop table $table: ".$result->getMessage()."\n" );
-        }
-
-        $db->update('parameter_type', array('CurrentGUITable'=>null), array('CurrentGUITable'=>$table['CurrentGUITable']));
-        print "At: $query, memory: " . memory_get_usage() . "\n";
-    }
-    unset($table);
-}
-// Free no longer needed memory
-unset($dataQueryTables);
-unset($query);
-
-print "About to create tables: " . memory_get_usage() . "\n";
-// get a list of all parameter types
-$query = "SELECT ParameterTypeID, Name, Type, SourceField, SourceFrom, SourceCondition, CurrentGUITable FROM parameter_type WHERE Queryable=1";
+// get a list of all parameter types that we're populating
+$query = "SELECT p.ParameterTypeID, p.Name, p.Type, p.SourceField, p.SourceFrom, p.SourceCondition, p.CurrentGUITable FROM parameter_type_running p JOIN parameter_type_category_rel USING (ParameterTypeID) JOIN parameter_type_category USING (ParameterTypeCategoryID) WHERE Queryable=1";
 $parameterTypes = array();
+
+// Parse command line options
+$options = getopt("m", array("metavars","sourcefrom:", "fields:", "help"));
+if(isset($options['help'])) {
+    print "Usage: $argv[0] [options]\n\n";
+    //print "\tValid options are:\n";
+    print "   -m, --metavars\t\tOnly populate metavar categories\n";
+    print "   --sourcefrom=table\t\tOnly populate fields from table\n";
+    print "   --fields=field1,field2,...\tOnly populate (comma separated) list of fields \n";
+    print "   --help\t\t\tShow this page\n";
+    exit(1);
+}
+if(isset($options['m']) || isset($options['metavars'])) {
+    $query .= " AND parameter_type_category.type='Metavars'";
+}
+if(isset($options['fields'])) {
+    $fields = explode(",", $options['fields']);
+    $q_fields = array();
+    foreach($fields as $field) {
+        $q_fields[] = $db->quote($field);
+    }
+   
+    $query .= " AND p.SourceField IN (";
+    $query .= implode(",", $q_fields);
+    $query .= ")";
+}
+$query .= " ORDER BY CurrentGUITable, SourceFrom";
+print "$query\n";
 $db->select($query, $parameterTypes);
-
-// loop over the parameter types to build the data query tables
-$createSQL = "";
-$columnCount = 0;
-$nextTableName = $quatTableBasename . $quatTableCounter;
-$countParameterTypes = count($parameterTypes);
-
-
-for($idx=0; $idx<$countParameterTypes; $idx++) {
-    $parameterType =& $parameterTypes[$idx];
-
-    // add column to the create table statement
-    $createSQL .= !empty($createSQL) ? ', ' : '';
-    $createSQL .= "`$parameterType[Name]` $parameterType[Type]";
-    $parameterTypes[$idx]['CurrentGUITable'] = $nextTableName;
-    $db->update('parameter_type', array('CurrentGUITable'=>$nextTableName), array('ParameterTypeID'=>$parameterType['ParameterTypeID']));
-    $columnCount++;
-
-    // if the number of columns is now greater than the threshhold
-    if($columnCount >= $columnThreshhold or $idx+1 == $countParameterTypes) {
-
-        // run the create table statement
-        $createSQL = "CREATE TABLE $nextTableName (SessionID int not null primary key, $createSQL)";
-        $log->addLog($createSQL);
-        $quatTableCounter++;
-        $nextTableName = $quatTableBasename . $quatTableCounter;
-        $result = $db->run($createSQL);
-        if($db->isError($result)) {
-            $log->addLog("Failed to create table $nextTableName: ".$result->getMessage()."\n");
-            die( "Failed to create table $nextTableName: ".$result->getMessage()."\n" );
-        }
-        // reset the column counter and create table statement
-        $createSQL = "";
-        $columnCount = 0;
-        print "Finished creating $nextTableName: " . memory_get_usage() . "\n";
-        $log->addLog("Finished creating $nextTableName: " . memory_get_usage() . "\n");
-    }
-}
-
-
-// These are no longer used below this point, so free up the memory
-unset($columnThreshhold);
-unset($quatTableBasename);
-unset($quatTableCounter);
-unset($createSQL);
-unset($columnCount);
-unset($nextTableName);
-unset($parameterType);
-unset($countParameterTypes);
-print "Finished creating tables: " . memory_get_usage() . "\n";
-
-// define the list of sessions to use, according to NIHPD criteria, or not.
-$query = "SELECT ID, CandID, Screening, Visit FROM session";
-$isNIHPD = $config->getSetting('isNIHPD');
-if ($isNIHPD == "true") {
-    $query .= " WHERE Approval='Pass'";
-}
-$sessions = array();
-$db->select($query, $sessions);
-
-unset($config);
-print "At make the data table: " . memory_get_usage() . "\n";
-// make the data table, insert a row for each session
-$queryableSessions = array();
-foreach($sessions AS $session) {
-    foreach($parameterTypes AS $parameterType) {
-        if($parameterType['CurrentGUITable'] == NULL || $session['ID'] == NULL) {
-            //print_r($parameterType);
-            //print_r($session);
-            continue;
-        }
-        if($db->selectOne("SELECT COUNT(*) FROM $parameterType[CurrentGUITable] WHERE SessionID=$session[ID]") == 0) {
-            $db->run("INSERT INTO $parameterType[CurrentGUITable] (SessionID) VALUES ($session[ID])");
-        }
-    }
-}
 
 function GetSelectStatement($parameterType, $field=NULL) {
     // construct query string dependant on parameter source
@@ -217,15 +139,18 @@ function GetSelectStatement($parameterType, $field=NULL) {
 
     case 'session':
     case 'candidate':
-        if($field == null) {
-            $field = "s.$parameterType[SourceField] Value";
+        if($field == null && $parameterType['SourceFrom']=='session') {
+            $field = "s.$parameterType[SourceField] ";
+        } else if($field == null && $parameterType['SourceFrom']=='candidate') {
+            $field = "c.$parameterType[SourceField] ";
         }
-        $query = "SELECT $field AS Value FROM session s LEFT JOIN candidate USING (CandID)";
+
+        $query = "SELECT $field AS Value FROM session s LEFT JOIN candidate c USING (CandID) WHERE 1=1 ";
         break;
 
     case 'psc':
         if($field == null) {
-            $field = "s.$parameterType[SourceField] Value";
+            $field = "psc.$parameterType[SourceField]";
         }
         $query = "SELECT $field AS Value FROM session s LEFT JOIN psc USING (CenterID)";
         break;
@@ -248,13 +173,59 @@ function GetSelectStatement($parameterType, $field=NULL) {
     return $query;
 }
 
+$today= getdate();
+$date = sprintf("%04d-%02d-%02d", $today['year'], $today['mon'], $today['mday']);
+
+$logfp = fopen("logs/quat_populate.$date.log", 'a');
+function log_msg($message) {
+        global $logfp;
+            $now_array = getdate();
+                $now_string = sprintf("%04d-%02d-%02d %02d:%02d:%02d", $now_array['year'], $now_array['mon'], $now_array['mday'], $now_array['hours'], $now_array['minutes'], $now_array['seconds']);
+                    fwrite($logfp, "[$now_string] $message\n");
+}
+
 // Update data
 foreach($parameterTypes AS $parameterType) {
     if($parameterType['CurrentGUITable'] != null) {
-    print "Updating $parameterType[Name], memory: " . memory_get_usage() . " bytes\n";
-    $log->addLog("Updating $parameterType[Name], memory: " . memory_get_usage() . " bytes\n");
-    $db->run("UPDATE $parameterType[CurrentGUITable] SET $parameterType[Name]=(" . GetSelectStatement($parameterType) . " AND $parameterType[CurrentGUITable].SessionID=s.ID AND flag.CommentID NOT LIKE 'DDE%') WHERE $parameterType[CurrentGUITable].SessionID=(" . GetSelectStatement($parameterType, "DISTINCT s.ID"). " AND flag.CommentID NOT LIKE 'DDE%' AND s.ID=quat_table_2.SessionID)");
-    //$db->run("UPDATE $parameterType[CurrentGUITable] SET $parameterType[Name]=(" . GetSelectStatement($parameterType) . " WHERE $parameterType[CurrentGUITable].SessionID=s.ID)");
+        print "Updating $parameterType[Name], memory: " . memory_get_usage() . " bytes\n";
+        if(($lastGUITable !== $parameterType['CurrentGUITable'] || $lastSourceFrom !== $parameterType['SourceFrom']) && (isset($lastGUITable) && isset($lastSourceFrom))) {
+            $updateStmt = "UPDATE $lastGUITable" . "_running SET " . join(", ", $setVals);
+            $setVals = array();
+            log_msg($updateStmt);
+            //print "$updateStmt\n";
+
+            $db->run($updateStmt);
+            
+        }
+        switch($parameterType['SourceFrom']) {
+            case 'session':
+            case 'candidate':
+            case 'psc':
+            case 'parameter_candidate';
+            case 'parameter_session':
+                $setVals[] = "`$parameterType[Name]`=(" . GetSelectStatement($parameterType) . " AND $parameterType[CurrentGUITable]_running.SessionID=s.ID)";
+                break;
+            default:
+                $setVals[] = "`$parameterType[Name]`=(" . GetSelectStatement($parameterType) . " AND $parameterType[CurrentGUITable]_running.SessionID=s.ID AND flag.CommentID NOT LIKE 'DDE%')";
+                break;
+        }
+        $lastGUITable= $parameterType['CurrentGUITable'];
+        $lastSourceFrom= $parameterType['SourceFrom'];
     }
 }
+// Once more, for good measure. (and because the if statement above wouldn't be executed
+// after the last parameterTypeId)
+$updateStmt = "UPDATE $lastGUITable" . "_running SET " . join(", ", $setVals);
+log_msg($updateStmt);
+fclose($logfp);
+
+$db->run($updateStmt);
+// Get _running tables from information_schema
+// $origTbl = cut off _running
+// drop $origTbl if exists;
+// rename _running to $origTbl
+// Update parameter_type SET CurrentGUITable = null
+// Update parameter_type SET CurrentGUITable = SELECT CurrentGUITable from parameter_type_running
+// Drop parameter_type_running
+
 ?>
