b2e0ec27348887d2250665db86446124ccd4a528
zia <zia.mohades@gmail.com>
2012-08-31 14:44:37 -0400

    returns the specific instruments based on the visit_label

---
+++ htdocs/GetInstruments.php
@@ -0,0 +1,24 @@
+<?php
+/* This is used by the stats page to get the list of scorable columns for an
+ * instrument via AJAX. It's used so that ScatterPlot-SVG.php can be run for
+ * any scorable in an instrument, dynamically */
+require_once "Database.class.inc";
+require_once 'NDB_Config.class.inc';
+require_once 'NDB_Client.class.inc';
+$config =& NDB_Config::singleton();
+$client = new NDB_Client();
+$client->makeCommandLine();
+$client->initialize();
+
+require_once "Utility.class.inc";
+//gets the given visit_label and returns the instrument
+
+		
+
+$instruments = Utility::getVisitInstruments($_REQUEST['visit_label']);
+
+print "All Instruments\n";
+foreach($instruments as $instrument){
+	print $instrument['Test_name_display'] . "\n";
+}
+?>
