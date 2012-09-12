45619f4302ea24a3528164334b497333e34ec29b
zia <zia.mohades@gmail.com>
2012-09-12 14:56:54 -0400

    Removed the $ from the variable names

--- htdocs/js/modules/datadict.js
+++ htdocs/js/modules/datadict.js
@@ -3,11 +3,11 @@ function save() {
     $('.description').
             bind('blur',function(event){
                 event.stopImmediatePropagation();   
-                $id = event.target.id;
-                $value = $("#" + $id) .text();
+                id = event.target.id;
+                value = $("#" + id) .text();
                // sendRemoteDataQuery("query_gui_data_loader.php?mode=loadQuery&action="+action+"&qid="+qid);
-                var $test = "UpdateDataDict.php?fieldname=" + $id + "&description=" + $value;
-                $.get("UpdateDataDict.php?fieldname=" + $id + "&description=" + $value, function(data) {
+                var test = "UpdateDataDict.php?fieldname=" + id + "&description=" + value;
+                $.get("UpdateDataDict.php?fieldname=" + id + "&description=" + value, function(data) {
                     }
                 );
             }
