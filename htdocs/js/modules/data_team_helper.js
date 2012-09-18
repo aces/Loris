9f91b0f32f4a913279b51940ea0cf3ef88b79891
zia <zia.mohades@gmail.com>
2012-09-18 11:32:39 -0400

    fixed Indentation

--- htdocs/js/modules/data_team_helper.js
+++ htdocs/js/modules/data_team_helper.js
@@ -1,5 +1,5 @@
-
-function getQueryVariable(variable) {
+function getQueryVariable(variable) 
+{
     var query = window.location.search.substring(1);
     var vars = query.split("&");
     for (var i = 0; i < vars.length; i++) {
@@ -10,42 +10,42 @@ function getQueryVariable(variable) {
     }
 }
 
-
-function changefieldOptions() {
+function changefieldOptions() 
+{
     changeVisitLabels();
 }
 
-
-function changeVisitLabels() {
+function changeVisitLabels() 
+{
     //get the value for the visit selected
     var instrument_dropdown = document.getElementById('instrument');
     var visit_label_dropdown = document.getElementById('visit_label');
     var visit_label_value = visit_label_dropdown.value;
-    
     request = getQueryVariable("visit_label");
     instrument_dropdown_value = getQueryVariable("instrument");
-    if (instrument_dropdown_value != undefined){
-    	    var instrument_dropdown_value = instrument_dropdown_value.replace(/\+/g,' ');
+    if (instrument_dropdown_value != undefined) {
+        var instrument_dropdown_value = instrument_dropdown_value.replace(/\+/g,' ');
     }
-
     $.get("GetInstruments.php?visit_label=" + visit_label_value ,
-    function(data){
-        instruments = data.split("\n");
-        instrument_dropdown.options.length = 0;
-        var i, numInstruments = instruments.length, val, selected = "";
-        for (i=0; i<numInstruments;i++){
-            val = instruments[i];
-            if(val !=''){
-                instrument_dropdown.options[i] = new Option(val,val);
-                if ((val==instrument_dropdown_value) && (instrument_dropdown_value!= undefined)){
-                    instrument_dropdown.options[i].selected = "selected";
+            function(data) 
+            {
+                instruments = data.split("\n");
+                instrument_dropdown.options.length = 0;
+                var i, numInstruments = instruments.length, val, selected = "";
+                for (i=0;i<numInstruments;i++) {
+                    val = instruments[i];
+                    if(val !='') {
+                        instrument_dropdown.options[i] = new Option(val,val);
+                        if ((val==instrument_dropdown_value) && (instrument_dropdown_value!= undefined)) {
+                            instrument_dropdown.options[i].selected = "selected";
+                        }
+                   }
                 }
             }
-        }
-    });
+    );
 }
 
 //runs the function when the page is loaded..
-$(function(){
-    changefieldOptions();
-});
+$(function() {
+     changefieldOptions();
+ });
