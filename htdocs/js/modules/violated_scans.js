76b5c64ca3840ef83fee0b0ab24e4d27cd0c6171
zia <zia.mohades@gmail.com>
2012-10-09 11:28:26 -0400

    Cosmetic changes

--- htdocs/js/modules/violated_scans.js
+++ htdocs/js/modules/violated_scans.js
@@ -5,10 +5,8 @@ TODO:
 **/
 function change(){
 	
-	
 	$('#hide').hide();
 	$('#tbl').hide();
-	  
 	$('td').filter('[id*=header]').css({
 		backgroundColor: 'green'
 	});
@@ -18,7 +16,6 @@ function change(){
 	  $('#tbl').show('slow',function(){});
 	  $('#hide').show();
 	  $('#show').hide();
-	  
 	}
 	);
 	
@@ -30,75 +27,7 @@ function change(){
 	});
 
 }
-	/*
-	$('td').filter('[id*=header]')
-	 .bind('click',function(event){
-	 	var id= this.id;
-	 	var splits=id.split('_');
-	 	var td_id = 'td_' + splits[1];
-	 	//$("#"+this.id).addClass("width_color");
-	    $("#"+this.id).toggleClass('width_color');
-	    
-	 	$('td').filter('[id$=' + td_id + ']').each(function(){ 
-	 	    $("#"+this.id).toggleClass('width_color');
-	 	    // slideToggle('fast', function() {                
-          //  });
-	 	});
-	 	
-	 }
-	)
-	*/
-	/*
-	$('#header_1').bind('click',function(event)
-	  {
-	   ($('td').filter('[id=td_1]')).toggle();
-	  
-	  }
-    );
-    */
-
-
-function swapThem(){
-	
-}
-/*
-function ShrinkTable() {
-    var FontSize = parseInt($("#tbl").css('font-size').replace('px', ''),10);
-    var TabWidth = $("#tbl").width();
-    var DivWidth = $("#narrorColumn");
-    
-    if (parseInt(DivWidth.css('width').replace('px', ''),10) < TabWidth) {
-        $("#tbl").css('font-size', FontSize - 4 + 'px');
-        //Shrink the font while div width is less than table width
-        ShrinkTable();
-    }
-}
-
-
-*/
 $(function(){
     change();
-  //  ShrinkTable();
 });
 
-
-
-
-
-
-/*
-$(".expand").click(function(e) {
-     e.preventDefault();
-     var common_parent = $(this).closest('.bdrroundfg');
-     var slide_content = common_parent.find('.slide_content'); // the added class!
-     var all_buttons   = common_parent.find('a.expand');   
-
-     var txt = $(this).text() === '+ Expand' ? '- Minimize' : '+ Expand';
-     all_buttons.eq(0).text(txt);    // toggle text - first button only!     
-
-     $(slide_content).slideToggle('fast', function(){
-          // do something here...
-     });
-
- });
- */
