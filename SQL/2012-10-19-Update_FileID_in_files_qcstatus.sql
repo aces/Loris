8251dd1c035903578d1a1e74ec17c5f7b73e216d
Cecile Madjar <cecile.madjar@gmail.com>
2012-10-19 15:29:41 -0400

    new file:   2012-10-19-Update_FileID_in_files_qcstatus.sql

---
+++ SQL/2012-10-19-Update_FileID_in_files_qcstatus.sql
@@ -0,0 +1,5 @@
+CREATE TEMPORARY TABLE FileIDs AS SELECT pf.FileID, pf.Value AS SeriesUID, fq.QCStatus, fq.QCFirstChangeTime, fq.QCLastChangeTime FROM files AS f LEFT JOIN parameter_file AS pf ON f.FileID=pf.FileID LEFT JOIN parameter_type AS pt ON pt.ParameterTypeID=pf.ParameterTypeID LEFT JOIN files_qcstatus AS fq ON fq.SeriesUID=pf.Value where pt.Name='series_instance_uid' GROUP BY SeriesUID;
+
+UPDATE files_qcstatus AS fq, FileIDs AS F SET fq.FileID=F.FileID where fq.SeriesUID=F.SeriesUID; 
+
+DROP TEMPORARY TABLE FileIDs;
