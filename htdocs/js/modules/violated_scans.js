8297b5f00f9bba8904c6d3c8d18e02e321c3b586
zia <zia.mohades@gmail.com>
2013-01-04 13:05:16 -0500

    Fixed bugs and also validated the javascript through jslint

--- htdocs/js/modules/violated_scans.js
+++ htdocs/js/modules/violated_scans.js
@@ -1,78 +1,71 @@
-/**
-TODO:
-//Change the width of the td by clicking on it
-1)
-**/
+/*global document: false, $: false, window: false*/
+function change() {
+    "use strict";
+    $('#hide').show();
+    $('#show').hide();
+    $('#tbl').show();
+    $('#show').bind('click', function () {
+        $('#tbl').show('slow', function () {});
+        $('#hide').show();
+        $('#show').hide();
+    });
 
-function change(){
-
-	$('#hide').hide();
-	$('#tbl').hide();
-	/*$('td').filter('[id*=header]').css({
-	backgroundColor: 'green'
-	});*/
-	//to show
-	$('#show').bind('click',function(event){
-		$('#tbl').show('slow',function(){});
-		$('#hide').show();
-		$('#show').hide();
-	}
-	);
-
-	//To hide : table hides...and the show shows...
-	$('#hide').bind('click',function(event){
-		$('#tbl').hide('slow',function(){});
-		$('#show').show();
-		$('#hide').hide();
-	});
-
+    //To hide : table hides...and the show shows...
+    $('#hide').bind('click', function () {
+        $('#tbl').hide('slow', function () {});
+        $('#show').show();
+        $('#hide').hide();
+    });
 }
 
-
 function save() {
+    "use strict";
+    var default_value, id, value;
+    /**To get the default value**/
+    $('.description').click(function (event) {
+        id = event.target.id;
+        default_value = $("#" + id).text();
+    });
 
-	
-	
-	/**To get the default value**/
-	$('.description').click(function(event){
-		id = event.target.id;
-		default_value = $("#" + id) .text();
-	});
-	
-	
-	
-	$('.description').bind('blur',function(event){
-			
-		event.stopImmediatePropagation();
-		id = event.target.id;
-		value = $("#" + id) .text();
-		$('<div></div>').appendTo('body')
-		.html('<div>Are you sure?</div>')
-		.dialog({
-			title: 'Modification', zIndex: 10000, autoOpen: true,
-			width: 'auto', resizable: false,
-			dialogClass:'transparent',
-			position: [800,90], 
-			buttons: {
-				Yes: function () {
-					$.get("UpdateMRIProtocol.php?field_id=" + id + "&field_value=" + value,  function(data) {});
-					$(this).dialog("close");
-				},
-				No: function () {
-					$("#" + id).text(default_value);
-					$(this).dialog("close");
-				}
-			},
-			close: function (event, ui) {
-				$(this).remove();
-			}
-		});
-	})
-};
+    $('.description').bind('blur', function (event) {
+        event.stopImmediatePropagation();
+        id = event.target.id;
+        value = $("#" + id).text();
+        $('<div/>').appendTo('body').
+            css({background : 'black', opacity: '0.9'})
+            .html("<div id ='asdfsaf'>Are you sure?</div>")
+            .dialog({
+                title: 'Modification',
+                zIndex: 10000,
+                autoOpen: true,
+                width: 'auto',
+                resizable: false,
+                //dialogClass:'transparent',
+                position: [800, 120],
+                buttons: {
+                    Yes: function () {
+                        $.get("UpdateMRIProtocol.php?field_id=" + id + "&field_value=" + value, function () {});
+                        $(this).dialog("close");
+                    },
+                    No: function () {
+                        $("#" + id).text(default_value);
+                        $(this).dialog("close");
+                    },
+                    close: function () {
+                        $(this).remove();
+                    }
+                }
+            })
+            .keypress(function (e) {
+                if (e.which === 13) { // Determine if the user pressed the enter button
+                    $(this).blur();
+                }
+            });
+    });
+}
 
-$(function(){
-	change();
-	save();
+$(function () {
+    "use strict";
+    change();
+    save();
 });
-
-
