166bde09bf1833b04a7e303e10f8e23503df6211
zia <zia.mohades@gmail.com>
2013-01-04 13:28:46 -0500

    Fixed a problem with keypress function

--- htdocs/js/modules/violated_scans.js
+++ htdocs/js/modules/violated_scans.js
@@ -55,12 +55,11 @@ function save() {
                         $(this).remove();
                     }
                 }
-            })
-            .keypress(function (e) {
-                if (e.which === 13) { // Determine if the user pressed the enter button
-                    $(this).blur();
-                }
             });
+    }).keypress(function (e) {
+        if (e.which === 13) { // Determine if the user pressed the enter button
+            $(this).blur();
+        }
     });
 }
 
