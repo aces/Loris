3ab443e59fbdefc661b2333986c18492743ee248
Dave MacFarlane <driusan@gmail.com>
2013-01-16 15:52:37 -0500

    Added help tables with stub text to default schema.
    (This work was done by Rathi.)

--- SQL/0000-00-00-schema.sql
+++ SQL/0000-00-00-schema.sql
@@ -1633,3 +1633,30 @@ UNLOCK TABLES;
 
 -- Dump completed on 2012-08-21 16:13:53
 
+--
+-- Table structure for table `parameter_type_override`
+--
+
+-- This table needs to be MyISAM because InnoDB doesn't
+-- support full text indexes
+CREATE TABLE `help` (
+    `helpID` int(10) unsigned NOT NULL AUTO_INCREMENT, 
+    `parentID` int(11) NOT NULL DEFAULT '-1',
+    `hash` varchar(32) DEFAULT NULL,
+    `topic` varchar(100) NOT NULL DEFAULT '',
+    `content` text NOT NULL,
+    `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `updated` datetime DEFAULT NULL, PRIMARY KEY (`helpID`), 
+    UNIQUE KEY `hash` (`hash`), 
+    FULLTEXT KEY `topic` (`topic`), 
+    FULLTEXT KEY `content` (`content`)
+) ENGINE=MYISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `help_related_links` (
+    `helpID` int(10) unsigned NOT NULL DEFAULT '0',
+    `relatedID` int(10) unsigned NOT NULL DEFAULT '0', 
+    PRIMARY KEY (`helpID`,`relatedID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO help (helpID, topic, content) VALUES ('1','LORIS HELP: Using the Database','Welcome to LORIS database. The help section provides you with guidelines for adding and updating information in the database'), ('2','HOW TO - Guide','Under Construction.Please visit us later'), ('3','Guidelines','Under Construction.Please visit us later'), ('5','Instruments - Guide','Under Construction.Please visit us later');
+
