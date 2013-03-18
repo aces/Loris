29ea71bdd0cf2652c85f1b4d9a4dc64d990f038a
Rathi Sekaran <sekaranrathi@gmail.com>
2013-03-18 12:29:42 -0400

    SQL script to update Visit_Windows table

---
+++ SQL/2013-03-18-Visit_Windows_update.sql
@@ -0,0 +1,3 @@
+UPDATE Visit_Windows SET Visit_label='v03' WHERE Visit_label='V03';
+UPDATE Visit_Windows SET Visit_label='v09' WHERE Visit_label='V09';
+UPDATE Visit_Windows SET Visit_label='v15' WHERE Visit_label='V15';
