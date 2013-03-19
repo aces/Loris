c6f8211307b9cf636db99841d7fbd91926171018
Rathi Sekaran <sekaranrathi@gmail.com>
2013-03-19 13:58:52 -0400

    removing sql script

--- SQL/2013-03-18-Visit_Windows_update.sql
+++
@@ -1,3 +0,0 @@
-UPDATE Visit_Windows SET Visit_label='v03' WHERE Visit_label='V03';
-UPDATE Visit_Windows SET Visit_label='v09' WHERE Visit_label='V09';
-UPDATE Visit_Windows SET Visit_label='v15' WHERE Visit_label='V15';
