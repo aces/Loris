18946bd86545a719c6804cabd0747848525864c7
zia <zia.mohades@gmail.com>
2013-03-27 14:49:55 -0400

    Cleaned up the table schema

--- SQL/2013-03-22-participant_status.sql
+++ SQL/2013-03-22-participant_status.sql
@@ -4,7 +4,7 @@ CandID int(6) NOT NULL default '0',
 UserID varchar(255) default NULL,
 Examiner varchar(255) default NULL,
 entry_staff varchar(255) default NULL,
-data_entry_date timestamp(14) NOT NULL,
+data_entry_date timestamp NOT NULL,
 qn_2_participant_status enum('active','ineligible','dropout','decline') default NULL,
 qn_3_dropout_ineligible_timing enum('1_before_visit','2_during_visit','3_after_visit','dnk','not_applicable','refusal','not_answered') default NULL,
 qn_4_others_specify_text text default NULL,
@@ -12,9 +12,8 @@ qn_4_others_specify_text_not_answered enum('dnk','not_applicable','refusal','not
 qn_5_drop_out_reasons enum('1_voluntary_withdrawal','2_lost_follow_up','3_others') default NULL,
 qn_5_others_specify_text text default NULL,
 qn_5_others_specify_text_not_answered enum('dnk','not_applicable','refusal','not_answered') default NULL,
+`other_reasons` text,
+`other_reasons_not_answered` enum('dnk','not_applicable','refusal','not_answered') DEFAULT NULL,
  PRIMARY KEY  (ID),
   UNIQUE KEY ID (ID) );
-
-
-
 
