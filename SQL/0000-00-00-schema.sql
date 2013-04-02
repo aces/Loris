ea0bd9f03c8de52df59b9cbfc709757d8c2a5e91
Rathi Sekaran <sekaranrathi@gmail.com>
2013-04-02 15:04:49 -0400

    removing extra comments around the schema

--- SQL/0000-00-00-schema.sql
+++ SQL/0000-00-00-schema.sql
@@ -1777,3 +1777,14 @@ CREATE TABLE `certification` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `certification`
+--
+
+LOCK TABLES `certification` WRITE;
+/*!40000 ALTER TABLE `certification` DISABLE KEYS */;
+/*!40000 ALTER TABLE `certification` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+
