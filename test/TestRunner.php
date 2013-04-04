ff0b45368ad3a44bcd19f38a9fd8f424ab46098c
Dave MacFarlane <driusan@gmail.com>
2013-04-04 13:33:02 -0400

    Added candidate_list tests to TestRunner

--- test/TestRunner.php
+++ test/TestRunner.php
@@ -2,20 +2,16 @@
 <?php
 require_once("simpletest/web_tester.php");
 require_once('simpletest/reporter.php');
-require_once("xmltime.php");
         
 set_include_path(get_include_path().":../project/libraries:../php/libraries:");
 $Reporter = new TextReporter();
-if($argv[$argc-1] == '-xml') {
-    $Reporter = new XMLTimeReporter();
-}
-$test = &new TestSuite('Core LORIS tests');
+$test = new TestSuite('Core LORIS tests');
 // Generally test functionality. Database class, permissions,
 // instruments save
 $test->addFile('tests/CoreFunctionality.php');
 // Test individual components
-$test->addFile('tests/CandidateList.php');
 $test->addFile('tests/NewProfile.php');
+$test->addFile('tests/CandidateList.php');
 // Project specific tests..
 $test->addFile('tests/SiteSpecific.php');
 exit ($test->run($Reporter) ? 0 : 1);
--- test/tests/CandidateList.php
+++ test/tests/CandidateList.php
@@ -1,32 +1,96 @@
 <?php
 require_once 'LorisTest.php';
 
-class TestOfCandidateList extends LorisTest {
+class TestOfCandidateList extends LorisTest
+{
 
-    function testCLLoggedOut() {
+    /**
+     * Test that the page is not accessible when not logged in
+     *
+     * @return null
+     */
+    function testCLLoggedOut()
+    {
         $this->get($this->url . "/main.php?logout=true");
         $this->get($this->url . "/main.php?test_name=candidate_list");
         // Returns 200, but with the login page if not logged in
-        //$this->assertResponse(403, "Could access new_profile while logged out");
         $this->assertPattern("/Password/", "Could access candidate_list logged out");
     }
 
-    function testCLGetPage() {
+    /**
+     * Test that the page, when accessed through GET with no parameters
+     * appears to have the right structure.
+     *
+     * @return null
+     */
+    function testCLGetPage()
+    {
+        $this->login("UnitTester", "4test4");
+        $this->get($this->url . "/main.php?test_name=candidate_list");
+        $this->assertResponse(200, "Could not access candidate_list as UnitTester");
+        $this->assertPattern(
+            "/Access Profile/", "Page does not appear to have Access Profile header"
+        );
+    }
+
+    /**
+     * Test various aspects of posting to the page while logged in.
+     * In particular, ensure that:
+     *  1. The page loads
+     *  2. Results are returned when the filters are empty
+     *  3. No results are returned when the filters have junk data
+     *  4. Results are returned when the filter has valid data.
+     *
+     *  Right now, only the DCCID filter is tested.
+     *
+     *  @return null
+     */
+    function testCLPostPage()
+    {
         $this->login("UnitTester", "4test4");
         $this->get($this->url . "/main.php?test_name=candidate_list");
         $this->assertBasicConditions();
-        $this->assertResponse(200, "Could not access candidate_list as UnitTester");
-        $this->assertPattern("/Access Profile/", "Page does not appear to have Access Profile header");
-        $this->assertNoPattern("/Password/", "lolwhat");
+        $this->assertPattern(
+            "/Open Profile/", "Open profile header appears to be missing"
+        );
+        $PostArray = array(
+            'test_name'           => 'candidate_list',
+            'centerID'            => '',
+            'SubprojectID'        => '',
+            'DCCID'               => '',
+            'PSCID'               => '',
+            'Gender'              => '',
+            'DoB'                 => '',
+            'Latest_Visit_Status' => '',
+            'Visit_Count'         => '',
+            'Feedback'            => '',
+            'ProjectID'           => '',
+            'edc'                 => '',
+            'filter'              => 'Show Data'
+        );
+        $this->post($this->url . '/main.php', $PostArray);
+        $this->assertBasicConditions();
+        $this->assertNoPattern(
+            "/No candidates found/", "No candidates found with empty filter"
+        );
+
+        $PostArray['DCCID'] = $this->CandID;
+        $this->post($this->url . '/main.php', $PostArray);
+        $this->assertBasicConditions();
+        $this->assertNoPattern(
+            "/No candidates found/",
+            "No candidates when searching candidate_list for $this->CandID"
+        );
+        $this->assertPattern(
+            "/$this->CandID/", "Did not find $this->CandID in results"
+        );
+
+        $PostArray['DCCID'] = "IAmInvalid";
+        $this->post($this->url . '/main.php?test_name=candidate_list', $PostArray);
+        $this->assertBasicConditions();
+        $this->assertPattern(
+            "/No candidates found/", "Returned results with invalid DCCID in filter"
+        );
     }
-
-    /*
-    function testPostPage() {
-        $this->login("UnitTester", "4test4");
-        // If any of these are buggy, data could be created..
-        if($this->RunDangerousTests) {
-        }
-    }*/
-
 }
 ?>
