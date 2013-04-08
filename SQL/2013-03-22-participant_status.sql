5c4393a051a53eb2694814af0839876928f72592
zia <zia.mohades@gmail.com>
2013-04-08 13:52:52 -0400

    Changed the _not_answered to _status

--- SQL/2013-03-22-participant_status.sql
+++ SQL/2013-03-22-participant_status.sql
@@ -7,12 +7,12 @@ CREATE TABLE participant_status (
         data_entry_date timestamp NOT NULL,
         participant_status int(6) default NULL,
         ineligible_others_specify text default NULL,
-        ineligible_others_specify_not_answered enum('dnk','not_applicable','refusal','not_answered') default NULL,
+        ineligible_others_specify_status enum('dnk','not_applicable','refusal','not_answered') default NULL,
         drop_out_reasons enum('1_voluntary_withdrawal','2_lost_follow_up','3_others') default NULL,
         drop_out_reasons_others_specify text default NULL,
-        drop_out_reasons_others_specify_not_answered enum('dnk','not_applicable','refusal','not_answered') default NULL,
+        drop_out_reasons_others_specify_status enum('dnk','not_applicable','refusal','not_answered') default NULL,
         `other_reasons` text,
-        `other_reasons_not_answered` enum('dnk','not_applicable','refusal','not_answered') DEFAULT NULL,
+        `other_reasons_status` enum('dnk','not_applicable','refusal','not_answered') DEFAULT NULL,
         PRIMARY KEY  (ID),
         UNIQUE KEY ID (ID) );
 
