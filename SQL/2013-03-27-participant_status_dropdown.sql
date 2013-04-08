9b8ba41930d2e921d44ccf8874ac22a3f24cf0bc
zia <zia.mohades@gmail.com>
2013-04-08 11:13:13 -0400

    Changed indentation

--- SQL/2013-03-27-participant_status_dropdown.sql
+++ SQL/2013-03-27-participant_status_dropdown.sql
@@ -1,9 +1,9 @@
 CREATE TABLE participant_status_options (
-ID int(10) unsigned NOT NULL auto_increment,
-Description varchar(255) default NULL,
- PRIMARY KEY  (ID),
-  UNIQUE KEY ID (ID) );
- 
+        ID int(10) unsigned NOT NULL auto_increment,
+        Description varchar(255) default NULL,
+        PRIMARY KEY  (ID),
+        UNIQUE KEY ID (ID) );
+
 
 INSERT INTO participant_status_options (Description) VALUES ('active');
 INSERT INTO participant_status_options (Description) VALUES ('ineligible');
