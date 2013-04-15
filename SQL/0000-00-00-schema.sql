4cd7b1bbb23a5977296a7f5bb6ec50269c321086
Dave MacFarlane <driusan@gmail.com>
2013-04-15 12:47:02 -0400

    Updated dummy schema.

--- SQL/0000-00-00-schema.sql
+++ SQL/0000-00-00-schema.sql
@@ -1667,31 +1667,30 @@ CREATE TABLE `help_related_links` (
 INSERT INTO help (helpID, topic, content) VALUES ('1','LORIS HELP: Using the Database','Welcome to LORIS database. The help section provides you with guidelines for adding and updating information in the database'), ('2','HOW TO - Guide','Under Construction.Please visit us later'), ('3','Guidelines','Under Construction.Please visit us later'), ('5','Instruments - Guide','Under Construction.Please visit us later');
 
 CREATE TABLE participant_status (
-ID int(10) unsigned NOT NULL auto_increment,
-CandID int(6) UNIQUE NOT NULL default '0',
-UserID varchar(255) default NULL,
-Examiner varchar(255) default NULL,
-entry_staff varchar(255) default NULL,
-data_entry_date timestamp NOT NULL,
-participant_status int(6) default NULL,
-ineligible_others_specify text default NULL,
-ineligible_others_specify_status enum('dnk','not_applicable','refusal','not_answered') default NULL,
-drop_out_reasons enum('1_voluntary_withdrawal','2_lost_follow_up','3_others') default NULL,
-drop_out_reasons_others_specify text default NULL,
-drop_out_reasons_others_specify_status enum('dnk','not_applicable','refusal','not_answered') default NULL,
-`other_reasons` text,
-`other_reasons_status` enum('dnk','not_applicable','refusal','not_answered') DEFAULT NULL,
- PRIMARY KEY  (ID),
-  UNIQUE KEY ID (ID) );
-
+        ID int(10) unsigned NOT NULL auto_increment,
+        CandID int(6) UNIQUE NOT NULL default '0',
+        UserID varchar(255) default NULL,
+        Examiner varchar(255) default NULL,
+        entry_staff varchar(255) default NULL,
+        data_entry_date timestamp NOT NULL,
+        participant_status integer DEFAULT NULL REFERENCES participant_status_options(ID),
+        reason_specify text default NULL,
+        reason_specify_status enum('dnk','not_applicable','refusal','not_answered') default NULL,
+        withdrawal_reasons enum('1_voluntary_withdrawal','2_lost_follow_up','3_other') default NULL,
+        withdrawal_reasons_other_specify text default NULL,
+        withdrawal_reasons_other_specify_status enum('dnk','not_applicable','refusal','not_answered') default NULL,
+        PRIMARY KEY  (ID),
+        UNIQUE KEY ID (ID) 
+);
 
 CREATE TABLE participant_status_options (
-ID int(10) unsigned NOT NULL auto_increment,
-Description varchar(255) default NULL,
- PRIMARY KEY  (ID),
-  UNIQUE KEY ID (ID) );
+        ID int(10) unsigned NOT NULL auto_increment,
+        Description varchar(255) default NULL,
+        Required boolean default NULL,
+        PRIMARY KEY  (ID),
+        UNIQUE KEY ID (ID) 
+);
 
 
-INSERT INTO participant_status_options (Description) VALUES ('active'), ('ineligible'), ('dropout'), ('death'),('others');
-~                                                                       
+INSERT INTO participant_status_options (Description, Required) VALUES ('active', false), ('ineligible', true), ('withdrawal', true), ('death', false), ('other', true);
 
