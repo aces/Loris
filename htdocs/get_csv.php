c149cf6d9f6739279a1f5e1445e6270f103fdd4c
zia <zia.mohades@gmail.com>
2013-04-22 15:56:12 -0400

    improved the code and ran it through phpcs

--- htdocs/get_csv.php
+++ htdocs/get_csv.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * This script download the data for the given instrument column
+ * And it is used by Data_team_helper
+ * 
+ * PHP Version 5
+ *
+ *  @category Loris
+ *  @package  Behavioural
+ *  @author   Zia Mohaddes <zia.mohades@gmail.com>
+ *  @license  Loris license
+ *  @link     https://github.com/aces/Loris-Trunk
+ *
+ */
 require_once "../tools/generic_includes.php";
 //fputcsv($output, array('Column 1', 'Column 2', 'Column 3'));
 header('Content-Type: text/csv; charset=utf-8');
@@ -10,30 +23,47 @@ $completion_status = $_REQUEST['completion_status'];
 $sent_to_dcc_status =  $_REQUEST['sent_to_dcc_status'];
 
 
+// Ensures the user is logged in, and parses the config file.
+require_once "NDB_Client.class.inc";
+$client = new NDB_Client();
+$client->initialize("../project/config.xml");
+
 
 /////////////////////////////////////////////////////////////////////
 /////////////////////////////Instrument Data Download/////////////////////////
 /////////////////////////////////////////////////////////////////////////
+if (empty($instrument) || empty($sourcefield)) {
+    error_log("ERROR: The neccessary parameters are missing");
+    header("HTTP/1.1 500 Internal Server Error"); 
+    exit(1);
+}
 
-$query = "Select c.PSCID, s.Visit_label, $sourcefield as Field,f.data_entry from $instrument i , flag f,session s,candidate c
-where i.commentid = f.commentid and f.sessionid = s.ID and s.Candid = c.Candid";
+$params = array();
+
+$query = "SELECT c.PSCID, s.Visit_label, `$sourcefield` AS Field,f.data_entry FROM
+          `$instrument` i 
+          JOIN flag f ON (i.commentid = f.commentid)
+          JOIN session s on (s.ID=f.sessionid)
+          JOIN candidate c on (c.CandID = s.CandID)
+          WHERE 1=1 ";
+
 if (($completion_status!='any') && (!empty($completion_status))) {
-	$query .= " And f.data_entry = '$completion_status'";
+
+    $query .= " AND f.data_entry = :cstatus ";
+    $params['cstatus'] = $completion_status;
 }
-if (($sent_to_dcc_status!='any') && (!empty($sent_to_dcc_status))){
-	$query .= " And s.current_stage = '$sent_to_dcc_status'";
+if (($sent_to_dcc_status!='any') && (!empty($sent_to_dcc_status))) {
+    $query .= " AND s.Current_stage = :dcc_status";
+    $params['dcc_status'] = $sent_to_dcc_status;
 }
 
-$header = array("PSCID","Visit_lable",$sourcefield,"data_entry");
+$header = array("PSCID", "Visit_label", $sourcefield, "data_entry");
 
-$DB->select($query,$results);
+$results = $DB->pselect($query, $params);
 
-
-fputcsv($output,$header,"\t");
+fputcsv($output, $header, "\t");
 ///Create the csv file
-foreach($results as $result){
-	fputcsv($output,$result,"\t");
+foreach ($results as $result) {
+    fputcsv($output, $result, "\t");
 }
-
-
 ?>
