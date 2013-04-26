f261e964dbfb402837e3a9cd43de74df33689d45
Dave MacFarlane <driusan@gmail.com>
2013-04-26 11:25:46 -0400

    Fixed somewarnigns in CouchDB Wrapper test

--- test/tests/CouchDBWrapper.php
+++ test/tests/CouchDBWrapper.php
@@ -12,7 +12,7 @@ class TestOfCouchDBWrapper extends UnitTestCase {
     }
     function testSingleton() {
         $db = CouchDB::singleton();
-        $this->assertIsA($db, CouchDB);
+        $this->assertIsA($db, 'CouchDB');
 
         $db2 = CouchDB::singleton();
 
@@ -103,6 +103,7 @@ class TestOfCouchDBWrapper extends UnitTestCase {
     function testDeleteDoc() {
         $Mock = new MockCouchDBWrap();
         $Mock->returns("_getRelativeURL", '{"ok":true,"id":"Demographics_Session_UNC0219_V06","rev":"2-c78967e246008b55daed336e61cb8342"}');
+        $id = 'Demographics_Session_UNC0219_V06';
         $result = $Mock->deleteDoc($id);
         $this->assertTrue($result);
 
