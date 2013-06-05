f1a634b207df159b8c848cee55304ef5939b60c3
Martin Weiss <martinwork527@gmail.com>
2013-06-05 09:54:26 -0400

    working on reliability statistics tab

--- htdocs/js/modules/form_stats_reliability.js
+++ htdocs/js/modules/form_stats_reliability.js
@@ -1,7 +1,7 @@
-        function updateSite() {
-            var site = document.getElementById("site");
+        function updateReliabilityTab() {
+            var ReliabilitySite = document.getElementById("ReliabilitySite");
             var request = $.ajax({
-                url: '/main.php?test_name=statistics&subtest=stats_reliability&dynamictabs=dynamictabs&site=' + site.value,
+                url: '/main.php?test_name=statistics&subtest=stats_reliability&dynamictabs=dynamictabs&ReliabilitySite=' + ReliabilitySite.value,
                 type: 'GET',
                 data: 'html',
                 success: function(response, textStatus, jqXHR)
