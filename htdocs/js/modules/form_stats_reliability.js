086d630a8a42780b0df6284a352c75c010beb080
Martin Weiss <martinwork527@gmail.com>
2013-06-05 10:37:50 -0400

    still working on reliability tabs

--- htdocs/js/modules/form_stats_reliability.js
+++ htdocs/js/modules/form_stats_reliability.js
@@ -1,13 +1,14 @@
-        function updateReliabilityTab() {
-            var ReliabilitySite = document.getElementById("ReliabilitySite");
-            var request = $.ajax({
-                url: '/main.php?test_name=statistics&subtest=stats_reliability&dynamictabs=dynamictabs&ReliabilitySite=' + ReliabilitySite.value,
-                type: 'GET',
-                data: 'html',
-                success: function(response, textStatus, jqXHR)
-                {
-                    $('#reliability').html(response);
-                }
-            });
+function updateReliabilityTab() 
+    {
+    var ReliabilitySite = document.getElementById("ReliabilitySite");
+    var request = $.ajax({
+        url: '/main.php?test_name=statistics&subtest=stats_reliability&dynamictabs=dynamictabs&ReliabilitySite=' + ReliabilitySite.value,
+        type: 'GET',
+        data: 'html',
+        success: function(response, textStatus, jqXHR)
+        {
+            $('#reliability').html(response);
         }
+    });
+}
 
