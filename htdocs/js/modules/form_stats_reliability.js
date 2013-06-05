a81e6256d7c8c781d0b41d2ba13dfe4fad71b803
Martin Weiss <martinwork527@gmail.com>
2013-06-05 16:24:50 -0400

    fixed bugs in JS

--- htdocs/js/modules/form_stats_reliability.js
+++ htdocs/js/modules/form_stats_reliability.js
@@ -1,12 +1,11 @@
-function updateReliabilityTab() 
-    {
+/*global document, $*/
+function updateReliabilityTab() {
     var ReliabilitySite = document.getElementById("ReliabilitySite");
     var request = $.ajax({
         url: '/main.php?test_name=statistics&subtest=stats_reliability&dynamictabs=dynamictabs&ReliabilitySite=' + ReliabilitySite.value,
         type: 'GET',
         data: 'html',
-        success: function(response, textStatus, jqXHR)
-        {
+        success: function(response) {
             $('#reliability').html(response);
         }
     });
