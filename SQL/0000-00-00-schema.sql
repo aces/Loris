e390946ff234c4b856ae34a5c920c80e4f45b25c
zia <zia.mohades@gmail.com>
2013-07-10 15:08:25 -0400

    Include the table structure for the table user_login_history

--- SQL/0000-00-00-schema.sql
+++ SQL/0000-00-00-schema.sql
@@ -1824,24 +1824,22 @@ CREATE TABLE `project_rel` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
+--
+-- Table structure for table `user_login_history`
+--
 
-
-DROP TABLE IF EXISTS `user_account_history`;
+DROP TABLE IF EXISTS `user_login_history`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user_account_history` (
-ID int(10) unsigned NOT NULL AUTO_INCREMENT,
-UserID varchar(255) NOT NULL DEFAULT '',
-PermID int(10) unsigned DEFAULT NULL,
-PermAction enum('I','D') DEFAULT NULL,
-DataField  varchar(255) DEFAULT NULL,
-DataFieldValueOld varchar(255) DEFAULT NULL,
-DataFieldValueNew varchar(255) DEFAULT NULL,
-ChangeDate timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
- PRIMARY KEY (`ID`)
+CREATE TABLE `user_login_history` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `UserID` varchar(255) NOT NULL DEFAULT '',
+  `Success` enum('Y','N') NOT NULL DEFAULT 'Y',
+  `Failcode` varchar(2) DEFAULT NULL,
+  `Login_timestamp` date NOT NULL DEFAULT '0000-00-00',
+  `IP_address` varchar(255) DEFAULT NULL,
+  `Page_requested` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=92 DEFAULT CHARSET=latin1;
-
-
-
-
+/*!40101 SET character_set_client = @saved_cs_client */;
 
