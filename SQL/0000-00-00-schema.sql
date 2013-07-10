17ff791a24b0877d9f3d5354504728b2c68ec4a7
zia <zia.mohades@gmail.com>
2013-07-10 10:44:20 -0400

    included the table structure for user_account_history for both new and existing database

--- SQL/0000-00-00-schema.sql
+++ SQL/0000-00-00-schema.sql
@@ -1824,3 +1824,24 @@ CREATE TABLE `project_rel` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
+
+
+DROP TABLE IF EXISTS `user_account_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_account_history` (
+ID int(10) unsigned NOT NULL AUTO_INCREMENT,
+UserID varchar(255) NOT NULL DEFAULT '',
+PermID int(10) unsigned DEFAULT NULL,
+PermAction enum('I','D') DEFAULT NULL,
+DataField  varchar(255) DEFAULT NULL,
+DataFieldValueOld varchar(255) DEFAULT NULL,
+DataFieldValueNew varchar(255) DEFAULT NULL,
+ChangeDate timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+ PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=92 DEFAULT CHARSET=latin1;
+
+
+
+
+
