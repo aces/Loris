6a520ad2dee065bc63fbdfcaf03e828771f4eb16
Martin Weiss <martinwork527@gmail.com>
2013-08-23 11:58:19 -0400

    formatting change

--- htdocs/js/modules/form_stats_demographic.js
+++ htdocs/js/modules/form_stats_demographic.js
@@ -3,7 +3,7 @@ function updateDemographicTab() {
     var DemographicSite = document.getElementById("DemographicSite");
     var DemographicProject = document.getElementById("DemographicProject");
     var request = $.ajax({
-         url: '/main.php?test_name=statistics&subtest=stats_demographic&dynamictabs=dynamictabs&DemographicSite=' + DemographicSite.value + '&DemographicProject=' + DemographicProject.value,
+        url: '/main.php?test_name=statistics&subtest=stats_demographic&dynamictabs=dynamictabs&DemographicSite=' + DemographicSite.value + '&DemographicProject=' + DemographicProject.value,
         type: 'GET',
         data: 'html',
         success: function(response) {
