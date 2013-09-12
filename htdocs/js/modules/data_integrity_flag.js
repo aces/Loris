5c97062355b2cb4fc8a1ff990244c22af2018a29
zia <zia.mohades@gmail.com>
2013-09-12 17:01:21 -0400

    Contains specific gusto-specific functionalities

--- htdocs/js/modules/data_integrity_flag.js
+++ htdocs/js/modules/data_integrity_flag.js
@@ -1,62 +1,87 @@
-/*global document: false, $: false, window: false, unescape: false, Option: false*/
-
 function getQueryVariable(variable) {
-    "use strict";
-    var query = window.location.search.substring(1),
-        vars = query.split("&"),
-        i,
-        pair;
-    for (i = 0; i < vars.length; i += 1) {
-        pair = vars[i].split("=");
-        if (pair[0] === variable) {
-            return unescape(pair[1]);
-        }
-    }
+	var query = window.location.search.substring(1);
+	var vars = query.split("&");
+	for (var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split("=");
+		if (pair[0] == variable) {
+			return unescape(pair[1]);
+		}
+	}
+}
+
+function changefieldOptions() {
+	changeVisitLabels();
 }
 
 function changeVisitLabels() {
-    "use strict";
-    //get the value for the visit selected
-    var instrument_dropdown = document.getElementById('instrument'),
-        visit_label_dropdown = document.getElementById('visit_label'),
-        visit_label_value = visit_label_dropdown.value,
-        request = getQueryVariable("visit_label"),
-        instrument_dropdown_value,
-        temp_array = ["All Instruments"],
-        instruments;
-    if ($.trim(visit_label_value) === 'All Visits') {visit_label_value = ''; }
-    if (request !== undefined) {
-        request = request.replace(/\+/g, ' ');
-    }
-    instrument_dropdown_value = getQueryVariable("instrument");
-    if (instrument_dropdown_value !== undefined) {
-        instrument_dropdown_value = instrument_dropdown_value.replace(/\+/g, ' ');
-    }
-    $.get("GetInstruments.php?visit_label=" + visit_label_value,
-        function (data) {
-            instruments = data.split("\n");
-            instruments = temp_array.concat(instruments); //adds 'All instruments to the array'
-            instrument_dropdown.options.length = 0;
-            var i, numInstruments = instruments.length, val;
-            for (i = 0; i < numInstruments; i += 1) {
-                val = instruments[i];
-                if (val !== '') {
-                    instrument_dropdown.options[i] = new Option(val, val);
-                    if ((instrument_dropdown_value === val) && (instrument_dropdown_value !== '')) {
-                        instrument_dropdown.options[i].selected = "selected";
-                    }
-                }
-            }
-            //jQuery('#visits').change();
-        });
+
+	//get the value for the visit selected
+	var instrument_dropdown = document.getElementById('instrument');
+	var visit_label_dropdown = document.getElementById('visit_label');
+	var visit_label_value = visit_label_dropdown.value;
+	request = getQueryVariable("visit_label");
+	if (request != undefined) {
+		request = request.replace(/\+/g,' ');
+	}
+	var instrument_dropdown_value = getQueryVariable("instrument");
+	if (instrument_dropdown_value != undefined) {
+		instrument_dropdown_value = instrument_dropdown_value.replace(/\+/g,' ');
+	}
+	
+	$.get("GetInstruments.php?visit_label=" + visit_label_value ,
+	function(data){
+	
+		instruments = data.split("\n");
+		instrument_dropdown.options.length = 0;
+		
+		var i, numInstruments = instruments.length, val, selected = false;
+		for (i=0; i<numInstruments;i++){
+			val = instruments[i];
+			if(val !=''){
+				instrument_dropdown.options[i] = new Option(val,val);
+				if ((instrument_dropdown_value == val) && (instrument_dropdown_value !='')){
+					instrument_dropdown.options[i].selected = "selected";
+				}
+			}
+		}
+
+		///Removes the space and replaces the instrument by a space
+		$("#instrument option").each(function(n){
+			if (this.value == '') {
+			     this.remove();
+			}
+			else if (this.value.match(/Instruments/g)) {
+			     $(this).text('');
+			}
+		});
+		
+	});
 }
 
 
 //runs the function when the page is loaded..
-$(function () {
-    "use strict";
-    changeVisitLabels();
-    $('#instrument,#visit_label,#users').bind('change', function () { $("#filter").trigger('click'); }); //The form is automatically loaded when the instrument dropdown is changed
-    $('#update_data').bind('change', function () { $("#filter").trigger('click'); }); //The form is automatically loaded when the dropdown is changed
-});
+$(function(){
+	
+	changefieldOptions();
 
+	$('#instrument').bind('change',function(event){$("#filter").trigger('click');}); //The form is automatically loaded when the dropdown is changed
+
+	 save();
+	});
+	
+	
+	
+function save() {
+    "use strict";
+    var default_value, id, value;
+    /**To get the default value**/
+    
+    $('.comment').blur(function (event) {
+         id = event.target.id;
+         value = $("#" + id).text();
+         $.get("UpdateDiComments.php?id=" + id + "&value=" + value , function () {});
+    }).keypress(function (e) {
+        if (e.which === 13) { // Determine if the user pressed the enter button
+            $(this).blur();
+        }
+})};
