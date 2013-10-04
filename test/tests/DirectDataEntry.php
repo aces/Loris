509f2db5fa3a7a8e85b4c29874b1b89f005441a1
Dave MacFarlane <driusan@gmail.com>
2013-10-04 10:24:24 -0400

    added Tests and direct pass at direct entry page

---
+++ test/tests/DirectDataEntry.php
@@ -0,0 +1,24 @@
+<?php
+require_once __DIR__ . '/../test_includes.php';
+require_once __DIR__ . '/../../htdocs/submit.php';
+
+Mock::generate('Database');
+Mock::generatePartial('DirectDataEntryMainPage', 'DirectDataEntryMainPageRunPartial', array('initialize', 'display'));
+
+class TestOfDirectDataEntry extends UnitTestCase {
+    function setUp() {
+        //$this->config = NDB_Config::singleton();
+    }
+
+    function tearDown() {
+    }
+
+
+    function testDirectDataEntryMain() {
+        $Page = new DirectDataEntryMainPageRunPartial();
+        $Page->expectOnce('initialize');
+        $Page->expectOnce('display');
+        $Page->run();
+    }
+}
+?>
