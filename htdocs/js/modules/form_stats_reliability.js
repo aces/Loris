dcbac2e460e1156ce865643ed3fd32b63b4312c8
Rathi Sekaran <sekaranrathi@gmail.com>
2013-10-08 02:39:44 -0400

    updating files to add project filter in statistics

--- htdocs/js/modules/form_stats_reliability.js
+++ htdocs/js/modules/form_stats_reliability.js
@@ -1,8 +1,9 @@
 /*global document, $*/
 function updateReliabilityTab() {
     var ReliabilitySite = document.getElementById("ReliabilitySite");
+    var ReliabilityProject = document.getElementById("ReliabilityProject");
     var request = $.ajax({
-        url: '/main.php?test_name=statistics&subtest=stats_reliability&dynamictabs=dynamictabs&ReliabilitySite=' + ReliabilitySite.value,
+        url: '/main.php?test_name=statistics&subtest=stats_reliability&dynamictabs=dynamictabs&ReliabilitySite=' + ReliabilitySite.value + '&ReliabilityProject=' + ReliabilityProject.value,
         type: 'GET',
         data: 'html',
         success: function(response) {
