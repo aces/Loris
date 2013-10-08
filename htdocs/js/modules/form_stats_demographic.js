beac337cdacc3e815e6185c841e2aed3cc3e4071
Dave MacFarlane <driusan@gmail.com>
2013-10-08 14:20:31 -0400

    Added project to demographics

--- htdocs/js/modules/form_stats_demographic.js
+++ htdocs/js/modules/form_stats_demographic.js
@@ -1,12 +1,14 @@
 /*global document, $*/
 function updateDemographicTab() {
-    var DemographicSite = document.getElementById("DemographicSite");
-    var request = $.ajax({
-        url: '/main.php?test_name=statistics&subtest=stats_demographic&dynamictabs=dynamictabs&DemographicSite=' + DemographicSite.value,
+    var DemographicSite = document.getElementById("DemographicSite"),
+        DemographicProject = document.getElementById("DemographicProject"),
+        request = $.ajax({
+            url: '/main.php?test_name=statistics&subtest=stats_demographic&dynamictabs=dynamictabs&DemographicSite=' + DemographicSite.value + '&DemographicProject=' + DemographicProject.value,
         type: 'GET',
         data: 'html',
         success: function(response) {
             $('#demographics').html(response);
         }
-    });
+        }
+    );
 }
