6466851434a471b36f6d6701cb569af9ad98e92d
Rathi Sekaran <sekaranrathi@gmail.com>
2013-10-28 20:15:26 -0400

    updating code to display default options for candidate

--- htdocs/GetParticipant_suboptions.php
+++ htdocs/GetParticipant_suboptions.php
@@ -22,21 +22,19 @@
           print $row['Description'] . "\n";
       }
   } else if(isset($_REQUEST['pscid']))  {
-      print "yyyY";
        $results = $DB->pselect("SELECT participant_statusID, participant_subOptions from participant_status p JOIN 
                               candidate c on c.CandID = p.CandID WHERE c.PSCID='".$_REQUEST['pscid']."'");
                             //  array('pid'=>"'".$_REQUEST['pscid']."'"));    
        foreach($results as $row){
            $id = $row['participant_statusID'];
-           $sub_id = $row['participant_subOptions'];
+          // $sub_id = $row['participant_subOptions'];
 
            $desc = $DB->pselectOne("SELECT Description FROM participant_status_options WHERE ID=:id",
                                      array('id'=> $id));
            $sub_desc = $DB->pselectOne("SELECT Description FROM participant_status_options WHERE ID=:sid",
-                                     array('sid'=> $sub_id));
+                                     array('sid'=> $row['participant_subOptions']));
 
-//           print $desc.";".$sub_desc;
-            print $id;$sub_id;
+           print $id.";".$sub_desc;
            
        }
   }
