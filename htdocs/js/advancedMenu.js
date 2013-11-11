2091225a2a07385862e35e79e56f797dbf1ed445
zia <zia.mohades@gmail.com>
2013-11-11 16:59:44 -0500

    Optimized the code

--- htdocs/js/advancedMenu.js
+++ htdocs/js/advancedMenu.js
@@ -1,50 +1,11 @@
 /*global document: false, $: false, window: false, unescape: false, Option: false,isElementsSet*/
 
-function toggleMe(a, b) {
-    "use strict";
-    var set = isElementsSet(),
-        rows = document.getElementsByName(a),
-        selector = document.getElementById(b),
-        i;
-    if (selector.style.display === "none") {
-        selector.style.display = "table-cell";
-    } else {
-        selector.style.display = "none";
-    }
-    for (i = 0; i < rows.length; i += 1) {
-        if (!rows[i]) {
-            return true;
-        }
-        if ((rows[i].style.display === "none") || (set === 1)) {
-            rows[i].style.display = "table-row";
-            $('#selector').css("display", "none");
-        } else {
-            rows[i].style.display = "none";
-        }
-    }
-}
 
-function showAdvancedOptionsCheck() {
-
-    "use strict";
-    var els = $('tr[ID=advancedOptions]'),///get all the TR elements with the ID advancedOptions
-        set = isElementsSet();
-    ///if the set is true, then keep style display as table-row
-    if (set) {
-        els.each(function () {
-            $(this).css("display", "table-row");
-            $('#selector').css("display", "none");
-        });
-    } else {
-        $(this).css("display", "none");
-    }
-}
-
-function isElementsSet() {
+function isElementSet() {
     "use strict";
     var set = 0,
-        options = $('tr[ID=advancedOptions] option:selected'),  ///get all the selected dropdowns for the TR with the ID advancedOptions
-        texts = $('tr[ID=advancedOptions] input[type=text]');
+        options = $('.advancedOptions option:selected'),  ///get all the selected dropdowns for the TR with the ID advancedOptions
+        texts = $('.advancedOptions input[type=text]');
     ///brows through the selected dropdowns
     ///if any of the dropdown is not equal to 'All' then set the variable set to true
     options.each(function () {
@@ -63,7 +24,27 @@ function isElementsSet() {
             return false;
         }
     });
-    return set;
+}
+
+
+function toggleMe() {
+    "use strict";
+    $(".selector").toggle();
+    $(".advancedOptions").toggle();
+}
+
+
+function showAdvancedOptionsCheck() {
+
+    "use strict";
+    var els = $('.advancedOptions'),///get all the TR elements with the ID advancedOptions
+        set = isElementSet();
+    if (set) {
+        els.show();
+        $("#basicSelector").hide();
+    } else {
+        els.hide();
+    }
 }
 
 $(function () {
