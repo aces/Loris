5f8400d3369dba4fb4b60952aae38cdf1175556f
zia <zia.mohades@gmail.com>
2013-12-02 12:57:17 -0500

    Removed the DataField,DataFieldValueOld and DataFieldValueNew column

--- SQL/0000-00-00-schema.sql
+++ SQL/0000-00-00-schema.sql
@@ -1832,9 +1832,6 @@ ID int(10) unsigned NOT NULL AUTO_INCREMENT,
 UserID varchar(255) NOT NULL DEFAULT '',
 PermID int(10) unsigned DEFAULT NULL,
 PermAction enum('I','D') DEFAULT NULL,
-DataField  varchar(255) DEFAULT NULL,
-DataFieldValueOld varchar(255) DEFAULT NULL,
-DataFieldValueNew varchar(255) DEFAULT NULL,
 ChangeDate timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
  PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
