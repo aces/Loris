d7d4fb4a5fa8fb1c9a2434b8369ce3d5c45179e5
zia <zia.mohades@gmail.com>
2013-12-02 11:03:55 -0500

    Removed additional comments

--- SQL/2013-05-15_user_account_history.sql
+++ SQL/2013-05-15_user_account_history.sql
@@ -1,5 +1,3 @@
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_account_history` (
 ID int(10) unsigned NOT NULL AUTO_INCREMENT,
 UserID varchar(255) NOT NULL DEFAULT '',
