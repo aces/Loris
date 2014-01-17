6451a748850e6cf62e1192d52af91dd2543b4ae0
Rathi Sekaran <sekaranrathi@gmail.com>
2014-01-17 12:14:12 -0500

    adding new qc sanity checks - Redmine 3303

--- tools/qc_checks.php
+++ tools/qc_checks.php
@@ -98,4 +98,90 @@ foreach($data as $row) {
     print implode("\t", $row);
     print "\n";
 }
+print "\n\n CANDIDATE THAT HAVE MORE THAN 1 ADOS MODULE ADMINISTRATION = All/Partial \n";
+print "--------------------------------------\n";
+$data = $DB->pselect("SELECT c.PSCID,c.CandID,f1.Administration as A1,f2.Administration as A2,f3.Administration as A3 from candidate c JOIN session s on s.CandID=c.CandID  join flag f1 on ( s.ID=f1.SessionID AND f1.Test_name='ados_module1' AND f1.CommentID NOT LIKE 'DDE%' ) join flag f2 on ( s.ID=f2.SessionID AND f2.Test_name='ados_module2' AND f2.CommentID NOT LIKE 'DDE%' ) join flag f3 on ( s.ID=f3.SessionID AND f3.Test_name='ados_module3' AND f3.CommentID NOT LIKE 'DDE%' ) WHERE s.CenterID IN (2,3,4,5) AND c.Active='Y'", array());
+$fields = array('All', 'Partial');
+
+foreach($data as $row) {
+    if(!empty($row['A1']) || !empty($row['A2']) || !empty($row['A3'])) {
+        if(in_array($row['A1'] , $fields) && (in_array($row['A2'], $fields) || in_array($row['A3'], $fields))) {
+            print implode("\t", $row);
+            print "\n";
+        }
+    }
+
+}
+print "\n\n CANDIDATE THAT HAVE MORE THAN 1 ADOS2 MODULE ADMINISTRATION = All/Partial \n";
+$data = $DB->pselect("SELECT c.PSCID,c.CandID,f1.Administration as A1,f2.Administration as A2,f3.Administration as A3 from candidate c JOIN session s on s.CandID=c.CandID  join flag f1 on ( s.ID=f1.SessionID AND f1.Test_name='ados2_module1' AND f1.CommentID NOT LIKE 'DDE%' ) join flag f2 on ( s.ID=f2.SessionID AND f2.Test_name='ados2_module2' AND f2.CommentID NOT LIKE 'DDE%' ) join flag f3 on ( s.ID=f3.SessionID AND f3.Test_name='ados2_module3' AND f3.CommentID NOT LIKE 'DDE%' ) WHERE s.CenterID IN (2,3,4,5) AND c.Active='Y'", array());
+$fields = array('All', 'Partial');
+
+foreach($data as $row) {
+    if(!empty($row['A1']) || !empty($row['A2']) || !empty($row['A3'])) {
+        if(in_array($row['A1'] , $fields) && (in_array($row['A2'], $fields) || in_array($row['A3'], $fields))) {
+            print implode("\t", $row);
+            print "\n";
+        }
+    }
+}
+print "\n\n FOR MRI PARAMETER FORM HAVE Date_take = Date of MRI = Date of 25dir DTI = Date of Spectroscopy = Date of 65 dir DTI = Date of BOLD";
+print "--------------------------------------\n";
+$data = $DB->pselect("SELECT c.PSCID,c.CandID, s.Visit_label,Date_taken, T1_Scan_done_date, DTI_Scan_done_date, DTI65Dir_Scan_done_date, BOLD_Scan_done_date, Spectroscopy_Scan_done_date from mri_parameter_form m join flag f on f.CommentID = m.CommentID join session s on s.ID = f.SessionID join candidate c on c.CandID = s.CandID WHERE f.CommentID NOT LIKE 'DDE%' AND f.CommentID NOT LIKE '%DCC%' AND Date_taken IS NOT NULL", array());
+$fields = array ('T1_Scan_done_date', 'DTI_Scan_done_date', 'DTI65Dir_Scan_done_date', 'BOLD_Scan_done_date', 'Spectroscopy_Scan_done_date');
+foreach ($data as $row) {
+    foreach($fields as $field) {
+        if(!empty($row[$field])  && $row[$field] == $row['Date_taken']) {
+              print implode("\t", $row);
+              print "\n";
+              break;
+        }
+    }
+}
+$tests = $DB->pselect("SELECT Test_name,Full_name FROM test_names WHERE Test_name NOT LIKE 'csbs_sponteneity' AND Test_name NOT LIKE '' AND Test_name NOT LIKE 'vineland_%'", array()) ;
+/* ************************************************************************* */
+// Determine instruments where Data_entry = Complete and DDE =N
+/* ************************************************************************* */
+
+print "\n\n INSTRUMENTS where Data_entry = Complete and DDE =N \n";
+print "--------------------------------------\n";
+foreach($tests as $test) {
+    $instrument = $test['Test_name'];
+    $data = $DB->pselect("SELECT c.PSCID,c.CandID, s.Visit_label, i.CommentID from $instrument i JOIN flag f ON (f.CommentID=i.CommentID) JOIN session s ON (s.ID=f.SessionID) JOIN candidate c ON (c.CandID=s.CandID) WHERE COALESCE(s.Current_stage, '') <> 'Recycling Bin' AND COALESCE(s.Visit, '') NOT IN ('Failure', 'Withdrawal') AND COALESCE(s.Screening, '') NOT IN ('Failure', 'Withdrawal') AND f.Administration='All' AND f.Data_entry='Complete' AND EXISTS (SELECT 'x' FROM flag df WHERE df.CommentID=CONCAT('DDE_', i.CommentID) AND df.Data_entry !='Complete')", array());
+    if(sizeof($data) > 1 ) {
+        print "\n INSTRUMENT - ".$test['Full_name']."\n";
+    }
+    foreach($data as $row) {
+        print implode("\t", $row);
+        print "\n";
+    }
+}
+/* ************************************************************************* */
+// Determine instruments where Data_entry = Complete and Conflicts exist =Y
+/* ************************************************************************* */
+
+print "\n\n INSTRUMENTS where Data_entry = Complete and Conflicts =Y \n";
+print "--------------------------------------\n";
+foreach($tests as $test) {
+        $instrument = $test['Test_name'];
+        $data = $DB->pselect("SELECT c.PSCID,c.CandID, s.Visit_label, i.CommentID from $instrument i JOIN flag f ON (f.CommentID=i.CommentID) JOIN session s ON (s.ID=f.SessionID) JOIN candidate c ON (c.CandID=s.CandID) WHERE COALESCE(s.Current_stage, '') <> 'Recycling Bin' AND COALESCE(s.Visit, '') NOT IN ('Failure', 'Withdrawal') AND COALESCE(s.Screening, '') NOT IN ('Failure', 'Withdrawal') AND f.Administration='All' AND f.Data_entry='Complete' AND EXISTS (SELECT 'x' FROM flag df WHERE df.CommentID=CONCAT('DDE_', i.CommentID) AND df.Data_entry='Complete') AND EXISTS (SELECT 'x' FROM conflicts_unresolved cu WHERE cu.CommentId1=i.CommentID OR cu.CommentId2=i.CommentID)", array());
+        if(sizeof($data) > 1 ) {
+                print "\n INSTRUMENT - ".$test['Full_name']."\n";
+        }
+        foreach($data as $row) {
+                          print implode("\t", $row);
+                                        print "\n";
+        }
+}
+/* ************************************************************************* */
+// CHECK IF FIGS date = 0002-02-02
+/* ************************************************************************* */
+
+print "\n\n CHECK IF FIGS date = 0002-02-02 \n";
+print "--------------------------------------\n";
+$data = $DB->pselect("SELECT c.PSCID,c.CandID, s.Visit_label, i.CommentID from figs_year3 i JOIN flag f ON (f.CommentID=i.CommentID) JOIN session s ON (s.ID=f.SessionID) JOIN candidate c ON (c.CandID=s.CandID) WHERE COALESCE(s.Current_stage, '') <> 'Recycling Bin' AND COALESCE(s.Visit, '') NOT IN ('Failure', 'Withdrawal') AND COALESCE(s.Screening, '') NOT IN ('Failure', 'Withdrawal') AND f.Administration='All' AND f.Data_entry='Complete' AND i.Date_taken= '0002-02-02'", array());
+foreach($data as $row) {
+    print implode("\t", $row);
+    print "\n";
+}
+
 ?>
