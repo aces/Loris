9bf6d37d8f8acc194214cf8518b1d6e161148b54
Jkat <fission@live.com>
2014-01-30 16:36:52 -0500

    Updating SQL

--- SQL/2014-01-27-data_dict_edit.sql
+++ SQL/2014-01-27-data_dict_edit.sql
@@ -1,1 +1,1 @@
-INSERT INTO permissions VALUES ('','data_dict_edit','Edit permissions for Data Dictionary','permission',2);
+INSERT INTO permissions VALUES ('','data_dict_edit','Edit permissions for Data Dictionary',2);
