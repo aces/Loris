00e2236923eb6709ca7dd78866d36c5e17f4a9d0
zia <zia.mohades@gmail.com>
2014-03-04 13:14:34 -0500

    removed the un-used functions

--- htdocs/js/modules/mri_upload.js
+++ htdocs/js/modules/mri_upload.js
@@ -23,20 +23,6 @@ function change() {
     });
 }
 
-function toggle() {
-    "use strict";
-    $('.advancedOptions').hide();
-    $('#advanced').bind('click', function () {
-        $('#advanced').hide();
-        $('.advancedOptions').show();
-    });
-    //To hide : table hides...and the show shows...
-    $('#basic').bind('click', function () {
-        $('#advanced').show();
-        $('.advancedOptions').hide();
-    });
-}
-
 function run_mri() {
     ///entire mri_pipeline
     "use strict";
@@ -53,63 +39,11 @@ function run_mri() {
         });
 }
 
-/*function doWork() {
-var stringData = $.ajax({
-url: log_file,
-success: function(data) {
-console.log(data);
-setTimeout(doWork, 3000) ;
-$("#log_box").html(data);
-}
-});
-
-//Split values of string data
-//var stringArray = stringData.split(",");
-//doWork();
-//$('#log_box').html(stringData);
-//    $('#log_box').load(log_file);
-///repeater = setTimeout(doWork, 1000);
-}
-
-*/
 
 //doWork();
 $(function () {
     "use strict";
     toggle();
     change();
-    /////////////////////////////////////
-    ////////////////Validation////////////
-    //////////////////////////////////////////
-
-    ///Get all the value
-    //$("#mri_upload").submit(function() {
-    //var elements = $(this).get(0).elements;
-        /*---validate
-        ---move the file and insert into the database....
-        ---Then run dicomtar...
-        ---then run minc-insertion...
-        $.ajax({
-        type: "POST",
-        url:'validation.php',
-        data : array{
-        something:
-        something:
-        }
-        success: function(data){
-        alert(data);//only for testing purposes
-        }
-        });
-        });
-        ////control the action...
-        $('#upload').
-        bind('click', function (event) {
-        });
-        //change();
-        //alert(log_file);
-
-
-        //run_mri();
-        */
-     //});
+  
 });
