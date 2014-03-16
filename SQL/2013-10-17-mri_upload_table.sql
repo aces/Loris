bf4967b0cd4ddac6dded5be13527c604a824a4b9
zia <zia.mohades@gmail.com>
2014-03-16 17:32:10 -0400

    include the table schema for mri_upload table for both existing and new database

---
+++ SQL/2013-10-17-mri_upload_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `mri_upload` (
+  `UploadID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `FileType` enum('mnc','obj','xfm','xfmmnc','imp','vertstat','xml','txt') DEFAULT NULL,
+  `UploadedBy` varchar(255) NOT NULL DEFAULT '',
+  `UploadDate` DateTime DEFAULT NULL,
+  `SourceLocation` varchar(255) NOT NULL DEFAULT '',
+  `number_of_mincInserted` int(11) DEFAULT NULL,
+  `number_of_mincCreated` int(11) DEFAULT NULL,
+  `TarchiveID` int(11) DEFAULT NULL,
+  `SessionID` int(10) unsigned DEFAULT NULL,
+  `IsValidated` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`UploadID`)
+) ;
