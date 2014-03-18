b1e85dbe4ec021d7d96735b2ecf2a241cf6e8d24
Dave MacFarlane <driusan@gmail.com>
2014-03-18 15:40:50 -0400

    Ran phpcs on survey.php

--- htdocs/survey.php
+++ htdocs/survey.php
@@ -1,9 +1,20 @@
 <?php
 /**
- * @package main
+ * This implements the portion of Loris which is user-facing for the survey
+ * module. Survey participants should have received an email with a URL
+ * pointing to this page with a unique one-time key, and this page will handle
+ * loading the correct instrument, displaying it to them in a user-friendly
+ * manner and saving the data to the database.
+ *
+ * PHP Version 5
+ *
+ * @category Survey
+ * @package  Loris
+ * @author   Dave MacFarlane <driusan@bic.mni.mcgill.ca>
+ * @license  Loris license
+ * @link     https://www.github.com/aces/Loris-Trunk/
  */
 set_include_path(get_include_path().":../project/libraries:../php/libraries:");
-// start benchmarking
 require_once 'NDB_Config.class.inc';
 require_once 'Smarty_hook.class.inc';
 require_once 'NDB_Caller.class.inc';
@@ -11,7 +22,17 @@ require_once 'NDB_Client.class.inc';
 require_once 'NDB_BVL_Instrument.class.inc';
 require_once 'Log.class.inc';
 
-class DirectDataEntryMainPage {
+/**
+ * Implements the survey page
+ *
+ * @category Survey
+ * @package  Loris
+ * @author   Dave MacFarlane <driusan@bic.mni.mcgill.ca>
+ * @license  Loris license
+ * @link     https://www.github.com/aces/Loris-Trunk/
+ */
+class DirectDataEntryMainPage
+{
     var $key;
 
     var $TestName;
@@ -24,17 +45,14 @@ class DirectDataEntryMainPage {
     var $tpl_data;
     var $caller;
 
-    function HandleError() {
-        switch($error->code) {
-        case 404: header("HTTP/1.1 404 Not Found"); break;
-        case 403: header("HTTP/1.1 403 Forbidden"); break;
-        }
-        if(empty($error->code)) {
-            //print $error->message;
-        }
-    }
-
-    function initialize() {
+    /**
+     * Initialize all of the class variables and things required from the
+     * REQUEST.
+     *
+     * @return none, but as a side-effect modifies class
+     */
+    function initialize()
+    {
         ob_start('ob_gzhandler');
         $client = new NDB_Client();
         $client->makeCommandLine();
@@ -42,37 +60,54 @@ class DirectDataEntryMainPage {
 
         $this->caller =& NDB_Caller::singleton();
 
-        //$this->caller->setErrorHandling(PEAR_ERROR_CALLBACK, array($this, 'HandleError'));
-        //PEAR::setErrorHandling(PEAR_ERROR_CALLBACK, array($this, 'HandleError'));
         $this->caller->setDataEntryType('Direct');
 
-        if(empty($_REQUEST['key'])) {
+        if (empty($_REQUEST['key'])) {
             throw new Exception("Missing parameter", 403);
         }
         $this->key = $_REQUEST['key'];
 
         $DB = Database::singleton();
-        $this->TestName = $DB->pselectOne("SELECT Test_name FROM participant_accounts WHERE OneTimePassword=:key AND Status <> 'Complete'", array('key' => $this->key));
-        $this->CommentID = $DB->pselectOne("SELECT CommentID FROM participant_accounts WHERE OneTimePassword=:key AND Status <> 'Complete'", array('key' => $this->key));
-        $this->NumPages = $DB->pselectOne("SELECT COUNT(*) FROM instrument_subtests WHERE Test_name=:TN", array('TN' => $this->TestName));
+        $this->TestName = $DB->pselectOne(
+            "SELECT Test_name FROM participant_accounts
+            WHERE OneTimePassword=:key AND Status <> 'Complete'",
+            array('key' => $this->key)
+        );
+        $this->CommentID = $DB->pselectOne(
+            "SELECT CommentID FROM participant_accounts 
+            WHERE OneTimePassword=:key AND Status <> 'Complete'",
+            array('key' => $this->key)
+        );
+        $this->NumPages = $DB->pselectOne(
+            "SELECT COUNT(*) FROM instrument_subtests WHERE Test_name=:TN",
+            array('TN' => $this->TestName)
+        );
 
-        if(empty($this->TestName) && empty($this->CommentID)) {
+        if (empty($this->TestName) && empty($this->CommentID)) {
             throw new Exception("Data has already been submitted.", 403);
         }
         $pageNum = null;
 
-        if(!empty($_REQUEST['pageNum']))  {
+        if (!empty($_REQUEST['pageNum'])) {
             $pageNum = $_REQUEST['pageNum'];
         }
 
 
-        if($pageNum === 'finalpage') {
+        if ($pageNum === 'finalpage') {
             $this->Subtest = 'finalpage';
         } else {
-            $this->Subtest = $DB->pselectOne("SELECT Subtest_name FROM instrument_subtests WHERE Test_name=:TN AND Order_number=:PN", array('TN' => $this->TestName, 'PN' => $pageNum));
+            $this->Subtest = $DB->pselectOne(
+                "SELECT Subtest_name
+                FROM instrument_subtests
+                WHERE Test_name=:TN AND Order_number=:PN",
+                array('TN' => $this->TestName, 'PN' => $pageNum)
+            );
         }
 
-        $totalPages = $DB->pselectOne("SELECT COUNT(*)+1 from instrument_subtests WHERE Test_name=:TN", array('TN' => $this->TestName));
+        $totalPages = $DB->pselectOne(
+            "SELECT COUNT(*)+1 from instrument_subtests WHERE Test_name=:TN",
+            array('TN' => $this->TestName)
+        );
         $this->NextPageNum = $this->getNextPageNum($pageNum);
         $this->PrevPageNum = $this->getPrevPageNum($pageNum);
 
@@ -87,59 +122,112 @@ class DirectDataEntryMainPage {
 
 
 
-    function GetNextPageNum($currentPage) {
+    /**
+     * Get the page which follows this page
+     *
+     * @param integer $currentPage The current page number
+     *
+     * @return The page which preceeded this one
+     */
+    function getNextPageNum($currentPage)
+    {
         $DB = Database::singleton();
-        if($currentPage === null) {
+        if ($currentPage === null) {
             return 1;
         }
-        return $DB->pselectOne("SELECT Order_number FROM instrument_subtests WHERE Test_name=:TN AND Order_number > :PN ORDER BY Order_number", array('TN' => $this->TestName, 'PN' => $currentPage));
+        return $DB->pselectOne(
+            "SELECT Order_number FROM instrument_subtests
+            WHERE Test_name=:TN AND Order_number > :PN 
+            ORDER BY Order_number",
+            array('TN' => $this->TestName, 'PN' => $currentPage)
+        );
     }
 
-    function GetPrevPageNum($currentPage) {
+    /**
+     * Get the previous page number
+     *
+     * @param integer $currentPage The current page number
+     *
+     * @return string the previous page number or "top" if the user is on
+     *         the top page
+     */
+    function getPrevPageNum($currentPage)
+    {
         $DB = Database::singleton();
-        if($currentPage === null) {
+        if ($currentPage === null) {
             // On the top page or no page specified, do not include link
             return null;
         }
 
-        if($currentPage == 1) {
+        if ($currentPage == 1) {
             // on the first subtest page the previous page is the top page
             return 'top';
         }
 
-        if($currentPage === 'finalpage') {
-            return $DB->pselectOne("SELECT MAX(Order_number) FROM instrument_subtests WHERE Test_name=:TN", array('TN' => $this->TestName));
+        if ($currentPage === 'finalpage') {
+            return $DB->pselectOne(
+                "SELECT MAX(Order_number) 
+                FROM instrument_subtests 
+                WHERE Test_name=:TN",
+                array('TN' => $this->TestName)
+            );
         }
-        return $DB->pselectOne("SELECT Order_number FROM instrument_subtests WHERE Test_name=:TN AND Order_number < :PN ORDER BY Order_number DESC", array('TN' => $this->TestName, 'PN' => $currentPage));
+        return $DB->pselectOne(
+            "SELECT Order_number FROM instrument_subtests 
+            WHERE Test_name=:TN AND Order_number < :PN ORDER BY Order_number DESC",
+            array('TN' => $this->TestName, 'PN' => $currentPage)
+        );
     }
 
-    function getDefaults() {
-    }
-
-    function run() {
+    /**
+     * Run the current page, consists of initializing and then displaying the page
+     *
+     * @return none
+     */
+    function run()
+    {
         try {
-
-        $this->initialize();
-        $this->display();
+            $this->initialize();
+            $this->display();
         } catch(Exception $e) {
             $this->displayError($e);
         }
     }
 
-    function getCommentID() {
+    /**
+     * Get the CommentID associated with the current page's key
+     *
+     * @return string a valid CommentID for this page
+     */
+    function getCommentID()
+    {
         $DB = Database::singleton();
         return $DB->pselectOne(
-            "SELECT CommentID FROM participant_accounts WHERE OneTimePassword=:key AND Status <> 'Complete'",
+            "SELECT CommentID FROM participant_accounts
+            WHERE OneTimePassword=:key AND Status <> 'Complete'",
             array(
                 'key' => $this->key
             )
         );
     }
 
-    function displayError($e) {
-        switch($e->getCode()) {
-        case 404: header("HTTP/1.1 404 Not Found"); break;
-        case 403: header("HTTP/1.1 403 Forbidden"); break;
+    /**
+     * Display an error page in the event of an exception.
+     *
+     * @param Exception $e The exception which was thrown by the code
+     *
+     * @return none, but as a side-effect changes the HTTP return code
+     */
+    function displayError($e)
+    {
+        switch($e->getCode())
+        {
+        case 404:
+            header("HTTP/1.1 404 Not Found");
+            break;
+        case 403:
+            header("HTTP/1.1 403 Forbidden");
+            break;
         }
 
         $this->tpl_data['workspace'] = $e->getMessage();
@@ -153,20 +241,25 @@ class DirectDataEntryMainPage {
     /**
      * Updates the status of the current key
      *
-     * @param status string The status to be updated to
+     * @param string $status The status to be updated to
      *
      * @return True on success, false on failure
      */
-    function updateStatus($status) {
+    function updateStatus($status)
+    {
         $DB = Database::singleton();
 
-        $currentStatus = $DB->pselectOne('SELECT Status FROM participant_accounts WHERE OneTimePassword=:key', array('key' => $this->key));
+        $currentStatus = $DB->pselectOne(
+            'SELECT Status FROM participant_accounts
+            WHERE OneTimePassword=:key',
+            array('key' => $this->key)
+        );
 
-        if(Utility::isErrorX($currentStatus)) {
+        if (Utility::isErrorX($currentStatus)) {
             return false;
         }
 
-        if($currentStatus === 'Complete') {
+        if ($currentStatus === 'Complete') {
             // Already completed, don't want to accidentally change it back to
             // started or some other status..
             return false;
@@ -181,7 +274,18 @@ class DirectDataEntryMainPage {
         return true;
     }
 
-    function updateComments($ease, $comments) {
+    /**
+     * Saves the comments that the user gave on the final page
+     * to the database
+     *
+     * @param integer $ease     Integer rating of how easy the user found the 
+     *                          survey to use
+     * @param string  $comments Comments entered by survey user on review page
+     *
+     * @return none
+     */
+    function updateComments($ease, $comments)
+    {
         $DB = Database::singleton();
         $DB->update(
             "participant_accounts",
@@ -192,10 +296,19 @@ class DirectDataEntryMainPage {
             array('OneTimePassword' => $this->key)
         );
     }
-    function logRequest() {
+
+    /**
+     * Saves all aspects of current request to a log file so that we ensure
+     * that we never lose user data and can retrieve it in the event of an
+     * emergency
+     *
+     * @return none
+     */
+    function logRequest()
+    {
         $log = new Log("direct_entry");
         $logmsg = $_SERVER['REMOTE_ADDR'];
-        if(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
             $logmsg .= " (" . $_SERVER['HTTP_X_FORWARDED_FOR'] . ")";
         }
 
@@ -203,18 +316,24 @@ class DirectDataEntryMainPage {
         $log->addLog($logmsg);
 
     }
-    function display() {
+
+    /**
+     * Loads the correct page and renders it to the user
+     *
+     * @return none
+     */
+    function display()
+    {
         $DB = Database::singleton();
             $nextpage = null;
 
         $this->logRequest();
         
         if (isset($_REQUEST['nextpage'])) {
-        //if (isset($_REQUEST['nextpage'])) {
             $nextpage = "survey.php?key=$_REQUEST[key]&pageNum=$_REQUEST[nextpage]"; 
         }
         
-        if(isset($_POST['ease'])) {
+        if (isset($_POST['ease'])) {
             // Comments is too comment of an instrument fieldname,
             // so just check if ease is set
             $this->updateComments(
@@ -223,12 +342,19 @@ class DirectDataEntryMainPage {
             );
         }
 
-        $workspace = $this->caller->load($this->TestName, $this->Subtest, null, $this->CommentID, $nextpage);
+        $workspace = $this->caller->load(
+            $this->TestName,
+            $this->Subtest,
+            null,
+            $this->CommentID,
+            $nextpage
+        );
 
-        // Caller calls instrument's save function and might have errors, so we still need to call it.
-        // But if nextpage is 'complete', then after that override with a "Thank you" message
+        // Caller calls instrument's save function and might have errors,
+        // so we still need to call it. But if nextpage is 'complete', 
+        // then after that override with a "Thank you" message
         if ($_REQUEST['pageNum'] === 'finalpage') {
-            if(isset($_POST['ease'])) {
+            if (isset($_POST['ease'])) {
                 // Data was submitted on the last page.
                 $this->tpl_data['workspace'] = $workspace;
             } else {
@@ -236,7 +362,10 @@ class DirectDataEntryMainPage {
 
                 $this->tpl_data['workspace'] = '';
                 $this->tpl_data['questions'] = $DB->pselect(
-                    "SELECT Description as question, SourceField FROM parameter_type WHERE SourceFrom=:TN AND SourceField NOT IN ('Validity', 'Administration')",
+                    "SELECT Description as question,
+                            SourceField FROM parameter_type 
+                        WHERE SourceFrom=:TN AND 
+                            SourceField NOT IN ('Validity', 'Administration')",
                     array(
                         'TN' => $this->TestName 
                     )
@@ -247,8 +376,8 @@ class DirectDataEntryMainPage {
                     array('CID' => $this->CommentID)
                 );
 
-                foreach($this->tpl_data['questions'] as &$row) {
-                    if(isset($Responses[$row['SourceField']])) {
+                foreach ($this->tpl_data['questions'] as &$row) {
+                    if (isset($Responses[$row['SourceField']])) {
                         $row['response'] = $Responses[$row['SourceField']];
                     }
                 }
@@ -281,7 +410,7 @@ class DirectDataEntryMainPage {
     }
 }
 
-if(!class_exists('UnitTestCase')) {
+if (!class_exists('UnitTestCase')) {
     $Runner = new DirectDataEntryMainPage();
     $Runner->run();
 }
