0493b318d9a8de4f55495b49af8d41df3278b53e
Rathi <sekaranrathi@gmail.com>
2014-05-09 11:52:03 -0400

    updating code to delete cookie properly

--- htdocs/js/modules/direct_entry.js
+++ htdocs/js/modules/direct_entry.js
@@ -1,54 +1,46 @@
 $(document).ready(function() {
- function createCookie(name,value,days) {
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime()+(days*24*60*60*1000));
-        var expires = "; expires="+date.toGMTString();
-    }
-    else var expires = "";
-    document.cookie = name+"="+value+expires+"; path=/";
- }
-
- function readCookie(name) {
+    function readCookie(name) {
 	var cookiename = name + "="; 
 	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++)
+	for (var i=0;i < ca.length;i++)
 	{
 		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		while (c.charAt(0) ==' ') c = c.substring(1,c.length);
 		if (c.indexOf(cookiename) == 0) return c.substring(cookiename.length,c.length);
 	}
 	return null;
     }
-    function eraseCookie(name) {
-       createCookie(name,"",-1);
+   
+    function delete_cookie( name ) {
+       document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
     }
     var cookie_name = 'MissingResponses';
-    if(readCookie(cookie_name)) {
-       var g = document.createElement('div');
-       g.id = 'dialog-modal';
-       g.title = "Missing responses";
-       g.class = "popup";
-       var str = readCookie(cookie_name); 
-       g.innerHTML = decodeURIComponent((str+'').replace(/\+/g, '%20')); 
-       document.body.appendChild(g);
-       var pageNum = readCookie('PrevPage');
-       var key = readCookie('URLKey');
+    if (readCookie(cookie_name)) {
+        var g = document.createElement('div');
+        g.id = 'dialog-modal';
+        g.title = "Missing responses";
+        g.class = "popup";
+        var str = readCookie(cookie_name); 
+        g.innerHTML = decodeURIComponent((str + '').replace(/\+/g, '%20')); 
+        document.body.appendChild(g);
+        var pageNum = readCookie('PrevPage');
+        var key = readCookie('URLKey');
         $("#dialog-modal").dialog({
-          height: 340,
-          width: 550,
-          modal: true,
-          buttons: {
-             "Go back": function() {
-		window.location = "?key="+ key +"&pageNum="+ pageNum ;
-                eraseCookie(cookie_name);
-             },
-             Cancel: function() {
-             $( this ).dialog( "close" );
+            height: 340,
+            width: 550,
+            modal: true,
+              buttons: {
+              "Go back": function() {
+		   window.location = "?key=" + key + "&pageNum=" + pageNum ;
+                   delete_cookie('MissingResponses');
+              },
+              Cancel: function() {
+                  $( this ).dialog( "close" );
+                  delete_cookie('MissingResponses');
+              }
              }
-          }
-         });
-        }
+       });
+   }
 
     ajaxSubmit = function(e) {
         var formEl = document.getElementById("test_form"),
@@ -75,18 +67,16 @@ $(document).ready(function() {
         //formEl.action = "submit.php?key=" + document.getElementById("key").textContent;
         $("#test_form").submit();
     }
-
-   
     $("#savecontinue").click(ajaxSubmit);
     $("#finalize").click(ajaxSubmit);
     $("#complete").click(ajaxSubmit);
     $("#goback").click(ajaxSubmit);
 
     var NavButtons = document.getElementById("buttons");
-    if(NavButtons) {
-    var InstrumentTable = document.getElementsByClassName("instrument")[0];
-    var Footer = document.createElement("tfoot");
-    Footer.appendChild(NavButtons);
-    InstrumentTable.appendChild(Footer);
- }
+    if (NavButtons) {
+	    var InstrumentTable = document.getElementsByClassName("instrument")[0];
+	    var Footer = document.createElement("tfoot");
+	    Footer.appendChild(NavButtons);
+	    InstrumentTable.appendChild(Footer);
+    }
 });
