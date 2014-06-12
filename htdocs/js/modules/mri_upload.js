d79c89ac35dd1043eb48247b0dfc2d8e1c649c9d
olgatsib <olgatsib@gmail.com>
2014-06-12 09:28:26 -0400

    Add css and js

---
+++ htdocs/js/modules/mri_upload.js
@@ -0,0 +1,23 @@
+/*global document: false, $: false, window: false*/
+$(function () {
+    "use strict";
+    $('#show').show();
+    $("#show").css("cursor", "pointer");
+    $('#hide').hide();
+    $('#log_box').hide();
+    $('#hide').bind('click', function () {
+        $('#log_box').hide('slow');
+        $('#hide').hide();
+        $('#show').show();
+    });
+
+    $('#show').bind('click', function () {
+        $('#log_box').show('slow');
+        $('#show').hide();
+        $("#hide").css("cursor", "pointer");
+        $('#hide').show();
+    });
+});
+
+
+
