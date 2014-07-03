66ca726ca5b7d646cd28331e8af3002713c5527a
Dave MacFarlane <driusan@gmail.com>
2014-07-03 13:01:05 -0400

    Updated Smarty to look in module directory

--- php/libraries/smarty/plugins/resource.neurodb.php
+++ php/libraries/smarty/plugins/resource.neurodb.php
@@ -4,7 +4,10 @@ function smarty_resource_neurodb_source($rsrc_name, &$source, $smarty) {
     else {
         $default_name=$smarty->template_dir.$rsrc_name;
         $custom_name=$smarty->project_template_dir.$rsrc_name;
-        if(is_file($custom_name)){
+        $module_name = $smarty->modules_dir . $smarty->ModuleName . "/templates/$rsrc_name";
+        if(is_file($module_name)) {
+            $filename=$module_name;
+        } else if(is_file($custom_name)) {
             $filename=$custom_name;
         } else if(is_file($default_name)) {
             $filename=$default_name;
@@ -23,7 +26,10 @@ function smarty_resource_neurodb_timestamp($rsrc_name, &$timestamp, $smarty) {
     else {
         $default_name=$smarty->template_dir.$rsrc_name;
         $custom_name=$smarty->project_template_dir.$rsrc_name;
-        if(is_file($custom_name)){
+        $module_name = $smarty->modules_dir . $smarty->ModuleName . "/templates/$rsrc_name";
+        if(is_file($module_name)) {
+            $filename=$module_name;
+        } else if(is_file($custom_name)){
             $filename=$custom_name;
         } else if(is_file($default_name)) {
             $filename=$default_name;
