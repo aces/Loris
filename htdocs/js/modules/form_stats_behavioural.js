2e35d25a5b4e0c4520d026ec027cc32eeb140ad4
Jordan Stirling <jstirl@dhcp56-240.mni.mcgill.ca>
2014-07-07 15:56:16 -0400

    create generic function for dynamic tables with side scrolls

--- htdocs/js/modules/form_stats_behavioural.js
+++ htdocs/js/modules/form_stats_behavioural.js
@@ -33,113 +33,23 @@ function hideStats(clicked){
     checkOverflow();
 }
 $(document).ready(function(){
-    console.log($(".centers").height());
+    $.getScript("js/modules/dynamic_table.table.js")
+        .done(function(){
+            Table.setup("content", "scrollRight", "scrollLeft");
+            Table.checkOverflow("content", "scrollRight", "scrollLeft", "headcol");
+            Table.setup("contentDD", "scrollRightDD", "scrollLeftDD");
+            Table.checkOverflow("contentDD", "scrollRightDD", "scrollLeftDD");
+        });
+    
     $(".spacer").height($(".centers").height());
-    checkOverflow();
+    // checkOverflow();
 });
 $(window).resize(function(){
     $(".spacer").height($(".centers").height());
-    checkOverflow();
+    Table.checkOverflow("contentDD", "scrollRightDD", "scrollLeftDD");
+    Table.checkOverflow("content", "scrollRight", "scrollLeft", "headcol");
+    // checkOverflow();
 });
-var step = 100;
-var scrolling = false;
-$("#scrollRight").bind("click", function(event) {
-    event.preventDefault();
-    // Animates the scrollTop property by the specified
-    // step.
-    scrolling = false;
-    $("#content").animate({
-        scrollLeft: $("#content").scrollLeft() + step
-    });
-}).bind("mouseover", function(event) {
-    scrolling = true;
-    scrollContent("right", "#content");
-}).bind("mouseout", function(event) {
-    scrolling = false;
-});
-$("#scrollLeft").bind("click", function(event) {
-    event.preventDefault();
-    // Animates the scrollTop property by the specified
-    // step.
-    scrolling = false;
-    $("#content").animate({
-        scrollLeft: $("#content").scrollLeft() - step
-    });
-}).bind("mouseover", function(event) {
-    scrolling = true;
-    scrollContent("left", "#content");
-}).bind("mouseout", function(event) {
-    scrolling = false;
-});
-$("#scrollRightDD").bind("click", function(event) {
-    event.preventDefault();
-    // Animates the scrollTop property by the specified
-    // step.
-    scrolling = false;
-    $("#contentDD").animate({
-        scrollLeft: $("#contentDD").scrollLeft() + step
-    });
-}).bind("mouseover", function(event) {
-    scrolling = true;
-    scrollContent("right", "#contentDD");
-}).bind("mouseout", function(event) {
-    scrolling = false;
-});
-$("#scrollLeftDD").bind("click", function(event) {
-    event.preventDefault();
-    // Animates the scrollTop property by the specified
-    // step.
-    scrolling = false;
-    $("#contentDD").animate({
-        scrollLeft: $("#contentDD").scrollLeft() - step
-    });
-}).bind("mouseover", function(event) {
-    scrolling = true;
-    scrollContent("left", "#contentDD");
-}).bind("mouseout", function(event) {
-    scrolling = false;
-});
-function scrollContent(direction, elem) {
-    var amount = (direction === "left" ? -3 : 3);
-    $(elem).animate({
-        scrollLeft: $(elem).scrollLeft() + amount
-    }, 1, function() {
-        if (scrolling) {
-            scrollContent(direction, elem);
-        }
-    });
-}
-function checkOverflow(){
-    var element = document.querySelector('#content');
-    if( (element.offsetHeight < element.scrollHeight) || (element.offsetWidth < element.scrollWidth)){
-        // your element have overflow
-        $(".table-scroll").addClass("scrollable");
-        $(".headcol").addClass("colm-static");
-        $("#scrollLeft").show();
-        $("#scrollRight").show();
-    }
-    else{
-        //your element don't have overflow
-        $(".table-scroll").removeClass("scrollable");
-        $(".headcol").removeClass("colm-static");
-        $("#scrollLeft").hide();
-        $("#scrollRight").hide();
-    }
-    element = document.querySelector('#contentDD');
-    if( (element.offsetHeight < element.scrollHeight) || (element.offsetWidth < element.scrollWidth)){
-        // your element have overflow
-        $("#contentDD").addClass("col-xs-10 col-xs-offset-1");
-        $("#scrollLeftDD").show();
-        $("#scrollRightDD").show();
-    }
-    else{
-        //your element don't have overflow
-        $("#contentDD").removeClass("col-xs-10 col-xs-offset-1");
-        $("#scrollLeftDD").hide();
-        $("#scrollRightDD").hide();
-    }
-}
-
 $(function(){
         $(".tip").tooltip();
     });
