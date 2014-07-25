e5f768196695b6245dd143cfd869f870d04cc2dd
Jordan Stirling <jstirl@dhcp56-240.mni.mcgill.ca>
2014-07-25 15:42:51 -0400

    format final_radiological_review form

--- htdocs/js/modules/final_radiological_review.js
+++ htdocs/js/modules/final_radiological_review.js
@@ -3,9 +3,17 @@ $(document).ready(function(){
         .done(function(){
             Table.setup("content", "scrollRight", "scrollLeft");
             Table.checkOverflow("content", "scrollRight", "scrollLeft");
+            if($("#content2").length){
+            	Table.setup("content2", "scrollRight2", "scrollLeft2");
+            	Table.checkOverflow("content2", "scrollRight2", "scrollLeft2");
+            }
+            
         });
 });
 $(window).resize(function(){
     Table.checkOverflow("content", "scrollRight", "scrollLeft");
+    if($("#content2").length){
+    	Table.checkOverflow("content2", "scrollRight2", "scrollLeft2");
+    }
     // checkOverflow();
 });
