dbb4347a0c84a1704e82ef86b5c6771123522338
samirdas <samir@bic.mni.mcgill.ca>
2014-08-07 11:30:54 -0400

    Update 0000-00-00-schema.sql

--- SQL/0000-00-00-schema.sql
+++ SQL/0000-00-00-schema.sql
@@ -1770,7 +1770,7 @@ CREATE TABLE `reliability` (
   `Manual_Swap` enum('no','yes') DEFAULT 'no',
   `EARLI_Candidate` enum('no','yes') DEFAULT 'no',
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET = utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 
