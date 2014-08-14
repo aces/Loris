12c807c695cfcc6c8559a66749a9a6b241a2b4d9
Rathi <sekaranrathi@gmail.com>
2014-08-14 17:21:53 -0400

    adding tpl file

--- htdocs/process_new_account.php
+++ htdocs/process_new_account.php
@@ -2,6 +2,12 @@
 set_include_path(get_include_path().":../project/libraries:../php/libraries:");
 ini_set('default_charset', 'utf-8');
 
+/**
+ * lost password form
+ * @package main
+ */
+ob_start('ob_gzhandler');
+
 require_once "Database.class.inc";
 require_once "Utility.class.inc";
 require_once 'NDB_Config.class.inc';
@@ -15,66 +21,88 @@ $DB =& Database::singleton();
 if (Utility::isErrorX($DB)) {
      return("Could not connect to database: ".$DB->getMessage());
 }
+$tpl_data = array();
+
+// create an instance of the config object
+$config =& NDB_Config::singleton();
+$tpl_data['css']=$config->getSetting('css');
+$tpl_data['rand'] = rand(0, 9999);
+$tpl_data['success'] = false;
 $err = array();
 if(!checkLen('name'))
-        $err[]='The First name field is empty!';
+$err[]='The First name field is empty!';
 
 if(!checkLen('lastname'))
-        $err[]='The Last name field is empty!';
+$err[]='The Last name field is empty!';
 
 if(!checkLen('from'))
-        $err[]='The email field is empty!';
+$err[]='The email field is empty!';
 else if(!checkEmail($_REQUEST['from']))
-        $err[]='Your email is not valid!';
+$err[]='Your email is not valid!';
 if(count($err))
 {
-  echo '<hr /><h3>The following occurred:</h3><ul>'; 
-  // Print each error. 
-  foreach ($err as $msg) { echo '<li>'. $msg . '</li>';}
-
- exit;
+	$tpl_data['error_message'] = $err;
 }
-$name = $_REQUEST["name"];
-$lastname = $_REQUEST["lastname"];
-$from = $_REQUEST["from"];
-$verif_box = $_REQUEST["verif_box"];
 
-// remove the backslashes that normally appears when entering " or '
-$name = stripslashes($name); 
-$lastname = stripslashes($lastname); 
-$from = stripslashes($from); 
+if (!count($err)) {
+/*
+	$err = array();
+	if(!checkLen('name'))
+		$err[]='The First name field is empty!';
 
-// check to see if verificaton code was correct
-if(md5($verif_box).'a4xn' == $_COOKIE['tntcon']){
-	// if verification code was correct send the message and show this page
-        $fullname = $name." ".$lastname;
-	$vals = array('UserID'=>$from, 'Real_name'=>$fullname, 'First_name'=>$name, 'Last_name'=>$lastname,'Pending_approval'=>1,'Email'=>$from);
-        // check email address' uniqueness
-        $result = $DB->pselectOne("SELECT COUNT(*) FROM users WHERE Email = :VEmail",
-                  array('VEmail' => $from));
-        if (Utility::isErrorX($result)) {
-            return PEAR::raiseError("DB Error: ".$result->getMessage());
-        }
+	if(!checkLen('lastname'))
+		$err[]='The Last name field is empty!';
 
-        if ($result > 0) {
-            echo 'The email address already exists';
-            exit;
-        }
-	
-        $success = $DB->insert('users', $vals);
-	if (Utility::isErrorX($success)) {
-		return PEAR::raiseError("DB Error: ".$success->getMessage());
+	if(!checkLen('from'))
+		$err[]='The email field is empty!';
+	else if(!checkEmail($_REQUEST['from']))
+		$err[]='Your email is not valid!';
+	if(count($err))
+	{
+            $tpl_data['error_message'] = $err;
+	}
+*/
+	$name = $_REQUEST["name"];
+	$lastname = $_REQUEST["lastname"];
+	$from = $_REQUEST["from"];
+	$verif_box = $_REQUEST["verif_box"];
+
+	// remove the backslashes that normally appears when entering " or '
+	$name = stripslashes($name); 
+	$lastname = stripslashes($lastname); 
+	$from = stripslashes($from); 
+
+	// check to see if verificaton code was correct
+	if(md5($verif_box).'a4xn' == $_COOKIE['tntcon']){
+		// if verification code was correct send the message and show this page
+		$fullname = $name." ".$lastname;
+		$vals = array('UserID'=>$from, 'Real_name'=>$fullname, 'First_name'=>$name, 'Last_name'=>$lastname,'Pending_approval'=>1,'Email'=>$from);
+		// check email address' uniqueness
+		$result = $DB->pselectOne("SELECT COUNT(*) FROM users WHERE Email = :VEmail",
+				array('VEmail' => $from));
+		if (Utility::isErrorX($result)) {
+			return PEAR::raiseError("DB Error: ".$result->getMessage());
+		}
+
+		if ($result > 0) {
+			echo 'The email address already exists';
+			exit;
+		}
+
+		$success = $DB->insert('users', $vals);
+		if (Utility::isErrorX($success)) {
+			return PEAR::raiseError("DB Error: ".$success->getMessage());
+		} 
+                $tpl_data['success'] = true;
+		// delete the cookie so it cannot sent again by refreshing this page
+		setcookie('tntcon','');
 	} else {
-                echo 'Your request has been received successfully';
-        }
+		// if verification code was incorrect then return to contact page and show error
+                $tpl_data['wrong_code'] = true;
+		header("Location:".$_SERVER['HTTP_REFERER']."?from=$from&wrong_code=true");
+		exit;
+	}
 
-
-	// delete the cookie so it cannot sent again by refreshing this page
-	setcookie('tntcon','');
-} else {
-	// if verification code was incorrect then return to contact page and show error
-	header("Location:".$_SERVER['HTTP_REFERER']."?subject=$subject&from=$from&message=$message&wrong_code=true");
-	exit;
 }
 function checkLen($str,$len=2)
 {
@@ -85,10 +113,20 @@ function checkEmail($str)
 {
         return preg_match("/^[\.A-z0-9_\-\+]+[@][A-z0-9_\-]+([.][A-z0-9_\-]+)+[A-z]{1,4}$/", $str);
 }
+
+//Output template using Smarty
+$smarty = new Smarty_neurodb;
+$smarty->assign($tpl_data);
+$smarty->display('process_new_account.tpl');
+
+ob_end_flush();
+
+exit;
+
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!--html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>PHP Contact Form Redirect</title>
@@ -96,4 +134,4 @@ function checkEmail($str)
 
 <body>
 </body>
-</html>
+</html-->
---
+++ smarty/templates/process_new_account.tpl
@@ -0,0 +1,74 @@
+{if $success}
+<p>Your request for an account has been received successfully.<br /></p>
+<br />
+{/if}
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta charset="utf-8"/>
+<link rel="stylesheet" href="{$css}" type="text/css" />
+<link rel="stylesheet" href="bootstrap-3.1.1/css/bootstrap.css">
+<!-- shortcut icon that displays on the browser window -->
+<link rel="shortcut icon" href="images/mni_icon.ico" type="image/ico" />
+<!-- page title -->
+<title>Request LORIS Account</title>
+<!--  end page header -->
+</head>
+
+<body>
+<div class="col-xs-12">
+{if $error_message != ""}
+            <p>The following errors have occured while submitting form :
+
+            <ul>
+    {section name=error loop=$error_message}
+                <li><strong>{$error_message[error]}</strong></li>
+    {/section}
+            </ul>
+{/if}
+
+
+<h2>Request LORIS Account</h2>
+<h3>Please fill in the form below. We will contact you once your account has been approved.</h3>
+
+<form action="process_new_account.php" method="post" name="form1" id="form1" class="form-horizontal" onsubmit="MM_validateForm('from','','RisEmail','subject','','R','verif_box','','R','message','','R');return document.MM_returnValue">
+<div class="form-group">
+    <label  class="col-sm-2 control-label" size="75">First Name:</label>
+    <div class="col-sm-6">
+        <input name="name" type="text" id="name" size="20" />
+    </div>
+</div>
+<div class="form-group">
+   <label class="col-sm-2 control-label">Last Name:</label>
+    <div class="col-sm-6">
+       <input name="lastname" type="text" id="lastname"/>
+    </div>
+</div>
+<div class="form-group">
+<label class="col-sm-2 control-label">Email Address: </label>
+      <div class="col-sm-6">
+         <input name="from" type="text" id="from"/>
+     </div>
+</div>
+<div class="form-group">
+<label class="col-sm-2 control-label">Type verification image:</label>
+ <div class="col-sm-6">
+<input name="verif_box" type="text" id="verif_box" />
+<img src="verificationimage.php?{$rand}" alt="verification image, type it in the box" width="50" height="24" align="absbottom" /><br />
+</div>
+</div>
+<!-- if the variable "wrong_code" is sent from previous page then display the error field -->
+{if $wrong_code}
+<div style="border:1px solid #990000; background-color:#D70000; color:#FFFFFF; padding:4px; padding-left:6px;width:295px;">Wrong verification code</div><br />
+{/if}
+  <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+                <input name="Submit" class="btn btn-default" type="submit" value="Submit"/>
+            </div>
+        </div>
+
+</form>
+</div>
+
+</body>
+</html>
