0a0db1cb63dde5d54af08b5cfcc80380ce36f5aa
Dave MacFarlane <driusan@gmail.com>
2014-09-12 12:55:19 -0400

    Ran jslint on file

--- htdocs/js/jquery.dynamictable.js
+++ htdocs/js/jquery.dynamictable.js
@@ -1,51 +1,53 @@
-(function ($ ) {
-    var Setup = function(wrapper, rightLink, leftLink) {
-        $(rightLink).bind("click", function(event) {
-            event.preventDefault();
-            // Animates the scrollTop property by the specified
-            // step.
-            scrolling = false;
-            $(wrapper).animate({
-                scrollLeft: $(wrapper).scrollLeft() + step
-            });
-        }).bind("mouseover", function(event) {
-            scrolling = true;
-            scrollContent("right", wrapper);
-        }).bind("mouseout", function(event) {
-            scrolling = false;
-        });
-        $(leftLink).bind("click", function(event) {
-            event.preventDefault();
-            // Animates the scrollTop property by the specified
-            // step.
-            scrolling = false;
-            $(wrapper).animate({
-                scrollLeft: $(wrapper).scrollLeft() - step
-            });
-        }).bind("mouseover", function(event) {
-            scrolling = true;
-            scrollContent("left", wrapper);
-        }).bind("mouseout", function(event) {
-            scrolling = false;
-        });
-        var scrollContent = function (direction, elem) {
+/*global window, jQuery*/
+(function ($) {
+    "use strict";
+    var setupScrolling = function (wrapper, rightLink, leftLink) {
+        var scrolling = false, scrollContent = function (direction, elem) {
             var amount = (direction === "left" ? -3 : 3);
             $(elem).animate({
                 scrollLeft: $(elem).scrollLeft() + amount
-            }, 1, function() {
+            }, 1, function () {
                 if (scrolling) {
                     scrollContent(direction, elem);
                 }
             });
-        }
-    }, checkOverflow = function(wrapper, rightLink, leftLink, headCol) {
-        var staticCol = headCol === undefined ? false : true;
-        var element = wrapper;
+        };
+        $(rightLink).bind("click", function (event) {
+            event.preventDefault();
+            // Animates the scrollTop property by the specified
+            // step.
+            scrolling = false;
+            $(wrapper).animate({
+                scrollLeft: $(wrapper).scrollLeft()
+            });
+        }).bind("mouseover", function (event) {
+            scrolling = true;
+            scrollContent("right", wrapper);
+        }).bind("mouseout", function (event) {
+            scrolling = false;
+        });
+        $(leftLink).bind("click", function (event) {
+            event.preventDefault();
+            // Animates the scrollTop property by the specified
+            // step.
+            scrolling = false;
+            $(wrapper).animate({
+                scrollLeft: $(wrapper).scrollLeft()
+            });
+        }).bind("mouseover", function (event) {
+            scrolling = true;
+            scrollContent("left", wrapper);
+        }).bind("mouseout", function (event) {
+            scrolling = false;
+        });
+    }, checkOverflow = function (wrapper, rightLink, leftLink, headCol) {
+        var staticCol = headCol === undefined ? false : true,
+            element = wrapper;
 
-        if( (element.offsetHeight < element.scrollHeight) || (element.offsetWidth < element.scrollWidth)) {
+        if ((element.offsetHeight < element.scrollHeight) || (element.offsetWidth < element.scrollWidth)) {
             // Your element has overflow
-            if(staticCol) {
-                if(headCol) {
+            if (staticCol) {
+                if (headCol) {
                     $("." + headCol).addClass("colm-static");
                 }
                 $(wrapper).addClass("scrollableStat");
@@ -57,13 +59,12 @@
 
         } else {
             // Your element has no overflow
-            if(staticCol){
-                if(headCol) {
+            if (staticCol) {
+                if (headCol) {
                     $("." + headCol).removeClass("colm-static");
                 }
                 $(wrapper).removeClass("scrollableStat");
-            }
-            else{
+            } else {
                 $(wrapper).removeClass("scrollable");
             }
             $(leftLink).hide();
@@ -72,9 +73,9 @@
 
 
     };
- 
-    $.fn.DynamicTable = function() {
-        this.filter("table").each(function() {
+
+    $.fn.DynamicTable = function () {
+        this.filter("table").each(function () {
             var leftLink, rightLink, table = this;
             // Add wrapper code necessary for bootstrap carousel
             $(this).wrap("<div class=\"carousel slide\" data-ride=\"carousel\"></div>");
@@ -90,15 +91,15 @@
             leftLink = this.nextSibling;
             rightLink = leftLink.nextSibling;
 
-            Setup( this.parentElement, rightLink, leftLink);
-            checkOverflow( this.parentElement, rightLink, leftLink);
+            setupScrolling(this.parentElement, rightLink, leftLink);
+            checkOverflow(this.parentElement, rightLink, leftLink);
 
-            window.addEventListener("resize", function() {
-                checkOverflow( table.parentElement, rightLink, leftLink);
+            window.addEventListener("resize", function () {
+                checkOverflow(table.parentElement, rightLink, leftLink);
             });
 
             return this;
-        })
+        });
         return this;
-    }
-}(jQuery ));
+    };
+}(jQuery));
