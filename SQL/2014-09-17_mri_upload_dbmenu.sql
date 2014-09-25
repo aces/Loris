c0b966816ba9cd72fb31f8998d7b1e7b6adeb9f4
zia <zia.mohades@gmail.com>
2014-09-25 14:59:24 -0400

    Added missing semicolon

--- SQL/2014-09-17_mri_upload_dbmenu.sql
+++ SQL/2014-09-17_mri_upload_dbmenu.sql
@@ -1,2 +1,2 @@
 INSERT INTO LorisMenu (Label, Link, Parent, OrderNumber) 
-SELECT 'MRI Upload', 'main.php?test_name=mri_upload', Parent, 4 from LorisMenu WHERE Label = 'Imaging'
+SELECT 'MRI Upload', 'main.php?test_name=mri_upload', Parent, 4 from LorisMenu WHERE Label = 'Imaging';
