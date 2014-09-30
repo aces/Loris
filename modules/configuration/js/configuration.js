cb604773b49e8ee390333fffe4bea4cc25610dcc
Tara Campbell <tara0campbell@gmail.com>
2014-09-30 15:19:51 -0400

    Config section now working - looks better, add button doesn't refresh page.

--- modules/configuration/js/configuration.js
+++ modules/configuration/js/configuration.js
@@ -6,46 +6,27 @@ $(function () {
     $(".add").click(function () {
         count = count + 1;
         var id = $(this).attr('id'),
-            new_id = id + "-" + count,
-            name = $("#" + id + ".name").html(),
-            parent = $(this).attr("name");
+        new_id = id + "-" + count,
+        name   = $("#" + id + ".name").html(),
+        parent = $(this).attr("name"),
+        formID = new_id + "-form";
 
-        $("#"+id+".row").after('<div class="row" id="' + new_id +'" style="margin-left:0px;margin-right:0px;">'
-                + '<li class="list-group-item list-group-item-info" id="' + new_id + '">'
-                + '<span class="collapsable" id="' + new_id + '">'
-                + '<div class="col-md-8"><span class="name" id="' + new_id + '">' + name + '</span></div>'
-                + '<div class="col-md-4">'
-                + '<div class="form" id="' + new_id + '">'
-                + '<div class="input-group">'
-                + '<form method="POST" action="">'
-                + '<input class="form-control" type="text" id="' + new_id + '" value="" name="' + parent + '">'
-                + '</form>'
-                + '<div class="input-group-btn">'
-                + '<button class="btn btn-default remove-new" id="' + new_id +'" type="button"><i class="glyphicon glyphicon-remove"></i></button>'
-                + '<button class="btn btn-default add" id="' + new_id +'" type="button"><i class="glyphicon glyphicon-plus"></i></button>'
-                + '</div>'
-                + '</div>'
-                + '</div>'
-                + '</div>'
-                + '</span>'                                                                                                                                
-                + '</li></div>'
+        $("#"+id+"-formsection").append('<div class="form-item" id="' + formID + '">'
+            + '<form class="inline" method="POST">'
+            + '<input class="form-control input-sm" id="' + new_id + '" name="'+ parent +'" type="text">'
+            + '</form>'
+            + '<form class="inline" method="POST">'
+            + '<button class="btn btn-default btn-small rm-btn remove-new" id="'+ new_id +'" type="button">Remove</button>'
+            + '</form>'
+            + '</div>'
+
         );
-
-        /*
-        <form class="form-inline" method="POST" action="">
-            <input class="form-control" name="{$k}" type="text" id="{$k}" value="{$v}">
-        </form>
-        <form class="inline" method="POST" action="">
-            <button class="btn btn-default remove" id="{$k}" type="submit" name="remove-{$k}"><i class="glyphicon glyphicon-remove"></i></button>
-        </form>
-        */
     });
 
-    $("ul.list-group").on("click", ".remove-new", function () {
-        var id = $(".remove-new").attr('id');
-        $("#" + id + ".row").remove();
+    $('body').on('click','.remove-new', function () {
+        var id = $(this).attr('id');
+        $("#" + id + "-form").remove();
     });
-
     
     $('.form-control').keypress(function(e) {
         if(e.which === 13) { // Determine if the user pressed the enter button                                   
--- modules/configuration/templates/form_configuration.tpl
+++ modules/configuration/templates/form_configuration.tpl
@@ -11,6 +11,8 @@
     });
 </script>
 
+<p>Configuration Module Description</p>
+
 {function name=printConfig}
     {foreach $items as $item}
         {if isset($item['Parent'])}
@@ -18,27 +20,32 @@
         {else}
             <tr class="treegrid-{$item['ID']}">
         {/if}
-                <td>{$item['Description']}</td>
                 <td>
-                    {if $item['AllowMultiple'] == 1}
-                        <form method="POST" action="">
-                            <button class="btn btn-default btn-sm add" id="{$item['ID']}" type="submit" name="add-{$item['ID']}">
-                                <span class="glyphicon glyphicon-plus"></span> Add field
-                            </button>
-                        </form>
-                    {/if}
-                    {if isset($item['Value'])}
-                        {foreach from=$item['Value'] key=k item=v}
-                                <form class="inline" method="POST" action="">
-                                    <input class="form-control input-sm" name="{$k}" type="text" id="{$k}" value="{$v}">
-                                </form>
-                            {if $item['AllowMultiple'] == 1}
-                                <form class="inline" method="POST" action="">
-                                    <button class="btn btn-default btn-small remove" id="{$k}" type="submit" name="remove-{$k}"><i class="glyphicon glyphicon-remove"></i></button>
-                                </form>
-                            {/if}
-                        {/foreach}
-                    {/if}
+                        {$item['Description']}
+                </td>
+                <td>
+                    <div class="form-section" id="{$item['ID']}-formsection">
+                        {if $item['AllowMultiple'] == 1}
+                            <form method="POST" action="">
+                                <button class="btn btn-default btn-sm add" id="{$item['ID']}" type="button" name="add-{$item['ID']}">
+                                    <span class="glyphicon glyphicon-plus"></span> Add field
+                                </button>
+                            </form>
+                        {/if}
+                        {if isset($item['Value'])}
+                            {foreach from=$item['Value'] key=k item=v}
+                                <div class="form-item">
+                                    <form method="POST">
+                                        <input class="form-control input-sm config-input" name="{$k}" type="text" id="{$k}" value="{$v}">
+                                    </form>
+                                {if $item['AllowMultiple'] == 1}
+                                    <form method="POST">
+                                        <button class="btn btn-default btn-small rm-btn" id="{$k}" type="submit" name="remove-{$k}">Remove</button>
+                                    </form>
+                                {/if}
+                            {/foreach}
+                        {/if}
+                    </div>
                 </td>
             </tr>
     {/foreach}
