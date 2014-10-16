b09af00e77c460d26ae9c2d5397e839c94e2263e
Jkat <fission@live.com>
2014-10-16 18:29:10 -0400

    Give admin user all permissions

--- SQL/0000-00-00-schema.sql
+++ SQL/0000-00-00-schema.sql
@@ -1374,23 +1374,7 @@ CREATE TABLE `user_perm_rel` (
 
 LOCK TABLES `user_perm_rel` WRITE;
 /*!40000 ALTER TABLE `user_perm_rel` DISABLE KEYS */;
-INSERT INTO `user_perm_rel` VALUES 
-	(1,1),
-	(1,2),
-	(1,3),
-	(1,4),
-	(1,5),
-	(1,6),
-	(1,7),
-	(1,8),
-	(1,9),
-	(1,10),
-	(1,11),
-	(1,12),
-	(1,13),
-	(1,14),
-	(1,15),
-	(1,16);
+INSERT INTO `user_perm_rel` (userID, permID) SELECT DISTINCT 1, permID from permissions;
 /*!40000 ALTER TABLE `user_perm_rel` ENABLE KEYS */;
 UNLOCK TABLES;
 
