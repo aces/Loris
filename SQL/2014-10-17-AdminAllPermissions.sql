1f77d73c300854efdf180c5b6a86fc6fd7e65712
Jkat <fission@live.com>
2014-10-17 14:45:08 -0400

    Add IGNORE for patch

--- SQL/2014-10-17-AdminAllPermissions.sql
+++ SQL/2014-10-17-AdminAllPermissions.sql
@@ -1,1 +1,1 @@
-INSERT INTO `user_perm_rel` (userID, permID) SELECT DISTINCT 1, permID FROM permissions;
+INSERT IGNORE INTO `user_perm_rel` (userID, permID) SELECT DISTINCT 1, permID FROM permissions;
