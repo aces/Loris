0901322e18a2041080204c7ad37a24e31af13ef2
Tara Campbell <tara0campbell@gmail.com>
2014-10-28 13:51:28 -0400

    Fix ordering and added comma to patch.

--- SQL/2014-10-21-AddDataIntegrityFlagandInstrumentManagerToLorisMenu.sql
+++ SQL/2014-10-21-AddDataIntegrityFlagandInstrumentManagerToLorisMenu.sql
@@ -1,5 +1,5 @@
 INSERT INTO LorisMenu (Label, Link, Parent, OrderNumber) 
-SELECT 'Data Integrity Flag', 'main.php?test_name=data_integrity_flag', 3 ID from LorisMenu WHERE Label = 'Tools';
+SELECT 'Data Integrity Flag', 'main.php?test_name=data_integrity_flag', ID, 3 from LorisMenu WHERE Label = 'Tools';
 
 INSERT INTO LorisMenuPermissions (MenuID, PermID) SELECT m.ID, p.PermID 
 FROM permissions p CROSS JOIN LorisMenu m WHERE p.code='data_integrity_flag' AND m.Label='Data Integrity Flag';
