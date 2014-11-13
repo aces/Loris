b0f6c44515b1d369ebb110f86ff4076f7b46465e
Cécile Madjar <cecile.madjar@gmail.com>
2014-11-13 17:33:54 -0500

    Added permissions imaging_browser_view_site and imaging_browser_view_allsites to the SQL schema with the associated patch.

--- SQL/2014-11-13-ImagingBrowserPermissions.sql
+++ SQL/2014-11-13-ImagingBrowserPermissions.sql
@@ -1,1 +1,3 @@
 UPDATE permissions SET code="imaging_browser_qc", description="Edit imaging browser QC status" WHERE code="mri_feedback";
+INSERT INTO permissions (code, description, categoryID) VALUES ("imaging_browser_view_site",     "View own-site Imaging Browser pages",  2);
+INSERT INTO permissions (code, description, categoryID) VALUES ("imaging_browser_view_allsites", "View all-sites Imaging Browser pages", 2);
