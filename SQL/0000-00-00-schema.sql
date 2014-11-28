5e757df306aaef0ac07e5e138f7fbda18195dc8e
Dave MacFarlane <driusan@gmail.com>
2014-11-28 12:44:11 -0500

    Updated default schema with StatisticsTabs

--- SQL/0000-00-00-schema.sql
+++ SQL/0000-00-00-schema.sql
@@ -2200,3 +2200,20 @@ INSERT INTO Config (ConfigID, Value) SELECT ID, "mri_parameter_form" FROM Config
 INSERT INTO Config (ConfigID, Value) SELECT ID, "no-reply@example.com" FROM ConfigSettings WHERE Name="From";
 INSERT INTO Config (ConfigID, Value) SELECT ID, "no-reply@example.com" FROM ConfigSettings WHERE Name="Reply-to";
 INSERT INTO Config (ConfigID, Value) SELECT ID, "Produced by LorisDB" FROM ConfigSettings WHERE Name="X-MimeOLE";
+
+
+CREATE TABLE StatisticsTabs(
+    ID INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    ModuleName varchar(255) NOT NULL,
+    SubModuleName varchar(255) NOT NULL,
+    Description varchar(255),
+    OrderNo INTEGER DEFAULT NULL,
+    PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Stores list of tabs for the statistics module';
+
+INSERT INTO StatisticsTabs (ModuleName, SubModuleName, Description, OrderNo) VALUES
+    ('statistics', 'stats_general', 'General Description', 1),
+    ('statistics', 'stats_demographic', 'Demographic Statistics', 2),
+    ('statistics', 'stats_behavioural', 'Behavioural Statistics', 3),
+    ('statistics', 'stats_reliability', 'Reliability Statistics', 4),
+    ('statistics', 'stats_MRI', 'Imaging Statistics', 5);
