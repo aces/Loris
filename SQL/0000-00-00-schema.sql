593f10a317b23d65dedce9e3d7dd7fce965a7914
Rathi <sekaranrathi@gmail.com>
2014-12-08 12:04:26 -0500

    Redmine 6654 - fixing bugs in radiological review module

--- SQL/0000-00-00-schema.sql
+++ SQL/0000-00-00-schema.sql
@@ -1589,7 +1589,7 @@ CREATE TABLE `final_radiological_review` (
       `Final_Review_Results2` enum('normal','abnormal','atypical','not_answered') DEFAULT NULL,
       `Final_Examiner2` int(11) DEFAULT NULL,
       `Final_Exclusionary2` enum('exclusionary','non_exclusionary','not_answered') DEFAULT NULL,
-      `Review_Done2` tinyint(1) DEFAULT NULL,
+      `Review_Done2` enum('yes','no','not_answered') DEFAULT NULL,
       `SAS2` int(11) DEFAULT NULL,
       `PVS2` int(11) DEFAULT NULL,
       `Final_Incidental_Findings2` text,
