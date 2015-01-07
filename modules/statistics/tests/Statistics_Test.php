65f39be8130f7a54e98ec0bf7d80f006aa3fa454
Dave MacFarlane <driusan@gmail.com>
2015-01-07 16:35:34 -0500

    Set up database and config wrappers in integration tests

--- modules/statistics/tests/Statistics_Test.php
+++ modules/statistics/tests/Statistics_Test.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . "/LorisIntegrationTest.class.inc";
+require_once __DIR__ . "/../../../test/integrationtests/LorisIntegrationTest.class.inc";
 class Statistics_Test extends LorisIntegrationTest
 {
     public function testTabsFramework()
