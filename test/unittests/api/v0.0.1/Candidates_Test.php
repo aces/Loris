fe3179a96b891742282301bbae0b470a069e13c5
Dave MacFarlane <driusan@gmail.com>
2015-01-21 15:27:25 -0500

    Added unit test for GET /candidates in REST API

--- test/unittests/api/v0.0.1/Candidates_Test.php
+++ test/unittests/api/v0.0.1/Candidates_Test.php
@@ -10,12 +10,37 @@ class Candidates_Test extends PHPUnit_Framework_TestCase
         if(!defined("UNIT_TESTING")) {
             define("UNIT_TESTING", true);
         }
+
+        $this->getMockBuilder('NDB_Config')->setMockClassName("MockNDB_Config")->getMock();
+        $this->getMockBuilder('Database')->setMockClassName("MockDatabase")->getMock();
+
+        $this->Factory = NDB_Factory::singleton();
+        $this->Factory->setTesting(true);
+
+        $this->Config = $this->Factory->config();
+        $this->Database = $this->Factory->database();
+
+        $this->Database->method("pselect")->will($this->returnCallback(
+            function ($query, $params) {
+                if($query == "SELECT CandID FROM candidate WHERE Active='Y'") {
+                    return [["CandID" => "123456"], ["CandID" => "222222"]];
+                }
+                return array();
+            }
+        ));
+
     }
 
     function testValidMethods() {
         $API = new \Loris\API\Candidates("GET");
 
         $this->assertEquals($API->AllowedMethods, ['GET', 'POST']);
+    }
+
+    function testGetCandidates() {
+        $API = new \Loris\API\Candidates("GET");
+
+        $this->assertEquals($API->JSON, ["Candidates" => ["123456", "222222"]]);
     }
 }
 ?>
