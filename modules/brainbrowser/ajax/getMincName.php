166ab1584e3e880a7b4ffb130ec8d3de25777bff
Dave MacFarlane <driusan@gmail.com>
2015-01-21 11:57:12 -0500

    Finished running PHPCS on BrainBrowser

--- modules/brainbrowser/ajax/getMincName.php
+++ modules/brainbrowser/ajax/getMincName.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * This is an AJAX script which returns the file name for a MINC,
+ * given a minc_id request parameter
+ *
+ * PHP Version 5
+ *
+ * @category BrainBrowser
+ * @package  LorisModules
+ * @author   Mia Petkova <mia.petkova@gmail.com>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link     https://www.github.com/aces/Loris/
+ */
 ini_set('default_charset', 'utf-8');
 require_once "Utility.class.inc";
 
