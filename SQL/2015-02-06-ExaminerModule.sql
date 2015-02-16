2602045cbb25928696ce1ce61947a09652404792
Tara Campbell <tara0campbell@gmail.com>
2015-02-16 14:54:10 -0500

    Fix examiner permission patch.

--- SQL/2015-02-06-ExaminerModule.sql
+++ SQL/2015-02-06-ExaminerModule.sql
@@ -1,5 +1,5 @@
 UPDATE permissions SET code='examiner', description='Add and certify examiners' where code='certification';
-UPDATE permissions SET code='examiner_multisite', description='Across all sites add and certify examiners' where code='certification';
+UPDATE permissions SET code='examiner_multisite', description='Across all sites add and certify examiners' where code='certification_multisite';
 
 UPDATE LorisMenu SET Label='Examiner', Link='main.php?test_name=examiner' WHERE Label='Certification';
 
