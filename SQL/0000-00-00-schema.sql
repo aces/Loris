57ce0316e5d04f6234687918693a0a4325bbf0f6
zia <zia.mohades@gmail.com>
2015-03-02 11:49:18 -0500

    New log table

--- SQL/0000-00-00-schema.sql
+++ SQL/0000-00-00-schema.sql
@@ -2343,3 +2343,27 @@ CREATE TABLE `CNV` (
   FOREIGN KEY (`GenomeLocID`) REFERENCES genome_loc(`GenomeLocID`),
   FOREIGN KEY (`CandID`) REFERENCES candidate(`CandID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `log` (
+  `LogID` int(11) NOT NULL AUTO_INCREMENT,
+  `LogTypeID` int(11) NOT NULL DEFAULT '0',
+  `ProcessID` int(11) NOT NULL DEFAULT '0',
+  `Date_Created` int(11) NOT NULL DEFAULT '0',
+  `Message` text,
+  `Error` tinyint(1) DEFAULT '0',
+  `CenterID` tinyint(2) unsigned DEFAULT NULL,
+  PRIMARY KEY (`LogID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 
+
+CREATE TABLE `log_types` (
+  `LogTypeID` int(11) NOT NULL AUTO_INCREMENT,
+  `Type` enum('mri','behavioural') DEFAULT NULL,
+  `private` tinyint(1) DEFAULT '0',
+  `Description` text,
+  `Origin` varchar(255),
+  PRIMARY KEY (`LogTypeID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO log_types (Type,Description,Origin) VALUES ('mri','Insertion of the mincs into the mri-table','minc_insertion');
+INSERT INTO log_types (Type,Description,Origin) VALUES ('mri','calls the different scrips','tarchive_loader');
+INSERT INTO log_types (Type,Description,Origin) VALUES ('mri','Validation of the dicoms','tarchive_validation');
