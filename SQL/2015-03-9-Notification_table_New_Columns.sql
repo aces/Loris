cb419ac1e2945daaeaf971624165f0388ec247bd
zia <zia.mohades@gmail.com>
2015-03-10 15:17:26 -0400

    Added the Origin column in the notification_spool instead of notification_types And changed the data_type for the Error Column

--- SQL/2015-03-9-Notification_table_New_Columns.sql
+++ SQL/2015-03-9-Notification_table_New_Columns.sql
@@ -1,11 +1,11 @@
-ALTER TABLE notification_types ADD COLUMN `Origin` varchar(255) DEFAULT NULL;
+ALTER TABLE notification_spool ADD COLUMN `Origin` varchar(255) DEFAULT NULL;
 ALTER TABLE notification_spool ADD COLUMN `ProcessID` int(11) NOT NULL DEFAULT '0' AFTER NotificationTypeID;
 ALTER TABLE notification_spool CHANGE COLUMN TimeSpooled TimeSpooled datetime DEFAULT NULL;
-ALTER TABLE notification_spool ADD COLUMN `Error` tinyint(1) DEFAULT '0' AFTER Message;
+ALTER TABLE notification_spool ADD COLUMN `Error` enum('Y','N') default NULL AFTER Message;
 
-INSERT INTO notification_types (Type,private,Description,Origin) VALUES
-    ('minc insertion',1,'Insertion of the mincs into the mri-table','minc_insertion'),
-    ('tarchive loader',1,'calls specific Insertion Scripts','tarchive_loader'),
-    ('tarchive validation',1,'Validation of the dicoms After uploading','tarchive_validation'),
-    ('mri upload',1,'Validation of DICOMS before uploading','imaging_upload_file'),
-    ('mri upload',1,'Validation and execution of DicomTar.pl and TarchiveLoader','ImagingUpload');
+INSERT INTO notification_types (Type,private,Description) VALUES
+    ('minc insertion',1,'Insertion of the mincs into the mri-table'),
+    ('tarchive loader',1,'calls specific Insertion Scripts'),
+    ('tarchive validation',1,'Validation of the dicoms After uploading'),
+    ('mri upload',1,'Validation of DICOMS before uploading'),
+    ('mri upload',1,'Validation and execution of DicomTar.pl and TarchiveLoader');
