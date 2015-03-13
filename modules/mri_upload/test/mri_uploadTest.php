a1f4978ff2c77e7ad85bc54c9129a304a9b4c1ff
zia <zia.mohades@gmail.com>
2015-03-13 09:41:01 -0400

    New file added

---
+++ modules/mri_upload/test/mri_uploadTest.php
@@ -0,0 +1,30 @@
+<?php
+class LorisIntegrationTest extends PHPUnit_Framework_TestCase
+{
+    protected $webDriver;
+
+    public function setUp()
+    {
+        $this->webDriver = RemoteWebDriver::create(
+            'http://localhost:4444/wd/hub',
+            array(\WebDriverCapabilityType::BROWSER_NAME => 'firefox')
+        );
+
+        $this->webDriver->get('http://localhost/main.php?test_name=mri_upload');
+
+        print "Page source: " . $this->webDriver->getPageSource();
+
+    }
+    
+    function testMriUploadDoespageLoad()
+    {
+       $bodyText = $this->webDriver->findElement(WebDriverBy::cssSelector("body"))->getText();
+       $this->assertContains("mri_upload", $bodyText);
+    }
+
+    public function tearDown()
+    {
+        $this->webDriver->quit();
+    }
+}
+?>
