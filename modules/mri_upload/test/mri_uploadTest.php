40b3da1803f2001a751d7f416bbe5119f0885b94
zia <zia.mohades@gmail.com>
2015-03-13 16:07:21 -0400

    Fixed a bug

--- modules/mri_upload/test/mri_uploadTest.php
+++ modules/mri_upload/test/mri_uploadTest.php
@@ -4,7 +4,7 @@ class mri_uploadTestIntegrationTest extends LorisIntegrationTest
 {
     function testMriUploadDoespageLoad()
     {
-        $this->webDriver->get('http://localhost/main.php?test_name=mri_upload'));
+        $this->webDriver->get('http://localhost/main.php?test_name=mri_upload');
         $bodyText = $this->webDriver->findElement(WebDriverBy::cssSelector("body"))->getText();
         $this->assertContains("mri_upload", $bodyText);
     }
