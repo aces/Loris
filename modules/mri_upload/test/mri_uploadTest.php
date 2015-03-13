460754baea632a01e371c2a9d82371e16b0299da
zia <zia.mohades@gmail.com>
2015-03-13 15:59:42 -0400

    modified the test-case

--- modules/mri_upload/test/mri_uploadTest.php
+++ modules/mri_upload/test/mri_uploadTest.php
@@ -2,25 +2,11 @@
 require_once __DIR__ . "/../../../test/integrationtests/LorisIntegrationTest.class.inc";
 class mri_uploadTestIntegrationTest extends LorisIntegrationTest
 {
-    protected $webDriver;
-
-    public function setUp()
-    {
-        $this->webDriver = RemoteWebDriver::create(
-            'http://localhost:4444/wd/hub',
-            array(\WebDriverCapabilityType::BROWSER_NAME => 'firefox')
-        );
-
-        $this->webDriver->get('http://localhost/main.php?test_name=mri_upload');
-
-        print "Page source: " . $this->webDriver->getPageSource();
-
-    }
-    
     function testMriUploadDoespageLoad()
     {
-       $bodyText = $this->webDriver->findElement(WebDriverBy::cssSelector("body"))->getText();
-       $this->assertContains("mri_upload", $bodyText);
+        $this->webDriver->get('http://localhost/main.php?test_name=mri_upload'));
+        $bodyText = $this->webDriver->findElement(WebDriverBy::cssSelector("body"))->getText();
+        $this->assertContains("mri_upload", $bodyText);
     }
 
     public function tearDown()
