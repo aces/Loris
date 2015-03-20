7d3dc21e0cdee5d701efce303f1dd1dbe6af8cdd
Tara Campbell <tara0campbell@gmail.com>
2015-03-20 14:14:38 -0400

    Ran phpcs on the dashboard test.

--- modules/dashboard/test/DashboardTest.php
+++ modules/dashboard/test/DashboardTest.php
@@ -1,14 +1,46 @@
 <?php
-require_once __DIR__ . "/../../../test/integrationtests/LorisIntegrationTest.class.inc";
+/**
+ * Dashboard automated integration tests
+ *
+ * PHP Version 5
+ *
+ * @category Test
+ * @package  Loris
+ * @author   Tara Campbell <tara.campbell@mail.mcgill.ca>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link     https://github.com/aces/Loris
+ */
+
+require_once __DIR__ .
+    "/../../../test/integrationtests/LorisIntegrationTest.class.inc";
+
+/**
+ * Dashboard module automated integration tests
+ *
+ * PHP Version 5
+ *
+ * @category Test
+ * @package  Loris
+ * @author   Tara Campbell <tara.campbell@mail.mcgill.ca>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link     https://github.com/aces/Loris
+ */
 class DashboardTest extends LorisIntegrationTest
 {
     protected $url = 'http://localhost/main.php?test_name=dashboard';
 
+    /**
+     * Tests that, when loading the Dashboard, the word "Welcome" appears
+     * in the welcome panel
+     *
+     * @return void
+     */
     public function testDashboardPageLoads()
     {
         $this->webDriver->get($this->url);
 
-        $welcomeText = $this->webDriver->findElement(WebDriverBy::cssSelector(".welcome"))->getText();
+        $welcomeText = $this->webDriver
+            ->findElement(WebDriverBy::cssSelector(".welcome"))->getText();
         $this->assertContains("Welcome", $welcomeText);
     }
 }
