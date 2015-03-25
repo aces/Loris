c1425d2da40bb060f3716c4fc588515035d835e8
Ted Strauss <ted.strauss@gmail.com>
2015-03-25 14:31:16 -0400

    phpcs failing > fix

--- modules/genomic_browser/test/genomic_browserTest.php
+++ modules/genomic_browser/test/genomic_browserTest.php
@@ -41,14 +41,16 @@ class GenomicBrowserTestIntegrationTest extends LorisIntegrationTest
     }
 
     /**
-     * Tests that, when loading the genomic_browser module > snp_browser submenu, some
+     * Tests that, when loading the genomic_browser module
+     *   > snp_browser submenu, some
      * text appears in the body.
      *
      * @return void
      */
     function testGenomicBrowserSnpBrowserDoespageLoad()
     {
-        $this->webDriver->get($this->url . "?test_name=genomic_browser&submenu=snp_browser");
+        $this->webDriver->get($this->url
+            . "?test_name=genomic_browser&submenu=snp_browser");
         $bodyText = $this->webDriver
             ->findElement(WebDriverBy::cssSelector("body"))->getText();
         $this->assertContains("Genomic Browser", $bodyText);
