18664d17897add799e4bab2f4bdf6668a63e5ed1
Ted Strauss <ted.strauss@gmail.com>
2015-03-27 12:56:09 -0400

    mri_violations upgrades

--- SQL/2015-02-26-ViolationCheckedFlag.sql
+++ SQL/2015-02-26-ViolationCheckedFlag.sql
@@ -2,12 +2,14 @@
 --
 -- Table structure for table `violations_resolved`
 --
-CREATE TABLE `violations_resolved` (
+DROP TABLE IF EXISTS `violations_resolved`;
+CREATE TABLE IF NOT EXISTS `violations_resolved` (
   `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `hash` varchar(255) NOT NULL,
   `ExtID` bigint(20) NOT NULL,
-  `TypeTable` varchar(255) DEFAULT NULL,
+  `TypeTable` varchar(255),
   `User` varchar(255) DEFAULT NULL,
   `ChangeDate` date DEFAULT NULL,
-  `Resolved` enum('0','1') DEFAULT '1',
+  `Resolved` enum('0','1','2','3','4','5','6','7','8','9') DEFAULT '0',
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
