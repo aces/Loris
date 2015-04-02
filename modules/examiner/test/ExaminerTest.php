9e7df04afb771c6f0f709220a2dc5b65b6598fdd
Tara Campbell <tara0campbell@gmail.com>
2015-04-02 14:59:35 -0400

    Add a test for edit examienr subtest.

--- modules/examiner/test/ExaminerTest.php
+++ modules/examiner/test/ExaminerTest.php
@@ -41,5 +41,19 @@ class ExaminerTest extends LorisIntegrationTest
             ->findElement(WebDriverBy::cssSelector("body"))->getText();
         $this->assertContains("Examiner", $bodyText);
     }
+
+    /**
+     * Tests that, when loading the examiner module, the word "Examiner" appears
+     * somewhere in the body
+     *
+     * @return void
+     */
+    public function testEditExaminerPageLoads()
+    {
+        $this->webDriver->get($this->url . "?test_name=examiner&subtest=editExaminer");
+        $bodyText = $this->webDriver
+            ->findElement(WebDriverBy::cssSelector("body"))->getText();
+        $this->assertContains("Edit Examiner", $bodyText);
+    }
 }
 ?>
