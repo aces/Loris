657b8716384af65434d9a0717dc49d806e3f53c0
Rathi <sekaranrathi@gmail.com>
2015-04-29 17:04:23 -0400

    Updating function call to use click attribute

--- modules/mri_upload/js/mri_upload.js
+++ modules/mri_upload/js/mri_upload.js
@@ -98,8 +98,8 @@ function progressHandler(event) {
 function uploadFile() {
     "use strict";
     //getMessage();
-    $("#progressbar").show();
     var formData = new FormData($("#mri_upload")[0]);
+    $("#progressbar").show();
 
     $.ajax({
         type: 'POST',
@@ -136,8 +136,8 @@ $(function () {
     change();
     $("#progressbar").hide();
 
-    $("#mri_upload").submit(
-        function (e) {
+    $("#upload").click(
+            function (e) {
             e.preventDefault();
             var time = getCurrentTime();
 //            $("#log_box").html(time + " Preparing... <br>");
