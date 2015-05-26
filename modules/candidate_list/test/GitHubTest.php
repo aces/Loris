bc5159c9bfaf14a15e2b1a759e8fc638470c44a2
ruichao zhang <zrc19950205@gmail.com>
2015-05-26 16:27:38 -0400

    unittest for candidate

---
+++ modules/candidate_list/test/GitHubTest.php
@@ -0,0 +1,25 @@
+<?php
+class GitHubTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * @var \RemoteWebDriver
+     */
+    protected $webDriver;
+
+    public function setUp()
+    {
+        $capabilities = array(\WebDriverCapabilityType::BROWSER_NAME => 'firefox');
+        $this->webDriver = RemoteWebDriver::create('http://localhost:4444/wd/hub', $capabilities);
+    }
+
+    protected $url = 'https://github.com';
+
+    public function testGitHubHome()
+    {
+        $this->webDriver->get($this->url);
+        // checking that page title contains word 'GitHub'
+        $this->assertContains('GitHub', $this->webDriver->getTitle());
+    }    
+
+}
+?>
