9bb1049f4a1776b466af30f1ba8102420a6d0697
karolina.marasinska <karolinam.mcin@gmail.com>
2015-06-10 17:32:29 -0400

    for add_instrument, create feedback type Other if it is missing

--- tools/fix_timepoint_date_problems.php
+++ tools/fix_timepoint_date_problems.php
@@ -313,13 +313,21 @@ function addInstrument($sessionID, $testName)
      */
     // feedback object
     print $user->getUsername();
-    $feedback =& NDB_BVL_Feedback::singleton($user->getUsername(), null, $sessionID, $commentID);
+    $feedback =& NDB_BVL_Feedback::singleton($user->getUsername(), null, $sessionID);
     if (PEAR::isError($feedback)) {
         return PEAR::raiseError("Failed to create feedback object: " . $feedback->getMessage());
     }
 
+    //get thread feedback type
+    $threadFeedbackType = $feedback->getFeedbackTypeIdByName('other');
+    if (empty($threadFeedbackType))
+    {
+        //create thread feedback type "Other", if it does not exist
+        $threadFeedbackType = $feedback->createFeedbackType("Other", "Other");
+    }
+
     // add the new thread
-    $success = $feedback->createThread('instrument', '5', "Instrument ($testName) has been added to the battery. You may now complete data entry for this instrument. Please respond to this feedback to acknowledge the changes.", 'Y');
+    $success = $feedback->createThread('instrument', $threadFeedbackType, "Instrument ($testName) has been added to the battery. You may now complete data entry for this instrument. Please respond to this feedback to acknowledge the changes.", 'Y');
     if (PEAR::isError($success)) {
         return PEAR::raiseError("Failed to create feedback: ". $success->getMessage());
     }
