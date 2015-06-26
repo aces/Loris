0b3da4851679c4d4f9a4a124fa3a4e7d70142b15
Xavier <xavierlb.mavan@gmail.com>
2015-06-26 16:58:54 -0400

    even

---
+++ modules/genomic_browser/js/genomic_browser.js
@@ -0,0 +1,27 @@
+function hideFilterCandidate() {
+    "use strict";
+    $("#panel-body-candidate").toggle();
+    $("#down-candidate").toggle();
+    $("#up-candidate").toggle();
+}
+
+function hideFilterGene() {
+    "use strict";
+    $("#panel-body-gene").toggle();
+    $("#down-gene").toggle();
+    $("#up-gene").toggle();
+}
+
+function hideFilterSNP() {
+    "use strict";
+    $("#panel-body-snp").toggle();
+    $("#down-snp").toggle();
+    $("#up-snp").toggle();
+}
+
+function hideFilterCNV() {
+    "use strict";
+    $("#panel-body-cnv").toggle();
+    $("#down-cnv").toggle();
+    $("#up-cnv").toggle();
+}
