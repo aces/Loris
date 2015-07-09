271f920bbc573cd487f59ae9880611135ff06030
karolina.marasinska <karolinam.mcin@gmail.com>
2015-07-09 17:44:06 -0400

    experimenting with integration test bash script

--- test/integration.sh
+++ test/integration.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 # This script can be used to run integration tests
 
-# set environment variable LORIS_DB_CONFIG to test config.xml file
-LORIS_DB_CONFIG=/var/www/loris/test/config.xml
+# The following needs to be set up for the test to work:
+#   - create a LorisTest DB from the default schema (SQL/0000-00-00-schema.sql)
+#   - create a MySQL user SQLTestUser with password ??????
 
+# set environment variable LORIS_DB_CONFIG which should be the path to test config.xml file
+export LORIS_DB_CONFIG=/var/www/loris/test/config.xml
+
+#start PHP's built in webserver
 php -S localhost:8000 -t htdocs 2>1 > /dev/null &
 
 # Start Selenium and redirect Selenium WebDriver
@@ -11,11 +16,17 @@ php -S localhost:8000 -t htdocs 2>1 > /dev/null &
 # screen in the middle of our other tests
 #java -jar /home/kmarasinska/Selenium_server/selenium-server-standalone-2.45.0.jar > /dev/null &
 
-mysql -D LorisTest -u SQLTestUser -pkarolina -e "UPDATE Config SET Value='http://localhost:8000' WHERE ConfigID=(SELECT ID FROM ConfigSettings WHERE Name='url')"
+# Set config values in LorisTest DB
+#mysql -D LorisTest -u SQLTestUser -pkarolina -e "UPDATE Config SET Value='http://localhost:8000' WHERE ConfigID=(SELECT ID FROM ConfigSettings WHERE Name='url')"
+mysql -D LorisTest -u SQLTestUser -pkarolina -e
+"UPDATE Config
+INNER JOIN ConfigSettings ON Config.`ConfigID` = ConfigSettings.ID
+SET Config.Value='http://localhost:8000'
+WHERE ConfigSettings.Name='url"
 
 
 # set environment variable LORIS_DB_CONFIG to test config.xml file
-LORIS_DB_CONFIG=/var/www/loris/test/config.xml
+#LORIS_DB_CONFIG=/var/www/loris/test/config.xml
 
 # Run integration tests
-vendor/bin/phpunit --configuration /var/www/loris/test/phpunit.xml modules/candidate_list/test
+vendor/bin/phpunit --configuration /var/www/loris/test/phpunit.xml ../modules/candidate_list/test
