12454f697b3f02f2f6f969c279caf5eb51495190
jstirling91 <jstirl@abunawas-2.local>
2015-07-15 13:17:38 -0400

    make first pass at making instrument builder into a react app

---
+++ modules/instrument_builder/js/react/react.elements.js
@@ -0,0 +1,139 @@
+HeaderElement = React.createClass({
+	render: function(){
+		return (
+			<h2>{this.props.header}</h2>
+		)
+	}
+});
+
+LabelElement = React.createClass({
+	render: function(){
+		return (
+			<p>{this.props.label}</p>
+		)
+	}
+});
+
+ScoredElement = React.createClass({
+	render: function(){
+		return (
+			<div>
+			<label className="lab col-sm-4 col-xs-12">
+				{this.props.label}
+			</label>
+			<div className="col-sm-8">
+				<div className="col-xs-12 element">
+					0
+				</div>
+			</div>
+			</div>
+		)
+	}
+});
+
+TextboxElement = React.createClass({
+	render: function(){
+		return (
+			<div>
+			<label className="lab col-sm-4 col-xs-12">
+				{this.props.label}
+			</label>
+			<div className="col-sm-8">
+				<div className="col-xs-12 col-sm-6 element">
+					<input type="text" className="form-control input-sm user-success" />
+				</div>
+			</div>
+			</div>
+		)
+	}
+});
+
+TextareaElement = React.createClass({
+	render: function(){
+		return (
+			<div>
+			<label className="lab col-sm-4 col-xs-12">
+				{this.props.label}
+			</label>
+			<div className="col-sm-8">
+				<div className="col-xs-12 col-sm-6 element">
+					<textarea cols="25" rows="4" className="form-control input-sm user-success"></textarea>
+				</div>
+			</div>
+			</div>
+		)
+	}
+});
+
+SelectElement = React.createClass({
+	render: function(){
+		var multiple = '';
+		if(this.props.multiple){
+			multiple = 'multiple';
+		}
+		return (
+			<div>
+			<label className="lab col-sm-4 col-xs-12">
+				{this.props.label}
+			</label>
+			<div className="col-sm-8">
+				<div className="col-xs-12 col-sm-6 element">
+					<select multiple={multiple} className="form-control input-sm">
+						{this.props.options.map(function(option){
+							return (
+								<option>
+									{option}
+								</option>
+							)
+						})}
+					</select>
+				</div>
+			</div>
+			</div>
+		)
+	}
+});
+
+DateElement = React.createClass({
+	render: function(){
+		var max = this.props.maxYear + "-12-31",
+			min = this.props.minYear + "-01-01";
+		return (
+			<div>
+			<label className="lab col-sm-4 col-xs-12">
+				{this.props.label}
+			</label>
+			<div className="col-sm-8">
+				<div className="col-xs-12 col-sm-6 element">
+					<input type="date"
+						   className="form-control input-sm user-success" 
+						   min={min}
+						   max={max}
+				    />
+				</div>
+			</div>
+			</div>
+		)
+	}
+});
+
+NumericElement = React.createClass({
+	render: function(){
+		return (
+			<div>
+			<label className="lab col-sm-4 col-xs-12">
+				{this.props.label}
+			</label>
+			<div className="col-sm-8">
+				<div className="col-xs-12 col-sm-6 element">
+					<input type="number"
+						   className="form-control input-sm user-success" 
+						   min={this.props.min}
+						   max={this.props.max}
+				    />
+				</div>
+			</div>
+			</div>
+		)
+	}
+});
