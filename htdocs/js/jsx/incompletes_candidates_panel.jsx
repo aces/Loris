110883d0b8b63deabd83e3add1bb7cfe05cf3cc2
Evan McIlroy <evanmcilroy@gmail.com>
2015-07-23 16:47:04 -0400

    this close!

--- htdocs/js/jsx/incompletes_candidates_panel.jsx
+++ htdocs/js/jsx/incompletes_candidates_panel.jsx
@@ -1,7 +1,18 @@
-/* var data = [
-	{feedback: "this is feedback"},
-	{feedback: "this is another feedback"}
-] */
+/*The config for the pagination of tables */
+var Config = React.createClass({
+  render: function() {
+    return <div>
+      <h2>Config</h2>
+      <label htmlFor="pageSize">Page Size:</label>
+      <select id="pageSize" value={this.props.pageSize} onChange={this.props.handlePageSizeChange}>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+      </select>
+	  </div>;
+  }
+});
 
 var IncompleteCandidatesTable = React.createClass({
 	propTypes: {
@@ -10,17 +21,18 @@ var IncompleteCandidatesTable = React.createClass({
 	},
 	getInitialState: function(){
 		return{
-			pageSize: 10
+			pageSize: 15,
+			currentPage: 1
 		}
 	},
-	componentWillReceiverProps: function(nextProps) {
+	componentWillReceiveProps: function(nextProps) {
 		this.setState({
 			currentPage: 1
 		})
 	},
 	getPage: function(){
-		var start = this.props.pageSize * (this.state.currentPage - 1);
-		var end = start + this.props.pageSize;
+		var start = this.state.pageSize * (this.state.currentPage - 1);
+		var end = start + this.state.pageSize;
 
 		return{
 			currentPage: this.state.currentPage,
@@ -34,8 +46,8 @@ var IncompleteCandidatesTable = React.createClass({
 		}	
 	},
 	getNumPages: function() {
-		var numPages = Math.floor(this.props.incomplete_candidates.length / this.props.pageSize);
-		if (this.props.incomplete_candidates.length % this.props.pageSize > 0){
+		var numPages = Math.floor(this.props.incomplete_candidates.length / this.state.pageSize);
+		if (this.props.incomplete_candidates.length % this.state.pageSize > 0){
 			numPages++
 		}
 		return numPages			
@@ -44,75 +56,70 @@ var IncompleteCandidatesTable = React.createClass({
 		this.setState({currentPage: pageNum})
 	},
 	render: function(){
+		var page = this.getPage();
+		var topics = page.incomplete_candidates.map(function(topic) {
+			return
+			<h1>hi</h1>
+		});
+		console.log({topics});
 		return(
-			<table className="table table-hover table-bordered dynamictable">
+
+			<table className="table table-hover table-primary table-bordered colm-freeze">
 			  <thead>
-			<tr className="info">
-			<th>Candidate ID</th>
-			      <th>Visit Label</th>
-			<th>Instrument</th>
-			<th>Fieldname</th>
+			    <tr className="info">
+			      <td>candid</td>
+			      <td>test_name</td>
+			      <td>vist_label</td>
 			    </tr>
 			  </thead>
 			  <tbody>
-			    {this.props.incomplete_candidates.map(function(candidate){
-				    return(
-					    <div className="PaginationLinks">
-					    <tr>
-					      <td>{candidate.candid}</td>
-					      <td>{candidate.visit_label}</td>
-					      <td>{candidate.test_name}</td>
-					      <td>{candidate.commentid}</td>
-					    </tr>
-					    </div>
-				    ) 
-			     })}
+			    {topics}
+
 			  </tbody>
 			</table>
-		);
-	}
-});
-
+				
+	)}
+}); 
 
 /**
  * Renders a pager component.
  */
 function pager(page) {
-  var pageLinks = []
-  if (page.currentPage > 1) {
-    if (page.currentPage > 2) {
-      pageLinks.push(<span className="pageLink" onClick={page.handleClick(1)}>«</span>)
-      pageLinks.push(' ')
-    }
-    pageLinks.push(<span className="pageLink" onClick={page.handleClick(page.currentPage - 1)}>‹</span>)
-    pageLinks.push(' ')
-  }
-  pageLinks.push(<span className="currentPage">Page {page.currentPage} of {page.numPages}</span>)
-  if (page.currentPage < page.numPages) {
-    pageLinks.push(' ')
-    pageLinks.push(<span className="pageLink" onClick={page.handleClick(page.currentPage + 1)}>›</span>)
-    if (page.currentPage < page.numPages - 1) {
-      pageLinks.push(' ')
-      pageLinks.push(<span className="pageLink" onClick={page.handleClick(page.numPages)}>»</span>)
-    }
-  }
-  return <div>{pageLinks}</div>
+	var pageLinks = []
+	if (page.currentPage > 1) {
+		if (page.currentPage > 2) {
+			pageLinks.push(<span className="pageLink" onClick={page.handleClick(1)}>«</span>)
+				pageLinks.push(' ')
+		}
+		pageLinks.push(<span className="pageLink" onClick={page.handleClick(page.currentPage - 1)}>‹</span>)
+			pageLinks.push(' ')
+	}
+	pageLinks.push(<span className="currentPage">Page {page.currentPage} of {page.numPages}</span>)
+		if (page.currentPage < page.numPages) {
+			pageLinks.push(' ')
+				pageLinks.push(<span className="pageLink" onClick={page.handleClick(page.currentPage + 1)}>›</span>)
+				if (page.currentPage < page.numPages - 1) {
+					pageLinks.push(' ')
+						pageLinks.push(<span className="pageLink" onClick={page.handleClick(page.numPages)}>»</span>)
+				}
+		}
+	return <div>{pageLinks}</div>
 }
 
-var DefaultPanel = React.createClass({displayName: 'CandidatesPanelTable',
-	propTypes: {
-		'incomplete_candidates' : React.PropTypes.array
-	},
-	render: function(){
-		return (
-			<div className="panel panel-primary">
-			  <div className="panel-heading">Incomplete Candidates</div>
-			  <div className="panel-body">
-			    <IncompleteCandidatesTable incomplete_candidates={this.props.incomplete_candidates}/>
-			  </div>
-			</div>
-		);
-	}
-});
+			var DefaultPanel = React.createClass({displayName: 'CandidatesPanelTable',
+							      propTypes: {
+								      'incomplete_candidates' : React.PropTypes.array
+							      },
+							      render: function(){
+								      return (
+									      <div className="panel panel-primary">
+									      <div className="panel-heading">Incomplete Candidates</div>
+									      <div className="panel-body">
+									      <IncompleteCandidatesTable incomplete_candidates={this.props.incomplete_candidates}/>
+									      </div>
+									      </div>
+								      );
+							      }
+							      });
 
-IncompleteCandidatesPanel = React.createFactory(DefaultPanel);
+									      IncompleteCandidatesPanel = React.createFactory(DefaultPanel);
