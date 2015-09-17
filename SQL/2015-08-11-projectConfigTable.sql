a396307c3a3e3125a8afc8bf6245bdde17cff9b1
root <brubo1992@gmail.com>
2015-09-17 18:16:59 -0400

    added sql patch for the project table

---
+++ SQL/2015-08-11-projectConfigTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `Project` (
+    `ProjectID` INT(2) Default NULL,
+    `Name` VARCHAR(255) NULL,
+    `recruitmentTarget` INT(6) Default NULL,
+    PRIMARY KEY (`ProjectID`)
+)ENGINE = InnoDB  DEFAULT CHARSET=utf8;
+
