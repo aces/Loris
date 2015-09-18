5f183a45004d7bdadcba9819082e6b5a58c7cdf9
Evan McIlroy <evanmcilroy@gmail.com>
2015-09-18 19:28:15 -0400

    Get candidates phpcs fixed.

--- modules/data_team_helper/ajax/GetCandidates.php
+++ modules/data_team_helper/ajax/GetCandidates.php
@@ -1,24 +1,40 @@
 <?php
+/**
+ * This is a file called use by the behavioural qc browser
+ * auto-complete to retrieve candidates for a given
+ * instrument and/or visit.
+ *
+ * PHP Version 5
+ *
+ * @category Behavioural
+ * @package  Loris
+ * @author   Evan McIlroy <evanmcilroy@gmail.com>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link     https://www.github.com/aces/Loris/
+ */
 header("content-type:application/json");
 ini_set('default_charset', 'utf-8');
 
 require_once "Database.class.inc";
 require_once "NDB_Client.class.inc";
 
-//TODO: create different querys for instrument and visit set
-
-$db =& Database::singleton();
+$db          =& Database::singleton();
 $searchArray = array();
 
-    $query = "SELECT DISTINCT ses.candID FROM session AS ses JOIN test_battery
-                AS tst ON ses.Visit_label = tst.Visit_label WHERE ses.candID LIKE :searchTerm";
+    $query = "SELECT DISTINCT ses.candID FROM session AS ses
+                JOIN test_battery AS tst
+                ON ses.Visit_label = tst.Visit_label
+                WHERE ses.candID
+                LIKE :searchTerm";
 
     $searchArray[':searchTerm'] = $_REQUEST["query"] . '%';
 
-//Instrument is set adding that to the query. 
-if (isset($_REQUEST['instrument']) && !empty($_REQUEST['instrument']) && $_REQUEST['instrument'] != 'All Instruments'){
-    $query .= " AND tst.Test_Name = :NAM";
-    $testname = Utility::getTestNameUsingFullName($_REQUEST['instrument']);    
+//Instrument is set adding that to the query.
+if (isset($_REQUEST['instrument']) && !empty($_REQUEST['instrument'])
+    && $_REQUEST['instrument'] != 'All Instruments'
+) {
+    $query   .= " AND tst.Test_Name = :NAM";
+    $testname = Utility::getTestNameUsingFullName($_REQUEST['instrument']);
     $searchArray['NAM'] = $testname;
 }
 
@@ -26,10 +42,9 @@ $result = $db->pselect($query, $searchArray);
 
 $flattened_result = array();
 
-if ($result == null){
+if ($result == null) {
     $flattened_result[0] = "Not found";
-}
-else {
+} else {
     foreach ($result as $k => $v) {
         $flattened_result[$k] = $v['candID'];
     }
