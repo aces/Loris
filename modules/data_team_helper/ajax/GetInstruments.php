c1e8baa0a62e115329b7022752571c35a8ac9afc
Evan McIlroy <evanmcilroy@gmail.com>
2015-09-18 12:35:25 -0400

    Initial commit.

--- modules/data_team_helper/ajax/GetInstruments.php
+++ modules/data_team_helper/ajax/GetInstruments.php
@@ -1,5 +1,6 @@
 <?php
 /* This is used by the data_team_helper module */
+header("content-type:application/json");
 ini_set('default_charset', 'utf-8');
 require_once "Database.class.inc";
 require_once 'NDB_Config.class.inc';
@@ -11,12 +12,42 @@ $client->initialize();
 
 require_once "Utility.class.inc";
 
+//Array containing the JSON information to return.
+$flattened_result = array();
 //gets the given visit_label and returns the instrument
 
-$instruments = Utility::getVisitInstruments($_REQUEST['visit_label']);
+//If all visits are selected return all visits
+if ($_REQUEST['visit_label'] == 'All Visits'){
+    $instruments = Utility::getAllInstruments();
+    array_unshift($instruments, "All Instruments");
 
-print "All Instruments\n";
-foreach($instruments as $instrument){
-    print $instrument['Test_name_display'] . "\n";
+    $counter = 0;
+    foreach ($instruments as $k => $v) {
+        $flattened_result[$counter] = $v;
+        $counter++;
+    }
+
+    print json_encode($flattened_result);
+    exit();
 }
-?>
+
+//else a specific visit is set and only return visit instruments 
+else{
+    $instruments = Utility::getVisitInstruments($_REQUEST['visit_label']);
+//flattening the array result for proper json encoding
+    if ($instruments == null){
+        $flattened_result[0] = "No instruments for this visit";
+        print json_encode($flattened_result);
+        exit();
+    }
+    else{
+        foreach ($instruments as $k => $v) {
+            $flattened_result[$k] = $v['Test_name_display'];
+        }
+        array_unshift($flattened_result, "All Instruments");
+        print json_encode($flattened_result);
+        exit();
+    }
+}
+
+
