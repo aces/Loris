92e9d2fe24ee23669545c5af2c55efdfd0fff1a3
Xavier <xavierlb.mavan@gmail.com>
2015-10-02 10:05:50 -0400

    phpcs

--- htdocs/request_account/process_new_account.php
+++ htdocs/request_account/process_new_account.php
@@ -40,10 +40,11 @@ session_start();
 $tpl_data = array();
 
 // create an instance of the config object
-$config           = NDB_Config::singleton();
-$tpl_data['baseurl']  = $config->getSetting('url');
-$tpl_data['css']  = $config->getSetting('css');
-$tpl_data['rand'] = rand(0, 9999);
+$config = NDB_Config::singleton();
+
+$tpl_data['baseurl']     = $config->getSetting('url');
+$tpl_data['css']         = $config->getSetting('css');
+$tpl_data['rand']        = rand(0, 9999);
 $tpl_data['success']     = false;
 $tpl_data['study_title'] = $config->getSetting('title');
 $tpl_data['currentyear'] = date('Y');
