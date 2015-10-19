37ab7f5550c3aad2411e51813654bcecfa37789a
jstirling91 <jstirl@abunawas-2.local>
2015-10-19 14:21:24 -0400

    update configuration ajax scripts to check basic permissions

--- modules/configuration/ajax/updateSubproject.php
+++ modules/configuration/ajax/updateSubproject.php
@@ -12,6 +12,12 @@
  * @link     https://github.com/aces/Loris
  */
 
+$user =& User::singleton();
+if (!$user->hasPermission('config')) {
+    header("HTTP/1.1 403 Forbidden");
+    exit;
+}
+
 require_once __DIR__ . "/../../../vendor/autoload.php";
 $client = new NDB_Client();
 $client->makeCommandLine();
