87804459d81339599754d52d7d876993267ed793
Jkat <fission@live.com>
2015-10-26 09:10:05 -0400

    Autoloaded so not necessary to require these

--- tools/generate_project_statistics_csv.php
+++ tools/generate_project_statistics_csv.php
@@ -8,9 +8,6 @@
 
 set_include_path(get_include_path().":../php/libraries:");
 require_once __DIR__ . "/../vendor/autoload.php";
-require_once "NDB_Client.class.inc";
-require_once "Utility.class.inc";
-require_once "Database.class.inc";
 
 $client = new NDB_Client();
 $client->makeCommandLine();
