80f5d8d6a278cad16c6cba95cbd2a239337c0088
Xavier <xavierlb.mavan@gmail.com>
2015-11-18 16:10:34 -0500

    LorisMenuPermission added for DICOM Archive, Imaging Browser, Statistics and Document Repository

---
+++ SQL/2015-11-18-Missing_LorisMenuPermission.sql
@@ -0,0 +1,10 @@
+INSERT IGNORE INTO LorisMenuPermissions (MenuID, PermID)
+    SELECT m.ID, p.PermID FROM permissions p CROSS JOIN LorisMenu m WHERE p.code='dicom_archive_view_allsites' AND m.Label='DICOM Archive';
+INSERT IGNORE INTO LorisMenuPermissions (MenuID, PermID)
+    SELECT m.ID, p.PermID FROM permissions p CROSS JOIN LorisMenu m WHERE p.code='imaging_browser_view_site' AND m.Label='Imaging Browser';
+INSERT IGNORE INTO LorisMenuPermissions (MenuID, PermID)
+    SELECT m.ID, p.PermID FROM permissions p CROSS JOIN LorisMenu m WHERE p.code='imaging_browser_view_allsites' AND m.Label='Imaging Browser';
+INSERT IGNORE INTO LorisMenuPermissions (MenuID, PermID)
+    SELECT m.ID, p.PermID FROM permissions p CROSS JOIN LorisMenu m WHERE p.code='data_entry' AND m.Label='Statistics';
+INSERT IGNORE INTO LorisMenuPermissions (MenuID, PermID) 
+    SELECT m.ID, p.PermID FROM permissions p CROSS JOIN LorisMenu m WHERE p.code='document_repository_view' AND m.Label='Document Repository';
