03612ff436417774b19005ae21d5ab5ed7f9e97e
Dave MacFarlane <david.macfarlane2@mcgill.ca>
2015-11-26 15:14:23 -0500

    Update URLs for clinical menu

--- SQL/2015-11-26-MenuPrettyURLs.sql
+++ SQL/2015-11-26-MenuPrettyURLs.sql
@@ -1,3 +1,8 @@
 update LorisMenu SET Link='/candidate_list/' WHERE Link='main.php?test_name=candidate_list';
 update LorisMenu SET Link='/new_profile/' WHERE Link='main.php?test_name=new_profile';
 
+update LorisMenu SET Link='/reliability/' WHERE Link="main.php?test_name=reliability";
+update LorisMenu SET Link='/conflict_resolver/' WHERE Link="main.php?test_name=conflict_resolver";
+update LorisMenu SET Link='/examiner/' WHERE Link="main.php?test_name=examiner";
+update LorisMenu SET Link='/training/' WHERE Link="main.php?test_name=training";
+
