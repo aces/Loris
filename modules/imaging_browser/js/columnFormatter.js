15b4b5b1efc244cce72a252b41919d71be680f73
Dave MacFarlane <david.macfarlane2@mcgill.ca>
2015-12-04 16:37:36 -0500

    Fixed formatting of table in imaging_browser

--- modules/imaging_browser/js/columnFormatter.js
+++ modules/imaging_browser/js/columnFormatter.js
@@ -3,17 +3,38 @@ function formatColumn(column, cell, rowData) {
         if (cell === 'new') {
             return React.createElement(
                 'td',
-                null,
+                { className: 'newdata' },
                 'NEW'
             );
         }
         return React.createElement('td', null);
     }
     if (column === 'Links') {
+        var cellTypes = cell.split(",");
+        var cellLinks = [];
+        for (var i = 0; i < cellTypes.length; i += 1) {
+            cellLinks.push(React.createElement(
+                'a',
+                { href: loris.BaseURL + "/imaging_browser/viewSession/?sessionID=" + rowData[11] + "&outputType=" + cellTypes[i] },
+                'all types'
+            ));
+            cellLinks.push(" ");
+        }
+        cellLinks.push(React.createElement(
+            'a',
+            { href: loris.BaseURL + "/imaging_browser/viewSession/?sessionID=" + rowData[11] + "&selectedOnly=1" },
+            'selected'
+        ));
+        cellLinks.push(" ");
+        cellLinks.push(React.createElement(
+            'a',
+            { href: loris.BaseURL + "/imaging_browser/viewSession/?sessionID=" + rowData[11] },
+            'all types'
+        ));
         return React.createElement(
             'td',
             null,
-            'Hiyoooo'
+            cellLinks
         );
     }
     return React.createElement(
--- modules/imaging_browser/jsx/columnFormatter.js
+++ modules/imaging_browser/jsx/columnFormatter.js
@@ -1,12 +1,26 @@
 function formatColumn(column, cell, rowData) {
     if(column === 'New Data') {
         if(cell === 'new') {
-            return <td>NEW</td>
+            return <td className="newdata">NEW</td>
         }
         return <td></td>;
     }
     if(column === 'Links') {
-        return <td>Hiyoooo</td>;
+        var cellTypes = cell.split(",");
+        var cellLinks = []
+        for(var i = 0; i < cellTypes.length; i += 1) {
+            cellLinks.push(<a href={loris.BaseURL + "/imaging_browser/viewSession/?sessionID=" + rowData[11] + "&outputType=" + cellTypes[i]}>all types</a>);
+            cellLinks.push(" ");
+
+        }
+        cellLinks.push(<a href={loris.BaseURL + "/imaging_browser/viewSession/?sessionID=" + rowData[11] + "&selectedOnly=1"}>selected</a> );
+        cellLinks.push(" ");
+        cellLinks.push(<a href={loris.BaseURL + "/imaging_browser/viewSession/?sessionID=" + rowData[11]}>all types</a> );
+        return (
+                <td>
+                    {cellLinks}
+                </td>
+               );
     }
     return <td>{cell}</td>;
 }
