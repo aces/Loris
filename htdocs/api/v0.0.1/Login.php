6cff6e4feb1a4d5b4ee00b24a5b0fea86c2268df
Dave MacFarlane <david.macfarlane2@mcgill.ca>
2015-12-17 16:22:09 -0500

    Ran PHPCS

--- htdocs/api/v0.0.1/Login.php
+++ htdocs/api/v0.0.1/Login.php
@@ -1,13 +1,42 @@
 <?php
+/**
+ * Handles a request to the login API endpoint in Loris
+ *
+ * PHP Version 5.4+
+ *
+ * @category Main
+ * @package  API
+ * @author   Dave MacFarlane <david.macfarlane2@mcgill.ca>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link     https://www.github.com/aces/Loris/
+ */
+
 namespace Loris\API;
 set_include_path(get_include_path() . ":" . __DIR__);
 require_once __DIR__ . '/APIBase.php';
 require_once __DIR__ . '/../../../vendor/autoload.php';
-class Login extends APIBase {
-    function __construct($method, $data = array()) {
-        $this->AllowedMethods = array(
-            'POST',
-        );
+
+/**
+ * Implementation of Login endpoint
+ *
+ * @category Main
+ * @package  API
+ * @author   Dave MacFarlane <david.macfarlane2@mcgill.ca>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link     https://www.github.com/aces/Loris/
+ */
+class Login extends APIBase
+{
+    /**
+     * Validates the data which was passed in the request.
+     *
+     * @param string $method The HTTP method of the request
+     * @param array  $data   The array-encode JSON posted to
+     *                       the URL.
+     */
+    function __construct($method, $data = array())
+    {
+        $this->AllowedMethods = array('POST');
         $this->RequestData    = $data;
 
         if (!in_array($method, $this->AllowedMethods)) {
@@ -16,7 +45,7 @@ class Login extends APIBase {
             $this->safeExit(0);
         }
 
-        $client  = new \NDB_Client();
+        $client = new \NDB_Client();
         // Bypass session handling because we're trying to authenticate a session
         $client->makeCommandLine();
         $client->initialize(__DIR__ . "/../../../project/config.xml");
@@ -25,12 +54,18 @@ class Login extends APIBase {
         $this->handleRequest();
     }
 
-    function handlePOST() {
-        if(empty($this->RequestData['username']) || empty($this->RequestData['password'])) {
+    /**
+     * Handle a POST request
+     *
+     * @return none
+     */
+    function handlePOST()
+    {
+        if (empty($this->RequestData['username'])
+            || empty($this->RequestData['password'])
+        ) {
             $this->header("HTTP/1.1 400 Bad Request");
-            $this->JSON = array(
-                "error" => "Missing username or password"
-            );
+            $this->JSON = array("error" => "Missing username or password");
             return;
 
         }
@@ -39,50 +74,74 @@ class Login extends APIBase {
 
         $login = $this->getLoginAuthenticator();
 
-        if($login->passwordAuthenticate($user, $password, false)) {
+        if ($login->passwordAuthenticate($user, $password, false)) {
             $this->JSON = array(
-                           "token" => $this->getEncodedToken($user)
+                           "token" => $this->getEncodedToken($user),
                           );
         } else {
             $this->header("HTTP/1.1 401 Unauthorized");
-            if(!empty($login->_lastError)) {
+            if (!empty($login->_lastError)) {
                 $this->JSON = array(
-                    "error" => $login->_lastError
-                    );
+                               "error" => $login->_lastError,
+                              );
             }
 
         }
     }
 
-    function getLoginAuthenticator() {
+    /**
+     * Get the SinglePointLogin class used to authenticate this request
+     * in a separate function so that it can be mocked out for testing.
+     *
+     * @return SinglePointLogin Loris Authenticator
+     */
+    function getLoginAuthenticator()
+    {
         return new \SinglePointLogin();
     }
 
-    function getEncodedToken($user) {
+    /**
+     * Return a valid JWT encoded identification token for the user
+     *
+     * @param string $user The user to return an identification token for
+     *
+     * @return string JWT encoded token
+     */
+    function getEncodedToken($user)
+    {
         $factory = \NDB_Factory::singleton();
-        $config = $factory->config();
+        $config  = $factory->config();
 
-        $www = $config->getSetting("www");
+        $www     = $config->getSetting("www");
         $baseURL = $www['url'];
 
         $token = array(
             // JWT related tokens to for the JWT library to validate
-            "iss"  => $baseURL,
-            "aud" => $baseURL,
+                  "iss"  => $baseURL,
+                  "aud"  => $baseURL,
             // Issued at
-            "iat" => time(),
-            "nbf" => time(),
+                  "iat"  => time(),
+                  "nbf"  => time(),
             // Expire in 1 day
-            "exp" => time() + 86400,
+                  "exp"  => time() + 86400,
             // Additional payload data
-            "user" => $user
-        );
+                  "user" => $user,
+                 );
 
         $key = $config->getSetting("JWTKey");
         return \Firebase\JWT\JWT::encode($token, $key, "HS256");
     }
 
-    function calculateETag() {
+    /**
+     * Calculate the ETag for this resource.
+     *
+     * Since this isn't a real resource, it just returns the
+     * empty string.
+     *
+     * @return empty string
+     */
+    function calculateETag()
+    {
         return;
     }
 }
