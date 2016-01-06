7a19b547802cc3e710102ae0d69cecc4a53a6a4d
Gregory Luneau <gluneau.mcin@gmail.com>
2016-01-06 12:16:49 -0500

    line formating

--- tools/assign_missing_instruments.php
+++ tools/assign_missing_instruments.php
@@ -127,8 +127,9 @@ function populateVisitLabel($result, $visit_label)
 }
 
 if (isset($visit_label)) {
-    $query   ="SELECT s.ID, s.subprojectID, s.CandID from session s 
-            LEFT JOIN candidate c USING (CandID) WHERE s.Active='Y'
+    $query   ="SELECT s.ID, s.subprojectID, s.CandID from session
+            s LEFT JOIN candidate c USING (CandID)
+            WHERE s.Active='Y'
             AND c.Active='Y' AND s.visit_label =:vl";
     $where   = array('vl' => $argv[1]);
     
