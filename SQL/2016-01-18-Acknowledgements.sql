b1094390de912885950c86eb3dce1daebf9aef37
Jkat <fission@live.com>
2016-01-18 13:16:59 -0500

    Acknowledgements SQL patch

--- SQL/2016-01-18-Acknowledgements.sql
+++ SQL/2016-01-18-Acknowledgements.sql
@@ -1,1 +1,21 @@
-todo
+DROP TABLE IF EXISTS `acknowledgements`;
+CREATE TABLE `acknowledgements` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(255) DEFAULT NULL,
+  `citation_name` varchar(255) DEFAULT NULL,
+  `title` enum('bachelors','masters','phd','postdoc','md','registered_nurse') DEFAULT NULL,
+  `start_date` date,
+  `end_date` date,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO LorisMenu (Label, OrderNumber) VALUES ('Acknowledgements', 7);
+INSERT INTO LorisMenu (Label, Link, Parent, OrderNumber) VALUES ('Acknowledgements','main.php?test_name=acknowledgements', (SELECT ID FROM LorisMenu as L WHERE Label='Acknowledgements'), 1);
+
+INSERT INTO permissions (permID,code,description,categoryID) VALUES (41,'acknowledgements_view','View Acknowledgements',2);
+INSERT INTO permissions (permID,code,description,categoryID) VALUES (42,'acknowledgements_edit','Edit Acknowledgements',2);
+
+INSERT INTO LorisMenuPermissions (MenuID, PermID) 
+    SELECT m.ID, p.PermID FROM permissions p CROSS JOIN LorisMenu m WHERE p.code='acknowledgements_view' AND m.Label='Acknowledgements';
+INSERT INTO LorisMenuPermissions (MenuID, PermID) 
+    SELECT m.ID, p.PermID FROM permissions p CROSS JOIN LorisMenu m WHERE p.code='acknowledgements_edit' AND m.Label='Acknowledgements';
