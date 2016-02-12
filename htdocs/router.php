d2204f5d39f0732f4c87b5d8cba8861fe42190a3
jstirling91 <jstirl@abunawas-2.local>
2016-02-12 16:45:41 -0500

    fix rebase errors

--- htdocs/router.php
+++ htdocs/router.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Implementation of .htacces as a PHP router file
  *
@@ -11,7 +12,9 @@
  * @link     https://www.github.com/aces/Loris/
  */
 
+
 $url = ltrim($_SERVER['REQUEST_URI'], "/");
+
 if (preg_match(
     '#^bootstrap/(.*)#',
     $url
@@ -29,9 +32,12 @@ if (preg_match(
     // RewriteRule
     //      ^([0-9]{6,6})/$
     //      /main.php?test_name=timepoint_list&candID=$1
+
     $getParams = explode("/", $url);
+
     $_REQUEST["test_name"] = "timepoint_list";
     $_REQUEST['candID']    = $getParams[0];
+
     include_once __DIR__ . "/main.php";
 } else if (preg_match(
     '#^([0-9]{6,6})/([0-9]+)/$#',
@@ -43,10 +49,13 @@ if (preg_match(
     // RewriteRule
     //      ^([0-9]{6,6})/([0-9]+)/$
     //      /main.php?test_name=instrument_list&candID=$1&sessionID=$2
+
     $getParams = explode("/", $url);
+
     $_REQUEST["test_name"] = "instrument_list";
     $_REQUEST['candID']    = $getParams[0];
     $_REQUEST['sessionID'] = $getParams[1];
+
     include_once __DIR__ . "/main.php";
 } else if (preg_match(
     '#^([0-9]{6,6})/([0-9]+)/([a-zA-Z_]+)/$#',
@@ -56,10 +65,13 @@ if (preg_match(
     // RewriteRule
     //      ^([0-9]{6,6})/([0-9]+)/([a-zA-Z_]+)/$
     //      /main.php?test_name=$3&candID=$1&sessionID=$2 [QSA]
+
     $getParams = explode("/", $url);
+
     $_REQUEST["test_name"] = $getParams[2];
     $_REQUEST['candID']    = $getParams[0];
     $_REQUEST['sessionID'] = $getParams[1];
+
     include_once __DIR__ . "/main.php";
 } else if (preg_match(
     '#^([0-9]{6,6})/([0-9]+)/([a-zA-Z_]+)/([a-zA-Z0-9_]+)/$#',
@@ -69,11 +81,14 @@ if (preg_match(
     // RewriteRule
     //      ^([0-9]{6,6})/([0-9]+)/([a-zA-Z_]+)/([a-zA-Z0-9_]+)/$
     //      /main.php?test_name=$3&candID=$1&sessionID=$2&subtest=$4 [QSA]
+
     $getParams = explode("/", $url);
+
     $_REQUEST["test_name"] = $getParams[2];
     $_REQUEST['candID']    = $getParams[0];
     $_REQUEST['sessionID'] = $getParams[1];
     $_REQUEST['subtest']   = $getParams[3];
+
     include_once __DIR__ . "/main.php";
 } else if (preg_match(
     '#^([a-zA-Z_-]+)/(\?|$)#',
@@ -82,8 +97,11 @@ if (preg_match(
     // RewriteRule
     //      ^([a-zA-Z_-]+)/$
     //      /main.php?test_name=$1 [QSA]
+
     $getParams = explode("/", $url);
+
     $_REQUEST["test_name"] = $getParams[0];
+
     include_once __DIR__ . "/main.php";
 } else if (preg_match(
     '#^([a-zA-Z_-]+)/css/([a-zA-Z0-9_.-]+)$#',
@@ -93,9 +111,12 @@ if (preg_match(
     //      ^([a-zA-Z_-]+)/css/([a-zA-Z0-9_.-]+)$
     //      /GetCSS.php?Module=$1&file=$2
     // NOT SURE IF THIS WORKS IF FILE IS NOT SPECIFIED
+
     $getParams = explode("/", $url);
+
     $_GET["Module"]   = $getParams[0];
     $_REQUEST['file'] = $getParams[2];
+
     include_once __DIR__ . "/GetCSS.php";
 } else if (preg_match(
     '#^([a-zA-Z_-]+)/js/([a-zA-Z0-9_.-]+)$#',
@@ -105,9 +126,12 @@ if (preg_match(
     //      ^([a-zA-Z_-]+)/js/([a-zA-Z0-9_.-]+)$
     //      /GetJS.php?Module=$1&file=$2
     // NOT SURE IF THIS WORKS IF FILE IS NOT SPECIFIED
+
     $getParams = explode("/", $url);
+
     $_GET["Module"] = $getParams[0];
     $_GET['file']   = $getParams[2];
+
     include_once __DIR__ . "/GetJS.php";
 } else if (preg_match(
     '#^([a-zA-Z_-]+)/static/([a-zA-Z0-9_.-/]+)$#',
@@ -117,9 +141,12 @@ if (preg_match(
     //      ^([a-zA-Z_-]+)/static/([a-zA-Z0-9_.-/]+)$
     //      /GetStatic.php?Module=$1&file=$2
     // NOT SURE IF THIS WORKS IF FILE IS NOT SPECIFIED
+
     $getParams = explode("/", $url);
+
     $_GET["Module"] = $getParams[0];
     $_GET['file']   = $getParams[2];
+
     include_once __DIR__ . "/GetStatic.php";
 } else if (preg_match(
     '#^([a-zA-Z_-]+)/ajax/([a-zA-Z0-9_.-/]+)$#',
@@ -129,9 +156,12 @@ if (preg_match(
     //      ^([a-zA-Z_-]+)/ajax/([a-zA-Z0-9_.-]+)$
     //      /AjaxHelper.php?Module=$1&script=$2 [QSA]
     // NOT SURE IF THIS WORKS IF FILE IS NOT SPECIFIED
+
     $getParams = explode("/", $url);
+
     $_GET["Module"] = $getParams[0];
     $_GET['script'] = $getParams[2];
+
     include_once __DIR__ . "/AjaxHelper.php";
 } else if (preg_match(
     '#^([a-zA-Z_-]+)/([a-zA-Z0-9_.-]+)/(\?|$)#',
@@ -140,9 +170,12 @@ if (preg_match(
     // RewriteRule
     //      ^([a-zA-Z_-]+)/([a-zA-Z0-9_.-]+)/$
     //      /main.php?test_name=$1&subtest=$2 [QSA]
+
     $getParams = explode("/", $url);
+
     $_REQUEST["test_name"] = $getParams[0];
     $_REQUEST['subtest']   = $getParams[1];
+
     include_once __DIR__ . "/main.php";
 } else if (preg_match(
     '#^preferences/$#',
@@ -152,10 +185,13 @@ if (preg_match(
     // RewriteRule
     //      ^preferences/$
     //      /main.php?test_name=user_accounts&subtest=my_preferences
+
     $_REQUEST["test_name"] = "user_accounts";
     $_REQUEST['subtest']   = "my_preferences";
+
     include_once __DIR__ . "/main.php";
 } else {
     return false;
 }
+
 ?>
