740ee163c1ba4b2649529ef68bfbcfd71a6c4bd0
xlecours <xavierlb.mavan@gmail.com>
2016-02-17 15:50:14 -0500

    trying to identify the active window 3

--- modules/imaging_browser/test/imaging_browserTest.php
+++ modules/imaging_browser/test/imaging_browserTest.php
@@ -809,6 +809,7 @@ class imagingBrowserTestIntegrationTest extends LorisIntegrationTest
         $handle = $this->webDriver->getWindowHandle(); 
         var_dump($this->webDriver->switchTo()->window($handle)->getTitle());
         var_dump($this->webDriver->findElement(WebDriverBy::xPath('//div[@class="panel panel-default"]'))->getText());
+        var_dump($this->webDriver->findElement(WebDriverBy::xPath('//div[@class="panel panel-default"]//div[@class="panel-body"]'))->getText());
         $this->webDriver->findElement(
             WebDriverBy::Name('status[1]')
         )->sendKeys("Pass");
