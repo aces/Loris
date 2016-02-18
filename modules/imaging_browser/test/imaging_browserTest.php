2a5d6deb4d5d28a0a2bc4ced2f0d68663843c1aa
xlecours <xavierlb.mavan@gmail.com>
2016-02-18 15:56:40 -0500

    get page source 3

--- modules/imaging_browser/test/imaging_browserTest.php
+++ modules/imaging_browser/test/imaging_browserTest.php
@@ -815,7 +815,8 @@ class imagingBrowserTestIntegrationTest extends LorisIntegrationTest
         var_dump($this->webDriver->findElement(WebDriverBy::xPath('//div[@class="panel panel-default"]'))->getText());
         var_dump($this->webDriver->findElement(WebDriverBy::xPath('//div[@class="panel panel-default"]//div[@class="panel-body"]'))->getText());
 
-        var_dump($this->webDriver->getPageSource());
+        $bob = $this->webDriver->getPageSource();
+        var_dump(strstr($bob, 'RImagePanel'));
         $this->webDriver->executeScript("$('body').html(navigator.userAgent + JSON.stringify(React))", array());
         var_dump($this->webDriver->findElement(WebDriverBy::xPath('//body'))->getText());
 $this->webDriver->executeScript('
