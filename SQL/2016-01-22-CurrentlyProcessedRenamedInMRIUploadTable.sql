48e1929d1d383a8110f03cbcb57d5ade88b4116d
MounaSafiHarab <mouna.safiharab@gmail.com>
2016-03-01 09:30:26 -0500

    More flag naming changes based on the Imaging meeting feedback

--- SQL/2016-01-22-CurrentlyProcessedRenamedInMRIUploadTable.sql
+++ SQL/2016-01-22-CurrentlyProcessedRenamedInMRIUploadTable.sql
@@ -1,1 +1,2 @@
-ALTER TABLE mri_upload CHANGE `CurrentlyProcessed` `Processing` tinyint(1) NOT NULL DEFAULT '0';
+ALTER TABLE mri_upload CHANGE `Processed` `InsertionComplete` tinyint(1) NOT NULL DEFAULT '0';
+ALTER TABLE mri_upload CHANGE `CurrentlyProcessed` `Inserting` tinyint(1) NOT NULL DEFAULT '0';
