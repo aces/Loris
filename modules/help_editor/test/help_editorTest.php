62c21566ee588e4b4ebb0295b5c30a738c43a65f
kongtiaowang <kongtiaowangshen@gmail.com>
2016-03-03 14:12:49 -0500

    Update help_editorTest.php

--- modules/help_editor/test/help_editorTest.php
+++ modules/help_editor/test/help_editorTest.php
@@ -6,11 +6,10 @@
  *
  * @category Test
  * @package  Loris
- * @author   Ted Strauss <ted.strauss@mcgill.ca>
+ * @author   Wang Shen 
  * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
  * @link     https://github.com/aces/Loris
  */
-
 require_once __DIR__ . "/../../../test/integrationtests/LorisIntegrationTest.class.inc";
 class helpEditorTestIntegrationTest extends LorisIntegrationTest
 {
@@ -23,8 +22,9 @@ class helpEditorTestIntegrationTest extends LorisIntegrationTest
     function testHelpEditorDoesPageLoad()
     {
         $this->safeGet($this->url . "/help_editor/");
-        $bodyText = $this->webDriver->findElement(WebDriverBy::cssSelector("body"))->getText();
-        $this->assertContains("Help Editor", $bodyText);
+        $assertText = $this->webDriver->findElement(WebDriverBy::cssSelector("body"))->getText();
+        
+        $this->assertContains("Help Editor", $assertText);
     }
     /**
      * Tests that, when loading the help_editor module > edit help submodule, some
@@ -35,8 +35,63 @@ class helpEditorTestIntegrationTest extends LorisIntegrationTest
     function testHelpEditorEditHelpContentDoesPageLoad()
     {
         $this->safeGet($this->url . "/help_editor/edit_help_content/");
-        $bodyText = $this->webDriver->findElement(WebDriverBy::cssSelector("body"))->getText();
-        $this->assertContains("Edit Help Content", $bodyText);
+        $assertText = $this->webDriver->findElement(WebDriverBy::cssSelector("body"))->getText();
+        
+        $this->assertContains("Edit Help Content", $assertText);
+    }
+    /**
+     * Tests that, when loading the help editor, search Hand Preference, click show data, the hand preference should appear.
+     * 
+     * @return void
+     */
+    public function testHelpEditor_SearchTopic()
+    {
+        $this->safeGet($this->url . '/help_editor/');
+         $searchbox = $this->webDriver
+            ->findElement(WebDriverBy::cssSelector("#panel-body > form > div:nth-child(1) > div:nth-child(1) > div > input"));
+        $searchbox->sendKeys("Hand Preference");
+         $showdata = $this->webDriver
+            ->findElement(WebDriverBy::cssSelector("#panel-body > form > div:nth-child(2) > div > div.col-sm-4.col-sm-offset-4 > input"));
+         $showdata->click();
+            $assertText = $this->webDriver
+            ->findElement(WebDriverBy::cssSelector("#Topic > a"))->getText();
+        $this->assertContains("Hand Preference", $assertText);
+    }
+// /**
+    //  * Tests that, when loading the help editor, search Hand Preference, click show data, the hand preference should appear.
+    //  * 
+    //  * @return void
+    //  */
+    public function testHelpEditor_SearchKeyWord()
+    {
+        $this->safeGet($this->url . '/help_editor/');
+         $searchbox = $this->webDriver
+            ->findElement(WebDriverBy::cssSelector("#panel-body > form > div:nth-child(1) > div:nth-child(2) > div > input"));
+        $searchbox->sendKeys("Under Construction");
+         $showdata = $this->webDriver
+            ->findElement(WebDriverBy::cssSelector("#panel-body > form > div:nth-child(2) > div > div.col-sm-4.col-sm-offset-4 > input"));
+         $showdata->click();
+            $assertText = $this->webDriver
+            ->findElement(WebDriverBy::cssSelector("#Topic > a"))->getText();
+        $this->assertContains("Hand Preference", $assertText);
+    }
+// /**
+    //  * Tests that, when loading the help editor, search Hand Preference, click clear form, the text Hand Preference should disappear.
+    //  * 
+    //  * @return void
+    //  */
+    public function testHelpEditor_ClearForm()
+    {
+        $this->safeGet($this->url . '/help_editor/');
+         $searchbox = $this->webDriver
+            ->findElement(WebDriverBy::cssSelector("#panel-body > form > div:nth-child(1) > div:nth-child(1) > div > input"));
+        $searchbox->sendKeys("Hand Preference");
+         $clearform = $this->webDriver
+            ->findElement(WebDriverBy::cssSelector("#panel-body > form > div:nth-child(2) > div > div.col-sm-4.col-sm-offset-4 > input"));
+         $clearform->click();
+            $assertText = $this->webDriver
+            ->findElement(WebDriverBy::cssSelector("#panel-body > form > div:nth-child(1) > div:nth-child(1) > div > input"))->getText();
+        $this->assertSame('',$assertText);
     }
 }
 ?>
