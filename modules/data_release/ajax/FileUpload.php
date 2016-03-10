709fcb95b5b8014843b4f33b8ec7ed6c61ed8cad
Jkat <fission@live.com>
2016-03-10 16:00:32 -0500

    phpcs

--- modules/data_release/ajax/FileUpload.php
+++ modules/data_release/ajax/FileUpload.php
@@ -1,23 +1,40 @@
 <?php
+
+/**
+  * Upload files, wow.
+  *
+  * PHP Version 5
+  *
+  *  @category Loris
+  *  @package  Data_Release
+  *  @author   Justin Kat <justinkat@gmail.com>
+  *  @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+  *  @link     https://github.com/aces/Loris
+  *
+  */
+
 $DB =& Database::singleton();
 if ($_POST['action'] == 'upload') {
-        $fileName = $_FILES["file"]["name"];
-        $version = $_POST['version'];
-        $upload_date = date('Y-m-d');
-        $base_path = __DIR__ . "/../user_uploads/";
-        if (!file_exists($base_path)) {
-            mkdir($base_path, 0777);
-        }
-        $target_path = $base_path . $fileName;
-        if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_path)) {
-            $success = $DB->insert('data_release',
-                            array('file_name'=>$fileName,
-                                  'version'=>$version, 
-                                  'upload_date'=>$upload_date,
-                            ));
-        }
-            header("Location: ../main.php?test_name=data_release&uploadSuccess=true");
+    $fileName    = $_FILES["file"]["name"];
+    $version     = $_POST['version'];
+    $upload_date = date('Y-m-d');
+    $base_path   = __DIR__ . "/../user_uploads/";
+    if (!file_exists($base_path)) {
+        mkdir($base_path, 0777);
+    }
+    $target_path = $base_path . $fileName;
+    if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_path)) {
+        $success = $DB->insert(
+            'data_release',
+            array(
+             'file_name'   => $fileName,
+             'version'     => $version,
+             'upload_date' => $upload_date,
+            )
+        );
+    }
+    header("Location: ../main.php?test_name=data_release&uploadSuccess=true");
 } else {
-            echo "There was an error uploading the file";
+    echo "There was an error uploading the file";
 }
 ?>
