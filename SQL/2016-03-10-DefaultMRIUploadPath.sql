608e35036bf5ba72c0172132e7f261463869a0c2
MounaSafiHarab <mouna.safiharab@gmail.com>
2016-03-21 10:12:21 -0400

    updated patch to not overwrite preset MRI upload incoming path

--- SQL/2016-03-10-DefaultMRIUploadPath.sql
+++ SQL/2016-03-10-DefaultMRIUploadPath.sql
@@ -1,1 +1,1 @@
-UPDATE Config AS c, ConfigSettings AS cs SET c.value="/data/incoming/" WHERE c.ConfigID=cs.ID AND cs.Name="MRIUploadIncomingPath";
+UPDATE Config AS c, ConfigSettings AS cs SET c.value="/data/incoming/" WHERE c.ConfigID=cs.ID AND c.Value="/PATH/TO/MRI-Upload/" AND cs.Name="MRIUploadIncomingPath";
