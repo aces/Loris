8e13cca272d3905659c77d92c9c8338ba759876c
jstirling91 <jstirl@abunawas-2.local>
2016-03-29 15:29:48 -0400

    add permission checks

--- modules/dataquery/ajax/queryStartsWith.php
+++ modules/dataquery/ajax/queryStartsWith.php
@@ -1,4 +1,11 @@
 <?php
+
+$user =& User::singleton();
+if (!$user->hasPermission('dataquery_view')) {
+    header("HTTP/1.1 403 Forbidden");
+    exit;
+}
+
 require_once __DIR__ . '/../../../vendor/autoload.php';
 $client = new NDB_Client();
 $client->makeCommandLine();
--- modules/dataquery/ajax/saveQuery.php
+++ modules/dataquery/ajax/saveQuery.php
@@ -1,6 +1,12 @@
 <?php
 ini_set("max_input_vars", 4000);
 
+$user =& User::singleton();
+if (!$user->hasPermission('dataquery_view')) {
+    header("HTTP/1.1 403 Forbidden");
+    exit;
+}
+
 require_once __DIR__ . '/../../../vendor/autoload.php';
 $client = new NDB_Client();
 $client->makeCommandLine();
