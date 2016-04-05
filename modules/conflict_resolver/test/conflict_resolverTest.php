0e070b12616c27db13ff03008f563700cccc331f
kongtiaowang <kongtiaowangshen@gmail.com>
2016-04-05 09:44:21 -0400

    insert test data

--- modules/conflict_resolver/test/conflict_resolverTest.php
+++ modules/conflict_resolver/test/conflict_resolverTest.php
@@ -27,6 +27,48 @@ require_once __DIR__
 class ConflictResolverTestIntegrationTest extends LorisIntegrationTest
 {
     /**
+     * Insert testing data into the database
+     * author: Wang Shen
+     *
+     * @return none
+     */
+    function setUp()
+    {
+        parent::setUp();
+        $this->DB->insert(
+            "conflicts_resolved",
+            array(
+             'ResolvedID'          => '999999',
+             'UserID'              => 'demo',
+             'ResolutionTimestamp' => '2015-11-03 16:21:49',
+             'User1'               => 'Null',
+             'User2'               => 'Null',
+             'TableName'           => 'Test',
+             'ExtraKey1'           => 'NULL',
+             'ExtraKey2'           => 'NULL',
+             'FieldName'           => 'TestTestTest',
+             'CommentId1'          => '589569DCC000012291366553230',
+             'CommentId2'          => 'DDE_589569DCC000012291366653254',
+             'OldValue1'           => 'Mother',
+             'OldValue2'           => 'Father',
+             'NewValue'            => 'NULL',
+             'ConflictID'          => 'NULL',
+            )
+        );
+    }
+    /**
+     * Delete testing data from database
+     * author: Wang Shen
+     *
+     * @return none
+     */
+    function tearDown()
+    {
+        parent::tearDown();
+        $this->DB->delete("conflicts_resolved", array('ResolvedID' => '999999'));
+    }
+ 
+   /**
      * Tests that, when loading the conflict_resolver module, some
      * text appears in the body.
      *
