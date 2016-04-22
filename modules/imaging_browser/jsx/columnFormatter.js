b54dfac3d84ee9ebeec8d1888edc2bd51e3be2f3
xlecours <xavierlb.mavan@gmail.com>
2016-04-22 14:30:52 -0400

    fix Navigation links
    Adding backURL in the columnFormater
    
    Setting the $_SESSION mriSessionsListed property in a overriden toArray function

--- modules/imaging_browser/jsx/columnFormatter.js
+++ modules/imaging_browser/jsx/columnFormatter.js
@@ -1,26 +1,33 @@
 function formatColumn(column, cell, rowData) {
-    if(column === 'New Data') {
-        if(cell === 'new') {
-            return <td className="newdata">NEW</td>
-        }
-        return <td></td>;
-    }
-    if(column === 'Links') {
-        var cellTypes = cell.split(",");
-        var cellLinks = []
-        for(var i = 0; i < cellTypes.length; i += 1) {
-            cellLinks.push(<a href={loris.BaseURL + "/imaging_browser/viewSession/?sessionID=" + rowData[11] + "&outputType=" + cellTypes[i]}>{cellTypes[i]}</a>);
+    switch (column) {
+        case 'SessionID':
+            // hide this column
+            return null;
+            break;
+        case 'New Data':
+            if(cell === 'new') {
+                return <td className="newdata">NEW</td>
+            }
+            return <td></td>;
+            break;
+        case 'Links':
+            var cellTypes = cell.split(",");
+            var cellLinks = []
+            for(var i = 0; i < cellTypes.length; i += 1) {
+                cellLinks.push(<a href={loris.BaseURL + "/imaging_browser/viewSession/?sessionID=" + rowData[11] + "&outputType=" + cellTypes[i] + "&backURL=" + loris.BaseURL + "/imaging_browser/"}>{cellTypes[i]}</a>);
+                cellLinks.push(" | ");
+            }
+            cellLinks.push(<a href={loris.BaseURL + "/imaging_browser/viewSession/?sessionID=" + rowData[11] + "&selectedOnly=1" + "&backURL=" + loris.BaseURL + "/imaging_browser/"}>selected</a> );
             cellLinks.push(" | ");
-
-        }
-        cellLinks.push(<a href={loris.BaseURL + "/imaging_browser/viewSession/?sessionID=" + rowData[11] + "&selectedOnly=1"}>selected</a> );
-        cellLinks.push(" | ");
-        cellLinks.push(<a href={loris.BaseURL + "/imaging_browser/viewSession/?sessionID=" + rowData[11]}>all types</a> );
-        return (
-                <td>
-                    {cellLinks}
-                </td>
-               );
+            cellLinks.push(<a href={loris.BaseURL + "/imaging_browser/viewSession/?sessionID=" + rowData[11] + "&backURL=" + loris.BaseURL + "/imaging_browser/" }>all types</a> );
+            return (
+                    <td>
+                        {cellLinks}
+                    </td>
+            );
+            break;
+        default:
+            return <td>{cell}</td>;
+            break;
     }
-    return <td>{cell}</td>;
 }
