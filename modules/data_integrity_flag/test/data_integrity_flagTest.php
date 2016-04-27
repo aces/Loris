a16b97467c1f48376625b206542c95f3da56d49c
kongtiaowang <kongtiaowangshen@gmail.com>
2016-04-27 10:00:06 -0400

    add data

--- modules/data_integrity_flag/test/data_integrity_flagTest.php
+++ modules/data_integrity_flag/test/data_integrity_flagTest.php
@@ -20,6 +20,44 @@ class dataIntegrityFlagTestIntegrationTest extends LorisIntegrationTest
      *
      * @return void
      */
+ /**
+     * Insert testing data into the database
+     *
+     * @return none
+     */
+    function setUp()
+    {
+        parent::setUp();
+        $this->DB->insert(
+            "data_integrity_flag",
+            array(
+             'dataflag_id'             => '9999999',
+             'dataflag_visitlabel'     => 'V_test',
+              'dataflag_instrument'    => 'mri_parameter_form',
+              'dataflag_date'          => '2015-12-03',
+              'dataflag_status'        => '4',
+              'dataflag_comment'       => '33',
+              'latest_entry'           => '1',
+              'dataflag_fbcreated'     => '0',
+              'dataflag_fbclosed'      => '0',
+              'dataflag_fbcomment'     => '0',
+              'dataflag_fbdeleted'     => '0',
+              'dataflag_userid'        => 'test_user'
+            )
+        );
+    }
+    /**
+     * Delete testing data from database
+     *
+     * @return none
+     */
+    function tearDown()
+    {
+        parent::tearDown();
+        $this->DB->delete("data_integrity_flag", array('dataflag_id' => '9999999'));
+    }
+
+    
     function testDataIntegrityFlagDoespageLoad()
     {
         $this->safeGet($this->url . "/data_integrity_flag/");
