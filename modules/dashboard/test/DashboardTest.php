05b1e557d3a90bdda4f148af9916aa69d91d3954
kongtiaowang <kongtiaowangshen@gmail.com>
2016-05-04 16:35:17 -0400

    add

--- modules/dashboard/test/DashboardTest.php
+++ modules/dashboard/test/DashboardTest.php
@@ -89,8 +89,9 @@ class DashboardTest extends LorisIntegrationTest
      */
     public function testTargetNumberOfParticipants()
     {
-        $this->safeGet($this->url . '/dashboard/');
-        
+        try{
+            $this->safeGet($this->url . '/dashboard/');
+
             $dashboardNum = $this->safeFindElement(
                 WebDriverBy::cssSelector("body")
             )
@@ -110,7 +111,7 @@ class DashboardTest extends LorisIntegrationTest
                 ->getAttribute('value');
 
             $this->assertEquals($dashboardNum, "Target: ".$configNum);
-       // }catch(WebDriverException $ex){
+        }catch(WebDriverException $ex){
         }
     }
     /**
