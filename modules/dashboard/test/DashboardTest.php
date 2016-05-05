e570073292e48ace239bd9f663733c8910bbc739
kongtiaowang <kongtiaowangshen@gmail.com>
2016-05-05 16:28:20 -0400

    test travis

--- modules/dashboard/test/DashboardTest.php
+++ modules/dashboard/test/DashboardTest.php
@@ -162,32 +162,39 @@ class DashboardTest extends LorisIntegrationTest
          $this->resetPermissions();
 
     }
-/**
+    /**
      * Tests that, when loading the Dashboard and Config,the number of the Target
      * number of participants should be same.
      *
      * @return void
      */
     public function testTargetNumberOfParticipants()
-    {  
-       try{
-        $this->safeGet($this->url . '/dashboard/');
+    {
+        try{
+            $this->safeGet($this->url . '/dashboard/');
 
-        $dashboardNum = $this->safeFindElement(
-            WebDriverBy::Xpath("//*[@id='overall-recruitment']/div/p"))
-            ->getText();
+            $dashboardNum = $this->safeFindElement(
+                WebDriverBy::Xpath("//*[@id='overall-recruitment']/div/p")
+            )
+                ->getText();
 
-        $this->safeGet($this->url . '/configuration/');
+            $this->safeGet($this->url . '/configuration/');
 
-        $this->safeFindElement
-           (WebDriverBy::Xpath("//*[@id='lorisworkspace']/div[1]/ul/li[5]/a"))
-            ->click();
-        $configNum =  $this->safeFindElement
-           (WebDriverBy::Xpath("//*[@id='41']/input"))
-            ->getAttribute('value');
+            $this->safeFindElement(
+                WebDriverBy::Xpath(
+                    "//*[@id='lorisworkspace']/div[1]/ul/li[5]/a"
+                )
+            )
+                ->click();
+            $configNum =  $this->safeFindElement(
+                WebDriverBy::Xpath(
+                    "//*[@id='41']/input"
+                )
+            )
+                ->getAttribute('value');
 
-
-        $this->assertEquals($dashboardNum,"Target: ".$configNum);
-        }catch(WebDriverException $ex){}
+            $this->assertEquals($dashboardNum, "Target: ".$configNum);
+        }catch(WebDriverException $ex){
+        }
     }
 }
