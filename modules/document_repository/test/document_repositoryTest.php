454b3fbcf2e114b4076add38600db8150d2dc73b
kongtiaowang <kongtiaowangshen@gmail.com>
2016-05-12 13:34:56 -0400

    document_repo

--- modules/document_repository/test/document_repositoryTest.php
+++ modules/document_repository/test/document_repositoryTest.php
@@ -26,5 +26,35 @@ class documentRepositoryTestIntegrationTest extends LorisIntegrationTest
         $bodyText = $this->webDriver->findElement(WebDriverBy::cssSelector("body"))->getText();
         $this->assertContains("Document Repository", $bodyText);
     }
+    /**
+     * Tests creating a category and a sub category.
+     *
+     * @return void
+     */
+    function testDocumentRepositoryCreateCategory()
+    {
+       //insert a category TestTestTest
+        $this->safeGet($this->url . "/document_repository/");
+        $this->safeFindElement(WebDriverBy::Name("addCategory"))->click();
+        $this->safeFindElement(WebDriverBy::Name("category_name"),2000)->sendKeys("TestTestTest");
+        $this->safeFindElement(WebDriverBy::Id("postCategory"))->click();
+        sleep(10);
+        $selectAll = $this->webDriver->findElement(WebDriverBy::Id("dir-tree"))->getText();
+        $this->assertContains("TestTestTest", $selectAll);
+       
+       //insert a sub category test under TestTestTest
+        $this->safeGet($this->url . "/document_repository/");
+        $this->safeFindElement(WebDriverBy::Name("addCategory"))->click();
+        $this->safeFindElement(WebDriverBy::Name("category_name"),2000)->sendKeys("test");
+        $select = $this->safeFindElement(WebDriverBy::Id("parent_id"));
+        $element = new WebDriverSelect($select);
+        $element->selectByVisibleText("TestTestTest");
+        $this->safeFindElement(WebDriverBy::Id("postCategory"))->click();
+        sleep(10);
+        $this->safeFindElement(WebDriverBy::Xpath("//*[@id='TestTestTesta']/td/span"))->click();
+        $test = $this->safeFindElement(WebDriverBy::Id("testa"))->getText();
+        $this->assertContains("test", $test);
+
+    }
 }
 ?>
