2f30427f704c98456479c4d4dafd97d51d1d44ac
MounaSafiHarab <mouna.safiharab@gmail.com>
2016-05-20 09:18:01 -0400

    Range in brainbrowser no longer fixed to 0-255

--- modules/brainbrowser/ajax/minc.php
+++ modules/brainbrowser/ajax/minc.php
@@ -29,7 +29,7 @@ $minc_file = getMincLocation() . $minc_file;
 $header      = $_REQUEST['minc_headers'];
 $header_data = $_REQUEST['raw_data'];
 if ($header_data) {
-    passthru("minctoraw -byte -unsigned -normalize $minc_file");
+    passthru("minctoraw -double -normalize $minc_file");
 }
 if ($header=='true' && $minc_file !=null) {
     print initialize($minc_file);
@@ -72,6 +72,7 @@ function initialize($minc_file)
                 'xspace' => extractDimension("xspace", $minc_file),
                 'yspace' => extractDimension("yspace", $minc_file),
                 'zspace' => extractDimension("zspace", $minc_file),
+                'datatype' => 'float64',
                );
 
     //minc2.0, if there's a time component
