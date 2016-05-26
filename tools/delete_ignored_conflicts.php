60e5ddc153737f6e4f33cc2938130c63f698c6d3
Stella Lee <slee.mcin@gmail.com>
2016-05-26 09:50:13 -0400

    temp

--- tools/delete_ignored_conflicts.php
+++ tools/delete_ignored_conflicts.php
@@ -57,8 +57,8 @@ if (!empty($argv[1]) && $argv[1]!="confirm") {
     $instruments = $config->getSetting('DoubleDataEntryInstruments');
 }
 
-if (isset($instrument)) {
-    detectIgnoreColumns($instrument);
+if (isset($instruments)) {
+    detectIgnoreColumns($instruments);
 }
 else {
     echo "No instruments found";
@@ -68,6 +68,44 @@ if ($confirm === false) {
     echo "\n\nRun this tool again with the argument 'confirm' to ".
         "perform the changes\n\n";
 }
+
+/**
+ *Get the commentids for the given instrument, candidate and visit_label
+ *
+ * @param String $test_name   The instrument been searched
+ * @param string $visit_label The  visit_label been searched
+ * @param string $candid      The  candid been searched
+ *
+ * @return array $commentids An array of commentids found
+ */
+
+function getCommentIDs($test_name, $visit_label=null, $candid=null)
+{
+    $db =& Database::singleton();
+    $params = array();
+    $query = "SELECT CommentID, s.visit_label,Test_name,
+        CONCAT('DDE_', CommentID) AS DDECommentID FROM flag f
+        JOIN session s ON (s.ID=f.SessionID)
+        JOIN candidate c ON (c.CandID=s.CandID)";
+    $where = " WHERE CommentID NOT LIKE 'DDE%'
+        AND s.Active='Y' AND c.Active='Y'
+        AND s.Visit <> 'Failure'";
+    if ($test_name!=null) {
+        $where .= " AND f.Test_name= :instrument ";
+        $params['instrument'] = $test_name;
+
+        if (($visit_label!=null) && (isset($visit_label))) {
+            $where .= " AND s.visit_label= :vlabel";
+            $params['vlabel'] = $visit_label ;
+        }
+    }
+
+    $query .=$where;
+    $commentids = $GLOBALS['DB']->pselect($query, $params);
+    return $commentids;
+}
+
+
 
 /**
  * Populates the DDE ignore fields for each instrument and runs
@@ -80,24 +118,30 @@ function detectIgnoreColumns($instruments)
     $instrumentFields = array();
 
     foreach ($instruments as $instrument) {
-        include "../instruments/NDB_BVL_Instrument_$instrument.class.inc";
-        $instance =& NDB_BVL_Instrument::factory($instrument, null, null);
-        $DDEIgnoreFields = $instance->_doubleDataEntryDiffIgnoreColumns;
+        $file = "../project/instruments/NDB_BVL_Instrument_$instrument.class.inc";
+        if (file_exists($file)) {
+            include $file;
+            $commentids = getCommentIDs($instrument, null);
+            $instance =& NDB_BVL_Instrument::factory($instrument, $commentids[0]['CommentID'], null);
+            $DDEIgnoreFields = $instance->_doubleDataEntryDiffIgnoreColumns;
 
-        if ($DDEIgnoreFields != null) {
-            foreach ($DDEIgnoreFields as $key => $DDEField) {
-                if (!in_array($DDEField, $this->defaultFields)) {
-                    $instrumentFields = array_merge($instrumentFields, array($DDEField => $instrument));
+            if ($DDEIgnoreFields != null) {
+                foreach ($DDEIgnoreFields as $key => $DDEField) {
+                    if (!in_array($DDEField, $this->defaultFields)) {
+                        $instrumentFields = array_merge($instrumentFields, array($DDEField => $instrument));
+                    }
                 }
+            } else {
+                echo "No DDE ignore fields found for " . $instrument;
             }
+            if (!$this->instrumentSpecified) {
+                defaultIgnoreColumns();
+            }
+            ignoreColumn($instrument, $instrumentFields);
         }
         else {
-            echo "No DDE ignore fields found for " . $instrument;
+            echo $file . " was not found.\n";
         }
-        if (!$this->instrumentSpecified) {
-            defaultIgnoreColumns();
-        }
-        ignoreColumn($instrument, $instrumentFields);
     }
 }
 
