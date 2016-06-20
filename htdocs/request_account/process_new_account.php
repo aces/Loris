7023e8fc39103e2cd447c15869126a21136e5005
nirtiac <caitrin.armstrong@mail.mcgill.ca>
2016-06-20 13:45:57 -0400

    tiny documentation change

--- htdocs/request_account/process_new_account.php
+++ htdocs/request_account/process_new_account.php
@@ -150,7 +150,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
  * Check that the user input for a field meets minimum length requirements
  *
  * @param string  $str The request parameter to check
- * @param integer $len The minimum length for the parameter
+ * @param integer $len The minimum length - 1 for the parameter
  *
  * @return True if the parameter was sent and meets minimum length, false
  *         otherwise
