ce917f78bc638f06320818ce1f70771fb25a3e64
xlecours <xavierlb.mavan@gmail.com>
2016-06-28 08:27:57 -0400

    Now mapping rowData with Headers

--- modules/imaging_browser/js/columnFormatter.js
+++ modules/imaging_browser/js/columnFormatter.js
@@ -1,48 +1,70 @@
-function formatColumn(column, cell, rowData) {
-    if (column === 'SessionID') {
-        return null;
-    }
-    if (column === 'New Data') {
-        if (cell === 'new') {
-            return React.createElement(
-                'td',
-                { className: 'newdata' },
-                'NEW'
-            );
+function formatColumn(column, cell, rowData, rowHeaders) {
+    reactElement = null;
+
+    if (-1 == loris.hiddenHeaders.indexOf(column)) {
+
+        var row = {};
+        rowHeaders.forEach(function (header, index) {
+            row[header] = rowData[index];
+        }, this);
+
+        switch (column) {
+            case 'New Data':
+                if (cell === 'new') {
+                    reactElement = React.createElement(
+                        'td',
+                        { className: 'newdata' },
+                        'NEW'
+                    );
+                } else {
+                    reactElement = React.createElement('td', null);
+                }
+                break;
+            case 'Links':
+                // 11 = SessionID
+                var cellTypes = cell.split(",");
+                var cellLinks = cellTypes.map(function (current, index) {
+                    return React.createElement(
+                        'span',
+                        null,
+                        React.createElement(
+                            'a',
+                            { href: loris.BaseURL + "/imaging_browser/viewSession/?sessionID=" + row.SessionID + "&outputType=" + current + "&backURL=/imaging_browser/" },
+                            current
+                        ),
+                        ' | '
+                    );
+                }, this);
+                cellLinks.push(React.createElement(
+                    'span',
+                    null,
+                    React.createElement(
+                        'a',
+                        { href: loris.BaseURL + "/imaging_browser/viewSession/?sessionID=" + row.SessionID + "&selectedOnly=1&backURL=/imaging_browser/" },
+                        'selected'
+                    ),
+                    ' | '
+                ));
+                cellLinks.push(React.createElement(
+                    'a',
+                    { href: loris.BaseURL + "/imaging_browser/viewSession/?sessionID=" + row.SessionID + "&backURL=/imaging_browser/" },
+                    'all types'
+                ));
+                reactElement = React.createElement(
+                    'td',
+                    null,
+                    cellLinks
+                );
+                console.log(row);
+                break;
+            default:
+                reactElement = React.createElement(
+                    'td',
+                    null,
+                    cell
+                );
+                break;
         }
-        return React.createElement('td', null);
     }
-    if (column === 'Links') {
-        var cellTypes = cell.split(",");
-        var cellLinks = [];
-        for (var i = 0; i < cellTypes.length; i += 1) {
-            cellLinks.push(React.createElement(
-                'a',
-                { href: loris.BaseURL + "/imaging_browser/viewSession/?sessionID=" + rowData[11] + "&outputType=" + cellTypes[i] + "&backURL=/imaging_browser/" },
-                cellTypes[i]
-            ));
-            cellLinks.push(" | ");
-        }
-        cellLinks.push(React.createElement(
-            'a',
-            { href: loris.BaseURL + "/imaging_browser/viewSession/?sessionID=" + rowData[11] + "&selectedOnly=1&backURL=/imaging_browser/" },
-            'selected'
-        ));
-        cellLinks.push(" | ");
-        cellLinks.push(React.createElement(
-            'a',
-            { href: loris.BaseURL + "/imaging_browser/viewSession/?sessionID=" + rowData[11] + "&backURL=/imaging_browser/" },
-            'all types'
-        ));
-        return React.createElement(
-            'td',
-            null,
-            cellLinks
-        );
-    }
-    return React.createElement(
-        'td',
-        null,
-        cell
-    );
+    return reactElement;
 }
