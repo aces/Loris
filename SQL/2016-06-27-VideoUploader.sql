380d5708b90080555d0a9f1471973a4ad60c4029
Alex Ilea <jr.rus13@gmail.com>
2016-07-06 11:00:32 -0400

    Update SQL to use 'media' instead of 'videos' naming

--- SQL/2016-06-27-VideoUploader.sql
+++ SQL/2016-06-27-VideoUploader.sql
@@ -1,7 +1,7 @@
 
 -- Add 'videos' tab to the menu under Clinical section
-DELETE FROM LorisMenu WHERE Label='Videos';
-INSERT INTO LorisMenu (Label, Link, Parent, OrderNumber) VALUES ('Videos', 'videos/', (SELECT ID FROM LorisMenu as L WHERE Label='Clinical'), 5);
+DELETE FROM LorisMenu WHERE Label='Media';
+INSERT INTO LorisMenu (Label, Link, Parent, OrderNumber) VALUES ('Media', 'media/', (SELECT ID FROM LorisMenu as L WHERE Label='Clinical'), 5);
 
 -- Add 'videos' table
 CREATE TABLE IF NOT EXISTS `videos` (
@@ -22,30 +22,27 @@ CREATE TABLE IF NOT EXISTS `videos` (
   PRIMARY KEY (`id`)
 );
 
-
 -- Add user permissions
 DELETE FROM permissions WHERE code='video_upload';
 INSERT INTO permissions (`code`, `description`, `categoryID`) VALUES (
-	'video_upload', 'Video uploading ', 1
+  'video_upload', 'Video uploading ', 1
 );
 
 DELETE FROM user_perm_rel WHERE permID=(SELECT permID FROM permissions WHERE code = 'video_upload');
 INSERT INTO user_perm_rel (`userID`, `permID`) VALUES (
-	(SELECT ID FROM users WHERE UserID = 'admin'),
-	(SELECT permID FROM permissions WHERE code = 'video_upload')
-);
-
+  (SELECT ID FROM users WHERE UserID = 'admin'), (SELECT permID FROM permissions WHERE code = 'video_upload')
+ );
 
 -- Set path to upload/download videos
 SET @parentID = (SELECT ID FROM ConfigSettings WHERE Name = 'paths');
 DELETE FROM ConfigSettings WHERE Name='videosPath';
 INSERT INTO ConfigSettings (`Name`, `Description`, `Visible`, `AllowMultiple`, `DataType`, `Parent`, `Label`, `OrderNumber`) VALUES (
-	'videosPath', 'Path to uploaded videos', 1, 0, 'text', @parentID, 'Videos', 10
+  'videosPath', 'Path to uploaded videos', 1, 0, 'text', @parentID, 'Videos', 10
 );
 
 DELETE FROM Config WHERE ConfigID=(SELECT ID FROM ConfigSettings WHERE Name='videosPath');
 INSERT INTO Config (`ConfigID`, `Value`) VALUES (
-	(SELECT ID FROM ConfigSettings WHERE Name='videosPath'), '/data/uploads/'
+  (SELECT ID FROM ConfigSettings WHERE Name='videosPath'), '/data/uploads/'
 );
 
 
