40f774ead184e4a2f7742c82b4a85abade1132cd
Gregory Luneau <gluneau.mcin@gmail.com>
2016-07-07 16:13:47 -0400

    add constraint to patch

--- SQL/Archive/17.0/2016-07-07-AddTarchiveSeriesIDTotarchive_filesTable.sql
+++ SQL/Archive/17.0/2016-07-07-AddTarchiveSeriesIDTotarchive_filesTable.sql
@@ -1,2 +1,2 @@
 ALTER TABLE tarchive_files ADD `TarchiveSeriesID` INT(11) DEFAULT NULL;
-ALTER TABLE tarchive_files ADD FOREIGN KEY (`TarchiveSeriesID`) REFERENCES tarchive_series(`TarchiveSeriesID`);
+ALTER TABLE tarchive_files ADD CONSTRAINT `tarchive_files_TarchiveSeriesID_fk` FOREIGN KEY (`TarchiveSeriesID`) REFERENCES tarchive_series(`TarchiveSeriesID`);
