601ebc1fc58d2442a35072ff717ee0267ac1fce7
Gregory Luneau <gluneau.mcin@gmail.com>
2016-07-07 13:39:24 -0400

    Added foreign key

--- SQL/0000-00-00-schema.sql
+++ SQL/0000-00-00-schema.sql
@@ -1188,7 +1188,8 @@ CREATE TABLE `test_battery` (
   PRIMARY KEY  (`ID`),
   KEY `age_test` (`AgeMinDays`,`AgeMaxDays`,`Test_name`),
   KEY `FK_test_battery_1` (`Test_name`),
-  CONSTRAINT `FK_test_battery_1` FOREIGN KEY (`Test_name`) REFERENCES `test_names` (`Test_name`)
+  CONSTRAINT `FK_test_battery_1` FOREIGN KEY (`Test_name`) REFERENCES `test_names` (`Test_name`),
+  FOREIGN KEY (`project_ProjectID`) REFERENCES project(`ProjectID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- SQL/Archive/17.0/2016-07-08-AddProjectIDTotest_batteryTable.sql
+++ SQL/Archive/17.0/2016-07-08-AddProjectIDTotest_batteryTable.sql
@@ -1,1 +1,2 @@
 ALTER TABLE test_battery ADD `ProjectID` int(11) DEFAULT NULL;
+ALTER TABLE test_battery ADD FOREIGN KEY (`project_ProjectID`) REFERENCES project(`ProjectID`);
