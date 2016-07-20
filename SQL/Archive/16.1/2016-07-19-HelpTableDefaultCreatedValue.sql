380e25078e5547cf692459098bb24553934ac3e5
xlecours <xavierlb.mavan@gmail.com>
2016-07-20 09:37:07 -0400

    For MySQL 5.5, TIMESTAMP value can`t be inserted in a DATETIME column

--- SQL/Archive/16.1/2016-07-19-HelpTableDefaultCreatedValue.sql
+++ SQL/Archive/16.1/2016-07-19-HelpTableDefaultCreatedValue.sql
@@ -1,4 +1,4 @@
 -- It would be preferable to run this UPDATE statement if your mysql server version is < 5.7
 -- UPDATE help SET created = '2014-09-01 00:00:00' WHERE created = '0000-00-00 00:00:00';
-ALTER TABLE help CHANGE `created` `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE help CHANGE `updated` `updated` DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP;
+ALTER TABLE help CHANGE `created` `created` DATETIME DEFAULT NULL;
+ALTER TABLE help CHANGE `updated` `updated` DATETIME DEFAULT NULL;
