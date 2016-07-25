a174b17133cac0ddb218e7eea41a50d4d8e35f0f
ridz1208 <ridz_1208@hotmail.com>
2016-07-25 11:08:08 -0400

    phpcs for GetParticipant_suboptions.php

--- modules/candidate_parameters/ajax/GetParticipant_suboptions.php
+++ modules/candidate_parameters/ajax/GetParticipant_suboptions.php
@@ -1,48 +1,69 @@
 <?php
-/** This is used by the candidate parameters to get the 
+/**
+ * This is used by the candidate parameters to get the
  * list of participant_suboptions via AJAX
+ *
+ * PHP Version 5
+ *
+ *  @category Loris
+ *  @package  Candidate_Parameters
+ *  @author   Loris Team <loris@mcgill.ca>
+ *  @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ *  @link     https://github.com/aces/Loris
  */
 set_include_path(get_include_path().":../project/libraries:../php/libraries:");
 ini_set('default_charset', 'utf-8');
 
-  require_once "Database.class.inc";
-  require_once 'NDB_Config.class.inc';
-  require_once 'NDB_Client.class.inc';
+require_once "Database.class.inc";
+require_once 'NDB_Config.class.inc';
+require_once 'NDB_Client.class.inc';
 
-  $user =& User::singleton();
-  if (!$user->hasPermission('candidate_parameter_edit')) {
-      header("HTTP/1.1 403 Forbidden");
-      exit;
-  }
+$user =& User::singleton();
+if (!$user->hasPermission('candidate_parameter_edit')) {
+    header("HTTP/1.1 403 Forbidden");
+    exit;
+}
 
-  $config =& NDB_Config::singleton();
-  $client = new NDB_Client();
-  $client->initialize();
+$config =& NDB_Config::singleton();
+$client = new NDB_Client();
+$client->initialize();
 
-  require_once "Utility.class.inc";
+require_once "Utility.class.inc";
 
-  $DB =& Database::singleton();
+$DB =& Database::singleton();
 if (isset($_REQUEST['p_status'])) {
-      print " \n";
-      $results = $DB->pselect("SELECT Description FROM participant_status_options 
-                      WHERE parentID=:pid", array('pid'=> $_REQUEST['p_status']));
+    print " \n";
+    $results = $DB->pselect(
+        "SELECT Description FROM participant_status_options
+                      WHERE parentID=:pid",
+        array('pid' => $_REQUEST['p_status'])
+    );
     foreach ($results as $row) {
-          print $row['Description'] . "\n";
+        print $row['Description'] . "\n";
     }
 } else if (isset($_REQUEST['pscid'])) {
-       $results = $DB->pselect("SELECT participant_status, participant_suboptions 
+    $results = $DB->pselect(
+        "SELECT participant_status, participant_suboptions
               FROM participant_status p JOIN candidate c ON (c.CandID = p.CandID)
-              WHERE c.PSCID=:pid", array('pid'=>"'".$_REQUEST['pscid']."'"));    
+              WHERE c.PSCID=:pid",
+        array('pid' => "'".$_REQUEST['pscid']."'")
+    );
     foreach ($results as $row) {
         $id = $row['participant_status'];
 
-        $desc = $DB->pselectOne("SELECT Description FROM participant_status_options 
-                                 WHERE ID=:id", array('id'=> $id));
-        $sub_desc = $DB->pselectOne("SELECT Description FROM participant_status_options 
-                         WHERE ID=:sid", array('sid'=> $row['participant_suboptions']));
+        $desc     = $DB->pselectOne(
+            "SELECT Description FROM participant_status_options 
+                                 WHERE ID=:id",
+            array('id' => $id)
+        );
+        $sub_desc = $DB->pselectOne(
+            "SELECT Description FROM participant_status_options 
+                         WHERE ID=:sid",
+            array('sid' => $row['participant_suboptions'])
+        );
 
-           print $id.";".$sub_desc;
-           
+        print $id.";".$sub_desc;
+
     }
 }
 ?>
