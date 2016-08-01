e2851ceb69034957425b4ea9511e0bd0843e7c5e
nirtiac <caitrin.armstrong@mail.mcgill.ca>
2016-08-01 15:58:48 -0400

    changed column formatting

--- modules/issue_tracker/js/columnFormatter.js
+++ modules/issue_tracker/js/columnFormatter.js
@@ -2,50 +2,64 @@ function formatColumn(column, cell, rowData, rowHeaders) {
     if (-1 == loris.hiddenHeaders.indexOf(column)) {
         // If this column is not a hidden one
 
+        console.log("in function formatcolumn");
+        console.log(column);
+        console.log(rowData);
+        console.log(rowHeaders);
         // Create the mapping between rowHeaders and rowData in a row object.
         var row = {};
         rowHeaders.forEach(function (header, index) {
             row[header] = rowData[index];
         }, this);
 
-        if (column === 'New Data') {
-            if (cell === 'new') {
-                return React.createElement(
-                    'td',
-                    { className: 'newdata' },
-                    'NEW'
-                );
-            }
-            return React.createElement('td', null);
-        }
-
-        if (column === 'Links') {
-            var cellTypes = cell.split(",");
+        if (column === 'Issue ID') {
             var cellLinks = [];
-            for (var i = 0; i < cellTypes.length; i += 1) {
-                cellLinks.push(React.createElement(
+            cellLinks.push(React.createElement(
                     'a',
-                    { href: loris.BaseURL + "/imaging_browser/viewSession/?sessionID=" + row.SessionID + "&outputType=" + cellTypes[i] + "&backURL=/imaging_browser/" },
-                    cellTypes[i]
+                    { href: loris.BaseURL + "/issue_tracker/edit/?issueID=" + row['Issue ID'] + "&backURL=/issue_tracker/" },
+                    cell
                 ));
-                cellLinks.push(" | ");
-            }
-            cellLinks.push(React.createElement(
-                'a',
-                { href: loris.BaseURL + "/imaging_browser/viewSession/?sessionID=" + row.SessionID + "&selectedOnly=1&backURL=/imaging_browser/" },
-                'selected'
-            ));
-            cellLinks.push(" | ");
-            cellLinks.push(React.createElement(
-                'a',
-                { href: loris.BaseURL + "/imaging_browser/viewSession/?sessionID=" + row.SessionID + "&backURL=/imaging_browser/" },
-                'all types'
-            ));
+
             return React.createElement(
                 'td',
                 null,
                 cellLinks
             );
+        }
+
+        if (column === 'Priority') {
+            switch (cell) {
+                case "normal":
+                    return React.createElement(
+                        "td",
+                        { style: { background: "#CCFFCC" } },
+                        "normal"
+                    );
+                case "high":
+                    return React.createElement(
+                        "td",
+                        { style: { background: "#EEEEAA" } },
+                        "high"
+                    );
+                case "urgent":
+                    return React.createElement(
+                        "td",
+                        { style: { background: "#CC6600" } },
+                        "urgent"
+                    );
+                case "immediate":
+                    return React.createElement(
+                        "td",
+                        { style: { background: "#E4A09E" } },
+                        "immediate"
+                    );
+                default:
+                    return React.createElement(
+                        "td",
+                        { style: { background: "#99CCFF" } },
+                        "Low"
+                    );
+            }
         }
 
         return React.createElement(
