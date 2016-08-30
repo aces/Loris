969285d268ec22af5d05ca905ccd1985ef592e54
kongtiaowang <kongtiaowangshen@gmail.com>
2016-08-30 15:28:58 -0400

    format fix

--- modules/reliability/js/columnFormatter.js
+++ modules/reliability/js/columnFormatter.js
@@ -1,5 +1,16 @@
 "use strict";
 
+/* global hasWritePermission */
+/* exported formatColumn */
+
+/**
+ * Modify behaviour of specified column cells in the Data Table component
+ * @param {string} column - column name
+ * @param {string} cell - cell content
+ * @param {arrray} rowData - array of cell contents for a specific row
+ * @param {arrray} rowHeaders - array of table headers (column names)
+ * @return {*} a formated table cell for a given column
+ */
 function formatColumn(column, cell, rowData, rowHeaders) {
   // If a column if set as hidden, don't display it
   if (loris.hiddenHeaders.indexOf(column) > -1) {
--- modules/reliability/jsx/columnFormatter.js
+++ modules/reliability/jsx/columnFormatter.js
@@ -1,3 +1,14 @@
+/* global hasWritePermission */
+/* exported formatColumn */
+
+/**
+ * Modify behaviour of specified column cells in the Data Table component
+ * @param {string} column - column name
+ * @param {string} cell - cell content
+ * @param {arrray} rowData - array of cell contents for a specific row
+ * @param {arrray} rowHeaders - array of table headers (column names)
+ * @return {*} a formated table cell for a given column
+ */
 function formatColumn(column, cell, rowData, rowHeaders) {
 // If a column if set as hidden, don't display it
   if (loris.hiddenHeaders.indexOf(column) > -1) {
@@ -8,19 +19,22 @@ function formatColumn(column, cell, rowData, rowHeaders) {
   var row = {};
   rowHeaders.forEach(function(header, index) {
     row[header] = rowData[index];
-}, this);
+  }, this);
 
- 
-    if(column === 'PSCID' ) {
-        if (row["Manual Swap"]==="yes") {
-        return <td>{cell} <span className="error">manual</span> </td>;
-        }
-        if(row["Invalid"] === 'yes' ) {
-        var url = loris.BaseURL + "/aosi_reliability?identifier=" +row['Visit Label'] + "&reliability_center_id=" + row['Reliability Center Id'];
-        return <td>{cell} <span className="error">invalid</span> </td>;}
-        var url = loris.BaseURL + "/aosi_reliability?identifier=" +row['Visit Label'] + "&reliability_center_id=" + row['Reliability Center Id'];
-        return <td><a href={url}>{cell}</a></td>;
+  if (column === 'PSCID') {
+    if (row["Manual Swap"] === "yes") {
+      return <td>{cell} <span className="error">manual</span> </td>;
     }
+    if (row["Invalid"] === 'yes') {
+      var url = loris.BaseURL + "/aosi_reliability?identifier=" +
+       row['Visit Label'] + "&reliability_center_id=" +
+       row['Reliability Center Id'];
+      return <td>{cell} <span className="error">invalid</span> </td>;}
+    var url = loris.BaseURL + "/aosi_reliability?identifier=" +
+       row['Visit Label'] + "&reliability_center_id=" +
+       row['Reliability Center Id'];
+    return <td><a href={url}>{cell}</a></td>;
+  }
 
-    return <td>{cell}</td>;
+  return <td>{cell}</td>;
 }
