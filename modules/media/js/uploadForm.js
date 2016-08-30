6e274f626ae647fc9766e5a81edae77d3c2f2122
Alex Ilea <jr.rus13@gmail.com>
2016-08-30 10:55:39 -0400

    Rerun transpiler

--- modules/media/js/uploadForm.js
+++ modules/media/js/uploadForm.js
@@ -76,11 +76,6 @@ var MediaUploadForm = React.createClass({
       );
     }
 
-    // var helpText = "File name should begin with " +
-    //   "<b>[PSCID]_[Visit Label]_[Instrument]</b><br> For example, " +
-    //   "for candidate <i>ABC123</i>, visit <i>V1</i> for " +
-    //   "<i>Body Mass Index</i> the file name should be prefixed by: " +
-    //   "<b>ABC123_V1_Body_Mass_Index</b>";
     var helpText = ["File name should begin with ", React.createElement(
       'b',
       null,
