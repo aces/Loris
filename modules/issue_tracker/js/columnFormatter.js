a2cbc51668b2163f269f7aa8904517c4e1fbafa1
nirtiac <caitrin.armstrong@mail.mcgill.ca>
2016-08-31 15:53:37 -0400

    some eslint changes plus recompiled columnFormatter

--- modules/issue_tracker/js/columnFormatter.js
+++ modules/issue_tracker/js/columnFormatter.js
@@ -1,71 +1,70 @@
 'use strict';
 
 function formatColumn(column, cell, rowData, rowHeaders) {
+  // Create the mapping between rowHeaders and rowData in a row object.
+  var row = {};
+  rowHeaders.forEach(function (header, index) {
+    row[header] = rowData[index];
+  }, this);
 
-    // Create the mapping between rowHeaders and rowData in a row object.
-    var row = {};
-    rowHeaders.forEach(function (header, index) {
-        row[header] = rowData[index];
-    }, this);
+  if (column === 'Issue ID') {
+    var cellLinks = [];
+    cellLinks.push(React.createElement(
+      'a',
+      {
+        href: loris.BaseURL + "/issue_tracker/edit/?issueID=" + row['Issue ID'] + "&backURL=/issue_tracker/" },
+      cell
+    ));
+    return React.createElement(
+      'td',
+      null,
+      cellLinks
+    );
+  }
 
-    if (column === 'Issue ID') {
-        var cellLinks = [];
-        cellLinks.push(React.createElement(
-            'a',
-            {
-                href: loris.BaseURL + "/issue_tracker/edit/?issueID=" + row['Issue ID'] + "&backURL=/issue_tracker/" },
-            cell
-        ));
+  if (column === 'Priority') {
+    switch (cell) {
+      case "normal":
         return React.createElement(
-            'td',
-            null,
-            cellLinks
+          'td',
+          { style: { background: "#CCFFCC" } },
+          'Normal'
+        );
+      case "high":
+        return React.createElement(
+          'td',
+          { style: { background: "#EEEEAA" } },
+          'High'
+        );
+      case "urgent":
+        return React.createElement(
+          'td',
+          { style: { background: "#CC6600" } },
+          'Urgent'
+        );
+      case "immediate":
+        return React.createElement(
+          'td',
+          { style: { background: "#E4A09E" } },
+          'Immediate'
+        );
+      case "low":
+        return React.createElement(
+          'td',
+          { style: { background: "#99CCFF" } },
+          'Low'
+        );
+      default:
+        return React.createElement(
+          'td',
+          null,
+          'None'
         );
     }
-
-    if (column === 'Priority') {
-        switch (cell) {
-            case "normal":
-                return React.createElement(
-                    'td',
-                    { style: { background: "#CCFFCC" } },
-                    'Normal'
-                );
-            case "high":
-                return React.createElement(
-                    'td',
-                    { style: { background: "#EEEEAA" } },
-                    'High'
-                );
-            case "urgent":
-                return React.createElement(
-                    'td',
-                    { style: { background: "#CC6600" } },
-                    'Urgent'
-                );
-            case "immediate":
-                return React.createElement(
-                    'td',
-                    { style: { background: "#E4A09E" } },
-                    'Immediate'
-                );
-            case "low":
-                return React.createElement(
-                    'td',
-                    { style: { background: "#99CCFF" } },
-                    'Low'
-                );
-            default:
-                return React.createElement(
-                    'td',
-                    null,
-                    'None'
-                );
-        }
-    }
-    return React.createElement(
-        'td',
-        null,
-        cell
-    );
+  }
+  return React.createElement(
+    'td',
+    null,
+    cell
+  );
 }
--- modules/issue_tracker/jsx/columnFormatter.js
+++ modules/issue_tracker/jsx/columnFormatter.js
@@ -1,40 +1,39 @@
 function formatColumn(column, cell, rowData, rowHeaders) {
-
     // Create the mapping between rowHeaders and rowData in a row object.
-    var row = {};
-    rowHeaders.forEach(
-        function (header, index) {
-            row[header] = rowData[index];
+  var row = {};
+  rowHeaders.forEach(
+        function(header, index) {
+          row[header] = rowData[index];
         },
         this
     );
 
-    if (column === 'Issue ID') {
-        var cellLinks = [];
-        cellLinks.push(<a
+  if (column === 'Issue ID') {
+    var cellLinks = [];
+    cellLinks.push(<a
             href={loris.BaseURL + "/issue_tracker/edit/?issueID=" + row['Issue ID'] + "&backURL=/issue_tracker/"}>{cell}</a>);
-        return (
+    return (
             <td>
                 {cellLinks}
             </td>
         );
-    }
+  }
 
-    if (column === 'Priority') {
-        switch (cell) {
-            case "normal":
-                return <td style={{ background : "#CCFFCC"}}>Normal</td>;
-            case "high":
-                return <td style={{ background: "#EEEEAA" }}>High</td>;
-            case "urgent":
-                return <td style={{ background: "#CC6600" }}>Urgent</td>
-            case "immediate":
-                return <td style={{ background: "#E4A09E" }}>Immediate</td>
-            case "low":
-                return <td style={{ background: "#99CCFF" }}>Low</td>
-            default:
-                return <td>None</td>;
-        }
+  if (column === 'Priority') {
+    switch (cell) {
+      case "normal":
+        return <td style={{background: "#CCFFCC"}}>Normal</td>;
+      case "high":
+        return <td style={{background: "#EEEEAA"}}>High</td>;
+      case "urgent":
+        return <td style={{background: "#CC6600"}}>Urgent</td>;
+      case "immediate":
+        return <td style={{background: "#E4A09E"}}>Immediate</td>;
+      case "low":
+        return <td style={{background: "#99CCFF"}}>Low</td>;
+      default:
+        return <td>None</td>;
     }
-    return <td>{cell}</td>;
+  }
+  return <td>{cell}</td>;
 }
