74d32090dbbced1c82c91d9c7221aebf5de5dcf5
Tara Campbell <tara0campbell@gmail.com>
2016-09-01 10:52:12 -0400

    Fix case on column name.

--- SQL/2016-08-29-AddForeignKeyToCertifcation.sql
+++ SQL/2016-08-29-AddForeignKeyToCertifcation.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `certification` MODIFY `testID` int(10) UNSIGNED NOT NULL;
 
-ALTER TABLE `certification` ADD CONSTRAINT `FK_certification` FOREIGN KEY (`TestID`) REFERENCES `test_names` (`ID`) ON UPDATE CASCADE;
+ALTER TABLE `certification` ADD CONSTRAINT `FK_certification` FOREIGN KEY (`testID`) REFERENCES `test_names` (`ID`) ON UPDATE CASCADE;
