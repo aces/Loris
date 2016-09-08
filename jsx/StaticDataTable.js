09665ab9a227ff2a47db7b9f71ec00b35e0da67e
Alex Ilea <jr.rus13@gmail.com>
2016-09-08 11:06:15 -0400

    Fix toCamelCase function

--- jsx/StaticDataTable.js
+++ jsx/StaticDataTable.js
@@ -125,7 +125,7 @@ var StaticDataTable = React.createClass({
     },
     toCamelCase: function(str) {
       return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
-        if (match === 0) return "";
+        if (Number(match) === 0) return "";
         return index === 0 ? match.toLowerCase() : match.toUpperCase();
       });
     },
