75a70399faf6dce9c8c191c68324817a4476fa75
kongtiaowang <kongtiaowangshen@gmail.com>
2016-09-12 10:59:03 -0400

    bug

--- modules/user_accounts/test/user_accountsTest.php
+++ modules/user_accounts/test/user_accountsTest.php
@@ -294,7 +294,7 @@ class UserAccountsIntegrationTest extends LorisIntegrationTest
         }
         $this->safeClick(WebDriverBy::Name("filter"));
         
-        $this->_assertUserReactTableContents ($testData,$expectedResults);
+        $this->_assertUserReactTableContents ($expectedResults,$testData);
     }
     /**
      * Compares the content of the candidate table with an expected content.
