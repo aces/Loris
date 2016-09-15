47f737057e9fe44c0b4390022dfbc41a81d90a90
MounaSafiHarab <mouna.safiharab@gmail.com>
2016-09-15 12:45:04 -0400

    ScannerID instead of ScannercandID as rightfully suggested by Dave

--- SQL/Archive/17.0/2016-09-13-AddScannerIDInFiles.sql
+++ SQL/Archive/17.0/2016-09-13-AddScannerIDInFiles.sql
@@ -1,5 +1,5 @@
--- Add ScannerCandID column to files, back-populate the newly added column, then add foreign key constraints 
--- NOTE: back-populating assumes that currently the candID used for phantoms is that of the scanner
-ALTER TABLE files ADD `ScannerCandID` int(11) DEFAULT NULL;
-UPDATE files f SET f.ScannerCandID=(SELECT s.CandID FROM session s WHERE s.ID=f.SessionID);
-ALTER TABLE files ADD CONSTRAINT `files_scannerID_fk` FOREIGN KEY (`ScannerCandID`) REFERENCES mri_scanner (`CandID`);
+-- Add ScannerCandID column to files, back-populate the newly added column from parameter_file table, then add foreign key constraints 
+ALTER TABLE files ADD `ScannerID` int(11) DEFAULT NULL;
+CREATE TEMPORARY TABLE ScannerIDs AS SELECT pf.FileID, pf.Value AS ScannerIDs FROM parameter_file AS pf LEFT JOIN parameter_type AS pt ON pf.ParameterTypeID=pt.ParameterTypeID WHERE pt.Name='ScannerID';
+UPDATE files AS f, ScannerIDs AS S SET f.FileID=S.FileID where f.FileID=S.FileID;
+ALTER TABLE files ADD CONSTRAINT `files_scannerID_fk` FOREIGN KEY (`ScannerID`) REFERENCES mri_scanner (`ID`);
