1382aaeb7759d578e973e3eb5c2103e3e7b80d17
MounaSafiHarab <mouna.safiharab@gmail.com>
2016-09-20 10:30:57 -0400

    updated the mysql patch as well

--- SQL/Archive/17.0/2016-09-13-AddScannerIDInFiles.sql
+++ SQL/Archive/17.0/2016-09-13-AddScannerIDInFiles.sql
@@ -1,5 +1,5 @@
 -- Add ScannerCandID column to files, back-populate the newly added column from parameter_file table, then add foreign key constraints 
-ALTER TABLE files ADD `ScannerID` int(11) DEFAULT NULL;
+ALTER TABLE files ADD `ScannerID` int(10) unsigned NOT NULL default '0';
 CREATE TEMPORARY TABLE ScannerIDs AS SELECT pf.FileID, pf.Value AS ScannerIDs FROM parameter_file AS pf LEFT JOIN parameter_type AS pt ON pf.ParameterTypeID=pt.ParameterTypeID WHERE pt.Name='ScannerID';
 UPDATE files AS f, ScannerIDs AS S SET f.FileID=S.FileID where f.FileID=S.FileID;
 ALTER TABLE files ADD CONSTRAINT `FK_files_scannerID` FOREIGN KEY (`ScannerID`) REFERENCES mri_scanner (`ID`);
