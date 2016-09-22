0e9120c3ce7c51b7ad224289c13073722734d675
MounaSafiHarab <mouna.safiharab@gmail.com>
2016-09-22 15:11:54 -0400

    display ScannerID in imaging browser from files table

--- tools/CouchDB_MRI_Importer.php
+++ tools/CouchDB_MRI_Importer.php
@@ -118,7 +118,8 @@ class CouchDBMRIImporter
         $inter_rej = 'IntergradientRejected_'.$type;
         $pipeline  = 'processing:pipeline';
 
-        $header['ScannerID_'.$type]           = $FileObj->getParameter('ScannerID');
+//        $header['ScannerID_'.$type]           = $FileObj->getParameter('ScannerID');
+        $header['ScannerID_'.$type]           = $this->_getScannerID((int)$FileObj->getParameter('FileID'));
         $header['Pipeline_'.$type]            = $FileObj->getParameter('Pipeline');
         $header['OutputType_'.$type]          = $FileObj->getParameter('OutputType');
         $header['AcquisitionProtocol_'.$type] = $FileObj->getAcquisitionProtocol();
@@ -192,6 +193,24 @@ class CouchDBMRIImporter
         }
     }
 
+    /**
+     * Gets the scannerID
+     *
+     * @param MRIFile $file  file object
+     *
+     * @return scannerID
+     */
+     function _getScannerID($FileID){
+ 
+         $selected = $this->SQLDB->pselectOne("SELECT ScannerID FROM files ".
+             "WHERE FileID =:FileID",
+             array(
+                 'FileID' => $FileID
+             )
+         );
+         return $scannerID;
+     }
+ 
     /**
      * Gets a rejected parameter according to its type
      *
