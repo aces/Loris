d4a71cc4b6af50b93d09c0fc6f34221ea7baef51
Shen <kongtiaowang@users.noreply.github.com>
2016-09-23 16:44:49 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,6 +39,7 @@ module.exports = function(grunt) {
                     'modules/media/js/uploadForm.js': 'modules/media/jsx/uploadForm.js',
                     'modules/survey_accounts/js/columnFormatter.js': 'modules/survey_accounts/jsx/columnFormatter.js',
 		    'modules/server_processes_manager/js/columnFormatter.js': 'modules/server_processes_manager/jsx/columnFormatter.js',
+		    'modules/mri_violations/js/mri_protocol_violations_columnFormatter.js': 'modules/mri_violations/jsx/mri_protocol_violations_columnFormatter.js',
                     'modules/user_accounts/js/columnFormatter.js': 'modules/user_accounts/jsx/columnFormatter.js',
                 }
             }
