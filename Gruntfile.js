ca8cd3af2a7d37168e4dfeb6407dfbad4de4c3fc
kongtiaowang <kongtiaowangshen@gmail.com>
2016-09-23 16:30:04 -0400

    rebase

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,13 +38,9 @@ module.exports = function(grunt) {
                     'modules/media/js/editForm.js': 'modules/media/jsx/editForm.js',
                     'modules/media/js/uploadForm.js': 'modules/media/jsx/uploadForm.js',
                     'modules/survey_accounts/js/columnFormatter.js': 'modules/survey_accounts/jsx/columnFormatter.js',
-<<<<<<< HEAD
-		    'modules/server_processes_manager/js/columnFormatter.js': 'modules/server_processes_manager/jsx/columnFormatter.js'
-                    'modules/mri_violations/js/mri_protocol_violations_columnFormatter.js': 'modules/mri_violations/jsx/mri_protocol_violations_columnFormatter.js'
-=======
-                    'modules/server_processes_manager/js/columnFormatter.js': 'modules/server_processes_manager/jsx/columnFormatter.js',
->>>>>>> fix conflict
-                    'modules/user_accounts/js/columnFormatter.js': 'modules/user_accounts/jsx/columnFormatter.js',
+		    'modules/server_processes_manager/js/columnFormatter.js': 'modules/server_processes_manager/jsx/columnFormatter.js',
+                    'modules/mri_violations/js/mri_protocol_violations_columnFormatter.js': 'modules/mri_violations/jsx/mri_protocol_violations_columnFormatter.js',
+                    'modules/user_accounts/js/columnFormatter.js': 'modules/user_accounts/jsx/columnFormatter.js'
                 }
             }
         },
