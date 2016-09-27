15b49c4a87c26bfea5335b8fa1ffafd4c8ed3504
kongtiaowang <kongtiaowangshen@gmail.com>
2016-09-27 16:41:15 -0400

    wait jsx

---
+++ modules/final_radiological_review/js/columnFormatter.js
@@ -0,0 +1,27 @@
+/* exported formatColumn */
+
+/**
+ * Modify behaviour of specified column cells in the Data Table component
+ * @param {string} column - column name
+ * @param {string} cell - cell content
+ * @param {arrray} rowData - array of cell contents for a specific row
+ * @param {arrray} rowHeaders - array of table headers (column names)
+ * @return {*} a formated table cell for a given column
+ */
+function formatColumn(column, cell, rowData, rowHeaders) {
+
+  if (loris.hiddenHeaders.indexOf(column) > -1) {
+    return null;
+  }
+  // Create the mapping between rowHeaders and rowData in a row object.
+  var row = {};
+  rowHeaders.forEach(function (header, index) {
+    row[header] = rowData[index];
+  }, this);
+
+  return React.createElement(
+    "td",
+    null,
+    cell
+  );
+}
