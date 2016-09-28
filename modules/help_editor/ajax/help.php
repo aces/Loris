4b94cee161cffb9b2f6e744d501b4e1561e9d18a
Dave MacFarlane <driusan@gmail.com>
2016-09-28 09:54:56 -0400

    [Core/Dicom_Archive] Markdown File Help Content (#2196)
    This adds the help content from markdown format files in the module `help/` directory.
    
    This should be significantly easier to maintain and review than storing the help content in the database where everything requires an SQL patch to update. It's also faster.
    
    This is only available for "new" style modules, and the dicom_archive (which previously had no help at all) is used as an example.
    
    Only a subset of markdown is supported. Namely:
    
    - Paragraphs (by separating text with an empty line)
    - Headers (by prefixing a paragraph with up to six `#` characters. Note that unlike real markdown, there must be an empty line before the first paragraph of the section)
    - Bold and italics with `*` or `**`
    - Markdown style links
    
    (The new React "Markdown" component can be used to render markdown other places in LORIS, if required.)

--- modules/help_editor/ajax/help.php
+++ modules/help_editor/ajax/help.php
@@ -11,6 +11,30 @@
  * @license  Loris license
  * @link     https://www.github.com/aces/Loris-Trunk/
  */
+
+try {
+        /**
+         * The link constructed from the front end sets the test_name
+         * parameter, not the Module parameter.
+         */
+        if (isset($_REQUEST['test_name'])) {
+		$mname = $_REQUEST['test_name'];
+	}
+	
+	$m = Module::factory($mname);
+} catch (Exception $e) {
+	$m = '';
+}
+if (!empty($m)) {
+	$page = !empty($_REQUEST['subtest']) ? $_REQUEST['subtest'] : $mname;
+	$help['content'] = $m->getHelp($page);
+	$help['format'] = 'markdown';
+	print json_encode($help);
+	ob_end_flush();
+	exit(0);
+}
+
+// Wasn't a module, so fall back on the old style of DB lookup.
 require_once "HelpFile.class.inc";
 
 if (!empty($_REQUEST['test_name'])) {
@@ -24,9 +48,7 @@ if (!empty($_REQUEST['test_name'])) {
 $help_file       = HelpFile::factory($helpID);
 $data            = $help_file->toArray();
 $data['content'] = utf8_encode(trim($data['content']));
-if (empty($data['content'])) {
-    $data['content'] = 'Under Construction';
-}
+
 if (empty($data['updated']) ) {
     $data['updated'] = "-";
     // if document was never updated should display date created  
