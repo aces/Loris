67f110d0b6cd8934a20135858f4a5e0c9179d42d
xlecours <xavierlb.mavan@gmail.com>
2016-09-28 10:49:39 -0400

    should be fine

--- modules/user_accounts/test/user_accountsTest.php
+++ modules/user_accounts/test/user_accountsTest.php
@@ -227,11 +227,6 @@ class UserAccountsIntegrationTest extends LorisIntegrationTest
         $field->sendKeys('email@gmail.com');
         $this->safeClick(WebDriverBy::Name('SendEmail'));
         $this->safeClick(WebDriverBy::Name('fire_away'));
-
-        $bodyText = $this->safeFindElement(
-            WebDriverBy::cssSelector("body")
-        )->getText();
-$this->assertContains('p8yuidabhs97cph0a9x72bdh',$bodyText);
         $this->_accessUser('user_accounts', 'userid');
         $field = $this->safeFindElement(WebDriverBy::Name('First_name'));
         $this->assertEquals($field->getAttribute('value'), 'first');
