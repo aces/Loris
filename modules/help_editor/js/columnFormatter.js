bbe10e2c8c703da244f3eae80c4659d0e0cb4722
kongtiaowang <kongtiaowangshen@gmail.com>
2016-09-29 16:25:33 -0400

    eslint

--- modules/help_editor/js/columnFormatter.js
+++ modules/help_editor/js/columnFormatter.js
@@ -9,18 +9,18 @@
  * @return {*} a formated table cell for a given column
  */
 function formatColumn(column, cell, rowData, rowHeaders) {
-
   if (loris.hiddenHeaders.indexOf(column) > -1) {
     return null;
   }
   // Create the mapping between rowHeaders and rowData in a row object.
   var row = {};
+  var url;
   rowHeaders.forEach(function (header, index) {
     row[header] = rowData[index];
   }, this);
 
   if (column === 'Topic') {
-    var url = loris.BaseURL + "/help_editor/edit_help_content/?helpID=" + row["HelpID"] + "&parentID=" + row["ParentID"];
+    url = loris.BaseURL + "/help_editor/edit_help_content/?helpID=" + row.HelpID + "&parentID=" + row.ParentID;
     return React.createElement(
       "td",
       null,
@@ -32,7 +32,7 @@ function formatColumn(column, cell, rowData, rowHeaders) {
     );
   }
   if (column === 'Parent Topic') {
-    var url = loris.BaseURL + "/help_editor/edit_help_content/?helpID=" + row["ParentID"] + "&parentID=" + row["ParentTopicID"];
+    url = loris.BaseURL + "/help_editor/edit_help_content/?helpID=" + row.ParentID + "&parentID=" + row.ParentTopicID;
     return React.createElement(
       "td",
       null,
--- modules/help_editor/jsx/columnFormatter.js
+++ modules/help_editor/jsx/columnFormatter.js
@@ -9,31 +9,30 @@
  * @return {*} a formated table cell for a given column
  */
 function formatColumn(column, cell, rowData, rowHeaders) {
-
   if (loris.hiddenHeaders.indexOf(column) > -1) {
     return null;
   }
   // Create the mapping between rowHeaders and rowData in a row object.
   var row = {};
+  var url;
   rowHeaders.forEach(function(header, index) {
     row[header] = rowData[index];
   }, this);
 
   if (column === 'Topic') {
-      var url = loris.BaseURL + "/help_editor/edit_help_content/?helpID=" + row["HelpID"] +"&parentID=" + row["ParentID"];
-      return <td>
+    url = loris.BaseURL + "/help_editor/edit_help_content/?helpID=" +
+           row.HelpID + "&parentID=" + row.ParentID;
+    return <td>
                 <a href ={url}>{cell}</a>
              </td>;
-
   }
   if (column === 'Parent Topic') {
-      var url = loris.BaseURL + "/help_editor/edit_help_content/?helpID=" + row["ParentID"] +"&parentID=" + row["ParentTopicID"];
-      return <td>
+    url = loris.BaseURL + "/help_editor/edit_help_content/?helpID=" + 
+           row.ParentID + "&parentID=" + row.ParentTopicID;
+    return <td>
                 <a href ={url}>{cell}</a>
              </td>;
-
   }
 
   return <td>{cell}</td>;
-
 }
