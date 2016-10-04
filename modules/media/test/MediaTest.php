e119b130eff57a4dc9365f0dcb6aacef5b689ac6
kongtiaowang <kongtiaowangshen@gmail.com>
2016-10-04 16:35:31 -0400

    test

--- modules/media/test/MediaTest.php
+++ modules/media/test/MediaTest.php
@@ -6,7 +6,7 @@
  *
  * @category Test
  * @package  Loris
- * @author   Tara Campbell <tara.campbell@mail.mcgill.ca>
+ * @author   Wang Shen <wangshen.mcin@gmail.ca>
  * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
  * @link     https://github.com/aces/Loris
  */
@@ -15,7 +15,7 @@ require_once __DIR__ .
     "/../../../test/integrationtests/LorisIntegrationTest.class.inc";
 
 /**
- * Training module automated integration tests
+ * Media module automated integration tests
  *
  * PHP Version 5
  *
