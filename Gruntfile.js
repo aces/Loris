f90eb8a5a79640e4c0d626b7216ff3f0d70192c6
kongtiaowang <kongtiaowangshen@gmail.com>
2016-10-04 14:38:11 -0400

    Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,9 +39,17 @@ module.exports = function(grunt) {
                     'modules/media/js/columnFormatter.js': 'modules/media/jsx/columnFormatter.js',
                     'modules/media/js/editForm.js': 'modules/media/jsx/editForm.js',
                     'modules/media/js/uploadForm.js': 'modules/media/jsx/uploadForm.js',
+		    'modules/issue_tracker/js/columnFormatter.js': 'modules/issue_tracker/jsx/columnFormatter.js',
+		    'modules/issue_tracker/js/editIssue.js': 'modules/issue_tracker/jsx/editIssue.js'
                     'modules/survey_accounts/js/columnFormatter.js': 'modules/survey_accounts/jsx/columnFormatter.js',
-		    'modules/server_processes_manager/js/columnFormatter.js': 'modules/server_processes_manager/jsx/columnFormatter.js',
-                    'modules/mri_violations/js/mri_protocol_check_violations_columnFormatter.js': 'modules/mri_violations/jsx/mri_protocol_check_violations_columnFormatter.js'
+                    'modules/server_processes_manager/js/columnFormatter.js': 'modules/server_processes_manager/jsx/columnFormatter.js',
+                    'modules/mri_violations/js/mri_protocol_check_violations_columnFormatter.js': 'modules/mri_violations/jsx/mri_protocol_check_violations_columnFormatter.js',
+                    'modules/candidate_parameters/js/candidateInfo.js': 'modules/candidate_parameters/jsx/candidateInfo.js',
+                    'modules/candidate_parameters/js/consentStatus.js': 'modules/candidate_parameters/jsx/consentStatus.js',
+                    'modules/candidate_parameters/js/familyInfo.js': 'modules/candidate_parameters/jsx/familyInfo.js',
+                    'modules/candidate_parameters/js/participantStatus.js': 'modules/candidate_parameters/jsx/participantStatus.js',
+                    'modules/candidate_parameters/js/probandInfo.js': 'modules/candidate_parameters/jsx/probandInfo.js'
+                    'modules/reliability/js/columnFormatter.js': 'modules/reliability/jsx/columnFormatter.js',
                 }
             }
         },
