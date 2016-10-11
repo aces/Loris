35c725557aab78742d4a19acb9cd5f56a36aca05
Stella Lee <stellajhlee@hotmail.com>
2016-10-11 11:31:03 -0400

    [Reliability] minor typo in comments (#2261)

--- modules/reliability/jsx/columnFormatter.js
+++ modules/reliability/jsx/columnFormatter.js
@@ -4,8 +4,8 @@
  * Modify behaviour of specified column cells in the Data Table component
  * @param {string} column - column name
  * @param {string} cell - cell content
- * @param {arrray} rowData - array of cell contents for a specific row
- * @param {arrray} rowHeaders - array of table headers (column names)
+ * @param {array} rowData - array of cell contents for a specific row
+ * @param {array} rowHeaders - array of table headers (column names)
  * @return {*} a formated table cell for a given column
  */
 function formatColumn(column, cell, rowData, rowHeaders) {
