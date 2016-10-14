7d7b6ac91f86543388ef1daff9e4cd2c5eb101af
christinerogers <christinerogers@users.noreply.github.com>
2016-10-14 11:51:34 -0400

    [DQT] Typo fix: Longitudial selected filter #2285
    In the Data Query Tool, when Longitudinal display mode (vs. cross-sectional) is selected,
    the displayed value is "Longitudial" (missing an n)

--- modules/dataquery/js/react.app.js
+++ modules/dataquery/js/react.app.js
@@ -739,7 +739,7 @@ DataQueryApp = React.createClass({
                 Identifiers.push(session);
             }
         } else {
-            // Displaying the data in the longitudial way
+            // Displaying the data in the longitudinal way
 
             var Visits = {},
                 visit,
@@ -877,7 +877,7 @@ DataQueryApp = React.createClass({
         }));
 
         // Define the data displayed type and add the view data tab
-        var displayType = this.state.grouplevel === 0 ? "Cross-sectional" : "Longitudial";
+        var displayType = this.state.grouplevel === 0 ? "Cross-sectional" : "Longitudinal";
         tabs.push(React.createElement(ViewDataTabPane, {
             TabId: "ViewData",
             Fields: this.state.fields,
