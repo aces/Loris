6bf67990da319917d5a6dc3a600702f947574f6d
xlecours <xavierlb.mavan@gmail.com>
2016-11-10 11:19:30 -0500

    schema changes

---
+++ SQL/Archive/17.1/20161110-LorisMenu_uniqueness.sql
@@ -0,0 +1,34 @@
+ALTER TABLE `LORIS`.`LorisMenu` 
+ADD INDEX `fk_LorisMenu_1_idx` (`Parent` ASC),
+ADD UNIQUE INDEX `index3` (`Parent` ASC, `Label` ASC);
+ALTER TABLE `LORIS`.`LorisMenu` 
+ADD CONSTRAINT `fk_LorisMenu_1`
+  FOREIGN KEY (`Parent`)
+  REFERENCES `LORIS`.`LorisMenu` (`ID`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+CREATE TEMPORARY TABLE tmp_lmp AS 
+  SELECT DISTINCT MenuId, PermID FROM LorisMenuPermissions;
+
+DELETE FROM LorisMenuPermissions;
+
+ALTER TABLE `LORIS`.`LorisMenuPermissions` 
+CHANGE COLUMN `MenuID` `MenuID` INT(10) UNSIGNED NOT NULL ,
+CHANGE COLUMN `PermID` `PermID` INT(10) UNSIGNED NOT NULL ,
+ADD PRIMARY KEY (`MenuID`, `PermID`);
+
+ALTER TABLE `LORIS`.`LorisMenuPermissions` 
+ADD CONSTRAINT `fk_LorisMenuPermissions_1`
+  FOREIGN KEY (`MenuID`)
+  REFERENCES `LORIS`.`LorisMenu` (`ID`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE,
+ADD CONSTRAINT `fk_LorisMenuPermissions_2`
+  FOREIGN KEY (`PermID`)
+  REFERENCES `LORIS`.`permissions` (`permID`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+INSERT INTO LorisMenuPermissions SELECT MenuID, PermID FROM tmp_lmp;
+DROP tmp_lmp;
