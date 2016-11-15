fdb4756cfeb3de2e6dc826b09121ef57ee5e359f
jstirling91 <jstirl@abunawas-2.local>
2016-11-15 14:18:18 -0500

    add pop out login for when users attempt to use ajax helpers when logged out

--- htdocs/js/helpHandler.js
+++ htdocs/js/helpHandler.js
@@ -80,5 +80,32 @@ $(document).ready(function(){
 
     $(".dynamictable").DynamicTable();
     $(".fileUpload").FileUpload();
+
+});
+$( document ).ajaxError(function(event, jqxhr, settings, thrownError) {
+    if (jqxhr.status === 403) {
+        if ($('#login-modal').hasClass('in')) {
+            $("#login-modal-error").show();
+        } else {
+            $('#login-modal').modal('show');
+            $("#modal-login").click(function(e) {
+                e.preventDefault();
+                var data = {
+                    "username" : $("#modal-username").val(),
+                    "password" : $("#modal-password").val(),
+                    "login"    : "Login"
+                }
+                $.ajax({
+                    type    : "post",
+                    url     : loris.BaseURL + "/main.php",
+                    data    : data,
+                    success : function() {
+                        $("#login-modal-error").hide();
+                        $('#login-modal').modal('hide')
+                    }
+                });
+            });
+        }
+    }
 });
 
