027b56a11441b21bfbee6635444e83223b16adb2
MounaSafiHarab <mouna.safiharab@gmail.com>
2016-11-21 16:46:18 -0500

    mysql patch and started looking at multiple modules

---
+++ SQL/Archive/17.1/2016-11-21-CreateUserSitesRelTable.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS `user_psc_rel`;
+CREATE TABLE `user_psc_rel` (
+  `UserID` int(10) unsigned NOT NULL default '0',
+  `CenterID` tinyint(2) unsigned default NULL,
+  PRIMARY KEY  (`UserID`,`CenterID`),
+  KEY `FK_user_psc_rel_2` (`CenterID`),
+  CONSTRAINT `FK_user_psc_rel_2` FOREIGN KEY (`CenterID`) REFERENCES `psc` (`CenterID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_user_psc_rel_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user_psc_site`
+--
+SET foreign_key_checks = 0;
+LOCK TABLES `user_psc_rel` WRITE, `psc` READ, `users` READ;
+/*!40000 ALTER TABLE `user_psc_rel` DISABLE KEYS */;
+INSERT INTO `user_psc_rel` (UserID, CenterID) SELECT ID, CenterID FROM users;
+/*!40000 ALTER TABLE `user_psc_rel` ENABLE KEYS */;
+UNLOCK TABLES;
+SET foreign_key_checks = 1;
