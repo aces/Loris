1fafad6cbe8cb09f12fe82a7a29cecb2e6fa145c
Alex Ilea <jr.rus13@gmail.com>
2016-11-24 17:29:42 -0500

    Remove all calls to getDOMNode() This function was removed in new version of React as this.refs['value'] is the actual DOM node now

--- modules/candidate_parameters/js/participantStatus.js
+++ modules/candidate_parameters/js/participantStatus.js
@@ -268,7 +268,7 @@ var ParticipantStatus = React.createClass({
       return;
     }
 
-    var alertMsg = this.refs["alert-message"].getDOMNode();
+    var alertMsg = this.refs["alert-message"];
     $(alertMsg).fadeTo(2000, 500).delay(3000).slideUp(500, function () {
       self.setState({
         updateResult: null
