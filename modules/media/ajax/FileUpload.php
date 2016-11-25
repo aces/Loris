3377d04a920928a37db09362e25039bb5443ae9c
Rida Abou-Haidar <ridz_1208@hotmail.com>
2016-11-24 22:00:55 -0500

    Modified sessionRecords query

--- modules/media/ajax/FileUpload.php
+++ modules/media/ajax/FileUpload.php
@@ -194,8 +194,11 @@ function getUploadFields()
     // Build array of session data to be used in upload media dropdowns
     $sessionData    = [];
     $sessionRecords = $db->pselect(
-        "SELECT c.PSCID, s.Visit_label, s.CenterID " .
-        "FROM candidate c LEFT JOIN session s USING(CandID) ORDER BY c.PSCID ASC",
+        "SELECT c.PSCID, s.Visit_label, s.CenterID, f.Test_name " .
+        "FROM candidate c ".
+        "LEFT JOIN session s USING(CandID) ".
+        "LEFT JOIN flag f ON (s.ID=f.SessionID) ".
+        "ORDER BY c.PSCID ASC",
         []
     );
 
@@ -228,6 +231,21 @@ function getUploadFields()
             $sessionData[$record["PSCID"]]['visits'][$record["Visit_label"]]
                 = $record["Visit_label"];
         }
+
+        // Populate instruments
+        if (!isset($sessionData[$record["PSCID"]][$record["Visit_label"]]['instruments'])) {
+            $sessionData[$record["PSCID"]][$record["Visit_label"]]['instruments'] = [];
+        }
+        if ($record["Test_name"] !== null && !in_array(
+                $record["Test_name"],
+                $sessionData[$record["PSCID"]][$record["Visit_label"]]['instruments'],
+                true
+            )
+        ) {
+            $sessionData[$record["PSCID"]][$record["Visit_label"]]['instruments'][$record["Test_name"]]
+                = $record["Test_name"];
+        }
+
     }
 
     // Build media data to be displayed when editing a media file
