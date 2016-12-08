aed7d8b952e3b9351ac0da8e51bf214c9f858819
David Blader <dblader.mcin@gmail.com>
2016-12-08 13:40:56 -0500

    made tooltip for multiselects

--- htdocs/js/instrument_form_control.js
+++ htdocs/js/instrument_form_control.js
@@ -46,4 +46,13 @@ $(document).ready(function () {
         }
         naList[i].onchange = notAnswered;
     }
+    var isMac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
+    if (isMac) {
+        var msg = "Hold CMD to select multiple options";
+    }
+    else {
+        var msg = "Hold CTRL to select multiple options";
+    }
+    $("select[multiple]").attr("title",msg);
+    $("select[multiple]").tooltip();
 });
