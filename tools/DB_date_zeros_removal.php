aefba5520e64966d57760af66f19e5322f550e0e
ridz1208 <ridz_1208@hotmail.com>
2016-12-08 19:47:33 -0500

    Replace type by original type

--- tools/DB_date_zeros_removal.php
+++ tools/DB_date_zeros_removal.php
@@ -43,7 +43,7 @@ echo "\n#################################################################\n\n".
 $database_name= $database['database'];
 
 $field_names = $db->pselect("
-                      SELECT TABLE_NAME,COLUMN_NAME,COLUMN_DEFAULT,DATA_TYPE,IS_NULLABLE 
+                      SELECT TABLE_NAME,COLUMN_NAME,COLUMN_DEFAULT,DATA_TYPE,IS_NULLABLE,COLUMN_TYPE 
                       FROM INFORMATION_SCHEMA.COLUMNS 
                       WHERE DATA_TYPE IN ('date','timestamp','datetime') 
                           AND TABLE_SCHEMA=:dbn",
@@ -59,10 +59,10 @@ foreach ($field_names as $key=>$field)
 {
     if ($field['COLUMN_DEFAULT']=='0000-00-00') {
         echo "The script will modify the date schema for TABLE: `".$field['TABLE_NAME']."` FIELD: `".$field['COLUMN_NAME']."` to default to NULL\n";
-        $alters .= "ALTER TABLE `".$field['TABLE_NAME']."` MODIFY `".$field['COLUMN_NAME']."` DATE DEFAULT NULL;\n";
+        $alters .= "ALTER TABLE `".$field['TABLE_NAME']."` MODIFY `".$field['COLUMN_NAME']."` ".$field['COLUMN_TYPE']." DEFAULT NULL;\n";
     } else if ($field['COLUMN_DEFAULT']=='0000-00-00 00:00:00') {
         echo "The script will modify the date schema for TABLE: `".$field['TABLE_NAME']."` FIELD: `".$field['COLUMN_NAME']."` to default to NULL\n";
-        $alters .= "ALTER TABLE `".$field['TABLE_NAME']."` MODIFY `".$field['COLUMN_NAME']."` DATETIME DEFAULT NULL;\n";
+        $alters .= "ALTER TABLE `".$field['TABLE_NAME']."` MODIFY `".$field['COLUMN_NAME']."` ".$field['COLUMN_TYPE']." DEFAULT NULL;\n";
     }
     if ($field['DATA_TYPE'] == 'date') {
         $updates .= "UPDATE ".$database['database'].".".$field['TABLE_NAME'].
