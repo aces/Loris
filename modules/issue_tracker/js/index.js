20ab3b1325910e18e68dc8848430e6380b039119
Alex Ilea <jr.rus13@gmail.com>
2016-12-09 15:44:23 -0500

    [Issue Tracker] PHPCS, ESLint, React 15

--- modules/issue_tracker/js/index.js
+++ modules/issue_tracker/js/index.js
@@ -1,16 +1,15 @@
 'use strict';
 
-/* global React, formatColumn */
+/* global ReactDOM, formatColumn */
 
 $(document).ready(function () {
-
   var url = loris.BaseURL + '/issue_tracker/?format=json';
   var currentPage = QueryString.get();
   if (currentPage && currentPage.submenu) {
     url += '&submenu=' + currentPage.submenu;
   }
 
-  React.render(React.createElement(DynamicDataTable, {
+  ReactDOM.render(React.createElement(DynamicDataTable, {
     DataURL: url,
     getFormattedCell: formatColumn
   }), document.getElementById("datatable"));
--- modules/issue_tracker/jsx/index.js
+++ modules/issue_tracker/jsx/index.js
@@ -1,19 +1,17 @@
-/* global React, formatColumn */
+/* global ReactDOM, formatColumn */
 
 $(document).ready(function() {
-
   let url = loris.BaseURL + '/issue_tracker/?format=json';
   const currentPage = QueryString.get();
   if (currentPage && currentPage.submenu) {
     url += '&submenu=' + currentPage.submenu;
   }
 
-  React.render(
+  ReactDOM.render(
     <DynamicDataTable
       DataURL={url}
       getFormattedCell={formatColumn}
     />,
     document.getElementById("datatable")
   );
-
 });
