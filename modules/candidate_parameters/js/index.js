ec21c83c0b02c28ff4089de7e1181ea4f2b97d72
Jacob Penny <jacob.penny@gmail.com>
2016-12-09 14:27:57 -0500

    [Candidate Params] Remove Javascript from template

---
+++ modules/candidate_parameters/js/index.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var queryStringArgs = QueryString.get();
+var scriptArgs = QueryString.get(document.currentScript.src);
+
+var candID = queryStringArgs.candID;
+var getDataUrl = loris.BaseURL + '/candidate_parameters/ajax/getData.php?candID=' + candID;
+var formHandlerUrl = loris.BaseURL + '/candidate_parameters/ajax/formHandler.php';
+
+$(function () {
+  // Adds tab href to url + opens tab based on hash on page load
+  // See: http://bit.ly/292MDI8
+  var hash = window.location.hash;
+  hash && $('ul.nav a[href="' + hash + '"]').tab('show');
+
+  $('.nav-tabs a').click(function (e) {
+    $(this).tab('show');
+    var scrollmem = $('body').scrollTop() || $('html').scrollTop();
+    window.location.hash = this.hash;
+    $('html,body').scrollTop(scrollmem);
+  });
+
+  var candidateInfo = React.createElement(CandidateInfo, {
+    dataURL: getDataUrl + '&data=candidateInfo',
+    action: formHandlerUrl,
+    tabName: 'candidateInfo'
+  });
+
+  React.render(candidateInfo, document.getElementById("cand-info"));
+
+  if (loris.config('useProband') === "true") {
+    var probandInfo = React.createElement(ProbandInfo, {
+      dataURL: getDataUrl + '&data=probandInfo',
+      action: formHandlerUrl,
+      tabName: 'probandInfo'
+    });
+    React.render(probandInfo, document.getElementById("proband-info"));
+  } else {
+    $('#proband-info-tab').hide();
+  }
+
+  if (loris.config('useFamilyID') === "true") {
+    var familyInfo = React.createElement(FamilyInfo, {
+      dataURL: getDataUrl + '&data=familyInfo',
+      action: formHandlerUrl,
+      tabName: 'familyInfo'
+    });
+    React.render(familyInfo, document.getElementById("family-info"));
+  } else {
+    $('#family-info-tab').hide();
+  }
+
+  var participantStatus = React.createElement(ParticipantStatus, {
+    dataURL: getDataUrl + '&data=participantStatus',
+    action: formHandlerUrl,
+    tabName: 'participantStatus'
+  });
+
+  React.render(participantStatus, document.getElementById("participant-status"));
+
+  if (scriptArgs.useConsent) {
+    var consentStatus = React.createElement(ConsentStatus, {
+      dataURL: getDataUrl + '&data=consentStatus',
+      action: formHandlerUrl,
+      tabName: 'consentStatus'
+    });
+    React.render(consentStatus, document.getElementById("consent-status"));
+  }
+});
