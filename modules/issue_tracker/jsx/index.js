7853e66bbe11b650565699280f4941d49b17d794
Alex Ilea <jr.rus13@gmail.com>
2016-12-09 16:45:29 -0500

    [Issue Tracker] Remove js from create/edit templates

--- modules/issue_tracker/jsx/index.js
+++ modules/issue_tracker/jsx/index.js
@@ -1,10 +1,23 @@
 /* global ReactDOM, formatColumn */
 
 $(document).ready(function() {
+  const querystring = QueryString.get();
+  if (loris.Subtest) {
+    createEditIssue(querystring);
+  } else {
+    issueTracker(querystring);
+  }
+});
+
+/**
+ * Renders DynamicTable for all tabs on the main issue tracker page
+ *
+ * @param querystring
+ */
+function issueTracker(querystring) {
   let url = loris.BaseURL + '/issue_tracker/?format=json';
-  const currentPage = QueryString.get();
-  if (currentPage && currentPage.submenu) {
-    url += '&submenu=' + currentPage.submenu;
+  if (querystring && querystring.submenu) {
+    url += '&submenu=' + querystring.submenu;
   }
 
   ReactDOM.render(
@@ -14,4 +27,22 @@ $(document).ready(function() {
     />,
     document.getElementById("datatable")
   );
-});
+}
+
+/**
+ * Renders the form when creating or editing an issue
+ *
+ * @param querystring
+ */
+function createEditIssue(querystring) {
+  const url = loris.BaseURL + '/issue_tracker/ajax/EditIssue.php';
+  const issueID = querystring.issueID ? querystring.issueID : '';
+
+  ReactDOM.render(
+    <IssueEditForm
+      DataURL={url + '?action=getData&issueID=' + issueID}
+      action={url + '?action=edit'}
+    />,
+    document.getElementById("issue-form")
+  );
+}
