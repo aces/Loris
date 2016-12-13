4dbe3986951c7d5f0cd04a89bb221a2fcff51adc
kongtiaowang <kongtiaowangshen@gmail.com>
2016-12-13 11:45:58 -0500

    test

---
+++ modules/issue_tracker/test/issue_trackerTest.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Issue Tracker automated integration tests
+ *
+ * PHP Version 5
+ *
+ * @category Test
+ * @package  Loris
+ * @author   Wang Shen <wangshen.mcin@gmail.com>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link     https://github.com/aces/Loris
+ */
+
+require_once __DIR__ .
+    "/../../../test/integrationtests/LorisIntegrationTest.class.inc";
+
+/**
+ * Issue Tracker automated integration tests
+ *
+ * PHP Version 5
+ *
+ * @category Test
+ * @package  Loris
+ * @author   Wang Shen <wangshen.mcin@gmail.com>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link     https://github.com/aces/Loris
+ */
+
+class issue_trackerTest extends LorisIntegrationTest
+{
+    /**
+     * Tests that, when loading the Issue Tracker module, some
+     * text appears in the body.
+     *
+     * @return void
+     */
+    function testIssueTrackerDoespageLoad()
+    {
+        $this->webDriver->get($this->url . "/issue_tracker/");
+        $bodyText = $this->webDriver->findElement(
+            WebDriverBy::cssSelector("body")
+        )->getText();
+        $this->assertContains("issue_tracker", $bodyText);
+    }
+
+    /**
+     * Tests that Issue Tracker loads with permission
+     *
+     * @return void
+     */
+    function testIssueTrackerDoespageLoadWithPermission()
+    {
+        $this->setupPermissions(array("issue_tracker_reporter"));
+        $this->webDriver->get($this->url . "/issue_tracker/");
+        $bodyText = $this->webDriver->findElement(
+            WebDriverBy::cssSelector("body")
+        )->getText();
+        $this->assertContains("issue_tracker", $bodyText);
+        $this->resetPermissions();
+    }
+    
+}
+?>
