982c751d79d28f526efbbe21c0a72a2682bb6eee
MounaSafiHarab <mouna.safiharab@gmail.com>
2016-12-15 13:07:49 -0500

    Daves first set of feedback

--- SQL/Archive/17.1/2016-11-21-CreateUserSitesRelTable.sql
+++ SQL/Archive/17.1/2016-11-21-CreateUserSitesRelTable.sql
@@ -11,7 +11,6 @@ CREATE TABLE `user_psc_rel` (
 --
 -- Dumping data for table `user_psc_site`
 --
-SET foreign_key_checks = 0;
 LOCK TABLES `user_psc_rel` WRITE, `psc` READ, `users` READ;
 /*!40000 ALTER TABLE `user_psc_rel` DISABLE KEYS */;
 INSERT INTO `user_psc_rel` (UserID, CenterID) SELECT ID, CenterID FROM users;
@@ -19,7 +18,6 @@ INSERT INTO `user_psc_rel` (UserID, CenterID) SELECT ID, CenterID FROM users;
 INSERT INTO `user_psc_rel` (1,1);
 /*!40000 ALTER TABLE `user_psc_rel` ENABLE KEYS */;
 UNLOCK TABLES;
-SET foreign_key_checks = 1;
 
 -- DROP column CenterID from the users table
 -- ALTER TABLE users DROP foreign key FK_users_1;
