b11158ef46acea2320fb3634e254d3263b6a6ec7
kongtiaowang <kongtiaowangshen@gmail.com>
2016-12-19 15:08:34 -0500

    format for bvl

--- modules/bvl_feedback/ajax/open_bvl_feedback_thread.php
+++ modules/bvl_feedback/ajax/open_bvl_feedback_thread.php
@@ -13,8 +13,8 @@
 ini_set('default_charset', 'utf-8');
 require_once "bvl_panel_ajax.php";
 
-if (isset($_POST['feedbackID']) && isset($_POST['candID'])){
-    $feedbackThread->openThread($_POST['feedbackID']);   
+if (isset($_POST['feedbackID']) && isset($_POST['candID'])) {
+    $feedbackThread->openThread($_POST['feedbackID']);
 }
 
 exit();
