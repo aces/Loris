b17bdc6be7d558240ad69b3d5f776e89ed521bc6
Dave MacFarlane <driusan@gmail.com>
2017-01-19 10:21:58 -0500

    Clean up the tools directory by deleting everything
    This cleans up the tools directory by deleting everything,
    as proposed in the LORIS meeting by @jstirling91. Any
    script that turns out to be necessary can be retrieved
    from the history and cleaned up.

--- tools/generate_tables_sql.php
+++ tools/generate_tables_sql.php
@@ -7,24 +7,13 @@
  * instrument it finds in the ip_output.txt file.  These sql files are output
  * to the tables_sql/ subdirectory.
  *
- * Usage: php generate_tables_sql.php [-D]
- * Options:
- *          [-D]: Adds DROP TABLE statement to output query
+ * ex cmd:  php generate_tables_sql.php
  *
  * @package behavioural
  */
 
- set_include_path(get_include_path().":../project/libraries:../php/libraries:");
-
 //Ensure php version compatability
 //taken from php.net notes
-if (version_compare(phpversion(),'4.3.0','<'))
-{
-    define('STDIN',fopen("php://stdin","r"));
-    register_shutdown_function( create_function( '' , 'fclose(STDIN);
-    fclose(STDOUT); fclose(STDERR); return true;' ) );
-}
-
 
 // require all relevant OO class libraries
 require_once __DIR__ . "/../vendor/autoload.php";
@@ -32,83 +21,79 @@ require_once "../php/libraries/Database.class.inc";
 require_once "../php/libraries/NDB_Config.class.inc";
 require_once "../php/libraries/NDB_BVL_Instrument.class.inc";
 
-// Get command line options
-$opts = getopt("D");
-
-$fp=fopen("ip_output.txt","r");
-$data=fread($fp, filesize("ip_output.txt"));
-fclose($fp);
+$data = stream_get_contents(STDIN);
 
 $instruments=explode("{-@-}",trim($data));
 
 $tblCount=0;
 $parameterCount=0;
+$pages = array();
 foreach($instruments AS $instrument){
     $catId="";
     $items=explode("\n",trim($instrument));
     foreach($items AS $item){
         $paramId="";
         $bits=explode("{@}",trim($item));
-        if(preg_match("/Examiner[0-9]*/" , (string)(array_key_exists(1,$bits) ? $bits[1] : null))){
+        if(preg_match("/Examiner[0-9]*/" , $bits[1])){
             continue;
         }
         switch($bits[0]){
             //generate the CREATE TABLE syntax
             case "table":
-                $filename = "../project/tables_sql/".$bits[1].".sql";
-                $output = "";
-                if (isset($opts["D"])) {
-                    $output = "DROP TABLE IF EXISTS `$bits[1]`;\n";
-                }
-                $output .= "CREATE TABLE `$bits[1]` (\n";
-                $output .= "`CommentID` varchar(255) NOT NULL default '',\n
-                            `UserID` varchar(255) default NULL,\n
-                            `Examiner` varchar(255) default NULL,\n
-                            `Testdate` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,\n
-                            `Data_entry_completion_status` enum('Incomplete','Complete') NOT NULL default 'Incomplete',\n";
+                $tablename = $bits[1];
+
+                $filename="../project/tables_sql/".$bits[1].".sql";
+                $output="CREATE TABLE `$bits[1]` (\n";
+                $output.="`CommentID` varchar(255) NOT NULL default '',
+                          `UserID` varchar(255) default NULL,
+                          `Examiner` varchar(255) default NULL,
+                          `Testdate` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                          `Data_entry_completion_status` enum('Incomplete','Complete') NOT NULL default 'Incomplete',\n";
             break;
+            case "page":
+                $pages[] = $bits[2];
+                continue;
 
             //no SQL need be generated.
             case "title":
+                $title = $bits[1];
             case "header":
                 continue;
             break;
 
             //generate specific column definitions for specific types of HTML elements
             default:
-                if((array_key_exists(1,$bits) ? $bits[1] : "") == "") {
+                if($bits[1] == "") {
                     continue;
                 }
                 if($bits[0]=="select"){
-                    $bits[0]=enumizeOptions(
-                        array_key_exists(3,$bits) ? $bits[3] : null,
-                        $table = array(),
-                        $bits[1]
-                    );
-                } else if((array_key_exists(0,$bits) ? $bits[0] : null) =="selectmultiple"){
+                    $bits[0]=enumizeOptions($bits[3], isset($tablename) ? $tablename : null, $bits[1]);
+                } else if($bits[0]=="selectmultiple"){
                     $bits[0]="varchar(255)";
-                } else if((array_key_exists(0,$bits) ? $bits[0] : null) == "textarea"){
+                } else if($bits[0]=="textarea"){
                     $bits[0]="text";
-                } else if((array_key_exists(0,$bits) ? $bits[0] : null) == "text"){
+                } else if($bits[0]=="text"){
                     $bits[0]="varchar(255)";
-                } else if((array_key_exists(0,$bits) ? $bits[0] : null) == "checkbox") {
+                } else if($bits[0]=="checkbox") {
                     $bits[0]="varchar(255)";
-                } else if ((array_key_exists(0,$bits) ? $bits[0] : null) == "static") {
+                } else if ($bits[0]=="static") {
                     $bits[0]="varchar(255)";
-                } else if ((array_key_exists(0,$bits) ? $bits[0] : null) == "radio") {
-                    $bits[0]=enumizeOptions($bits[3], $table = array(), $bits[1]);
                 }
-                if(array_key_exists(2,$bits)){
-                    $bits[2]=htmlspecialchars($bits[2]);
-                }
+
+                $bits[2]=htmlspecialchars($bits[2]);
                 $output.="`$bits[1]` $bits[0] default NULL,\n";
         }
 
     }
-    $output.="PRIMARY KEY  (`CommentID`)\n
-              );\n";
+    $output.="PRIMARY KEY  (`CommentID`)\n);\n";
+    print "Filename: $filename\n";
     $fp=fopen($filename, "w");
     fwrite($fp, $output);
+    fwrite($fp, "REPLACE INTO test_names (Test_name, Full_name, Sub_group) VALUES ('$tablename', '$title', 1);\n");
+    foreach($pages as $pageNo => $page) {
+        fwrite($fp, "INSERT INTO instrument_subtests (Test_name, Subtest_name, Description) VALUES ('$tablename', '" . $tablename  . "_page" . ($pageNo+1) . "', '$page');\n");
+
+    }
     fclose($fp);
 }
 
