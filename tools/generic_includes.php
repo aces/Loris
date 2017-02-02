fcfc557b77df372a9f3758713833968f112bc02c
ridz1208 <ridz1208@users.noreply.github.com>
2017-02-02 13:54:51 -0500

    [Tools] Generic includes are not generic enough (#2516)
    * [Tools] Generic includes are not generic enough
    
    * phpcs

--- tools/generic_includes.php
+++ tools/generic_includes.php
@@ -12,10 +12,14 @@
  * @link     https://github.com/aces/Loris
  */
 
-set_include_path(get_include_path().":../project/libraries:../php/libraries:");
+set_include_path(
+    get_include_path().":".
+    __DIR__."/../project/libraries:".
+    __DIR__."/../php/libraries:"
+);
 
 require_once __DIR__ . "/../vendor/autoload.php";
-$configFile = "../project/config.xml";
+$configFile = __DIR__."/../project/config.xml";
 $client     = new NDB_Client();
 $client->makeCommandLine();
 $client->initialize($configFile);
