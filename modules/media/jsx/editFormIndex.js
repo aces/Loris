05593acf0e9f62546c6a8109d4e6bcd3123add1c
Jacob Penny <jacob.penny@gmail.com>
2017-02-14 11:22:17 -0500

    Fix param name

--- modules/media/jsx/editFormIndex.js
+++ modules/media/jsx/editFormIndex.js
@@ -5,7 +5,7 @@ const args = QueryString.get(document.currentScript.src);
 
 $(function() {
   var mediaEditForm = <MediaEditForm
-    DataURL={`${loris.BaseURL}/media/ajax/FileUpload.php?action=getData&idMediaFile=${args.idMediaFile}`}
+    DataURL={`${loris.BaseURL}/media/ajax/FileUpload.php?action=getData&idMediaFile=${args.id}`}
     action={`${loris.BaseURL}/media/ajax/FileUpload.php?action=edit`}
   />;
 
