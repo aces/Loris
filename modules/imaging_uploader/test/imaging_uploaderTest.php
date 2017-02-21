80fd49de85961390e42ae6b838f46281ad0af83b
kongtiaowang <kongtiaowangshen@gmail.com>
2017-02-21 15:55:31 -0500

    test imaging uploader

--- modules/imaging_uploader/test/imaging_uploaderTest.php
+++ modules/imaging_uploader/test/imaging_uploaderTest.php
@@ -2,11 +2,69 @@
 require_once __DIR__ . "/../../../test/integrationtests/LorisIntegrationTest.class.inc";
 class imaging_uploaderTestIntegrationTest extends LorisIntegrationTest
 {
+    /**
+     * Backs up the useEDC config value and sets the value to a known
+     * value (true) for testing.
+     *`
+     * @return none
+     */
+    function setUp()
+    {
+        parent::setUp();
+        $this->DB->insert(
+            "mri_upload",
+            array(
+             'UploadID'                    => '9999999',
+             'UploadedBy'                  => 'test',
+             'UploadDate'                  => 'TestTestTest',
+             'UploadLocation'              => '/data/incoming/test',
+             'DecompressedLocation'        => '/tmp/ImagingUpload-test',
+             'InsertionComplete'           => '0',
+             'Inserting'                   => '0',
+             'PatientName'                 => 'TestTestTest',
+             'number_of_mincInserted'      => 'NULL',
+             'number_of_mincCreated'       => 'NULL',
+             'TarchiveID'                  => 'NULL',
+             'SessionID'                   => '999999',
+             'IsCandidateInfoValidated'    => '1',
+             'IsTarchiveValidated'         => '0',
+             'IsPhantom'                   => 'N',
+            )
+        );
+    }
+    /**
+     * Restore the values backed up in the setUp function
+     *
+     * @return none
+     */
+    function tearDown()
+    {
+        parent::tearDown();
+        $this->DB->delete("mri_upload", array('PatientName' => 'TestTestTest'));
+    }
+
     function testImagingUploaderDoespageLoad()
     {
         $this->safeGet($this->url . '/imaging_uploader/');
         $bodyText = $this->webDriver->findElement(WebDriverBy::cssSelector("body"))->getText();
         $this->assertContains("Imaging Upload", $bodyText);
     }
+ 
+    function testImagingUploaderLoadWithoutPermission()
+    {
+        $this->setupPermissions(array(""));
+        $this->safeGet($this->url . '/imaging_uploader/');
+        $bodyText = $this->webDriver->findElement(WebDriverBy::cssSelector("body"))->getText();
+        $this->assertContains("You do not have access to this page.", $bodyText);
+    }
+    
+    function testImagingUploaderLoadWithPermission()
+    {
+        $this->setupPermissions(array("imaging_uploader"));
+        $this->safeGet($this->url . '/imaging_uploader/');
+        $bodyText = $this->webDriver->findElement(WebDriverBy::cssSelector("body"))->getText();
+        $this->assertNotContains("You do not have access to this page.", $bodyText);
+    }
+
 }
 ?>
