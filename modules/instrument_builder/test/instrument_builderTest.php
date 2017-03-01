93f0816ed31b26c6e92eb25ed19915234fa0baa1
kongtiaowang <kongtiaowangshen@gmail.com>
2017-03-01 14:23:00 -0500

    add new test module

--- modules/instrument_builder/test/instrument_builderTest.php
+++ modules/instrument_builder/test/instrument_builderTest.php
@@ -21,31 +21,6 @@ class instrumentBuilderTestIntegrationTest extends LorisIntegrationTest
      *
      * @return void
      */
-    public function setUp()
-    {
-        parent::setUp();
-        $this->DB->insert("candidate", array(
-            'CandID' => '900000',
-            'PSCID'  => 'TST0001',
-            'CenterID' => 1,
-            'Active' => 'Y',
-            'UserID' => 1,
-            'Entity_type' => 'Human'
-        ));
-        $this->DB->insert('session', array(
-            'ID' => '999999',
-            'CandID' => '900000',
-            'Visit_label' => 'V1',
-            'CenterID' => 1,
-            'Current_stage'   => 'Not Started',
-        ));
-        // Set up database wrapper and config
-    }
-    public function tearDown() {
-        $this->DB->delete("session", array('CandID' => '900000'));
-        $this->DB->delete("candidate", array('CandID' => '900000'));
-        parent::tearDown();
-    }
     function testInstrumentBuilderDoespageLoad()
     {
         $this->safeGet($this->url . "/instrument_builder/");
@@ -82,33 +57,6 @@ class instrumentBuilderTestIntegrationTest extends LorisIntegrationTest
                    ->getText();
         $this->assertContains("You do not have access to this page.", $bodyText);
         $this->resetPermissions();
-    }
-    /**
-     * 
-     * Testing $content appears in the body.
-     *
-     * @param string  $content      testing content
-     *
-     * @return void
-     */
-    private function _testContent($content)
-    {
-      $this->safeGet($this->url . "/testtest/?candID=900000&sessionID=999999");
-      $bodyText = $this->webDriver->findElement(WebDriverBy::cssSelector("body"))
-                   ->getText();
-      $this->assertContains($content, $bodyText);
-    }
-    /**
-     * Testing instrument element appears in the body.
-     *
-     * @return void
-     */
-    function testInstrumentWithLorisForm()
-    {
-        $this->_testContent("IBIS Environment Residential History");
-
-        //add more test case 
-        // $this->_testContent("instrument element");
     }
 }
 ?>
