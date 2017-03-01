4e6a7b339799a964fd042360d46b245543ce77ef
kongtiaowang <kongtiaowangshen@gmail.com>
2017-03-01 14:45:12 -0500

    add test_instrument

---
+++ modules/test_instrument/test_instrumentTest.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Instrument_builder automated integration tests
+ *
+ * PHP Version 5
+ *
+ * @category Test
+ * @package  Loris
+ * @author   Wang Shen <wangshen.mcin@gmail.com>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link     https://github.com/aces/Loris
+ */
+
+require_once __DIR__ . "/../../../test/integrationtests/LorisIntegrationTest.class.inc";
+class TestInstrumentTestIntegrationTest extends LorisIntegrationTest
+{
+    /**
+     * Tests that, when loading the Instrument builder module, some
+     * text appears in the body.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->DB->insert("candidate", array(
+            'CandID' => '900000',
+            'PSCID'  => 'TST0001',
+            'CenterID' => 1,
+            'Active' => 'Y',
+            'UserID' => 1,
+            'Entity_type' => 'Human'
+        ));
+        $this->DB->insert('session', array(
+            'ID' => '999999',
+            'CandID' => '900000',
+            'Visit_label' => 'V1',
+            'CenterID' => 1,
+            'Current_stage'   => 'Not Started',
+        ));
+        // Set up database wrapper and config
+    }
+    /**
+     * 
+     * Testing $content appears in the body.
+     *
+     * @param string  $content      testing content
+     *
+     * @return void
+     */
+    private function _testContent($content)
+    {
+      $this->safeGet($this->url . "/testtest/?candID=900000&sessionID=999999");
+      $bodyText = $this->webDriver->findElement(WebDriverBy::cssSelector("body"))
+                   ->getText();
+      $this->assertContains($content, $bodyText);
+    }
+    /**
+     * Testing instrument element appears in the body.
+     *
+     * @return void
+     */
+    function testInstrumentWithLorisForm()
+    {
+        $this->_testContent("IBIS Environment Residential History");
+
+        //add more test case 
+        // $this->_testContent("instrument element");
+    }
+   /**
+    * Testing instrument element appears in the body.
+    * After editing NDB_instrument php file, modify $instrument_element
+    * 
+    * @return void
+    */
+//    function testInstrumentWithLorisForm()
+//    {
+
+//         $this->_testContent("$instrument_element");
+
+//    }
+}
+?>
