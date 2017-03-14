0aff03b2009753ad0385943de1addfcf1572fa2e
xlecours <xavierlb.mavan@gmail.com>
2017-03-14 13:14:11 -0400

    adding genome_loc & gene fields to the variable properties

--- SQL/Archive/17.1/2017-03-13_migrating_genome_loc.sql
+++ SQL/Archive/17.1/2017-03-13_migrating_genome_loc.sql
@@ -7,8 +7,17 @@ ALTER TABLE `CNV`
   ADD COLUMN `Size` int(11) DEFAULT NULL,
   ADD COLUMN `StartLoc` int(11) DEFAULT NULL;
 
--- Fill thos fields
+-- Add fields from gene table
+ALTER TABLE `CNV`
+  ADD COLUMN `Symbol` varchar(255) DEFAULT NULL,
+  ADD COLUMN `Name` varchar(255) DEFAULT NULL,
+  ADD COLUMN `NCBIID` varchar(255) DEFAULT NULL,
+  ADD COLUMN `OfficialSymbol` varchar(255) DEFAULT NULL,
+  ADD COLUMN `OfficialName` text;
+
+-- Fill those fields
 UPDATE CNV t JOIN genome_loc gl USING (GenomeLocID) set t.Chromosome = gl.Chromosome, t.Strand = gl.Strand, t.EndLoc = gl.EndLoc, t.Size = gl.Size, t.StartLoc = gl.StartLoc;
+UPDATE CNV t JOIN gene g USING (GenomeLocID) set t.Symbol = g.Symbol, t.Name = g.Name, t.NCBIID = g.NCBIID, t.OfficialSymbol = g.OfficialSymbol, t.OfficialName = g.OfficialName;
 
 -- Remove the FOREIGN KEY using constraint name from infomration_schema.
 SET @constraint_name = (SELECT CONSTRAINT_NAME FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS WHERE table_name = 'CNV' AND CONSTRAINT_SCHEMA = DATABASE() AND REFERENCED_TABLE_NAME = 'genome_loc');
@@ -21,7 +30,6 @@ DEALLOCATE PREPARE stmt;
 ALTER TABLE CNV 
   DROP COLUMN GenomeLocID;
 
-
 -- SNP
 -- Add fields from genome_loc table
 ALTER TABLE `SNP`
@@ -31,8 +39,17 @@ ALTER TABLE `SNP`
   ADD COLUMN `Size` int(11) DEFAULT NULL,
   ADD COLUMN `StartLoc` int(11) DEFAULT NULL;
 
+-- Add fields from gene table
+ALTER TABLE `CNV`
+  ADD COLUMN `Symbol` varchar(255) DEFAULT NULL,
+  ADD COLUMN `Name` varchar(255) DEFAULT NULL,
+  ADD COLUMN `NCBIID` varchar(255) DEFAULT NULL,
+  ADD COLUMN `OfficialSymbol` varchar(255) DEFAULT NULL,
+  ADD COLUMN `OfficialName` text;
+
 -- Fill thos fields
 UPDATE SNP t JOIN genome_loc gl USING (GenomeLocID) set t.Chromosome = gl.Chromosome, t.Strand = gl.Strand, t.EndLoc = gl.EndLoc, t.Size = gl.Size, t.StartLoc = gl.StartLoc;
+UPDATE SNP t JOIN gene g USING (GenomeLocID) set t.Symbol = g.Symbol, t.Name = g.Name, t.NCBIID = g.NCBIID, t.OfficialSymbol = g.OfficialSymbol, t.OfficialName = g.OfficialName;
 
 -- Remove the FOREIGN KEY using constraint name from infomration_schema.
 SET @constraint_name = (SELECT CONSTRAINT_NAME FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS WHERE table_name = 'SNP' AND CONSTRAINT_SCHEMA = DATABASE() AND REFERENCED_TABLE_NAME = 'genome_loc');
@@ -67,4 +84,7 @@ DEALLOCATE PREPARE stmt;
 -- Drop the reference column
 ALTER TABLE genomic_cpg_annotation
   DROP COLUMN location_id;
+
+RENAME TABLE genome_loc TO to_be_deleted_genome_loc;
+RENAME TABLE gene TO to_be_deleted_gene;
 
