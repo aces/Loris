5676875ca14fd6a319a0d7b0a93b5ec2fdf65d1a
kongtiaowang <kongtiaowangshen@gmail.com>
2017-03-16 10:33:39 -0400

    GetScoreLabels.php

--- modules/statistics/ajax/GetScoreLabels.php
+++ modules/statistics/ajax/GetScoreLabels.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Get CSV files
+ *
+ * PHP Version 5
+ *
+ * @category Statistics
+ * @package  Loris
+ * @author   Dave MacFarlane <driusan@bic.mni.mcgill.ca>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link     https://www.github.com/aces/Loris/
+ */
 /* This is used by the stats page to get the list of scorable columns for an
  * instrument via AJAX. It's used so that ScatterPlot-SVG.php can be run for
  * any scorable in an instrument, dynamically */
@@ -15,7 +26,7 @@ $client->initialize();
 require_once "Utility.class.inc";
 
 $cols = Utility::getScoreColsForInstrument($_REQUEST['instrument']);
-if(strrpos($_REQUEST['instrument'], "_proband") === false) {
+if (strrpos($_REQUEST['instrument'], "_proband") === false) {
     print "Candidate_Age\n";
 }
 foreach ($cols as $val) {
