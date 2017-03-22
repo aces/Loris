b64aacf85d4067ad77c3f12a8e60d7359a8d99f1
kongtiaowang <kongtiaowangshen@gmail.com>
2017-03-22 10:50:59 -0400

    fix formatting

--- modules/document_repository/ajax/addCategory.php
+++ modules/document_repository/ajax/addCategory.php
@@ -12,7 +12,7 @@
  */
 
 $user =& User::singleton();
-if (!$user->hasPermission('document_repository_view') && !$user->hasPermission('document_repository_delete')) {
+if (!$user->hasPermission('document_repository_view') && !$user->hasPermission('document_repository_delete')) {// @codingStandardsIgnoreLine
     header("HTTP/1.1 403 Forbidden");
     exit;
 }
@@ -24,7 +24,7 @@ require_once "Email.class.inc";
 
 $factory = NDB_Factory::singleton();
 $baseURL = $factory->settings()->getBaseURL();
-$client = new NDB_Client();
+$client  = new NDB_Client();
 $client->initialize("../../project/config.xml");
 
 $config = NDB_Config::singleton();
@@ -52,12 +52,14 @@ if ($_POST['comments'] !== '') {
 
 $user =& User::singleton();
 //if user has document repository permission
-if ($user->hasPermission('document_repository_view') || $user->hasPermission('document_repository_delete')) {
+if ($user->hasPermission('document_repository_view') || $user->hasPermission('document_repository_delete')) {// @codingStandardsIgnoreLine
     $DB->insert(
         "document_repository_categories",
-        array("category_name" => $category_name,
-              "parent_id"     => $parent_id,
-              "comments"      => $comments)
+        array(
+         "category_name" => $category_name,
+         "parent_id"     => $parent_id,
+         "comments"      => $comments,
+        )
     );
 
 
@@ -66,8 +68,8 @@ if ($user->hasPermission('document_repository_view') || $user->hasPermission('do
     $msg_data['study']       = $config->getSetting('title');
 
     $Doc_Repo_Notification_Emails = $DB->pselect(
-        "SELECT Email from users where Active='Y' and Doc_Repo_Notifications='Y' and UserID<>:uid",
-        array("uid"=>$user->getUsername())
+        "SELECT Email from users where Active='Y' and Doc_Repo_Notifications='Y' and UserID<>:uid",// @codingStandardsIgnoreLine
+        array("uid" => $user->getUsername())
     );
     foreach ($Doc_Repo_Notification_Emails as $email) {
         Email::send($email['Email'], 'document_repository.tpl', $msg_data);
