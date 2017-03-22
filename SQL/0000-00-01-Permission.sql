4aec2d233371eb0a8e9ddd7eb32a4fc6aa2b6e3f
MounaSafiHarab <mouna.safiharab@gmail.com>
2017-03-22 09:33:19 -0400

    typo

--- SQL/0000-00-01-Permission.sql
+++ SQL/0000-00-01-Permission.sql
@@ -109,7 +109,7 @@ INSERT INTO `permissions` VALUES
     (45,'media_write','Media files: Uploading/Downloading/Editing','2'),
     (46,'media_read','Media files: Browsing','2'),
     (47,'issue_tracker_reporter', 'Can add a new issue, edit own issue, comment on all', 2),
-    (48,'issue_tracker_developer', 'Can re-assign issues, mark issues as closed, comment on all, edit issues.', 2);
+    (48,'issue_tracker_developer', 'Can re-assign issues, mark issues as closed, comment on all, edit issues.', 2),
     (49,'imaging_browser_phantom_data', 'Can acess phantom data in Imaging Browser', 2);
 
 
