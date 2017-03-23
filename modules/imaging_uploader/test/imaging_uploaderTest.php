4be1117f1f47b7ca6ec1cd6ceb17d9a9d8fa6ad1
kongtiaowang <kongtiaowangshen@gmail.com>
2017-03-23 14:53:06 -0400

    imaging uploader test

--- modules/imaging_uploader/test/imaging_uploaderTest.php
+++ modules/imaging_uploader/test/imaging_uploaderTest.php
@@ -1,11 +1,41 @@
 <?php
-require_once __DIR__ . "/../../../test/integrationtests/LorisIntegrationTest.class.inc";
+/**
+ * Imaging_uploader
+ * 
+ *
+ * PHP Version 5
+ *
+ *  @category   Behavioural
+ *  @package    Main
+ *  @subpackage Imaging
+ *  @author     Dave MacFarlane <driusan@bic.mni.mcgill.ca>
+ *  @license    Loris License
+ *  @link       https://www.github.com/aces/Loris-Trunk/
+ */
+
+require_once __DIR__ . 
+           "/../../../test/integrationtests/LorisIntegrationTest.class.inc";
+/**
+ * Imaging_uploader
+ *
+ * PHP Version 5
+ *
+ *  @category   Behavioural
+ *  @package    Main
+ *  @subpackage Imaging
+ *  @author     Dave MacFarlane <driusan@bic.mni.mcgill.ca>
+ *  @license    Loris License
+ *  @link       https://www.github.com/aces/Loris-Trunk/
+ */
+
 class imaging_uploaderTestIntegrationTest extends LorisIntegrationTest
 {
     function testImagingUploaderDoespageLoad()
     {
         $this->safeGet($this->url . '/imaging_uploader/');
-        $bodyText = $this->webDriver->findElement(WebDriverBy::cssSelector("body"))->getText();
+        $bodyText = $this->webDriver->findElement(
+                        WebDriverBy::cssSelector("body")
+                    )->getText();
         $this->assertContains("Imaging Upload", $bodyText);
     }
 }
