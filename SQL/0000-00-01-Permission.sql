6665d2c136613999aaca2c9c8851fba48b6003aa
MounaSafiHarab <mouna.safiharab@gmail.com>
2017-03-29 15:09:56 -0400

    Jakes feedback

--- SQL/0000-00-01-Permission.sql
+++ SQL/0000-00-01-Permission.sql
@@ -110,7 +110,7 @@ INSERT INTO `permissions` VALUES
     (46,'media_read','Media files: Browsing','2'),
     (47,'issue_tracker_reporter', 'Can add a new issue, edit own issue, comment on all', 2),
     (48,'issue_tracker_developer', 'Can re-assign issues, mark issues as closed, comment on all, edit issues.', 2),
-    (49,'imaging_browser_phantom_data', 'Can acess phantom data in Imaging Browser', 2);
+    (49,'imaging_browser_phantom_data', 'Can access only phantom data in Imaging Browser', 2);
 
 
 
