fb5c87189755ab73c416cb38382a32f80e78f93c
sruthymathew123 <sruthy.mcin@gmail.com>
2017-03-29 10:58:33 -0400

    Update help.php

--- modules/help_editor/ajax/help.php
+++ modules/help_editor/ajax/help.php
@@ -47,8 +47,7 @@ if (!empty($_REQUEST['testName'])) {
 
 $help_file = HelpFile::factory($helpID);
 $data = $help_file->toArray();
-$data['content'] = trim($data['content']);
-$data['content'] = html_entity_decode($data['content']);
+$data['content'] = html_entity_decode(trim($data['content']));
 
 if (empty($data['updated'])) {
     $data['updated'] = "-";
