472a8ff67e804ecee1ebb8a5fd27a01041981a96
kongtiaowang <kongtiaowangshen@gmail.com>
2017-03-30 16:13:18 -0400

    categoryEdit.php

--- modules/document_repository/ajax/categoryEdit.php
+++ modules/document_repository/ajax/categoryEdit.php
@@ -12,7 +12,9 @@
  */
 
 $user =& User::singleton();
-if (!$user->hasPermission('document_repository_view') && !$user->hasPermission('document_repository_delete')) {// @codingStandardsIgnoreLine
+if (!$user->hasPermission('document_repository_view')
+    && !$user->hasPermission('document_repository_delete')
+) {
     header("HTTP/1.1 403 Forbidden");
     exit;
 }
@@ -37,7 +39,9 @@ if (get_magic_quotes_gpc()) {
 $user =& User::singleton();
 
 //if user has document repository permission
-if ($user->hasPermission('document_repository_view') || $user->hasPermission('document_repository_delete')) {// @codingStandardsIgnoreLine
+if ($user->hasPermission('document_repository_view')
+    || $user->hasPermission('document_repository_delete')
+) {
     $DB->update(
         'document_repository_categories',
         array('comments' => $comments),
