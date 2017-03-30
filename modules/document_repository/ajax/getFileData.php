3ba4c0c613c31b08e99c20938f2adf2081e0e63a
kongtiaowang <kongtiaowangshen@gmail.com>
2017-03-30 16:54:55 -0400

    getFileData.php

--- modules/document_repository/ajax/getFileData.php
+++ modules/document_repository/ajax/getFileData.php
@@ -11,7 +11,9 @@
   * @link     https://github.com/aces/Loris
   */
 $user =& User::singleton();
-if (!$user->hasPermission('document_repository_view') && !$user->hasPermission('document_repository_delete')) {// @codingStandardsIgnoreLine
+if (!$user->hasPermission('document_repository_view')
+    && !$user->hasPermission('document_repository_delete')
+) {
     header("HTTP/1.1 403 Forbidden");
     exit;
 }
@@ -24,7 +26,10 @@ $client->initialize("../../project/config.xml");
 // create Database object
 $DB =& Database::singleton();
 
-$result = $DB->pselectRow("SELECT * FROM document_repository where record_id =:identifier", array(':identifier' => $_GET['id']));// @codingStandardsIgnoreLine
+$result = $DB->pselectRow(
+    "SELECT * FROM document_repository where record_id =:identifier",
+    array(':identifier' => $_GET['id'])
+);
 
 echo json_encode($result);
 
