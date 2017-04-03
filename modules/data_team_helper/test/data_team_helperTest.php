01a65871e89c4227e2124465329176597e02ef0d
Shen <kongtiaowang@users.noreply.github.com>
2017-04-03 11:40:42 -0400

    [Data team helper] PHPCS (#2663)

--- modules/data_team_helper/test/data_team_helperTest.php
+++ modules/data_team_helper/test/data_team_helperTest.php
@@ -1,19 +1,29 @@
 <?php
 /**
- * data_team_helper automated integration tests
+ * Data_team_helper automated integration tests
  *
  * PHP Version 5
  *
  * @category Test
  * @package  Loris
- * @author   Ted Strauss <ted.strauss@mcgill.ca>
  * @author   Wang Shen <wangshen.mcin@gmail.com>
  * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
  * @link     https://github.com/aces/Loris
  */
-
-require_once __DIR__ . "/../../../test/integrationtests/LorisIntegrationTest.class.inc";
-class dataTeamHelperTestIntegrationTest extends LorisIntegrationTest
+require_once __DIR__ .
+        "/../../../test/integrationtests/LorisIntegrationTest.class.inc";
+/**
+ * Data_team_helper automated integration tests
+ *
+ * PHP Version 5
+ *
+ * @category Test
+ * @package  Loris
+ * @author   Wang Shen <wangshen.mcin@gmail.com>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link     https://github.com/aces/Loris
+ */
+class DataTeamHelperTestIntegrationTest extends LorisIntegrationTest
 {
     /**
      * Tests that, when loading the data_team_helper module, some
@@ -24,7 +34,9 @@ class dataTeamHelperTestIntegrationTest extends LorisIntegrationTest
     function testDataTeamHelperDoespageLoad()
     {
         $this->safeGet($this->url . "/data_team_helper/");
-        $bodyText = $this->webDriver->findElement(WebDriverBy::cssSelector("body"))->getText();
+        $bodyText = $this->webDriver->findElement(
+            WebDriverBy::cssSelector("body")
+        )->getText();
         $this->assertContains("Data Team Helper", $bodyText);
     }
      /**
@@ -32,16 +44,19 @@ class dataTeamHelperTestIntegrationTest extends LorisIntegrationTest
       *
       * @return void
       */
-     function testDataTeamHelperWithoutPermission()
-     {
-          $this->setupPermissions(array());
-          $this->safeGet($this->url . "/data_team_helper/");
-          $bodyText = $this->safeFindElement(
-              WebDriverBy::cssSelector("body")
-          )->getText();
-          $this->assertContains("You do not have access to this page.", $bodyText);
-          $this->resetPermissions();
-     }
+    function testDataTeamHelperWithoutPermission()
+    {
+         $this->setupPermissions(array());
+         $this->safeGet($this->url . "/data_team_helper/");
+         $bodyText = $this->safeFindElement(
+             WebDriverBy::cssSelector("body")
+         )->getText();
+         $this->assertContains(
+             "You do not have access to this page.",
+             $bodyText
+         );
+         $this->resetPermissions();
+    }
     /**
      * Tests that help editor loads with the permission
      *
@@ -52,11 +67,14 @@ class dataTeamHelperTestIntegrationTest extends LorisIntegrationTest
          $this->setupPermissions(array("data_team_helper"));
          $this->safeGet($this->url . "/data_team_helper/");
          $bodyText = $this->safeFindElement(
-              WebDriverBy::cssSelector("body")
-          )->getText();
-          $this->assertNotContains("You do not have access to this page.", $bodyText);
+             WebDriverBy::cssSelector("body")
+         )->getText();
+          $this->assertNotContains(
+              "You do not have access to this page.",
+              $bodyText
+          );
           $this->resetPermissions();
-     }
+    }
 
 }
 ?>
