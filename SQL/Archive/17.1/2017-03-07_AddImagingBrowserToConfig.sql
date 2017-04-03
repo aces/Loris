b800865e500704de49e263bf6be861eccfee4633
Gregory Luneau <gluneau.mcin@gmail.com>
2017-04-03 11:09:09 -0400

    Update 2017-03-07_AddImagingBrowserToConfig.sql
    Making it easier for existing study

--- SQL/Archive/17.1/2017-03-07_AddImagingBrowserToConfig.sql
+++ SQL/Archive/17.1/2017-03-07_AddImagingBrowserToConfig.sql
@@ -8,7 +8,7 @@ UPDATE ConfigSettings SET Name='imaging_modules', Description='DICOM Archive and
 INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber) SELECT 'tblScanTypes', 'Scan types from the mri_scan_type table that the project wants to see displayed in Imaging Browser table', 1, 1, 'scan_type', ID, 'Imaging Browser Tabulated Scan Types', 6 FROM ConfigSettings WHERE Name="imaging_modules";
 
 -- default imaging_browser settings
-INSERT INTO Config (ConfigID, Value) SELECT cs.ID, GROUP_CONCAT(mst.Scan_Type) FROM ConfigSettings cs JOIN mri_scan_type mst WHERE cs.Name="tblScanTypes" AND mst.Scan_type='t1';
-INSERT INTO Config (ConfigID, Value) SELECT cs.ID, GROUP_CONCAT(mst.Scan_Type) FROM ConfigSettings cs JOIN mri_scan_type mst WHERE cs.Name="tblScanTypes" AND mst.Scan_type='t2';
+INSERT INTO Config (ConfigID, Value) SELECT cs.ID, GROUP_CONCAT(mst.Scan_Type) FROM ConfigSettings cs JOIN mri_scan_type mst WHERE cs.Name="tblScanTypes" AND mst.ID=44;
+INSERT INTO Config (ConfigID, Value) SELECT cs.ID, GROUP_CONCAT(mst.Scan_Type) FROM ConfigSettings cs JOIN mri_scan_type mst WHERE cs.Name="tblScanTypes" AND mst.ID=45;
 
 
