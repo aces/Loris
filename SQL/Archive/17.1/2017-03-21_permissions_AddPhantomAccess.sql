e0ee9ff4961ca625d3d4c1090773da80935975a3
MounaSafiHarab <mouna.safiharab@gmail.com>
2017-04-06 15:17:34 -0400

    force Travis to start

--- SQL/Archive/17.1/2017-03-21_permissions_AddPhantomAccess.sql
+++ SQL/Archive/17.1/2017-03-21_permissions_AddPhantomAccess.sql
@@ -3,4 +3,3 @@ INSERT INTO permissions (code,description,categoryID) VALUES ('imaging_browser_p
 INSERT INTO user_perm_rel (userID,permID) SELECT u.ID, p.permID FROM users u JOIN permissions p WHERE u.UserID='admin' AND p.code = 'imaging_browser_phantom_data';
 INSERT INTO LorisMenuPermissions (MenuID, PermID)
     SELECT m.ID, p.PermID FROM permissions p CROSS JOIN LorisMenu m WHERE p.code='imaging_browser_phantom_data' AND m.Label='Imaging Browser';
-
