bba1f9e6e22b47d54cce4f7e1a2eb43244092807
kongtiaowang <kongtiaowangshen@gmail.com>
2017-04-26 11:10:56 -0400

    frr

--- modules/final_radiological_review/test/final_radiological_reviewTest.php
+++ modules/final_radiological_review/test/final_radiological_reviewTest.php
@@ -15,6 +15,107 @@ require_once __DIR__ . "/../../../test/integrationtests/LorisIntegrationTest.cla
 class finalRadiologicalReviewTestIntegrationTest extends LorisIntegrationTest
 {
     /**
+     * Setup the testing data
+     *
+     * @return void
+     */
+    function setUp()
+    {
+        parent::setUp();
+        //Insert two violation scan
+        $this->DB->insert(
+            "psc",
+            array(
+             'CenterID'  => '55',
+             'Name'      => 'TESTinPSC',
+             'Alias'     => 'tst',
+             'MRI_alias' => 'test',
+            )
+        );
+        $this->DB->insert(
+            "candidate",
+            array(
+             'CandID'      => '999888',
+             'CenterID'    => '55',
+             'UserID'      => '1',
+             'PSCID'       => '8888',
+             'ProjectID'   => '7777',
+             'Entity_type' => 'Human',
+             'Active'      => 'Y',
+            )
+        );
+        $this->DB->insert(
+            "session",
+            array(
+             'ID'           => '222222',
+             'CandID'       => '999888',
+             'CenterID'     => '55',
+             'UserID'       => '1',
+             'MRIQCStatus'  => 'Pass',
+             'SubprojectID' => '6666',
+             'Active'       => 'Y',
+            )
+        );
+         $this->DB->insert(
+             "flag",
+             array(
+              'ID'         => '111111',
+              'SessionID'  => '222222',
+              'Test_name'  => 'TestName11111111111',
+              'CommentID'  => 'commentID111',
+              'Data_entry' => 'In Progress',
+             )
+         );
+          $this->DB->insert(
+              "final_radiological_review",
+              array(
+               'CommentID'            => 'CommentID111',
+               'Final_Review_Results' => 'not_answered',
+              )
+          );
+    }
+    /**
+     * Delete the test data
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        $this->DB->run('SET foreign_key_checks =0');
+        $this->DB->delete(
+            "issues",
+            array('issueID' => '999999')
+        );
+        $this->DB->delete(
+            "final_radiological_review",
+            array('CommentID' => 'CommentID111')
+        );
+        $this->DB->delete(
+            "session",
+            array('ID' => '222222')
+        );
+        $this->DB->delete(
+            "candidate",
+            array(
+             'CandID'   => '999888',
+             'CenterID' => '55',
+            )
+        );
+        $this->DB->delete(
+            "psc",
+            array(
+             'CenterID' => '55',
+             'Name'     => 'TESTinPSC',
+            )
+        );
+        $this->DB->delete(
+            "flag",
+            array('CommentID' => 'commentID111')
+        );
+        $this->DB->run('SET foreign_key_checks =1');
+        parent::tearDown();
+    }
+    /**
      * Tests that the final Radiological Review loads if the user has the correct
      * permissions (edit_final_radiological_review or view_final_radiological_review)
      * It should only be able to see the menu item.
@@ -52,6 +153,9 @@ class finalRadiologicalReviewTestIntegrationTest extends LorisIntegrationTest
 
         $this->resetPermissions();
     }
+
+
+
 }
 ?>
 
