b1ea59574ba733ae0e44f35e38b26dccb987d2bc
zainvirani <zvirani.mcin@gmail.com>
2017-05-11 15:36:49 -0400

    [Core] Fixed constraint to explicitly name unique key

--- SQL/Archive/17.1/2017-05-11-unique-sub-tests.sql
+++ SQL/Archive/17.1/2017-05-11-unique-sub-tests.sql
@@ -3,6 +3,6 @@ SET SQL_NOTES=1;
 
 SELECT 'Alter instrument_subtests to force unique Subtest_name' as 'Step #1';
 ALTER TABLE `instrument_subtests`
-ADD UNIQUE (Subtest_name);
+ADD UNIQUE KEY `Subtest_name` (`Subset_name`);
 
 SELECT 'Patch complete' as 'Status';
