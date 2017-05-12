b4cde042c7613a7f29d0f320d65e2852c9b26bb1
zainvirani <zvirani.mcin@gmail.com>
2017-05-12 15:35:28 -0400

    [Core] Delete duplicate entries in instrument_subtests

--- SQL/Archive/17.1/2017-05-11-unique-sub-tests.sql
+++ SQL/Archive/17.1/2017-05-11-unique-sub-tests.sql
@@ -1,7 +1,25 @@
 WARNINGS;
 SET SQL_NOTES=1;
 
-SELECT 'Alter instrument_subtests to force unique Subtest_name' as 'Step #1';
+SELECT 'Delete duplicate entries' as 'Step #1';
+DELETE 
+    i
+FROM 
+    instrument_subtests i
+LEFT JOIN
+    (
+        SELECT 
+            ID,
+            Subtest_name AS keep 
+        FROM 
+            instrument_subtests 
+        GROUP BY keep
+    ) AS KeepRows 
+ON
+  i.ID = KeepRows.ID
+WHERE KeepRows.ID IS NULL;
+
+SELECT 'Alter instrument_subtests to force unique Subtest_name' as 'Step #2';
 ALTER TABLE `instrument_subtests`
 ADD UNIQUE KEY `Subtest_name` (`Subtest_name`);
 
