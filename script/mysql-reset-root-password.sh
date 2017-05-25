551c95e679267d828bc200412806968855c6f4d8
kongtiaowang <kongtiaowangshen@gmail.com>
2017-05-25 14:22:51 -0400

    mysql-reset-root-password.sh

--- script/mysql-reset-root-password.sh
+++ script/mysql-reset-root-password.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env bash
 set -x
 set -e
@@ -6,6 +5,8 @@ sudo service mysql stop || echo "mysql not stopped"
 sudo stop mysql-5.6 || echo "mysql-5.6 not stopped"
 sudo  mysqld_safe --skip-grant-tables &
 sleep 4
-mysql -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('')"
-sudo /etc/init.d/mysql restart
+sudo mysql -e "use mysql; update user set authentication_string=PASSWORD('') where User='root'; update user set plugin='mysql_native_password';FLUSH PRIVILEGES;"
+sudo kill -9 `sudo cat /var/lib/mysql/mysqld_safe.pid`
+sudo kill -9 `sudo cat /var/run/mysqld/mysqld.pid`
+sudo service mysql restart
 sleep 4
