31a112665e328fd98a3a0ec4c6ec98895f649608
kongtiaowang <kongtiaowangshen@gmail.com>
2017-05-25 14:11:48 -0400

    mysql-reset-root-password.sh

--- script/mysql-reset-root-password.sh
+++ script/mysql-reset-root-password.sh
@@ -7,6 +7,5 @@ sudo stop mysql-5.6 || echo "mysql-5.6 not stopped"
 sudo  mysqld_safe --skip-grant-tables &
 sleep 4
 mysql -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('')"
-
-sudo service mysql restart
+sudo /etc/init.d/mysql restart
 sleep 4
