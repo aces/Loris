7e5bc9e16e4263bf63181365006315368b607939
Leo Thomas <leo.thomas@mail.mcgill.ca>
2017-05-25 15:20:02 -0400

    javascript cleanup to make travis happy

--- modules/media/jsx/columnFormatter.js
+++ modules/media/jsx/columnFormatter.js
@@ -11,47 +11,45 @@ function formatColumn(column, cell, rowData, rowHeaders) {
   if (loris.hiddenHeaders.indexOf(column) > -1) {
     return null;
   }
-  
+
   // Create the mapping between rowHeaders and rowData in a row object.
   var row = {};
   rowHeaders.forEach(function(header, index) {
     row[header] = rowData[index];
   }, this);
-
-  var classes=[];
-  if(row['Hide File']==1){
+  var classes = [];
+  if (row['Hide File'] === 1) {
     classes.push("bg-danger");
   }
-
-  classes = classes.join(" ");
   
+  classes = classes.join(" ");
   const hasWritePermission = loris.userHasPermission('media_write');
+  
   if (column === 'File Name' && hasWritePermission === true) {
-    var downloadURL = loris.BaseURL + "/media/ajax/FileDownload.php?File=" +
-      row['File Name'];
-      return (
-        <td className= {classes}>
-          <a href={downloadURL} target="_blank" download={row['File Name']}>
-            {cell}
-          </a>
-        </td>
-      );
+    var downloadURL = loris.BaseURL + "/media/ajax/FileDownload.php?File=" 
+    +row['File Name'];
+    return (
+      <td className= {classes}>
+        <a href={downloadURL} target="_blank" download={row['File Name']}>
+          {cell}
+        </a>
+      </td>
+    );
   }
-
+  
   if (column === 'Visit Label') {
     if (row["Cand ID"] !== null && row["Session ID"]) {
       var sessionURL = loris.BaseURL + "/instrument_list/?candID=" +
         row["Cand ID"] + "&sessionID=" + row["Session ID"];
       return <td className={classes}><a href={sessionURL}>{cell}</a></td>;
-      
     }
   }
-
+  
   if (column === 'Edit Metadata') {
     var editURL = loris.BaseURL + "/media/edit/?id=" + row['Edit Metadata'];
-    return <td className={classes}><a href={editURL}>Edit</a></td>;
-      
+    return <td className={classes}><a href={editURL}>Edit</a></td>;     
     }
+  
   return <td className= {classes}>{cell}</td>;
 }
 
