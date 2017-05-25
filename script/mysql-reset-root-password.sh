62c09fe198eacd80798c0f7c6229aa5f610f107d
kongtiaowang <kongtiaowangshen@gmail.com>
2017-05-25 12:35:11 -0400

    mysql-reset-root-password.sh

--- script/mysql-reset-root-password.sh
+++ script/mysql-reset-root-password.sh
@@ -6,6 +6,7 @@ sudo stop mysql-5.6 || echo "mysql-5.6 not stopped"
 sudo  mysqld_safe --skip-grant-tables &
 sleep 4
 echo "flush"
+sudo mysql -e "use mysql; update user set authentication_string=PASSWORD('') where User='root'; update user set plugin='mysql_native_password';FLUSH PRIVILEGES;"
 sudo kill -9 `sudo cat /var/lib/mysql/mysqld_safe.pid`
 echo "kill a"
 sudo kill -9 `sudo cat /var/run/mysqld/mysqld.pid`
