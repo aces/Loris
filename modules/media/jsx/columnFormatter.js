0499a49e3c44ceb05d0e94c2f409f301b9503428
Leo Thomas <leo.thomas@mail.mcgill.ca>
2017-05-26 11:22:09 -0400

    formatting to columnFormatter.js to make travis happy

--- modules/media/jsx/columnFormatter.js
+++ modules/media/jsx/columnFormatter.js
@@ -17,17 +17,18 @@ function formatColumn(column, cell, rowData, rowHeaders) {
   rowHeaders.forEach(function(header, index) {
     row[header] = rowData[index];
   }, this);
+
+  // create array of classes to be added to td tag
   var classes = [];
   if (row['Hide File'] === 1) {
     classes.push("bg-danger");
   }
-  
+  // convert array to string, with blank space separator
   classes = classes.join(" ");
+
   const hasWritePermission = loris.userHasPermission('media_write');
-  
   if (column === 'File Name' && hasWritePermission === true) {
-    var downloadURL = loris.BaseURL + "/media/ajax/FileDownload.php?File=" 
-    +row['File Name'];
+    var downloadURL = loris.BaseURL + "/media/ajax/FileDownload.php?File=" + row['File Name'];
     return (
       <td className= {classes}>
         <a href={downloadURL} target="_blank" download={row['File Name']}>
@@ -36,7 +37,7 @@ function formatColumn(column, cell, rowData, rowHeaders) {
       </td>
     );
   }
-  
+
   if (column === 'Visit Label') {
     if (row["Cand ID"] !== null && row["Session ID"]) {
       var sessionURL = loris.BaseURL + "/instrument_list/?candID=" +
@@ -44,12 +45,12 @@ function formatColumn(column, cell, rowData, rowHeaders) {
       return <td className={classes}><a href={sessionURL}>{cell}</a></td>;
     }
   }
-  
+
   if (column === 'Edit Metadata') {
     var editURL = loris.BaseURL + "/media/edit/?id=" + row['Edit Metadata'];
-    return <td className={classes}><a href={editURL}>Edit</a></td>;     
-    }
-  
+    return <td className={classes}><a href={editURL}>Edit</a></td>;
+  }
+
   return <td className= {classes}>{cell}</td>;
 }
 
