b33529897a79252e859753294dc8e896c585a1bb
kongtiaowang <kongtiaowangshen@gmail.com>
2017-05-30 16:10:07 -0400

    unittests/CandidateTest.php

--- test/unittests/CandidateTest.php
+++ test/unittests/CandidateTest.php
@@ -726,8 +726,6 @@ class CandidateTest extends PHPUnit_Framework_TestCase
         $site = $this->getMockBuilder('Site')->getMock();  
         $site->method('singleton')
              ->will($this->returnValue('1'));
-        $site->method('singleton')
-             ->will($this->returnValue('1'));
          $this->assertEquals(
             1,
             Candidate::createNew('1', NULL,NULL,NULL,NULL)
