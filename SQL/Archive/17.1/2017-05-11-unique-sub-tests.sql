eb8d43843f3596b64c8b0ee69218e9f9c1beccd0
zainvirani <zvirani.mcin@gmail.com>
2017-06-02 15:45:32 -0400

    Safer query to delete duplicates

--- SQL/Archive/17.1/2017-05-11-unique-sub-tests.sql
+++ SQL/Archive/17.1/2017-05-11-unique-sub-tests.sql
@@ -3,21 +3,15 @@ SET SQL_NOTES=1;
 
 SELECT 'Delete duplicate entries' as 'Step #1';
 DELETE 
-    i
 FROM 
-    instrument_subtests i
-LEFT JOIN
-    (
-        SELECT 
-            ID,
-            Subtest_name AS keep 
-        FROM 
-            instrument_subtests 
-        GROUP BY keep
-    ) AS KeepRows 
-ON
-  i.ID = KeepRows.ID
-WHERE KeepRows.ID IS NULL;
+	instrument_subtests 
+USING 
+	instrument_subtests,
+	instrument_subtests to_keep 
+WHERE 
+	instrument_subtests.ID < to_keep.ID 
+	AND instrument_subtests.Test_name = to_keep.Test_name
+	AND instrument_subtests.Subtest_name = to_keep.Subtest_name;
 
 SELECT 'Alter instrument_subtests to force unique Subtest_name' as 'Step #2';
 ALTER TABLE `instrument_subtests`
