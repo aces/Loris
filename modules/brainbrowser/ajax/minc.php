cd0a0415b672d720b22c240554e0e7f0e152cda6
Gregory Luneau <gluneau.mcin@gmail.com>
2017-06-06 10:41:50 -0400

    load minc directly

--- modules/brainbrowser/ajax/minc.php
+++ modules/brainbrowser/ajax/minc.php
@@ -30,8 +30,6 @@ $minc_file = getMincLocation() . $minc_file;
 $header      = $_REQUEST['minc_headers'];
 $header_data = $_REQUEST['raw_data'];
 if ($header_data) {
-    //passthru("minctoraw -double -normalize $minc_file");
-// https://greg-dev.loris.ca/main.php?test_name=brainbrowser&minc_id=[56780] ace-db-2    
     $fp = fopen($minc_file, 'r');
     fpassthru($fp);
     fclose($fp);
