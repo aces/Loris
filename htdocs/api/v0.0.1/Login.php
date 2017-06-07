ca6def2513976644f978a9ea289d75bd13a8af18
John Saigle <john.saigle@gmail.com>
2017-06-07 11:47:05 -0400

    Change HTTP status code from 403 to 500

--- htdocs/api/v0.0.1/Login.php
+++ htdocs/api/v0.0.1/Login.php
@@ -79,7 +79,7 @@ class Login extends APIBase
             if ($token) {
                 $this->JSON = array("token" => $token);
             } else {
-                $this->header("HTTP/1.1 403 Forbidden");
+                $this->header("HTTP/1.1 500 Internal Server Error");
                 $this->JSON = array("error" => "Unacceptable JWT key.");
             }
         } else {
