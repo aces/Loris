58d565fc1e2c5f6d2a11fa77c0f9ae28148b1ec8
xlecours <xavier.lecoursboucher@mcgill.ca>
2017-06-14 15:58:26 -0400

    Adjusting directory name and namespace
    class comments

--- htdocs/api/v0.0.3/index.php
+++ htdocs/api/v0.0.3/index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../../../vendor/autoload.php';
 header('Content-type: application/json');
 ob_start();
 
-$api = new \LORIS\API\EntryPoint();
+$api = new \LORIS\RestAPI\EntryPoint();
 $api->run();
 
 ob_end_flush();
