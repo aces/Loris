abaeed73b6b765bad02f8ff36e99aa8b623cb709
MounaSafiHarab <mouna.safiharab@gmail.com>
2017-06-14 14:40:08 -0400

    API: Update candidate and visit creation message for multisite users

--- htdocs/api/v0.0.2/Candidates.php
+++ htdocs/api/v0.0.2/Candidates.php
@@ -110,24 +110,45 @@ class Candidates extends APIBase
                 $this->safeExit(0);
             }
 
-            $this->verifyField($data, 'Gender', ['Male', 'Female']);
-            $this->verifyField($data, 'EDC', 'YYYY-MM-DD');
-            $this->verifyField($data, 'DoB', 'YYYY-MM-DD');
-            //Candidate::createNew
-            try {
-                $candid = $this->createNew(
-                    $data['Candidate']['DoB'],
-                    $data['Candidate']['EDC'],
-                    $data['Candidate']['Gender'],
-                    $data['Candidate']['PSCID']
-                );
-                $this->header("HTTP/1.1 201 Created");
-                $this->JSON = [
-                               'Meta' => ["CandID" => $candid],
-                              ];
-            } catch(\LorisException $e) {
-                $this->header("HTTP/1.1 400 Bad Request");
+            // This version of the API does not handle candidate creation
+            // when users are at multiple sites
+            $user      = \User::singleton();
+            $centerIDs = $user->getCenterIDs();
+            $num_sites = count($centerIDs);
+            if ($num_sites == 0) {
+                $this->header("HTTP/1.1 401 Unauthorized");
+                $this->error("You are not affiliated with any site");
                 $this->safeExit(0);
+            } else if ($num_sites > 1) {
+                $this->header("HTTP/1.1 501 Not Implemented");
+                $this->error(
+                    "This API version does not support candidate creation " .
+                    "by users with multiple site affiliations. This will be ".
+                    "implemented in a future release."
+                );
+                $this->safeExit(0);
+            } else {
+                $centerID = $centerIDs[0];
+                $this->verifyField($data, 'Gender', ['Male', 'Female']);
+                $this->verifyField($data, 'EDC', 'YYYY-MM-DD');
+                $this->verifyField($data, 'DoB', 'YYYY-MM-DD');
+                //Candidate::createNew
+                try {
+                    $candid = $this->createNew(
+                        $centerID,
+                        $data['Candidate']['DoB'],
+                        $data['Candidate']['EDC'],
+                        $data['Candidate']['Gender'],
+                        $data['Candidate']['PSCID']
+                    );
+                    $this->header("HTTP/1.1 201 Created");
+                    $this->JSON = [
+                                   'Meta' => ["CandID" => $candid],
+                                  ];
+                } catch(\LorisException $e) {
+                    $this->header("HTTP/1.1 400 Bad Request");
+                    $this->safeExit(0);
+                }
             }
         } else {
             $this->header("HTTP/1.1 400 Bad Request");
@@ -174,11 +195,11 @@ class Candidates extends APIBase
      *
      * @return none
      */
-    public function createNew($DoB, $edc, $gender, $PSCID)
+    public function createNew($centerID, $DoB, $edc, $gender, $PSCID)
     {
         $user = \User::singleton();
         return \Candidate::createNew(
-            $user->getCenterID(),
+            $centerID,
             $DoB,
             $edc,
             $gender,
