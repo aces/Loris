bb90d299bc753324c3e6c0c2033e4f29296474e1
xlecours <xavier.lecoursboucher@mcgill.ca>
2017-06-19 11:35:47 -0400

    Login
    moving getEncodedToken in SinglePointLogin
    
    static header function

--- htdocs/api/v0.0.3/index.php
+++ htdocs/api/v0.0.3/index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../../../vendor/autoload.php';
 header('Content-type: application/json');
 ob_start();
 
-$api = new \LORIS\RestAPI\EntryPoint();
+$api = new \LORIS\RestAPI\Entrypoint();
 $api->run();
 
 ob_end_flush();
