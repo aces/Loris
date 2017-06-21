aces/Loris

commit d30f5877a5d2d38bbc7edc5c5d0984804eec43dc

[Security] Improve file validation when downloading files (#2868)
Update get_file.php to use PHP's realpath to fully resolve file
paths before doing validation.

This resolves a security issue identified by @johnsaigle where attackers could escape the path in a way that evades the checks that are in place. (Thanks, @johnsaigle!)

htdocs/mri/jiv/get_file.php
@@ -57,9 +57,11 @@ if ($imagePath === '/' || $DownloadPath === '/' || $mincPath === '/') {
     exit(2);
 }
 
-// Now get the file and do file validation
-$File = $_GET['file'];
+// Now get the file and do file validation.
+// Resolve the filename before doing anything.
+$File = realpath($_GET['file']);
 
+// Extra sanity checks, just in case something went wrong with realpath.
 // File validation
 if (strpos($File, ".") === false) {
     error_log("ERROR: Could not determine file type.");

commit f460ad76a3c046c8e262ec6e8aeb356ce1ae7523

[Security] Amend earlier download fix (#2876)
This restores the broken functionality merged in #2868. This fixes the access control issue while allowing valid files to download.

See also: Redmine 12586.

htdocs/mri/jiv/get_file.php
@@ -19,6 +19,37 @@
  *  @link     https://github.com/aces/Loris-Trunk
  */
 
+/**
+ * Effectively resolve '..' characters in a file path
+ *
+ * @param string $path A potentially-relative filepath to be resolved
+ *
+ * @return string $resolvedPath a path containing no .. sequences
+ */
+function resolvePath($path)
+{
+    $resolvedPath = array();
+    // do some normalization
+    $path        = str_replace('//', '/', $path);
+    $path_pieces = explode('/', $path);
+    foreach ($path_pieces as $piece) {
+        if ($piece == '.') {
+            continue;
+        }
+        if ($piece == '..') {
+            if (!is_array($resolvedPath)) {
+                error_log("ERROR: Resolved path not an array");
+                return "";
+            }
+            array_pop($resolvedPath);
+            continue;
+        }
+        array_push($resolvedPath, $piece);
+    }
+    $resolvedPath = implode('/', $resolvedPath);
+    return $resolvedPath;
+}
+
 
 // Load config file and ensure paths are correct
 set_include_path(
@@ -59,9 +90,9 @@ if ($imagePath === '/' || $DownloadPath === '/' || $mincPath === '/') {
 
 // Now get the file and do file validation.
 // Resolve the filename before doing anything.
-$File = realpath($_GET['file']);
+$File = resolvePath($_GET['file']);
 
-// Extra sanity checks, just in case something went wrong with realpath.
+// Extra sanity checks, just in case something went wrong with path resolution.
 // File validation
 if (strpos($File, ".") === false) {
     error_log("ERROR: Could not determine file type.");