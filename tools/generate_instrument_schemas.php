46e0226f67ca97aab040469f50431312a77c1c35
zainvirani <zainilabideen.virani@mail.mcgill.ca>
2017-07-04 09:50:18 -0400

    added schema generator functionality

--- tools/generate_instrument_schemas.php
+++ tools/generate_instrument_schemas.php
@@ -4,19 +4,19 @@
  */
 
 // require all relevant OO class libraries
-require_once __DIR__ . "/../vendor/autoload.php";
-require_once "../php/libraries/Database.class.inc";
-require_once "../php/libraries/NDB_Config.class.inc";
-require_once "../php/libraries/NDB_BVL_Instrument.class.inc";
+//require_once __DIR__ . "/../vendor/autoload.php";
+//require_once "../php/libraries/Database.class.inc";
+//require_once "../php/libraries/NDB_Config.class.inc";
+//require_once "../php/libraries/NDB_BVL_Instrument.class.inc";
 
-class generate_instrument_schemas
+class schema_generator 
 {
 	/**
 	 * Takes JSON as a string and dumps the data into an array.
      *
-     * @param string $json		A string of JSON elements
+     * param string $json		A string of JSON elements
 	 *
-	 * @return array
+	 * return array
      */
 	function decodeJSON($json)
     {
@@ -27,33 +27,91 @@ class generate_instrument_schemas
 	 * Takes an array of JSON elements and returns corresponding
 	 * SQL schema statements.
 	 *
-	 * @param array $elements	The array of parsed JSON elements
+	 * param array $elements	The array of parsed JSON elements
      *
-     * @return string
+     * return string
 	 */
 	function generateSchema($elements)
 	{
 		//Handle meta info
 		$metaInfo = $elements['Meta'];
-		$tableName = $metaInfo[2];
+		$tableName = $metaInfo['ShortName'];
         
+        $output = '';
+        $output .= "DROP TABLE IF EXISTS `$tableName`;\n";
+        $output .= "CREATE TABLE `$tableName` (\n";
+        $output .= "`CommentID` varchar (255) NOT NULL default '',\n";
+        $output .= "`UserID` varchar(255) default NULL,\n";
+        $output .= "`Examiner` varchar(255) default NULL,\n";
+        $output .= "`Testdate` timestamp DEFAULT CURRENT_TIMESTAMP ON ";
+        $output .= "UPDATE CURRENT_TIMESTAMP,\n";
+        $output .= "`Data_entry_completion_status` enum('Incomplete','Complete') ";
+        $output .= "NOT NULL default 'Incomplete',\n";
+
 		//Handle individual elements
 		foreach($elements['Elements'] AS $element) {
-		    switch($element['Type']) {
+            $switchType = htmlspecialchars($element['Type']);
+			switch($switchType) {
                 case "label":
-                break;
-                case "radio-label":
-                break;
-                case "radio":
-
-                case "text":
-
-                case "checkbox":
-
-                case "
-
-                case "
+                    continue 2;
+                    break;
+                case "radio-labels":
+                    continue 2;
+                    break;
             }
+            $elName = htmlspecialchars($element['Name']);
+            switch($switchType) {
+                case "radio":
+                    $enumOpts = $element['Options']['Values']['en-ca'];
+                    $allowMult = $element['Options']['AllowMultiple'];
+                    if(!$allowMult) {
+                        $type = $this->enumizeOptions($enumOpts);
+                    } else {
+                        $type = "varchar(255)";
+                    }
+                    break;
+                case "select":
+                    $enumOpts = $element['Options']['Values']['en-ca'];
+					$allowMult = $element['Options']['AllowMultiple'];
+                    if(!$allowMult) {                    
+                        $type = $this->enumizeOptions($enumOpts);
+                    } else {
+                        $type = "varchar(255)";
+                    }
+                    break;
+                case "text":
+                    $type = "varchar(255)";
+                    break;
+                case "checkbox":
+                    $type = "varchar(255)";
+                    break;
+                case "calc":
+                    $type = "varchar(255)";
+                    break;
+                default:
+                    echo("unhandled type: $switchType\n");
+                    continue 2;
+                    break;
+            }
+            $type = htmlspecialchars($type);
+            $output .= "`$elName` $type default NULL,\n";
 		}
+
+        $output .= "PRIMARY KEY (`CommentID`))";
+        return $output;
 	}
+    
+    /*
+     * Takes an array of values to be enumerized as options for a field
+     *
+     * param array $values     The array of values
+     *
+     * return string
+     */
+    function enumizeOptions($values)
+    {
+        $enum = implode(",",array_keys($values));
+        return "enum($enum)";
+    }
+}
 ?>
