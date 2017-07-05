78cf1de22e8b1a1cea3213c221d7067507e5adda
llevitis <llevitis@cbrain.mcgill.ca>
2017-07-05 13:35:06 -0400

    in progress, creating firstincomplete candidates table

--- modules/quality_control/jsx/qualityControlIndex.js
+++ modules/quality_control/jsx/qualityControlIndex.js
@@ -5,33 +5,30 @@ class QualityControlIndex extends React.Component {
 
   constructor(props) {
     super(props);
-
-
-    this.state = {
-      isLoaded: false,
-      filter: {}
-    };
-
-    // Bind component instance to custom methods
-    this.fetchData = this.fetchData.bind(this);
-    this.updateFilter = this.updateFilter.bind(this);
   }
 
   render() {
 
-    let uploadTab;
     let tabList = [
         {id: "behavioral", label: "Behavioral"},
         {id: "imaging", label: "Imaging"}
     ];
+
+    let tabBehaviouralList = [
+        {id: "incomplete_forms", label: "Incomplete Forms"},
+        {id: "data_conflicts", label: "Data Conflicts"},
+        {id: "behavioural_feedback", label: "Behavioural Feedback"}
+    ];
+
+
 
     return (
       <Tabs tabs={tabList} defaultTab="behavioral" updateURL={true}>
         <TabPane TabId={tabList[0].id}>
           <FilterForm
             Module="quality_control"
-            name="quality_control_behavioral_filter"
-            id="quality_control_behavioral_filter"
+            name="quality_control_behavioural"
+            id="quality_control_behavioural_filter"
             columns={2}
             formElements={this.state.Data.form}
             onUpdate={this.updateFilter}
@@ -40,6 +37,11 @@ class QualityControlIndex extends React.Component {
             <br/>
             <ButtonElement type="reset" label="Clear Filters" />
           </FilterForm>
+          <StaticDataTable
+            Data={this.state.Data.Data}
+            Headers={this.state.Data.Headers}
+            Filter={this.state.filter}
+          />
         </TabPane>
         <TabPane TabId={tabList[1].id}>
         </TabPane>
@@ -51,10 +53,9 @@ class QualityControlIndex extends React.Component {
 $(function() {
   const qualityControlIndex = (
     <div className="page-qualityControl">
-      <QualityControlIndex UselessURL={`${loris.BaseURL}/quality_control`}/>
+      <QualityControlIndex UselessURL={`${loris.BaseURL}/quality_control/?format=json`}/>
     </div>
   );
 
   ReactDOM.render(qualityControlIndex, document.getElementById("lorisworkspace"));
 });
-
