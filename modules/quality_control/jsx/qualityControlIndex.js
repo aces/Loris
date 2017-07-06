a6204c1cc3d707ba6a8c7e12218d7beab3ead941
Leo Thomas <leo.thomas@mail.mcgill.ca>
2017-07-06 17:46:43 -0400

    nothing works, my life is bleak. Existence is pain

--- modules/quality_control/jsx/qualityControlIndex.js
+++ modules/quality_control/jsx/qualityControlIndex.js
@@ -9,28 +9,26 @@ class QualityControlIndex extends React.Component {
 
   this.state = {
     isloaded: false,
-    //filter: {}
+    filter: {}
   };
 
   this.fetchData = this.fetchData.bind(this);
-  //this.updateFilter= this.updateFilter.bind(this);
+  this.updateFilter= this.updateFilter.bind(this);
   }
   componentDidMount(){
     this.fetchData();
   }
   fetchData(){
-    $.ajax(this.props.ImgDataURL, {
+    $.ajax(this.props.DataURL, {
       method:"GET",
       dataType: "json",
       success: function(data){
+        console.log(data);
         this.setState({
           Data:data,
           isLoaded:true
         });
       }.bind(this),
-      error: function(error){
-        console.error(error);
-      }
     });
   }
   
@@ -52,7 +50,7 @@ class QualityControlIndex extends React.Component {
     }
 
     let uploadTab;
-    let tabBehaviouralList = [
+    let tabList = [
         {id: "incomplete_forms", label: "Incomplete Forms"},
         {id: "data_conflicts", label: "Data Conflicts"},
         {id: "behavioural_feedback", label: "Behavioural Feedback"},
@@ -62,36 +60,37 @@ class QualityControlIndex extends React.Component {
 
 
     return (
-      <Tabs tabs={tabList} defaultTab="behavioral" updateURL={true}>
-        <TabPane TabId={tabList[0].id}>
-          <FilterForm
+      <div>
+      <FilterForm
             Module="quality_control"
             name="quality_control_behavioural"
             id="quality_control_behavioural_filter"
             columns={2}
             formElements={this.state.Data.form}
             onUpdate={this.updateFilter}
-            filter={this.state.filter}
-          >
+            filter={this.state.filter}>
             <br/>
             <ButtonElement type="reset" label="Clear Filters" />
-          </FilterForm>
+       </FilterForm>
+      <Tabs tabs={tabList} defaultTab="behavioral" updateURL={true}>
+        <TabPane TabId={tabList[0].id}>
           <StaticDataTable
-            Data={this.state.Data.Data}
-            Headers={this.state.Data.Headers}
+            Data={this.state.Data.behaviouralData}
+            Headers={this.state.Data.behaviouralHeaders}
             Filter={this.state.filter}
           />
         </TabPane>
-        <TabPane TabId={tabList[1].id}
-        DataUrl = {`{loris.BaseURL}/quality_control/ajax/imgQualityControl.php`}>
+        <TabPane TabId={tabList[1].id}>
         <StaticDataTable
-        Data={this.state.Data.data}
-        Headers={this.state.Data.headers}
+        Data={this.state.Data.imgData}
+        Headers={this.state.Data.imgHeaders}
+        Filter={this.state.filter}
         getFormattedCell={formatColumn}
         freezeColumn="PatientName"
         />
         </TabPane>
       </Tabs>
+      </div>
     );
   }
 }
@@ -99,8 +98,7 @@ class QualityControlIndex extends React.Component {
 $(function() {
   const qualityControlIndex = (
     <div className="page-qualityControl">
-      <QualityControlIndex ImgDataURL={`${loris.BaseURL}/quality_control/ajax/imgQualityControl.php`}/>
-      <QualityControlIndex DataURL={`${loris.BaseURL}/quality_control/?format=json`}/>
+      <QualityControlIndex DataURL={`${loris.BaseURL}/quality_control/ajax/qualityControl.php`}/>
     </div>
   );
 
