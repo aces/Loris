416ec1e60c80b19cbe571fad0b166136d34b2acd
Rida CCNA <ridz_1208@hotmail.com>
2017-07-20 23:30:51 -0400

    removed default

--- SQL/Archive/17.1/2017-07-30-Notifications_for_bvl_feedback.sql
+++ SQL/Archive/17.1/2017-07-30-Notifications_for_bvl_feedback.sql
@@ -4,11 +4,12 @@ INSERT INTO notification_modules (module_name, operation_type, as_admin, templat
   ('bvl_feedback', 'close', 'N', 'notifier_bvl_feedback_close.tpl', 'Behavioural Feedback: Feedback Closed'),
   ('bvl_feedback', 'comment', 'N', 'notifier_bvl_feedback_Comment.tpl', 'Behavioural Feedback: New Comment');
 
-INSERT INTO notification_modules_services_rel
-SELECT nm.id, ns.id
-    FROM notification_modules nm
-        JOIN notification_services ns
-    WHERE nm.module_name='bvl_feedback' AND ns.service='email_text';
+-- Command adding it to default loris notification-ready modules. Otherwise, project decision
+-- INSERT INTO notification_modules_services_rel
+-- SELECT nm.id, ns.id
+--     FROM notification_modules nm
+--         JOIN notification_services ns
+--     WHERE nm.module_name='bvl_feedback' AND ns.service='email_text';
 
 INSERT INTO notification_modules_perm_rel
 SELECT nm.id, p.permID
