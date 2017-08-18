b7f6e2f2cd62a6b53fe2d47c16271165d2a1217d
Jacob Penny <jacob.penny@gmail.com>
2017-08-18 17:00:00 -0400

    Fix bad merge

--- jsx/lib/localize-instrument.js
+++ jsx/lib/localize-instrument.js
@@ -7,12 +7,23 @@ function localizeInstrument(rawInstrument, lang = 'en-ca') {
     const convertedElements = [];
 
     instrument['Elements'].forEach((element) => {
-      if (['label', 'text', 'calc'].includes(element.Type)  && element['Description'][lang]) {
-        element['Description'] = element['Description'][lang];
-        convertedElements.push(element);
-      } else if (['select', 'radio', 'checkbox'].includes(element.Type) && element['Description'][lang]) {
-        element['Description'] = element['Description'][lang];
-        element['Options']['Values'] = element['Options']['Values'][lang];
+      if (['label', 'text', 'calc', 'date', 'select', 'radio', 'checkbox'].includes(element.Type)) {
+        if (element['Description'][lang]) {
+          element['Description'] = element['Description'][lang];
+        } else {
+          if (['text', 'date'].includes(element.Type)) {
+            element['Description'] = element.fieldNote ? element.fieldNote : "Enter a value: ";
+          } else if (['select', 'radio', 'checkbox'].includes(element.Type)) {
+            element['Description'] = "Choose a value: ";
+          } else if (['label'].includes(element.Type)) {
+            element['Description'] = "";
+          } else if (['calc'].includes(element.Type)) {
+            element['Description'] = "Result: ";
+          }
+        }
+        if (['select', 'radio', 'checkbox'].includes(element.Type)) {
+          element['Options']['Values'] = element['Options']['Values'][lang];
+        }
         convertedElements.push(element);
       } else if (['radio-labels'].includes(element.Type) && element['Labels'][lang]) {
         element['Labels'] = element['Labels'][lang];
