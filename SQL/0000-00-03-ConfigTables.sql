aces/Loris

commit 9f5ee4e4e26d2aa9df079ebf5fc2cc7e2d6e8e29

[Imaging Browser] Scan type columns now automatically populated: Redmine 11818 (#2587)
This makes the following changes to the imaging browser menu:
- QC Status is now becomes Visit QC Status
- Column names are now T2 QC Status as opposed to T2 Passed
- "Links" column moved to an earlier column in the table
- Most importantly, the columns for the modalities are populated based on a project configuration (example entries in the screenshot below), & T1 and T2 are no longer hard coded at 44, 45.

SQL/0000-00-03-ConfigTables.sql
@@ -17,7 +17,7 @@ CREATE TABLE `ConfigSettings` (
     `Description` varchar(255) DEFAULT NULL,
     `Visible` tinyint(1) DEFAULT '0',
     `AllowMultiple` tinyint(1) DEFAULT '0',
-    `DataType` ENUM('text', 'boolean', 'email', 'instrument', 'textarea') DEFAULT NULL,
+    `DataType` ENUM('text', 'boolean', 'email', 'instrument', 'textarea', 'scan_type') DEFAULT NULL,
     `Parent` int(11) DEFAULT NULL,
     `Label` varchar(255) DEFAULT NULL,
     `OrderNumber` int(11) DEFAULT NULL,
@@ -101,13 +101,13 @@ INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, Label, Or
 INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber) SELECT 'projectDescription', 'Description of the study displayed in main dashboard panel', 1, 0, 'textarea', ID, 'Project Description', 1 FROM ConfigSettings WHERE Name="dashboard";
 INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber) SELECT 'recruitmentTarget', 'Target number of participants for the study', 1, 0, 'text', ID, 'Target number of participants', 2 FROM ConfigSettings WHERE Name="dashboard";
 
-
-INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, Label, OrderNumber) VALUES ('dicom_archive', 'DICOM Archive settings', 1, 0, 'DICOM Archive', 6);
-INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber) SELECT 'patientIDRegex', 'Regex for masking the Patient ID header field', 1, 0, 'text', ID, 'Patient ID regex', 1 FROM ConfigSettings WHERE Name="dicom_archive";
-INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber) SELECT 'patientNameRegex', 'Regex for masking the Patient Name header field', 1, 0, 'text', ID, 'Patient name regex', 2 FROM ConfigSettings WHERE Name="dicom_archive";
-INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber) SELECT 'LegoPhantomRegex', 'Regex for identifying a Lego Phantom scan header', 1, 0, 'text', ID, 'Lego phantom regex', 3 FROM ConfigSettings WHERE Name="dicom_archive";
-INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber) SELECT 'LivingPhantomRegex', 'Regex to be used on Living Phantom scan header', 1, 0, 'text', ID, 'Living phantom regex', 4 FROM ConfigSettings WHERE Name="dicom_archive";
-INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber) SELECT 'showTransferStatus', 'Show transfer status in the DICOM Archive table', 1, 0, 'boolean', ID, 'Show transfer status', 5 FROM ConfigSettings WHERE Name="dicom_archive";
+INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, Label, OrderNumber) VALUES ('imaging_modules', 'DICOM Archive and Imaging Browser settings', 1, 0, 'Imaging Modules', 6);
+INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber) SELECT 'patientIDRegex', 'Regex for masking the Patient ID header field', 1, 0, 'text', ID, 'Patient ID regex', 1 FROM ConfigSettings WHERE Name="imaging_modules";
+INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber) SELECT 'patientNameRegex', 'Regex for masking the Patient Name header field', 1, 0, 'text', ID, 'Patient name regex', 2 FROM ConfigSettings WHERE Name="imaging_modules";
+INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber) SELECT 'LegoPhantomRegex', 'Regex for identifying a Lego Phantom scan header', 1, 0, 'text', ID, 'Lego phantom regex', 3 FROM ConfigSettings WHERE Name="imaging_modules";
+INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber) SELECT 'LivingPhantomRegex', 'Regex to be used on Living Phantom scan header', 1, 0, 'text', ID, 'Living phantom regex', 4 FROM ConfigSettings WHERE Name="imaging_modules";
+INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber) SELECT 'showTransferStatus', 'Show transfer status in the DICOM Archive table', 1, 0, 'boolean', ID, 'Show transfer status', 5 FROM ConfigSettings WHERE Name="imaging_modules";
+INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber) SELECT 'tblScanTypes', 'Scan types from the mri_scan_type table that the project wants to see displayed in Imaging Browser table', 1, 0, 'text', ID, 'Imaging Browser Tabulated Scan Types', 6 FROM ConfigSettings WHERE Name="imaging_modules";
 
 
 INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, Label, OrderNumber) VALUES ('statistics', 'Statistics module settings', 1, 0, 'Statistics', 7);
@@ -182,12 +182,13 @@ INSERT INTO Config (ConfigID, Value) SELECT ID, "http://localhost/" FROM ConfigS
 
 INSERT INTO Config (ConfigID, Value) SELECT ID, "This database provides an on-line mechanism to store both imaging and behavioral data collected from various locations. Within this framework, there are several tools that will make this process as efficient and simple as possible. For more detailed information regarding any aspect of the database, please click on the Help icon at the top right. Otherwise, feel free to contact us at the DCC. We strive to make data collection almost fun." FROM ConfigSettings WHERE Name="projectDescription";
 
-
 INSERT INTO Config (ConfigID, Value) SELECT ID, "/./" FROM ConfigSettings WHERE Name="patientIDRegex";
 INSERT INTO Config (ConfigID, Value) SELECT ID, "/./i" FROM ConfigSettings WHERE Name="patientNameRegex";
 INSERT INTO Config (ConfigID, Value) SELECT ID, "/./i" FROM ConfigSettings WHERE Name="LegoPhantomRegex";
 INSERT INTO Config (ConfigID, Value) SELECT ID, "/./i" FROM ConfigSettings WHERE Name="LivingPhantomRegex";
 INSERT INTO Config (ConfigID, Value) SELECT ID, "false" FROM ConfigSettings WHERE Name="showTransferStatus";
+INSERT INTO Config (ConfigID, Value) SELECT cs.ID, GROUP_CONCAT(mst.Scan_Type) FROM ConfigSettings cs JOIN mri_scan_type mst WHERE cs.Name="tblScanTypes" AND mst.ID=44;
+INSERT INTO Config (ConfigID, Value) SELECT cs.ID, GROUP_CONCAT(mst.Scan_Type) FROM ConfigSettings cs JOIN mri_scan_type mst WHERE cs.Name="tblScanTypes" AND mst.ID=45;
 
 
 INSERT INTO Config (ConfigID, Value) SELECT ID, "radiology_review" FROM ConfigSettings WHERE Name="excludedMeasures";

SQL/Archive/17.1/2017-03-07_AddImagingBrowserToConfig.sql
@@ -1,0 +1,14 @@
+-- Add scan_type to ENUM
+ALTER TABLE ConfigSettings MODIFY COLUMN DataType ENUM('text', 'boolean', 'email', 'instrument', 'textarea', 'scan_type');
+
+-- Change Dicom Archive name to Imaging Modules
+UPDATE ConfigSettings SET Name='imaging_modules', Description='DICOM Archive and Imaging Browser settings', Label='Imaging Modules' WHERE Name ='dicom_archive';
+
+-- Add Imaging Browser to Imaging Modules
+INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber) SELECT 'tblScanTypes', 'Scan types from the mri_scan_type table that the project wants to see displayed in Imaging Browser table', 1, 1, 'scan_type', ID, 'Imaging Browser Tabulated Scan Types', 6 FROM ConfigSettings WHERE Name="imaging_modules";
+
+-- default imaging_browser settings
+INSERT INTO Config (ConfigID, Value) SELECT cs.ID, GROUP_CONCAT(mst.Scan_Type) FROM ConfigSettings cs JOIN mri_scan_type mst WHERE cs.Name="tblScanTypes" AND mst.ID=44;
+INSERT INTO Config (ConfigID, Value) SELECT cs.ID, GROUP_CONCAT(mst.Scan_Type) FROM ConfigSettings cs JOIN mri_scan_type mst WHERE cs.Name="tblScanTypes" AND mst.ID=45;
+
+

modules/configuration/php/NDB_Form_configuration.class.inc
@@ -45,6 +45,18 @@ class NDB_Form_Configuration extends NDB_Form
     function configuration()
     {
         $config =& NDB_Config::singleton();
+        $DB     =& Database::singleton();
+
+        $scans = $DB->pselect(
+            "SELECT Scan_type
+             FROM mri_scan_type",
+            array()
+        );
+        foreach ($scans as $type) {
+            $val = $type['Scan_type'];
+            $scan_types[$val] =$val;
+        }
+        $scan_types = array_merge(array('' => ''), $scan_types);
 
         $instruments = Utility::getAllInstruments();
         $instruments = array_merge(array('' => ''), $instruments);
@@ -54,6 +66,7 @@ class NDB_Form_Configuration extends NDB_Form
         $this->tpl_data['config']          = $this->_getConfigSettingTree();
         $this->tpl_data['instruments']     = $instruments;
         $this->tpl_data['sandbox']         = $config->getSetting("sandbox");
+        $this->tpl_data['scan_types']      = $scan_types;
 
     }
 

modules/configuration/templates/form_configuration.tpl
@@ -24,6 +24,14 @@
     </select>
 {/function}
 
+{function name=createScanType}
+    <select class="form-control" name="{$k}" {if $d eq "Yes"}disabled{/if}>
+        {foreach from=$scan_types key=name item=label}
+            <option {if $v eq $name}selected{/if} value="{$name}">{$label}</option>
+        {/foreach}
+    </select>
+{/function}
+
 {function name=createEmail}
     <input class="form-control" type="email" name="{$k}" value="{$v}" {if $d eq "Yes"}disabled{/if}>
 {/function}
@@ -66,6 +74,8 @@
             {call createRadio k=$k v=$v d=$node['Disabled']}
         {elseif $node['DataType'] eq 'instrument'}
             {call createInstrument k=$k v=$v d=$node['Disabled']}
+        {elseif $node['DataType'] eq 'scan_type'}
+            {call createScanType k=$k v=$v d=$node['Disabled']}
         {elseif $node['DataType'] eq 'email'}
             {call createEmail k=$k v=$v d=$node['Disabled']}
         {elseif $node['DataType'] eq 'textarea'}
@@ -86,6 +96,8 @@
         {assign var=id value={"add-"|cat:$node['ID']} }
         {if $node['DataType'] eq 'instrument'}
             {call createInstrument k=$id d=$node['Disabled']}
+        {elseif $node['DataType'] eq 'scan_type'}
+            {call createScanType k=$id d=$node['Disabled']}
         {elseif $node['DataType'] eq 'email'}
             {call createEmail k=$id d=$node['Disabled']}
         {elseif $node['DataType'] eq 'textarea'}

modules/configuration/test/ConfigurationTest.php
@@ -148,7 +148,7 @@ class ConfigurationTest extends LorisIntegrationTest
         $this->_linkTest("GUI");
         $this->_linkTest("WWW");
         $this->_linkTest("Dashboard");
-        $this->_linkTest("DICOM Archive");
+        $this->_linkTest("Imaging Modules");
         $this->_linkTest("Statistics");
         $this->_linkTest("Email");
         $this->_linkTest("Uploads");

modules/dicom_archive/php/dicom_archive.class.inc
@@ -146,7 +146,7 @@ class Dicom_Archive extends \NDB_Menu_Filter
     function _setDicomArchiveSettings()
     {
         $config =& \NDB_Config::singleton();
-        $this->dicomArchiveSettings = $config->getSetting('dicom_archive');
+        $this->dicomArchiveSettings = $config->getSetting('imaging_modules');
     }
 
     /**

modules/dicom_archive/php/viewdetails.class.inc
@@ -140,7 +140,7 @@ class ViewDetails extends \NDB_Form
     function _validateNamesIds()
     {
         $config =& \NDB_Config::singleton();
-        $dicomArchiveSettings = $config->getSetting('dicom_archive');
+        $dicomArchiveSettings = $config->getSetting('imaging_modules');
 
         if ((preg_match(
             $dicomArchiveSettings['patientNameRegex'],

modules/imaging_browser/php/NDB_Menu_Filter_imaging_browser.class.inc
@@ -60,24 +60,49 @@ class NDB_Menu_Filter_Imaging_Browser extends NDB_Menu_Filter
      */
     function _setupVariables()
     {
+        $config = NDB_Config::singleton();
+        $DB     = Database::singleton();
+        $toTable_scan_types = $config->getSetting('tblScanTypes');
+
+        $all_scan_types = $DB->pselect(
+            "SELECT ID, Scan_type FROM mri_scan_type mri",
+            array()
+        );
+        foreach ($all_scan_types as $row) {
+            $type = $row['Scan_type'];
+            $all_scan_types[$row['ID']] = $type;
+        }
+
+        // Get the intersection between all the scan types and those
+        // that are desired to go into imaging browser table, based on
+        // array values rather than keys (hence the array_flip), then flip
+        // the resulting array back to revert it to a key/value (i.e.
+        // acquisition protocol ID/scan type) combination.
+        $scan_id_types =array_flip(
+            array_intersect_key(
+                array_flip($all_scan_types),
+                array_flip($toTable_scan_types)
+            )
+        );
+
         $NewDataSubquery = "CASE 
             COALESCE(nd.QCLastChangeTime, 'new')
             WHEN 'new' THEN 'new' 
             WHEN '' THEN 'new'
             ELSE ''
             END";
-        $T1PassSubquery  = "CASE 
-            COALESCE(t1pass.T1QC, '') 
-            WHEN '' THEN ''
-            WHEN 1 THEN 'Passed'
-            WHEN 2 THEN 'Failed'
-            END";
-        $T2PassSubquery  = "CASE 
-            COALESCE(t2pass.T2QC, '') 
-            WHEN '' THEN ''
-            WHEN 1 THEN 'Passed'
-            WHEN 2 THEN 'Failed'
-            END";
+
+        foreach ($scan_id_types as $id => $type) {
+            $pass[$id]          = $type . 'pass';
+            $qc[$id]            = $type . 'QC';
+            $coalesce_desc[$id] = $pass[$id] . '.' . $qc[$id];
+            $case_desc[$id]     = "CASE
+                COALESCE($coalesce_desc[$id], '')
+                WHEN '' THEN ''
+                WHEN 1 THEN 'Passed'
+                WHEN 2 THEN 'Failed'
+                END";
+        }
 
         $PendingFailSubquery = "
             CASE s.MRIQCStatus
@@ -100,24 +125,30 @@ class NDB_Menu_Filter_Imaging_Browser extends NDB_Menu_Filter
             LEFT JOIN files_qcstatus USING (FileID)
             WHERE OutputType='native' AND AcquisitionProtocolID 
             NOT IN (1, 2, 3, 52) GROUP BY files.SessionID) nd 
-            ON (nd.SessionID=f.SessionID)
-            LEFT JOIN (SELECT files.SessionID,
-            MIN(files_qcstatus.QCStatus+0) as T1QC FROM files 
-            JOIN files_qcstatus USING (FileID)
-            WHERE files.AcquisitionProtocolID=44 
-            AND files_qcstatus.QCStatus IN (1, 2)
-            GROUP BY files.SessionID) t1pass ON (t1pass.SessionID=f.SessionID)
+            ON (nd.SessionID=f.SessionID) ";
+
+        $left_joins = "";
+        foreach ($case_desc as $key => $value) {
+            $left_joins .= "
             LEFT JOIN (SELECT files.SessionID, 
-            MIN(files_qcstatus.QCStatus+0) as T2QC FROM files 
-            JOIN files_qcstatus USING (FileID)
-            WHERE files.AcquisitionProtocolID=45 
-            AND files_qcstatus.QCStatus IN (1, 2)
-            GROUP BY files.SessionID) t2pass ON (t2pass.SessionID=f.SessionID)
+            MIN(files_qcstatus.QCStatus+0) as " . $DB->quote($qc[$key]) . " 
+            FROM files 
+            JOIN files_qcstatus USING (FileID) 
+            WHERE files.AcquisitionProtocolID=" . $DB->quote($key) . " 
+            AND files_qcstatus.QCStatus IN (1, 2) 
+            GROUP BY files.SessionID) `" . $pass[$key] . "` 
+            ON (`" . $pass[$key] . "`.SessionID=f.SessionID) ";
+        }
+
+        $where = "
             WHERE 
             s.Active = 'Y' AND
             f.PendingStaging=0 AND 
             f.FileType='mnc' AND 
             f.AcquisitionProtocolID not in (1, 2, 3, 52)";
+
+        $this->query .= $left_joins;
+        $this->query .= $where;
 
         $config =& NDB_Config::singleton();
         $user   =& User::singleton();
@@ -127,39 +158,60 @@ class NDB_Menu_Filter_Imaging_Browser extends NDB_Menu_Filter
             $this->query .= " AND c.CenterID IN (" . $site_arr . ")";
         }
 
+        // Initialize the arrays that go into this->columns and
+        // $this->headers so that array_merge works even if no
+        // $scan_types are set in the configuration module
+        $modalities_subquery_as = array();
+        $as = array();
+
+        foreach ($case_desc as $key => $value) {
+            $modalities_subquery[$key] = $value;
+            $as[$key] = $scan_id_types[$key] . '_QC_Status';
+            $modalities_subquery_as[$key] = $modalities_subquery[$key] .
+                                            ' as ' .
+                                            $as[$key];
+        }
+
         $this->columns = array(
                           'p.MRI_alias as Site',
                           'c.PSCID as PSCID',
                           'c.CandID as DCCID',
                           's.visit_label as Visit_Label',
-                          "$PendingFailSubquery as QC_Status",
-                          'MIN(md.AcquisitionDate) as First_Acq_Date',
-                          'FROM_UNIXTIME(MIN(f.InsertTime)) as First_Insertion_Date',
+                          "$PendingFailSubquery as Visit_QC_Status",
+                          'MIN(md.AcquisitionDate) as First_Acquisition',
+                          'FROM_UNIXTIME(MIN(f.InsertTime)) as First_Insertion',
                           'FROM_UNIXTIME(MAX(fqc.QCLastChangeTime)) as Last_QC',
                           "$NewDataSubquery as New_Data",
-                          "$T1PassSubquery as T1_Passed",
-                          "$T2PassSubquery as T2_Passed",
-                          "GROUP_CONCAT(DISTINCT OutputType) as Links",
-                          's.ID as sessionID',
                          );
+        $this->columns = array_merge(
+            $this->columns,
+            array(
+             "GROUP_CONCAT(DISTINCT OutputType) as Links",
+             's.ID as sessionID',
+            ),
+            $modalities_subquery_as
+        );
 
         $this->order_by = 'c.PSCID, s.Visit_label';
         $this->group_by = 's.ID';
-        $this->headers  = array(
-                           'Site',
-                           'PSCID',
-                           'DCCID',
-                           'Visit_Label',
-                           'QC_Status',
-                           'First_Acq_Date',
-                           'First_Insertion_Date',
-                           'Last_QC',
-                           'New_Data',
-                           'T1_Passed',
-                           'T2_Passed',
-                           'Links',
-                           'sessionID',
-                          );
+
+        $this->headers = array(
+                          'Site',
+                          'PSCID',
+                          'DCCID',
+                          'Visit_Label',
+                          'Visit_QC_Status',
+                          'First_Acquisition',
+                          'First_Insertion',
+                          'Last_QC',
+                          'New_Data',
+                          'Links',
+                         );
+        $this->headers = array_merge(
+            $this->headers,
+            $as,
+            'sessionID'
+        );
 
         // Insert project column after DCCID, if useProject config is enabled
         if ($config->getSetting('useProjects') === "true") {
@@ -240,7 +292,8 @@ class NDB_Menu_Filter_Imaging_Browser extends NDB_Menu_Filter
         }
 
         $DB    = Database::singleton();
-        $allAr = array('' => 'All');
+        $allAr = array('' => All);
+
         $this->addBasicText(
             'pscid',
             'PSCID',
@@ -279,7 +332,7 @@ class NDB_Menu_Filter_Imaging_Browser extends NDB_Menu_Filter
         $this->addSelect('SiteID', 'Site', $list_of_sites);
         $this->addSelect(
             'VisitQCStatus',
-            'QC Status',
+            'Visit QC Status',
             array(
              ''     => 'All',
              'Pass' => 'Pass',

commit 6a156eda098d09a6a7b09f418a40222add946c63

Imaging Browser: Fix DataType from the configuration module for new installs in the scan types: Redmine 13122 (#3085)
Fixes bug introduced in #2587 with misnamed config variable.

SQL/0000-00-03-ConfigTables.sql
@@ -107,7 +107,7 @@ INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType,
 INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber) SELECT 'LegoPhantomRegex', 'Regex for identifying a Lego Phantom scan header', 1, 0, 'text', ID, 'Lego phantom regex', 3 FROM ConfigSettings WHERE Name="imaging_modules";
 INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber) SELECT 'LivingPhantomRegex', 'Regex to be used on Living Phantom scan header', 1, 0, 'text', ID, 'Living phantom regex', 4 FROM ConfigSettings WHERE Name="imaging_modules";
 INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber) SELECT 'showTransferStatus', 'Show transfer status in the DICOM Archive table', 1, 0, 'boolean', ID, 'Show transfer status', 5 FROM ConfigSettings WHERE Name="imaging_modules";
-INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber) SELECT 'tblScanTypes', 'Scan types from the mri_scan_type table that the project wants to see displayed in Imaging Browser table', 1, 0, 'text', ID, 'Imaging Browser Tabulated Scan Types', 6 FROM ConfigSettings WHERE Name="imaging_modules";
+INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber) SELECT 'tblScanTypes', 'Scan types from the mri_scan_type table that the project wants to see displayed in Imaging Browser table', 1, 1, 'scan_type', ID, 'Imaging Browser Tabulated Scan Types', 6 FROM ConfigSettings WHERE Name="imaging_modules";
 
 
 INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, Label, OrderNumber) VALUES ('statistics', 'Statistics module settings', 1, 0, 'Statistics', 7);