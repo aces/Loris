dbeecc9880461b4f60c12ec8dcc6c24e19672de3
llevitis <llevitis@cbrain.mcgill.ca>
2017-08-28 11:46:17 -0400

    user preferences help text

---
+++ SQL/Archive/17.1/2017-08-28_my_preference_help_text.sql
@@ -0,0 +1,2 @@
+UPDATE help set content = 'User Preferences allows users to modify their first name, last name, email address as they appear in the database, and current password as well as Document Repository and notification preferences. All changes made to the user’s preferences must be saved by clicking "Save" after completion. Information can be reset using the "Reset" button.'
+WHERE topic = 'My Preferences';
