26f19bc4c15eface2e3a27fc9717dba5d7bac2da
llevitis <llevitis@cbrain.mcgill.ca>
2017-08-28 12:14:44 -0400

    updated the date in the patch

--- SQL/Archive/17.1/2017-08-28_my_preference_help_text.sql
+++ SQL/Archive/17.1/2017-08-28_my_preference_help_text.sql
@@ -1,2 +1,3 @@
-UPDATE help set content = 'User Preferences allows users to modify their first name, last name, email address as they appear in the database, and current password as well as Document Repository and notification preferences. All changes made to the user’s preferences must be saved by clicking "Save" after completion. Information can be reset using the "Reset" button.'
+UPDATE help
+  SET content = 'User Preferences allows users to modify their first name, last name, email address as they appear in the database, and current password as well as Document Repository and notification preferences. All changes made to the user’s preferences must be saved by clicking "Save" after completion. Information can be reset using the "Reset" button.', created = '2017-08-28 00:00:00'
 WHERE topic = 'My Preferences';
