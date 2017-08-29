755873de2d2afe5177b3fd875dddec604de51152
llevitis <llevitis@cbrain.mcgill.ca>
2017-08-29 15:03:39 -0400

    user accounts help text

---
+++ SQL/Archive/17.1/2017_08_29_user_accounts_help_text.sql
@@ -0,0 +1,3 @@
+UPDATE help
+  SET content = 'The User Accounts module allows an administrator to create accounts and set Roles and Permissions for database users.\r\n\r\nUse the Selection Filter, to search for specific users.  Filter or search for Site, Active user accounts, accounts pending approval, Username, Full Name, or Email. After entering filter criteria, click “Show Data” to display results in the data table below.  Click “Clear Form” to reset all filter.\r\n\r\nClick on any Username in the table to view or edit user account information, reset password and alter the user’s database privileges.\r\n\r\n To add a new user, click the "Add User" button in the Selection Filter panel.', created = '2017-08-29 00:00:00'
+WHERE topic = 'User Accounts';
