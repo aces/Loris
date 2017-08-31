272da7b001bddc40f4855ca034879a9a628f2f7b
Rida CCNA <ridz_1208@hotmail.com>
2017-08-31 16:19:16 -0400

    Change target directory of the output script to be run as first patch of the release

--- tools/DB_date_zeros_removal.php
+++ tools/DB_date_zeros_removal.php
@@ -27,7 +27,7 @@ $base = $config->getSetting('base');
 $db->_trackChanges = false;
 
 // Set up variables
-$filename = __DIR__ . "/../project/tables_sql/update_zero_fields_statements.sql";
+$filename = __DIR__ . "/../SQL/Archive/17.1/2016-06-01-update_zero_fields_statements.sql";
 $output= "";
 $alters="";
 $updates="";
