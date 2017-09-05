1651b850e0d114c52921d86a7553469bcc17bd1a
kongtiaowang <kongtiaowangshen@gmail.com>
2017-09-05 15:42:54 -0400

    create timpoint

--- modules/create_timepoint/test/create_timepointTest.php
+++ modules/create_timepoint/test/create_timepointTest.php
@@ -33,8 +33,14 @@ class CreateTimepointTestIntegrationTest extends LorisIntegrationTestWithCandida
      */
     function setUp()
     {
-        $this->login('admin','testpassword');
         parent::setUp();
+        $this->safeGet($this->url."/main.php?logout=true");
+        $usernameEl = $this->webDriver->findElement(WebDriverBy::Name("username"));
+        $passwordEl = $this->webDriver->findElement(WebDriverBy::Name("password"));
+        $usernameEl->sendKeys('admin');
+        $passwordEl->sendKeys('testpassword');
+        $login = $this->webDriver->findElement(WebDriverBy::Name("login"));
+        $login->click();
 
     }
 
