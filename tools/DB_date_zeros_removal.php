8161727ddddea0248e32cab3fe4b54dcfb6f0f84
ridz1208 <ridz1208@users.noreply.github.com>
2017-09-06 16:05:32 -0400

    [Core] Pre-Release minor modifications (#3126)
    * Change target directory of the output script to be run as first patch of the release
    * Add clean up patch aggregating all clean up commands to be run seperately by administrator

--- tools/DB_date_zeros_removal.php
+++ tools/DB_date_zeros_removal.php
@@ -27,16 +27,17 @@ $base = $config->getSetting('base');
 $db->_trackChanges = false;
 
 // Set up variables
-$filename = __DIR__ . "/../project/tables_sql/update_zero_fields_statements.sql";
+$filename = __DIR__ . "/../SQL/Archive/17.1/2016-06-01-update_zero_fields_statements.sql";
 $output= "";
 $alters="";
 $updates="";
+$nonNullUpdates="";
 
 // Begin Script
 echo "\n#################################################################\n\n".
     "This Script will generate an UPDATE statement for every date field ".
-    "currently in the database. \nThe output file is ".
-    "tables_sql/update_zero_fields_statements.sql and includes foreign key ".
+    "currently in the database. \nThe output file is SQL/Archive/17.1/".
+    "2016-06-01-update_zero_fields_statements.sql and includes foreign key ".
     "checks disabling and re-enabling.\n".
     "\n#################################################################\n\n";
 
@@ -66,7 +67,6 @@ foreach ($field_names as $key=>$field) {
         $autoUpdateFields[$field['TABLE_NAME']][]= $field['COLUMN_NAME'];
     }
 }
-print_r($autoUpdateFields);
 
 // BEGIN building script
 
