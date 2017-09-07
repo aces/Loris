9be890e20fab9077c7699692a571631e0d605df7
David Blader <dblader.mcin@gmail.com>
2017-09-07 16:28:32 -0400

    first attempt

--- modules/mri_violations/js/mri_protocol_violations.js
+++ modules/mri_violations/js/mri_protocol_violations.js
@@ -22,41 +22,35 @@ function save() {
     "use strict";
     var default_value, id, value;
     /**To get the default value**/
-    $('.description').click(function (event) {
+    $('.description').focus(function (event) {
         id = event.target.id;
         default_value = $("#" + id).text();
     });
 
-    $('.description').bind('blur', function (event) {
-        event.stopImmediatePropagation();
-        id = event.target.id;
-        value = $("#" + id).text();
-        $('<div/>').appendTo('body')
-            //.css({background : 'black', opacity: '0.9'})
-            .html("<div id ='asdfsaf'>Are you sure?</div>")
-            .dialog({
-                title: 'Modification',
-                width: 'auto',
-                resizable: false,
-                //dialogClass:'transparent',
-                position: [800, 120],
-                buttons: {
-                    Yes: function () {
-                        $.get(loris.BaseURL + "/mri_violations/ajax/UpdateMRIProtocol.php?field_id=" + id + "&field_value=" + value, function () {});
-                        $(this).dialog("close");
-                    },
-                    close: function () {
-                        $(this).remove();
-                        $("#" + id).text(default_value);
-                    }
+    $('.description').blur(
+      function(event) {
+          event.stopImmediatePropagation();
+          id = event.target.id;
+          value = $('#'+id).text();
+          if (value != default_value) {
+            $('#' + id).css({});
+            $('.description').attr('contenteditable', false);
+            var userIsSure = confirm('Are you sure you want to edit ?');
+            if (userIsSure) {
+              $.post(
+                loris.BaseURL + '/mri_violations/ajax/UpdateMRIProtocol.php',
+                {
+                  field_id: id,
+                  field_value: value
                 }
-            });
-    }).keypress(function (e) {
-        if (e.which === 13) { // Determine if the user pressed the enter button
-	    e.preventDefault();
-            $(this).blur();
-        }
-    });
+              );
+            } else {
+              $('#' + id).text(default_value);
+            }
+            $('.description').attr('contenteditable', true);
+          }
+      }
+    );
 }
 
 $(function () {
