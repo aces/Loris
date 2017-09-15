00fb5ba9443aa0ae3b8294ce7bd292c0371bd2d2
David Blader <dblader.mcin@gmail.com>
2017-09-15 15:33:18 -0400

    prevent upload duplication and handle other mriFile statuses

--- modules/imaging_uploader/jsx/UploadForm.js
+++ modules/imaging_uploader/jsx/UploadForm.js
@@ -101,6 +101,17 @@ class UploadForm extends React.Component {
       return;
     }
 
+    // File in the middle of insertion pipeline
+    if (mriFile.status === "In progress") {
+      swal({
+        title: "File is currently processing!",
+        text: "A file with this name is currently going through the MRI pipeline!\n",
+        type: "error",
+        confirmButtonText: 'OK'
+      });
+      return;
+    }
+
     // File uploaded but failed during mri pipeline
     if (mriFile.status === "Failure") {
       swal({
@@ -117,8 +128,28 @@ class UploadForm extends React.Component {
           swal("Cancelled", "Your imaginary file is safe :)", "error");
         }
       }.bind(this));
-      return;
     }
+
+    // Pipeline has not been triggered yet
+    if (mriFile.status === "Not Started") {
+      swal({
+        title: "Are you sure?",
+        text: "A file with this name has been uploaded but has not yet started the MRI pipeline." +
+          "\n Would you like to override the existing file?",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonText: 'Yes, I am sure!',
+        cancelButtonText: 'No, cancel it!'
+      }, function(isConfirm) {
+        if (isConfirm) {
+          this.uploadFile(true);
+        } else {
+          swal("Cancelled", "Your upload has been cancelled.", "error");
+        }
+      }.bind(this));
+    }
+
+    return;
   }
 
   /*
