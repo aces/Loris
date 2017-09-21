e1afee376ce387ca577acbba0c0147c49c2e93cd
Dave MacFarlane <driusan@gmail.com>
2017-09-21 15:29:35 -0400

    Added helper script for Debian to create a LORIS project directory at an arbitrary location

---
+++ tools/create-project.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Creates a stub LORIS project directory at the location passed as the first argument
+mkdir -p $1/data $1/libraries $1/instruments $1/templates $1/tables_sql $1/modules $1/smarty/templates_c
