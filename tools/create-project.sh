78f7662d6c67e5c3de75db6f11f75f81fa0df5e8
Dave MacFarlane <driusan@gmail.com>
2017-09-22 09:16:30 -0400

    Quote directories to make spaces happy and change ownership of templates_c directory.

--- tools/create-project.sh
+++ tools/create-project.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 # Creates a stub LORIS project directory at the location passed as the first argument
-mkdir -p $1/data $1/libraries $1/instruments $1/templates $1/tables_sql $1/modules $1/smarty/templates_c
+for d in data libraries instruments templates tables_sql modules smarty/templates_c ; do
+    mkdir -p "${1}/${d}"
+done
+
+# Make sure the smarty templates_c directory is writable by Apache.
+# FIXME: Should this be here? If so, does it need to be sudo'ed?
+# FIXME 2: If this stays here, it should be smarty enough to detect
+# the correct user based on distro. For now, this is only used by
+# the Debian packaging script.
+chown www-data "$1/smarty/templates_c"
+chmod 770 "$1/smarty/templates_c"
