2e001e199cfad7267ef247575d8fce0b4b515feb
llevitis <llevitis@cbrain.mcgill.ca>
2017-10-20 15:00:19 -0400

    combining all previous help text additions

---
+++ SQL/Archive/18.1/2017-08-28_my_preference_help_text.sql
@@ -0,0 +1,3 @@
+UPDATE help
+  SET content = 'User Preferences allows users to modify their first name, last name, email address as they appear in the database, and current password as well as Document Repository and notification preferences. All changes made to the user’s preferences must be saved by clicking "Save" after completion. Information can be reset using the "Reset" button.', created = '2017-08-28 00:00:00'
+WHERE topic = 'My Preferences';
---
+++ SQL/Archive/18.1/2017-08-29_edit_help_text.sql
@@ -0,0 +1,3 @@
+UPDATE help
+  SET content = 'This page will display a title and existing content for each module. Both the title and the content are editable. The help text can be updated directly within the page. Users may also preview their text as it would appear when users click on the Help icon by pressing “Preview”. Click “Save” to finalize changes. To reset the help text to its most recently saved version , click “Reset”. To return to the main Help Editor page, click “Return to Edit Help Content”.', created = '2017-08-29 00:00:00'
+WHERE topic = 'Edit Help Content';
