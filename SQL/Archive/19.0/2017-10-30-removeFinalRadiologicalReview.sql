818d3c58c9fba645edc133450c4eade2fdeac418
kongtiaowang <kongtiaowangshen@gmail.com>
2017-11-02 11:29:49 -0400

    add SQL

--- SQL/Archive/19.0/2017-10-30-removeFinalRadiologicalReview.sql
+++ SQL/Archive/19.0/2017-10-30-removeFinalRadiologicalReview.sql
@@ -4,3 +4,7 @@ DROP TABLE IF EXISTS `final_radiological_review`;
     
 DELETE FROM LorisMenu where Label = "Radiological Reviews";
 update LorisMenu set OrderNumber = (OrderNumber-1) where Parent = 3;
+
+delete from permissions where code = "edit_final_radiological_review";
+delete from permissions where code = "view_final_radiological_review";
+update permissions set permID = (permID-2) where permID > 22;
