3a8cf564791ee56ccacd4b71032fa4bc488221da
kongtiaowang <kongtiaowangshen@gmail.com>
2017-11-02 10:53:39 -0400

    remove final radiological review

---
+++ SQL/Release_patches/18.0_19.0_upgade.sql
@@ -0,0 +1,6 @@
+/*! remove Radiological Reviews from menu */
+DROP TABLE IF EXISTS `final_radiological_review_history`;
+DROP TABLE IF EXISTS `final_radiological_review`;
+
+DELETE FROM LorisMenu where Label = "Radiological Reviews";
+update LorisMenu set OrderNumber = (OrderNumber-1) where Parent = 3;
