7e20c8c8e6f16f7be8feff1f768821cc97463c00
xlecours <xavier.lecoursboucher@mcgill.ca>
2017-11-03 13:00:50 -0400

    adding integration test to check that

--- modules/issue_tracker/test/Issue_TrackerTest.php
+++ modules/issue_tracker/test/Issue_TrackerTest.php
@@ -166,5 +166,18 @@ class Issue_TrackerTest extends LorisIntegrationTest
          )->getText();
          $this->assertNotContains("TestTestTest", $bodyText);
     }
+
+    /**
+     * Test that Edit Issue form load
+     */
+    function testEditIssueFormLoad()
+    {
+        $this->safeGet($this->url . "/issue_tracker/issue/?issueID=999999&backURL");
+        $side_bar = $this->webDriver->findElement(
+             WebDriverBy::Id('sidebar-wrapper')
+        );
+        $text = $side_bar->getText();
+        $this->assertContains("Back to list ", $text);
+    }
 }
 ?>
