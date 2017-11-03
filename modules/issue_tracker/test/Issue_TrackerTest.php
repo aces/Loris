bcc528f0252df7372f371b5dcfeb7a128a4830b7
xlecours <xavier.lecoursboucher@mcgill.ca>
2017-11-03 13:05:18 -0400

    phpcs

--- modules/issue_tracker/test/Issue_TrackerTest.php
+++ modules/issue_tracker/test/Issue_TrackerTest.php
@@ -169,14 +169,16 @@ class Issue_TrackerTest extends LorisIntegrationTest
 
     /**
      * Test that Edit Issue form load
+     *
+     * @return void
      */
     function testEditIssueFormLoad()
     {
         $this->safeGet($this->url . "/issue_tracker/issue/?issueID=999999&backURL");
         $side_bar = $this->webDriver->findElement(
-             WebDriverBy::Id('sidebar-wrapper')
+            WebDriverBy::Id('sidebar-wrapper')
         );
-        $text = $side_bar->getText();
+        $text     = $side_bar->getText();
         $this->assertContains("Back to list ", $text);
     }
 }
