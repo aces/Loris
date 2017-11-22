e0a0888ae533b3bb9ebbba6070e59da0a0ab69a3
MounaSafiHarab <mouna.safiharab@gmail.com>
2017-11-22 10:41:06 -0500

    Cleanup multiple modules: if user is in one study site and/or in "DCC": Redmine 13485 (#3272)
    Cleanup the code to use the isInOneStudySite() in the User class that was introduced in #2996 for data team helper. In this series of changes, we check if the user is in at least one study site and/or at the "DCC" (or centerID=1).
    
    In reference to Redmine 13485, and related to redmine 12946;

--- modules/candidate_list/ajax/validateProfileIDs.php
+++ modules/candidate_list/ajax/validateProfileIDs.php
@@ -12,15 +12,9 @@
  * @link     https://www.github.com/aces/Loris-Trunk/
  */
 
-$user     =& User::singleton();
-$site_arr = $user->getData('CenterIDs');
-foreach ($site_arr as $key=>$val) {
-    $site[$key]        = & Site::singleton($val);
-    $isStudySite[$key] = $site[$key]->isStudySite();
-}
-$oneIsStudySite = in_array("1", $isStudySite);
+$user =& User::singleton();
 if (!($user->hasPermission('access_all_profiles')
-    || ($oneIsStudySite && $user->hasPermission('data_entry')))
+    || ($user->hasStudySite() && $user->hasPermission('data_entry')))
 ) {
     header("HTTP/1.1 403 Forbidden");
     exit;
