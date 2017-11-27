69ec3a27a7ef34c6da9397c87eed30295d8110c0
llevitis <llevitis@cbrain.mcgill.ca>
2017-11-27 01:47:57 -0500

    rebased onto minor and bg-error is working

--- modules/candidate_list/js/candidate_list_helper.js
+++ modules/candidate_list/js/candidate_list_helper.js
@@ -1,20 +1,24 @@
 /*global document: false, $: false, window: false, unescape: false, Option: false,isElementsSet, alert*/
+
+loris.hiddenHeaders = ['Consent To Study'];
+
+/*global document: false, $: false, window: false, unescape: false, Option: false,isElementsSet, alert*/
 function hideFilter(obj) {
     'use strict';
 
-     var heading = $(obj);
-     var arrow   = $(obj).children('.arrow');
-     if (heading.hasClass('panel-collapsed')) {
-            // expand the panel
-            heading.parents('.panel').find('.panel-body').slideDown();
-            heading.removeClass('panel-collapsed');
-            arrow.removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
-        } else {
-            // collapse the panel
-            heading.parents('.panel').find('.panel-body').slideUp();
-            heading.addClass('panel-collapsed');
-            arrow.removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
-        }
+    var heading = $(obj);
+    var arrow   = $(obj).children('.arrow');
+    if (heading.hasClass('panel-collapsed')) {
+        // expand the panel
+        heading.parents('.panel').find('.panel-body').slideDown();
+        heading.removeClass('panel-collapsed');
+        arrow.removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+    } else {
+        // collapse the panel
+        heading.parents('.panel').find('.panel-body').slideUp();
+        heading.addClass('panel-collapsed');
+        arrow.removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+    }
 }
 function toggleMe() {
     "use strict";
@@ -25,9 +29,9 @@ function toggleMe() {
 
 
 $(function(){
-        $('input[name=dob]').datepicker({
-            dateFormat: 'yy-mm-dd',
-            changeMonth: true,
-            changeYear: true
-        });
+    $('input[name=dob]').datepicker({
+        dateFormat: 'yy-mm-dd',
+        changeMonth: true,
+        changeYear: true
+    });
 });
