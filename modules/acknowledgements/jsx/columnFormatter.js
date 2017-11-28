d484ff419cf4c8e12f9ed8fcf66d6e29fe12600e
kongtiaowang <kongtiaowangshen@gmail.com>
2017-11-28 16:16:31 -0500

    comment

--- modules/acknowledgements/jsx/columnFormatter.js
+++ modules/acknowledgements/jsx/columnFormatter.js
@@ -5,6 +5,7 @@
  * @param {string} column - column name
  * @param {string} cell - cell content
  * @param {arrray} rowData - array of cell contents for a specific row
+ * @param {arrray} rowHeaders - array of headers for the table
  * @return {*} a formated table cell for a given column
  */
 function formatAcknowledgementsColumn(column, cell, rowData, rowHeaders) {
