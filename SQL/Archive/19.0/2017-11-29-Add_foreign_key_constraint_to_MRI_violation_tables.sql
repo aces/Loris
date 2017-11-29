30976352510fd33734892baecff286ebed607666
cmadjar <cecile.madjar@gmail.com>
2017-11-29 15:51:31 -0500

    Forgot the ; at the end of the statements...

--- SQL/Archive/19.0/2017-11-29-Add_foreign_key_constraint_to_MRI_violation_tables.sql
+++ SQL/Archive/19.0/2017-11-29-Add_foreign_key_constraint_to_MRI_violation_tables.sql
@@ -1,2 +1,2 @@
-ALTER TABLE MRICandidateErrors ADD CONSTRAINT `FK_tarchive_MRICandidateError_1` FOREIGN KEY (`TarchiveID`) REFERENCES `tarchive` (`TarchiveID`)
-ALTER TABLE mri_violations_log ADD CONSTRAINT `FK_tarchive_mriViolationsLog_1` FOREIGN KEY (`TarchiveID`) REFERENCES `tarchive` (`TarchiveID`)
+ALTER TABLE MRICandidateErrors ADD CONSTRAINT `FK_tarchive_MRICandidateError_1` FOREIGN KEY (`TarchiveID`) REFERENCES `tarchive` (`TarchiveID`);
+ALTER TABLE mri_violations_log ADD CONSTRAINT `FK_tarchive_mriViolationsLog_1` FOREIGN KEY (`TarchiveID`) REFERENCES `tarchive` (`TarchiveID`);
