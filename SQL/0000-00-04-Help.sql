6463bda9eba19bb83b73c58d1939a5fa548db5cc
kongtiaowang <kongtiaowangshen@gmail.com>
2017-12-01 12:54:23 -0500

    help

--- SQL/0000-00-04-Help.sql
+++ SQL/0000-00-04-Help.sql
@@ -7,12 +7,13 @@ DROP TABLE IF EXISTS `help`;
 
 CREATE TABLE `help` (
     `helpID` int(10) unsigned NOT NULL AUTO_INCREMENT, 
+    `parentID` int(11) NOT NULL DEFAULT '-1',
     `hash` varchar(32) DEFAULT NULL,
     `topic` varchar(100) NOT NULL DEFAULT '',
+    `content` varchar(20) DEFAULT NULL,
     `projectContent` text DEFAULT NULL,
     `created` datetime DEFAULT NULL,
     `updated` datetime DEFAULT NULL, 
     PRIMARY KEY (`helpID`), 
     UNIQUE KEY `hash` (`hash`) 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
