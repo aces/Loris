5d7fffc21c460a997e95287c7db619101c7ef3a4
Dave MacFarlane <driusan@gmail.com>
2018-01-03 12:12:49 -0500

    Rename mantis_url to issue_tracker_url in NDB_Page

--- test/integrationtests/Login_Test.php
+++ test/integrationtests/Login_Test.php
@@ -1,10 +1,20 @@
 <?php
+
 require_once __DIR__ . '/LorisIntegrationTest.class.inc';
 class LorisLoginTest extends LorisIntegrationTest
 {
     function testLoginFailure()
     {
        $this->webDriver->get($this->url . '/main.php?logout=true');
+       // ?logout=true returns a 303 redirect to the login page.
+       // wait for the request account text, since it's non-customizable
+       // and on the login page before trying to login.
+       $this->webDriver->wait(10, 500)->until(
+           WebDriverExpectedCondition::presenceOfElementLocated
+           (
+               WebDriverBy::Name("username")
+           )
+       );
 
        $username = $this->webDriver->findElement(WebDriverBy::Name("username"));
        $this->assertEquals('', $username->getAttribute("value"));
