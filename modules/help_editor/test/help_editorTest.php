ce1b384160ae4ddb848ddea06d093d57363a5994
kongtiaowang <kongtiaowangshen@gmail.com>
2018-01-03 11:34:05 -0500

    phpcs

--- modules/help_editor/test/help_editorTest.php
+++ modules/help_editor/test/help_editorTest.php
@@ -40,12 +40,12 @@ class HelpEditorTestIntegrationTest extends LorisIntegrationTest
         $this->DB->insert(
             "help",
             array(
-             'helpID'         => '999999',
-             'parentID'       => '-1',
-             'hash'           => $md5String,
-             'topic'          => 'Test Topic',
-             'content'        => 'This is a test content.',
-             'created'        => '2013-04-05 00:00:00',
+             'helpID'   => '999999',
+             'parentID' => '-1',
+             'hash'     => $md5String,
+             'topic'    => 'Test Topic',
+             'content'  => 'This is a test content.',
+             'created'  => '2013-04-05 00:00:00',
             )
         );
     }
