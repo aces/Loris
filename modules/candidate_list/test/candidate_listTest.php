3e70688f9bef964d3b619ed12e323189d702f196
kongtiaowang <kongtiaowangshen@gmail.com>
2018-01-04 12:46:12 -0500

    ca

--- modules/candidate_list/test/candidate_listTest.php
+++ modules/candidate_list/test/candidate_listTest.php
@@ -349,8 +349,9 @@ class CandidateListTestIntegrationTest extends LorisIntegrationTestWithCandidate
         $this->webDriver->executescript(
             "document.querySelectorAll('#dynamictable > tbody > tr >".
             " td.dynamictableFrozenColumn > a')[0].click()"
-        );sleep(1);
- $bodyText = $this->webDriver->getPageSource();
+        );
+        sleep(1);
+        $bodyText = $this->webDriver->getPageSource();
         print($bodyText);
 
         //make sure that breadcrumb contains DCCID
