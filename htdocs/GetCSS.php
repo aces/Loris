0dc4db17ca99983d33e47a868f873d30b8bb8b58
zainvirani <zainilabideen.virani@mail.mcgill.ca>
2018-01-08 12:15:35 -0500

    fixed minor mistake

--- htdocs/GetCSS.php
+++ htdocs/GetCSS.php
@@ -98,7 +98,7 @@ try {
 } catch(LorisModuleMissingException $e) {
     $public = false;
 }
-if ($anonymous === true && $m->isPublicModule() === false) {
+if ($anonymous === true && $public->isPublicModule() === false) {
     header("HTTP/1.1 403 Forbidden");
     exit(6);
 }
