92aa470db0fa48a37692eb790233f8d83691daa1
xlecours <xavier.lecoursboucher@mcgill.ca>
2018-01-23 14:45:22 -0500

    also add a 500 page

--- htdocs/main.php
+++ htdocs/main.php
@@ -213,8 +213,14 @@ try {
         $errorPage->assign($tpl_data);
         $tpl_data['workspace'] = $errorPage->fetch('403.tpl');
         break;
+    default:
+        header("HTTP/1.1 500 ");
+        $tpl_data['error_message'][] = htmlspecialchars($e->getMessage());
+        $errorPage = new Smarty_neurodb;
+        $errorPage->assign($tpl_data);
+        $tpl_data['workspace'] = $errorPage->fetch('500.tpl');
+        break;
     }
-    $tpl_data['error_message'][] = htmlspecialchars($e->getMessage());
 } finally {
     // Set dependencies if they are not set
     if (!isset($tpl_data['jsfiles']) || !isset($tpl_data['cssfiles'])) {
