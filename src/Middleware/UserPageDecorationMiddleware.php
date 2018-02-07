144c6dc7e8ab10181f20ce7f4921ad1dc50431a7
Dave MacFarlane <driusan@gmail.com>
2018-02-07 16:20:19 -0500

    Add page decoration middleware
    This adds PSR15 based page decoration middleware for both
    anonymous, and logged in users. The middleware takes an undecorated
    request and adds all the "chrome" (headers, footers, etc) that
    are required to make the requests look consistent and will be
    required by an upcoming PSR15-based router pull request.
    
    This is an attempt to split it into smaller, more manageable
    chunks. The whole PSR15-based router can be seen in my
    PSR7RouterZend branch, for context, but this code should
    be self-contained.

--- src/Middleware/UserPageDecorationMiddleware.php
+++ src/Middleware/UserPageDecorationMiddleware.php
@@ -21,13 +21,12 @@ class UserPageDecorationMiddleware implements MiddlewareInterface {
     }
 
     /**
-     * Displays a page to an logged in user, adding the necessary
-     * header/footer/etc page decorations.
+     * Displays a page to an anonymous (not logged in) user.
      *
-     * @param ServerRequestInterface  $request The incoming request
-     * @param RequestHandlerInterface $handler The handler to decorate
+     * @param array $get  The GET parameters from the request.
+     * @param array $post The POST parameters from the request.
      *
-     * @return ResponseInterface a PSR15 response of handler, after adding decorations.
+     * @return string the page content
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler) : ResponseInterface { 
         ob_start();
