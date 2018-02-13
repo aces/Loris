840dcb6b93e7522c3188dfda1d01b7ef7d0db44e
kongtiaowang <kongtiaowangshen@gmail.com>
2018-02-12 23:09:12 -0500

    d

--- test/dockerized-integration-tests.sh
+++ test/dockerized-integration-tests.sh
@@ -7,5 +7,5 @@ then
 else
     CONTAINER=integration-tests
 fi
-docker-compose run -T --rm ${CONTAINER} vendor/bin/phpunit --configuration test/phpunit.xml  $*
+docker-compose run -T --rm ${CONTAINER} npm run lint:php && npm run lint:javascript && vendor/bin/phpunit --configuration test/phpunit.xml  $*
 docker-compose run -T --rm ${CONTAINER} vendor/bin/phpunit --configuration test/phpunit.xml --testsuite LorisModuleIntegrationTests $*
