b6620a51eaca9ac45eaf9c85124484d9c681645d
Liza Levitis <llevitis.mcin@gmail.com>
2018-02-27 18:20:44 -0500

    added let keyword to use previous naming convention

--- jsx/StaticDataTable.js
+++ jsx/StaticDataTable.js
@@ -301,10 +301,10 @@ var StaticDataTable = React.createClass({
     if (typeof filterData === 'object') {
       var match = false;
       for (let i = 0; i < filterData.length; i += 1) {
-        var searchKey2 = filterData[i].toLowerCase();
-        var searchString2 = data.toLowerCase();
+        let searchKey  = filterData[i].toLowerCase();
+        let searchString = data.toLowerCase();
 
-        match = (searchString2.indexOf(searchKey2) > -1);
+        match = (searchString.indexOf(searchKey) > -1);
         if (match) {
           return true;
         }
