6d66dc2dcb65bdb62eb45c432af4943c40e3adde
Liza Levitis <llevitis.mcin@gmail.com>
2018-03-02 14:40:38 -0500

    added empty line to print statement

--- tools/normalize_protocol_split_rows.php
+++ tools/normalize_protocol_split_rows.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * This script is the first step to normalizing the mri_protocol table for projects which store 
- * comma-separated values. This script splits up cells containing commas by creating new rows for 
- * each value and maintaining unique combinations of values. 
+ * This script is the first step to normalizing the  table for projects which store
+ * comma-separated values. This script splits up cells containing commas by creating new rows for
+ * each value and maintaining unique combinations of values.
  *
  * PHP Version 7
  *
@@ -17,12 +17,12 @@ require_once 'generic_includes.php';
 require_once 'Database.class.inc';
 require_once 'Utility.class.inc';
 
-$DB = \Database::singleton(); 
+$DB = \Database::singleton();
 
-/* 
- * Converts an array of arrays of values for different fields into  
- * an array of unique combinations of the different values. 
- */ 
+/*
+ * Converts an array of arrays of values for different fields into
+ * an array of unique combinations of the different values.
+ */
 function cartesian($input) {
     // filter out empty values
     $result = array(array());
@@ -31,10 +31,10 @@ function cartesian($input) {
         foreach($result as $product) {
             foreach($values as $item) {
                 $product[$key] = $item;
-                // remove $key with empty value so that 
+                // remove $key with empty value so that
                 // an empty string isn't inserted into the database instead of a default NULL value
-                if ($product[$key] == "") { 
-                    unset($product[$key]); 
+                if ($product[$key] == "") {
+                    unset($product[$key]);
                 }
                 $append[] = $product;
             }
@@ -44,30 +44,30 @@ function cartesian($input) {
     return $result;
 }
 
-// Get the list of unique IDs from the mri_protocol table
-$mp_idx = $DB->pselect("SELECT ID FROM mri_protocol", array()); 
-$idx = array(); 
-foreach($mp_idx as $num => $value) { 
-    array_push($idx, $value["ID"]);  
+// Get the list of unique IDs from the mri_protocol_3_3 table
+$mp_idx = $DB->pselect("SELECT ID FROM mri_protocol_3", array());
+$idx = array();
+foreach($mp_idx as $num => $value) {
+    array_push($idx, $value["ID"]);
 }
 
-// insert new rows for comma separated values 
-foreach ($idx as $id) { 
-    $mp_data = $DB->pselectRow("SELECT * FROM mri_protocol mp WHERE mp.ID=:id", array('id' => $id));
+// insert new rows for comma separated values
+foreach ($idx as $id) {
+    $mp_data = $DB->pselectRow("SELECT * FROM mri_protocol_3 mp WHERE mp.ID=:id", array('id' => $id));
     foreach($mp_data as $key => $value) {
-       $mp_data[$key] = explode(",", $value); 
-    } 
-    $all_mp_combinations = cartesian($mp_data); 
+       $mp_data[$key] = explode(",", $value);
+    }
+    $all_mp_combinations = cartesian($mp_data);
     foreach ($all_mp_combinations as $mp_combination) {
         unset($mp_combination["ID"]);
-        $DB->insert("mri_protocol", $mp_combination); 
-    } 
-    // remove the row for the original ID 
-    $DB->delete("mri_protocol", array("ID" => $id)); 
+        $DB->insert("mri_protocol_3", $mp_combination);
+    }
+    // remove the row for the original ID
+    $DB->delete("mri_protocol_3", array("ID" => $id));
 }
 
-echo("All mri_protocol entries are now unique and not comma-separated."); 
+echo("All mri_protocol_3 entries are now unique and not comma-separated.\n\n");
 
- 
-  
+
+
 ?>
