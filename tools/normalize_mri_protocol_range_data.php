220b90504c93d8c8c415aae3e9b5df8a7d8e530d
Liza Levitis <llevitis.mcin@gmail.com>
2018-03-02 15:38:50 -0500

    updated table name reference

--- tools/normalize_mri_protocol_range_data.php
+++ tools/normalize_mri_protocol_range_data.php
@@ -33,14 +33,14 @@ if (!empty($argv[1]) && $argv[1] == "tosql") {
 $DB = \Database::singleton();
 
 // get all "ID" values from the mri_protocol table
-$mp_idx = $DB->pselect("SELECT ID FROM mri_protocol_3", array());
+$mp_idx = $DB->pselect("SELECT ID FROM mri_protocol", array());
 $idx = array();
 foreach($mp_idx as $num => $value) {
     array_push($idx, $value["ID"]);
 }
 
 // get all column names like "%_range" from mri_protocol table
-$mp_columns = $DB->pselect("SELECT column_name FROM information_schema.columns WHERE table_name='mri_protocol_3'", array());
+$mp_columns = $DB->pselect("SELECT column_name FROM information_schema.columns WHERE table_name='mri_protocol'", array());
 $mp_range_columns = array();
 foreach($mp_columns as $id => $value) {
     if (substr($value["column_name"], -5) == "range") {
@@ -49,7 +49,7 @@ foreach($mp_columns as $id => $value) {
 }
 
 foreach($idx as $id) {
-    $mp_data = $DB->pselect("SELECT * FROM mri_protocol_3 mp WHERE mp.ID=:id", array('id' => $id));
+    $mp_data = $DB->pselect("SELECT * FROM mri_protocol mp WHERE mp.ID=:id", array('id' => $id));
     $data_to_insert = array();
     foreach($mp_range_columns as $range_col) {
         $col_data = explode("-", $mp_data[0][$range_col]);
@@ -64,13 +64,13 @@ foreach($idx as $id) {
             $data_to_insert[$col_max] = $col_data[0];
         }
     }
-    $DB->update("mri_protocol_3", $data_to_insert, array('ID' => $id));
+    $DB->update("mri_protocol", $data_to_insert, array('ID' => $id));
 }
 
 echo("Data insertion for new min & max columns is complete.\n");
 
 foreach($mp_range_columns as $range_column) {
-    $output .= "ALTER TABLE mri_protocol_3 DROP $range_column;\n";
+    $output .= "ALTER TABLE mri_protocol DROP $range_column;\n";
 }
 
 if ($printToSQL) {
