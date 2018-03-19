dbcc77fe31fa9f9ac272a04b33af74da943f257b
ridz1208 <ridz1208@users.noreply.github.com>
2018-03-19 14:10:23 -0400

    [CouchDB] Adding support for multiple databases on different servers (#3546)
    This builds on #2795 to extend the functionality to be able to support multiple databases on different servers. This is backwards compatible as it will revert to the DQT credentials if none are passed. This throws deprecation warnings and exceptions when parameters are not properly passed
    
    This is removing the "auto-load" of credentials from the libraries and explicitely passing them when instantiating a couchDB instance.

--- modules/dataquery/ajax/queryNotEqual.php
+++ modules/dataquery/ajax/queryNotEqual.php
@@ -19,10 +19,18 @@ require_once __DIR__ . '/../../../vendor/autoload.php';
 $client = new NDB_Client();
 $client->makeCommandLine();
 $client->initialize(__DIR__ . "/../../../project/config.xml");
-$cdb       = \NDB_Factory::singleton()->couchDB();
-$category  = $_REQUEST['category'];
-$fieldName = $_REQUEST['field'];
-$value     = $_REQUEST['value'];
+$config      = \NDB_Config::singleton();
+$couchConfig = $config->getSetting('CouchDB');
+$cdb         = \NDB_Factory::singleton()->couchDB(
+    $couchConfig['dbName'],
+    $couchConfig['hostname'],
+    $couchConfig['port'],
+    $couchConfig['admin'],
+    $couchConfig['adminpass']
+);
+$category    = $_REQUEST['category'];
+$fieldName   = $_REQUEST['field'];
+$value       = $_REQUEST['value'];
 // There's no way to do "not" in an index, so we need to
 // get all the values of the field name, and then iterate
 // through them and exclude the ones where the value is
