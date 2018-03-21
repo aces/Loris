17d1636f84b8464594100f4ce82e668c1d9a96f7
zaliqarosli <zaliqa.rosli@mcin.ca>
2018-03-20 20:35:43 -0400

    Fix preview help content

--- modules/help_editor/js/help_editor_helper.js
+++ modules/help_editor/js/help_editor_helper.js
@@ -9,20 +9,23 @@ $("input[name=preview]").click(function(e) {
         content = $('textarea[name="content"]').val(),
         myDate  = new Date(),
         div     = document.createElement("div"),
-        pre     = document.createElement("pre"),
         btn     = document.createElement("BUTTON"),
-        text    = document.createTextNode("Edit"),
         button  = document.createTextNode("Close");
+        wrap    = document.createElement("pre"),
 
-     pre.innerHTML  = "<h3>" + title + "</h3>";
-     pre.innerHTML += content;
-     pre.innerHTML  =  pre.innerHTML + "<hr>Last updated: " + myDate.getFullYear() + "-" +
+     wrap.setAttribute("id", "help-wrapper");
+     wrap.innerHTML = "<hr id='help-separator'>";
+     wrap.innerHTML  = "<h3>" + title + "</h3>";
+     markdownContent = document.createElement("div");
+     ReactDOM.render(RMarkdown({content: content}), markdownContent);
+     wrap.appendChild(markdownContent);
+     wrap.innerHTML  =  wrap.innerHTML + "<hr>Last updated: " + myDate.getFullYear() + "-" +
                       (myDate.getMonth() +1) + "-" + myDate.getDate() + " " +
                       myDate.getHours() + ":" + myDate.getMinutes() + ":" + myDate.getSeconds();
      btn.appendChild(button);
      btn.className="btn btn-default";
      btn.setAttribute("id","helpclose");
-     div.appendChild(pre);
+     div.appendChild(wrap);
      div.appendChild(btn);
      document.getElementById('page').appendChild(div);
      div.setAttribute("class", "help-content");
