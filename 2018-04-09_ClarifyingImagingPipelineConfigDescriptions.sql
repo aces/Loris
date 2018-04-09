652129f0b6823e01997dbba33cc53e3682ba490e
Cecile Madjar <cecile.madjar@gmail.com>
2018-04-09 15:04:22 -0400

    Dave's feedback on the description wording

--- 2018-04-09_ClarifyingImagingPipelineConfigDescriptions.sql
+++ 2018-04-09_ClarifyingImagingPipelineConfigDescriptions.sql
@@ -1,16 +1,16 @@
 UPDATE ConfigSettings
-    SET Description='Enable to generate horizontal pictures'
+    SET Description='Enable generation of horizontal pictures'
     WHERE Name='horizontalPics';
 
 UPDATE ConfigSettings
-    SET Description='Enable to create NIfTI files'
+    SET Description='Enable creation of NIfTI files'
     WHERE Name="create_nii";
 
 UPDATE ConfigSettings
-    SET Description='Enable to create candidates from the upload'
+    SET Description='Enable creation of candidates from the upload'
         WHERE Name='createCandidates';
 
 UPDATE ConfigSettings
-    SET Description='Enable to use batch management of the imaging pipeline'
+    SET Description='Enable use of batch management in the imaging pipeline'
         WHERE Name='is_qsub';
 
