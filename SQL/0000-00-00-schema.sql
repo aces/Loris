8ab06b308588e9024e8227e2cd698c7b50c82673
Mélanie Legault <melanie.legault2@mcgill.ca>
2018-04-11 13:10:09 -0400

    enclosed constraint name inside backticks

--- SQL/0000-00-00-schema.sql
+++ SQL/0000-00-00-schema.sql
@@ -2021,9 +2021,9 @@ CREATE TABLE `visit` (
   `VisitID` int(10) unsigned NOT NULL auto_increment,
   `VisitLabel` varchar(255) NOT NULL,
   `VisitLegacyLabel` varchar(255) DEFAULT NULL,
-  CONSTRAINT visit_PK PRIMARY KEY (`VisitID`),
-  CONSTRAINT visit_label_UK UNIQUE KEY (`VisitLabel`),
-  CONSTRAINT visit_legacy_label_UK UNIQUE KEY (`VisitLegacyLabel`)
+  CONSTRAINT `visit_PK` PRIMARY KEY (`VisitID`),
+  CONSTRAINT `visit_label_UK` UNIQUE KEY (`VisitLabel`),
+  CONSTRAINT `visit_legacy_label_UK` UNIQUE KEY (`VisitLegacyLabel`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `visit_project_subproject_rel` (
@@ -2031,13 +2031,13 @@ CREATE TABLE `visit_project_subproject_rel` (
   `VisitID` int(10) unsigned NOT NULL,
   `ProjectID` int(2) DEFAULT NULL,
   `SubprojectID` int(10) unsigned DEFAULT NULL,
-  CONSTRAINT visit_project_subprojet_rel_PK PRIMARY KEY (`VisitProjetSubProjetID`),
-  CONSTRAINT visit_project_subprojet_rel_UK UNIQUE KEY (`VisitID`, `ProjectID`, `SubprojectID`),
-  CONSTRAINT visit_project_subproject_rel_VisitID_visit_VisitID_FK FOREIGN KEY (`VisitID`) 
+  CONSTRAINT `visit_project_subprojet_rel_PK` PRIMARY KEY (`VisitProjetSubProjetID`),
+  CONSTRAINT `visit_project_subprojet_rel_UK` UNIQUE KEY (`VisitID`, `ProjectID`, `SubprojectID`),
+  CONSTRAINT `visit_project_subproject_rel_VisitID_visit_VisitID_FK` FOREIGN KEY (`VisitID`) 
     REFERENCES `visit`(`VisitID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT visit_project_subproject_ProjectID_visit_ProjectID_FK FOREIGN KEY (`ProjectID`)
+  CONSTRAINT `visit_project_subproject_ProjectID_visit_ProjectID_FK` FOREIGN KEY (`ProjectID`)
     REFERENCES `Project`(`ProjectID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT visit_project_subproject_SubprojectID_visit_SubProjectID_FK FOREIGN KEY (`SubprojectID`)
+  CONSTRAINT `visit_project_subproject_SubprojectID_visit_SubProjectID_FK` FOREIGN KEY (`SubprojectID`)
     REFERENCES `subproject`(`SubprojectID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
