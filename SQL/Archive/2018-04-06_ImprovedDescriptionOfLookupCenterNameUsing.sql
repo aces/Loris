d95a33c1fac807402f7088441328829f837f67ad
Cecile Madjar <cecile.madjar@gmail.com>
2018-04-12 13:49:22 -0400

    Mouna's feedback on a better phrasing

--- SQL/Archive/2018-04-06_ImprovedDescriptionOfLookupCenterNameUsing.sql
+++ SQL/Archive/2018-04-06_ImprovedDescriptionOfLookupCenterNameUsing.sql
@@ -1,5 +1,5 @@
 -- Change description and label of the ConfigSetting named lookupCenterNameUsing
 UPDATE ConfigSettings SET
-    Description = "DICOM field to use to get the patient and center name of the DICOM study, being either PatientID or PatientName",
-    Label = "Patient and center name lookup variable"
+    Description = "DICOM field to use to get the patient identifiers (being stored either in PatientName or PatientID) and the center name of the DICOM study",
+    Label = "Patient identifiers and center name lookup variable"
     WHERE Name = "lookupCenterNameUsing";
