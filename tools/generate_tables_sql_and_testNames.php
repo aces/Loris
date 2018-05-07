1e3df08aa495d2815b7fed178268aaade2fbf7f2
Alizée <intralizee@users.noreply.github.com>
2018-05-07 18:44:25 -0400

    # This is a combination of 3 commits. # This is the 1st commit message:
    fixing sql injection
    
    fixing sql injection
    
    Remove debugging line
    
    Use ?? idiom replacement
    
    # This is the commit message #2:
    
    remove  variable that was used for debugging
    
    # This is the commit message #3:
    
    update comment to PHP 7

--- tools/generate_tables_sql_and_testNames.php
+++ tools/generate_tables_sql_and_testNames.php
@@ -1,21 +1,26 @@
 #!/usr/bin/php
 <?php
-
 /**
- * generate_tables_sql.php takes the ip_output.txt file generated from
+ * Behavioural
+ *
+ * PHP Version 7
+ *
+ * The generate_tables_sql.php takes the ip_output.txt file generated from
  * quickform_parser.php and outputs an sql build file for the table of each
  * instrument it finds in the ip_output.txt file.  These sql files are output
  * to the tables_sql/ subdirectory.
+ * Ex cmd:  php generate_tables_sql.php
  *
- * ex cmd:  php generate_tables_sql.php
- *
- * @package behavioural
+ * @category Tools
+ * @package  Behavioural
+ * @author   Loris Team <loris.mni@bic.mni.mcgill.ca>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link     https://github.com/aces/Loris
  */
-
-//Ensure php version compatability
+//Ensure php version compatibility
 //taken from php.net notes
-
 // require all relevant OO class libraries
+
 require_once __DIR__ . "/../vendor/autoload.php";
 require_once "../php/libraries/Database.class.inc";
 require_once "../php/libraries/NDB_Config.class.inc";
@@ -23,96 +28,188 @@ require_once "../php/libraries/NDB_BVL_Instrument.class.inc";
 
 $data = stream_get_contents(STDIN);
 
-$instruments=explode("{-@-}",trim($data));
+$instruments = explode("{-@-}", trim($data));
 
-$tblCount=0;
-$parameterCount=0;
+$tblCount       = 0;
+$parameterCount = 0;
+
 $pages = array();
-foreach($instruments AS $instrument){
-    $catId="";
-    $items=explode("\n",trim($instrument));
-    foreach($items AS $item){
-        $paramId="";
-        $bits=explode("{@}",trim($item));
-        if(preg_match("/Examiner[0-9]*/" , $bits[1])){
+
+$sql_query_statement  = "";
+$sql_query_table_name = "";
+$sql_query_columns    = array();
+
+// Setup Database object.
+$config    =& \NDB_Config::singleton();
+$db_config = $config->getSetting('database');
+$db        = \Database::singleton(
+    $db_config['database'],
+    $db_config['quatUser'],
+    $db_config['quatPassword'],
+    $db_config['host']
+);
+
+foreach ($instruments AS $instrument) {
+    $catId = "";
+    $items = explode("\n", trim($instrument));
+    foreach ($items AS $item) {
+        $paramId = "";
+        $bits    = explode("{@}", trim($item));
+        if (preg_match("/Examiner[0-9]*/", $bits[1])) {
             continue;
         }
-        switch($bits[0]){
+        switch($bits[0]) {
             //generate the CREATE TABLE syntax
-            case "table":
-                $tablename = $bits[1];
-
-                $filename="../project/tables_sql/".$bits[1].".sql";
-                $output="CREATE TABLE `$bits[1]` (\n";
-                $output.="`CommentID` varchar(255) NOT NULL default '',
-                          `UserID` varchar(255) default NULL,
-                          `Examiner` varchar(255) default NULL,
-                          `Testdate` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                          `Data_entry_completion_status` enum('Incomplete','Complete') NOT NULL default 'Incomplete',\n";
+        case "table":
+            $tablename            = $bits[1];
+            $sql_query_table_name = $db->escape($bits[1]);
+            $filename = "../project/tables_sql/".$bits[1].".sql";
+            $output   = "CREATE TABLE `$bits[1]` (\n";
+            $output  .= "`CommentID` varchar(255) NOT NULL default '',\n"
+                ."`UserID` varchar(255) default NULL,\n"
+                ."`Examiner` varchar(255) default NULL,\n"
+                ."`Testdate` timestamp DEFAULT CURRENT_TIMESTAMP ON "
+                ."UPDATE CURRENT_TIMESTAMP,\n"
+                ."`Data_entry_completion_status` "
+                ."enum('Incomplete','Complete') "
+                ."NOT NULL default 'Incomplete',\n";
+            $sql_query_statement = "CREATE TABLE `$sql_query_table_name` (\n";
+            // created first five query entries.
+            array_push(
+                $sql_query_columns,
+                "`CommentID` varchar(255) NOT NULL default '',\n"
+            );
+            array_push(
+                $sql_query_columns,
+                "`UserID` varchar(255) default NULL,\n"
+            );
+            array_push(
+                $sql_query_columns,
+                "`Examiner` varchar(255) default NULL,\n"
+            );
+            array_push(
+                $sql_query_columns,
+                "`Testdate` timestamp DEFAULT CURRENT_TIMESTAMP 
+            ON UPDATE CURRENT_TIMESTAMP,\n"
+            );
+            array_push(
+                $sql_query_columns,
+                "`Data_entry_completion_status` enum('Incomplete',
+            'Complete') NOT NULL default 'Incomplete',\n"
+            );
             break;
-            case "page":
-                $pages[] = $bits[2];
-                continue;
-
-            //no SQL need be generated.
-            case "title":
-                $title = $bits[1];
-            case "header":
-                continue;
+        case "page":
+            $pages[] = $bits[2];
+            continue;
+        //no SQL need be generated.
+        case "title":
+            $title = $bits[1];
+        case "header":
+            continue;
             break;
-
-            //generate specific column definitions for specific types of HTML elements
-            default:
-                if($bits[1] == "") {
-                    continue;
-                }
-                if($bits[0]=="select"){
-                    $bits[0]=enumizeOptions($bits[3], isset($tablename) ? $tablename : null, $bits[1]);
-                } else if($bits[0]=="selectmultiple"){
-                    $bits[0]="varchar(255)";
-                } else if($bits[0]=="textarea"){
-                    $bits[0]="text";
-                } else if($bits[0]=="text"){
-                    $bits[0]="varchar(255)";
-                } else if($bits[0]=="checkbox") {
-                    $bits[0]="varchar(255)";
-                } else if ($bits[0]=="static") {
-                    $bits[0]="varchar(255)";
-                }
-
-                $bits[2]=htmlspecialchars($bits[2]);
-                $output.="`$bits[1]` $bits[0] default NULL,\n";
+        //generate specific column definitions for specific types of HTML elements
+        default:
+            if ($bits[1] == "") {
+                continue;
+            }
+            if ($bits[0]=="select") {
+                $bits[0] = enumizeOptions(
+                    $bits[3],
+                    isset($tablename)
+                    ?? null,
+                    $bits[1]
+                );
+            } else if ($bits[0]=="selectmultiple") {
+                $bits[0] = "varchar(255)";
+            } else if ($bits[0]=="textarea") {
+                $bits[0] = "text";
+            } else if ($bits[0]=="text") {
+                $bits[0] = "varchar(255)";
+            } else if ($bits[0]=="checkbox") {
+                $bits[0] = "varchar(255)";
+            } else if ($bits[0]=="static") {
+                $bits[0] = "varchar(255)";
+            } else {
+                // Safety SQL injection prevention.
+                $bits[0] = $db->escape($bits[0]);
+            }
+            $bits[2]     = htmlspecialchars($bits[2]);
+            $output     .= "`$bits[1]` $bits[0] default NULL,\n";
+            $column_name = $db->escape($bits[1]);
+            array_push(
+                $sql_query_columns,
+                "`$column_name` $bits[0] default NULL,\n"
+            );
         }
-
     }
-    $output.="PRIMARY KEY  (`CommentID`)\n) ENGINE=InnoDB DEFAULT CHARSET=utf8;\n";
-    print "Filename: $filename\n";
-    $fp=fopen($filename, "w");
+    $output .= "PRIMARY KEY  (`CommentID`)\n) ENGINE=InnoDB DEFAULT CHARSET=utf8;\n";
+    $fp      = fopen($filename, "w");
     fwrite($fp, $output);
-    fwrite($fp, "REPLACE INTO test_names (Test_name, Full_name, Sub_group) VALUES ('$tablename', '$title', 1);\n");
-    foreach($pages as $pageNo => $page) {
-        fwrite($fp, "INSERT INTO instrument_subtests (Test_name, Subtest_name, Description) VALUES ('$tablename', '" . $tablename  . "_page" . ($pageNo+1) . "', '$page');\n");
-
+    fwrite(
+        $fp,
+        "REPLACE INTO test_names (Test_name, Full_name, Sub_group) "
+        ."VALUES ('$tablename', '$title', 1);\n"
+    );
+    // Writes a record of the changes to a file but is not sourced directly.
+    foreach ($pages as $pageNo => $page) {
+        fwrite(
+            $fp,
+            "INSERT INTO instrument_subtests (Test_name, Subtest_name, Description)"
+            . " VALUES ('$tablename', '" . $tablename  . "_page" . ($pageNo+1)
+            . "', '$page');\n"
+        );
     }
     fclose($fp);
 }
 
-echo "\n\Table SQL Generation complete\n";
-
-function enumizeOptions($options, $table, $name){
-    $options=explode("{-}",$options);
-    foreach($options as $option){
-        $option=explode("=>",$option);
-        if($option[0]!='NULL'){
-            $enum[]=$option[0];
+if ($db->isConnected()) {
+    if (!$db->tableExists($sql_query_table_name)) {
+        // Table doesn't exist!
+        $sql_query_statement .= implode($sql_query_columns);
+        $sql_query_statement .=
+            "PRIMARY KEY  (`CommentID`)\n) ENGINE=InnoDB DEFAULT CHARSET=utf8;\n";
+        $statement            = $db->prepare($sql_query_statement);
+        $statement->execute();
+    }
+    $statement = $db->prepare(
+        'REPLACE INTO test_names (Test_name, Full_name, Sub_group) VALUES (?, ?, 1)'
+    );
+    $statement->bindParam(1, $tablename, PDO::PARAM_STR);
+    $statement->bindParam(2, $title, PDO::PARAM_STR);
+    $statement->execute();
+    foreach ($pages as $pageNo => $page) {
+        $statement    = $db->prepare(
+            'INSERT INTO instrument_subtests (Test_name, Subtest_name, Description)'
+            . ' VALUES (?, ?, ?)'
+        );
+        $subtest_name = $tablename  . "_page" . ($pageNo+1);
+        $statement->bindParam(1, $tablename, PDO::PARAM_STR);
+        $statement->bindParam(2, $subtest_name, PDO::PARAM_STR);
+        $statement->bindParam(3, $page, PDO::PARAM_STR);
+        $statement->execute();
+    }
+}
+/**
+ * The enumizeOptions function
+ *
+ * @param array  $options the enum values.
+ * @param string $table   the name used.
+ * @param string $name    the table uses.
+ *
+ * @return string
+ */
+function enumizeOptions($options, $table, $name)
+{
+    $options = explode("{-}", $options);
+    foreach ($options as $option) {
+        $option = explode("=>", $option);
+        if ($option[0] != 'NULL') {
+            $enum[] = $option[0];
         }
     }
-    if(!is_array($enum)){
+    if (!is_array($enum)) {
         echo "$table $name $options\n";
     }
-    $enum=implode(",",$enum);
+    $enum = implode(",", $enum);
     return "enum($enum)";
 }
-
-
-?>
