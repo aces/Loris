52bada6bf7461043c7b0e72c77a9d181743c8b62
Mélanie Legault <melanie.legault2@mcgill.ca>
2018-05-16 10:38:47 -0400

    some typo fix from comment

--- SQL/2018-01-17_normalisation_visit.sql
+++ SQL/2018-01-17_normalisation_visit.sql
@@ -8,13 +8,13 @@ CREATE TABLE `visit` (
 CREATE TABLE `visit_project_subproject_rel` (
   `VisitID` int(10) unsigned NOT NULL,
   `ProjectID` int(2) NOT NULL,
-  `SubProjectID` int(10) unsigned NOT NULL,
-  CONSTRAINT visit_project_subprojet_rel_PK PRIMARY KEY (`VisitID`, `ProjectID`, `SubProjectID`),
-  CONSTRAINT visit_project_subproject_rel_VisitID_visit_VisitID_FK FOREIGN KEY (`VisitID`) 
+  `SubprojectID` int(10) unsigned NOT NULL,
+  CONSTRAINT `visit_project_subprojet_rel_PK` PRIMARY KEY (`VisitID`, `ProjectID`, `SubprojectID`),
+  CONSTRAINT `visit_project_subproject_rel_VisitID_visit_VisitID_FK` FOREIGN KEY (`VisitID`) 
     REFERENCES `visit`(`VisitID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT visit_project_subproject_ProjectID_visit_ProjectID_FK FOREIGN KEY (`ProjectID`)
+  CONSTRAINT `visit_project_subproject_ProjectID_visit_ProjectID_FK` FOREIGN KEY (`ProjectID`)
     REFERENCES `Project`(`ProjectID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT visit_project_subproject_SubprojectID_visit_SubProjectID_FK FOREIGN KEY (`SubProjectID`)
+  CONSTRAINT `visit_project_subproject_SubprojectID_visit_SubprojectID_FK` FOREIGN KEY (`SubprojectID`)
     REFERENCES `subproject`(`SubprojectID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
