c922c511fdc269c1c1a7cb266508f5acc1618f71
Alizée <intralizee@users.noreply.github.com>
2018-05-30 11:38:51 -0400

    added new logic for upload

--- modules/document_repository/ajax/documentDelete.php
+++ modules/document_repository/ajax/documentDelete.php
@@ -12,16 +12,16 @@
   */
 $user =& User::singleton();
 if (!$user->hasPermission('document_repository_delete')) {
-    header("HTTP/1.1 403 Forbidden");
+    header('HTTP/1.1 403 Forbidden');
     exit;
 }
 
-set_include_path(get_include_path().":../../project/libraries:../../php/libraries:");
-require_once "NDB_Client.class.inc";
-require_once "NDB_Config.class.inc";
-require_once "Email.class.inc";
+set_include_path(get_include_path().':../../project/libraries:../../php/libraries:');
+require_once 'NDB_Client.class.inc';
+require_once 'NDB_Config.class.inc';
+require_once 'Email.class.inc';
 $client = new NDB_Client();
-$client->initialize("../../project/config.xml");
+$client->initialize('../../project/config.xml');
 $factory = NDB_Factory::singleton();
 $baseURL = $factory->settings()->getBaseURL();
 
@@ -31,26 +31,26 @@ $config = NDB_Config::singleton();
 $DB =& Database::singleton();
 
 $Notifier = new NDB_Notifier(
-    "document_repository",
-    "delete"
+    'document_repository',
+    'delete'
 );
 
 $rid = $_POST['id'];
 
 $fileName = $DB->pselectOne(
-    "SELECT File_name FROM document_repository WHERE record_id =:identifier",
+    'SELECT File_name FROM document_repository WHERE record_id =:identifier',
     array(':identifier' => $rid)
 );
 $userName = $DB->pselectOne(
-    "SELECT uploaded_by FROM document_repository WHERE record_id =:identifier",
+    'SELECT uploaded_by FROM document_repository WHERE record_id =:identifier',
     array(':identifier' => $rid)
 );
 $dataDir  = $DB->pselectOne(
-    "SELECT Data_dir FROM document_repository WHERE record_id =:identifier",
+    'SELECT Data_dir FROM document_repository WHERE record_id =:identifier',
     array(':identifier' => $rid)
 );
 $version  = $DB->pselectOne(
-    "SELECT version FROM document_repository WHERE record_id =:identifier",
+    'SELECT version FROM document_repository WHERE record_id =:identifier',
     array(':identifier' => $rid)
 );
 
@@ -58,21 +58,21 @@ $user =& User::singleton();
 
 //if user has document repository delete permission
 if ($user->hasPermission('document_repository_delete')) {
-    $DB->delete("document_repository", array("record_id" => $rid));
-    $msg_data['deleteDocument'] = $baseURL. "/document_repository/";
+    $DB->delete('document_repository', array('record_id' => $rid));
+    $msg_data['deleteDocument'] = $baseURL. '/document_repository/';
     $msg_data['document']       = $fileName;
 
     $Notifier->notify($msg_data);
 }
 
-$path = __DIR__ . "/../user_uploads/$dataDir";
+$path = __DIR__ . '/../user_uploads/$dataDir';
 
 if (file_exists($path)) {
     unlink($path);
 }
 
-$rm_directory = __DIR__ . "/../user_uploads/" . $userName . "/"
-                        . str_replace(".", "_", $version);
+$rm_directory = __DIR__ . '/../user_uploads/' . $userName . '/'
+                        . str_replace('.', '_', $version);
 set_error_handler(
     function () {
         // Silence the E_WARNING when files exist in the directory.
--- modules/document_repository/ajax/documentEditUpload.php
+++ modules/document_repository/ajax/documentEditUpload.php
@@ -14,20 +14,20 @@ $userSingleton =& User::singleton();
 if (!$userSingleton->hasPermission('document_repository_view')
     && !$userSingleton->hasPermission('document_repository_delete')
 ) {
-    header("HTTP/1.1 403 Forbidden");
+    header('HTTP/1.1 403 Forbidden');
     exit;
 }
 
 set_include_path(
     get_include_path().
-    ":../../project/libraries:../../php/libraries:"
+    ':../../project/libraries:../../php/libraries:'
 );
-require_once "NDB_Client.class.inc";
-require_once "NDB_Config.class.inc";
-require_once "Email.class.inc";
+require_once 'NDB_Client.class.inc';
+require_once 'NDB_Config.class.inc';
+require_once 'Email.class.inc';
 
 $client = new NDB_Client();
-$client->initialize("../../project/config.xml");
+$client->initialize('../../project/config.xml');
 
 $factory = NDB_Factory::singleton();
 $baseURL = $factory->settings()->getBaseURL();
@@ -43,13 +43,13 @@ $db        = \Database::singleton(
 );
 
 $editNotifier = new NDB_Notifier(
-    "document_repository",
-    "edit"
+    'document_repository',
+    'edit'
 );
 
 $uploadNotifier = new NDB_Notifier(
-    "document_repository",
-    "upload"
+    'document_repository',
+    'upload'
 );
 
 $action = $_POST['action'];
@@ -66,42 +66,61 @@ if ($userSingleton->hasPermission('document_repository_view')
         $pscid      = $_POST['pscid']      !== '' ? $_POST['pscid'] : null;
         $visit      = $_POST['visit']      !== '' ? $_POST['visit'] : null;
         $comments   = $_POST['comments']   !== '' ? $_POST['comments'] : null;
+        $version    = $_POST['version']    !== '' ? $_POST['version'] : null;
+        $uuid       = uuid4();
 
-        $fileSize = $_FILES["file"]["size"];
-        $fileName = $_FILES["file"]["name"];
-        $fileType = end(explode(".", $fileName));
-
+        $fileSize = $_FILES['file']['size'];
+        $fileName = $_FILES['file']['name'];
+        $fileType = '';
+        // Handle retrieving the file type.
+        if (preg_match('/\./', $fileName)) {
+            $pos = strrpos($fileName, '.', -1);
+            if ($pos+1 != strlen($fileName)) {
+                $fileType = substr(
+                    $fileName,
+                    strrpos($fileName, '.', -1)+1
+                );
+            }
+        }
         $sql_statement = $db->prepare(
-            "SELECT File_name, version FROM document_repository "
-            ."WHERE File_name=? AND uploaded_by=?"
+            'SELECT File_name, version FROM document_repository '
+            .'WHERE File_name=? AND uploaded_by=?'
         );
         $sql_statement->bindParam(1, $fileName, PDO::PARAM_STR);
         $sql_statement->bindParam(2, $puser, PDO::PARAM_STR);
         $sql_statement->execute();
         $sql_result = $sql_statement->fetchAll(PDO::FETCH_ASSOC);
 
-        $version = versionInspectionGenerator($_POST['version'] ?? '', $sql_result);
-
-        $version_underscore = str_replace(".", "_", $version);
-
         // __DIR__ is the document_repository ajax directory
         // when this script is executing. Go up a level to the
         // document_repository module directory, and use a
         // user_uploads directory as a base for user uploads
-        $base_path = __DIR__ . "/../user_uploads/";
-        $fileBase  = $puser . "/" . $version_underscore . "/" . $fileName;
+        $base_path = __DIR__ . '/../user_uploads/';
+        $fileBase  = $puser . '/docs_repo/'
+            . $fileName
+            . '/' . $uuid
+            . '/' . $fileName;
 
+        // Create user directory /base_path/user
         if (!file_exists($base_path . $puser)) {
             mkdir($base_path . $puser, 0777);
         }
-
-        if (!file_exists($base_path . $puser . "/" . $version_underscore)) {
-            mkdir($base_path . $puser . "/" . $version_underscore, 0777);
+        // Create user directory /base_path/user/docs_repo
+        if (!file_exists($base_path . $puser . '/docs_repo')) {
+            mkdir($base_path . $puser . '/docs_repo', 0777);
+        }
+        // Create filename directory /base_path/user/docs_repo/fileName
+        if (!file_exists($base_path . $puser . '/docs_repo/' . $fileName)) {
+            mkdir($base_path . $puser . '/docs_repo/' . $fileName, 0777);
+        }
+        // Create uuid directory /base_path/user/docs_repo/fileName/uuid
+        if (!file_exists($base_path . $puser . '/docs_repo/' . $fileName . '/' . $uuid)) {
+            mkdir($base_path . $puser . '/docs_repo/' . $fileName . '/' . $uuid, 0777);
         }
 
         $target_path = $base_path . $fileBase;
 
-        if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_path)) {
+        if (move_uploaded_file($_FILES['file']['tmp_name'], $target_path)) {
             $success = $db->insert(
                 'document_repository',
                 array(
@@ -118,20 +137,19 @@ if ($userSingleton->hasPermission('document_repository_view')
                  'visitLabel'    => $visit,
                  'File_type'     => $fileType,
                  'version'       => $version,
+                 'uuid'          => $uuid,
                 )
             );
-            $msg_data['newDocument']
-                = $baseURL . "/document_repository/";
+            $msg_data['newDocument'] = $baseURL . '/document_repository/';
             $msg_data['document']    = $fileName;
 
             $uploadNotifier->notify($msg_data);
 
-            $header = "Location:".
-                      " $baseURL/document_repository/?uploadSuccess=true";
+            $header = 'Location: '.$baseURL
+                .'/document_repository/?uploadSuccess=true';
             header($header);
-
         } else {
-            echo "There was an error uploading the file";
+            echo 'There was an error uploading the file';
         }
     } elseif ($action == 'edit') {
         $id         = $_POST['idEdit'];
@@ -144,7 +162,7 @@ if ($userSingleton->hasPermission('document_repository_view')
         $version    = $_POST['versionEdit'];
 
         if (empty($category) && $category !== '0') {
-            header("HTTP/1.1 400 Bad Request");
+            header('HTTP/1.1 400 Bad Request');
             exit;
         }
 
@@ -160,10 +178,10 @@ if ($userSingleton->hasPermission('document_repository_view')
         $db->update('document_repository', $values, array('record_id' => $id));
 
         $fileName = $db->pselectOne(
-            "select File_name from document_repository where record_id=:record_id",
+            'SELECT File_name FROM document_repository WHERE record_id=:record_id',
             array('record_id' => $id)
         );
-        $msg_data['updatedDocument'] = $baseURL . "/document_repository/";
+        $msg_data['updatedDocument'] = $baseURL . '/document_repository/';
         $msg_data['document']        = $fileName;
 
         $editNotifier->notify($msg_data);
@@ -235,6 +253,28 @@ function versionUpdate($version, $increment)
         $new_version[count($new_version)-1]--;
     }
     return implode('.', $new_version);
+}
+
+/**
+ * Create a UUID v4 string.
+ *
+ * Source from comments:
+ * http://php.net/manual/en/function.com-create-guid.php
+ * Maybe move to Utilities class.
+ *
+ * @return string $version
+ */
+function uuid4()
+{
+    if (function_exists('com_create_guid') === true) {
+        // Windows Server
+        return trim(com_create_guid(), '{}');
+    }
+    // Linux Server
+    $data    = openssl_random_pseudo_bytes(16);
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
 }
 
 ?>
