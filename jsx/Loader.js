1e17db1fe34003761e46252d4dd07865cebfbaa3
Loris Admin <hrabalais.mcin@gmail.com>
2018-06-11 15:12:01 -0400

    Added Re-usable React Loader Component

---
+++ jsx/Loader.js
@@ -0,0 +1,23 @@
+/**
+ * This file contains the React component for Loader
+ *
+ * @author Henri Rabalais
+ * @version 1.0.0
+ *
+ */
+
+/**
+ * Loader component
+ */
+class Loader extends React.Component {
+
+  render() {
+    return (
+      <div className='loader-container'>
+        <div className="loader"/>
+      </div>
+    );
+  }
+}
+
+export default Loader;
