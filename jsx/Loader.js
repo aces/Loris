17abc613606c958f03887d2f73d60ec8f733f50e
Loris Admin <hrabalais.mcin@gmail.com>
2018-06-14 12:31:03 -0400

    Fixed Travis Errors

--- jsx/Loader.js
+++ jsx/Loader.js
@@ -13,7 +13,7 @@ class Loader extends React.Component {
 
   render() {
     return (
-      <div className='loader-container'>
+      <div className="loader-container">
         <div className="loader"/>
       </div>
     );
