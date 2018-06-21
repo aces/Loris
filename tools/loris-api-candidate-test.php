fd2abb5706cfc9dabadd914ce7e35c66b8a45604
John Saigle <john.saigle@gmail.com>
2018-06-21 18:55:48 -0400

    Add more candidate API test cases

--- tools/loris-api-candidate-test.php
+++ tools/loris-api-candidate-test.php
@@ -19,5 +19,32 @@ if (!$logged_in) {
     echo "Login failed!\n";
 }
 
-// Do basic testing of responses
-echo $api->lorisGET('candidates/');
+// Do basic testing
+
+$responses[] = $api->lorisGET('candidates/');
+$responses[] = $api->lorisGET('candidates/105657');
+$responses[] = $api->lorisGET('candidates/182999/V1');
+$responses[] = $api->lorisGET('candidates/182999/V1/instruments');
+
+
+$errors = checkForErrors($responses);
+if (count($errors) > 0) {
+    echo "The following errors occurred:" . PHP_EOL;
+    print_r($errors);
+    die;
+}
+echo "All tests went as expected. :)" . PHP_EOL;
+
+function checkForErrors($responses, $key = '') : Array {
+
+    if (is_null($responses)) {
+        throw new Exception("Only null responses received...");
+    }
+    $failures = [];
+    foreach($responses as $r) {
+        if (array_key_exists('error', json_decode($r))) {
+            $failures[] = $r;
+        }
+    }
+    return $failures;
+}
