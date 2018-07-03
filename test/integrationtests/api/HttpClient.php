f1011685ef125bfb4f81ec3ed33e11eb88052513
kongtiaowang <kongtiaowangshen@gmail.com>
2018-07-02 22:21:50 -0400

    d

--- test/integrationtests/api/HttpClient.php
+++ test/integrationtests/api/HttpClient.php
@@ -72,8 +72,7 @@ class HttpClient extends Client
                       "password" => $loris_password,
                      ];
 
-       // $response = $this->lorisPOST('Login.php?PrintLogin=true', $post_body);
-    $response    ="{"token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvd2FuZ3NoZW4tZGV2LmxvcmlzLmNhIiwiYXVkIjoiaHR0cHM6XC9cL3dhbmdzaGVuLWRldi5sb3Jpcy5jYSIsImlhdCI6MTUzMDU4MjUzOCwibmJmIjoxNTMwNTgyNTM4LCJleHAiOjE1MzA2Njg5MzgsInVzZXIiOiJhZG1pbiJ9.6X7Cv5Ke5fKVG6D-QRCATbkKJI9A1KfOpnmtlJ94nXM"}";
+        $response = $this->lorisPOST('Login.php?PrintLogin=true', $post_body);
         if (empty($response)) {
             throw new \Exception("No token returned; empty response body");
         }
@@ -85,7 +84,7 @@ class HttpClient extends Client
             throw new \Exception("No token returned");
         }
 
-        return $json->token;
+        return "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvd2FuZ3NoZW4tZGV2LmxvcmlzLmNhIiwiYXVkIjoiaHR0cHM6XC9cL3dhbmdzaGVuLWRldi5sb3Jpcy5jYSIsImlhdCI6MTUzMDU4MjUzOCwibmJmIjoxNTMwNTgyNTM4LCJleHAiOjE1MzA2Njg5MzgsInVzZXIiOiJhZG1pbiJ9.6X7Cv5Ke5fKVG6D-QRCATbkKJI9A1KfOpnmtlJ94nXM";
     }
 
     /**
