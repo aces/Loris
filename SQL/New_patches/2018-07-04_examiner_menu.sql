f6832853fa922aed43d478176c77b0728b4e56d6
Shen <kongtiaowang@users.noreply.github.com>
2018-07-04 13:45:59 -0400

    Update 2018-07-04_examiner_menu.sql

--- SQL/New_patches/2018-07-04_examiner_menu.sql
+++ SQL/New_patches/2018-07-04_examiner_menu.sql
@@ -1,2 +1,2 @@
-NSERT INTO LorisMenuPermissions (MenuID, PermID)
+INSERT INTO LorisMenuPermissions (MenuID, PermID)
     SELECT m.ID, p.PermID FROM permissions p CROSS JOIN LorisMenu m WHERE p.code='examiner_view' AND m.Label='Examiner';
