821dc869a318e2f2b08afec2bee1fefadbca8ee5
John Saigle <john.saigle@gmail.com>
2018-08-14 14:39:34 -0400

    Change ID to unsigned and change auto_increment

--- SQL/New_patches/2018_08_14_csrf_tokens.sql
+++ SQL/New_patches/2018_08_14_csrf_tokens.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `csrf_tokens` (
-  `CsrfTokenID` int(11) NOT NULL AUTO_INCREMENT,
+  `CsrfTokenID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `Token` varchar(64) NOT NULL,
   `CreatedAt` date NOT NULL,
   `PhpSessionID` varchar(30) NOT NULL,
   PRIMARY KEY (`CsrfTokenID`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
