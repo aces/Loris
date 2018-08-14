f254050cca6c11634c205fe67f0c724a8b7dbaa1
John Saigle <john.saigle@gmail.com>
2018-08-14 18:45:43 -0400

    Generic downloader. Needs work

--- modules/document_repository/ajax/GetFile.php
+++ modules/document_repository/ajax/GetFile.php
@@ -20,41 +20,12 @@ if (!$user->hasPermission('document_repository_view')
     header("HTTP/1.1 403 Forbidden");
     exit;
 }
-
-// Load config file and ensure paths are correct
-set_include_path(
-    get_include_path() . ":" .
-    __DIR__ . "/../project/libraries:" .
-    __DIR__ . "/../php/libraries"
-);
+require_once '../tools/generic_includes.php';
+require_once __DIR__ . '/../php/DocRepoFileDownloader.class.inc';
 
 $file = $_GET['File'];
 
-// Ensure file exists in the document_repository table before serving
-$db     =& Database::singleton();
-$record = $db->pselectOne(
-    "SELECT record_id FROM document_repository WHERE "
-    . "Data_dir=:dd",
-    array('dd' => $file)
-);
-
-if (empty($record)) {
-    error_log("ERROR: Invalid filename");
-    header("HTTP/1.1 400 Bad Request");
-    exit(4);
-}
-
-$path = __DIR__ . "/../user_uploads/$file";
-
-if (!file_exists($path)) {
-    error_log("ERROR: File $path does not exist");
-    header("HTTP/1.1 404 Not Found");
-    exit(5);
-}
-
-// Output file in downloadable format
-header('Content-Description: File Transfer');
-header('Content-Type: application/force-download');
-header("Content-Transfer-Encoding: Binary");
-header("Content-disposition: attachment; filename=\"" . basename($path) . "\"");
-readfile($path);
+$path = __DIR__ . "/../user_uploads/";
+$downloader = new DocRepoFileDownloader($path);
+$fileName = $downloader->getFileNameFromDatabase(intval($file));
+$downloader->downloadFile($path . $fileName);
