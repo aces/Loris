0d62aad467278d2efd54e4dabb74d907a186b453
John Saigle <john.saigle@gmail.com>
2018-08-15 15:14:07 -0400

    Generic file downloader proof-of-concept

--- modules/document_repository/ajax/GetFile.php
+++ modules/document_repository/ajax/GetFile.php
@@ -13,19 +13,33 @@
  *  @link     https://github.com/aces/Loris-Trunk
  */
 
-$user =& User::singleton();
+$user =& \User::singleton();
 if (!$user->hasPermission('document_repository_view')
     && !$user->hasPermission('document_repository_delete')
 ) {
     header("HTTP/1.1 403 Forbidden");
-    exit;
+    exit(1);
 }
 require_once '../tools/generic_includes.php';
+
+// Initialize downloader.
 require_once __DIR__ . '/../php/DocRepoFileDownloader.class.inc';
+$db =& \Database::singleton();
+$config = NDB_Config::singleton();
+$paths  = $config->getSetting('paths');
+$lorisRoot = $paths['base'];
 
-$file = $_GET['File'];
+$downloadBasePath = $lorisRoot . 'modules/document_repository/user_uploads/';
+$downloader = new DocRepoFileDownloader($downloadBasePath);
 
-$path = __DIR__ . "/../user_uploads/";
-$downloader = new DocRepoFileDownloader($path);
-$fileName = $downloader->getFileNameFromDatabase(intval($file));
-$downloader->downloadFile($path . $fileName);
+// Format: username/filename.ext
+$partialPath = $_GET['File'];
+
+// Verify file exists in the database.
+if (!$downloader->isFileInDatabase($partialPath)) {
+    error_log("ERROR Requested file is not in the database");
+    http_response_code(400);
+    exit(2);
+}
+// All generic verification and sanitization occurs here.
+$downloader->downloadFile($downloadBasePath . $partialPath);
