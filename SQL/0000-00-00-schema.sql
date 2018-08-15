0ac8ed0db694564b5eabe1e458f5e0d9417f5162
John Saigle <john.saigle@gmail.com>
2018-08-15 13:34:49 -0400

    Add CSRF token table to schema

--- SQL/0000-00-00-schema.sql
+++ SQL/0000-00-00-schema.sql
@@ -2041,3 +2041,15 @@ CREATE TABLE `candidate_consent_history` (
   `EntryStaff` varchar(255) DEFAULT NULL,
   CONSTRAINT `PK_candidate_consent_history` PRIMARY KEY (`CandidateConsentHistoryID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ********************************
+-- CSRF Token table
+-- ********************************
+
+CREATE TABLE `csrf_tokens` (
+  `CsrfTokenID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `Token` varchar(64) NOT NULL,
+  `CreatedAt` date NOT NULL,
+  `PhpSessionID` varchar(30) NOT NULL,
+  PRIMARY KEY (`CsrfTokenID`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
