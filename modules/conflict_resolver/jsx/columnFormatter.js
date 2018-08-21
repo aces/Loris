6e62244882983b92106e4275baba002255069d83
Alizée <intralizee@users.noreply.github.com>
2018-08-21 16:04:31 -0400

    react conflict resolver

--- modules/conflict_resolver/jsx/columnFormatter.js
+++ modules/conflict_resolver/jsx/columnFormatter.js
@@ -4,23 +4,23 @@
  * Modify behaviour of specified column cells in the Data Table component
  * @param {string} column - column name
  * @param {string} cell - cell content
- * @param {arrray} rowData - array of cell contents for a specific row
- * @param {arrray} rowHeaders - array of table headers (column names)
- * @return {*} a formated table cell for a given column
+ * @param {array} rowData - array of cell contents for a specific row
+ * @param {array} rowHeaders - array of table headers (column names)
+ * @return {*} a formatted table cell for a given column
  */
 function formatColumn(column, cell, rowData, rowHeaders) {
   if (loris.hiddenHeaders.indexOf(column) > -1) {
     return null;
   }
-
   // Create the mapping between rowHeaders and rowData in a row object.
-  var row = {};
+  let row = {};
+
   rowHeaders.forEach(function(header, index) {
     row[header] = rowData[index];
   }, this);
-  var value1;
-  var value2;
-  var hash;
+  let value1;
+  let value2;
+  let hash;
 
   if (column === 'Correct Answer') {
     value1 = row.Value1;
@@ -28,9 +28,9 @@ function formatColumn(column, cell, rowData, rowHeaders) {
     hash = row.Hash;
     return <td>
                 <select name={hash} className="form-control input-sm" >
-                     <option value="none" >Unresolved</option>
-                     <option value="1" >{value1}</option>
-                     <option value="2" >{value2}</option>
+                     <option value="none">Unresolved</option>
+                     <option value="1">{value1}</option>
+                     <option value="2">{value2}</option>
                 </select>
              </td>;
   }
--- modules/conflict_resolver/jsx/resolved_conflicts_columnFormatter.js
+++ modules/conflict_resolver/jsx/resolved_conflicts_columnFormatter.js
@@ -4,8 +4,8 @@
  * Modify behaviour of specified column cells in the Data Table component
  * @param {string} column - column name
  * @param {string} cell - cell content
- * @param {arrray} rowData - array of cell contents for a specific row
- * @param {arrray} rowHeaders - array of table headers (column names)
+ * @param {array} rowData - array of cell contents for a specific row
+ * @param {array} rowHeaders - array of table headers (column names)
  * @return {*} a formated table cell for a given column
  */
 function formatColumn(column, cell, rowData, rowHeaders) {
@@ -13,17 +13,17 @@ function formatColumn(column, cell, rowData, rowHeaders) {
     return null;
   }
   // Create the mapping between rowHeaders and rowData in a row object.
-  var row = {};
+  let row = {};
 
   rowHeaders.forEach(function(header, index) {
     row[header] = rowData[index];
   }, this);
 
   if (column === 'Correct Answer') {
-    var correctAnswer = '';
-    var newValue = row['New Value'];
-    var oldValue1 = row['Correct Answer'];
-    var oldValue2 = row.OldValue2;
+    let correctAnswer = '';
+    let newValue = row['New Value'];
+    let oldValue1 = row['Correct Answer'];
+    let oldValue2 = row.OldValue2;
 
     if (newValue === '1' && oldValue1 !== null) {
       correctAnswer = oldValue1;
