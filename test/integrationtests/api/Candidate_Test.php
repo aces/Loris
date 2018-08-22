e442ef6378a0400d3c790f1c9b642f7a24b982a6
xlecours <xavier.lecoursboucher@mcgill.ca>
2018-08-22 14:40:00 -0400

    setUp

--- test/integrationtests/api/Candidate_Test.php
+++ test/integrationtests/api/Candidate_Test.php
@@ -22,10 +22,35 @@ class Canditate_Test extends ApiTestCase
 
     public function setUp()
     {
+        $db = $this->factory->database();
+        $db->insert(
+            "candidate",
+            array(
+             'CandID'      => '900000',
+             'PSCID'       => 'TST0001',
+             'CenterID'    => 1,
+             'Active'      => 'Y',
+             'UserID'      => 1,
+             'Entity_type' => 'Human',
+            )
+        );
+        $db->insert(
+            'session',
+            array(
+             'ID'            => '999999',
+             'CandID'        => '900000',
+             'Visit_label'   => 'Test',
+             'CenterID'      => 1,
+             'Current_stage' => 'Not Started',
+            )
+        );
     }
 
     public function tearDown()
     {
+        $db = $this->factory->database();
+        $db->delete("session", array('CandID' => '900000'));
+        $db->delete("candidate", array('CandID' => '900000'));
     }
 
     public function testCandidatesGet()
@@ -33,14 +58,14 @@ class Canditate_Test extends ApiTestCase
         $response = $this->httpclient->lorisGET(
             'Candidates.php?PrintCandidates=true'
         );
-        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(200, $response->getStatusCode(), $response->getBody());
     }
 
     public function testCandidatesCandIdGet()
     {
         $response = $this->httpclient->lorisGET(
-            'candidates/Candidate.php?CandID=105657&PrintCandidate=true'
+            'candidates/Candidate.php?CandID=900000&PrintCandidate=true'
         );
-        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(200, $response->getStatusCode(), $response->getBody());
     }
 }
