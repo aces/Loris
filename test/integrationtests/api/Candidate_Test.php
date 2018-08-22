07aa6cf2febf2be068f53924a623bd42ab9432cb
xlecours <xavier.lecoursboucher@mcgill.ca>
2018-08-22 11:24:00 -0400

    get user list

--- test/integrationtests/api/Candidate_Test.php
+++ test/integrationtests/api/Candidate_Test.php
@@ -1,37 +1,47 @@
 <?php
 namespace LORIS\integrationtests;
 
-require_once('ApiTestCase.php');
+require_once 'ApiTestCase.php';
 
 class Canditate_Test extends ApiTestCase
 {
     public function __construct()
     {
         parent::__construct();
- 
-        $config   = $this->factory->Config(CONFIG_XML);
+
+        $config = $this->factory->Config(CONFIG_XML);
+        $db     = $this->factory->database();
+        $users  = $dp->pselect(
+            'SELECT * FROM users',
+            array()
+        );
+        var_dump($users);
 
         $api_credentials = $config->getSetting('api');
-        $token = $this->httpclient->getAuthorizationToken(
+        $token           = $this->httpclient->getAuthorizationToken(
             $api_credentials['username'],
             $api_credentials['password']
         );
- 
+
         $this->httpclient = $this->httpclient->withAuthorizationToken($token);
     }
 
-    public function setUp() {
+    public function setUp()
+    {
     }
 
-    public function tearDown() {
+    public function tearDown()
+    {
     }
 
-    public function testCandidatesGet() {
+    public function testCandidatesGet()
+    {
         $response = $this->httpclient->lorisGET('candidates/');
         $this->assertEquals(200, $response->getStatusCode());
     }
 
-    public function testCandidatesCandIdGet() {
+    public function testCandidatesCandIdGet()
+    {
         $response = $this->httpclient->lorisGET('candidates/105657');
         $this->assertEquals(200, $response->getStatusCode());
     }
