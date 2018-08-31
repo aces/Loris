f260270029242f9ef031508fbee379a2e9dfe675
MounaSafiHarab <mouna.safiharab@gmail.com>
2018-08-31 11:18:10 -0400

    made int 3 instead of int 11 and added the change to the patch as well not just schema

--- SQL/New_patches/2018_08_27_BIDSScanTypeTable.sql
+++ SQL/New_patches/2018_08_27_BIDSScanTypeTable.sql
@@ -2,12 +2,12 @@ DROP TABLE IF EXISTS bids_mri_scan_type_rel;
 DROP TABLE IF EXISTS bids_category;
 
 CREATE TABLE `bids_category` (
- `BIDSCategoryID` int(11) NOT NULL AUTO_INCREMENT,
+ `BIDSCategoryID` int(3) NOT NULL AUTO_INCREMENT,
  `ImagingCategory` varchar(10) NOT NULL,
  PRIMARY KEY (`BIDSCategoryID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `bids_category` VALUES
+INSERT INTO `bids_category` (ImagingCategory) VALUES
       ('anat'),
       ('func'),
       ('dwi'),
