6ec60c18a26a48ad15e67e389ec64c3e6ca92674
Alizée <intralizee@users.noreply.github.com>
2018-09-06 14:34:25 -0400

    undo

--- tools/single_use/Normalize_Consent_Data.php
+++ tools/single_use/Normalize_Consent_Data.php
@@ -121,7 +121,7 @@ foreach ($consentList as $consentName=>$consentLabel) {
         // Check if consent status is no and consent date given, but withdrawal date is empty
         if($status === "no" && !empty($date)) {
             if(empty($withdrawal)) {
-                array_push($errors, "The date of withdrawal is missing for " . $consentName . ":		
+                array_push($errors, "The date of withdrawal is missing for " . $consentName . ": 
                            [ID]     => " . $consentID . "		
                            [CandID] => " . $candID . "		
                            All 'no' statuses with given consent date must have a withdrawal date.");
