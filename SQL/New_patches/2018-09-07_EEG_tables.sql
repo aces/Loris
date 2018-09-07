102f0ff3f9c45ca4cf1e4ad50939843e0ffdf1c3
Cecile Madjar <cecile.madjar@gmail.com>
2018-09-07 14:41:59 -0400

    Adding the SQL patch for the EEG tables

---
+++ SQL/New_patches/2018-09-07_EEG_tables.sql
@@ -0,0 +1,259 @@
+-- Create a physiological_modality table
+CREATE TABLE `physiological_modality` (
+  `PhysiologicalModalityID` TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `PhysiologicalModality`   VARCHAR(50)      NOT NULL,
+  PRIMARY KEY (`PhysiologicalModalityID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- Create a physiological_output_type table
+CREATE TABLE `physiological_output_type` (
+  `PhysiologicalOutputTypeID` INT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `OutputType`                VARCHAR(20)     NOT NULL,
+  `OutputTypeDescription`     VARCHAR(255),
+  PRIMARY KEY (`PhysiologicalOutputTypeID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- Create a physiological_file table
+CREATE TABLE `physiological_file` (
+  `PhysiologicalFileID`       INT(10)    UNSIGNED  NOT NULL      AUTO_INCREMENT,
+  `PhysiologicalModalityID`   TINYINT(3) UNSIGNED  DEFAULT NULL,
+  `Caveat`                    TINYINT(1) UNSIGNED  DEFAULT 0,
+  `PhysiologicalOutputTypeID` INT(5)     UNSIGNED  NOT NULL,
+  `SessionID`                 INT(10)    UNSIGNED  NOT NULL,
+  `InsertTime`                INT(10)    UNSIGNED  NOT NULL,
+  `FileType`                  VARCHAR(12)          DEFAULT NULL,
+  `AcquisitionTime`           VARCHAR(20)          DEFAULT NULL,
+  `InsertedByUser`            VARCHAR(50)          NOT NULL,
+  `File`                      VARCHAR(255)         NOT NULL,
+  PRIMARY KEY (`PhysiologicalFileID`),
+  CONSTRAINT `FK_session_ID`
+    FOREIGN KEY (`SessionID`)
+    REFERENCES `session` (`ID`),
+  CONSTRAINT `FK_ImagingFileTypes_type`
+    FOREIGN KEY (`FileType`)
+    REFERENCES `ImagingFileTypes` (`type`),
+  CONSTRAINT `FK_physiological_modality_PhysiologicalModalityID`
+    FOREIGN KEY (`PhysiologicalModalityID`)
+    REFERENCES `physiological_modality` (`PhysiologicalModalityID`),
+  CONSTRAINT `FK_physiological_output_type_PhysiologicalOutputTypeID`
+    FOREIGN KEY (`PhysiologicalOutputTypeID`)
+    REFERENCES `physiological_output_type` (`PhysiologicalOutputTypeID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+-- Create a physiological_parameter_file table that will store all JSON
+-- information that accompanies the BIDS physiological dataset
+CREATE TABLE `physiological_parameter_file` (
+  `PhysiologicalParameterFileID` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `PhysiologicalFileID`          INT(10) UNSIGNED NOT NULL,
+  `ParameterTypeID`              INT(10) UNSIGNED NOT NULL,
+  `InsertTime`                   INT(10) UNSIGNED NOT NULL,
+  `Value`                        VARCHAR(255),
+  PRIMARY KEY (`PhysiologicalParameterFileID`),
+  CONSTRAINT `FK_physiological_file_PhysiologicalFileID`
+    FOREIGN KEY (`PhysiologicalFileID`)
+    REFERENCES `physiological_file` (`PhysiologicalFileID`)
+    ON DELETE CASCADE,
+  CONSTRAINT `FK_parameter_type_ParameterTypeID`
+    FOREIGN KEY (`ParameterTypeID`)
+    REFERENCES `parameter_type` (`ParameterTypeID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+-- Create a physiological_status_type table
+CREATE TABLE `physiological_status_type` (
+  `PhysiologicalStatusTypeID` TINYINT(2) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ChannelStatus`             VARCHAR(10)         NOT NULL,
+  PRIMARY KEY (`PhysiologicalStatusTypeID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+-- Create a physiological_channel_type table
+CREATE TABLE `physiological_channel_type` (
+  `PhysiologicalChannelTypeID` TINYINT(3) UNSIGNED NOT NULL      AUTO_INCREMENT,
+  `ChannelType`                VARCHAR(255)        NOT NULL,
+  `ChannelDescription`         VARCHAR(255)        DEFAULT NULL,
+  PRIMARY KEY (`PhysiologicalChannelTypeID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+-- Create a physiological_channel table that will store all information from
+CREATE TABLE `physiological_channel` (
+  `PhysiologicalChannelID`     INT(10) UNSIGNED    NOT NULL      AUTO_INCREMENT,
+  `PhysiologicalFileID`        INT(10) UNSIGNED    NOT NULL,
+  `PhysiologicalChannelTypeID` TINYINT(3) UNSIGNED NOT NULL,
+  `PhysiologicalStatusTypeID`  TINYINT(2) UNSIGNED DEFAULT NULL,
+  `Name`                       VARCHAR(50)         NOT NULL,
+  `Description`                VARCHAR(255)        DEFAULT NULL,
+  `SamplingFrequency`          INT(6)              DEFAULT NULL,
+  `LowCutoff`                  DECIMAL(8,3)        DEFAULT NULL,
+  `HighCutoff`                 DECIMAL(8,3)        DEFAULT NULL,
+  `ManualFlag`                 DECIMAL(5,4)        DEFAULT NULL,
+  `Notch`                      INT(6)              DEFAULT NULL,
+  `StatusDescription`          VARCHAR(255)        DEFAULT NULL,
+  `Unit`                       VARCHAR(255)        DEFAULT NULL,
+  `File`                       VARCHAR(255)        DEFAULT NULL,
+  `SoftwareFilters`            VARCHAR(255)        DEFAULT NULL,
+  PRIMARY KEY (`PhysiologicalChannelID`),
+  CONSTRAINT `FK_physiological_file_PhysiologicalFileID_2`
+    FOREIGN KEY (`PhysiologicalFileID`)
+    REFERENCES `physiological_file` (`PhysiologicalFileID`)
+    ON DELETE CASCADE,
+  CONSTRAINT `FK_physiological_channel_type_PhysiologicalChannelTypeID`
+    FOREIGN KEY (`PhysiologicalChannelTypeID`)
+    REFERENCES `physiological_channel_type` (`PhysiologicalChannelTypeID`),
+  CONSTRAINT `FK_physiological_status_type_PhysiologicalStatusTypeID`
+    FOREIGN KEY (`PhysiologicalStatusTypeID`)
+    REFERENCES `physiological_status_type` (`PhysiologicalStatusTypeID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+-- Create a physiological_electrode table
+CREATE TABLE `physiological_electrode` (
+  `PhysiologicalElectrodeID` INT(10) UNSIGNED NOT NULL      AUTO_INCREMENT,
+  `PhysiologicalFileID`      INT(10) UNSIGNED NOT NULL,
+  `Name`                     VARCHAR(50)      NOT NULL,
+  `X`                        DECIMAL(12,6)    NOT NULL,
+  `Y`                        DECIMAL(12,6)    NOT NULL,
+  `Z`                        DECIMAL(12,6)    NOT NULL,
+  `Type`                     VARCHAR(50)      NOT NULL,
+  `Material`                 VARCHAR(50)      NOT NULL,
+  `File`                     VARCHAR(255)     DEFAULT NULL,
+  PRIMARY KEY (`PhysiologicalElectrodeID`),
+  CONSTRAINT `FK_physiological_file_PhysiologicalFileID_3`
+    FOREIGN KEY (`PhysiologicalFileID`)
+    REFERENCES `physiological_file` (`PhysiologicalFileID`)
+    ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+-- Create physiological_task_event table that will store all information
+-- regarding the task executed during the physiological recording
+CREATE TABLE `physiological_task_event` (
+  `PhysiologicalTaskEventID` INT(10) UNSIGNED NOT NULL       AUTO_INCREMENT,
+  `PhysiologicalFileID`      INT(10) UNSIGNED NOT NULL,
+  `Onset`                    DECIMAL(11,6)    NOT NULL,
+  `Duration`                 DECIMAL(11,6)    NOT NULL,
+  `TrialType`                VARCHAR(255)     DEFAULT NULL,
+  `ResponseTime`             TIME             DEFAULT NULL,
+  `EventCode`                VARCHAR(255)     DEFAULT NULL,
+  `Sample`                   VARCHAR(255)     DEFAULT NULL,
+  `File`                     VARCHAR(255)     DEFAULT NULL,
+  PRIMARY KEY (`PhysiologicalTaskEventID`),
+  CONSTRAINT `FK_physiological_file_PhysiologicalFileID_4`
+    FOREIGN KEY (`PhysiologicalFileID`)
+    REFERENCES `physiological_file` (`PhysiologicalFileID`)
+    ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+-- Create physiological_archive which will store archives of all the files for
+-- Front-end download
+CREATE TABLE `physiological_archive` (
+  `PhysiologicalArchiveID`   INT(10) UNSIGNED NOT NULL       AUTO_INCREMENT,
+  `PhysiologicalFileID`      INT(10) UNSIGNED NOT NULL,
+  `InsertTime`               INT(10) UNSIGNED NOT NULL,
+  `Blake2bHash`              VARCHAR(128)     NOT NULL,
+  `File`                     VARCHAR(255)     NOT NULL,
+  PRIMARY KEY (`PhysiologicalArchiveID`),
+  CONSTRAINT `FK_physiological_file_PhysiologicalFileID_5`
+    FOREIGN KEY (`PhysiologicalFileID`)
+    REFERENCES `physiological_file` (`PhysiologicalFileID`)
+    ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- Insert into physiological_output_type
+INSERT INTO physiological_output_type
+  (`OutputType`, `OutputTypeDescription`)
+  VALUES
+  ('raw',         'raw dataset'),
+  ('derivatives', 'derivative/processed dataset');
+
+-- Insert into physiological_channel_type
+INSERT INTO physiological_channel_type
+  (`ChannelType`, `ChannelDescription`)
+  VALUES
+  ('EEG',              'ElectoEncephaloGram: EEG sensors'                    ),
+  ('VEOG',             'Vertical ElectroOculoGram (eyes)'                    ),
+  ('HEOG',             'Horizontal ElectOculoGram'                           ),
+  ('EOG',              'Generic EOG channel, if HEOG or VEOG information not available'),
+  ('ECG',              'ElectroCardioGram (heart)'                           ),
+  ('EMG',              'ElectroMyoGram (muscle)'                             ),
+  ('TRIG',             'System Triggers'                                     ),
+  ('REF',              'Reference electrode'                                 ),
+  ('GRD',              'Ground electrode'                                    ),
+  ('MISC',             'Miscellaneous'                                       ),
+  ('MEGMAG',           'MEG magnetometer'                                    ),
+  ('MEGGRADAXIAL',     'MEG axial gradiometer'                               ),
+  ('MEGGRADPLANAR',    'MEG planar gradiometer'                              ),
+  ('MEGGREFMAG',       'MEG reference magnetometer'                          ),
+  ('MEGREFGRADAXIAL',  'MEG reference axial gradiometer'                     ),
+  ('MEGREFGRADPLANAR', 'MEG reference planar gradiometer'                    ),
+  ('MEGOTHER',         'Any other type of MEG sensor'                        ),
+  ('ECOG',             'Electrode channel: electrocortigogram (intracranial)'),
+  ('SEEG',             'Electrode channel: stereo-electroencephalogram (intracranial)'),
+  ('DBS',              'Electrode channel: deep brain stimulation (intracranial)'),
+  ('AUDIO',            'Audio signal'                                        ),
+  ('PD',               'Photodiode'                                          ),
+  ('EYEGAZE',          'Eye Tracker gaze'                                    ),
+  ('PUPIL',            'Eye Tracker pupil diameter'                          ),
+  ('SYSCLOCK',         'System time showing elapsed time since trial started'),
+  ('ADC',              'Analog to Digital input'                             ),
+  ('DAC',              'Digital to Analog input'                             ),
+  ('HLU',              'Measure position of head and head coils'             ),
+  ('FITERR',           'Fit error signal from each head localization coil'   ),
+  ('OTHER',            'Any other type of channel'                           );
+
+
+-- Insert into physiological_modality
+INSERT INTO physiological_modality
+  (PhysiologicalModality)
+  VALUES
+  ('eeg'),
+  ('meg'),
+  ('ieeg');
+
+
+-- Insert into physiological_status_type
+INSERT INTO physiological_status_type
+  (ChannelStatus)
+  VALUES
+  ('good'),
+  ('bad');
+
+-- Insert into ImagingFileTypes
+INSERT INTO ImagingFileTypes
+  (type, description)
+  VALUES
+  ('set',  'EEGlab file format (EEG)'),
+  ('bdf',  'Biosemi file format (EEG)'),
+  ('vhdr', 'Brainvision file format (EEG)'),
+  ('vsm',  'BrainStorm file format (EEG)'),
+  ('edf',  'European data format (EEG)'),
+  ('cnt',  'Neuroscan CNT data format (EEG)');
+
+
+-- Insert into Config tables
+INSERT INTO ConfigSettings
+  (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber)
+  SELECT
+    'default_bids_vl',
+    'Default visit label to use when no visit label set in the BIDS dataset',
+    1,
+    0,
+    'text',
+    ID as parentID,
+    'Default visit label for BIDS dataset',
+    (SELECT MAX(OrderNumber) + 1 FROM ConfigSettings WHERE Parent=parentID)
+  FROM ConfigSettings
+  WHERE Name="imaging_pipeline";
