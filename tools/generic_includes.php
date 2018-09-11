9f84eb659a1355b5a72558a8ba7781a0b8885682
ridz1208 <ridz_1208@hotmail.com>
2018-09-11 09:26:56 -0400

    One script to rule them all

--- tools/generic_includes.php
+++ tools/generic_includes.php
@@ -25,19 +25,3 @@ $client->makeCommandLine();
 $client->initialize($configFile);
 $DB     = Database::singleton();
 $config = NDB_Config::singleton();
-
-/**
- * Helper function that writes a string supplied in the parameters to a filepath
- * specified in the parameters using the indicated mode. This function should be
- * used as a standard in the tools PHP scripts.
- *
- * @param string $filePath Desired path for the generated file (including extension)
- * @param string $output   String to be written to the file specified in $filePath
- * @param string $mode     Creation mode of the file
- */
-function writeToFile(string $filePath, string $output, string $mode = "w")
-{
-    $fp =fopen($filePath, $mode);
-    fwrite($fp, $output);
-    fclose($fp);
-}
