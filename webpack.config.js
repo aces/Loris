585198e134846cfe59227d2ecd404bf94f9380a7
Alizée <intralizee@users.noreply.github.com>
2018-09-18 12:30:42 -0400

    update

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,6 @@ const config = [{
     './htdocs/js/components/Breadcrumbs.js': './jsx/Breadcrumbs.js',
     './htdocs/js/components/Form.js': './jsx/Form.js',
     './htdocs/js/components/Markdown.js': './jsx/Markdown.js',
-    './modules/acknowledgements/js/react.acknowledgements.js': './modules/acknowledgements/jsx/react.acknowledgements.js',
     './modules/media/js/mediaIndex.js': './modules/media/jsx/mediaIndex.js',
     './modules/media/js/editFormIndex.js': './modules/media/jsx/editFormIndex.js',
     './modules/issue_tracker/js/columnFormatter.js': './modules/issue_tracker/jsx/columnFormatter.js',
