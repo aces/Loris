1db2956b83afecf5539c3ea06761afd2ecb1a4d3
Liza Levitis <llevitis.mcin@gmail.com>
2018-09-29 19:02:47 -0400

    same thing

--- tools/DB_date_zeros_removal.php
+++ tools/DB_date_zeros_removal.php
@@ -106,8 +106,8 @@ foreach ($field_names as $key=>$field)
             " SET ".$db->escape($field['COLUMN_NAME'])."=NULL".$autoUpdateSQL.
             " WHERE CAST(".$db->escape($field['COLUMN_NAME'])." AS CHAR(20))='0000-00-00 00:00:00';\n";
     } else {
-      echo "COLUMN ".$field['COLUMN_NAME']." in TABLE ".$field['TABLE_NAME']." is NOT NULLABLE. ".
-          "A date '1000-01-01' will be entered instead of '0000-00-00' values.\n";
+  echo "COLUMN ".$field['COLUMN_NAME']." in TABLE ".$field['TABLE_NAME']." is NOT NULLABLE. ".
+      "A date '1000-01-01' will be entered instead of '0000-00-00' values.\n";
         $nonNullUpdates .= "UPDATE ".$db->escape($database['database']).".".$db->escape($field['TABLE_NAME']).
             " SET ".$db->escape($field['COLUMN_NAME'])."='1000-01-01'".$autoUpdateSQL.
             " WHERE CAST(".$db->escape($field['COLUMN_NAME'])." AS CHAR(20))='0000-00-00';\n";
