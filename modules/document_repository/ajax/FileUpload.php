c558c987f482c51bff34a56df5aad895aeff7d2c
kongtiaowang <kongtiaowangshen@gmail.com>
2018-10-01 12:57:37 -0400

    phpcs

--- modules/document_repository/ajax/FileUpload.php
+++ modules/document_repository/ajax/FileUpload.php
@@ -45,8 +45,8 @@ function editFile()
     }
 
     // Read JSON from STDIN
-    $stdin       = file_get_contents('php://input');
-    $req         = json_decode($stdin, true);
+    $stdin     = file_get_contents('php://input');
+    $req       = json_decode($stdin, true);
     $idDocFile = $req['idDocFile'];
 
     if (!$idDocFile) {
@@ -81,35 +81,33 @@ function editFile()
  */
 function uploadFile()
 {
-$editNotifier = new NDB_Notifier(
-    "document_repository",
-    "edit"
-);
-$uploadNotifier = new NDB_Notifier(
-    "document_repository",
-    "upload"
-);
-$userSingleton =& \User::singleton();
-if (!$userSingleton->hasPermission('document_repository_view')
-    && !$userSingleton->hasPermission('document_repository_delete')
-) {
-    http_response_code(403);
-    exit;
-}
+    $editNotifier   = new NDB_Notifier(
+        "document_repository",
+        "edit"
+    );
+    $uploadNotifier = new NDB_Notifier(
+        "document_repository",
+        "upload"
+    );
+    $userSingleton  =& \User::singleton();
+    if (!$userSingleton->hasPermission('document_repository_view')
+        && !$userSingleton->hasPermission('document_repository_delete')
+    ) {
+        http_response_code(403);
+        exit;
+    }
 
-$factory = \NDB_Factory::singleton();
-$baseURL = $factory->settings()->getBaseURL();
-$config = $factory->config();
-$base   = $config->getSetting('base');
-$name   = $userSingleton->getUsername();
-$DB = $factory->database();
+    $factory = \NDB_Factory::singleton();
+    $baseURL = $factory->settings()->getBaseURL();
+    $config  = $factory->config();
+    $base    = $config->getSetting('base');
+    $name    = $userSingleton->getUsername();
+    $DB      = $factory->database();
 
-
-
-//if user has document repository permission
-//if ($userSingleton->hasPermission('document_repository_view')
-//    || $userSingleton->hasPermission('document_repository_delete')
-//) {
+    //if user has document repository permission
+    //if ($userSingleton->hasPermission('document_repository_view')
+    //    || $userSingleton->hasPermission('document_repository_delete')
+    //) {
         $category   = $_POST['category']; // required
         $site       = $_POST['forSite']       !== '' ? $_POST['forSite'] : null;
         $instrument = $_POST['instrument'] !== '' ? $_POST['instrument'] : null;
@@ -117,70 +115,70 @@ $DB = $factory->database();
         $visit      = $_POST['visitLabel']      !== '' ? $_POST['visitLabel'] : null;
         $comments   = $_POST['comments']   !== '' ? $_POST['comments'] : null;
         $version    = $_POST['version']    !== '' ? $_POST['version'] : null;
-        $fileSize = $_FILES["file"]["size"];
-        $fileName = $_FILES["file"]["name"];
-        $fileType = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
+        $fileSize   = $_FILES["file"]["size"];
+        $fileName   = $_FILES["file"]["name"];
+        $fileType   = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
         $uploadPath = "$base/modules/document_repository/user_uploads/$name/";
         $fullPath   = $uploadPath . $fileName;
         // $category is a string representation of an ID, and so should be at
         // least equal to zero.
-        if (intval($category) < 0) {
-            throw new LorisException(
-                "'Category' parameter must be a positive integer."
-            );
-        }
+    if (intval($category) < 0) {
+        throw new LorisException(
+            "'Category' parameter must be a positive integer."
+        );
+    }
         // Check to see if $fullPath is writable. If not, throw an error. If it
         // doesn't exist, create an uploads folder for the logged-in user.
-        if (!is_writable($fullPath)) {
-            if (file_exists($fullPath)) {
-                throw new LorisException(
-                    "User uploads path in Document Repository is not writable."
-                );
-            }
-            mkdir($fullPath, 0770);
+    if (!is_writable($fullPath)) {
+        if (file_exists($fullPath)) {
+            throw new LorisException(
+                "User uploads path in Document Repository is not writable."
+            );
         }
+        mkdir($fullPath, 0770);
+    }
         // Copy the uploaded file to the user's upload folder if possible.
         // Insert a record of the file into the document_repository table
-        if (!move_uploaded_file(
-            $_FILES['file']['tmp_name'],
-            $fullPath . $fileName
-        )) {
-            throw new LorisException(
-                'ERROR: Could not upload file. Contact your administrator.'
-            );
-        } else {
-            $success = $DB->insert(
-                'document_repository',
-                array(
-                 'File_category' => $category,
-                 'For_site'      => $site,
-                 'comments'      => $comments,
-                 'version'       => $version,
-                 'File_name'     => $fileName,
-                 'File_size'     => $fileSize,
-                 'Data_dir'      => "$name/$fileName", // e.g. 'admin/file.png'
-                 'uploaded_by'   => $name,
-                 'Instrument'    => $instrument,
-                 'PSCID'         => $pscid,
-                 'visitLabel'    => $visit,
-                 'File_type'     => $fileType,
-                )
-            );
-            $msg_data['newDocument']
-                = $baseURL . "/document_repository/";
-            $msg_data['document']    = $fileName;
-            $uploadNotifier->notify($msg_data);
-            http_response_code(303);
-            header('Location:' . $baseURL . '/document_repository/');
-        }
+    if (!move_uploaded_file(
+        $_FILES['file']['tmp_name'],
+        $fullPath . $fileName
+    )) {
+        throw new LorisException(
+            'ERROR: Could not upload file. Contact your administrator.'
+        );
+    } else {
+        $success = $DB->insert(
+            'document_repository',
+            array(
+             'File_category' => $category,
+             'For_site'      => $site,
+             'comments'      => $comments,
+             'version'       => $version,
+             'File_name'     => $fileName,
+             'File_size'     => $fileSize,
+             'Data_dir'      => "$name/$fileName", // e.g. 'admin/file.png'
+             'uploaded_by'   => $name,
+             'Instrument'    => $instrument,
+             'PSCID'         => $pscid,
+             'visitLabel'    => $visit,
+             'File_type'     => $fileType,
+            )
+        );
+        $msg_data['newDocument']
+            = $baseURL . "/document_repository/";
+        $msg_data['document']    = $fileName;
+        $uploadNotifier->notify($msg_data);
+        http_response_code(303);
+        header('Location:' . $baseURL . '/document_repository/');
+    }
 }
 
 function uploadCategory()
 {
     $DB =& Database::singleton();
-        $category_name   = $_POST['category_name']; // required
-        $parent_id  = $_POST['parent_id']  !== '' ? $_POST['parent_id'] : null;
-        $comment    = $_POST['comments']    !== '' ? $_POST['comments'] : null;
+        $category_name = $_POST['category_name']; // required
+        $parent_id     = $_POST['parent_id']  !== '' ? $_POST['parent_id'] : null;
+        $comment       = $_POST['comments']    !== '' ? $_POST['comments'] : null;
     $DB->insert(
         "document_repository_categories",
         array(
@@ -197,7 +195,7 @@ function viewCategory()
         header("HTTP/1.1 403 Forbidden");
         exit;
     }
-    echo json_encode(getCategoryFields());   
+    echo json_encode(getCategoryFields());
 }
 /**
  * Returns a list of fields from database
@@ -207,22 +205,20 @@ function viewCategory()
  */
 function getCategoryFields()
 {
-    $db =& Database::singleton();
-$query = $db->pselect(
-    "SELECT * FROM document_repository_categories",
-    array()
-);
-//categories
-$categoriesList = array();
+    $db    =& Database::singleton();
+    $query = $db->pselect(
+        "SELECT * FROM document_repository_categories",
+        array()
+    );
+    //categories
+    $categoriesList = array();
 
-foreach ($query as $value) {
-       $arr = parseCategory($value);
-       $categoriesList[$arr['id']]=$arr['name'];
-}
+    foreach ($query as $value) {
+           $arr = parseCategory($value);
+           $categoriesList[$arr['id']] =$arr['name'];
+    }
 
-    $result = [
-               'categories'  => $categoriesList,
-              ];
+    $result = ['categories' => $categoriesList];
 
     return $result;
 }
@@ -246,28 +242,28 @@ function viewData()
 function getUploadFields()
 {
 
-    $db =& Database::singleton();
-$query = $db->pselect(
-    "SELECT * FROM document_repository_categories",
-    array()
-);
-//categories
-$categoriesList = array();
+    $db    =& Database::singleton();
+    $query = $db->pselect(
+        "SELECT * FROM document_repository_categories",
+        array()
+    );
+    //categories
+    $categoriesList = array();
 
-foreach ($query as $value) {
-       $arr = parseCategory($value);
-       $categoriesList[$arr['id']]=$arr['name'];
-}
-//site
-$siteList        = Utility::getSiteList(false);
-//instrument
+    foreach ($query as $value) {
+           $arr = parseCategory($value);
+           $categoriesList[$arr['id']] =$arr['name'];
+    }
+    //site
+    $siteList = Utility::getSiteList(false);
+    //instrument
 
-    $instruments = $db->pselect(
+    $instruments     = $db->pselect(
         "SELECT Test_name FROM test_names ORDER BY Test_name",
         []
     );
- $instrumentsList = toSelect($instruments, "Test_name", null);
-//docFile
+    $instrumentsList = toSelect($instruments, "Test_name", null);
+    //docFile
     $docData = null;
     if (isset($_GET['idDocFile'])) {
         $idDocFile = $_GET['idDocFile'];
@@ -288,40 +284,39 @@ $siteList        = Utility::getSiteList(false);
         );
     }
 
-
-
-
-
     $result = [
                'categories'  => $categoriesList,
                'sites'       => $siteList,
                'instruments' => $instrumentsList,
-               'docData'   => $docData,
+               'docData'     => $docData,
               ];
 
     return $result;
 }
 function parseCategory($value)
-    {
-        $id = $value['id'];
+{
+        $id    = $value['id'];
         $depth = 0;
         $DB    = \Database::singleton();
             $categoryName = $value['category_name'];
-        do {
-            if ($value['parent_id'] != 0) {
-                $depth       += 1;
-                $parent_id    = $value['parent_id'];
-                $query        = "SELECT * FROM document_repository_categories".
-                                " where id=$parent_id";
-                $value        = $DB->pselectRow($query, array());
-                $categoryName = $value['category_name'] . ">" . $categoryName;
-            }
-        } while ($value['parent_id'] != 0);
-//try to return array{{name:"ddd",id:'1'}{}}
-//        return $categoryName;
-//          return $id;
-         return array("name"=>$categoryName,"id"=>$id);
-    }
+    do {
+        if ($value['parent_id'] != 0) {
+            $depth       += 1;
+            $parent_id    = $value['parent_id'];
+            $query        = "SELECT * FROM document_repository_categories".
+                            " where id=$parent_id";
+            $value        = $DB->pselectRow($query, array());
+            $categoryName = $value['category_name'] . ">" . $categoryName;
+        }
+    } while ($value['parent_id'] != 0);
+    //try to return array{{name:"ddd",id:'1'}{}}
+    //        return $categoryName;
+    //          return $id;
+         return array(
+                 "name" => $categoryName,
+                 "id"   => $id,
+                );
+}
 
 /**
  * Utility function to return errors from the server
