0099e288a9ac620a0fa6f6ddc55aecdd4d01766e
Loris Admin <hrabalais.mcin@gmail.com>
2018-10-04 13:18:14 -0400

    Added necessary loris.hiddenHeaders array

--- modules/dicom_archive/jsx/dicom_archive.js
+++ modules/dicom_archive/jsx/dicom_archive.js
@@ -45,6 +45,9 @@ class DicomArchive extends React.Component {
       method: 'GET',
       dataType: 'json',
       success: data => {
+        // FIXME: Remove the following line of code as soon as hiddenHeaders is
+        // accepted as a prop by the StaticDataTable Component.
+        loris.hiddenHeaders = data.hiddenHeaders || [];
         this.setState({
           data: data,
           isLoaded: true
