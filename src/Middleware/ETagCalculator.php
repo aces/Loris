3c17c2356f4437e01dffac8698312680e77f45aa
Dave MacFarlane <driusan@gmail.com>
2018-10-15 13:31:02 -0400

    [API] Convert API /login and /projects endpoint to LORIS module  (#3993)
    This adds a new API module to replace the ad-hoc PHP
    scripts and modrewrite rules which currently make up the
    LORIS API. The module currently handled the login and
    projects endpoints only, but does so for all versions of the
    API rather than having a separate handler for each.
    
    The mod_rewrite rules are updated such that the login
    endpoint is delegated to the module, while other endpoints
    fall back on the older implementations to help the transition.
    
    It also adds a PSR15 ETag middleware component that can
    be used for any requests.

---
+++ src/Middleware/ETagCalculator.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Defines the ETagCalculator interface.
+ *
+ * PHP Version 7
+ *
+ * @category Router
+ * @package  Middleware
+ * @author   Dave MacFarlane <david.macfarlane2@mcgill.ca>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link     https://www.github.com/aces/Loris/
+ */
+
+namespace LORIS\Middleware;
+use \Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * An ETagCalculator provides the ability to calculate an ETag for
+ * an incoming HTTP request.
+ *
+ * @category Router
+ * @package  Middleware
+ * @author   Dave MacFarlane <david.macfarlane2@mcgill.ca>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link     https://www.github.com/aces/Loris/
+ */
+interface ETagCalculator {
+    /**
+     * An ETagCalculator provides the ability to calculate an ETag for
+     * an incoming HTTP request.
+     *
+     * @param ServerRequestInterface  $request The incoming PSR7 request.
+     *
+     * @return string The value to use for the ETag header.
+     */
+    public function ETag(ServerRequestInterface $request) : string;
+}
