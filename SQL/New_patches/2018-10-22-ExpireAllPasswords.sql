082f04f91c0831d94d9fb75d1bf58cb3b37c13a3
John Saigle <john.saigle@gmail.com>
2018-10-22 15:46:05 -0400

    Remove old password hash records from history

--- SQL/New_patches/2018-10-22-ExpireAllPasswords.sql
+++ SQL/New_patches/2018-10-22-ExpireAllPasswords.sql
@@ -1,3 +1,6 @@
 -- This forces a password reset for all users the next time they login. Before
 -- this patch, all passwords changed by the front-end were stored in plaintext.
 UPDATE users SET Password_expiry='1990-01-01';
+-- Remove old plaintext passwords. Also affects old history entries when the
+-- column was named `hash` instead of `Password_hash`
+DELETE FROM history WHERE col LIKE "%hash%" AND tbl='users';
