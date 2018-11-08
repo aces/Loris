1f89a3eba659738330211ec3672e8ddf91a47851
kongtiaowang <kongtiaowangshen@gmail.com>
2018-11-08 16:00:55 -0500

    try

---
+++ modules/new_profile/jsx/newIndex.js
@@ -0,0 +1,18 @@
+import NewProfileForm from './NewProfileForm';
+ class NewIndex extends React.Component {
+ render() {
+         return (
+           <div>
+              <NewProfileForm />
+           </div>
+         );
+ }
+}
+ $(function() {
+  const newIndex = (
+    <div className="page-document">
+      <NewIndex />
+    </div>
+  );
+   ReactDOM.render(newIndex, document.getElementById('lorisworkspace'));
+});
