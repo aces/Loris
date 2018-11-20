4a898e092c46a17b475afd7a40158be316beda6c
kongtiaowang <kongtiaowangshen@gmail.com>
2018-11-20 14:47:12 -0500

    phpcs

--- modules/dicom_archive/test/dicom_archiveTest.php
+++ modules/dicom_archive/test/dicom_archiveTest.php
@@ -27,14 +27,14 @@ require_once __DIR__
 class DicomArchiveTestIntegrationTest extends LorisIntegrationTest
 {
     //$location: css selector for react items
-    static $patientID   = "#dicom_filter_filter > div > div:nth-child(1) > div > div > input";
-    static $patientName = "#dicom_filter_filter > div > div:nth-child(2) > div > div > input";
-    static $site        = "#dicom_filter_filter > div > div:nth-child(8) > div > div > select";
-    static $gender      = "#dicom_filter_filter > div > div:nth-child(3) > div > div > input";
-    static $dateOfBirth = "#dicom_filter_filter > div > div:nth-child(4) > div > div > input";
-    static $clearFilter = "#dicom_filter_filter > div > div:nth-child(9) > div > div > button";
+    static $patientID   = "#dicom_filter_filter>div>div:nth-child(1)>div>div>input";
+    static $patientName = "#dicom_filter_filter>div>div:nth-child(2)>div>div>input";
+    static $site        = "#dicom_filter_filter>div>div:nth-child(8)>div>div>select";
+    static $gender      = "#dicom_filter_filter>div>div:nth-child(3)>div>div>input";
+    static $dateOfBirth = "#dicom_filter_filter>div>div:nth-child(4)>div>div>input";
+    static $clearFilter = "#dicom_filter_filter>div>div:nth-child(9)>div>div>button";
     // first row of react table
-    static $table       = "#dynamictable > tbody > tr:nth-child(1)";
+    static $table = "#dynamictable > tbody > tr:nth-child(1)";
     /**
      * Insert testing data into the database
      *
@@ -95,17 +95,22 @@ class DicomArchiveTestIntegrationTest extends LorisIntegrationTest
     {
         $this->safeGet($this->url . "/dicom_archive/");
         //testing data from RBdata.sql
-        $this-> _testFilter(self::$patientID,self::$table,null,"ibis");
-        $this-> _testFilter(self::$patientName,self::$table,null,"MTL022_300022_V1");
-        $this-> _testFilter(self::$gender,self::$table,null, "M");
-        $this-> _testFilter(self::$dateOfBirth,self::$table,null,"1972-10-10");
-        $this-> _testFilter(self::$site,self::$table, "4","4");
+        $this-> _testFilter(self::$patientID, self::$table, null, "ibis");
+        $this-> _testFilter(
+            self::$patientName,
+            self::$table,
+            null,
+            "MTL022_300022_V1"
+        );
+        $this-> _testFilter(self::$gender, self::$table, null, "M");
+        $this-> _testFilter(self::$dateOfBirth, self::$table, null, "1972-10-10");
+        $this-> _testFilter(self::$site, self::$table, "4", "4");
     }
     /**
-     * testing filter funtion and clear button
+     * Testing filter funtion and clear button
      *
      * @param string $element The input element loaction
-     * @param string $table   The first row location in the table 
+     * @param string $table   The first row location in the table
      * @param string $records The records number in the table
      * @param string $value   The test value
      *
@@ -114,8 +119,8 @@ class DicomArchiveTestIntegrationTest extends LorisIntegrationTest
     function _testFilter($element,$table,$records,$value)
     {
         // get element from the page
-        if (strpos($element,"select") == false){
-        $this->webDriver->executescript(
+        if (strpos($element, "select") == false) {
+            $this->webDriver->executescript(
                 "input = document.querySelector('$element');
                  lastValue = input.value;
                  input.value = '$value';
@@ -124,41 +129,46 @@ class DicomArchiveTestIntegrationTest extends LorisIntegrationTest
                  input.dispatchEvent(event);
                 "
             );
-        $bodyText = $this->webDriver->executescript(
-            "return document.querySelector('$table').textContent"
-        );
-        $this->assertContains($value, $bodyText);
+            $bodyText = $this->webDriver->executescript(
+                "return document.querySelector('$table').textContent"
+            );
+            $this->assertContains($value, $bodyText);
         } else {
-        $this->webDriver->executescript(
+            $this->webDriver->executescript(
                 "input = document.querySelector('$element');
                  input.selectedIndex = '$value';
                  event = new Event('change', { bubbles: true });
                  input.dispatchEvent(event);
                 "
-            );        
-        $bodyText = $this->webDriver->executescript(
-            "return document.querySelector('#lorisworkspace > div >".
-            " div.panel.panel-default > div.table-header.panel-heading".
-            " > div > div').textContent"
-        );  
-        // 4 means there are 4 records under this site. 
-        $this->assertContains($records, $bodyText);
-        } 
+            );
+            $bodyText = $this->webDriver->executescript(
+                "return document.querySelector('#lorisworkspace > div >".
+                " div.panel.panel-default > div.table-header.panel-heading".
+                " > div > div').textContent"
+            );
+            // 4 means there are 4 records under this site.
+            $this->assertContains($records, $bodyText);
+        }
         //test clear filter
         $btn = self::$clearFilter;
         $this->webDriver->executescript(
-                "document.querySelector('$btn').click();"
-            );
+            "document.querySelector('$btn').click();"
+        );
         $inputText = $this->webDriver->executescript(
             "return document.querySelector('$element').value"
         );
-        $this->assertEquals("",$inputText);
-       
+        $this->assertEquals("", $inputText);
+
     }
+    /**
+     * Tests that the (view-details) link works
+     *
+     * @return void
+     */
     function testLinksViewDetails()
     {
         $this->safeGet($this->url . "/dicom_archive/");
-        $location = "#dynamictable > tbody > tr:nth-child(1) > td:nth-child(8) > a";
+        $location = "#dynamictable>tbody>tr:nth-child(1)>td:nth-child(8)>a";
         $text     = $this->webDriver->executescript(
             "return document.querySelector('$location').textContent"
         );
