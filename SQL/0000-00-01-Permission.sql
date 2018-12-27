84217df1af13d1ca03d6a7226dca50d27ce5628b
Justin Kat <601027+Jkat@users.noreply.github.com>
2018-12-27 18:47:23 -0500

    [Data Release] Improvements (#3062)
    New feature in Data Release module whereby you can manage permissions on a larger scale.
    Also it breaks less for MySQL 5.7, and also it's more secure.

--- SQL/0000-00-01-Permission.sql
+++ SQL/0000-00-01-Permission.sql
@@ -109,7 +109,9 @@ INSERT INTO `permissions` VALUES
     (45,'issue_tracker_reporter', 'Can add a new issue, edit own issue, comment on all', 2),
     (46,'issue_tracker_developer', 'Can re-assign issues, mark issues as closed, comment on all, edit issues.', 2),
     (47,'imaging_browser_phantom_allsites', 'Can access only phantom data from all sites in Imaging Browser', 2),
-    (48,'imaging_browser_phantom_ownsite', 'Can access only phantom data from own site in Imaging Browser', 2);
+    (48,'imaging_browser_phantom_ownsite', 'Can access only phantom data from own site in Imaging Browser', 2),
+    (49,'data_release_upload', 'Data Release: Upload file', 2),
+    (50,'data_release_edit_file_access', 'Data Release: Grant other users view-file permissions', 2);
 
 
 INSERT INTO `user_perm_rel` (userID, permID)
