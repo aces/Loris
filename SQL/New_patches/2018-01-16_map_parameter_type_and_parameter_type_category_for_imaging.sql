08eda648b20bda9810d1b4af9755d3449a6a0ce5
Cecile Madjar <cecile.madjar@gmail.com>
2019-01-17 12:03:32 -0500

    Dave's feedback

--- SQL/New_patches/2018-01-16_map_parameter_type_and_parameter_type_category_for_imaging.sql
+++ SQL/New_patches/2018-01-16_map_parameter_type_and_parameter_type_category_for_imaging.sql
@@ -1,5 +1,5 @@
 INSERT INTO parameter_type_category_rel (ParameterTypeID, ParameterTypeCategoryID)
-  SELECT ParameterTypeID, ParameterTypeCategoryID
+  SELECT DISTINCT ParameterTypeID, ParameterTypeCategoryID
   FROM   parameter_type, parameter_type_category ptc
   WHERE  SourceFrom="parameter_file" AND ptc.Name="MRI Variables";
 
@@ -8,7 +8,7 @@ INSERT INTO parameter_type_category (Name, Type)
   VALUES ('Electrophysiology Variables', 'Metavars');
 
 INSERT INTO parameter_type_category_rel (ParameterTypeID, ParameterTypeCategoryID)
-  SELECT ParameterTypeID, ParameterTypeCategoryID
+  SELECT DISTINCT ParameterTypeID, ParameterTypeCategoryID
   FROM   parameter_type, parameter_type_category ptc
   WHERE  SourceFrom="physiological_parameter_file" AND ptc.Name="Electrophysiology Variables";
 
