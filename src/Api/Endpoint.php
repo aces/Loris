a416d0c6f3a16e409404579143b940333f98ec75
xlecours <xavier.lecoursboucher@mcgill.ca>
2019-01-22 10:49:16 -0500

    Delegating api-version and method checks to sub-endpoints

--- src/Api/Endpoint.php
+++ src/Api/Endpoint.php
@@ -60,24 +60,16 @@ abstract class Endpoint implements RequestHandlerInterface
         ServerRequestInterface $request,
         RequestHandlerInterface $handler
     ): ResponseInterface {
-        $methods = $this->allowedMethods();
-        if (!in_array($request->getMethod(), $methods)) {
-            return new \LORIS\Http\Response\MethodNotAllowed(
-                $methods
-            );
-        }
-
         $versions = $this->supportedVersions() ?? [];
         $version  = $request->getAttribute("LORIS-API-Version") ?? "unknown";
-
         if (!in_array($version, $versions)) {
-            return new \LORIS\Http\Response\BadRequest(
-                'Unsupported LORIS API version'
-            );
+            return new \LORIS\Http\Response\BadRequest('unsupported version');
         }
+
         if ($handler instanceof \LORIS\Middleware\ETagCalculator) {
             return (new \LORIS\Middleware\ETag())->process($request, $handler);
         }
+
         return $handler->handle($request);
     }
 }
