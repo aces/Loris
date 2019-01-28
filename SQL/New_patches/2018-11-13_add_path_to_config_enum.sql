5e33d3cf550447ffd7475e6e6b2d234052dc6ef7
John Saigle <john.saigle@gmail.com>
2019-01-28 12:58:09 -0500

    Cecile's feedback

--- SQL/New_patches/2018-11-13_add_path_to_config_enum.sql
+++ SQL/New_patches/2018-11-13_add_path_to_config_enum.sql
@@ -4,5 +4,5 @@
 -- will be validated when configured from the front-end and will throw errors
 -- if not reachable by the server.
 ALTER TABLE ConfigSettings MODIFY COLUMN DataType enum('text','boolean','email','instrument','textarea','scan_type','lookup_center','path','web_path');
-UPDATE ConfigSettings SET DataType='web_path' where Name IN ('imagePath', 'base', 'data', 'extLibs', 'mincPath', 'DownloadPath', 'IncomingPath', 'MRICodePath', 'MRIUploadIncomingPath', 'GenomicDataPath', 'mediaPath');
-UPDATE ConfigSettings SET DataType='path' WHERE Name='log';
+UPDATE ConfigSettings SET DataType='web_path' where Name IN ('imagePath', 'base', 'data', 'extLibs', 'mincPath', 'DownloadPath', 'IncomingPath', 'MRICodePath', 'MRIUploadIncomingPath', 'GenomicDataPath', 'mediaPath', 'dataDirBasepath');
+UPDATE ConfigSettings SET DataType='path' WHERE Name IN ('log', 'MRICodePath', 'tarchiveLibraryDir', 'get_dicom_info', 'niak_path');
