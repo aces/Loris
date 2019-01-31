cbe460de6077693792b6b1bd085bb66d3717d562
xlecours <xavier.lecoursboucher@mcgill.ca>
2019-01-31 14:12:28 -0500

    /candidates/$candid/$visit_label/images

---
+++ src/Api/Endpoints/Candidate/Visit/Images.php
@@ -0,0 +1,148 @@
+<?php declare(strict_types=1);
+/**
+ * This implements the visit's images endpoint class
+ *
+ * PHP Version 7
+ *
+ * @category API
+ * @package  Loris
+ * @author   Xavier Lecours Boucher <xavier.lecours@mcin.ca>
+ * @license  Loris license
+ * @link     https://github.com/aces/Loris
+ */
+namespace LORIS\Api\Endpoints\Candidate\Visit;
+
+use \Psr\Http\Message\ServerRequestInterface;
+use \Psr\Http\Message\ResponseInterface;
+use \LORIS\Api\Endpoint;
+
+/**
+ * A class for handling request for a visit dicoms.
+ *
+ * @category API
+ * @package  Loris
+ * @author   Xavier Lecours Boucher <xavier.lecours@mcin.ca>
+ * @license  Loris license
+ * @link     https://github.com/aces/Loris
+ */
+class Images extends Endpoint implements \LORIS\Middleware\ETagCalculator
+{
+    /**
+     * The requested Visit
+     *
+     * @var \Timepoint
+     */
+    protected $visit;
+
+    /**
+     * Contructor
+     *
+     * @param \Timepoint $visit The requested visit
+     */
+    public function __construct(\Timepoint $visit)
+    {
+        $this->visit = $visit;
+    }
+
+    /**
+     * Return which methods are supported by this endpoint.
+     *
+     * @return array supported HTTP methods
+     */
+    protected function allowedMethods() : array
+    {
+        return array('GET');
+    }
+
+    /**
+     * Versions of the LORIS API which are supported by this
+     * endpoint.
+     *
+     * @return array a list of supported API versions.
+     */
+    protected function supportedVersions() : array
+    {
+        return array(
+                'v0.0.2',
+                'v0.0.3-dev',
+               );
+    }
+
+    /**
+     * Handles a request that starts with /candidates/$candid
+     *
+     * @param ServerRequestInterface $request The incoming PSR7 request
+     *
+     * @return ResponseInterface The outgoing PSR7 response
+     */
+    public function handle(ServerRequestInterface $request) : ResponseInterface
+    {
+        $pathparts = $request->getAttribute('pathparts');
+        if (count($pathparts) === 0) {
+            switch ($request->getMethod()) {
+            case 'GET':
+                return $this->_handleGET($request);
+
+            case 'OPTIONS':
+                return (new \LORIS\Http\Response())
+                    ->withHeader('Allow', $this->allowedMethods());
+
+            default:
+                return new \LORIS\Http\Response\MethodNotAllowed(
+                    $this->allowedMethods()
+                );
+            }
+        }
+
+        // Delegate to sub-endpoints
+        $filename   = array_shift($pathparts);
+        $newrequest = $request
+            ->withAttribute('pathparts', $pathparts);
+
+        $handler = new Image\Image(
+            $this->visit,
+            $filename
+        );
+
+        return $handler->process(
+            $newrequest,
+            $handler
+        );
+    }
+
+    /**
+     * Create an array representation of this endpoint's reponse body
+     *
+     * @param ServerRequestInterface $request The incoming PSR7 request
+     *
+     * @return ResponseInterface The outgoing PSR7 response
+     */
+    private function _handleGET(ServerRequestInterface $request): ResponseInterface
+    {
+        if (!isset($this->cache)) {
+            $images = $this->visit->getImages(
+                $request->getAttribute('user')
+            );
+
+            $view = (new \LORIS\Api\Views\Visit\Images(
+                $this->visit,
+                ...$images
+            ))->toArray();
+
+            $this->cache = new \LORIS\Http\Response\JsonResponse($view);
+        }
+        return $this->cache;
+    }
+
+    /**
+     * Implements the ETagCalculator interface
+     *
+     * @param ServerRequestInterface $request The PSR7 incoming request.
+     *
+     * @return string etag summarizing value of this request.
+     */
+    public function ETag(ServerRequestInterface $request) : string
+    {
+        return md5(json_encode($this->_handleGET($request)->getBody()));
+    }
+}
