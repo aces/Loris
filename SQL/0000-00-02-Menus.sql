d562b22906cd570b7bd6545878dd7cc70ef5a7d5
John Saigle <4022790+johnsaigle@users.noreply.github.com>
2019-02-04 16:22:35 -0500

    [Instrument Manager] Add more specific permissions to instrument_manager (#4191)
    Add instrument_manager_read and instrument_manager_write permissions to instrument manager module, in order to see what instruments are installed or to install a new one.
    
    Previously, the superuser was required for both.

--- SQL/0000-00-02-Menus.sql
+++ SQL/0000-00-02-Menus.sql
@@ -188,7 +188,9 @@ INSERT INTO LorisMenuPermissions (MenuID, PermID)
 
 
 INSERT INTO LorisMenuPermissions (MenuID, PermID)
-    SELECT m.ID, p.PermID FROM permissions p CROSS JOIN LorisMenu m WHERE p.code='superuser' AND m.Label='Instrument Manager';
+   SELECT m.ID, p.PermID FROM permissions p CROSS JOIN LorisMenu m WHERE p.code='instrument_manager_read' AND m.Label='Instrument Manager';
+INSERT INTO LorisMenuPermissions (MenuID, PermID)
+   SELECT m.ID, p.PermID FROM permissions p CROSS JOIN LorisMenu m WHERE p.code='instrument_manager_write' AND m.Label='Instrument Manager';
 
 
 INSERT INTO LorisMenuPermissions (MenuID, PermID)
@@ -217,5 +219,7 @@ INSERT INTO LorisMenuPermissions (MenuID, PermID)
    SELECT m.ID, p.PermID FROM permissions p CROSS JOIN LorisMenu m WHERE p.code='media_read' AND m.Label='Media';
 INSERT INTO LorisMenuPermissions (MenuID, PermID)
    SELECT m.ID, p.PermID FROM permissions p CROSS JOIN LorisMenu m WHERE p.code='media_write' AND m.Label='Media';
+
+
 
 
