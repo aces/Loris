f4812b9d297b077e933c20b6b824f1ae4c7a2e48
Xavier Lecours <xavier.lecours@mcin.ca>
2019-02-04 11:20:02 -0500

    [Core] Adding CandID Value Object (#4055)
    This adds a CandID value object with unit test and a simple usage in the RestAPI.
    
    The goal is to start using objects instead of array or scalar values to migrate towards a typed dependency injection pattern. This will facilitate unit tests and further developments.

---
+++ test/unittests/studyentities/CandID_Test.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * This file contains unit test for the CandID value object.
+ *
+ * PHP Version 7
+ *
+ * @category Tests
+ * @package  StudyEntities
+ * @author   Xavier Lecours <xavier.lecours@mcin.ca>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link     https://www.github.com/aces/Loris/
+ */
+namespace LORIS\StudyEntitites\Candidate\Test;
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+use \PHPUnit\Framework\TestCase;
+use \LORIS\StudyEntities\Candidate\CandID;
+
+/**
+ * Unit test class for the CandID value object
+ *
+ * PHP Version 7
+ *
+ * @category Tests
+ * @package  StudyEntities
+ * @author   Xavier Lecours <xavier.lecours@mcin.ca>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link     https://www.github.com/aces/Loris/
+ */
+class CandID_Test extends TestCase
+{
+    /*
+     * dataProviders for constructor invalid values
+     */
+    public function invalidValues(): array
+    {
+        return array(
+                ['A'],
+                [1],
+                [000000],
+                ['11111a'],
+                [' 11111'],
+               );
+    }
+
+    /**
+     * @dataProvider invalidValues
+     * @expectedException \DomainException
+     */
+    public function testContructorInvalidValues($invalidValue): void
+    {
+        $candid = new CandID($invalidValue);
+    }
+
+    public function testGetType(): void
+    {
+        $candid = new CandID(123456);
+        $this->assertEquals('CandID', $candid->getType());
+    }
+
+    public function testToString(): void
+    {
+        $candid = new CandID(123456);
+        $this->assertEquals('123456', (string) $candid);
+    }
+}
+
