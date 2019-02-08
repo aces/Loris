9a9c623dba72bb1c71d245831643601bc57d17b8
David Blader <dblader.mcin@gmail.com>
2019-02-08 15:37:50 -0500

    continued

--- SQL/Archive/20.2/2018-11-23_insert_imaging_non_minc_file_insertion_in_notification_types.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO notification_types
-  (Type, private, Description)
-  VALUES
-  ('imaging non minc file insertion', 1, 'Insertion of a non-MINC file into the MRI tables (files/parameter_file)');
-
-UPDATE notification_types
-  SET Description = 'Insertion of a MINC file into the MRI tables (files/parameter_file)'
-  WHERE Type = 'minc insertion';
