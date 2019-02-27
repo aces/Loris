81a40cfed949edb2cc46f82e52963a9159ad8394
John Saigle <john.saigle@gmail.com>
2019-02-27 17:03:29 -0500

    Revert bvl_feedback changes and add rule

--- .phan/config.php
+++ .phan/config.php
@@ -56,4 +56,14 @@ return [
         // because Phan disables xdebug by default.
         'xdebug'     => 'vendor/phan/phan/.phan/internal_stubs/xdebug.phan_php',
     ],
+
+    // The line below is required to prevent PhanUndeclaredVariable problems in
+    // the bvl_feedback ajax scripts. They all require() a file in this directory
+    // that declares the $feedbackThread variable. Phan doesn't know about this.
+    //
+    // Toggling the below rule to true is the suggested fix by the phan devs.
+    // see https://github.com/phan/phan/issues/1650.
+    //
+    // When the module is refactored, this line should be deleted.
+    'ignore_undeclared_variables_in_global_scope' => true
 ];
