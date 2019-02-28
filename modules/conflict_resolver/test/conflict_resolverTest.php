ff67c4fb913ef2ab60298c23a683f4ddf7cfe5de
kongtiaowang <kongtiaowangshen@gmail.com>
2019-02-28 14:05:47 -0500

    phpcs

--- modules/conflict_resolver/test/conflict_resolverTest.php
+++ modules/conflict_resolver/test/conflict_resolverTest.php
@@ -25,23 +25,23 @@
 class ConflictResolverTestIntegrationTest extends LorisIntegrationTest
 {
     //filter location on conflict_resolver page
-    static $ForSite        = ".col-xs-12:nth-child(1) .form-control, [select]";
-    static $Instrument     = ".col-xs-12:nth-child(2) .form-control, [select]";
-    static $VisitLabel     = ".col-xs-12:nth-child(3) .form-control, [select]";
-    static $CandID         = ".col-xs-12:nth-child(4) .form-control";
-    static $PSCID          = ".col-xs-12:nth-child(5) .form-control";
-    static $Question       = ".col-xs-12:nth-child(6) .form-control";
-    static $Project        = ".col-xs-12:nth-child(7) .form-control, [select]";
+    static $ForSite    = ".col-xs-12:nth-child(1) .form-control, [select]";
+    static $Instrument = ".col-xs-12:nth-child(2) .form-control, [select]";
+    static $VisitLabel = ".col-xs-12:nth-child(3) .form-control, [select]";
+    static $CandID     = ".col-xs-12:nth-child(4) .form-control";
+    static $PSCID      = ".col-xs-12:nth-child(5) .form-control";
+    static $Question   = ".col-xs-12:nth-child(6) .form-control";
+    static $Project    = ".col-xs-12:nth-child(7) .form-control, [select]";
 
     //filter location on resolved_conflicts page
-    static $Timestamp      = ".col-xs-12:nth-child(7) .form-control";
-    static $Project_RC     = ".col-xs-12:nth-child(8) .form-control, [select]";
+    static $Timestamp  = ".col-xs-12:nth-child(7) .form-control";
+    static $Project_RC = ".col-xs-12:nth-child(8) .form-control, [select]";
 
     //public location for both pages
-    static $clearFilter    = ".col-sm-9 > .btn";
-    static $display        = ".table-header .col-xs-12";
-    static $saveBtn        = ".btn-sm:nth-child(1)";
-    static $resetBtn       = ".btn-sm:nth-child(2)";
+    static $clearFilter = ".col-sm-9 > .btn";
+    static $display     = ".table-header .col-xs-12";
+    static $saveBtn     = ".btn-sm:nth-child(1)";
+    static $resetBtn    = ".btn-sm:nth-child(2)";
     /**
      * Insert testing data into the database
      * author: Wang Shen
@@ -52,6 +52,39 @@ class ConflictResolverTestIntegrationTest extends LorisIntegrationTest
     {
         parent::setUp();
         $this->setUpConfigSetting("useProjects", "true");
+        $this->DB->insert(
+            "conflicts_resolved",
+            array(
+             'ResolvedID'          => '999999',
+             'UserID'              => 'demo',
+             'ResolutionTimestamp' => '2015-11-03 16:21:49',
+             'User1'               => 'Null',
+             'User2'               => 'Null',
+             'TableName'           => 'Test',
+             'ExtraKey1'           => 'NULL',
+             'ExtraKey2'           => 'NULL',
+             'FieldName'           => 'TestTestTest',
+             'CommentId1'          => '589569DCC000012291366553230',
+             'CommentId2'          => 'DDE_589569DCC000012291366653254',
+             'OldValue1'           => 'Mother',
+             'OldValue2'           => 'Father',
+             'NewValue'            => 'NULL',
+            )
+        );
+         $this->DB->insert(
+             "conflicts_unresolved",
+             array(
+              'TableName'      => 'TestTestTest',
+              'ExtraKeyColumn' => 'Test',
+              'ExtraKey1'      => 'Null',
+              'ExtraKey2'      => 'Null',
+              'FieldName'      => 'TestTestTest',
+              'CommentId1'     => '963443000111271151398976899',
+              'Value1'         => 'no',
+              'CommentId2'     => 'DDE_963443000111271151398976899',
+              'Value2'         => 'no',
+             )
+         );
     }
      /**
      * Delete testing data from database
@@ -63,6 +96,11 @@ class ConflictResolverTestIntegrationTest extends LorisIntegrationTest
     {
         parent::tearDown();
         $this->restoreConfigSetting("useProjects");
+        $this->DB->delete("conflicts_resolved", array('ResolvedID' => '999999'));
+        $this->DB->delete(
+            "conflicts_unresolved",
+            array('TableName' => 'TestTestTest')
+        );
     }
 
      /**
@@ -84,7 +122,7 @@ class ConflictResolverTestIntegrationTest extends LorisIntegrationTest
             "Resolved Conflicts",
             $bodyText
         );
-        
+
         $this->resetPermissions();
     }
 
@@ -132,8 +170,8 @@ class ConflictResolverTestIntegrationTest extends LorisIntegrationTest
     {
         $this->setUpConfigSetting("useProjects", "true");
         $this->safeGet($this->url . "/conflict_resolver/");
-       
-        //testing data 
+
+        //testing data
         $this-> _testFilter(self::$ForSite, "20 rows displayed of 319", '2');
         $this-> _testFilter(self::$VisitLabel, "displayed of 581", '1');
         $this-> _testFilter(self::$CandID, "8 rows displayed of 8", '300001');
@@ -220,22 +258,22 @@ class ConflictResolverTestIntegrationTest extends LorisIntegrationTest
          $element = "tr:nth-child(1) .form-control";
          $value   = "2";
          $btn     = self::$saveBtn;
-         $row     = self::$display; 
+         $row     = self::$display;
          $this->webDriver->executescript(
-                "input = document.querySelector('$element');
+             "input = document.querySelector('$element');
                  input.selectedIndex = '$value';
                 "
-            );        
+         );
          $this->webDriver->executescript(
-                "document.querySelector('$btn').click()"
+             "document.querySelector('$btn').click()"
          );sleep(1);
          //todo find this
          $bodyText = $this->webDriver->executescript(
-                "return document.querySelector('$row').textContent"
-            );
+             "return document.querySelector('$row').textContent"
+         );
             // 4 means there are 4 records under this site.
-         $this->assertContains("of 584", $bodyText); 
-         
+         $this->assertContains("of 584", $bodyText);
+
     }
 }
 
