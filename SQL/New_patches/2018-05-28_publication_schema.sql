624bd56646a53b2aef3898c9f8751014218da2fd
Rida Abou-Haidar <ridz1208@users.noreply.github.com>
2019-03-09 15:35:05 -0500

    fix ordering

--- SQL/New_patches/2018-05-28_publication_schema.sql
+++ SQL/New_patches/2018-05-28_publication_schema.sql
@@ -3,10 +3,18 @@ CREATE TABLE `publication_status` (
   `PublicationStatusID` int(2) unsigned NOT NULL AUTO_INCREMENT,
   `Label` varchar(255) NOT NULL,
   CONSTRAINT `PK_publication_status` PRIMARY KEY(`PublicationStatusID`)
-) ENGINE=InnoDB DEFAULT CHARSET='utf8mb4';
+) ENGINE=InnoDB DEFAULT CHARSET='utf8';
 INSERT INTO publication_status (`Label`) VALUES ('Pending');
 INSERT INTO publication_status (`Label`) VALUES ('Approved');
 INSERT INTO publication_status (`Label`) VALUES ('Rejected');
+
+CREATE TABLE `publication_collaborator` (
+  `PublicationCollaboratorID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) NOT NULL,
+  `Email` varchar(255),
+  CONSTRAINT `PK_publication_collaborator` PRIMARY KEY(`PublicationCollaboratorID`),
+  CONSTRAINT `UK_publication_collaborator_Email` UNIQUE (`Email`)
+) ENGINE=InnoDB DEFAULT CHARSET='utf8';
 
 -- Main table
 CREATE TABLE `publication` (
@@ -26,7 +34,7 @@ CREATE TABLE `publication` (
     CONSTRAINT `FK_publication_PublicationStatusID` FOREIGN KEY(`PublicationStatusID`) REFERENCES `publication_status` (`PublicationStatusID`),
     CONSTRAINT `FK_publication_LeadInvestigatorID` FOREIGN KEY(`LeadInvestigatorID`) REFERENCES `publication_collaborator` (`PublicationCollaboratorID`),
     CONSTRAINT `UK_publication_Title` UNIQUE (`Title`)
-) ENGINE=InnoDB DEFAULT CHARSET='utf8mb4';
+) ENGINE=InnoDB DEFAULT CHARSET='utf8';
 
 -- Separate table for Keywords
 CREATE TABLE `publication_keyword` (
@@ -34,7 +42,7 @@ CREATE TABLE `publication_keyword` (
   `Label` varchar(255) NOT NULL,
   CONSTRAINT `PK_publication_keyword` PRIMARY KEY(`PublicationKeywordID`),
   CONSTRAINT `UK_publication_Label` UNIQUE (`Label`)
-) ENGINE=InnoDB DEFAULT CHARSET='utf8mb4';
+) ENGINE=InnoDB DEFAULT CHARSET='utf8';
 
 -- Publication - Keyword relational table
 CREATE TABLE `publication_keyword_rel` (
@@ -43,16 +51,7 @@ CREATE TABLE `publication_keyword_rel` (
   CONSTRAINT `PK_publication_keyword_rel` PRIMARY KEY(PublicationID, PublicationKeywordID),
   CONSTRAINT `FK_publication_keyword_PublicationID` FOREIGN KEY(`PublicationID`) REFERENCES `publication` (`PublicationID`),
   CONSTRAINT `FK_publication_keyword_PublicationKeywordID` FOREIGN KEY(`PublicationKeywordID`) REFERENCES `publication_keyword` (`PublicationKeywordID`)
-) ENGINE=InnoDB DEFAULT CHARSET='utf8mb4';
-
-
-CREATE TABLE `publication_collaborator` (
-  `PublicationCollaboratorID` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `Name` varchar(255) NOT NULL,
-  `Email` varchar(255),
-  CONSTRAINT `PK_publication_collaborator` PRIMARY KEY(`PublicationCollaboratorID`),
-  CONSTRAINT `UK_publication_collaborator_Email` UNIQUE (`Email`)
-) ENGINE=InnoDB DEFAULT CHARSET='utf8mb4';
+) ENGINE=InnoDB DEFAULT CHARSET='utf8';
 
 CREATE TABLE `publication_collaborator_rel` (
   `PublicationID` int(10) unsigned NOT NULL,
@@ -60,7 +59,7 @@ CREATE TABLE `publication_collaborator_rel` (
   CONSTRAINT `PK_publication_collaborator_rel` PRIMARY KEY(PublicationID, PublicationCollaboratorID),
   CONSTRAINT `FK_publication_collaborator_rel_PublicationID` FOREIGN KEY(`PublicationID`) REFERENCES `publication` (`PublicationID`),
   CONSTRAINT `FK_publication_collaborator_rel_PublicationCollaboratorID` FOREIGN KEY(`PublicationCollaboratorID`) REFERENCES `publication_collaborator` (`PublicationCollaboratorID`)
-) ENGINE=InnoDB DEFAULT CHARSET='utf8mb4';
+) ENGINE=InnoDB DEFAULT CHARSET='utf8';
 
 -- Publication - Variable of Interest  relational table
 CREATE TABLE `publication_parameter_type_rel` (
@@ -69,7 +68,7 @@ CREATE TABLE `publication_parameter_type_rel` (
     CONSTRAINT `PK_publication_parameter_type_rel` PRIMARY KEY (PublicationID, ParameterTypeID),
     CONSTRAINT `FK_publication_parameter_type_rel_PublicationID` FOREIGN KEY (`PublicationID`) REFERENCES `publication` (`PublicationID`),
     CONSTRAINT `FK_publication_parameter_type_rel_ParameterTypeID` FOREIGN KEY (`ParameterTypeID`) REFERENCES `parameter_type` (`ParameterTypeID`)
-) ENGINE=InnoDB DEFAULT CHARSET='utf8mb4';
+) ENGINE=InnoDB DEFAULT CHARSET='utf8';
 
 CREATE TABLE `publication_test_names_rel` (
     `PublicationID` int(10) unsigned NOT NULL,
@@ -85,7 +84,7 @@ CREATE TABLE `publication_upload_type` (
   `Label` varchar(255) NOT NULL,
   CONSTRAINT `PK_publication_upload_type` PRIMARY KEY (`PublicationUploadTypeID`),
   CONSTRAINT `UK_publication_Label` UNIQUE (`Label`)
-) ENGINE=InnoDB DEFAULT CHARSET='utf8mb4';
+) ENGINE=InnoDB DEFAULT CHARSET='utf8';
 
 INSERT INTO publication_upload_type (`Label`) VALUES ('Paper');
 INSERT INTO publication_upload_type (`Label`) VALUES ('Poster');
@@ -103,14 +102,14 @@ CREATE TABLE `publication_upload` (
     CONSTRAINT `UK_publication_upload_Filename` UNIQUE (Filename),
     CONSTRAINT `FK_publication_upload_PublicationID` FOREIGN KEY (`PublicationID`) REFERENCES `publication` (`PublicationID`),
     CONSTRAINT `FK_publication_upload_PublicationUploadTypeID` FOREIGN KEY (`PublicationUploadTypeID`) REFERENCES `publication_upload_type` (`PublicationUploadTypeID`)
-) ENGINE=InnoDB DEFAULT CHARSET='utf8mb4';
+) ENGINE=InnoDB DEFAULT CHARSET='utf8';
 
 CREATE TABLE `publication_users_edit_perm_rel` (
   `PublicationID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `UserID` int(10) unsigned NOT NULL,
   CONSTRAINT `FK_publication_users_edit_perm_rel_PublicationID` FOREIGN KEY (`PublicationID`) REFERENCES `publication` (`PublicationID`),
   CONSTRAINT `FK_publication_users_edit_perm_rel_UserID` FOREIGN KEY (`UserID`) REFERENCES `users` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET='utf8mb4';
+) ENGINE=InnoDB DEFAULT CHARSET='utf8';
 
 SET @reportsTab = (SELECT ID FROM LorisMenu WHERE Label='Reports');
 SET @orderNum = (SELECT MAX(OrderNumber) + 1 FROM LorisMenu WHERE Parent=@reportsTab);
