bbc6c9330f5e72bd306c48903cafa361cb798440
Alizée Bellerose <misotheism@users.noreply.github.com>
2019-03-12 10:43:28 -0400

    update

--- modules/login/jsx/loginIndex.js
+++ modules/login/jsx/loginIndex.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Loader from 'Loader';
 import Panel from 'Panel';
+import ResetPassword from './resetPassword';
+import RequestAccount from './requestAccount';
 
 /**
  * Login form.
@@ -17,10 +19,33 @@ class Login extends Component {
     super(props);
     this.state = {
       url: '',
+<<<<<<< HEAD
+=======
+      study: {
+        logo: '',
+        title: '',
+        links: [],
+        description: '',
+      },
+      form: {
+        value: {
+          username: '',
+          password: '',
+        },
+        error: '',
+      },
+      mode: 'login',
+>>>>>>> update
       isLoaded: false,
     };
     // Bind component instance to custom methods
     this.fetchInitializerData = this.fetchInitializerData.bind(this);
+<<<<<<< HEAD
+=======
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.setForm = this.setForm.bind(this);
+    this.setMode = this.setMode.bind(this);
+>>>>>>> update
   }
   /**
    * Executes after component mounts.
@@ -66,13 +91,63 @@ class Login extends Component {
       .then(
         (data) => {
           console.log('success');
+<<<<<<< HEAD
           this.setState({isLoaded: true});
+=======
+          console.log(data);
+          const state = Object.assign({}, this.state);
+          state.study.description = data.study_description;
+          state.study.title = data.study_title;
+          this.state.study.logo = window.location.origin
+            + '/' + data.study_logo;
+          state.isLoaded = true;
+          this.setState(state);
+>>>>>>> update
         }).catch((error) => {
           console.log('error: ');
           console.log(error);
     });
   }
   /**
+<<<<<<< HEAD
+=======
+   * Set the form data based on state values of child elements/components
+   *
+   * @param {string} formElement - name of the selected element
+   * @param {string} value - selected value for corresponding form element
+   */
+  setForm(formElement, value) {
+    const state = Object.assign({}, this.state);
+    state.form.value[formElement] = value;
+    this.setState(state);
+  }
+  /**
+   * Handle form submission
+   *
+   * @param {object} e - Form submission event
+   */
+  handleSubmit(e) {
+    let form = document.getElementById('form');
+    let inputLogin = document.createElement('input');
+    inputLogin.setAttribute('name', 'login');
+    inputLogin.setAttribute('value', 'true');
+    inputLogin.setAttribute('type', 'hidden');
+    form.append(inputLogin);
+    form.submit();
+  }
+  /**
+   * Set mode.
+   *
+   * @param {string} mode - set as mode.
+   */
+  setMode(mode) {
+    console.log(mode);
+    const state = Object.assign({}, this.state);
+    state.mode = mode;
+    this.setState(state);
+  }
+  /**
+>>>>>>> update
    * @return {DOMRect}
    */
   render() {
@@ -80,6 +155,7 @@ class Login extends Component {
     if (!this.state.isLoaded) {
       return <Loader/>;
     }
+<<<<<<< HEAD
     return (
       <Panel
         title={'Login to LORIS'}
@@ -89,6 +165,102 @@ class Login extends Component {
         <div>test</div>
       </Panel>
     );
+=======
+    if (this.state.mode === 'login') {
+      const study = (
+        <div dangerouslySetInnerHTML={{__html: this.state.study.description}}/>
+      );
+      const login = (
+        <div>
+          <section className={'study-logo'}>
+            <img src={this.state.study.logo}
+                 alt={this.state.study.title}/>
+          </section>
+          <FormElement
+            name={'login'}
+            action={'col-sm-12'}
+            id={'form'}
+            fileUpload={'false'}
+            onSubmit={this.handleSubmit}
+          >
+            <TextboxElement
+              name={'username'}
+              value={this.state.form.value.username}
+              onUserInput={this.setForm}
+              placeholder={'Username'}
+              class={'col-sm-12'}
+              required={true}
+            />
+            <TextboxElement
+              name={'password'}
+              value={this.state.form.value.password}
+              onUserInput={this.setForm}
+              placeholder={'Password'}
+              class={'col-sm-12'}
+              required={true}
+              type={'password'}
+            />
+            <ButtonElement
+              label={'Login'}
+              type={'submit'}
+              columnSize={'col-sm-12'}
+              buttonClass={'btn btn-primary btn-block'}
+            />
+          </FormElement>
+          <div className={'help-links'}>
+            <a onClick={() => this.setMode('reset')}
+               style={{cursor: 'pointer'}}>Forgot your password?</a>
+            <br/>
+            <a onClick={() => this.setMode('request')}
+               style={{cursor: 'pointer'}}>Request Account</a>
+          </div>
+          <div className={'help-text'}>
+            A WebGL-compatible browser is required for full functionality (Mozilla Firefox, Google Chrome)
+          </div>
+        </div>
+      );
+      return (
+        <div className={'container'}>
+          <div className={'row'}>
+            <section className={'col-md-4 col-md-push-8'}>
+              <Panel
+                title={'Login to LORIS'}
+                class={'panel-default login-panel'}
+                collapsing={false}
+              >
+                {login}
+              </Panel>
+            </section>
+            <section className={'col-md-8 col-md-pull-4'}>
+              <Panel
+                title={this.state.study.title}
+                class={'panel-default login-panel'}
+                collapsing={false}
+              >
+                {study}
+              </Panel>
+            </section>
+          </div>
+        </div>
+      );
+    }
+    if (this.state.mode === 'reset') {
+      return (
+        <ResetPassword
+          module={'reset'}
+          setMode={this.setMode}
+        />
+      );
+    }
+    if (this.state.mode === 'request') {
+      return (
+        <RequestAccount
+          module={'reset'}
+          setMode={this.setMode}
+        />
+      );
+    }
+>>>>>>> update
   }
 }
 Login.propTypes = {
---
+++ modules/login/jsx/resetPassword.js
@@ -0,0 +1,100 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import Panel from 'Panel';
+
+/**
+ * Reset password form.
+ *
+ * @description form for reset password.
+ *
+ * @author Alizée Wickenheiser
+ * @version 1.0.0
+ *
+ */
+class ResetPassword extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      form: {
+        value: {
+          username: '',
+        },
+        error: '',
+      },
+    };
+    this.setForm = this.setForm.bind(this);
+  }
+
+  /**
+   * Set the form data based on state values of child elements/components
+   *
+   * @param {string} formElement - name of the selected element
+   * @param {string} value - selected value for corresponding form element
+   */
+  setForm(formElement, value) {
+    const state = Object.assign({}, this.state);
+    state.form.value[formElement] = value;
+    this.setState(state);
+  }
+  /**
+   * Handle form submission
+   *
+   * @param {object} e - Form submission event
+   */
+  handleSubmit(e) {
+    let form = document.getElementById('form');
+    form.submit();
+  }
+  render() {
+    const reset = (
+      <div>
+        <FormElement
+          name={'login'}
+          action={''}
+          id={'form'}
+          fileUpload={'false'}
+          onSubmit={this.handleSubmit}
+        >
+          <StaticElement
+            text={'Please enter your username below, and a new password will be sent to you.'}
+            class={'col-sm-12'}
+            textClass={'text-center'}
+          />
+          <TextboxElement
+            name={'username'}
+            value={this.state.form.value.username}
+            onUserInput={this.setForm}
+            class={'col-sm-12'}
+            required={true}
+            type={'text'}
+          />
+          <ButtonElement
+            label={'Reset'}
+            type={'submit'}
+            columnSize={'col-sm-12'}
+            buttonClass={'btn btn-primary btn-block'}
+          />
+        </FormElement>
+        <a onClick={()=>this.props.setMode('login')}
+           style={{cursor: 'pointer'}}>Back to login page</a>
+      </div>
+    );
+    return (
+      <div className={'container'}>
+        <Panel
+          title={'Reset Password'}
+          class={'panel-default panel-center'}
+          collapsing={false}
+        >
+          {reset}
+        </Panel>
+      </div>
+    );
+  }
+}
+ResetPassword.propTypes = {
+  module: PropTypes.string,
+  setMode: PropTypes.func,
+};
+
+export default ResetPassword;
