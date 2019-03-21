c4c1dcaaa7489d3ba05c49f926be508b922bcbfe
Dave MacFarlane <dave.macfarlane@mcin.ca>
2019-03-21 15:02:26 -0400

    Go back to MockBuilder
    The constructor still needs to be disabled

--- test/unittests/NDB_BVL_Instrument_ToJSON_Test.php
+++ test/unittests/NDB_BVL_Instrument_ToJSON_Test.php
@@ -42,7 +42,7 @@ class NDB_BVL_Instrument_ToJSON_Test extends TestCase
         $this->Client->makeCommandLine();
         $this->Client->initialize(__DIR__ . "/../../project/config.xml");
 
-        $this->i = $this->getMockForAbstractClass(\NDB_BVL_Instrument::class)->setMethods(array("getFullName"))->getMock();
+        $this->i = $this->getMockBuilder(\NDB_BVL_Instrument::class)->disableOriginalConstructor()->setMethods(array("getFullName", "getSubtestList"))->getMock();
         $this->i->method('getFullName')->willReturn("Test Instrument");
         $this->i->method('getSubtestList')->willReturn(array());
         $this->i->form = $this->QuickForm;
@@ -365,7 +365,7 @@ class NDB_BVL_Instrument_ToJSON_Test extends TestCase
     }
 
     function testPageGroup() {
-        $this->i = $this->getMockForAbstractClass(\NDB_BVL_Instrument::class)->setMethods(array("getFullName", "getSubtestList", '_setupForm'))->getMock();
+        $this->i = $this->getMockBuilder(\NDB_BVL_Instrument::class)->disableOriginalConstructor()->setMethods(array("getFullName", "getSubtestList", '_setupForm'))->getMock();
         $this->i->method('getFullName')->willReturn("Test Instrument");
         $this->i->method('getSubtestList')->willReturn(
             array(
