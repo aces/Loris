9f20e794661984363c89b8c68646737d6e6ce9ac
Alizée Bellerose <misotheism@users.noreply.github.com>
2019-04-01 10:16:54 -0400

    update

--- modules/create_timepoint/jsx/createTimepointIndex.js
+++ modules/create_timepoint/jsx/createTimepointIndex.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Panel from 'Panel';
 import Loader from 'Loader';
-import PropTypes from 'prop-types';
 
 /**
  * Create Timepoint.
