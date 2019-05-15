2e81a9b2822b5c050ae07f51861fa23d0228fc10
Jkat <601027+Jkat@users.noreply.github.com>
2019-05-15 13:09:39 -0400

    better

--- modules/data_release/ajax/AddPermission.php
+++ modules/data_release/ajax/AddPermission.php
@@ -38,9 +38,7 @@ if (isset($_POST['action']) && $_POST['action'] == 'addpermission'
         && !empty($_POST['data_release_version'])
     ) {
         $userid = $_POST['userid'];
-        $data_release_version = $_POST['data_release_version'];
-
-        $data_release_version = $data_release_version == 'Unversioned' ? '' : $data_release_version;
+        $data_release_version = $_POST['data_release_version'] == 'Unversioned' ? '' : $_POST['data_release_version'];
 
         $IDs = $DB->pselectCol(
             "SELECT id 
