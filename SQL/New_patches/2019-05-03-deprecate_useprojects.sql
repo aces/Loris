624f18ec1682d7a31275d4857d526d6702e3ad39
Mélanie Legault <melanie.legault2@mcgill.ca>
2019-05-15 08:55:42 -0400

    replace INSERT ... WHERE EXIST where INSERT IGNORE

--- SQL/New_patches/2019-05-03-deprecate_useprojects.sql
+++ SQL/New_patches/2019-05-03-deprecate_useprojects.sql
@@ -6,10 +6,10 @@ SELECT 'loris'
 WHERE NOT EXISTS (SELECT * FROM Project);
 
 -- associate all subprojects to the loris project by default if project table was empty
-INSERT INTO project_rel (ProjectID,SubprojectID)
-SELECT ProjectID, SubprojectID 
-FROM Project JOIN subproject 
-WHERE Project.Name='loris';
+INSERT IGNORE INTO Project (ProjectID, Name)
+  (SELECT * FROM
+     (SELECT min(ProjectID), 'loris' FROM Project AS q1)
+  AS q2);
 
 -- if the loris project was added, set all candidates to that default project
 UPDATE candidate SET ProjectID=(SELECT ProjectID FROM Project WHERE Name='loris') WHERE ProjectID IS NULL AND Entity_type='Human';
