07d3e64abda2828307bb10afeea204f6348de933
PapillonMcGill <34311645+PapillonMcGill@users.noreply.github.com>
2019-05-21 14:22:22 -0400

    Fix INSERT ... WHERE EXIST (#4597)
    Fix patch by SELECTing from DUAL table as we can't use WHERE clause in a SELECT without a FROM clause.

--- SQL/New_patches/2019-05-03-deprecate_useprojects.sql
+++ SQL/New_patches/2019-05-03-deprecate_useprojects.sql
@@ -2,8 +2,8 @@ DELETE cs, c FROM ConfigSettings cs JOIN Config c ON c.ConfigID=cs.ID WHERE cs.N
 
 -- if Project table is empty, add default 'loris' project
 INSERT INTO Project (Name)
-SELECT 'loris'
-WHERE NOT EXISTS (SELECT * FROM Project);
+(SELECT 'loris' FROM DUAL
+WHERE NOT EXISTS (SELECT * FROM Project));
 
 -- associate all subprojects to the loris project by default if project table was empty
 INSERT INTO project_rel (ProjectID,SubprojectID)
