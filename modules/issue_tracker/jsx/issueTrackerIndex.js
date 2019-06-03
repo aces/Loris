aces/Loris

commit ca0d525fa7cfda0fc52258acb8cd0c1ed19c5064

[Issue Tracker] Reactify Menu Filter + Add DataFramework + Update FilterableDataTable (#4112)
- Reactifies the Issue Tracker Menu Filter
- Adds the Data Framework to Issue Tracker
- Updates FilterableDataTable
- Cleans up the DataTable

See https://github.com/aces/Loris/pull/4112 for details.

modules/issue_tracker/jsx/issueTrackerIndex.js
@@ -1,0 +1,250 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+import Loader from 'Loader';
+import FilterableDataTable from 'FilterableDataTable';
+
+class IssueTrackerIndex extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: {},
+      error: false,
+      isLoaded: false,
+    };
+
+    this.fetchData = this.fetchData.bind(this);
+    this.formatColumn = this.formatColumn.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchData()
+      .then(() => this.setState({isLoaded: true}));
+  }
+
+  /**
+   * Retrieve data from the provided URL and save it in state
+   * Additionally add hiddenHeaders to global loris variable
+   * for easy access by columnFormatter.
+   *
+   * @return {object}
+   */
+  fetchData() {
+    return fetch(this.props.dataURL, {credentials: 'same-origin'})
+      .then((resp) => resp.json())
+      .then((data) => this.setState({data}))
+      .catch((error) => {
+        this.setState({error: true});
+        console.error(error);
+      });
+  }
+
+  /**
+   * Modify behaviour of specified column cells in the Data Table component
+   *
+   * @param {string} column - column name
+   * @param {string} cell - cell content
+   * @param {object} row - row content indexed by column
+   *
+   * @return {*} a formated table cell for a given column
+   */
+  formatColumn(column, cell, row) {
+    let result = <td>{cell}</td>;
+    let link;
+    switch (column) {
+    case 'Title':
+      link = (
+        <a
+          href={loris.BaseURL+'/issue_tracker/issue/?issueID='+row['Issue ID']}
+        >
+          {row.Title}
+        </a>
+      );
+      result = <td>{link}</td>;
+      break;
+    case 'Issue ID':
+      link = (
+        <a
+          href={loris.BaseURL+'/issue_tracker/issue/?issueID='+row['Issue ID']}
+        >
+          {cell}
+        </a>
+      );
+      result = <td>{link}</td>;
+      break;
+    case 'Priority':
+      switch (cell) {
+      case 'normal':
+        result = <td style={{background: '#CCFFCC'}}>Normal</td>;
+        break;
+      case 'high':
+        result = <td style={{background: '#EEEEAA'}}>High</td>;
+        break;
+      case 'urgent':
+        result = <td style={{background: '#CC6600'}}>Urgent</td>;
+        break;
+      case 'immediate':
+        result = <td style={{background: '#E4A09E'}}>Immediate</td>;
+        break;
+      case 'low':
+        result = <td style={{background: '#99CCFF'}}>Low</td>;
+        break;
+      default:
+        result = <td>None</td>;
+      };
+      break;
+    case 'Site':
+      result = <td>{this.state.data.fieldOptions.sites[cell]}</td>;
+      break;
+    case 'PSCID':
+      if (row.PSCID !== null) {
+        link = (
+          <a href={loris.BaseURL+'/'+row.CandID + '/'}>
+            {cell}
+          </a>
+        );
+        result = <td>{link}</td>;
+      }
+      break;
+    case 'Visit Label':
+      if (row['Visit Label'] !== null) {
+        link =(
+          <a href={loris.BaseURL + '/instrument_list/?candID=' +
+                  row.CandID + '&sessionID=' + row.SessionID }>
+            {cell}
+          </a>
+        );
+        result = <td>{link}</td>;
+      }
+      break;
+    }
+
+    return result;
+  }
+
+  render() {
+    // If error occurs, return a message.
+    // XXX: Replace this with a UI component for 500 errors.
+    if (this.state.error) {
+      return <h3>An error occured while loading the page.</h3>;
+    }
+
+    // Waiting for async data to load
+    if (!this.state.isLoaded) {
+      return <Loader/>;
+    }
+
+   /**
+    * XXX: Currently, the order of these fields MUST match the order of the
+    * queried columns in _setupVariables() in media.class.inc
+    */
+    const options = this.state.data.fieldOptions;
+    const fields = [
+      {label: 'Issue ID', show: true, filter: {
+        name: 'issueID',
+        type: 'text',
+      }},
+      {label: 'Title', show: true, filter: {
+        name: 'title',
+        type: 'text',
+      }},
+      {label: 'Module', show: true, filter: {
+        name: 'module',
+        type: 'select',
+        options: options.modules,
+      }},
+      {label: 'Category', show: true, filter: {
+        name: 'category',
+        type: 'select',
+        options: options.categories,
+      }},
+      {label: 'Reporter', show: true, filter: {
+        name: 'reporter',
+        type: 'select',
+        options: options.reporters,
+      }},
+      {label: 'Assignee', show: true, filter: {
+        name: 'assignee',
+        type: 'select',
+        options: options.assignees,
+      }},
+      {label: 'Status', show: true, filter: {
+        name: 'status',
+        type: 'select',
+        options: options.statuses,
+        }},
+      {label: 'Priority', show: true, filter: {
+        name: 'priority',
+        type: 'select',
+        options: options.priorities,
+        }},
+      {label: 'Site', show: true, filter: {
+        name: 'site',
+        type: 'select',
+        options: options.sites,
+        }},
+      {label: 'PSCID', show: true, filter: {
+        name: 'pscid',
+        type: 'text',
+        }},
+      {label: 'Visit Label', show: true, filter: {
+        name: 'visitLabel',
+        type: 'text',
+        }},
+      {label: 'Date Created', show: false, filter: {
+        name: 'dateCreated',
+        type: 'date',
+      }},
+      {label: 'Last Update', show: true},
+      {label: 'SessionID', show: false},
+      {label: 'CandID', show: false},
+      {label: 'Watching', show: false, filter: {
+        name: 'watching',
+        type: 'checkbox',
+        }},
+    ];
+
+    const filterPresets = [
+      {label: 'All Issues', filter: {}},
+      {label: 'Active Issues', filter: {status: {value: 'closed', opposite: true}}},
+      {label: 'Closed Issues', filter: {status: {value: 'closed', exactMatch: true}}},
+      {label: 'My Issues', filter: {assignee: {value: this.state.data.fieldOptions.userID, exactMatch: true}}},
+    ];
+
+    const addIssue = () => {
+      window.location.replace(
+        loris.BaseURL+'/issue_tracker/issue/?issueID=0'
+      );
+    };
+    const actions = [
+      {label: 'New Issue', action: addIssue},
+    ];
+
+    return (
+      <FilterableDataTable
+        name="issuesTracker"
+        data={this.state.data.data}
+        fields={fields}
+        filterPresets={filterPresets}
+        actions={actions}
+        getFormattedCell={this.formatColumn}
+      />
+    );
+  }
+}
+
+IssueTrackerIndex.propTypes = {
+  dataURL: PropTypes.string.isRequired,
+  hasPermission: PropTypes.func.isRequired,
+};
+
+window.addEventListener('load', () => {
+  ReactDOM.render(
+    <IssueTrackerIndex
+      dataURL={`${loris.BaseURL}/issue_tracker/?format=json`}
+      hasPermission={loris.userHasPermission}
+    />,
+    document.getElementById('lorisworkspace')
+  );
+});

modules/issue_tracker/php/issue.class.inc
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * This file features the code for the form portion of the Loris issue
  * tracker.
@@ -16,7 +15,6 @@
 namespace LORIS\issue_tracker;
 use \Psr\Http\Message\ServerRequestInterface;
 use \Psr\Http\Message\ResponseInterface;
-
 /**
  * Creates the form for NDB_Form_issue_tracker
  *
@@ -26,10 +24,8 @@ use \Psr\Http\Message\ResponseInterface;
  * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
  * @link     https://github.com/aces/Loris-Trunk
  */
-
 class Issue extends \NDB_Form
 {
-
     protected $issueID;
     public $skipTemplate = true;
 
@@ -43,7 +39,6 @@ class Issue extends \NDB_Form
         parent::setup();
         $user    =& \User::singleton();
         $issueID = $this->issueID;
-
         if ((empty($issueID)
             || !isset($issueID))
             && !$user->hasPermission('issue_tracker_reporter')
@@ -68,7 +63,6 @@ class Issue extends \NDB_Form
         );
     }
 
-
     /**
     * Gets control panel
     *
@@ -79,7 +73,6 @@ class Issue extends \NDB_Form
         $controlPanel = new issue_tracker_ControlPanel($this->issueID);
         return $controlPanel->display();
     }
-
 
     /**
      * Include issueIndex.js
@@ -101,33 +94,32 @@ class Issue extends \NDB_Form
      * Checks if the user has the ability to *view* the issue page
      * Does not govern editing abilities; those are controlled by issueIndex.js
      *
-     * @param \User $user The user whose access is being checked
+     * @param \User $user User Instance
      *
      * @return bool
+     *
+     * @throws \ConfigurationException
      */
     function _hasAccess(\User $user) : bool
     {
         $db      = \Database::singleton();
         $issueID = $this->issueID;
-
         if (!$issueID) {
             return true;
         }
-
         $issueData = $db->pselectRow(
             "SELECT centerID FROM issues WHERE issueID=:issueID",
             ['issueID' => $issueID]
         );
-
         if (in_array($issueData['centerID'], $user->getData('CenterIDs'))
             || ($user->hasPermission('access_all_profiles'))
             || (!empty($issueData['centerID']))
         ) {
             return true;
+        } else {
+            return false;
         }
-        return false;
     }
-
     /**
      * Handle the incoming request for an issue. This extracts URLs of the form
      * /issue_tracker/issue/3 and extracts the issueID so that it's accessible
@@ -139,9 +131,7 @@ class Issue extends \NDB_Form
      */
     public function handle(ServerRequestInterface $request) : ResponseInterface
     {
-        $results = [];
-        preg_match('#/issue/([0-9])+$#', $request->getURI()->getPath(), $results);
-        $this->issueID = $results[1] ?? null;
+        $this->issueID = $request->getQueryParams()['issueID'];
         if (empty($this->issueID)) {
             // Should probably be a bad request, but we don't have a 400.tpl
             // template.

jsx/DataTable.js
@@ -1,11 +1,3 @@
-/**
- * This file contains React component for Data Table
- *
- * @author Loris Team
- * @version 1.0.0
- *
- */
-
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import PaginationLinks from 'jsx/PaginationLinks';
@@ -18,18 +10,22 @@ import createFragment from 'react-addons-create-fragment';
 class DataTable extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      PageNumber: 1,
-      SortColumn: -1,
-      SortOrder: 'ASC',
-      RowsPerPage: 20,
-      Hide: this.props.Hide,
+      page: {
+        number: 1,
+        rows: 20,
+      },
+      sort: {
+       column: 0,
+       ascending: true,
+      },
     };
 
-    this.changePage = this.changePage.bind(this);
     this.setSortColumn = this.setSortColumn.bind(this);
-    this.changeRowsPerPage = this.changeRowsPerPage.bind(this);
+    this.updateSortColumn = this.updateSortColumn.bind(this);
+    this.toggleSortOrder = this.toggleSortOrder.bind(this);
+    this.updatePageNumber = this.updatePageNumber.bind(this);
+    this.updatePageRows = this.updatePageRows.bind(this);
     this.downloadCSV = this.downloadCSV.bind(this);
     this.countFilteredRows = this.countFilteredRows.bind(this);
     this.getSortedRows = this.getSortedRows.bind(this);//
@@ -37,98 +33,46 @@ class DataTable extends Component {
     this.renderActions = this.renderActions.bind(this);
   }
 
-  componentDidMount() {
-    if (jQuery.fn.DynamicTable) {
-      if (this.props.freezeColumn) {
-        $('#dynamictable').DynamicTable({
-          freezeColumn: this.props.freezeColumn,
-        });
-      } else {
-        $('#dynamictable').DynamicTable();
-      }
-      if (this.state.Hide.defaultColumn) {
-        $('#dynamictable').find('tbody td:eq(0)').hide();
-      }
-    }
-
-    // Retrieve module preferences
-    let modulePrefs = JSON.parse(localStorage.getItem('modulePrefs'));
-
-    // Init modulePrefs object
-    if (modulePrefs === null) {
-      modulePrefs = {};
-    }
-
-    // Init modulePrefs for current module
-    if (modulePrefs[loris.TestName] === undefined) {
-      modulePrefs[loris.TestName] = {};
-      modulePrefs[loris.TestName].rowsPerPage = this.state.RowsPerPage;
-    }
-
-    // Set rows per page
-    let rowsPerPage = modulePrefs[loris.TestName].rowsPerPage;
-    this.setState({
-      RowsPerPage: rowsPerPage,
-    });
-
-    // Make prefs accesible within component
-    this.modulePrefs = modulePrefs;
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (jQuery.fn.DynamicTable) {
-      if (this.props.freezeColumn) {
-        $('#dynamictable').DynamicTable({
-          freezeColumn: this.props.freezeColumn,
-        });
-      } else {
-        $('#dynamictable').DynamicTable();
-      }
-    }
-    if (this.props.onSort &&
-      (this.state.SortColumn !== prevState.SortColumn ||
-        this.state.SortOrder !== prevState.SortOrder)
-    ) {
-      let index = this.getSortedRows();
-      const headerList = this.props.fields.map((field) => field.label);
-      this.props.onSort(index, this.props.data, headerList);
+  setSortColumn(column) {
+    if (this.state.sort.column === column) {
+      this.props.toggleSortOrder();
+    } else {
+      this.updateSortColumn(column);
     }
   }
 
-  changePage(pageNo) {
-    this.setState({
-      PageNumber: pageNo,
-    });
+  updateSortColumn(column) {
+    const sort = this.state.sort;
+    sort.column = column;
+    this.setState({sort});
   }
 
-  setSortColumn(colNumber) {
-    return function(e) {
-      if (this.state.SortColumn === colNumber) {
-        this.setState({
-          SortOrder: this.state.SortOrder === 'ASC' ? 'DESC' : 'ASC',
-        });
-      } else {
-        this.setState({
-          SortColumn: colNumber,
-        });
-      }
-    };
+  toggleSortOrder() {
+    const sort = this.state.sort;
+    sort.ascending = !sort.ascending;
+    this.setState({sort});
   }
 
-  changeRowsPerPage(val) {
-    let rowsPerPage = val.target.value;
-    let modulePrefs = this.modulePrefs;
+  /**
+   * Updates page state
+   *
+   * @param {int} number of page
+   */
+  updatePageNumber(number) {
+    const page = this.sate.page;
+    page.number = number;
+    this.setState({page});
+  }
 
-    // Save current selection
-    modulePrefs[loris.TestName].rowsPerPage = rowsPerPage;
-
-    // Update localstorage
-    localStorage.setItem('modulePrefs', JSON.stringify(modulePrefs));
-
-    this.setState({
-      RowsPerPage: rowsPerPage,
-      PageNumber: 1,
-    });
+  /**
+   * Update number of rows per page
+   *
+   * @param {object} e event from which to abstract value
+   */
+  updatePageRows(e) {
+    const page = this.state.page;
+    page.rows = e.target.value;
+    this.setState({page});
   }
 
   downloadCSV(csvData) {
@@ -211,10 +155,10 @@ class DataTable extends Component {
     const index = [];
 
     for (let i = 0; i < this.props.data.length; i += 1) {
-      let val = this.props.data[i][this.state.SortColumn] || undefined;
-      // If SortColumn is equal to default No. column, set value to be
+      let val = this.props.data[i][this.state.sort.column] || undefined;
+      // If sortColumn is equal to default No. column, set value to be
       // index + 1
-      if (this.state.SortColumn === -1) {
+      if (this.state.sort.column === -1) {
         val = i + 1;
       }
       const isString = (typeof val === 'string' || val instanceof String);
@@ -240,8 +184,8 @@ class DataTable extends Component {
       }
     }
 
-    index.sort(function(a, b) {
-      if (this.state.SortOrder === 'ASC') {
+    index.sort((a, b) => {
+      if (this.state.sort.ascending) {
         if (a.Value === b.Value) {
           // If all values are equal, sort by rownum
           if (a.RowIdx < b.RowIdx) return -1;
@@ -270,7 +214,7 @@ class DataTable extends Component {
       }
       // They're equal..
       return 0;
-    }.bind(this));
+    });
     return index;
   }
 
@@ -287,6 +231,7 @@ class DataTable extends Component {
   hasFilterKeyword(name, data) {
     let filterData = null;
     let exactMatch = false;
+    let opposite = false;
     let result = false;
     let searchKey = null;
     let searchString = null;
@@ -294,6 +239,7 @@ class DataTable extends Component {
     if (this.props.filter[name]) {
       filterData = this.props.filter[name].value;
       exactMatch = this.props.filter[name].exactMatch;
+      opposite = this.props.filter[name].opposite;
     }
 
     // Handle null inputs
@@ -326,11 +272,18 @@ class DataTable extends Component {
             searchString = data.toLowerCase();
             if (exactMatch) {
               result = (searchString === searchKey);
+            } else if (opposite) {
+              result = searchString !== searchKey;
             } else {
               result = (searchString.indexOf(searchKey) > -1);
             }
           break;
       }
+    }
+
+    // Handle boolean inputs
+    if (typeof filterData === 'boolean') {
+      result = filterData;
     }
 
     // Handle array inputs for multiselects
@@ -355,7 +308,7 @@ class DataTable extends Component {
       return this.props.actions.map((action, key) => {
         if (action.show !== false) {
           return (
-            < CTA
+            <CTA
               key = {key}
               label = {action.label}
               onUserInput = {action.action}
@@ -383,10 +336,12 @@ class DataTable extends Component {
         </div>
       );
     }
-    let rowsPerPage = this.state.RowsPerPage;
-    let headers = this.state.Hide.defaultColumn === true ? [] : [
-      <th key='th_col_0' onClick={this.setSortColumn(-1).bind(this)}>
-        {this.props.RowNumLabel}
+    let rowsPerPage = this.state.page.rows;
+    let headers = this.props.hide.defaultColumn === true ? [] : [
+      <th key='th_col_0' onClick={() => {
+        this.setSortColumn(-1);
+      }}>
+        {this.props.rowNumLabel}
       </th>,
     ];
 
@@ -396,13 +351,17 @@ class DataTable extends Component {
         if (this.props.fields[i].freezeColumn === true) {
           headers.push(
             <th key={'th_col_' + colIndex} id={this.props.freezeColumn}
-                onClick={this.setSortColumn(i).bind(this)}>
+                onClick={() => {
+                  this.setSortColumn(i);
+                }}>
               {this.props.fields[i].label}
             </th>
           );
         } else {
           headers.push(
-            <th key={'th_col_' + colIndex} onClick={this.setSortColumn(i).bind(this)}>
+            <th key={'th_col_' + colIndex} onClick={() => {
+              this.setSortColumn(i);
+            }}>
               {this.props.fields[i].label}
             </th>
           );
@@ -414,7 +373,7 @@ class DataTable extends Component {
     let index = this.getSortedRows();
     let matchesFound = 0; // Keeps track of how many rows where displayed so far across all pages
     let filteredRows = this.countFilteredRows();
-    let currentPageRow = (rowsPerPage * (this.state.PageNumber - 1));
+    let currentPageRow = (rowsPerPage * (this.state.page.number - 1));
     let filteredData = [];
     let useKeyword = false;
 
@@ -505,11 +464,11 @@ class DataTable extends Component {
       }
     }
 
-    let RowsPerPageDropdown = (
+    let rowsPerPageDropdown = (
       <select
         className="input-sm perPage"
-        onChange={this.changeRowsPerPage}
-        value={this.state.RowsPerPage}
+        onChange={this.props.updatePageRows}
+        value={this.state.page.rows}
       >
         <option>20</option>
         <option>50</option>
@@ -526,13 +485,13 @@ class DataTable extends Component {
       csvData = filteredData;
     }
 
-    let header = this.state.Hide.rowsPerPage === true ? '' : (
-      <div className="table-header">
+    let header = this.props.hide.rowsPerPage === true ? '' : (
+      <div className="table-header panel-heading">
         <div className="row">
           <div className="col-xs-12">
             <div>
               {rows.length} rows displayed of {filteredRows}.
-              (Maximum rows per page: {RowsPerPageDropdown})
+              (Maximum rows per page: {rowsPerPageDropdown})
             </div>
             <div className="pull-right" style={{marginTop: '-43px'}}>
               {this.renderActions()}
@@ -554,13 +513,21 @@ class DataTable extends Component {
       </div>
     );
 
-    let footer = this.state.Hide.downloadCSV === true ? '' : (
-      <div>
+    let footer = this.props.hide.downloadCSV === true ? '' : (
+      <div className="panel-footer table-footer">
         <div className="row">
           <div className="col-xs-12" style={{marginTop: '10px'}}>
             <div className="footerText">
               {rows.length} rows displayed of {filteredRows}.
-              (Maximum rows per page: {RowsPerPageDropdown})
+              (Maximum rows per page: {rowsPerPageDropdown})
+            </div>
+            <div className="col-xs-6">
+              <button
+                className="btn btn-primary downloadCSV"
+                onClick={this.downloadCSV.bind(null, csvData)}
+              >
+                Download Table as CSV
+              </button>
             </div>
             <div className="pull-right" style={{marginTop: '-23px'}}>
               <PaginationLinks
@@ -593,18 +560,20 @@ class DataTable extends Component {
 }
 DataTable.propTypes = {
   data: PropTypes.array.isRequired,
-  RowNumLabel: PropTypes.string,
+  rowNumLabel: PropTypes.string,
   // Function of which returns a JSX element for a table cell, takes
   // parameters of the form: func(ColumnName, CellData, EntireRowData)
   getFormattedCell: PropTypes.func,
   onSort: PropTypes.func,
-  Hide: PropTypes.object,
   actions: PropTypes.object,
+  hide: PropTypes.object,
 };
 DataTable.defaultProps = {
-  RowNumLabel: 'No.',
+  headers: [],
+  data: {},
+  rowNumLabel: 'No.',
   filter: {},
-  Hide: {
+  hide: {
     rowsPerPage: false,
     downloadCSV: false,
     defaultColumn: false,

jsx/Filter.js
@@ -58,6 +58,9 @@ class Filter extends Component {
         case 'date':
           element = <DateElement key={filter.name}/>;
           break;
+        case 'checkbox':
+          element = <CheckboxElement key={filter.name}/>;
+          break;
         default:
           element = <TextboxElement key={filter.name}/>;
         }

jsx/FilterableDataTable.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 import Panel from 'jsx/Panel';
+import {Tabs, TabPane} from 'jsx/Tabs';
 import DataTable from 'jsx/DataTable';
 import Filter from 'jsx/Filter';
 
@@ -43,27 +44,55 @@ class FilterableDataTable extends Component {
   }
 
   render() {
+    const filter = (
+      <Filter
+        name={this.props.name + '_filter'}
+        id={this.props.name + '_filter'}
+        columns={this.props.columns}
+        filter={this.state.filter}
+        fields={this.props.fields}
+        updateFilter={this.updateFilter}
+        clearFilter={this.clearFilter}
+      />
+    );
+
+    const dataTable = (
+      <DataTable
+        data={this.props.data}
+        fields={this.props.fields}
+        filter={this.state.filter}
+        actions={this.props.actions}
+        getFormattedCell={this.props.getFormattedCell}
+      />
+    );
+
+    const filterPresets = () => {
+      if (this.props.filterPresets) {
+        const tabPanes = this.props.filterPresets.map((preset) => {
+          return <TabPane TabId={preset.label} key={preset.label}/>;
+        });
+        const tabs = this.props.filterPresets.map((preset) => {
+          return {id: preset.label, label: preset.label};
+        });
+
+        return (
+          <Tabs tabs={tabs} updateURL={true} onTabChange={(tabId) => {
+            const active = this.props.filterPresets.find((preset) => {
+              return preset.label === tabId;
+            });
+            this.updateFilter(active.filter);
+          }}>
+            {tabPanes}
+          </Tabs>
+        );
+      };
+    };
+
     return (
-      <Panel
-        title={this.props.title}
-      >
-        <Filter
-          name={this.props.name + '_filter'}
-          id={this.props.name + '_filter'}
-          title='Selection Filter'
-          columns={this.props.columns}
-          filter={this.state.filter}
-          fields={this.props.fields}
-          updateFilter={this.updateFilter}
-          clearFilter={this.clearFilter}
-        />
-        <DataTable
-          data={this.props.data}
-          fields={this.props.fields}
-          filter={this.state.filter}
-          getFormattedCell={this.props.getFormattedCell}
-          actions={this.props.actions}
-        />
+      <Panel title={this.props.title}>
+        {filter}
+        {filterPresets()}
+        {dataTable}
       </Panel>
     );
   }
@@ -77,7 +106,6 @@ FilterableDataTable.propTypes = {
   name: PropTypes.string.isRequired,
   title: PropTypes.string,
   data: PropTypes.object.isRequired,
-  filter: PropTypes.object.isRequired,
   fields: PropTypes.object.isRequired,
   columns: PropTypes.number,
   getFormattedCell: PropTypes.func,

modules/issue_tracker/ajax/EditIssue.php
@@ -467,8 +467,8 @@ function getComments($issueID)
             $comment['fieldChanged'] = 'Visit Label';
         }
     }
-    return $unformattedComments; //now formatted I guess
 
+    return $unformattedComments; //now formatted I guess
 }
 
 /**
@@ -659,6 +659,12 @@ WHERE Parent IS NOT NULL ORDER BY Label ",
     if (!empty($_GET['issueID'])) { //if an existing issue
         $issueID    = $_GET['issueID'];
         $issueData  = getIssueData($issueID);
+        $desc       = $db->pselect(
+            "SELECT issueComment
+FROM issues_comments WHERE issueID=:i
+ORDER BY dateAdded LIMIT 1",
+            array('i' => $issueID)
+        );
         $isWatching = $db->pselectOne(
             "SELECT userID, issueID FROM issues_watching
             WHERE issueID=:issueID AND userID=:userID",
@@ -670,12 +676,7 @@ WHERE Parent IS NOT NULL ORDER BY Label ",
         $issueData['watching']       = is_array($isWatching) ? "No" : "Yes";
         $issueData['commentHistory'] = getComments($issueID);
         $issueData['othersWatching'] = getWatching($issueID);
-        $issueData['desc']           = $db->pSelectOne(
-            "SELECT issueComment
-FROM issues_comments WHERE issueID=:issueID
-ORDER BY dateAdded",
-            array('issueID' => $issueID)
-        );
+        $issueData['desc']           = $desc[0]['issueComment'];
     }
     $issueData['comment'] = null;
 
@@ -705,7 +706,7 @@ ORDER BY dateAdded",
 
 /**
  * If issueID is passed retrieves issue data from database,
- * othewise return empty issue data object
+ * otherwise return empty issue data object
  *
  * @param string $issueID the ID of the requested issue
  *
@@ -714,8 +715,8 @@ ORDER BY dateAdded",
 function getIssueData($issueID=null)
 {
 
-    $user =& User::singleton();
-    $db   =& Database::singleton();
+    $user = \User::singleton();
+    $db   = \Database::singleton();
 
     if (!empty($issueID)) {
         return $db->pselectRow(

commit 38312a5c9af7a6c6355322e0385071c204bc48ca

[Issue Tracker] Fix incorrect links to individual issues
There was a regression introduced in #4112 where the issue tracker
links reverted to getting the ID from a query parameter instead
of the URL. This was done inconsistently, and as a result some
places loaded URLs such as "...&issueID=?issueID=1". This fixes
the links to parse the ID from the URL, so that issues can now
be loaded.

modules/issue_tracker/jsx/issueTrackerIndex.js
@@ -56,7 +56,7 @@ class IssueTrackerIndex extends Component {
     case 'Title':
       link = (
         <a
-          href={loris.BaseURL+'/issue_tracker/issue/?issueID='+row['Issue ID']}
+          href={loris.BaseURL+'/issue_tracker/issue/'+row['Issue ID']}
         >
           {row.Title}
         </a>
@@ -66,7 +66,7 @@ class IssueTrackerIndex extends Component {
     case 'Issue ID':
       link = (
         <a
-          href={loris.BaseURL+'/issue_tracker/issue/?issueID='+row['Issue ID']}
+          href={loris.BaseURL+'/issue_tracker/issue/'+row['Issue ID']}
         >
           {cell}
         </a>

modules/issue_tracker/php/issue.class.inc
@@ -131,7 +131,9 @@ class Issue extends \NDB_Form
      */
     public function handle(ServerRequestInterface $request) : ResponseInterface
     {
-        $this->issueID = $request->getQueryParams()['issueID'];
+        $results = [];
+        preg_match('#/issue/([0-9]+)$#', $request->getURI()->getPath(), $results);
+        $this->issueID = $results[1] ?? null;
         if (empty($this->issueID)) {
             // Should probably be a bad request, but we don't have a 400.tpl
             // template.