919e07e0855cfcce9642996486bcb2630fef4a13
zaliqarosli <zaliqa.rosli@mcin.ca>
2019-06-06 17:24:22 -0400

    to string boolean data for url filtering

--- jsx/DataTable.js
+++ jsx/DataTable.js
@@ -298,7 +298,7 @@ class DataTable extends Component {
       let match = false;
       for (let i = 0; i < filterData.length; i += 1) {
         searchKey = filterData[i].toLowerCase();
-        searchString = data.toLowerCase();
+        searchString = data.toString().toLowerCase();
 
         match = (searchString.indexOf(searchKey) > -1);
         if (match) {
