c97056b0d5d0621a343efa47fd5cba5294888505
AlexandraLivadas <alexandra.livadas@mail.mcgill.ca>
2019-07-02 16:31:22 -0400

    [Test] Adding to unittests for LorisForm.class.inc

--- test/unittests/LorisForms_Test.php
+++ test/unittests/LorisForms_Test.php
@@ -83,11 +83,36 @@ class LorisForms_Test extends TestCase
         );
     }
 
+    /**
+     * Custom assertion to assert that some attribute of an element
+     * is correct
+     *
+     * @param string $el           The element name
+     * @param string $attribute    The attribute name to assert
+                                   (ie class, id, value, etc)
+     * @param string $attribValue  The expected content of the attribute
+     *
+     * @return void but makes assertions
+     */
+    function assertAttribute($el, $attribute, $attribValue)
+    {
+        if (!isset($this->form->form[$el])) {
+            $this->fail("Element $el does not exist");
+            return;
+        }
+        $this->assertEquals(
+            $this->form->form[$el][$attribute],
+            $attribValue,
+            "Element $el's $attribute did not match $attribValue"
+        );
+    }
+
 
     /**
      * Test that the addSelect wrapper adds an element of the appropriate
      * type to the page
      *
+     * @covers LorisForm::addSelect
      * @return void
      */
     function testAddSelect()
@@ -101,8 +126,9 @@ class LorisForms_Test extends TestCase
 
     /**
      * Test that the addSelect wrapper adds an element of the appropriate
-     * type to the page
+     * type to the page and that it correctly sets the 'multiple' attribute
      *
+     * @covers LorisForm::addSelect
      * @return void
      */
     function testAddMultiSelect()
@@ -127,10 +153,26 @@ class LorisForms_Test extends TestCase
         $this->assertEquals($element->nodeName, "select");
         $this->assertTrue($element->hasAttribute("multiple"));
     }
+
+    /**
+     * Test that the addStatic wrapper adds an elements of the appropriate
+     * type to the page
+     *
+     * @covers LorisForm::addStatic
+     * @return void
+     */
+    function testAddStatic()
+    {
+        $this->form->addStatic("abc", "Hello");
+        $this->assertType("abc", "static");
+        $this->assertLabel("abc", "Hello");
+    }
+
     /**
      * Test that the addText wrapper adds an element of the appropriate
      * type to the page
      *
+     * @covers LorisForm::addText
      * @return void
      */
     function testAddText()
@@ -142,22 +184,25 @@ class LorisForms_Test extends TestCase
 
     /**
      * Test that the addDate wrapper adds an element of the appropriate
-     * type to the page
+     * type to the page and that the 'options' attribute is set
      *
+     * @covers LorisForm::addDate
      * @return void
      */
     function testAddDate()
     {
-        $this->form->addDate("abc", "Hello", array());
+        $this->form->addDate("abc", "Hello", array(), array());
         $this->assertType("abc", "date");
         $this->assertLabel("abc", "Hello");
+        $this->assertTrue(isset($this->form->form["abc"]["options"]));
     }
 
 
     /**
-     * Test that the addDate wrapper adds an element of the appropriate
+     * Test that the addFile wrapper adds an element of the appropriate
      * type to the page
      *
+     * @covers LorisForm::addFile
      * @return void
      */
     function testAddFile()
@@ -169,9 +214,10 @@ class LorisForms_Test extends TestCase
 
 
     /**
-     * Test that the addDate wrapper adds an element of the appropriate
+     * Test that the addPassword wrapper adds an element of the appropriate
      * type to the page
      *
+     * @covers LorisForm::addPassword
      * @return void
      */
     function testAddPassword()
@@ -181,11 +227,126 @@ class LorisForms_Test extends TestCase
         $this->assertLabel("abc", "Hello");
     }
 
+    /**
+     * Test that the addHidden wrapper adds an element of the appropriate
+     * type to the page
+     *
+     * @covers LorisForm::addHidden
+     * @return void
+     */
+    function testAddHidden()
+    {
+        $this->form->addHidden("abc", "value1", array());
+        $this->assertType("abc", "hidden");
+        //The addHidden method sets the label to null in every case
+        $this->assertLabel("abc", null);
+        //The second parameter taken by the addHidden method sets the "value" attribute of the element
+        $this->assertAttribute("abc", "value", "value1");   
+    }
+
+    /**
+     * Test that the addTextArea wrapper adds an element of the appropriate
+     * type to the page
+     *
+     * @covers LorisForm::addTextArea
+     * @return void
+     */
+    function testAddTextArea()
+    {
+        $this->form->addTextArea("abc", "Hello", array());
+        $this->assertType("abc", "textarea");
+        $this->assertLabel("abc", "Hello");
+    }
+
+    /**
+     * Test that the addTextArea wrapper adds an element of the appropriate
+     * type to the page and that the rows and cols attributes are set correctly if specified
+     * @note This test is added because the addTextArea wrapper sets the rows and cols
+     *       attributes itself and not using the createBase method
+     * 
+     * @covers LorisForm::addTextArea
+     * @return void
+     */
+    function testAddTextAreaWithRowsOrCols()
+    {
+        $rowAttrib = array('rows' => 'row1');
+        $colAttrib = array('cols' => 'col1');
+
+        $this->form->addTextArea("abc", "Hello", $rowAttrib);
+        $this->assertType("abc", "textarea");
+        $this->assertLabel("abc", "Hello");
+        $this->assertAttribute("abc", "rows", "row1");
+
+        $this->form->addTextArea("abc", "Hello", $colAttrib);
+        $this->assertType("abc", "textarea");
+        $this->assertLabel("abc", "Hello");
+        $this->assertAttribute("abc", "cols", "col1");    
+    }
+
+    /**
+     * Test that the addCheckbox wrapper adds an element of the appropriate
+     * type to the page
+     *
+     * @covers LorisForm::addCheckbox
+     * @return void
+     */
+    function testAddCheckbox()
+    {
+        $this->form->addCheckbox("abc", "Hello", array());
+        $this->assertType("abc", "advcheckbox");
+        $this->assertLabel("abc", "Hello");
+    }
+
+    /**
+     * Test that the addHeader wrapper adds an element of the appropriate
+     * type to the page
+     *
+     * @covers LorisForm::addHeader
+     * @return void
+     */
+    function testAddHeader()
+    {
+        $this->form->addHeader("abc", "Hello", array());
+        $this->assertType("abc", "header");
+        $this->assertLabel("abc", "Hello");
+    }
+
+    /**
+     * Test that the addLink wrapper adds an element of the appropriate
+     * type to the page
+     *
+     * @covers LorisForm::addLink
+     * @return void
+     */
+    function testAddLink()
+    {
+        $this->form->addLink("abc", "Hello", "example_url.com", "link1");
+        $this->assertType("abc", "link");
+        $this->assertLabel("abc", "Hello");
+        $this->assertAttribute("abc", "url", "example_url.com");
+        $this->assertAttribute("abc", "link", "link1");
+    }
+
+    /**
+     * Test that the addRadio wrapper adds an element of the appropriate
+     * type to the page and that the options attribute is set
+     *
+     * @covers LorisForm::addRadio
+     * @return void
+     */
+    function testAddRadio()
+    {
+        $this->form->addRadio("abc", "Hello", array(), array());
+        $this->assertType("abc", "radio");
+        $this->assertLabel("abc", "Hello");
+        $this->assertTrue(isset($this->form->form['abc']['options']));
+    }
 
     /**
      * Test that the addElement wrapper with type "select" adds an element of
      * the appropriate type to the page
      *
+     * @covers LorisForm::addElement
      * @return void
      */
     function testAddElementSelect()
@@ -202,6 +363,7 @@ class LorisForms_Test extends TestCase
      * Test that the addElement wrapper with type "date" adds an element of
      * the appropriate type to the page
      *
+     * @covers LorisForm::addElement
      * @return void
      */
     function testAddElementDate()
@@ -218,6 +380,7 @@ class LorisForms_Test extends TestCase
      * Test that the addElement wrapper with type "text" adds an element of
      * the appropriate type to the page
      *
+     * @covers LorisForm::addElement
      * @return void
      */
     function testAddElementText()
@@ -234,6 +397,7 @@ class LorisForms_Test extends TestCase
      * Test that the addElement wrapper with type "text" adds an element of
      * the appropriate type to the page
      *
+     * @covers LorisForm::addElement
      * @return void
      */
     function testAddElementFile()
@@ -250,6 +414,7 @@ class LorisForms_Test extends TestCase
      * Test that the addElement wrapper with type "password" adds an element of
      * the appropriate type to the page
      *
+     * @covers LorisForm::addElement
      * @return void
      */
     function testAddElementPassword()
@@ -262,6 +427,137 @@ class LorisForms_Test extends TestCase
         $this->form->addElement("password", "abc", "Hello");
     }
 
+    /**
+     * Test that getValue returns the correct default value for the form element
+     * TODO This should ideally not return null
+     *
+     * @covers LorisForm::getValue
+     * @return void
+     */
+    function testGetValue()
+    {
+        $this->form->addSelect("abc", "Hello", array());
+        $this->assertEquals(null, $this->form->getValue("abc"));
+    }
+
+    /**
+     * Test that staticHTML returns the correctly formatted HTML
+     * TODO This returns null as of now because it uses the getValue method
+     *      Should return an actual value
+     *     
+     * @covers LorisForm::staticHTML
+     * @return void
+     */
+    function testStaticHTML()
+    {
+	$this->form->addStatic("abc", "Hello");
+	$this->assertEquals(null, $this->form->staticHTML($this->form->form["abc"]));
+    }
+
+    /**
+     * Test that dateHTML returns the correctly formatted HTML when no attributes or options are specified
+     *
+     * @covers LorisForm::dateHTML
+     * @return void
+     */
+    function testDateHTMLWithNoOptions()
+    {
+        $this->form->addDate("abc", "Hello", array());
+        $this->assertEquals(
+            "<input name=\"abc\" type=\"date\"    onChange=\"this.setCustomValidity('')\" >", 
+            $this->form->dateHTML($this->form->form["abc"])
+        );
+    } 
+
+    /**
+     * Test that dateHTML returns the correctly formatted HTML with the given attributes and options
+     * This returns the HTML when 'format' is set to either 'my' or 'ym' in the options array
+     *
+     * @covers LorisForm::dateHTML
+     * @return void
+     */
+    function testDateHTMLWithFormatNotSetToY()
+    {
+        $testAttributes = array('class' => 'class1',
+                             'disabled' => 'yes',
+                             'requireMsg' => 'requireMsg1',
+                             'required' => 'required1');
+        $testOptions = array('minYear' => '2010',
+                             'maxYear' => '2019',
+                             'format' => 'ym');
+        
+        $this->form->addDate("abc", "Hello", $testOptions, $testAttributes);
+        $this->assertEquals(
+            "<input name=\"abc\" type=\"month\" class=\"class1\" min=\"2010-01\" max=\"2019-12\" onChange=\"this.setCustomValidity('')\" disabled >", 
+            $this->form->dateHTML($this->form->form["abc"])
+        );
+    }
+
+    /**
+     * Test that dateHTML calls the yearHTML function when 'format' is set to 'y' in the options array
+     *
+     * @covers LorisForm::dateHTML
+     * @return void
+     */
+    function testDateHTMLWithFormatSetToY()
+    {
+        $testAttributes = array('class' => 'class1',
+                             'disabled' => 'yes',
+                             'requireMsg' => 'requireMsg1',
+                             'required' => 'required1');
+        $testOptions = array('minYear' => '2010',
+                             'maxYear' => '2019',
+                             'format' => 'y');
+
+        $this->form = $this->getMockBuilder('LorisForm')
+            ->setMethods(array('yearHTML'))
+            ->getMock();
+        $this->form->expects($this->once())
+            ->method('yearHTML');
+        $this->form->addDate("abc", "Hello", $testOptions, $testAttributes);
+        $this->form->dateHTML($this->form->form["abc"]);
+    }
+
+    /**
+     * Test that createText creates an element of type text
+     *
+     * @covers LorisForm::createText
+     * @return void
+     */
+    function testCreateText()
+    {
+        $testText = $this->form->createText("abc", "Hello", array());
+        $this->assertEquals("text", $testText['type']);
+        $this->assertEquals("Hello", $testText['label']);
+    }
+
+    /**
+     * Test that createSubmit creates an element of type submit
+     * with a null label and the proper value
+     *
+     * @covers LorisForm::createSubmit
+     * @return void
+     */
+    function testCreateSubmit()
+    {
+        $testSubmit = $this->form->createSubmit("abc", "value1", array());
+        $this->assertEquals("submit", $testSubmit['type']);
+        $this->assertEquals(null, $testSubmit['label']);
+        $this->assertEquals("value1", $testSubmit['value']);
+    }
+
+    /**
+     * Test that createTextArea creates an element of type textarea
+     *
+     * @covers LorisForm::createTextArea
+     * @return void
+     */
+    function testCreateTextArea()
+    {
+        $testText = $this->form->createTextArea("abc", "Hello");
+        $this->assertEquals("textarea", $testText['type']);
+        $this->assertEquals("Hello", $testText['label']);
+    }
 
 }
 ?>
