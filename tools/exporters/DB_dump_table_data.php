232b99da7920703885b1d4e25c1fd674181c6549
ridz1208 <ridz_1208@hotmail.com>
2019-07-17 17:28:46 -0400

    [tools/raisinbread exporter] Use config.xml credentials

--- tools/exporters/DB_dump_table_data.php
+++ tools/exporters/DB_dump_table_data.php
@@ -35,6 +35,18 @@ $tableNames = $DB->pselectCol("
                       WHERE TABLE_SCHEMA =:dbn",
     array("dbn"=>$databaseInfo['database'])
 );
+
+$dbUser = $databaseInfo["quatUser"];
+$dbPassword = $databaseInfo["quatPassword"];
+$dbHost = $databaseInfo["host"];
+
+if (empty($dbUser) || empty($dbPassword) || empty($dbHost)) {
+    echo "\n\n Some database credentials are missing, please make sure administrator 
+    credentials (quatUser, quatPassword) and a host value are available in your 
+    configuration file. \n\n";
+    die();
+}
+
 /*
  * Definitions of the flags used in the command below (ORDERING is IMPORTANT):
  * --complete-insert -> Use complete INSERT statements that include column names
@@ -54,11 +66,13 @@ $tableNames = $DB->pselectCol("
  *                      values to accommodate for timezone differences.
  */
 
+
 // Loop through all tables to generate insert statements for each.
 foreach ($tableNames as $tableName) {
     $paths = \NDB_Config::singleton()->getSetting('paths');
     $filename = $paths['base'] . "/raisinbread/RB_files/RB_$tableName.sql";
-    exec('mysqldump '.$databaseInfo['database'].' '.
+    exec('mysqldump -u '.$dbUser.' -p'.$dbPassword.' -h '.$dbHost.' '.
+        $databaseInfo['database'].' '.
         '--complete-insert '.
         '--no-create-db '.
         '--no-create-info '.
