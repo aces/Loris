f67bcd78138c8e5c10be1a1ebe35d524157b730e
John Saigle <john.saigle@gmail.com>
2019-07-30 13:22:03 -0400

    Add CandID to more places (Rida's feedback)

--- tools/fix_timepoint_date_problems.php
+++ tools/fix_timepoint_date_problems.php
@@ -40,6 +40,7 @@
  *
  * @package behavioural
  */
+use LORIS\StudyEntities\Candidate\CandID;
 
 set_include_path(get_include_path().":../project/libraries:../php/libraries:");
 
@@ -51,7 +52,7 @@ $client = new NDB_Client();
 $client->makeCommandLine();
 $client->initialize($configFile);
 
-$db =& Database::singleton();
+$db = \Database::singleton();
 
 /**
  * HELP SCREEN
@@ -78,7 +79,7 @@ if (empty($argv[1]) || $argv[1] == 'help') {
 $action = strtolower($argv[1]);
 
 // CandID
-$candID = $argv[2];
+$candID = new CandID($argv[2]);
 
 // get the rest of the arguments
 switch ($action) {
