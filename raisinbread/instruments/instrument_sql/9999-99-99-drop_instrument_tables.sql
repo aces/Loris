4db6e6c1c475217ee25f6782a6ec5bf835c2d03a
Cecile Madjar <cecile.madjar@gmail.com>
2019-07-31 11:02:48 -0400

    adding the instructions on how to delete the RB tables

---
+++ raisinbread/instruments/instrument_sql/9999-99-99-drop_instrument_tables.sql
@@ -0,0 +1,7 @@
+-- Drop RB instrument tables
+DROP TABLE IF EXISTS `aosi`;
+DROP TABLE IF EXISTS `bmi`;
+DROP TABLE IF EXISTS `medical_history`;
+DROP TABLE IF EXISTS `mri_parameter_form`;
+DROP TABLE IF EXISTS `radiology_review`;
+DROP TABLE IF EXISTS `testtest`;
