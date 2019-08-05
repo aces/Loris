08ddc514343987d2d50fac3d3549899ab7c3d347
kongtiaowang <kongtiaowangshen@gmail.com>
2019-08-05 14:42:26 -0400

    Revert "Revert "remove skip test""
    This reverts commit 40f759d193e0549c87b76bf0715b804c3ffb6011.

--- modules/acknowledgements/test/AcknowledgementsTest.php
+++ modules/acknowledgements/test/AcknowledgementsTest.php
@@ -93,7 +93,6 @@ class AcknowledgementsIntegrationTest extends LorisIntegrationTest
      */
     function testPageLoads()
     {
-$this->markTestSkipped("d");
         $this->safeGet($this->url . "/acknowledgements/");
         $bodyText = $this->getReactElementContent(
                        '.panel:nth-child(3)>.panel-heading'
@@ -108,7 +107,6 @@ $this->markTestSkipped("d");
      */
     function testPageLoadsWithoutPermissions()
     {
-$this->markTestSkipped("d");
         $this->setupPermissions(array("violated_scans_view_allsites"));
         $this->safeGet($this->url . "/acknowledgements/");
         $bodyText = $this->getReactElementContent(
@@ -128,7 +126,6 @@ $this->markTestSkipped("d");
      */
     function testFilterWithData()
     {
-        $this->markTestSkipped("d");
         $this->safeGet($this->url . "/acknowledgements/");
         $this->testFilter(self::$fullName,"1 rows",self::$testData['full_name'],self::$row,self::$btn);
         $this->testFilter(self::$citatioName,"1 rows",self::$testData['citation_name'],self::$row,self::$btn);
