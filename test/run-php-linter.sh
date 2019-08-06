8531a6e2a202f81be091943222c3e8a8249688a5
John Saigle <john.saigle@gmail.com>
2019-08-06 11:51:55 -0400

    Fix phan error in Feedback; add colors to phpcs output

--- test/run-php-linter.sh
+++ test/run-php-linter.sh
@@ -7,7 +7,7 @@ find docs modules htdocs php src -name '*.class.inc' -print0 -o -name '*.php' -p
 
 # Run PHPCS on all .php and .inc files in folders: 
 #  php/, htdocs/, modules/
-vendor/bin/phpcs --standard=docs/LorisCS.xml --extensions=php,inc php/ htdocs/ modules/ --ignore=htdocs/api/*|| exit $?;
+vendor/bin/phpcs --standard=docs/LorisCS.xml --extensions=php,inc --colors php/ htdocs/ modules/ --ignore=htdocs/api/*|| exit $?;
 
 # Run PHPCS on src/ directory using a different ruleset conforming to PSR2.
 vendor/bin/phpcs --standard=docs/SrcCS.xml --extensions=php/php src/ || exit $?;
