2be8218f4f679c1dc626e25202de03a71bc592f8
jesscall <34260251+jesscall@users.noreply.github.com>
2019-08-23 17:12:51 -0400

    [Media Uploader] Bugfix date of administration field (#4999)
    This PR fixes issue reported in #4998. When entering a date in the past and attempting to upload a file, you get an error saying that the date cannot be in the future.
    
    date_diff is used to find the difference between the date entered and now.
    $now - $date returns a positive number if $date is in the past, therefore the PR simply switches the comparison operator.

--- modules/media/ajax/FileUpload.php
+++ modules/media/ajax/FileUpload.php
@@ -414,9 +414,9 @@ function checkDateTaken($dateTaken)
         }
 
         $now  = new DateTime();
-        $diff = date_diff($date, $now)->format("%a");
-        if ($diff > 0) {
-            showMediaError("Date of administration can not be in the future", 400);
+        $diff = intval(date_diff($date, $now)->format("%R%a"));
+        if ($diff < 0) {
+            showMediaError("Date of administration cannot be in the future", 400);
         }
     }
 }
