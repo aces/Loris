7773e3f91e6c3f85a98c40b2196031fb9da0528d
Rida Abou-Haidar <ridz1208@users.noreply.github.com>
2019-08-26 17:48:49 -0400

    Update formHandler.php

--- modules/candidate_parameters/ajax/formHandler.php
+++ modules/candidate_parameters/ajax/formHandler.php
@@ -482,7 +482,8 @@ function editConsentStatusFields($db, $user)
                     // consent date and empty withdrawal date still required
                     // old withdrawal may be filled in
                 if (($oldStatus === null || $oldStatus === 'no') && !empty($date)
-                    && ((empty($oldWithdrawal) && empty($withdrawal)) || (!empty($oldWithdrawal) && !empty($withdrawal)))
+                    && ((empty($oldWithdrawal) && empty($withdrawal)) 
+                       || (!empty($oldWithdrawal) && !empty($withdrawal)))
                 ) {
                     $validated = true;
                 } else if ($oldStatus === 'yes' && !empty($date)
