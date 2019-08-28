e28caffcc5c56866d5484b5048c245409cfb5786
xlecours <xavier.lecoursboucher@mcgill.ca>
2019-08-28 10:20:47 -0400

    missing file

---
+++ modules/conflict_resolver/jsx/fix_conflict_form.js
@@ -0,0 +1,53 @@
+import {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class FixConflictForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.fix = this.fix.bind(this);
+  }
+
+  fix(e) {
+    const conflictid = e.target.name;
+    const correctanswer = e.target.value;
+    fetch(loris.BaseURL.concat('/conflict_resolver/unresolved'), {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({conflicid: conflictid, correctanswer: correctanswer}), // body data type must match "Content-Type" header
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+   }
+
+  render() {
+    const options = [
+      <option ></option>,
+      <option name='1'>{this.props.values[0].value}</option>,
+      <option name='2'>{this.props.values[1].value}</option>,
+    ];
+    return (
+      <td>
+      <form action={loris.BaseURL.concat('/conflict_resolver/fix_conflict')}>
+      <select className='form-control input-sm' name={this.props.conflictid} onChange={this.fix}>
+        {options}
+      </select>
+      </form>
+      </td>
+    );
+  }
+}
+
+FixConflictForm.propTypes = {
+    conflictid: PropTypes.string.isRequired,
+    values: PropTypes.arrayOf(PropTypes.shape({
+     name: PropTypes.string.isRequired,
+     value: PropTypes.string.isRequired,
+   })).isRequired,
+};
+
+export default FixConflictForm;
