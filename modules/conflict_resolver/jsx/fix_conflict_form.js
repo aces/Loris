635587fe25f802d1c6bac7dffc06df0fd9fd5614
xlecours <xavier.lecoursboucher@mcgill.ca>
2019-09-03 15:25:25 -0400

    adding form feedback

--- modules/conflict_resolver/jsx/fix_conflict_form.js
+++ modules/conflict_resolver/jsx/fix_conflict_form.js
@@ -11,6 +11,17 @@ class FixConflictForm extends Component {
   fix(e) {
     const conflictid = e.target.name;
     const correctanswer = e.target.value;
+    const icon = e.target.parentElement.getElementsByTagName('span')[0];
+
+    icon.style.display = 'none';
+
+    try {
+      // Remove the empty option from the options to prevent sending an empty value.
+      e.target.querySelector('option[name="0"]').remove();
+    } catch (e) {
+      // Do nothing, already removed
+    }
+
     fetch(loris.BaseURL.concat('/conflict_resolver/unresolved'), {
         method: 'POST',
         credentials: 'same-origin',
@@ -20,27 +31,42 @@ class FixConflictForm extends Component {
         body: JSON.stringify({conflicid: conflictid, correctanswer: correctanswer}),
     })
     .then((resp) => {
-      // TODO::Add feedback.
-      console.info('value saved');
+      if (resp.status == 200) {
+        icon.className = 'glyphicon glyphicon-ok-circle';
+        icon.style.color = 'green';
+      } else {
+        resp.text().then((text) => {
+          console.error(text);
+        });
+        icon.className = 'glyphicon glyphicon-remove-sign';
+        icon.style.color = 'red';
+      }
     })
     .catch((error) => {
-      // TODO::Add feedback.
       console.error(error);
+      icon.className = 'glyphicon glyphicon-remove-sign';
+      icon.style.color = 'red';
+    })
+    .finally(() => {
+      setTimeout(() => {
+        icon.style.display = 'inline';
+      }, 200);
     });
    }
 
   render() {
     const options = [
-      <option ></option>,
+      <option key='option0' name='0'></option>,
       <option key='option1' name='1'>{this.props.values[0].value}</option>,
       <option key='option2' name='2'>{this.props.values[1].value}</option>,
     ];
     return (
       <td>
         <form action={loris.BaseURL.concat('/conflict_resolver/fix_conflict')}>
-          <select className='form-control input-sm' name={this.props.conflictid} onChange={this.fix}>
-            {options}
-          </select>
+            <span className="glyphicon glyphicon-ok" style={{color: 'green', display: 'none'}} />
+            <select style={{width: '85%', marginLeft: '10px'}} name={this.props.conflictid} onChange={this.fix}>
+              {options}
+            </select>
         </form>
       </td>
     );
