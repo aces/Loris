e1b065d028e5d7624a6444685edf09be0fc66c9a
Alizée Bellerose <misotheism@users.noreply.github.com>
2019-09-09 11:15:44 -0400

    update

--- modules/dashboard/jsx/dashboardIndex.js
+++ modules/dashboard/jsx/dashboardIndex.js
@@ -81,6 +81,7 @@ class Dashboard extends React.Component {
     ).then((response) => response.json())
         .then(
             (data) => {
+              console.log(data);
               this.setState({
                 data: data,
                 isLoaded: true,
