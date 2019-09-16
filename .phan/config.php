05d153ce6478d23f168fe7d2d174f87738f8afee
John Saigle <john.saigle@gmail.com>
2019-09-16 16:01:51 -0400

    Fix tabs to spaces

--- .phan/config.php
+++ .phan/config.php
@@ -1,25 +1,25 @@
 <?php
 
 return [
-    "backward_compatibility_checks" => true,
-	// The docs on quick_mode at
-	// https://github.com/phan/phan/wiki/Incrementally-Strengthening-Analysis
-	// don't seem reasonable. They claim that quick_mode=true add more errors.
-	// This is false on the assumption that that's a typo.
-	// It doesn't seem to have any effect on LORIS's codebase anyways.
-	"quick_mode" => false,
-	"analyze_signature_compatibility" => true,
+"backward_compatibility_checks" => true,
+    // The docs on quick_mode at
+    // https://github.com/phan/phan/wiki/Incrementally-Strengthening-Analysis
+    // don't seem reasonable. They claim that quick_mode=true add more errors.
+    // This is false on the assumption that that's a typo.
+    // It doesn't seem to have any effect on LORIS's codebase anyways.
+    "quick_mode" => false,
+    "analyze_signature_compatibility" => true,
     // The default severity level for phan is 5. After removing all the
     // suppressed rules, we should consider reducing this value to detect more
     // suspicious code.
     "minimum_severity" => 5,
     // TODO: Update this value to the minimum required by LORIS.
     "target_php_version" => "7.2",
-	// FIXME: allow_missing_properties should be false, but there's
-	// too many other things to fix first.
-	"allow_missing_properties" => true,
-	"null_casts_as_any_type" => false,
-	"scalar_implicit_cast" => false,
+    // FIXME: allow_missing_properties should be false, but there's
+    // too many other things to fix first.
+    "allow_missing_properties" => true,
+    "null_casts_as_any_type" => false,
+    "scalar_implicit_cast" => false,
     // The line below is required to prevent PhanUndeclaredVariable problems in
     // the bvl_feedback ajax scripts. They all require() a file in this directory
     // that declares the $feedbackThread variable. Phan doesn't know about this.
@@ -28,13 +28,13 @@ return [
     // see https://github.com/phan/phan/issues/1650.
     //
     // When the module is refactored, this line should be deleted.
-	"ignore_undeclared_variables_in_global_scope" => true,
+    "ignore_undeclared_variables_in_global_scope" => true,
     // FIXME: We should add this.
     "dead_code_detection" => false,
     // FIXME: We should add this. Note that dead_code_detection also covers
     // unused_variable_detection. It might be a good idea to enable this first.
     "unused_variable_detection" => false,
-	"suppress_issue_types" => [
+    "suppress_issue_types" => [
         "PhanTypeInvalidDimOffset",
         "PhanTypePossiblyInvalidDimOffset",
 		"PhanUndeclaredMethod",
@@ -44,26 +44,34 @@ return [
 		"PhanTypeMismatchArgument",
 		"PhanTypeMismatchProperty",
         "PhanTypeArraySuspiciousNullable",
+<<<<<<< HEAD
         "PhanPossiblyUndeclaredVariable",
         "PhanNoopNew",
+		"PhanNonClassMethodCall",
+        "PhanTypeSuspiciousStringExpression"
 	],
 	"analyzed_file_extensions" => ["php", "inc"],
 	"directory_list" => [
-		/* This doesn't include php/installer, because there's
-		   (intentionally) classes in the installer namespace
+		// This doesn't include php/installer, because there's
+		//  (intentionally) classes in the installer namespace
+    ],
+    "analyzed_file_extensions" => ["php", "inc"],
+    "directory_list" => [
+        /* This doesn't include php/installer, because there's
+           (intentionally) classes in the installer namespace
            which redeclare classes from php/libraries, in order
-		   to bootstrap the installer before the config/database
-		   is set up */
-		"php",
-		"htdocs",
-		"modules",
+           to bootstrap the installer before the config/database
+           is set up */
+        "php",
+        "htdocs",
+        "modules",
         "src",
-		"vendor",
+        "vendor",
         "test"
 	],
-	"exclude_analysis_directory_list" => [
-		"vendor"
-	],
+    "exclude_analysis_directory_list" => [
+        "vendor"
+    ],
     "autoload_internal_extension_signatures" => [
         // Xdebug stubs are bundled with Phan 0.10.1+/0.8.9+ for usage,
         // because Phan disables xdebug by default.
