6295aa9e57d00c511c5669cf620f18970a9a427e
Zaliqa <zaliqa.rosli@mcin.ca>
2019-10-09 10:14:20 -0400

    [BVL Feedback] Add dynamictable and fix margin-top (#5089)
    Add carousel to 'Open Thread Summary' table and horizontal scrolling to 'Feedback Threads' table.
    
    Fixes #5074.

--- modules/bvl_feedback/js/bvl_feedback_panel_jquery.js
+++ modules/bvl_feedback/js/bvl_feedback_panel_jquery.js
@@ -3,23 +3,21 @@
  * Created by evanmcilroy on 15-06-09.
  */
 $(document).ready(function() {
+  var candID = $('meta[itemprop="candID"]').attr("context");
+  var sessionID = $('meta[itemprop="sessionID"]').attr("context");
+  var commentID = $('meta[itemprop="commentID"]').attr("context");
 
-    var candID = $('meta[itemprop="candID"]').attr("context");
-    var sessionID = $('meta[itemprop="sessionID"]').attr("context");
-    var commentID = $('meta[itemprop="commentID"]').attr("context");
+  $('.navbar-toggle').on('click',function(event){
+    $("#bvl_feedback_menu").toggleClass("active_panel");
+    $("#bvl_panel_wrapper").toggleClass("bvl_panel");
 
-    $('.navbar-toggle').on('click',function(event){
-        $("#bvl_feedback_menu").toggleClass("active_panel");
-        $("#bvl_panel_wrapper").toggleClass("bvl_panel");
-
-	//We check if a sidebar exists on the page and toggle it if such.
-	if ($("#page_wrapper_sidebar").length){
+	  //We check if a sidebar exists on the page and toggle it if such.
+	  if ($("#page_wrapper_sidebar").length){
 	    $("#sidebar-wrapper").toggle("#sidebar-wrapper hide_sidebar");
 	    $("#page_wrapper_sidebar").toggleClass("wrapper");
-	}
-
-    });
-
+	  }
+    $('.dynamictable').DynamicTable();
+  });
 }); // end of document
 
 
