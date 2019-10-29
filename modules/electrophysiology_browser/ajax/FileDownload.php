ac55d951326e16289eeffcc5f8853ca64caeb0a7
cmadjar <cecile.madjar@gmail.com>
2019-10-29 16:57:19 -0400

    tried to do the file download using a PHP script but somehow for now it is telling me 'No webpage was found for the web address http://localhost:8080/electrophysiology_browser/ajax/FileDownload.php?File=bids_imports/Face13_BIDSVersion_1.1.0/sub-OTT166/ses-V1/eeg/sub-OTT166_ses-V1_task-faceO_eeg.edf'. Not sure how to fix this... Tomorrow?

---
+++ modules/electrophysiology_browser/ajax/FileDownload.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Electrophysiology downloader.
+ *
+ * This ensures that the file exists and the user is logged in to
+ * LORIS before trying to return the file to the user.
+ *
+ * PHP Version 5
+ *
+ * @category Loris
+ * @package  Media
+ * @author   Alex I. <ailea.mcin@gmail.com>
+ * @license  Loris license
+ * @link     https://github.com/aces/Loris-Trunk
+ */
+
+$user =& User::singleton();
+//NOTE Should this be 'media_read' instead? It seems that downloading files
+//should be a read permission, not write.
+
+$hasAccess = $user->hasPermission('electrophysiology_browser_view_allsites')
+    || ($user->hasCenter($timePoint->getCenterID())
+        && $user->hasPermission('electrophysiology_browser_view_site'));
+
+if (!$hasAccess) {
+    header("HTTP/1.1 403 Forbidden");
+    exit;
+}
+
+// Make sure that the user isn't trying to break out of the $path
+// by using a relative filename.
+$file     = basename($_GET['File']);
+$config   =& NDB_Config::singleton();
+$path     = $config->getSetting('imagePath');
+$filePath = $path . $file;
+
+if (!file_exists($filePath)) {
+    error_log("ERROR: File $filePath does not exist");
+    header("HTTP/1.1 404 Not Found");
+    exit(5);
+}
+
+// Output file in downloadable format
+header('Content-Description: File Transfer');
+header('Content-Type: application/force-download');
+header("Content-Transfer-Encoding: Binary");
+header("Content-disposition: attachment; filename=\"" . basename($filePath) . "\"");
+readfile($filePath);
