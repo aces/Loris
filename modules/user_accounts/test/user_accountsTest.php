b866792790ec2160adde7566111910dda991d4fd
John Saigle <john.saigle@gmail.com>
2019-11-11 13:35:14 -0500

    Finish integration tests

--- modules/user_accounts/test/user_accountsTest.php
+++ modules/user_accounts/test/user_accountsTest.php
@@ -23,12 +23,26 @@ require_once __DIR__
  */
 class UserAccountsIntegrationTest extends LorisIntegrationTest
 {
-    private const UNIT_TESTER_EMAIL = 'tester@example.com'
-    private const UNIT_TESTER_EMAIL_NEW = 'tester@example.com'
-    private const FORM_FIELD_PASSWORD = 'Password_hash';
-    private const FORM_FIELD_CONFIRM_PASSWORD = '__Confirm';
+    // The paths to the pages to which the form must submit.
+    private const FILEPATH_EDITUSER      = 'user_accounts';
+    private const FILEPATH_MYPREFERENCES = 'user_accounts/my_preferences';
+    // The names of the form elements for the Password and Confirm Password
+    // fields.
+    private const FORM_FIELD_PASSWORD        = 'Password_hash';
+    private const FORM_FIELD_CONFIRMPASSWORD = '__Confirm';
+    // Regular (non-admin) user details
+    private const UNITTESTER_USERNAME  = 'UnitTester';
+    private const UNITTESTER_REALNAME  = 'Unit Tester';
+    private const UNITTESTER_EMAIL     = 'tester@example.com';
+    private const UNITTESTER_EMAIL_NEW = 'newemail@example.com';
+    // Admin user details
+    private const ADMIN_USERNAME  = 'admin';
+    private const ADMIN_REALNAME  = 'Admin account';
+    private const ADMIN_EMAIL     = 'admin@example.com';
+    private const ADMIN_EMAIL_NEW = 'tester@example.com';
 
-    private static $_UNIT_TESTER = array(
+
+    private static $_UNITTESTER = array(
         'Data Coordinating Center',
         'UnitTester',
         'Unit Tester',
@@ -36,7 +50,7 @@ class UserAccountsIntegrationTest extends LorisIntegrationTest
         'Y',
         'N',
     );
-    private static $_ADMIN       = array(
+    private static $_ADMIN      = array(
         'Data Coordinating Center',
         'admin',
         'Admin account',
@@ -174,30 +188,35 @@ class UserAccountsIntegrationTest extends LorisIntegrationTest
      */
     function testUserAccountEdits()
     {
+        // Test changing first name
         $this->_verifyUserModification(
             'user_accounts',
             'UnitTester',
             'First_name',
             'NewFirst'
         );
+        // Test changing last name
         $this->_verifyUserModification(
             'user_accounts',
             'UnitTester',
             'Last_name',
             'NewLast'
         );
+        // Test changing 'Active' status
         $this->_verifyUserModification(
             'user_accounts',
             'UnitTester',
             'Active',
             'No'
         );
+        // Test changing Email
         $this->_verifyUserModification(
             'user_accounts',
             'UnitTester',
             'Email',
             'newemail@example.com'
         );
+        // Test changing Approval status
         $this->_verifyUserModification(
             'user_accounts',
             'UnitTester',
@@ -231,6 +250,35 @@ class UserAccountsIntegrationTest extends LorisIntegrationTest
             'newemail@example.com'
         );
     }
+
+    /**
+     * Ensure that password errors are successfully triggered on the
+     * My Preferences page.
+     *
+     * @return void
+     */
+    function testMyPreferencesPasswordErrors()
+    {
+        $this->_verifyPasswordErrors(
+            self::FILEPATH_MYPREFERENCES,
+            self::UNITTESTER_USERNAME
+        );
+    }
+
+    /**
+     * Ensure that password errors are successfully triggered on the Edit User
+     * page.
+     *
+     * @return void
+     */
+    function testEditUserPasswordErrors()
+    {
+        $this->_verifyPasswordErrors(
+            self::FILEPATH_EDITUSER,
+            self::UNITTESTER_USERNAME
+        );
+    }
+
     /**
      * Tests that the creation of a new user works.
      *
@@ -298,7 +346,7 @@ class UserAccountsIntegrationTest extends LorisIntegrationTest
     function _verifyUserModification($page, $userId, $fieldName, $newValue)
     {
         // Submit the change
-        $this->submitUserData($page, $userId, $fieldName, $newValue);
+        $this->submit($page, $userId);
 
         // Verify changes appear on the page
         $this->_accessUser($page, $userId);
@@ -316,6 +364,11 @@ class UserAccountsIntegrationTest extends LorisIntegrationTest
 
     /**
      * Submit user data to the form specified by $page.
+     *
+     * @param string $page   The page to submit to.
+     * @param string $userId ID of the user to modify.
+     *
+     * @return void
      */
     function submit($page, $userId): void
     {
@@ -331,10 +384,22 @@ class UserAccountsIntegrationTest extends LorisIntegrationTest
             $projectsOption  = new WebDriverSelect($projectsElement);
             $projectsOption->selectByValue("1");
         }
+        // 'fire_away' is the name of the Submit button on the form.
         $this->safeClick(WebDriverBy::Name('fire_away'));
     }
 
-    function setValue($fieldName, $newValue): void {
+    /**
+     * Finds a field on the page and changes its value.
+     * This function should be followed by a submission of a form in order
+     * to actually take effect.
+     *
+     * @param string $fieldName The CSS name of the field.
+     * @param string $newValue  The new value for that field.
+     *
+     * @return void
+     */
+    function setValue($fieldName, $newValue): void
+    {
         $field = $this->safeFindElement(WebDriverBy::Name($fieldName));
         if ($field->getTagName() == 'input') {
             $field->clear();
@@ -345,54 +410,146 @@ class UserAccountsIntegrationTest extends LorisIntegrationTest
         }
     }
 
-    function verifyPasswordNotEqualToEmail($page, $userId): void {
+    /**
+     * Runs all of the password tests for a page.
+     *
+     * @param string $page   The page to submit to.
+     * @param string $userId The user to edit
+     *
+     * @return void
+     */
+    function _verifyPasswordErrors($page, $userId): void
+    {
+        $this->_verifyPasswordMustNotEqualEmail($page, $userId);
+        $this->_verifyPasswordAndConfirmPasswordMustMatch($page, $userId);
+        $this->_verifyNewPasswordMustBeDifferent($page, $userId);
+    }
+
+    /**
+     * Ensures that the user cannot set their password to be the same value
+     * as their email address.
+     *
+     * @param string $page   The page to submit to.
+     * @param string $userId The user to edit
+     *
+     * @return void
+     */
+    function _verifyPasswordMustNotEqualEmail($page, $userId): void
+    {
+        // Make sure the user's email is set to a known value
+        $this->_verifyUserModification(
+            $page,
+            'UnitTester',
+            'Email',
+            self::UNITTESTER_EMAIL_NEW
+        );
+        // Try changing the password to the same value.
+        $this->_sendPasswordValues($page, $userId, self::UNITTESTER_EMAIL_NEW);
+        // This text comes from the class constants in Edit User/My Preferences
+        assertContains('cannot be your email', $this->getBody());
+    }
+
+    /**
+     * Ensures that the module checks that the password and confirm password
+     * field match.
+     *
+     * @param string $page   The page to submit to.
+     * @param string $userId The user to edit
+     *
+     * @return void
+     */
+    function _verifyPasswordAndConfirmPasswordMustMatch($page, $userId): void
+    {
+        // Send two different random strings to the password and confirm
+        // password values.
+        $this->_sendPasswordValues(
+            $page,
+            $userId,
+            \Utility::randomString(),
+            \Utility::randomString()
+        );
+        // This text comes from the class constants in Edit User/My Preferences
+        assertContains('do not match', $this->getBody());
+    }
+
+    /**
+     * Ensures that the module checks that the password and confirm password
+     * field match.
+     *
+     * @param string $page   The page to submit to.
+     * @param string $userId The user to edit
+     *
+     * @return void
+     */
+    function _verifyNewPasswordMustBeDifferent($page, $userId): void
+    {
+        $newPassword = \Utility::randomString();
+        // Change the user's password to $newPassword
+        $this->_sendPasswordValues(
+            $page,
+            $userId,
+            $newPassword
+        );
+        // Change the password again using the same value. This should cause
+        // and error.
+        $this->_sendPasswordValues(
+            $page,
+            $userId,
+            $newPassword
+        );
+        // This text comes from the class constants in Edit User/My Preferences
+        assertContains('New and old passwords are identical', $this->getBody());
+    }
+
+    /**
+     * Loads a module, sets the password and confirm password value, submits
+     * the form, and reloads the page.
+     *
+     * @param string $page            The module to load
+     * @param string $userId          The user to edit.
+     * @param string $password        The plaintext password to use
+     * @param string $confirmPassword The plaintext password to use. Will be
+     *                                set equal to $password by default.
+     *
+     * @return void
+     */
+    function _sendPasswordValues(
+        string $page,
+        string $userId,
+        string $password,
+        string $confirmPassword = ''
+    ): void {
+        // Go to page
         $this->_accessUser($page, $userId);
-        // Set Password and Confirm Password equal to the user's email
+        // Set Password and Confirm Password form values to be equal to the
+        // user's email.
         $this->setValue(
             self::FORM_FIELD_PASSWORD,
-            self::UNIT_TESTER_EMAIL_NEW
+            self::UNITTESTER_EMAIL_NEW
         );
         $this->setValue(
-            self::FORM_FIELD_PASSWORD_CONFIRM,
-            self::UNIT_TESTER_EMAIL_NEW
+            self::FORM_FIELD_CONFIRMPASSWORD,
+            ($confirmPassword === '') ? $password : $confirmPassword,
         );
         $this->submit(
             $page,
             $userId
         );
-        
+
         // Reload
         $this->_accessUser($page, $userId);
-        assertContains('', $this->getBody());
     }
 
-    function getBody() {
+    /**
+     * Returns the body text of the page.
+     *
+     * @return string The HTML body.
+     */
+    function getBody(): string
+    {
         return $this->safeFindElement(
             WebDriverBy::cssSelector("body")
         )->getText();
-    }
-
-    function verifyPasswordErrors($page, $userId): void {
-        $this->_accessUser($page, $userId);
-        // Set the password 
-        $passwordElement = $this->safeFindElement(
-            WebDriverBy::Name('Password_hash')
-        );
-        $confirmPasswordElement = $this->safeFindElement(
-            WebDriverBy::Name('__Confirm')
-        );
-
-        $passwordValue = $passwordElement->getAttribute('value');
-        $confirmPasswordValue = $confirmPasswordElement->getAttribute('value');
-
-        
-        $this->submitUserData(
-            'user_accounts',
-            'UnitTester',
-            'Email',
-            'newemail@example.com'
-        );
-
     }
 
     /**
