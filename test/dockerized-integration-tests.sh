eec8d83b0698a045f35650a36aeca8142113b145
John Saigle <john.saigle@gmail.com>
2019-11-13 16:04:21 -0500

    oops I deleted most of the integration tests

--- test/dockerized-integration-tests.sh
+++ test/dockerized-integration-tests.sh
@@ -8,4 +8,7 @@ else
     CONTAINER=integration-tests
 fi
 
+# Core integration tests
 docker-compose run -T -e HEADLESS=true --rm ${CONTAINER} vendor/bin/phpunit --configuration test/phpunit.xml --testsuite LorisCoreIntegrationTests $*
+# module integration tests
+docker-compose run -T --rm ${CONTAINER} vendor/bin/phpunit --configuration test/phpunit.xml --testsuite LorisModuleIntegrationTests $*
