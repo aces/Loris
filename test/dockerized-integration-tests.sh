7e72915716a4ec9239519f4d0b9a62db9ef13e48
John Saigle <john.saigle@gmail.com>
2019-11-13 14:09:38 -0500

    add headless

--- test/dockerized-integration-tests.sh
+++ test/dockerized-integration-tests.sh
@@ -8,5 +8,4 @@ else
     CONTAINER=integration-tests
 fi
 
-docker-compose run -T --rm ${CONTAINER} vendor/bin/phpunit --configuration test/phpunit.xml --testsuite LorisCoreIntegrationTests $*
-docker-compose run -T --rm ${CONTAINER} vendor/bin/phpunit --configuration test/phpunit.xml --testsuite LorisModuleIntegrationTests $*
+docker-compose run -T -e HEADLESS=true --rm ${CONTAINER} vendor/bin/phpunit --configuration test/phpunit.xml --testsuite LorisCoreIntegrationTests $*
