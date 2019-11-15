8fc11f8b4ead7f7d5f77705965f39362bd693ae8
Alizée Bellerose <misotheism@users.noreply.github.com>
2019-11-15 16:25:40 -0500

    update with shen help

--- modules/instrument_builder/jsx/react.questions.js
+++ modules/instrument_builder/jsx/react.questions.js
@@ -37,6 +37,8 @@ class LorisElement extends Component {
       case 'label':
         elementHtml = <p>{element.Description}</p>;
         break;
+      case 'line':
+        elementHtml = <div></div>;
       case 'score':
         elementHtml = <StaticElement text={0} label={element.Description} />;
 
@@ -779,6 +781,7 @@ class AddElement extends Component {
     // Setup the desired element to be added
     switch (selected) {
       case 'header':
+      case 'line':
       case 'label':
         questionName = '';
         break;
@@ -859,6 +862,7 @@ class AddElement extends Component {
         // Set the inputs to display based on the desired element type
     switch (this.state.selected.id) {
       case 'header':
+      case 'line':
       case 'label':
         questionInput = <QuestionText updateState={this.updateState} element={this.state}/>;
         break;
