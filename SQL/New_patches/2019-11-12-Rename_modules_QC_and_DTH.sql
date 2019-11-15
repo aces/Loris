e3e9ab63ba379f532ed58fbd06615a28db8284f1
Xavier Lecours <xavier.lecours@mcin.ca>
2019-11-15 14:48:11 -0500

    [SQL] using variable instead of subselect (#5652)
    Modify the SQL patch to avoid the mysql server error:
    
    ERROR 1093 (HY000): You can't specify target table 'LorisMenu' for update in FROM clause

--- SQL/New_patches/2019-11-12-Rename_modules_QC_and_DTH.sql
+++ SQL/New_patches/2019-11-12-Rename_modules_QC_and_DTH.sql
@@ -1,12 +1,22 @@
 -- rename modules and change order number according to the new parent menu they belong to
-UPDATE LorisMenu SET Label='Behavioural Quality Control', Link='behavioural_qc/', Parent=(SELECT ID FROM LorisMenu WHERE Label='Clinical'), OrderNumber=(SELECT MAX(OrderNumber)+1 FROM LorisMenu WHERE Parent=(SELECT ID FROM LorisMenu WHERE Label='Clinical')) WHERE Link='data_team_helper/';
-UPDATE LorisMenu SET Label='Imaging Quality Control', Link='imaging_qc/', Parent=(SELECT ID FROM LorisMenu WHERE Label='Imaging'), OrderNumber=(SELECT MAX(OrderNumber)+1 FROM LorisMenu WHERE Parent=(SELECT ID FROM LorisMenu WHERE Label='Imaging')) WHERE Link='quality_control/';
+SELECT MAX(OrderNumber)+1 INTO @ordernumber FROM LorisMenu WHERE Parent=(SELECT ID FROM LorisMenu WHERE Label='Clinical');
+SELECT ID INTO @parentid FROM LorisMenu WHERE Label='Clinical';
+
+UPDATE LorisMenu SET Label='Behavioural Quality Control', Link='behavioural_qc/', Parent=@parentid, OrderNumber=@ordernumber  WHERE Link='data_team_helper/';
+
+SELECT MAX(OrderNumber)+1 INTO @ordernumber FROM LorisMenu WHERE Parent=(SELECT ID FROM LorisMenu WHERE Label='Imaging');
+SELECT ID INTO @parentid FROM LorisMenu WHERE Label='Imaging';
+UPDATE LorisMenu SET Label='Imaging Quality Control', Link='imaging_qc/', Parent=@parentid, OrderNumber=@ordernumber WHERE Link='quality_control/';
 
 -- change location of Genomic Browser to place right after imaging tab
-UPDATE LorisMenu SET OrderNumber=(OrderNumber+1) WHERE Parent IS NULL AND OrderNumber>(SELECT OrderNumber FROM LorisMenu WHERE Label='Imaging');
+SELECT OrderNumber INTO @ordernumber FROM LorisMenu WHERE Label='Imaging';
+UPDATE LorisMenu SET OrderNumber=(OrderNumber+1) WHERE Parent IS NULL AND OrderNumber>@ordernumber;
+
 INSERT INTO LorisMenu (Label, OrderNumber)
 SELECT 'Genomics', OrderNumber+1 FROM LorisMenu WHERE Label='Imaging';
-UPDATE LorisMenu SET Parent=(SELECT ID FROM LorisMenu WHERE Label='Genomics') WHERE Link='genomic_browser/';
+
+SELECT ID INTO @parentid FROM LorisMenu WHERE Label='Genomics';
+UPDATE LorisMenu SET Parent=@parentid WHERE Link='genomic_browser/';
 
 UPDATE permissions SET Code='quality_control', description='Quality Control access' WHERE code='data_team_helper';
 
