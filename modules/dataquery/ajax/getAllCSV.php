5175d6a60d67a748b5250a6eaebc57cf05a48287
Alizée Bellerose <misotheism@users.noreply.github.com>
2020-01-07 14:25:55 -0500

    update

--- modules/dataquery/ajax/getAllCSV.php
+++ modules/dataquery/ajax/getAllCSV.php
@@ -2,15 +2,15 @@
 
 $user =& User::singleton();
 if (!$user->hasPermission('dataquery_view')) {
-    header("HTTP/1.1 403 Forbidden");
+    header('HTTP/1.1 403 Forbidden');
     exit;
 }
 require_once __DIR__ . '/../../../vendor/autoload.php';
 
 $DB    = \Database::singleton();
 $query = $_GET['type'] === 'PSCID'
-    ? 'Select PSCID as `value` from candidate'//' limit 10'
-    : 'Select CandID as `value` from candidate';//' limit 10';
+    ? 'Select PSCID as `value` from candidate'
+    : 'Select CandID as `value` from candidate';
 
 $statement = $DB->prepare($query);
 $results   = $DB->execute($statement, array());
@@ -18,31 +18,6 @@ $results   = $DB->execute($statement, array());
 $sessions = array();
 
 foreach ($results as &$val) {
-//    $val['candidates'] = array(
-//        'allCandiates' => array(
-//            $val['value'] => array(
-//                'V1', 'V2', 'V3'
-//            )
-//        ),
-//        'allSessions' => array(
-//            'V1' => array($val['value']),
-//            'V2' => array($val['value']),
-//            'V3' => array($val['value']),
-//        )
-//    );
-//    $val['fields'] = array(
-//        array(
-//            'id' => 'DataDictionary:Demographics',
-//            'key' => ['demographics', 'bread_consent'],
-//            'value' => array(
-//                array('Description' => 'Consent to Bread'),
-//                array('Type' => "enum('yes','no')")
-//            )
-//        ),
-//        array(
-//
-//        )
-//    );
     $val['field'] = $_GET['type'] === 'PSCID'
     ? 'PSCID'
     : 'CandID';
