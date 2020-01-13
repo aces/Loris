c7243e97daecb3b711caff5ae3a8d34ac5b96312
John Saigle <john.saigle@gmail.com>
2020-01-13 10:52:32 -0500

    Update patch to retain status of expired passwords

--- SQL/New_patches/2019-12-17-RemovePasswordExpiry.sql
+++ SQL/New_patches/2019-12-17-RemovePasswordExpiry.sql
@@ -1,2 +1,3 @@
-ALTER TABLE users DROP COLUMN Password_expiry;
 ALTER TABLE users ADD COLUMN Password_expired TINYINT(1) NOT NULL DEFAULT 0;
+UPDATE users SET Password_expired=1 WHERE Password_expiry < CURDATE();
+ALTER TABLE users DROP COLUMN Password_expiry;
