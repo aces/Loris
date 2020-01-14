62c97a6fc6643682bd52d5a2993395f18683906f
John Saigle <john.saigle@gmail.com>
2020-01-14 16:30:49 -0500

    Update patch

---
+++ SQL/New_patches/2019-09-18_DocRepoEdit.sql
@@ -0,0 +1,1 @@
+INSERT INTO `permissions` VALUES (56,'document_repository_edit','Upload and edit files in Document Repository','2');
--- SQL/New_patches/2019-09-18_DocRepoUploadPath.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber) SELECT 'docRepoPath', 'File path to folder where document repository files will be uploaded to and downloaded from.', 1, 0, 'web_path', ID, 'Document Repository file folder.', 27 FROM ConfigSettings WHERE Name="paths";
-INSERT INTO Config (ConfigID, Value) SELECT ID, '/data/document_repository_uploads/'  FROM ConfigSettings WHERE Name="docRepoPath";
-
-
-INSERT INTO `permissions` VALUES (56,'document_repository_edit','Upload and edit files in Document Repository','2');
