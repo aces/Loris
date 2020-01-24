380e1f71e3b50ec7769e44091403fa1c559d39de
zaliqarosli <zaliqa.rosli@mcin.ca>
2020-01-24 12:18:16 -0500

    add card component

---
+++ jsx/Card.js
@@ -0,0 +1,62 @@
+/**
+ * This file contains React component for Card
+ *
+ * @author Zaliqa Rosli
+ * @version 1.0.0
+ *
+ */
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+import Panel from 'jsx/Panel';
+
+/**
+ * Card component - Wraps children in a customizable card
+ *
+ */
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.props.onClick(e);
+  }
+
+  render() {
+    const cursorStyle = this.props.onClick ? {
+      cursor: 'pointer',
+    } : null;
+    return (
+      <div style={{marginLeft: '5px', marginRight: '5px'}}>
+        <Panel
+          id={this.props.id}
+          title={this.props.title}
+          initCollapsed={this.props.initCollapsed}
+        >
+          <div
+            onClick={this.handleClick}
+            style={cursorStyle}
+          >
+            {this.props.children}
+          </div>
+        </Panel>
+      </div>
+    );
+  }
+}
+
+Card.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  initCollapsed: PropTypes.bool,
+};
+
+Card.defaultProps = {
+  onClick: null,
+  initCollapsed: false,
+};
+
+export default Card;
