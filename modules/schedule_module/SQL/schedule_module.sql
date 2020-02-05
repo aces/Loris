044f0b1f75e9ebf5103bb0a6faa5c96781e588ee
kongtiaowang <kongtiaowangshen@gmail.com>
2020-02-05 11:19:46 -0500

    patch

--- modules/schedule_module/SQL/schedule_module.sql
+++ modules/schedule_module/SQL/schedule_module.sql
@@ -1,117 +1,40 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2.1
--- http://www.phpmyadmin.net
---
--- Host: ace-db-2.cbrain.mcgill.ca:3306
--- Generation Time: Oct 05, 2018 at 12:28 AM
--- Server version: 5.7.23
--- PHP Version: 7.0.32-1+ubuntu16.04.1+deb.sury.org+1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Table structure for table `appointment_type`
---
-
+-- Create appointment_type table
 CREATE TABLE `appointment_type` (
   `AppointmentTypeID` int(10) UNSIGNED NOT NULL,
-  `Name` varchar(32) NOT NULL
+  `Name` varchar(32) NOT NULL,
+  PRIMARY KEY (`AppointmentTypeID`),
+  UNIQUE KEY (`Name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `appointment_type`
---
 
+-- Insert initial data
 INSERT INTO `appointment_type` (`AppointmentTypeID`, `Name`) VALUES
 (3, 'Behavioral'),
 (2, 'Blood Collection'),
-(1, 'MRI');
+(1, 'MRI'); 
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `appointment_type`
---
-ALTER TABLE `appointment_type`
-  ADD PRIMARY KEY (`AppointmentTypeID`),
-  ADD UNIQUE KEY `Name` (`Name`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2.1
--- http://www.phpmyadmin.net
---
--- Host: ace-db-2.cbrain.mcgill.ca:3306
--- Generation Time: Oct 05, 2018 at 12:27 AM
--- Server version: 5.7.23
--- PHP Version: 7.0.32-1+ubuntu16.04.1+deb.sury.org+1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Table structure for table `appointment`
---
-
+-- Create appointment table
 CREATE TABLE `appointment` (
-  `AppointmentID` int(10) UNSIGNED NOT NULL,
+  `AppointmentID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `SessionID` int(10) UNSIGNED NOT NULL,
   `AppointmentTypeID` int(10) UNSIGNED NOT NULL,
-  `StartsAt` datetime NOT NULL
+  `StartsAt` datetime NOT NULL,
+  PRIMARY KEY (`AppointmentID`),
+  KEY `AppointmentTypeID` (`AppointmentTypeID`),
+  KEY `SessionID` (`SessionID`),
+  CONSTRAINT `appointment_belongsToSession` FOREIGN KEY (`SessionID`) REFERENCES `session` (`ID`),
+  CONSTRAINT `appointment_hasAppointmentType` FOREIGN KEY (`AppointmentTypeID`) REFERENCES `appointment_type` (`AppointmentTypeID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Indexes for table `appointment`
---
-ALTER TABLE `appointment`
-  ADD PRIMARY KEY (`AppointmentID`),
-  ADD KEY `AppointmentTypeID` (`AppointmentTypeID`),
-  ADD KEY `SessionID` (`SessionID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `appointment`
---
-ALTER TABLE `appointment`
-  MODIFY `AppointmentID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `appointment`
---
-ALTER TABLE `appointment`
-  ADD CONSTRAINT `appointment_belongsToSession` FOREIGN KEY (`SessionID`) REFERENCES `session` (`ID`),
-  ADD CONSTRAINT `appointment_hasAppointmentType` FOREIGN KEY (`AppointmentTypeID`) REFERENCES `appointment_type` (`AppointmentTypeID`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
+-- Insert data for schedule_module
 INSERT INTO `permissions` (`code`, `description`, `categoryID`) values ('schedule_module', 'Schedule Module', 2);
 INSERT INTO LorisMenu (Label, Link, Parent, OrderNumber) VALUES
 ('Schedule Module', 'schedule_module/', (SELECT ID FROM LorisMenu as L WHERE Label='Tools'), 11);
 INSERT INTO LorisMenuPermissions (MenuID, PermID)
     SELECT m.ID, p.PermID FROM permissions p CROSS JOIN LorisMenu m WHERE p.code='schedule_module' AND m.Label='Schedule Module';
+
+-- Insert schedule_module id into user_perm_rel table
+INSERT INTO `user_perm_rel` (userID, permID)
+  SELECT u.ID, p.permID 
+  FROM users u JOIN permissions p 
+  WHERE u.userid = 'admin' and p.code = 'schedule_module';
