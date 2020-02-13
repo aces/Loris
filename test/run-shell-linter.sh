14f104c1065723cf442fd9fb0ae4dc1c95d720b8
John Saigle <john.saigle@gmail.com>
2020-02-13 14:01:18 -0500

    Use Gabriel's fix

--- test/run-shell-linter.sh
+++ test/run-shell-linter.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 shopt -s globstar nullglob
-GLOBIGNORE="vendor*:node_modules*"
+GLOBIGNORE="vendor*:node_modules*:.git*"
 
 declare -a excluded=(
     'test/dockerized-integration-tests.sh'
@@ -13,23 +13,23 @@ declare -a excluded=(
     'test/run-js-linter.sh'
     'test/wait-for-services.sh'
     'tools/gen-version.sh'
-    'tools/install.sh'
     'tools/create-project.sh'
     'tools/deprecated/dump_all_tables_data_only.sh'
     'tools/package_files.sh'
 )
 
-# Get all files ending with .sh that are not in the following folders:
-#   - vendor
-#   - node_modules
-#   - tools/deprecated
-#scripts=$(find . -not -path "./.git/*" -not -path "./node_modules/*" -not -path "./vendor/*" -not -path "./tools/deprecated/*" -name '*.sh')
-#scripts=$(find . -not -path "./.git/*" -not -path "./node_modules/*" -not -path "./vendor/*" -not -path "./tools/deprecated/*" -name '*.sh')
+#Build array of files
+files=(**/*.{sh,ksh,bash})
 
-scripts=()
-while IFS= read -d $'\0' -r file ; do
-    scripts=("${scripts[@]}" "$file")
-done < <(find . -not -path "./.git/*" -not -path "./node_modules/*" -not -path "./vendor/*" -not -path "./tools/deprecated/*" -name '*.sh' -print0)
+#Delete array elements present in exclude
+for exclude in "${excluded[@]}"; do
+  files=( "${files[@]/${exclude}}" )
+done
 
-echo "${scripts[@]}"
-shellcheck "${scripts[@]}"
+#Cleanup null array entries after delete
+for i in "${!files[@]}"; do
+  [[ -n "${files[$i]}" ]] || unset "files[$i]"
+done
+
+#Run shellcheck on files
+shellcheck "${files[@]}"
