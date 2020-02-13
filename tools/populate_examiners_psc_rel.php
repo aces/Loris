133dcb8fce42e23e5d724d440a20a77cef168027
John Saigle <4022790+johnsaigle@users.noreply.github.com>
2020-02-13 09:33:19 -0500

    [Tools] Enable PHPCS for populate_examiners_psc_rel.php (#5996)

--- tools/populate_examiners_psc_rel.php
+++ tools/populate_examiners_psc_rel.php
@@ -4,13 +4,13 @@
  * This script is written for a one time use only to populate the examiners_psc_rel
  * table in the database after decoupling the examiners from the CenterID.
  *
- * affected tables:
+ * Affected tables:
  *  - all instrument tables
  *  - examiners
  *  - examiners_psc_rel
  *  - certification
  *
- * PHP Version 5
+ * PHP Version 7
  *
  * @category Main
  * @package  Loris
@@ -20,9 +20,6 @@
  */
 require_once 'generic_includes.php';
 
-$client = new NDB_Client();
-$client->makeCommandLine();
-$client->initialize("../project/config.xml");
 $config = NDB_Config::singleton();
 $db     =& Database::singleton();
 
@@ -46,8 +43,9 @@ $pscCheck = $db->pselect(
 );
 if (!empty($pscCheck)) {
     $errorPRE =true;
-    foreach ($pscCheck as $k=>$row) {
-        echo "examiners table contains site ID: ".$row['centerID']." which does not exist in the database (psc).\n";
+    foreach ($pscCheck as $k => $row) {
+        echo "examiners table contains site ID: ".$row['centerID']
+            ." which does not exist in the database (psc).\n";
     }
 }
 ///////// examiners.centerID is null
@@ -57,8 +55,9 @@ $pscNullCheck = $db->pselect(
 );
 if (!empty($pscNullCheck)) {
     $errorPRE =true;
-    foreach ($pscNullCheck as $k=>$row) {
-        echo "examiners table contains NULL sites for examinerID: $row[examinerID]\n";
+    foreach ($pscNullCheck as $k => $row) {
+        echo "examiners table contains NULL sites for examinerID: " .
+            "$row[examinerID]\n";
     }
 }
 ///////// examiners.full_name is null
@@ -68,8 +67,9 @@ $nameNullCheck = $db->pselect(
 );
 if (!empty($nameNullCheck)) {
     $errorPRE =true;
-    foreach ($nameNullCheck as $k=>$row) {
-        echo "examiners table contains NULL names for examinerID: $row[examinerID]\n";
+    foreach ($nameNullCheck as $k => $row) {
+        echo "examiners table contains NULL names for examinerID: "
+            . "$row[examinerID]\n";
     }
 }
 ///////// examiners.full_name is null
@@ -79,8 +79,9 @@ $activeNullCheck = $db->pselect(
 );
 if (!empty($activeNullCheck)) {
     $errorPRE =true;
-    foreach ($activeNullCheck as $k=>$row) {
-        echo "examiners table contains NULL active statuses for examinerID: $row[examinerID]\n";
+    foreach ($activeNullCheck as $k => $row) {
+        echo "examiners table contains NULL active statuses for examinerID: "
+            . "$row[examinerID]\n";
     }
 }
 ///////// examiners.full_name is null
@@ -90,37 +91,42 @@ $pendingNullCheck = $db->pselect(
 );
 if (!empty($pendingNullCheck)) {
     $errorPRE =true;
-    foreach ($pendingNullCheck as $k=>$row) {
-        echo "examiners table contains NULL pending approval statuses for examinerID: $row[examinerID]\n";
+    foreach ($pendingNullCheck as $k => $row) {
+        echo "examiners table contains NULL pending approval statuses for "
+            . "examinerID: $row[examinerID]\n";
     }
 }
 ///////// certification.examinerID NOT in examiners.examinerID
 $certCheck = $db->pselect(
-    "SELECT DISTINCT examinerID FROM certification WHERE examinerID NOT IN (SELECT examinerID FROM examiners)",
+    "SELECT DISTINCT examinerID 
+     FROM certification 
+     WHERE examinerID NOT IN (SELECT examinerID FROM examiners)",
     array()
 );
 if (!empty($certCheck)) {
     $errorPRE =true;
-    foreach ($certCheck as $k=>$row) {
-        echo "Certification table contains examiner ID: ".$row['examinerID']." which does not exist in the database (examiners).\n";
+    foreach ($certCheck as $k => $row) {
+        echo "Certification table contains examiner ID: ".$row['examinerID']
+            ." which does not exist in the database (examiners).\n";
     }
 }
 ///////// instrument.Examiner NOT IN examiners.examinerID
-foreach ($instruments as $table=>$name) {
+foreach ($instruments as $table => $name) {
     if ($db->tableExists($table) && $db->columnExists($table, 'Examiner')) {
         $instCheck = $db->pselect(
-            "SELECT * FROM ".$table." WHERE Examiner NOT IN (SELECT examinerID FROM examiners)",
+            "SELECT * FROM ".$table
+            ." WHERE Examiner NOT IN (SELECT examinerID FROM examiners)",
             array()
         );
         if (!empty($instCheck)) {
             $errorPRE =true;
-            foreach ($instCheck as $k=>$row) {
-                echo "Instrument $table contains examiner ID: ".$row['Examiner']." which does not exist in the database (examiners).\n";
+            foreach ($instCheck as $k => $row) {
+                echo "Instrument $table contains examiner ID: ".$row['Examiner']
+                    ." which does not exist in the database (examiners).\n";
             }
         }
     }
 }
-///////// final_radiological_review.Final_Examiner2 final_radiological_review.Final_Examiner NOT in examiners.examinerID
 $frrCheck = $db->pselect(
     "SELECT * FROM final_radiological_review 
       WHERE Final_Examiner2 NOT IN (SELECT examinerID FROM examiners) 
@@ -129,33 +135,50 @@ $frrCheck = $db->pselect(
 );
 if (!empty($frrCheck)) {
     $errorPRE =true;
-    foreach ($frrCheck as $k=>$row) {
-        echo "Table final_radiological_review contains Final_Examiner IDs: ".$row['Final_Examiner']." and ".$row['Final_Examiner2']." which do not exist in the database (examiners).\n";
+    foreach ($frrCheck as $k => $row) {
+        echo "Table final_radiological_review contains Final_Examiner IDs: "
+            .$row['Final_Examiner']." and ".$row['Final_Examiner2']
+            ." which do not exist in the database (examiners).\n";
     }
 }
 
 if ($errorPRE) {
-    echo "\n\nSome errors were detected, this script will now terminate.\nPlease Fix errors and try running script again.\n\n";
-    die();
+    $err = <<<'ERRORS'
+Some errors were detected, this script will now terminate.
+Please Fix errors and try running script again.
+
+
+ERRORS;
+    die($err);
+
 }
 
-foreach ($examinersRows as $k=>$row) {
+foreach ($examinersRows as $k => $row) {
     $item = array(
-             'examinerID'       => $row['examinerID'],
-             'centerID'         => $row['centerID'],
-             'active'           => $row['active'],
-             'pending_approval' => $row['pending_approval'],
-            );
+        'examinerID'       => $row['examinerID'],
+        'centerID'         => $row['centerID'],
+        'active'           => $row['active'],
+        'pending_approval' => $row['pending_approval'],
+    );
     // Compare without spaces, without cases
-    $examinersOrganized[strtolower(str_replace(' ','',$row['full_name']))][] = $item;
+    $examinersOrganized[strtolower(
+        str_replace(
+            ' ',
+            '',
+            $row['full_name']
+        )
+    )][] = $item;
 }
 
 print_r($examinersOrganized);
 
-echo "\nA copy of the `examiners` table has been created as `examiners_17.0`. \n\n";
+echo <<<'OUTPUT'
+A copy of the `examiners` table has been created as `examiners_17.0`.
+
+OUTPUT;
 
 
-foreach ($examinersOrganized as $name=>$data_array) {
+foreach ($examinersOrganized as $name => $data_array) {
     echo "Changes for Examiner: ".$name."\n";
     // the first value will be maintained as the final examiner entry
     echo "    -> Site ".$data_array[0]['centerID']." added\n";
@@ -166,7 +189,7 @@ foreach ($examinersOrganized as $name=>$data_array) {
 
     // all other entries for this examiner will be deleted
     // certification table and instruments will be updated
-    foreach ($data_array as $k=>$dupData) {
+    foreach ($data_array as $k => $dupData) {
         // replace old EID by final one
         $oldExaminerID         =$dupData['examinerID'];
         $dupData['examinerID'] =$finalExaminerID;
@@ -176,18 +199,39 @@ foreach ($examinersOrganized as $name=>$data_array) {
         $db->insertIgnore('examiners_psc_rel', $dupData);
 
         // UPDATE certification table
-        echo "        -> Updating Certification examinerIDs: ".$oldExaminerID."=>".$finalExaminerID."\n";
-        $db->update('certification', array('examinerID' => $finalExaminerID), array('examinerID' => $oldExaminerID));
+        echo "        -> Updating Certification examinerIDs: "
+            .$oldExaminerID."=>".$finalExaminerID."\n";
+        $db->update(
+            'certification',
+            array('examinerID' => $finalExaminerID),
+            array('examinerID' => $oldExaminerID)
+        );
 
         // UPDATE final_radiological_review
-        echo "        -> Updating final radiological review examinerIDs: ".$oldExaminerID."=>".$finalExaminerID."\n";
-        $db->update('final_radiological_review', array('Final_Examiner' => $finalExaminerID), array('Final_Examiner' => $oldExaminerID));
-        $db->update('final_radiological_review', array('Final_Examiner2' => $finalExaminerID), array('Final_Examiner2' => $oldExaminerID));
+        echo "        -> Updating final radiological review examinerIDs: "
+            .$oldExaminerID."=>".$finalExaminerID."\n";
+        $db->update(
+            'final_radiological_review',
+            array('Final_Examiner' => $finalExaminerID),
+            array('Final_Examiner' => $oldExaminerID)
+        );
+        $db->update(
+            'final_radiological_review',
+            array('Final_Examiner2' => $finalExaminerID),
+            array('Final_Examiner2' => $oldExaminerID)
+        );
         // UPDATE instrument tables
-        foreach ($instruments as $table=>$name) {
-            if ($db->tableExists($table) && $db->columnExists($table, 'Examiner')) {
-                echo "        -> Updating Instrument ".$table." examinerIDs: ".$oldExaminerID."=>".$finalExaminerID."\n";
-                $db->update($table, array('Examiner' => $finalExaminerID), array('Examiner' => $oldExaminerID));
+        foreach ($instruments as $table => $name) {
+            if ($db->tableExists($table)
+                && $db->columnExists($table, 'Examiner')
+            ) {
+                echo "        -> Updating Instrument $table examinerIDs: "
+                    . "$oldExaminerID => $finalExaminerID\n";
+                $db->update(
+                    $table,
+                    array('Examiner' => $finalExaminerID),
+                    array('Examiner' => $oldExaminerID)
+                );
             }
         }
         // DELETE examiners additional entries
@@ -196,5 +240,12 @@ foreach ($examinersOrganized as $name=>$data_array) {
     }
 }
 
-echo "\n\nThe script ran successfully, make sure to run the clean-up commands available in the SQL/Archive/18.0/clean-up directory. \n
-These commands will drop the now redundant fields and add new constraints to the examiner tables.\n\n";
+echo <<<'DONE'
+
+
+The script ran successfully, make sure to run the clean-up commands available 
+in the SQL/Archive/18.0/clean-up directory.
+These commands will drop the now redundant fields and add new constraints to 
+the examiner tables.
+
+DONE;
