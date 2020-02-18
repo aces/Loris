1a9c6dc65855dee423820a325dfc737664d05148
John Saigle <john.saigle@gmail.com>
2020-02-18 16:05:18 -0500

    Fix ID error in patch

--- SQL/New_patches/2019-09-18_DocRepoEdit.sql
+++ SQL/New_patches/2019-09-18_DocRepoEdit.sql
@@ -1,1 +1,1 @@
-INSERT INTO `permissions` VALUES (56,'document_repository_edit','Upload and edit files in Document Repository','2');
+INSERT INTO `permissions` (code,description,categoryID) VALUES ('document_repository_edit','Upload and edit files in Document Repository','2');
