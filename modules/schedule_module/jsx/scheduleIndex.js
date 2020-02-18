4ec7fd6fe44905951732858e16dd62effa242b1e
kongtiaowang <kongtiaowangshen@gmail.com>
2020-02-18 16:29:06 -0500

    todo pscid and dccid and session

--- modules/schedule_module/jsx/scheduleIndex.js
+++ modules/schedule_module/jsx/scheduleIndex.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Loader from 'Loader';
 import FilterableDataTable from 'FilterableDataTable';
+import Modal from 'Modal';
 import swal from 'sweetalert2';
 
 class ScheduleIndex extends Component {
@@ -12,15 +13,27 @@ class ScheduleIndex extends Component {
       data: {},
       error: false,
       isLoaded: false,
+      formData: {
+        DCCID: null,
+        PSCID: null,
+        Session: null,
+        AppointmentDate: null,
+        AppointmentTime: null,
+        AppointmentType: null,
+        SessionFieldOptions: null,
+      },
+      showModal: false,
     };
 
     this.fetchData = this.fetchData.bind(this);
-
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.setFormData = this.setFormData.bind(this);
     this.formatColumn = this.formatColumn.bind(this);
     this.mapColumn = this.mapColumn.bind(this);
-
-    this.toggleActive = this.toggleActive.bind(this);
-    this.setModuleDisplayStatus = this.setModuleDisplayStatus.bind(this);
+    this.renderAddScheduleForm = this.renderAddScheduleForm.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.fetchDataForDccid = this.fetchDataForDccid.bind(this);
   }
 
   componentDidMount() {
@@ -43,6 +56,76 @@ class ScheduleIndex extends Component {
       });
   }
 
+  fetchDataForDccid(value) {
+    return fetch(this.props.dccidCheck+'/'+value, {credentials: 'same-origin'})
+      .then((resp) => resp.json())
+      .then((data) => {
+       console.log(data['pscid']);
+      this.setState({formData: {...this.state.formData, PSCID: data['pscid']}});
+      })
+      .catch((error) => {
+        this.setState({error: true});
+        console.error(error);
+      });
+  }
+  /**
+   * Store the value of the element in this.state.formData
+   *
+   * @param {string} formElement - name of the form element
+   * @param {string} value - value of the form element
+   */
+  setFormData(formElement, value) {
+    let formData = this.state.formData;
+    formData[formElement] = value;
+    console.log(formData['DCCID']);
+     this.fetchDataForDccid();
+// {this.state.formData.PSCID} set pscid baseon dccid
+// {this.state.formData.DcCID} set pscid baseon DCCid
+// {{this.state.formData.type} set session baseon DCCid and pscid}
+
+    this.setState({
+      formData: formData,
+    });
+  }
+  /**
+   * Handles the submission of the Add Examiner form
+   *
+   * @param {event} e - event of the form
+   */
+  handleSubmit(e) {
+    let formData = this.state.formData;
+    let formObject = new FormData();
+    for (let key in formData) {
+      if (formData[key] !== '') {
+        formObject.append(key, formData[key]);
+      }
+    }
+    formObject.append('fire_away', 'Add');
+
+    fetch(this.props.submitURL, {
+      method: 'POST',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      body: formObject,
+    })
+    .then((resp) => {
+      if (resp.ok && resp.status === 200) {
+        swal.fire('Success!', 'Examiner added.', 'success').then((result) => {
+          if (result.value) {
+            this.closeModal();
+            this.fetchData();
+          }
+        });
+      } else {
+        resp.text().then((message) => {
+          swal.fire('Error!', message, 'error');
+        });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
     mapColumn(column, cell) {
       switch (column) {
       case 'Active':
@@ -57,51 +140,16 @@ class ScheduleIndex extends Component {
           default: return cell;
       }
   }
-
-  toggleActive(name, value, id) {
-      fetch(
-          this.props.BaseURL + '/schedule_module/modules/' + id,
-          {
-              method: 'PATCH',
-              body: JSON.stringify({
-                  'Active': value,
-              }),
-              mode: 'same-origin',
-              credentials: 'same-origin',
-              cache: 'no-cache',
-          }
-      ).then((response) => {
-          if (response.status != 205) {
-              swal.fire('Error!', 'Could not update ' + id + '.', 'error');
-          } else {
-              const success = this.setModuleDisplayStatus(id, value);
-              if (success === true) {
-                  swal.fire('Success!', 'Updated ' + id + ' status!', 'success');
-              } else {
-                  // If we get here something went very wrong, because somehow
-                  // a module was toggled that isn't in the table.
-                  swal.fire('Error!', 'Could not find module ' + id + '.', 'error');
-              }
-          }
-      });
+  openModal() {
+    this.setState({showModal: true});
   }
 
-  setModuleDisplayStatus(modulename, value) {
-      let data = this.state.data;
-      for (let i = 0; i < data.Data.length; i++) {
-          let row = data.Data[i];
-          if (row[0] == modulename) {
-              // Module names are unique, so there's
-              // no reason to keep going once we update
-              // one.
-              row[2] = value;
-              this.setState({data});
-              return true;
-          }
-      }
-      return false;
+  closeModal() {
+    this.setState({
+      formData: {},
+      showModal: false,
+    });
   }
-
   /**
    * Modify behaviour of specified column cells in the Data Table component
    *
@@ -115,7 +163,56 @@ class ScheduleIndex extends Component {
     cell = this.mapColumn(column, cell);
     return <td>{cell}</td>;
   }
-
+  renderAddScheduleForm() {
+    return (
+      <Modal
+        title='Add Appointment'
+        onClose={this.closeModal}
+        show={this.state.showModal}
+      >
+        <FormElement
+          Module="examiner"
+          name="addExaminer"
+          id="addExaminerForm"
+          onSubmit={this.handleSubmit}
+          method="POST"
+        >
+          <TextboxElement
+            name="DCCID"
+            label="DCCID"
+            value={this.state.formData.DCCID}
+            required={true}
+            onUserInput={this.setFormData}
+          />
+          <TextboxElement
+            name="PSCID"
+            label="PSCID"
+            value={this.state.formData.PSCID}
+            required={true}
+            onUserInput={this.setFormData}
+          />
+          <SelectElement
+            name="Session"
+            options={this.state.data.fieldOptions.session}
+            label="Session"
+            value={this.state.formData.Session}
+            required={true}
+            onUserInput={this.setFormData}
+          />
+          <ButtonElement
+            name="fire_away"
+            label={
+              <div>
+                <span className="glyphicon glyphicon-plus"/> Create An Appointment
+              </div>
+            }
+            type="submit"
+            buttonClass="btn btn-sm btn-success"
+          />
+        </FormElement>
+      </Modal>
+    );
+  }
   render() {
     // If error occurs, return a message.
     // XXX: Replace this with a UI component for 500 errors.
@@ -181,14 +278,20 @@ class ScheduleIndex extends Component {
         comparison: 'smallerthanorequal',
       }},
     ];
+    const actions = [
+      {name: 'addExaminer', label: 'Add Examiner', action: this.openModal},
+    ];
     return (
+    <div>
+      {this.renderAddScheduleForm()}
       <FilterableDataTable
         name="schedule_module"
         data={this.state.data.Data}
         fields={fields}
         getFormattedCell={this.formatColumn}
+        actions={actions}
       />
-
+    </div>
     );
   }
 }
@@ -201,7 +304,9 @@ window.addEventListener('load', () => {
   ReactDOM.render(
     <ScheduleIndex
       dataURL={`${loris.BaseURL}/schedule_module/?format=json`}
+      dccidCheck={`${loris.BaseURL}/schedule_module/appointment`}
       BaseURL={loris.BaseURL}
+      submitURL={`${loris.BaseURL}/examiner/`}
       hasEditPermission={loris.userHasPermission('schedule_module')}
     />,
     document.getElementById('lorisworkspace')
