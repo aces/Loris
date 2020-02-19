4211300beacf8968902abc5bb7fc418d9f8f7410
John Saigle <john.saigle@gmail.com>
2020-02-19 16:18:52 -0500

    Add PHPCS

--- test/run-php-linter.sh
+++ test/run-php-linter.sh
@@ -18,6 +18,7 @@ find docs modules htdocs php src tools \
 # Also run PHPCS on all tools/ scripts in this array
 declare -a tools_list=(
     'assign_missing_instruments.php'
+    'cli_helper.class.inc'
     'delete_candidate.php'
     'delete_ignored_conflicts.php'
     'delete_timepoint.php'
