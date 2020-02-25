483fc2adb2c51122a73b480aa7678fc508ccee7e
ridz1208 <ridz_1208@hotmail.com>
2020-02-25 10:44:08 -0500

    restore menu permissions

--- SQL/Cleanup_patches/2019-12-05-Remove_lorisMenuPermissions_for_data_release.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM LorisMenuPermissions WHERE PermID=(SELECT PermID FROM permissions WHERE code='data_release_upload');
-DELETE FROM LorisMenuPermissions WHERE PermID=(SELECT PermID FROM permissions WHERE code='data_release_edit_file_access');
-
