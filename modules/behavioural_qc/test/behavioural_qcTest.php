82e5a0fe198255bf1348ed9422defb9066c5a106
John Saigle <john.saigle@gmail.com>
2020-02-25 15:00:37 -0500

    Cleanup QC integration tests

--- modules/behavioural_qc/test/behavioural_qcTest.php
+++ modules/behavioural_qc/test/behavioural_qcTest.php
@@ -1,3 +1,4 @@
+<<<<<<< 121fe1ea319ff7def685e706b9f4aa349ad8bb14
 <?php
 /**
  * Behavioural_QC automated integration tests
@@ -11,22 +12,20 @@
  * @link     https://github.com/aces/Loris
  */
 use Facebook\WebDriver\WebDriverBy;
+=======
+<?php declare(strict_types=1);
+>>>>>>> Cleanup QC integration tests
 require_once __DIR__ .
         "/../../../test/integrationtests/LorisIntegrationTest.class.inc";
 /**
  * Behavioural_QC automated integration tests
  *
- * PHP Version 5
- *
- * @category Test
- * @package  Loris
- * @author   Wang Shen <wangshen.mcin@gmail.com>
- * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
- * @link     https://github.com/aces/Loris
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
  */
 class Behavioural_QCTest extends LorisIntegrationTest
 {
     /**
+<<<<<<< 121fe1ea319ff7def685e706b9f4aa349ad8bb14
      * Tests that, when loading the behavioural_qc module, some
      * text appears in the body.
      *
@@ -60,11 +59,16 @@ class Behavioural_QCTest extends LorisIntegrationTest
     }
     /**
      * Tests that help editor loads with the permission
+=======
+     * Ensures that the module loads if and only if the user has one of the
+     * module permissions codes.
+>>>>>>> Cleanup QC integration tests
      *
      * @return void
      */
-    function testBehaviouralQCPermission()
+    public function testPermissions(): void
     {
+<<<<<<< 121fe1ea319ff7def685e706b9f4aa349ad8bb14
 <<<<<<< 95f40eb6a80aa506b5375353c3934bbc0b158ba9
          $this->setupPermissions(["behavioural_quality_control_view"]);
 =======
@@ -77,7 +81,14 @@ class Behavioural_QCTest extends LorisIntegrationTest
         $this->assertNotContains(
             "You do not have access to this page.",
             $bodyText
+=======
+        $this->checkPagePermissions(
+            '/behavioural_qc/',
+            array(
+                'behavioural_quality_control_view',
+            ),
+            "Behavioural Quality Control"
+>>>>>>> Cleanup QC integration tests
         );
-          $this->resetPermissions();
     }
 }
--- modules/imaging_qc/test/imaging_qcTest.php
+++ modules/imaging_qc/test/imaging_qcTest.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * ImagingQCIntegrationTest automated integration tests
  *
@@ -11,33 +11,26 @@
  * @link     https://github.com/aces/Loris
  */
 use Facebook\WebDriver\WebDriverBy;
+
  require_once __DIR__
     . "/../../../test/integrationtests/LorisIntegrationTest.class.inc";
-/**
- * ImagingQCIntegrationTest
- *
- * @category Test
- * @package  Loris
- * @author   Wang Shen <wangshen.mcin@gmail.com>
- * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
- * @link     https://github.com/aces/Loris
- */
+
 class ImagingQCIntegrationTest extends LorisIntegrationTest
 {
     /**
-     * Tests that, the homepage should have "Imaging Quality Control" on the page.
+     * Ensures that the module loads if and only if the user has one of the
+     * module permissions codes.
      *
      * @return void
      */
-    function testPageLoads()
+    public function testPermissions(): void
     {
-        $this->safeGet($this->url . "/imaging_qc/");
-        $bodyText = $this->webDriver
-            ->findElement(WebDriverBy::cssSelector("body"))->getText();
-        $this->assertContains("Imaging Quality Control", $bodyText);
-        $bodyText = $this->webDriver
-            ->findElement(WebDriverBy::cssSelector("body"))->getText();
-        $this->assertNotContains("An error occurred", $bodyText);
-
+        $this->checkPagePermissions(
+            '/imaging_qc/',
+            array(
+                'imaging_quality_control_view',
+            ),
+            "Imaging Quality Control"
+        );
     }
 }
