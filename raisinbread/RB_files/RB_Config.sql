3ed27effb842653c6af83cfd49f0a57a7a6dbc2b
Rida Abou-Haidar <ridz1208@users.noreply.github.com>
2020-04-01 12:08:31 -0400

    Update RB_Config.sql

--- raisinbread/RB_files/RB_Config.sql
+++ raisinbread/RB_files/RB_Config.sql
@@ -19,7 +19,7 @@ INSERT INTO `Config` (`ID`, `ConfigID`, `Value`) VALUES (16,20,'false');
 INSERT INTO `Config` (`ID`, `ConfigID`, `Value`) VALUES (17,21,'true');
 INSERT INTO `Config` (`ID`, `ConfigID`, `Value`) VALUES (18,22,'true');
 INSERT INTO `Config` (`ID`, `ConfigID`, `Value`) VALUES (19,23,'0');
-INSERT INTO `Config` (`ID`, `ConfigID`, `Value`) VALUES (20,24,"Modify this to your project\'s citation policy");
+INSERT INTO `Config` (`ID`, `ConfigID`, `Value`) VALUES (20,24,'Modify this to your project\'s citation policy');
 INSERT INTO `Config` (`ID`, `ConfigID`, `Value`) VALUES (22,12,'YMd');
 INSERT INTO `Config` (`ID`, `ConfigID`, `Value`) VALUES (23,27,'/data-raisinbread/');
 INSERT INTO `Config` (`ID`, `ConfigID`, `Value`) VALUES (24,28,'/var/www/loris/');
