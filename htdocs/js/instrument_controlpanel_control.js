0b7482770f76f9b90c88e8c5e914f3821534d5ce
pierre-p-s <pierre.pacsoo@mcin.ca>
2020-04-21 13:18:09 -0400

    Removing not needed file

--- htdocs/js/instrument_controlpanel_control.js
+++ htdocs/js/instrument_controlpanel_control.js
@@ -1,21 +1,3 @@
 $(function() {
     $('[data-toggle="tooltip"]').tooltip();
 });
-
-function swalFunction(event) {
-  swal({
-    title: 'Please confirm deletion',
-    text: 'The instrument data will be deleted',
-    type: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Yes, delete the data'
-  }, function(result) {
-    if(result) {
-      // swal does not work here. The window instantly disappears
-      alert("\t\t\t Deleted! (You will need to reload the page again)");
-      document.getElementById('ConfirmDelete').submit();
-    }
-  });
-}
