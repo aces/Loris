ed5e23cea5f78435a5549810f0acc629296d1cdd
maltheism <16293415+maltheism@users.noreply.github.com>
2020-04-22 14:41:51 -0400

    [jsx/(Filter.js FilterableDataTable.js DataTable.js)] Browser console error cleanup (#6385)
    Updated the propTypes to correspond with the correct "type" of the data being passed to the components. Updated DataTable.js to have a key be passed using React.cloneElement.
    
    Resolves #6379

--- jsx/DataTable.js
+++ jsx/DataTable.js
@@ -439,10 +439,7 @@ class DataTable extends Component {
                 );
             }
             if (cell !== null) {
-                // Note: Can't currently pass a key, need to update columnFormatter
-                // to not return a <td> node. Using createFragment instead.
-                // let key = 'td_col_' + j;
-                curRow.push(cell);
+                curRow.push(React.cloneElement(cell, {key: 'td_col_' + j}));
             } else {
                 curRow.push(createFragment({celldata}));
             }
@@ -451,8 +448,8 @@ class DataTable extends Component {
         const rowIndexDisplay = index[i].Content;
         rows.push(
             <tr key={'tr_' + rowIndex} colSpan={headers.length}>
-            <td>{rowIndexDisplay}</td>
-            {curRow}
+              <td key={'td_' + rowIndex}>{rowIndexDisplay}</td>
+              {curRow}
             </tr>
         );
     }
@@ -575,7 +572,7 @@ DataTable.propTypes = {
   // parameters of the form: func(ColumnName, CellData, EntireRowData)
   getFormattedCell: PropTypes.func,
   onSort: PropTypes.func,
-  actions: PropTypes.object,
+  actions: PropTypes.array,
   hide: PropTypes.object,
   nullTableShow: PropTypes.bool,
 };
