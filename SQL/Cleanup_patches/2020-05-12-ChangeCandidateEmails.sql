d93ead9bcd4f2adc5d439b1587ff598e8124446d
Karim H <karimh96@hotmail.com>
2020-05-13 12:40:07 -0400

    Remove RB cleanup file

--- SQL/Cleanup_patches/2020-05-12-ChangeCandidateEmails.sql
+++
@@ -1,4 +0,0 @@
---Changes survey accounts emails to person[1...]@example.com
---Change the '-3' if you wish to change the numbering
-UPDATE participant_accounts SET Email=(CONCAT ('person',(participant_accounts.ID - 3),'@example.com'));
-
