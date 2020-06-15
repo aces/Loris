4816353d5c00884d51c872a33501bb75f6629326
John Saigle <4022790+johnsaigle@users.noreply.github.com>
2020-06-15 17:49:50 -0400

    Update tools/importers/openScienceDataImporter.php
    Co-authored-by: cmadjar <cecile.madjar@mcin.ca>

--- tools/importers/openScienceDataImporter.php
+++ tools/importers/openScienceDataImporter.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-require_once 'generic_includes.php';
+require_once '../generic_includes.php';
 require_once 'CandidateImporter.php';
 require_once 'VisitImporter.php';
 require_once 'InstrumentImporter.php';
