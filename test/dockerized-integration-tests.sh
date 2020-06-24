b07ce836b17d2535b31a4caf0fe111aecdb96f34
kongtiaowang <kongtiaowangshen@gmail.com>
2020-06-23 21:25:16 -0400

    headless remove

--- test/dockerized-integration-tests.sh
+++ test/dockerized-integration-tests.sh
@@ -9,4 +9,4 @@ else
 fi
 
 # Core integration tests
-docker-compose run -T -e HEADLESS=true --rm ${CONTAINER} vendor/bin/phpunit --configuration test/phpunit.xml
+docker-compose run -T --rm ${CONTAINER} vendor/bin/phpunit --configuration test/phpunit.xml
