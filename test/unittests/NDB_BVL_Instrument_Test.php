63d9785dfe47ca6d2680c3dad1a393448b5cabd2
AlexandraLivadas <alexandra.livadas@mail.mcgill.ca>
2020-07-08 11:06:01 -0400

    [Test] unittests for NDB_BVL_Instrument libraries

--- test/unittests/NDB_BVL_Instrument_Test.php
+++ test/unittests/NDB_BVL_Instrument_Test.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Unit test for NDB_BVL_Instrument class
+ *
+ * PHP Version 7
+ *
+ * @category Tests
+ * @package  Main
+ * @author   Dave MacFarlane <david.macfarlane2@mcgill.ca>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link     https://www.github.com/aces/Loris/
+ */
 namespace Loris\Tests;
 set_include_path(get_include_path().":" .  __DIR__  . "/../../php/libraries:");
 use PHPUnit\Framework\TestCase;
@@ -6,25 +17,45 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../php/libraries/NDB_BVL_Instrument.class.inc';
 require_once 'Smarty_hook.class.inc';
 require_once 'NDB_Config.class.inc';
-
-class NDB_BVL_Instrument_ToJSON_Test extends TestCase
+/**
+ * Unit test for NDB_BVL_Instrument class
+ *
+ * PHP Version 7
+ *
+ * @category Tests
+ * @package  Main
+ * @author   Dave MacFarlane <david.macfarlane2@mcgill.ca>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link     https://www.github.com/aces/Loris/
+ */
+class NDB_BVL_Instrument_Test extends TestCase
 {
+    private $_instrument;
     /**
      * Set up sets a fake $_SESSION object that we can use for
      * assertions
+     *
+     * @return void
      */
-    function setUp() {
+    function setUp()
+    {
         global $_SESSION;
-        if(!defined("UNIT_TESTING")) {
+        if (!defined("UNIT_TESTING")) {
             define("UNIT_TESTING", true);
         }
         date_default_timezone_set("UTC");
-        $this->Session = $this->getMockBuilder(\stdClass::class)->setMethods(array('getProperty', 'setProperty', 'getUsername', 'isLoggedIn'))->getMock();
-        $this->MockSinglePointLogin = $this->getMockBuilder('SinglePointLogin')->getMock();
-        $this->Session->method("getProperty")->willReturn($this->MockSinglePointLogin);
+        $this->session = $this->getMockBuilder(\stdClass::class)
+            ->setMethods(
+                array('getProperty', 'setProperty', 'getUsername', 'isLoggedIn')
+            )
+            ->getMock();
+        $this->mockSinglePointLogin = $this->getMockBuilder('SinglePointLogin')
+            ->getMock();
+        $this->session->method("getProperty")
+            ->willReturn($this->mockSinglePointLogin);
 
         $_SESSION = array(
-            'State' => $this->Session
+            'State' => $this->session
         );
 
         $factory = \NDB_Factory::singleton();
@@ -37,30 +68,41 @@ class NDB_BVL_Instrument_ToJSON_Test extends TestCase
         \NDB_Factory::$testdb = $mockdb;
         \NDB_Factory::$config = $mockconfig;
 
-        $this->QuickForm = new \LorisForm(); //$this->getMock("HTML_Quickform");
-        $this->Client = new \NDB_Client;
-        $this->Client->makeCommandLine();
-        $this->Client->initialize(__DIR__ . "/../../project/config.xml");
+        $this->quickForm = new \LorisForm();
 
-        $this->i = $this->getMockBuilder(\NDB_BVL_Instrument::class)->disableOriginalConstructor()->setMethods(array("getFullName", "getSubtestList"))->getMock();
-        $this->i->method('getFullName')->willReturn("Test Instrument");
-        $this->i->method('getSubtestList')->willReturn(array());
-        $this->i->form = $this->QuickForm;
-        $this->i->testName = "Test";
+        $this->_instrument = $this->getMockBuilder(\NDB_BVL_Instrument::class)
+            ->disableOriginalConstructor()
+            ->setMethods(array("getFullName", "getSubtestList"))->getMock();
+        $this->_instrument->method('getFullName')->willReturn("Test Instrument");
+        $this->_instrument->method('getSubtestList')->willReturn(array());
+        $this->_instrument->form = $this->quickForm;
+        $this->_instrument->testName = "Test";
     }
 
     /**
      * Helper function to use for creating stubs that stub out everything except
      * the method being tested
+     *
+     * @param $methods The method being tested
+     *
+     * @return array
      */
-    function _getAllMethodsExcept($methods) {
+    function _getAllMethodsExcept($methods)
+    {
         $AllMethods = get_class_methods('NDB_BVL_Instrument');
 
         return array_diff($AllMethods, $methods);
     }
 
-    function testMetaData() {
-        $json = $this->i->toJSON();
+    /**
+     * Test that the toJSON method returns the correct metadata
+     *
+     * @covers NDB_BVL_Instrument::toJSON
+     * @return void
+     */
+    function testMetaData()
+    {
+        $json = $this->_instrument->toJSON();
         $outArray = json_decode($json, true);
         $ExpectedMeta = [
             'InstrumentVersion' => "1l",
@@ -72,14 +114,34 @@ class NDB_BVL_Instrument_ToJSON_Test extends TestCase
         $this->assertEquals($ExpectedMeta, $outArray['Meta']);
     }
 
-    function testSelectElement() {
+    /**
+     * Test that addSelect and addElement add the correct information
+     * and that toJSON returns this data
+     *
+     * @covers NDB_BVL_Instrument::toJSON
+     * @return void
+     */
+    function testSelectElement()
+    {
         $value = array('value' => "Option");
         $not_answered = array('value' => 'Option', 'not_answered' => 'Not Answered');
-        $this->i->addSelect("FieldName", "Field Description", $value);
-        $this->i->addSelect("FieldName2", "Field Description 2", $not_answered);
-        $this->i->form->addElement('select', "multiselect1", "Test Question", $value, array("multiple" => 'multiple'));
-        $this->i->form->addElement('select', "multiselect2", "Test Question", $not_answered, array('multiple' => "multiple"));
-        $json = $this->i->toJSON();
+        $this->_instrument->addSelect("FieldName", "Field Description", $value);
+        $this->_instrument->addSelect(
+            "FieldName2", "Field Description 2", $not_answered
+        );
+        $this->_instrument->form->addElement(
+            'select',
+            "multiselect1",
+            "Test Question",
+            $value, array("multiple" => 'multiple')
+        );
+        $this->_instrument->form->addElement(
+            'select',
+            "multiselect2",
+            "Test Question",
+            $not_answered, array('multiple' => "multiple")
+        );
+        $json = $this->_instrument->toJSON();
         $outArray = json_decode($json, true);
         $selectElement = $outArray['Elements'][0];
         $selectElement2 = $outArray['Elements'][1];
@@ -87,7 +149,8 @@ class NDB_BVL_Instrument_ToJSON_Test extends TestCase
         $multiselectElement = $outArray['Elements'][2];
         $multiselectElement2 = $outArray['Elements'][3];
 
-        $this->assertEquals($selectElement,
+        $this->assertEquals(
+            $selectElement,
             [
                 'Type' => "select",
                 "Name" => "FieldName",
@@ -101,7 +164,8 @@ class NDB_BVL_Instrument_ToJSON_Test extends TestCase
             ]
         );
 
-        $this->assertEquals($selectElement2,
+        $this->assertEquals(
+            $selectElement2,
             [
                 'Type' => "select",
                 "Name" => "FieldName2",
@@ -115,7 +179,8 @@ class NDB_BVL_Instrument_ToJSON_Test extends TestCase
             ]
         );
 
-        $this->assertEquals($multiselectElement,
+        $this->assertEquals(
+            $multiselectElement,
             [
                 'Type' => "select",
                 "Name" => "multiselect1",
@@ -130,7 +195,8 @@ class NDB_BVL_Instrument_ToJSON_Test extends TestCase
             ]
         );
 
-        $this->assertEquals($multiselectElement2,
+        $this->assertEquals(
+            $multiselectElement2,
             [
                 'Type' => "select",
                 "Name" => "multiselect2",
@@ -147,15 +213,30 @@ class NDB_BVL_Instrument_ToJSON_Test extends TestCase
     }
 
 
-    function testTextElement() {
-        $this->i->addTextElement("FieldName", "Field Description for Text", array("value" => "Option"));
-        $this->i->addTextAreaElement("FieldName2", "Field Description2 for Text", array("value" => "Option"));
-        $json = $this->i->toJSON();
+    /**
+     * Test that addTextElement and addTextAreaElement adds the correct data to
+     * the instrument
+     *
+     * @covers NDB_BVL_Instrument::addTextElement
+     * @covers NDB_BVL_Instrument::addTextAreaElement
+     * @covers NDB_BVL_Instrument::toJSON
+     * @return void
+     */
+    function testTextElement()
+    {
+        $this->_instrument->addTextElement(
+            "FieldName", "Field Description for Text", array("value" => "Option")
+        );
+        $this->_instrument->addTextAreaElement(
+            "FieldName2", "Field Description2 for Text", array("value" => "Option")
+        );
+        $json = $this->_instrument->toJSON();
         $outArray = json_decode($json, true);
         $textElement = $outArray['Elements'][0];
         $textareaElement = $outArray['Elements'][1];
 
-        $this->assertEquals($textElement,
+        $this->assertEquals(
+            $textElement,
             [
                 'Type'        => "text",
                 "Name"        => "FieldName",
@@ -167,7 +248,8 @@ class NDB_BVL_Instrument_ToJSON_Test extends TestCase
             ]
         );
 
-        $this->assertEquals($textareaElement,
+        $this->assertEquals(
+            $textareaElement,
             [
                 'Type'        => "text",
                 "Name"        => "FieldName2",
@@ -180,8 +262,18 @@ class NDB_BVL_Instrument_ToJSON_Test extends TestCase
         );
     }
 
-    function testDateElement() {
-        $this->i->addBasicDate(
+    /**
+     * Test that addBasicDate (from NDB_Page) and addDateElement
+     * adds the correct date to the instrument object
+     *
+     * @covers NDB_Page::addBasicDate
+     * @covers NDB_Page::addDateElement
+     * @covers NDB_Page::toJSON
+     * @return void
+     */
+    function testDateElement()
+    {
+        $this->_instrument->addBasicDate(
             "FieldName",
             "Field Description",
             [
@@ -191,7 +283,7 @@ class NDB_BVL_Instrument_ToJSON_Test extends TestCase
                 "addEmptyOption" => false,
             ]
         );
-        $this->i->addBasicDate(
+        $this->_instrument->addBasicDate(
             "FieldName2",
             "Field Description",
             [
@@ -202,7 +294,7 @@ class NDB_BVL_Instrument_ToJSON_Test extends TestCase
             ]
         );
 
-        $this->i->addDateElement(
+        $this->_instrument->addDateElement(
             "FieldName3",
             "Field Description",
             [
@@ -212,7 +304,7 @@ class NDB_BVL_Instrument_ToJSON_Test extends TestCase
                 "addEmptyOption" => false,
             ]
         );
-        $this->i->addDateElement(
+        $this->_instrument->addDateElement(
             "FieldName4",
             "Field Description",
             [
@@ -222,7 +314,7 @@ class NDB_BVL_Instrument_ToJSON_Test extends TestCase
                 "addEmptyOption" => true,
             ]
         );
-        $json = $this->i->toJSON();
+        $json = $this->_instrument->toJSON();
         $outArray = json_decode($json, true);
         $dateElement = $outArray['Elements'][0];
         $dateElement2 = $outArray['Elements'][1];
@@ -258,13 +350,22 @@ class NDB_BVL_Instrument_ToJSON_Test extends TestCase
         $this->assertEquals($dateElement4, $expectedResult);
     }
 
-    function testNumericElement() {
-        $this->i->addNumericElement("TestElement", "Test Description");
-        $json = $this->i->toJSON();
+    /**
+     * Test that addNumericElement adds the correct data to the instrument
+     *
+     * @covers NDB_BVL_Instrument::addNumericElement
+     * @covers NDB_BVL_Instrument::toJSON
+     * @return void
+     */
+    function testNumericElement()
+    {
+        $this->_instrument->addNumericElement("TestElement", "Test Description");
+        $json = $this->_instrument->toJSON();
         $outArray = json_decode($json, true);
         $numericElement = $outArray['Elements'][0];
 
-        $this->assertEquals($numericElement,
+        $this->assertEquals(
+            $numericElement,
             [
                 "Type" => "numeric",
                 "Name" => "TestElement",
@@ -277,29 +378,40 @@ class NDB_BVL_Instrument_ToJSON_Test extends TestCase
 
     }
 
-    function testScoreElement() {
-        $this->i->addScoreColumn(
+    /**
+     * Test that addScoreColumn (from NDB_Page) adds the data to the
+     * instrument object
+     *
+     * @covers NDB_Page::addScoreColumn
+     * @covers NDB_BVL_Instrument::toJSON
+     * @return void
+     */
+    function testScoreElement()
+    {
+        $this->_instrument->addScoreColumn(
             "FieldName",
             "Field Description",
             "45"
         );
-        $this->i->addScoreColumn(
+        $this->_instrument->addScoreColumn(
             "FieldName2",
             null
         );
-        $json = $this->i->toJSON();
+        $json = $this->_instrument->toJSON();
         $outArray = json_decode($json, true);
         $scoreElement = $outArray['Elements'][0];
         $scoreElement2 = $outArray['Elements'][1];
 
-        $this->assertEquals($scoreElement,
+        $this->assertEquals(
+            $scoreElement,
             [
                 'Type'        => "score",
                 "Name"        => "FieldName",
                 "Description" => "Field Description",
             ]
         );
-        $this->assertEquals($scoreElement2,
+        $this->assertEquals(
+            $scoreElement2,
             [
                 'Type'        => "score",
                 "Name"        => "FieldName2",
@@ -308,26 +420,39 @@ class NDB_BVL_Instrument_ToJSON_Test extends TestCase
 
     }
 
-    function testHeaderElement() {
+    /**
+     * Test that when a header element is added, the toJSON method returns the
+     * element in the correct format
+     *
+     * @covers NDB_BVL_Instrument::toJSON
+     * @return void
+     */
+    function testHeaderElement()
+    {
         // Since QuickForm arbitrarily decides to split things into "sections"
         // when there's a header, the header test adds 2 headers to ensure that
         // the JSON serialization was done according to spec, and not according
         // to QuickForm's whims.
         // The first "section" has no elements, and the second one, to make sure
         // that the serialization won't die on a 0 element "section"
-        $this->i->form->addElement("header", null, "I am your test header");
-        $this->i->form->addElement("header", null, "I am another test header");
-        $this->i->addScoreColumn(
+        $this->_instrument->form->addElement(
+            "header", null, "I am your test header"
+        );
+        $this->_instrument->form->addElement(
+            "header", null, "I am another test header"
+        );
+        $this->_instrument->addScoreColumn(
             "FieldName2",
             "Field Description",
             "45"
         );
-        $json = $this->i->toJSON();
+        $json = $this->_instrument->toJSON();
         $outArray = json_decode($json, true);
         $headerElement = $outArray['Elements'][0];
         $headerElement2= $outArray['Elements'][1];
 
-        $this->assertEquals($headerElement,
+        $this->assertEquals(
+            $headerElement,
             [
                 'Type'        => "header",
                 "Description" => "I am your test header",
@@ -336,7 +461,8 @@ class NDB_BVL_Instrument_ToJSON_Test extends TestCase
                 ]
             ]
         );
-        $this->assertEquals($headerElement2,
+        $this->assertEquals(
+            $headerElement2,
             [
                 'Type'        => "header",
                 "Description" => "I am another test header",
@@ -349,13 +475,23 @@ class NDB_BVL_Instrument_ToJSON_Test extends TestCase
         $this->assertEquals(count($outArray['Elements']), 3);
     }
 
-    function testLabelElement() {
-        $this->i->addLabel("I am a label");
-        $json = $this->i->toJSON();
+    /**
+     * Test that addLabel (from NDB_Page) adds the label element
+     * to the instrument object
+     *
+     * @covers NDB_Page::addLabel
+     * @covers NDB_BVL_Instrument::toJSON
+     * @return void
+     */
+    function testLabelElement()
+    {
+        $this->_instrument->addLabel("I am a label");
+        $json = $this->_instrument->toJSON();
         $outArray = json_decode($json, true);
         $labelElement = $outArray['Elements'][0];
 
-        $this->assertEquals($labelElement,
+        $this->assertEquals(
+            $labelElement,
             [
                 'Type'        => "label",
                 "Description" => "I am a label"
@@ -364,25 +500,37 @@ class NDB_BVL_Instrument_ToJSON_Test extends TestCase
         $this->assertEquals(count($outArray['Elements']), 1);
     }
 
-    function testPageGroup() {
-        $this->i = $this->getMockBuilder(\NDB_BVL_Instrument::class)->disableOriginalConstructor()->setMethods(array("getFullName", "getSubtestList", '_setupForm'))->getMock();
-        $this->i->method('getFullName')->willReturn("Test Instrument");
-        $this->i->method('getSubtestList')->willReturn(
+    /**
+     * Test that toJSON gets the subtest list and full name elements of
+     * the instrument
+     *
+     * @covers NDB_BVL_Instrument::toJSON
+     * @return void
+     */
+    function testPageGroup()
+    {
+        $this->_instrument = $this->getMockBuilder(\NDB_BVL_Instrument::class)
+            ->disableOriginalConstructor()
+            ->setMethods(
+                array("getFullName", "getSubtestList", '_setupForm')
+            )->getMock();
+        $this->_instrument->method('getFullName')->willReturn("Test Instrument");
+        $this->_instrument->method('getSubtestList')->willReturn(
             array(
                 array('Name' => 'Page 1', 'Description' => 'The first page'),
                 array('Name' => 'Page 2', 'Description' => 'The second page'),
             )
         );
 
-        $this->i->form = $this->QuickForm;
-        $this->i->testName = "Test";
+        $this->_instrument->form = $this->quickForm;
+        $this->_instrument->testName = "Test";
 
-
-        $json = $this->i->toJSON();
+        $json = $this->_instrument->toJSON();
         $outArray = json_decode($json, true);
         $page1 = $outArray['Elements'][0];
         $page2 = $outArray['Elements'][1];
-        $this->assertEquals($page1,
+        $this->assertEquals(
+            $page1,
             [
                 'Type' => 'ElementGroup',
                 'GroupType' => 'Page',
@@ -390,7 +538,8 @@ class NDB_BVL_Instrument_ToJSON_Test extends TestCase
                 'Description' => 'The first page'
             ]
         );
-        $this->assertEquals($page2,
+        $this->assertEquals(
+            $page2,
             [
                 'Type' => 'ElementGroup',
                 'GroupType' => 'Page',
