83c6161681c0890c4f546b33336ca4904544b30c
Laetitia Fesselier <laetitia.fesselier@mail.mcgill.ca>
2020-07-22 10:34:03 -0400

    [eslint] Fix .eslint rules (#6857)
    - js-doc rule has been raised to error on all files except the one already lacking js-doc comments (to be fixed later). This will ensure that all future modules will conform to the rule.
    - max-len has been raised to error on all files since all problem has been solved in #6756 and #6761
    - 0 has been replaced by it's more verbose "off" equivalent.
    
        Resolves #6750

--- modules/module_manager/jsx/modulemanager.js
+++ modules/module_manager/jsx/modulemanager.js
@@ -4,7 +4,14 @@ import Loader from 'Loader';
 import FilterableDataTable from 'FilterableDataTable';
 import swal from 'sweetalert2';
 
+/**
+ * Module Manager React Component
+ */
 class ModuleManagerIndex extends Component {
+  /**
+   * @constructor
+   * @param {object} props - React Component properties
+   */
   constructor(props) {
     super(props);
 
@@ -23,6 +30,9 @@ class ModuleManagerIndex extends Component {
     this.setModuleDisplayStatus = this.setModuleDisplayStatus.bind(this);
   }
 
+  /**
+   * Called by React when the component has been rendered on the page.
+   */
   componentDidMount() {
     this.fetchData()
       .then(() => this.setState({isLoaded: true}));
@@ -43,81 +53,105 @@ class ModuleManagerIndex extends Component {
       });
   }
 
-    mapColumn(column, cell) {
-      switch (column) {
+  /**
+   * Modify value of specified column cells in the Data Table component
+   *
+   * @param {string} column - column name
+   * @param {string} cell - cell value
+   *
+   * @return {string} a mapped value for the table cell at a given column
+   */
+  mapColumn(column, cell) {
+    switch (column) {
       case 'Active':
-              if (cell === 'Y') {
-                  return 'Yes';
-              } else if (cell === 'N') {
-                  return 'No';
-              }
-              // This shouldn't happen, it's a non-nullable
-              // enum in the backend.
-              return '?';
-          default: return cell;
-      }
+        if (cell === 'Y') {
+          return 'Yes';
+        } else if (cell === 'N') {
+          return 'No';
+        }
+        // This shouldn't happen, it's a non-nullable
+        // enum in the backend.
+        return '?';
+      default: return cell;
+    }
   }
 
+  /**
+   * Toggle active
+   *
+   * @param {string} name
+   * @param {boolean} value
+   * @param {int} id - module id
+   *
+   */
   toggleActive(name, value, id) {
-      fetch(
-          this.props.BaseURL + '/module_manager/modules/' + id,
-          {
-              method: 'PATCH',
-              body: JSON.stringify({
-                  'Active': value,
-              }),
-              mode: 'same-origin',
-              credentials: 'same-origin',
-              cache: 'no-cache',
-          }
-      ).then((response) => {
-          if (response.status != 205) {
-              swal.fire('Error!', 'Could not update ' + id + '.', 'error');
-          } else {
-              const success = this.setModuleDisplayStatus(id, value);
-              if (success === true) {
-                swal.fire({
-                  title: 'Success!',
-                  text: 'Updated ' + id + ' status! '
-                        + 'To apply changes the interface must be reloaded. '
-                        + 'Proceed ?',
-                  type: 'success',
-                  showCancelButton: true,
-                  confirmButtonText: 'Reload the page',
-                  cancelButtonText: 'Continue',
-                }).then((status) => {
-                  if (status.value) {
-                    window.location.href = this.props.BaseURL
-                                           + '/module_manager';
-                  }
-                });
-              } else {
-                  // If we get here something went very wrong, because somehow
-                  // a module was toggled that isn't in the table.
-                  swal.fire(
-                    'Error!',
-                    'Could not find module ' + id + '.',
-                    'error'
-                  );
-              }
-          }
-      });
+    fetch(
+      this.props.BaseURL + '/module_manager/modules/' + id,
+      {
+        method: 'PATCH',
+        body: JSON.stringify({
+          'Active': value,
+        }),
+        mode: 'same-origin',
+        credentials: 'same-origin',
+        cache: 'no-cache',
+      }
+    ).then((response) => {
+      if (response.status != 205) {
+        swal.fire('Error!', 'Could not update ' + id + '.', 'error');
+      } else {
+        const success = this.setModuleDisplayStatus(id, value);
+        if (success === true) {
+          swal.fire({
+            title: 'Success!',
+            text: 'Updated ' + id + ' status! '
+                  + 'To apply changes the interface must be reloaded. '
+                  + 'Proceed ?',
+            type: 'success',
+            showCancelButton: true,
+            confirmButtonText: 'Reload the page',
+            cancelButtonText: 'Continue',
+          }).then((status) => {
+            if (status.value) {
+              window.location.href = this.props.BaseURL
+                                     + '/module_manager';
+            }
+          });
+        } else {
+          // If we get here something went very wrong, because somehow
+          // a module was toggled that isn't in the table.
+          swal.fire(
+            'Error!',
+            'Could not find module ' + id + '.',
+            'error'
+          );
+        }
+      }
+    });
   }
 
+  /**
+   * Set Module Display Status
+   *
+   * @param {string} modulename
+   * @param {boolean} value
+   *
+   * @return {boolean}
+   *
+   */
   setModuleDisplayStatus(modulename, value) {
-      let data = this.state.data;
-      for (let i = 0; i < data.Data.length; i++) {
-          let row = data.Data[i];
-          if (row[0] == modulename) {
-              // Module names are unique, so there's
-              // no reason to keep going once we update
-              // one.
-              row[2] = value;
-              this.setState({data});
-              return true;
-          }
+    let data = this.state.data;
+    for (let i = 0; i < data.Data.length; i++) {
+      let row = data.Data[i];
+      if (row[0] == modulename) {
+        // Module names are unique, so there's
+        // no reason to keep going once we update one.
+        row[2] = value;
+        this.setState({data});
+        return true;
       }
-      return false;
+    }
+    return false;
   }
 
   /**
@@ -131,20 +165,25 @@ class ModuleManagerIndex extends Component {
    */
   formatColumn(column, cell, row) {
     if (column == 'Active' && this.props.hasEditPermission) {
-        return <td><SelectElement
-              name='active'
-              id={row.Name}
-              label=''
-              emptyOption={false}
-              options={{'Y': 'Yes', 'N': 'No'}}
-              value={cell}
-              onUserInput={this.toggleActive}
-            /></td>;
+      return <td><SelectElement
+        name='active'
+        id={row.Name}
+        label=''
+        emptyOption={false}
+        options={{'Y': 'Yes', 'N': 'No'}}
+        value={cell}
+        onUserInput={this.toggleActive}
+      /></td>;
     }
     cell = this.mapColumn(column, cell);
     return <td>{cell}</td>;
   }
 
+  /**
+   * Renders the React component.
+   *
+   * @return {JSX} - React markup for the component
+   */
   render() {
     // If error occurs, return a message.
     // XXX: Replace this with a UI component for 500 errors.
