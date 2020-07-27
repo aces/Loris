de98c51b536dafa3ebe4e0844f06e83d8b98d525
Dave MacFarlane <driusan@gmail.com>
2020-07-27 15:40:29 -0400

    Change indexes
    Co-authored-by: Shen  <kongtiaowangshen@gmail.com>

--- modules/datadict/test/datadictTest.php
+++ modules/datadict/test/datadictTest.php
@@ -35,11 +35,11 @@ class DatadictTestIntegrationTest extends LorisIntegrationTest
     private $_loadingUI
         =  [
             'Data Dictionary'    => '#bc2 > a:nth-child(2) > div',
-            'Source From'        => '.col-xs-12:nth-child(2) .col-sm-3',
-            'Name'               => '.col-xs-12:nth-child(3) .col-sm-3',
-            'Source Field'       => '.col-xs-12:nth-child(4) .col-sm-3',
-            'Description'        => '.col-xs-12:nth-child(5) .col-sm-3',
-            'Description Status' => '.col-xs-12:nth-child(6) .col-sm-3',
+            'Source From'        => '.col-xs-12:nth-child(3) .col-sm-3',
+            'Name'               => '.col-xs-12:nth-child(4) .col-sm-3',
+            'Source Field'       => '.col-xs-12:nth-child(5) .col-sm-3',
+            'Description'        => '.col-xs-12:nth-child(6) .col-sm-3',
+            'Description Status' => '.col-xs-12:nth-child(7) .col-sm-3',
         ];
 
     /**
@@ -114,4 +114,3 @@ class DatadictTestIntegrationTest extends LorisIntegrationTest
         }
     }
 }
-
