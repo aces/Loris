87ea93fcb6bce79086114a9b7c2a16176d032524
CamilleBeau <camille.beaudoin@mcin.ca>
2020-08-13 14:08:08 -0400

    Reforming

--- tools/single_use/Set_Data_entry_completion_status_flag.php
+++ tools/single_use/Set_Data_entry_completion_status_flag.php
@@ -1,7 +1,9 @@
 
 <?php
 /**
- * This script is written for a one time use only to clean up existing data in the 'candidate_consent_rel' table to conform to the new front-end validation for entering consent.
+ * This script is written for a one time use only to migrate the
+ * 'Data_entry_completion_status' field from the instrument table /
+ * Data column of the flag table to it's own column in the flag table.
  *
  * PHP Version 7
  *
@@ -15,24 +17,46 @@ require_once __DIR__ . '/../generic_includes.php';
 
 $db     = \Database::singleton();
 
-$query       = "SELECT Data, CommentID FROM flag WHERE Data IS NOT NULL";
-$flagData = $db->pselectWithIndexKey($query, array(), 'CommentID');
-$dataToUpdate = array();
+$flagData = $db->pselectWithIndexKey(
+    "SELECT Data, CommentID, Test_name 
+                FROM flag",
+    [],
+    'CommentID'
+);
 
 foreach($flagData as $cmid => $data) {
+
+    $instrument = NDB_BVL_Instrument::factory(
+        $data['Test_name'],
+        $cmid,
+        ''
+    );
+
     $dataArray = json_decode($data['Data'], true);
 
-    if(isset($dataArray['Data_entry_completion_status'])) {
-        print_r($dataArray);
+    $dataToUpdate = [];
 
-        if($dataArray['Data_entry_completion_status'] === 'Complete') {
-            $dataToUpdate['Data_entry_completion_status'] = 'Complete';
+    if ($instrument->jsonData === true) {
+
+        // If json instrument, take value from flag data
+        if(isset($dataArray['Data_entry_completion_status'])) {
+            $dataToUpdate['Data_entry_completion_status']
+                = $dataArray['Data_entry_completion_status'];
         }
+    } else {
 
+        // Otherwise, take value from instrument table
+        $instrData = NDB_BVL_Instrument::loadInstanceData($instrument);
+        $dataToUpdate['Data_entry_completion_status']
+            = $instrData['Data_entry_completion_status'];
+    }
+
+    if(isset($dataArray['Data_entry_completion_status'])) {
         unset($dataArray['Data_entry_completion_status']);
-        $dataToUpdate['Data'] = $dataArray;
+        $dataToUpdate['Data'] = json_encode($dataArray);
+    }
 
-
+    if (!empty($dataToUpdate)) {
         $db->update(
             'flag',
             $dataToUpdate,
