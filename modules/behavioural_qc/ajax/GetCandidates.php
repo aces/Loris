0733ee5ce3ce6cf0a059c21e6801d2dd30c37ca6
Alizée Bellerose <misotheism@users.noreply.github.com>
2020-08-18 15:47:27 -0400

    fix

---
+++ modules/behavioural_qc/ajax/GetCandidates.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * This is a file called used by the behavioural qc browser
+ * auto-complete to retrieve candidates for a given
+ * instrument and/or visit.
+ *
+ * PHP Version 7
+ *
+ * @category Behavioural
+ * @package  Loris
+ * @author   Evan McIlroy <evanmcilroy@gmail.com>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link     https://www.github.com/aces/Loris/
+ */
+header("content-type:application/json");
+ini_set('default_charset', 'utf-8');
+
+require_once "Database.class.inc";
+require_once "NDB_Client.class.inc";
+
+$user = \User::singleton();
+if (!$user->hasPermission('behavioural_quality_control_view')) {
+    header("HTTP/1.1 403 Forbidden");
+    exit;
+}
+
+$db          = \NDB_Factory::singleton()->database();
+$searchArray = [];
+
+$query = "SELECT DISTINCT ses.candID FROM session AS ses
+            JOIN test_battery AS tst
+            ON ses.Visit_label = tst.Visit_label
+            WHERE ses.candID
+            LIKE :searchTerm";
+
+$searchArray[':searchTerm'] = $_REQUEST["query"] . '%';
+
+//Instrument is set adding that to the query.
+if (isset($_REQUEST['instrument']) && !empty($_REQUEST['instrument'])
+    && $_REQUEST['instrument'] != 'All Instruments'
+) {
+    $query   .= " AND tst.Test_Name = :NAM";
+    $testname = $db->pselectOne(
+        "SELECT Test_name FROM test_names WHERE Full_name =:fname",
+        ['fname' => $_REQUEST['instrument']]
+    );
+    $searchArray['NAM'] = $testname;
+}
+
+$result = $db->pselect($query, $searchArray);
+
+$flattened_result = [];
+
+if ($result == null) {
+    $flattened_result[0] = "Not found";
+} else {
+    foreach ($result as $k => $v) {
+        $flattened_result[$k] = $v['candID'];
+    }
+}
+
+$response = [];
+$response["suggestions"] = $flattened_result;
+print json_encode($response);
+
+
