09235a615502d56c9dd2acdf31fa22a81d2b0121
Dave MacFarlane <driusan@gmail.com>
2020-11-12 11:19:14 -0500

    Add missing class

---
+++ php/libraries/ProjectID.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+/**
+ * A representation of a ProjectID object. A ProjectID is always an integer
+ *
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ */
+class ProjectID extends ValidatableIdentifier
+{
+    /**
+     * Returns this identifier type
+     *
+     * @return string
+     */
+    public function getType(): string
+    {
+        return 'ProjectID';
+    }
+
+    /**
+     * Validate that the value of the ProjectID is a positive integer that does
+     *
+     * This does not check for uniqueness in the database or any other
+     * state-related facts.
+     *
+     * @param string $value The value to be validated
+     *
+     * @return bool True if the value format is valid
+     */
+    protected function validate(string $value): bool
+    {
+        return \Utility::valueIsPositiveInteger($value);
+    }
+
+    /**
+     * Generates a string representation of this SessionID.
+     *
+     * @return string The SessionID value.
+     */
+    public function __toString(): string
+    {
+        return $this->value;
+    }
+}
+
