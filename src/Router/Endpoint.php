87e8872daceeed277f293320683a7bf30f1aa844
Loris Admin <henri.rabalais@mcin.ca>
2020-11-20 14:45:29 -0500

    addressing dave review

--- src/Router/Endpoint.php
+++ src/Router/Endpoint.php
@@ -1,16 +1,4 @@
 <?php declare(strict_types=1);
-/**
- * An endpoint is a HTTP request handler which abstracts away common element
- * of different LORIS API endpoints.
- *
- * PHP Version 7
- *
- * @category Main
- * @package  Loris
- * @author   Dave MacFarlane <dave.macfarlane@mcin.ca>
- * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
- * @link     https://github.com/aces/Loris
- */
 namespace LORIS\Router;
 
 use \Psr\Http\Message\ServerRequestInterface;
@@ -22,7 +10,6 @@ use \Psr\Http\Message\ResponseInterface;
  *
  * @category Main
  * @package  Loris
- * @author   Dave MacFarlane <dave.macfarlane@mcin.ca>
  * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
  * @link     https://github.com/aces/Loris
  */
@@ -31,16 +18,13 @@ abstract class Endpoint implements RequestHandlerInterface
     /**
      * Return an array of valid HTTP methods for this endpoint
      *
-     * @return string[] Valid versions
+     * @return string[]
      */
     abstract protected function allowedMethods() : array;
 
     /**
-     * An API endpoint overrides the default LORIS middleware to remove the
-     * PageDecorationMiddleware, since the API only deals with JSON.
-     *
-     * It also acts as its own middleware by validating that an endpoint supports
-     * the HTTP method request type.
+     * Processes an incoming request by delegating it to $handler, unless it is
+     * an instance of ETagCalculator in which case it is processed via ETag.
      *
      * @param ServerRequestInterface  $request The incoming PSR7 request
      * @param RequestHandlerInterface $handler The PSR15 request handler
