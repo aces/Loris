e1d0d7bc6d8eb4e40e9866d770add1307ce6dcb1
Dave MacFarlane <driusan@gmail.com>
2020-11-25 14:29:28 -0500

    Reset Study Site before testing for permission denied

--- raisinbread/test/api/LorisApiVisitsTest.php
+++ raisinbread/test/api/LorisApiVisitsTest.php
@@ -135,6 +135,9 @@ class LorisApiVisitsTest extends LorisApiAuthenticatedTest
         $body = $response->getBody();
         $this->assertNotEmpty($body);
 
+        // Erase sites that were setup in LorisApiAuthenticatedTest
+        // setup for data access in other tests.
+        $this->resetStudySite();
         /**
         * Test changing from a site with no affiliation to a site with affiliation
         * Candidate 400266 is from site Rome. The test user only has access to
