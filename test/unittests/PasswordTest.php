38677ca3a7438603952b938e6b741e0f2e155e26
Dave MacFarlane <driusan@gmail.com>
2020-12-03 10:33:12 -0500

    Fix classname

--- test/unittests/PasswordTest.php
+++ test/unittests/PasswordTest.php
@@ -29,7 +29,7 @@ class PasswordTest extends TestCase
     /**
      * Test double for NDB_Config object
      *
-     * @var \NDB_Config | PHPUnit_Framework_MockObject_MockObject
+     * @var \NDB_Config | \PHPUnit\Framework\MockObject\MockObject
      */
     private $_configMock;
 
