563fa6bd053fbfd6734fdf85832461474bf41eef
Dave MacFarlane <driusan@gmail.com>
2020-12-03 10:33:12 -0500

    Fix classname

--- test/unittests/SettingsTest.php
+++ test/unittests/SettingsTest.php
@@ -27,7 +27,7 @@ class SettingsTest extends TestCase
     /**
      * Test double for NDB_Config object
      *
-     * @var NDB_Config | PHPUnit_Framework_MockObject_MockObject
+     * @var NDB_Config | \PHPUnit\Framework\MockObject\MockObject
      */
     private $_configMock;
 
