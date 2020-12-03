fc16205a72c3069ff746da692b85b10a3ea6344a
kongtiaowang <kongtiaowangshen@gmail.com>
2020-12-03 14:25:13 -0500

    add to module

--- SQL/New_patches/2020-09-21_add_ParentID.sql
+++ SQL/New_patches/2020-09-21_add_ParentID.sql
@@ -1,2 +1,2 @@
 ALTER TABLE candidate ADD COLUMN ParentID VARCHAR(15) NULL, ADD UNIQUE INDEX `UK_candidate_ParentID` (`ParentID` ASC);
-
+INSERT INTO modules (Name, Active) VALUES ('parent_portal', 'Y');
