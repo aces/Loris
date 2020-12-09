3f7e2f0a3de8b3f66338464e14d1bb201822e163
Dave MacFarlane <driusan@gmail.com>
2020-12-09 15:45:49 -0500

    Fix changed namespace

--- src/Http/Client.php
+++ src/Http/Client.php
@@ -15,7 +15,7 @@
 */
 namespace LORIS\Http;
 
-use \Http\Adapter\Guzzle6\Client as GuzzleClient;
+use \GuzzleHttp\Client as GuzzleClient;
 use \Psr\Http\Client\ClientInterface;
 use \Psr\Http\Message\RequestInterface;
 use \Psr\Http\Message\ResponseInterface;
