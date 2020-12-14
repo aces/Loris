ab9b3013fd56e3a60b95f820acd908cc4cdddf1b
Dave MacFarlane <driusan@gmail.com>
2020-12-14 14:54:34 -0500

    Add some missing sets

--- test/unittests/CandidateTest.php
+++ test/unittests/CandidateTest.php
@@ -1321,5 +1321,8 @@ class CandidateTest extends TestCase
             $database['host'],
             1
         );
+
+        $this->_factoryForDB->setDatabase($this->_DB);
+        $this->_factoryForDB->setConfig($this->_config);
     }
 }
