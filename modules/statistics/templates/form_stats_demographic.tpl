aces/Loris

commit 80fd0b736e7febf9610d9dd726d69f2dc56426b1

[Core: Utility]  Add type declarations to function signatures (#4046)
This adds type declarations to the signatures in the Utility.class.inc

modules/statistics/php/stats_demographic.class.inc
@@ -187,16 +187,18 @@ class Stats_Demographic extends \NDB_Form
         }
 
         //SUBPROJECTS
-        $subprojList ="";
-        $subprojects = \Utility::getSubprojectsForProject($currentProject);
-        foreach ($subprojects as $key=>$val) {
-            $subprojList .= $key.",";
-        }
-        $subprojList = substr($subprojList, 0, -1);
-        if (!empty($subprojList)) {
-            $suproject_query ="AND s.SubprojectID IN ($subprojList)";
-        } else {
-            $suproject_query ='';
+        $suproject_query = '';
+        // Only search for subprojects if $currentProject is a positive int.
+        if (is_numeric($currentProject) && intval($currentProject) >= 0) {
+            $subprojList ="";
+            $subprojects = \Utility::getSubprojectsForProject($currentProject);
+            foreach ($subprojects as $key=>$val) {
+                $subprojList .= $key.",";
+            }
+            $subprojList = substr($subprojList, 0, -1);
+            if (!empty($subprojList)) {
+                $suproject_query ="AND s.SubprojectID IN ($subprojList)";
+            }
         }
 
         //SITES

modules/statistics/php/stats_behavioural.class.inc
@@ -112,22 +112,24 @@ class Stats_Behavioural extends \NDB_Form
             $currentProject = '';
             $Param_Project  = '';
         }
-
-        $subprojects = \Utility::getSubprojectsForProject($currentProject);
-
-        $this->tpl_data['Subprojects'] = $subprojects;
-        foreach ($subprojects as $key=>$val) {
-            $subprojList .= $key.",";
+        //
+        //SUBPROJECTS
+        $suproject_query = '';
+        // Only search for subprojects if $currentProject is a positive int.
+        if (is_numeric($currentProject) && intval($currentProject) >= 0) {
+            $subprojList ="";
+            $subprojects = \Utility::getSubprojectsForProject($currentProject);
+            foreach ($subprojects as $key=>$val) {
+                $subprojList .= $key.",";
+            }
+            $subprojList = substr($subprojList, 0, -1);
+            if (!empty($subprojList)) {
+                $suproject_query ="AND s.SubprojectID IN ($subprojList)";
+            }
+            $Visits = \Utility::getExistingVisitLabels($currentProject);
         }
-        $subprojList = substr($subprojList, 0, -1);
-        if (!empty($subprojList)) {
-            $suproject_query ="AND s.SubprojectID IN ($subprojList)";
-        } else {
-            $suproject_query ='';
-        }
 
-        $Visits = \Utility::getExistingVisitLabels($currentProject);
-        $this->tpl_data['Visits'] = $Visits;
+        $this->tpl_data['Visits'] = $Visits ?? '';
 
         //---- BEHAVIORAL STATS -----
         $result = $DB->pselect(

commit 3c0af361fa16ae4585f735277d04633af5579b26

[statistics] Fix subprojects on demographics page (#7125)
Fix display of subprojects on stats demographic broken in PR #4046

modules/statistics/php/stats_demographic.class.inc
@@ -250,6 +250,7 @@ class Stats_Demographic extends \NDB_Form
 
         //SUBPROJECTS
         $subprojectQuery = '';
+        $subprojects     = \Utility::getSubprojectList();
         // Only search for subprojects if $demographicProject is a positive int.
         if (is_numeric($demographicProject) && intval($demographicProject) >= 0) {
             $subprojects = \Utility::getSubprojectsForProject($demographicProject);
@@ -257,7 +258,6 @@ class Stats_Demographic extends \NDB_Form
             if (!empty($subprojList)) {
                 $subprojectQuery ="AND s.SubprojectID IN ($subprojList)";
             }
-            $this->tpl_data['Subprojects'] = $subprojects;
         }
 
         //SITES
@@ -308,9 +308,10 @@ class Stats_Demographic extends \NDB_Form
             }
         }
 
-        $this->tpl_data['Sites']    = $sites;
-        $this->tpl_data['Projects'] = $projects;
-        $this->tpl_data['Visits']   = $visits;
+        $this->tpl_data['Sites']       = $sites;
+        $this->tpl_data['Projects']    = $projects;
+        $this->tpl_data['Visits']      = $visits;
+        $this->tpl_data['Subprojects'] = $subprojects;
 
         /**
          * REGISTERED CANDIDATES ROW

modules/statistics/templates/form_stats_demographic.tpl
@@ -16,7 +16,7 @@
         <tr>
             <th colspan="2" id="demog">Demographics</th>
             {if {$CurrentProject.ID|default > 0}}
-                <th>Undefined</th>
+                <th>No defined subproject</th>
             {/if}
             {foreach from=$Subprojects|default item=name key=proj}
                 <th>{$name}</th>