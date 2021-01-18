aces/Loris

commit 2457142f547d0e7beadae5c52749b51212dc77d0

Use the SessionID class in LORIS (#5352)
This updates core libraries and related code to use the SessionID class.

Some TLC and optimization is also given to aging code in these files.

    Resolves #4369
    Class first created (but not implemented) in #4333
    Assists with push for stronger typing in #4210

modules/instrument_list/php/instrument_list_controlpanel.class.inc
@@ -5,7 +5,7 @@
  * This class provides the management GUI for the status flags
  * (session table) of a time point in the NeuroDB framework.
  *
- * PHP Version 5
+ * PHP Version 7
  *
  * @category Instrument_List
  * @package  Loris
@@ -21,7 +21,7 @@ namespace LORIS\instrument_list;
  * This class provides the management GUI for the status flags
  * (session table) of a time point in the NeuroDB framework.
  *
- * PHP Version 5
+ * PHP Version 7
  *
  * @category Instrument_List
  * @package  Loris
@@ -32,9 +32,9 @@ namespace LORIS\instrument_list;
 class Instrument_List_ControlPanel extends \TimePoint
 {
     /**
-     * Construct funcion
+     * Construct function
      *
-     * @param string $sessionID the value of sessionID
+     * @param \SessionID $sessionID the value of sessionID
      *
      * @return void
      */
@@ -55,8 +55,8 @@ class Instrument_List_ControlPanel extends \TimePoint
         $factory = \NDB_Factory::singleton();
         $this->tpl_data['baseurl'] = $factory->settings()->getBaseURL();
 
-        $this->tpl_data['candID']    = $this->getData('CandID');
-        $this->tpl_data['sessionID'] = $this->getData('SessionID');
+        $this->tpl_data['candID']    = $this->getCandID();
+        $this->tpl_data['sessionID'] = $this->getSessionID();
 
         // start next stage
         $this->tpl_data['access']['next_stage'] = $this->_displayStartNextStage();
@@ -139,7 +139,7 @@ class Instrument_List_ControlPanel extends \TimePoint
 
                 // set the SessionID for the battery
                 $battery->selectBattery(
-                    $this->getData('SessionID')
+                    $this->getSessionID()
                 );
 
                 // get the list of instruments
@@ -201,7 +201,7 @@ class Instrument_List_ControlPanel extends \TimePoint
                 $battery = new \NDB_BVL_Battery();
                 // set the SessionID for the battery
                 $battery->selectBattery(
-                    $this->getData('SessionID')
+                    $this->getSessionID()
                 );
                 // get the list of instruments
                 $batteryList = $battery->getBatteryVerbose();
@@ -335,13 +335,13 @@ class Instrument_List_ControlPanel extends \TimePoint
         $battery = new \NDB_BVL_Battery;
 
         // set the SessionID for the battery
-        $battery->selectBattery($_REQUEST['sessionID']);
+        $battery->selectBattery(new \SessionID($_REQUEST['sessionID']));
 
         // get the list of instruments
         $batteryList = $battery->getBatteryVerbose();
 
         // Get the list of DDE instruments
-        $config         =& \NDB_Config::singleton();
+        $config         = \NDB_Config::singleton();
         $ddeInstruments = $config->getSetting('DoubleDataEntryInstruments');
 
         // loop the list and check the Data Entry status
@@ -441,9 +441,8 @@ class Instrument_List_ControlPanel extends \TimePoint
             return ($user->hasPermission('bvl_feedback'));
         }
 
-        return ($user->hasPermission('data_entry') &&
-                in_array($this->getData('CenterID'), $user->getData('CenterIDs'))
-               );
+        return $user->hasPermission('data_entry')
+            && $user->hasCenter($this->getCenterID());
     }
     /**
      * DisplayStatus function
@@ -543,7 +542,7 @@ class Instrument_List_ControlPanel extends \TimePoint
         $battery = new \NDB_BVL_Battery();
 
         // set the SessionID for the battery
-        $battery->selectBattery($this->getData('SessionID'));
+        $battery->selectBattery($this->getSessionID());
 
         // get the list of instruments
         $batteryList = $battery->getBatteryVerbose();
@@ -591,8 +590,8 @@ class Instrument_List_ControlPanel extends \TimePoint
     function _getSendToDCCStatusMessage()
     {
         // create user object
-        $user   =& \User::singleton();
-        $config =& \NDB_Config::singleton();
+        $user   = \User::singleton();
+        $config = \NDB_Config::singleton();
 
         // get the value of the session.Scan_done field
         $scanDone = $this->getData('Scan_done');
@@ -600,19 +599,13 @@ class Instrument_List_ControlPanel extends \TimePoint
         if (!$user->hasPermission('send_to_dcc')) {
             return "User does not have permission to send to DCC";
         }
-        if (!in_array(
-            $this->getData('CenterID'),
-            $user->getData('CenterIDs')
-        )
-        ) {
-            $returnStr = "Users must belong to the same site".
-                         " as the candidate to submit to DCC.";
-            return $returnStr;
+        if (!$user->hasCenter($this->getCenterID())) {
+            return "Users must belong to the same site"
+                . " as the candidate to submit to DCC.";
         }
         if ($this->isSubmitted()) {
-            $returnStr = "This timepoint has already been sent to DCC.".
+            return "This timepoint has already been sent to DCC.".
                          "  Try refreshing your page.";
-            return $returnStr;
         }
         if (!in_array(
             $this->getData('Current_stage'),
@@ -622,9 +615,8 @@ class Instrument_List_ControlPanel extends \TimePoint
             ]
         )
         ) {
-            $returnStr = "This stage is not a Screening or Visit stage.".
-                         "  Only Screening or Visit stages can be sent to DCC.";
-            return $returnStr;
+            return "This stage is not a Screening or Visit stage. "
+                . "Only Screening or Visit stages can be sent to DCC.";
         }
         if (in_array(
             $this->getData($this->getData('Current_stage')),
@@ -634,12 +626,11 @@ class Instrument_List_ControlPanel extends \TimePoint
             ]
         )
         ) {
-            $returnStr = "The status of this stage is either".
-                         " In Progress or has not been set.";
-            return $returnStr;
+            return "The status of this stage is either"
+                . " In Progress or has not been set.";
         }
-        if ($config->getSetting("useScanDone")==="false"
-            || ($this->getData('Current_stage') !=            'Screening'
+        if (! $config->settingEnabled("useScanDone")
+            || ($this->getData('Current_stage') != 'Screening'
             && empty($scanDone))
         ) {
             return "The Scan done field must be entered for the Visit stage";
@@ -648,13 +639,12 @@ class Instrument_List_ControlPanel extends \TimePoint
         $battery = new \NDB_BVL_Battery();
 
         // set the SessionID for the battery
-        $battery->selectBattery($this->getData('SessionID'));
+        $battery->selectBattery($this->getSessionID());
 
         // get the list of instruments
         $batteryList = $battery->getBatteryVerbose();
 
         // Get the list of DDE instruments
-        $config         =& \NDB_Config::singleton();
         $ddeInstruments = $config->getSetting('DoubleDataEntryInstruments');
 
         // clear the unresolved conflicts for all the instruments
@@ -665,7 +655,7 @@ class Instrument_List_ControlPanel extends \TimePoint
             $status->select($instrument['CommentID']);
             $dataEntry = $status->getDataEntryStatus();
             if ($dataEntry != 'Complete') {
-                $allDataEntryComplete =false;
+                $allDataEntryComplete = false;
                 break;
             }
 
@@ -676,7 +666,7 @@ class Instrument_List_ControlPanel extends \TimePoint
                 $status->select($instrument['DDECommentID']);
                 $dataEntry = $status->getDataEntryStatus();
                 if ($dataEntry != 'Complete') {
-                    $allDataEntryComplete =false;
+                    $allDataEntryComplete = false;
                     break;
                 }
             }
@@ -707,10 +697,7 @@ class Instrument_List_ControlPanel extends \TimePoint
         ) {
             return true;
         } elseif ($user->hasPermission('send_to_dcc')
-            && in_array(
-                $this->getData('CenterID'),
-                $user->getData('CenterIDs')
-            )
+            && $user->hasCenter($this->getData('CenterID'))
             && $this->getData('Submitted')=='N'
             && in_array(
                 $this->getData('Current_stage'),
@@ -726,9 +713,9 @@ class Instrument_List_ControlPanel extends \TimePoint
                     'In Progress',
                 ]
             )
-            && ($config->getSetting("useScanDone")==="false"
-            || ($this->getData('Current_stage') == 'Screening'
-            || !empty($scanDone)            )            )
+            && ! $config->settingEnabled("useScanDone")
+            || $this->getData('Current_stage') == 'Screening'
+            || !empty($scanDone)
         ) {
             return true;
         }
@@ -747,10 +734,8 @@ class Instrument_List_ControlPanel extends \TimePoint
         $user =& \User::singleton();
 
         // check user permissions
-        if (!$user->hasPermission('data_entry') || !in_array(
-            $this->getData('CenterID'),
-            $user->getData('CenterIDs')
-        )
+        if (! $user->hasPermission('data_entry')
+            || ! $user->hasCenter($this->getCenterID())
         ) {
             return false;
         }

modules/timepoint_list/php/timepoint_list.class.inc
@@ -19,7 +19,7 @@ use \Loris\StudyEntities\Candidate\CandID;
 /**
  * The timepoint list menu
  *
- * PHP Version 5
+ * PHP Version 7
  *
  * @category Main
  * @package  Timepoint_List
@@ -60,7 +60,8 @@ class Timepoint_List extends \NDB_Menu
         ServerRequestInterface $request,
         RequestHandlerInterface $handler
     ) : ResponseInterface {
-        // FIXME: Update the code to add the singleton here, not the identifier.
+        // FIXME: At some point this class and related classes should use a
+        // Candidate object rather than passing a CandID around.
         $this->candID = new CandID($request->getAttribute('CandID'));
         return parent::process($request, $handler);
     }
@@ -70,10 +71,10 @@ class Timepoint_List extends \NDB_Menu
      *
      * @return void
      */
-    function setup()
+    function setup(): void
     {
         // create candidate object
-        $candidate =& \Candidate::singleton($this->candID);
+        $candidate = \Candidate::singleton($this->candID);
 
         $numberOfVisits           = 0;
         $this->tpl_data['candID'] = $this->candID;
@@ -82,7 +83,7 @@ class Timepoint_List extends \NDB_Menu
         $this->tpl_data['candidate'] = $candidate->getData();
 
         if (!empty($listOfTimePoints)) {
-            $user     =& \User::singleton();
+            $user     = \User::singleton();
             $username = $user->getData('UserID');
 
             $feedback_select_inactive = null;
@@ -95,7 +96,8 @@ class Timepoint_List extends \NDB_Menu
              */
             $x = 0;
             foreach ($listOfTimePoints as $currentTimePoint) {
-                $timePoint =& \TimePoint::singleton($currentTimePoint);
+                $sessionID = new \SessionID($currentTimePoint);
+                $timePoint = \TimePoint::singleton($sessionID);
 
                 // get the first date of visit in order to
                 // turn on the future time points bit if we have a date of visit
@@ -122,7 +124,7 @@ class Timepoint_List extends \NDB_Menu
                 $feedback = \NDB_BVL_Feedback::singleton(
                     $username,
                     null,
-                    $timePoint->getData('SessionID')
+                    $sessionID
                 );
 
                 $feedback_status = $feedback->getMaxThreadStatus(
@@ -159,8 +161,7 @@ class Timepoint_List extends \NDB_Menu
                     $outcomeStage
                         = $this->_determinePreviousStage($currentTimePoint);
 
-                    $getStatusMethod
-                        = 'get'.$outcomeStage.'Status';
+                    $getStatusMethod = 'get'.$outcomeStage.'Status';
 
                     $getDateMethod = 'getDateOf'.$outcomeStage;
 
@@ -184,7 +185,9 @@ class Timepoint_List extends \NDB_Menu
              * List of future visits
              */
             if (isset($firstDateOfVisit)) {
-                $timePoint = \TimePoint::singleton($listOfTimePoints[0]);
+                $timePoint = \TimePoint::singleton(
+                    new \SessionID($listOfTimePoints[0])
+                );
 
                 $this->tpl_data['SubprojectID'] = $timePoint->getSubprojectID();
 
@@ -192,19 +195,18 @@ class Timepoint_List extends \NDB_Menu
         }
 
         $this->tpl_data['actions'] = $this->getActionButtons();
-
     }
     /**
      * Determining Previous Stage
      *
-     * @param string $sessionID sessionID
+     * @param \SessionID $sessionID that refers to this TimePoint
      *
      * @return string
      */
-    function _determinePreviousStage($sessionID)
+    function _determinePreviousStage(\SessionID $sessionID): string
     {
         // create timepoint object
-        $timePoint =& \TimePoint::singleton($sessionID);
+        $timePoint = \TimePoint::singleton($sessionID);
 
         // outcome stage is the last stage
         //(approval || visit || screening || not started, in that order)
@@ -222,26 +224,22 @@ class Timepoint_List extends \NDB_Menu
      * Used by the NDB_caller class when loading a page.
      * Call the display function of the appropriate modules feedback panel.
      *
-     * @param CandID $candID    candID
-     * @param string $sessionID sessionID
+     * @param CandID $candID candID
      *
      * @return string
      */
-    function getFeedbackPanel(CandID $candID, $sessionID = null)
+    function getFeedbackPanel(CandID $candID): string
     {
-        $feedbackPanel = new \BVL_Feedback_Panel($candID);
-        $html          = $feedbackPanel->display();
-        return $html;
+        return (new \BVL_Feedback_Panel($candID))->display();
     }
     /**
      * Get Action Buttons
      *
      * @return string
      */
-    function getActionButtons()
+    function getActionButtons(): string
     {
-        $controlPanel = new TimePoint_List_ControlPanel($this->candID);
-        return $controlPanel->display();
+        return (new TimePoint_List_ControlPanel($this->candID))->display();
     }
 
     /**

php/libraries/BVL_Feedback_Panel.class.inc
@@ -49,12 +49,12 @@ class BVL_Feedback_Panel
      * sessionID and commentID.
      *
      * @param CandID      $candID    The candidate ID.
-     * @param int|null    $sessionID The session ID for a given visit.
+     * @param ?SessionID  $sessionID The session ID for a given visit.
      * @param string|null $commentID The comment ID for a given instrument.
      */
     function __construct(
         CandID $candID,
-        ?int $sessionID=null,
+        ?SessionID $sessionID=null,
         ?string $commentID=null
     ) {
 

php/libraries/NDB_BVL_Feedback.class.inc
@@ -63,17 +63,17 @@ class NDB_BVL_Feedback
      * can be passed but not necessarily all of them, depending on which
      * page is receiving feedback.
      *
-     * @param string   $username  The currently logged in user (why?)
-     * @param ?CandID  $candID    The 6 digit CandID
-     * @param ?integer $sessionID The session ID with feedback
-     * @param ?string  $commentID The commentID receiving feedback.
+     * @param string     $username  The currently logged in user (why?)
+     * @param ?CandID    $candID    The 6 digit CandID
+     * @param ?SessionID $sessionID The session ID with feedback
+     * @param ?string    $commentID The commentID receiving feedback.
      *
      * @return NDB_BVL_Feedback object
      */
     static function &singleton(
         $username,
         ?CandID $candID=null,
-        $sessionID=null,
+        ?SessionID $sessionID=null,
         $commentID=null
     ) {
         // at least one of the args have to be passed
@@ -120,18 +120,18 @@ class NDB_BVL_Feedback
     /**
      * Sets up a newly instantiated feedback thread object
      *
-     * @param string  $username  The name of the currently logged in user.
-     *                           (why is this here?)
-     * @param ?CandID $candID    The CandID we're getting a feedback thread for.
-     * @param ?string $sessionID The sessionID we're getting a feedback thread for.
-     * @param ?string $commentID The commentID we're getting a feedback thread for.
+     * @param string     $username  The name of the currently logged in user.
+     *                              FIXME (why is this here?)
+     * @param ?CandID    $candID    The CandID for this feedback thread.
+     * @param ?SessionID $sessionID The sessionID for this feedback thread.
+     * @param ?string    $commentID The commentID for this feedback thread.
      *
      * @return void
      */
     function setup(
         string $username,
         ?CandID $candID=null,
-        ?string $sessionID=null,
+        ?SessionID $sessionID=null,
         $commentID=null
     ) {
 
@@ -164,10 +164,10 @@ class NDB_BVL_Feedback
      *
      * @return void
      */
-    function _setCandidateProfileInfo()
+    function _setCandidateProfileInfo(): void
     {
         // new DB Object
-        $db =& Database::singleton();
+        $db = NDB_Factory::singleton()->database();
 
         // SELECT section
         $query = "SELECT c.CandID";
@@ -196,7 +196,7 @@ class NDB_BVL_Feedback
             $query          .= " WHERE c.CandID = :CaID";
             $qparams['CaID'] = (string) $this->_feedbackObjectInfo['CandID'];
         } else {
-            throw new Exception(
+            throw new InvalidArgumentException(
                 "You need to pass at least one of the following to setup "
                 . "the Feedback Object: CandID, SessionID, or CommentID"
             );
@@ -205,7 +205,7 @@ class NDB_BVL_Feedback
 
         $result = $db->pselect($query, $qparams);
         if (!is_array($result) || count($result) == 0) {
-            throw new Exception(
+            throw new LorisException(
                 "Error, unable to select data for the feedback object"
             );
         }
@@ -224,7 +224,7 @@ class NDB_BVL_Feedback
      *
      * @return string feedback_level (either profile, visit, or instrument)
      */
-    function getFeedbackLevel()
+    function getFeedbackLevel(): string
     {
         return $this->_feedbackLevel;
     }
@@ -234,14 +234,14 @@ class NDB_BVL_Feedback
      *
      * @return array
      */
-    static public function getFeedbackTypes()
+    static public function getFeedbackTypes(): array
     {
-        // new DB Object
 
         $query = "SELECT Feedback_type as Type, Name, Description
             FROM feedback_bvl_type";
 
-        $result = \Database::singleton()->pselect($query, []);
+        $result = NDB_Factory::singleton()->database()
+            ->pselect($query, []);
 
         $list = [];
         foreach ($result as $record) {
@@ -277,25 +277,29 @@ class NDB_BVL_Feedback
     /**
      * Create new feedback type
      *
-     * @param string $name        feedback type name
-     * @param string $description feedback type description
+     * @param string  $name        feedback type name
+     * @param ?string $description feedback type description
      *
      * @return int feedback type id
      * @throws DatabaseException
      * @throws LorisException
      */
-    public function createFeedbackType($name, $description = null)
-    {
-        if (empty($name) || strlen($name)>100 ) {
-            throw new LorisException("Invalid feedback type name");
+    public function createFeedbackType(
+        string $name,
+        ?string $description = null
+    ): int {
+        if (empty($name) || strlen($name) > 100 ) {
+            throw new InvalidArgumentException(
+                "Invalid feedback type name: name must not be empty and must "
+                . "not exceed 100 characters"
+            );
         }
 
         if (empty($description)) {
             $description = null;
         }
 
-        $factory = NDB_Factory::singleton();
-        $db      = $factory->Database();
+        $db = NDB_Factory::singleton()->database();
 
         $values = [
             'Name'        => $name,
@@ -314,6 +318,7 @@ class NDB_BVL_Feedback
      */
     function getThreadType($feedbackID): ?string
     {
+
         // new DB Object
         $db = \NDB_Factory::singleton()->database();
 
@@ -585,7 +590,7 @@ class NDB_BVL_Feedback
             )";
             $qparams['Username'] = $this->_username;
         } else {
-            $query            .= " AND ft.Active ='Y' 
+            $query            .= " AND ft.Active ='Y'
                                    AND FIND_IN_SET(s.CenterID, :CentID)";
             $qparams['CentID'] = implode(',', $user->getCenterIDs());
         }

modules/api/php/endpoints/candidate/candidate.class.inc
@@ -105,7 +105,9 @@ class Candidate extends Endpoint implements \LORIS\Middleware\ETagCalculator
         );
 
         try {
-            $visit = \NDB_Factory::singleton()->timepoint((int) $sessionid);
+            $visit = \NDB_Factory::singleton()->timepoint(
+                new \SessionID(strval($sessionid))
+            );
         } catch (\LorisException | \TypeError $e) {
             $visit = null;
         }

modules/api/php/endpoints/candidate/visit/visit.class.inc
@@ -92,7 +92,7 @@ class Visit extends Endpoint implements \LORIS\Middleware\ETagCalculator
         if (count($pathparts) === 0) {
             switch ($request->getMethod()) {
             case 'GET':
-                return $this->_handleGET($request);
+                return $this->_handleGET();
 
             case 'PUT':
                 return $this->_handlePUT($request);
@@ -228,7 +228,9 @@ class Visit extends Endpoint implements \LORIS\Middleware\ETagCalculator
 
         if ($sessionid !== false) {
             // If the visit label already exists for that candidate
-            $timepoint = \NDB_Factory::singleton()->timepoint($sessionid);
+            $timepoint = \NDB_Factory::singleton()->timepoint(
+                new \SessionID(strval($sessionid))
+            );
             if ($timepoint->getCurrentStage() !== 'Not Started') {
                 return new \LORIS\Http\Response\JSON\Conflict(
                     'This visit is already started'

commit 53d2ec0721b2674a27949ae65c037e33ad1137c7

[cand. profile/instr. list] Send to DCC/BVL Feedback sessionID fixes (#7063)
This completes the work done on #7036 to solve some errors introduced by #5352 (candidate profile/instrument list)

error 500 if $_POST['sessionID'] is an empty string (react_get_bvl_threads.php)
console warning: sessionID is of type object (BVL_Feedback_Panel.class.inc)
one omitted conversion of $_POST['sessionID'] to new \SessionID($_POST['sessionID']) (bvl_panel_ajax.php)

Resolves #7139
Resolves #7017
Resolves #6987

modules/bvl_feedback/ajax/bvl_panel_ajax.php
@@ -20,26 +20,24 @@ use \LORIS\StudyEntities\Candidate\CandID;
 
 $username = \User::singleton()->getUsername();
 
-if (isset($_POST['candID']) && (empty($_POST['sessionID']))) {
-    $candID         = new CandID($_POST['candID']);
-    $feedbackThread = \NDB_BVL_Feedback::Singleton($username, $candID);
-} elseif (isset($_POST['candID']) && isset($_POST['sessionID'])
-    && (empty($_POST['commentID']))
-) {
-    $candID         = new CandID($_POST['candID']);
+if (isset($_POST['candID']) && !empty($_POST['candID'])) {
+    $candID    = new CandID($_POST['candID']);
+    $sessionID = null;
+    $commentID = null;
+
+    if (isset($_POST['sessionID']) && !empty($_POST['sessionID'])) {
+        $sessionID = new \SessionID($_POST['sessionID']);
+    }
+
+    if (isset($_POST['commentID']) && !empty($_POST['commentID'])) {
+        $commentID = $_POST['commentID'];
+    }
+
     $feedbackThread =& \NDB_BVL_Feedback::Singleton(
         $username,
         $candID,
-        $_POST['sessionID']
-    );
-} elseif (isset($_POST['candID']) && isset($_POST['sessionID'])
-    && isset($_POST['commentID'])
-) {
-    $candID         = new CandID($_POST['candID']);
-    $feedbackThread =& \NDB_BVL_Feedback::Singleton(
-        $username,
-        $candID,
-        new \SessionID($_POST['sessionID']),
-        $_POST['commentID']
+        $sessionID,
+        $commentID
     );
 }
+

modules/bvl_feedback/ajax/react_get_bvl_threads.php
@@ -24,34 +24,29 @@ require_once __DIR__ . "/../../../vendor/autoload.php";
 
 $username = \User::singleton()->getUsername();
 
+if (isset($_POST['candID']) && !empty($_POST['candID'])) {
+    $candID    = new CandID($_POST['candID']);
+    $sessionID = null;
+    $commentID = null;
 
-if (isset($_POST['candID']) && !(isset($_POST['sessionID']))) {
-    $candID         = new CandID($_POST['candID']);
-    $feedbackThread =& \NDB_BVL_Feedback::Singleton($username, $candID);
-} elseif (isset($_POST['candID']) && isset($_POST['sessionID'])
-    && !(isset($_POST['commentID']))
-) {
-    $candID         = new CandID($_POST['candID']);
-    $feedbackThread =&
-        \NDB_BVL_Feedback::Singleton(
-            $username,
-            $candID,
-            new \SessionID($_POST['sessionID'])
-        );
-} elseif (isset($_POST['candID']) && isset($_POST['sessionID'])
-    && isset($_POST['commentID'])
-) {
-    $candID         = new CandID($_POST['candID']);
-    $feedbackThread =&
-        \NDB_BVL_Feedback::Singleton(
-            $username,
-            $candID,
-            new \SessionID($_POST['sessionID']),
-            $_POST['commentID']
-        );
+    if (isset($_POST['sessionID']) && !empty($_POST['sessionID'])) {
+        $sessionID = new \SessionID($_POST['sessionID']);
+    }
+
+    if (isset($_POST['commentID']) && !empty($_POST['commentID'])) {
+        $commentID = $_POST['commentID'];
+    }
+
+    $feedbackThread =& \NDB_BVL_Feedback::Singleton(
+        $username,
+        $candID,
+        $sessionID,
+        $commentID
+    );
 }
 
 $feedbackThreadList = $feedbackThread->getThreadList();
 echo json_encode($feedbackThreadList);
 
 exit();
+

modules/instrument_list/php/instrument_list_controlpanel.class.inc
@@ -186,7 +186,7 @@ class Instrument_List_ControlPanel extends \TimePoint
                 ['Submitted' => $submitted]
             );
 
-                // revert from approval (or the bin)
+            // revert from approval (or the bin)
             if ($currentStage == 'Approval') {
                 $this->setData(
                     [
@@ -713,9 +713,9 @@ class Instrument_List_ControlPanel extends \TimePoint
                     'In Progress',
                 ]
             )
-            && ! $config->settingEnabled("useScanDone")
+            && (!$config->settingEnabled("useScanDone")
             || $this->getData('Current_stage') == 'Screening'
-            || !empty($scanDone)
+            || !empty($scanDone))
         ) {
             return true;
         }

modules/timepoint_list/php/timepoint_list.class.inc
@@ -174,7 +174,7 @@ class Timepoint_List extends \NDB_Menu
 
                 // get the outcome data
                 $outcomeStage
-                    = $this->_determinePreviousStage($timePoint);
+                    = $this->_determinePreviousStage($sessionID);
 
                 $getStatusMethod = 'get'.$outcomeStage.'Status';
 

php/libraries/BVL_Feedback_Panel.class.inc
@@ -85,7 +85,9 @@ class BVL_Feedback_Panel
         $candidateObject         = Candidate::singleton($candidateID);
         $this->tpl_data['pscid'] = $candidateObject->getPSCID();
 
-        $this->tpl_data['sessionID'] = $feedbackProfile["SessionID"] ?? null;
+        $this->tpl_data['sessionID'] = isset($feedbackProfile["SessionID"]) ?
+            strval($feedbackProfile["SessionID"]) :
+            null;
 
         $feedbackObject = $this->feedbackThread->_feedbackObjectInfo;
         $this->tpl_data['commentID'] = $feedbackObject["CommentID"] ?? null;

php/libraries/NDB_BVL_Feedback.class.inc
@@ -215,7 +215,9 @@ class NDB_BVL_Feedback
             || isset($this->_feedbackObjectInfo['SessionID'])
         ) {
             $this->_feedbackCandidateProfileInfo['SessionID']
-                = $result[0]['SessionID'] ?? $this->_feedbackObjectInfo['SessionID'];
+                = isset($result[0]['SessionID']) ?
+                  new \SessionID($result[0]['SessionID']) :
+                  $this->_feedbackObjectInfo['SessionID'];
         }
     }
 