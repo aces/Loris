aces/Loris

commit d78c9fbb0329e46a5a326aac538b80985bb14873

sweetalert update (#6195)
This updates sweetalert to sweetalert2 in most javascript files. However, the only file that does not change is NDB_Page.class.inc, since there is some error in Travis.

 Resolves (partially) #6114
 See also #6192

modules/publication/jsx/projectFields.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import swal from 'sweetalert2';
 
 class EmailElement extends React.Component {
   constructor() {
@@ -101,7 +102,7 @@ class ProjectFormFields extends React.Component {
 
   deleteUpload(uploadID) {
     let self = this;
-    swal({
+    swal.fire({
       title: 'Are you sure?',
       text: 'Are you sure you want to delete this file?',
       type: 'warning',

modules/candidate_parameters/jsx/CandidateDOB.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Loader from 'Loader';
+import swal from 'sweetalert2';
 
 class CandidateDOB extends Component {
   constructor(props) {
@@ -111,7 +112,7 @@ class CandidateDOB extends Component {
     let dob = this.state.formData.dob ?
       this.state.formData.dob : null;
     if (dob > today) {
-      swal({
+      swal.fire({
         title: 'Error!',
         text: 'Date of birth cannot be later than today!',
         type: 'error',
@@ -140,7 +141,7 @@ class CandidateDOB extends Component {
     })
     .then((resp) => {
         if (resp.ok && resp.status === 200) {
-          swal({
+          swal.fire({
             title: 'Success!',
             text: 'Date of birth updated!',
             type: 'success',
@@ -150,7 +151,7 @@ class CandidateDOB extends Component {
               this.fetchData();
           }
         } else {
-          swal({
+          swal.fire({
             title: 'Error!',
             text: 'Something went wrong.',
             type: 'error',

modules/candidate_parameters/jsx/CandidateDOD.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Loader from 'Loader';
+import swal from 'sweetalert2';
 
 class CandidateDOD extends Component {
   constructor(props) {
@@ -112,7 +113,7 @@ class CandidateDOD extends Component {
       this.state.data.dob : null;
 
     if (dod > today) {
-      swal({
+      swal.fire({
         title: 'Invalid date',
         text: 'Date of death cannot be later than today!',
         type: 'error',
@@ -122,7 +123,7 @@ class CandidateDOD extends Component {
     }
 
     if (dob > dod) {
-      swal({
+      swal.fire({
         title: 'Invalid date',
         text: 'Date of death must be after date of birth!',
         type: 'error',
@@ -152,7 +153,7 @@ class CandidateDOD extends Component {
     }
     ).then((resp) => resp.text()
     ).then((result) => {
-      swal({
+      swal.fire({
             title: 'Success!',
             text: 'Date of death updated!',
             type: 'success',
@@ -163,7 +164,7 @@ class CandidateDOD extends Component {
       }
     }).catch((error) => {
         console.error(error);
-        swal({
+        swal.fire({
           title: 'Error!',
           text: 'Something went wrong.',
           type: 'error',

modules/candidate_parameters/jsx/ConsentStatus.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-
+import swal from 'sweetalert2';
 import Loader from 'Loader';
 
 /**
@@ -181,13 +181,13 @@ class ConsentStatus extends Component {
             contentType: false,
             processData: false,
             success: (data) => {
-                swal('Success!', 'Update successful.', 'success');
+                swal.fire('Success!', 'Update successful.', 'success');
                 this.fetchData();
             },
             error: (error) => {
                 console.error(error);
                 let errorMessage = error.responseText || 'Update failed.';
-                swal('Error!', errorMessage, 'error');
+                swal.fire('Error!', errorMessage, 'error');
             },
         });
     }

modules/data_release/jsx/addPermissionForm.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Loader from 'jsx/Loader';
+import swal from 'sweetalert2';
 
 /**
  * Add Permission Form
@@ -161,7 +162,7 @@ class AddPermissionForm extends Component {
       body: formObj,
     }).then( (response) => {
       if (response.ok) {
-        swal({
+        swal.fire({
           text: 'Permission Update Success!',
           title: '',
           type: 'success',
@@ -173,7 +174,7 @@ class AddPermissionForm extends Component {
         let msg = response.statusText ?
           response.statusText :
           'Submission Error!';
-        swal(msg, '', 'error');
+        swal.fire(msg, '', 'error');
         console.error(msg);
       }
     });

modules/data_release/jsx/managePermissionsForm.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Loader from 'jsx/Loader';
+import swal from 'sweetalert2';
 import Modal from 'Modal';
 
 /**
@@ -130,7 +131,7 @@ class ManagePermissionsForm extends Component {
     })
     .then((response) => {
       if (response.ok) {
-        swal({
+        swal.fire({
           text: 'Permission Update Success!',
           title: '',
           type: 'success',
@@ -140,7 +141,7 @@ class ManagePermissionsForm extends Component {
       } else {
         let msg = response.statusText ?
           response.statusText : 'Submission Error!';
-        swal(msg, '', 'error');
+        swal.fire(msg, '', 'error');
         console.error(msg);
         return Promise.reject();
       }

modules/data_release/jsx/uploadFileForm.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import ProgressBar from 'ProgressBar';
+import swal from 'sweetalert2';
 
 
 /**
@@ -134,7 +135,7 @@ class UploadFileForm extends Component {
                 + ')';
       errorMessage['Filesize'] = msg;
       hasError['Filesize'] = true;
-      swal({
+      swal.fire({
         title: 'Error',
         text: msg,
         type: 'error',
@@ -175,12 +176,12 @@ class UploadFileForm extends Component {
           errorMessage: msg,
           uploadProgress: -1,
         });
-        swal(msg, '', 'error');
+        swal.fire(msg, '', 'error');
         console.error(msg);
       } else {
         const responseUrl = new URL(response.url);
         if (responseUrl.searchParams.has('duplicate')) {
-          swal({
+          swal.fire({
             title: 'Are you sure?',
             text: 'A file with this name already exists!\n '
                   + 'Would you like to overwrite existing file?\n '
@@ -207,7 +208,7 @@ class UploadFileForm extends Component {
             formData: {}, // reset form data after successful file upload
             uploadProgress: -1,
           });
-          swal({
+          swal.fire({
             text: 'Upload Successful!',
             title: '',
             type: 'success',
@@ -223,7 +224,7 @@ class UploadFileForm extends Component {
         errorMessage: msg,
         uploadProgress: -1,
       });
-      swal(msg, '', 'error');
+      swal.fire(msg, '', 'error');
       console.error(error);
     });
   }

modules/document_repository/jsx/categoryForm.js
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
 import Loader from 'Loader';
+import swal from 'sweetalert2';
+
 /**
  * Document Upload Form
  *
@@ -129,7 +131,7 @@ class DocCategoryForm extends React.Component {
         this.setState({
           formData: {}, // reset form data after successful file upload
         });
-        swal('Add Successful!', '', 'success');
+        swal.fire('Add Successful!', '', 'success');
     });
   }
   /**

commit d3203f72321167ed3aa41ba4f83c8e6f67e152c1

[Publication] File delete not working (#6195 regression) (#7284)
Fix an error introduced by #6195 when a file previously uploaded is deleted.

modules/publication/jsx/projectFields.js
@@ -140,7 +140,7 @@ class ProjectFormFields extends React.Component {
       showCancelButton: true,
       confirmButtonText: 'Yes, I am sure!',
       cancelButtonText: 'No, cancel it!',
-    }, function(willDelete) {
+    }).then(function(willDelete) {
         if (willDelete) {
           let url = loris.BaseURL
                     + '/publication/ajax/FileDelete.php?uploadID='