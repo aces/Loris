f690894b28e4c828e3ecc785dc213d48cc41a44a
Dave MacFarlane <driusan@gmail.com>
2021-01-22 11:05:59 -0500

    Fix final PhanUndeclaredProperty errors from Database class

--- .phan/config.php
+++ .phan/config.php
@@ -15,7 +15,7 @@ return [
     "minimum_severity" => 1,
     // FIXME: allow_missing_properties should be false, but there's
     // too many other things to fix first.
-    "allow_missing_properties" => true,
+    "allow_missing_properties" => false,
     "null_casts_as_any_type" => false,
     "scalar_implicit_cast" => false,
     // The line below is required to prevent PhanUndeclaredVariable problems in
