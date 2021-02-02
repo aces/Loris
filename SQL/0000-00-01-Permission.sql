70c94f04ecca4bd8328571477b1fb2decaf2ca8a
Rida Abou-Haidar <ridz1208@users.noreply.github.com>
2021-02-02 12:29:39 -0500

    Update SQL/0000-00-01-Permission.sql

--- SQL/0000-00-01-Permission.sql
+++ SQL/0000-00-01-Permission.sql
@@ -89,7 +89,7 @@ INSERT INTO `permissions` VALUES
     (46,'issue_tracker_developer','Issue Tracker Module Edit All', 2),
     (47,'imaging_browser_phantom_allsites','Imaging Browser Module Access Phantom Data All Sites', 2),
     (48,'imaging_browser_phantom_ownsite','Imaging Browser Module Access Phantom Data Own Site', 2),
-    (49,'data_release_view','Data Rlease Module View', 2),
+    (49,'data_release_view','Data Release Module View', 2),
     (50,'data_release_upload','Data Release: Upload file', 2),
     (51,'data_release_edit_file_access','Data Release Module: Grant View Permission', 2),
     (52,'instrument_manager_read','Instrument Manager Module View', 2),
