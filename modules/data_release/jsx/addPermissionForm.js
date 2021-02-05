aces/Loris

commit d78c9fbb0329e46a5a326aac538b80985bb14873

sweetalert update (#6195)
This updates sweetalert to sweetalert2 in most javascript files. However, the only file that does not change is NDB_Page.class.inc, since there is some error in Travis.

 Resolves (partially) #6114
 See also #6192

modules/data_release/jsx/addPermissionForm.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Loader from 'jsx/Loader';
+import swal from 'sweetalert2';
 
 /**
  * Add Permission Form
@@ -161,7 +162,7 @@ class AddPermissionForm extends Component {
       body: formObj,
     }).then( (response) => {
       if (response.ok) {
-        swal({
+        swal.fire({
           text: 'Permission Update Success!',
           title: '',
           type: 'success',
@@ -173,7 +174,7 @@ class AddPermissionForm extends Component {
         let msg = response.statusText ?
           response.statusText :
           'Submission Error!';
-        swal(msg, '', 'error');
+        swal.fire(msg, '', 'error');
         console.error(msg);
       }
     });

modules/data_release/jsx/uploadFileForm.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import ProgressBar from 'ProgressBar';
+import swal from 'sweetalert2';
 
 
 /**
@@ -134,7 +135,7 @@ class UploadFileForm extends Component {
                 + ')';
       errorMessage['Filesize'] = msg;
       hasError['Filesize'] = true;
-      swal({
+      swal.fire({
         title: 'Error',
         text: msg,
         type: 'error',
@@ -175,12 +176,12 @@ class UploadFileForm extends Component {
           errorMessage: msg,
           uploadProgress: -1,
         });
-        swal(msg, '', 'error');
+        swal.fire(msg, '', 'error');
         console.error(msg);
       } else {
         const responseUrl = new URL(response.url);
         if (responseUrl.searchParams.has('duplicate')) {
-          swal({
+          swal.fire({
             title: 'Are you sure?',
             text: 'A file with this name already exists!\n '
                   + 'Would you like to overwrite existing file?\n '
@@ -207,7 +208,7 @@ class UploadFileForm extends Component {
             formData: {}, // reset form data after successful file upload
             uploadProgress: -1,
           });
-          swal({
+          swal.fire({
             text: 'Upload Successful!',
             title: '',
             type: 'success',
@@ -223,7 +224,7 @@ class UploadFileForm extends Component {
         errorMessage: msg,
         uploadProgress: -1,
       });
-      swal(msg, '', 'error');
+      swal.fire(msg, '', 'error');
       console.error(error);
     });
   }

modules/help_editor/js/help_editor_helper.js
@@ -1,3 +1,5 @@
+import swal from 'sweetalert2';
+
 $(document).ready(function(){
 $("input[name=preview]").click(function(e) {
     if($('div.help-content').length) {
@@ -65,7 +67,7 @@ $("#save-help").click(function(e) {
             helpID: helpID ? helpID : '',
         },
         success: function() {
-            swal({
+            swal.fire({
                 title: "Content update successful!",
                 type: "success",
                 showCancelButton: true,
@@ -80,7 +82,7 @@ $("#save-help").click(function(e) {
         },
         error: function(xhr, errorCode, errorMsg) {
             console.error(xhr);
-            swal({
+            swal.fire({
                 title: "Content update unsuccessful.",
                 text: errorCode + ": " + xhr.status + " " + errorMsg,
                 type: "error",

modules/instrument_manager/jsx/uploadForm.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import swal from 'sweetalert2';
 
 class InstrumentUploadForm extends Component {
   constructor(props) {
@@ -31,7 +32,7 @@ class InstrumentUploadForm extends Component {
     })
     .then((resp) => {
       if (resp.status == 201) {
-        swal({
+        swal.fire({
           title: 'Installation Successful!',
           type: 'success',
         }, function() {
@@ -42,7 +43,7 @@ class InstrumentUploadForm extends Component {
     })
     .then((data) => {
       if (data.message) {
-         swal({
+         swal.fire({
           title: 'Upload Successful!',
           type: 'success',
           text: data.message,

modules/media/jsx/uploadForm.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import ProgressBar from 'ProgressBar';
 import Loader from 'jsx/Loader';
+import swal from 'sweetalert2';
 
 /**
  * Media Upload Form
@@ -229,7 +230,7 @@ class MediaUploadForm extends Component {
       formData.pscid, formData.visitLabel, instrument
     );
     if (!this.isValidFileName(requiredFileName, fileName)) {
-      swal(
+      swal.fire(
         'Invalid file name!',
         'File name should begin with: ' + requiredFileName,
         'error'
@@ -240,7 +241,7 @@ class MediaUploadForm extends Component {
     // Check for duplicate file names
     let isDuplicate = mediaFiles.indexOf(fileName);
     if (isDuplicate >= 0) {
-      swal({
+      swal.fire({
         title: 'Are you sure?',
         text: 'A file with this name already exists!\n '
               + 'Would you like to override existing file?',
@@ -252,7 +253,7 @@ class MediaUploadForm extends Component {
         if (isConfirm) {
           this.uploadFile();
         } else {
-          swal('Cancelled', 'Your imaginary file is safe :)', 'error');
+          swal.fire('Cancelled', 'Your imaginary file is safe :)', 'error');
         }
       }.bind(this));
     } else {
@@ -304,7 +305,7 @@ class MediaUploadForm extends Component {
           formData: {}, // reset form data after successful file upload
           uploadProgress: -1,
         });
-        swal('Upload Successful!', '', 'success');
+        swal.fire('Upload Successful!', '', 'success');
       }.bind(this),
       error: function(err) {
         console.error(err);
@@ -313,7 +314,7 @@ class MediaUploadForm extends Component {
           errorMessage: msg,
           uploadProgress: -1,
         });
-        swal(msg, '', 'error');
+        swal.fire(msg, '', 'error');
       }.bind(this),
     });
   }

modules/issue_tracker/jsx/attachments/uploadForm.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 import ProgressBar from 'ProgressBar';
+import swal from 'sweetalert2';
 
 /**
  * Issue Upload Attachment Form
@@ -88,14 +89,14 @@ class IssueUploadAttachmentForm extends Component {
             },
             uploadProgress: -1,
           });
-          swal('Upload Successful!', '', 'success');
+          swal.fire('Upload Successful!', '', 'success');
           window.location.href = this.props.baseURL
             + '/issue_tracker/issue/'
             + this.props.issue;
         } else if (data.error) {
-          swal(data.error, '', 'error');
+          swal.fire(data.error, '', 'error');
         } else {
-          swal('Permission denied', '', 'error');
+          swal.fire('Permission denied', '', 'error');
         }
       }).catch((error) => {
       console.error(error);
@@ -106,7 +107,7 @@ class IssueUploadAttachmentForm extends Component {
         errorMessage: msg,
         uploadProgress: -1,
       });
-      swal(msg, '', 'error');
+      swal.fire(msg, '', 'error');
     });
   }
 

modules/publication/jsx/uploadForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ProjectFormFields from './projectFields';
+import swal from 'sweetalert2';
 
 class PublicationUploadForm extends React.Component {
   constructor(props) {
@@ -94,7 +95,7 @@ class PublicationUploadForm extends React.Component {
     e.preventDefault();
 
     if (Object.keys(this.state.formErrors).length > 0) {
-      swal(
+      swal.fire(
         'Please fix any remaining form errors before submission',
         '',
         'error'
@@ -129,7 +130,7 @@ class PublicationUploadForm extends React.Component {
           formData: {},
           numFiles: 0,
         });
-        swal(
+        swal.fire(
           {
             title: 'Submission Successful!',
             type: 'success',
@@ -147,7 +148,7 @@ class PublicationUploadForm extends React.Component {
         } catch (e) {
           console.error(e);
         }
-        swal('Something went wrong!', resp, 'error');
+        swal.fire('Something went wrong!', resp, 'error');
       },
     });
   }

modules/candidate_parameters/jsx/CandidateDOB.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Loader from 'Loader';
+import swal from 'sweetalert2';
 
 class CandidateDOB extends Component {
   constructor(props) {
@@ -111,7 +112,7 @@ class CandidateDOB extends Component {
     let dob = this.state.formData.dob ?
       this.state.formData.dob : null;
     if (dob > today) {
-      swal({
+      swal.fire({
         title: 'Error!',
         text: 'Date of birth cannot be later than today!',
         type: 'error',
@@ -140,7 +141,7 @@ class CandidateDOB extends Component {
     })
     .then((resp) => {
         if (resp.ok && resp.status === 200) {
-          swal({
+          swal.fire({
             title: 'Success!',
             text: 'Date of birth updated!',
             type: 'success',
@@ -150,7 +151,7 @@ class CandidateDOB extends Component {
               this.fetchData();
           }
         } else {
-          swal({
+          swal.fire({
             title: 'Error!',
             text: 'Something went wrong.',
             type: 'error',

commit fa172d66cdb71587d284259ec309032a0620b7c2

Fix swal callback not working (#6195 regressions) (#7324)
This fixes a number of callback regressions introduced by #6195.

modules/data_release/jsx/addPermissionForm.js
@@ -173,7 +173,7 @@ class AddPermissionForm extends Component {
           text: 'Permission Update Success!',
           title: '',
           type: 'success',
-        }, function() {
+        }).then(function() {
           window.location.assign('/data_release');
         });
         this.props.fetchData();

modules/data_release/jsx/uploadFileForm.js
@@ -203,7 +203,7 @@ class UploadFileForm extends Component {
             showCancelButton: true,
             confirmButtonText: 'Yes, I am sure!',
             cancelButtonText: 'No, cancel it!',
-          }, (isConfirm) => {
+          }).then((isConfirm) => {
             if (isConfirm) {
               this.uploadFile(true);
             }
@@ -224,7 +224,7 @@ class UploadFileForm extends Component {
             text: 'Upload Successful!',
             title: '',
             type: 'success',
-          }, function() {
+          }).then(function() {
             window.location.assign('/data_release');
           });
           this.props.fetchData();

modules/help_editor/js/help_editor_helper.js
@@ -73,8 +73,7 @@ $("#save-help").click(function(e) {
                 cancelButtonText: "Close",
                 closeOnConfirm: false,
                 closeOnCancel: true,
-            },
-            function(){
+            }).then(function(){
                 location.href = document.referrer;
             });
         },

modules/instrument_manager/jsx/uploadForm.js
@@ -51,7 +51,7 @@ class InstrumentUploadForm extends Component {
         swal.fire({
           title: 'Installation Successful!',
           type: 'success',
-        }, function() {
+        }).then(function() {
           window.location.assign(loris.BaseURL + '/instrument_manager/');
         });
       }
@@ -63,7 +63,7 @@ class InstrumentUploadForm extends Component {
           title: 'Upload Successful!',
           type: 'success',
           text: data.message,
-        }, function() {
+        }).then(function() {
           window.location.assign(loris.BaseURL + '/instrument_manager/');
         });
       }

modules/media/jsx/uploadForm.js
@@ -261,7 +261,7 @@ class MediaUploadForm extends Component {
         showCancelButton: true,
         confirmButtonText: 'Yes, I am sure!',
         cancelButtonText: 'No, cancel it!',
-      }, function(isConfirm) {
+      }).then(function(isConfirm) {
         if (isConfirm) {
           this.uploadFile();
         } else {