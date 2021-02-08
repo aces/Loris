63ff2235332daa50be2f673a9359c9b6f7f03fc3
Xavier Lecours <xavier.lecours@mcin.ca>
2021-02-08 09:06:53 -0500

    [Media] Distinguish between file and other fields missing. (#7249)
    This adds a specific error message for when the file can't be or is not uploaded due to file size. The current error message was incorrectly claiming fields were missing.
    
    Fixes #7202.

--- modules/media/ajax/FileUpload.php
+++ modules/media/ajax/FileUpload.php
@@ -114,7 +114,15 @@ function uploadFile()
     $language   = isset($_POST['language']) ? $_POST['language'] : null;
 
     // If required fields are not set, show an error
-    if (!isset($_FILES, $pscid, $visit)) {
+    if (empty($_FILES)) {
+        showMediaError(
+            "File could not be uploaded successfully. 
+            Please contact the administrator.",
+            400
+        );
+    }
+
+    if (!isset($pscid, $visit)) {
         showMediaError("Please fill in all required fields!", 400);
         return;
     }
