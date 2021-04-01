72847a16b90938381bc864f653539f5411a83287
Shen <kongtiaowangshen@gmail.com>
2021-04-01 00:01:21 -0400

    Update 0000-00-02-Permission.sql

--- SQL/0000-00-02-Permission.sql
+++ SQL/0000-00-02-Permission.sql
@@ -133,7 +133,6 @@ INSERT INTO `permissions` VALUES
     (62,'electrophysiology_browser_edit_annotations','Annotations',(SELECT ID FROM modules WHERE Name='electrophysiology_browser'),'Create/Edit','2'),
     (63,'monitor_eeg_uploads','Monitor EEG uploads',(SELECT ID FROM modules WHERE Name='electrophysiology_uploader'),NULL,'2'),
     (64,'schedule_module','Schedule Module - edit and delete the appointment',(SELECT ID FROM modules WHERE Name='schedule_module'),'View/Create/Edit','2');
-
 INSERT INTO `user_perm_rel` (userID, permID)
   SELECT u.ID, p.permID
   FROM users u JOIN permissions p
