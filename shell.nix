20a38152204fc555977e57ae4b38257c5abb9a2e
Dave MacFarlane <driusan@mcin.ca>
2021-05-10 12:02:38 -0400

    Add reminder for how to start dev server

--- shell.nix
+++ shell.nix
@@ -5,4 +5,11 @@ let
 in
 pkgs.mkShell {
   buildInputs = with pkgs; [ php git nodejs php80Packages.composer ];
+  shellHook =
+    ''
+       php -v;
+       echo;
+       echo 'To start a development LORIS web server run:';
+       echo '    php -S localhost:8000 -t htdocs/ htdocs/router.php';
+    '';
 }
