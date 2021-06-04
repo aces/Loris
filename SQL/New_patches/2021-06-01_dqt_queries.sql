9871a7409746f28a807c2d09c62d710b8ea2bc18
xlecours <xavier.lecoursboucher@mcgill.ca>
2021-06-04 10:04:29 -0400

    now saving and loading queries from database
    now caching result on filesystem
    
    results endpoint

--- SQL/New_patches/2021-06-01_dqt_queries.sql
+++ SQL/New_patches/2021-06-01_dqt_queries.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `dqt_queries` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `hash` char(40) NOT NULL,
-  `content` text NOT NULL,
+  `content` BLOB NOT NULL,
   `creator` int(10) unsigned NOT NULL,
   `creation_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`creator`, `hash`),
@@ -17,3 +17,14 @@ CREATE TABLE `dqt_shared_queries` (
   KEY `U_dqt_shared_queries_1` (`id`),
   CONSTRAINT `FK_dqt_shared_queries_1` FOREIGN KEY (`shared_id`) REFERENCES `dqt_queries` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `dqt_results` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `query_id` int(10) unsigned NOT NULL,
+  `filepath` text NOT NULL,
+  `creation_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`query_id`),
+  KEY `U_dqt_results_1` (`id`),
+  CONSTRAINT `FK_dqt_results_1` FOREIGN KEY (`query_id`) REFERENCES `dqt_queries` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
