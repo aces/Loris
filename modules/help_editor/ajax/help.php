6bb478c171997b9e37c0c0cb45550d1186823448
Rida Abou-Haidar <ridz1208@users.noreply.github.com>
2021-07-08 13:51:21 -0400

    Update modules/help_editor/ajax/help.php

--- modules/help_editor/ajax/help.php
+++ modules/help_editor/ajax/help.php
@@ -43,7 +43,7 @@ try {
             $data = [
                 'content' => '',
                 'topic'   => '',
-                'updated' => '-'
+                'updated' => ''
             ];
         }
 
