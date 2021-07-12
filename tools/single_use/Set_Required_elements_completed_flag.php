3c3b19debf6962a2281e907b263e76ae23e6c6b9
CamilleBeau <camille.beaudoin@mcin.ca>
2021-07-12 15:54:20 -0400

    rm rb file & changes to scripts

--- tools/single_use/Set_Required_elements_completed_flag.php
+++ tools/single_use/Set_Required_elements_completed_flag.php
@@ -15,9 +15,22 @@
  */
 require_once __DIR__ . '/../generic_includes.php';
 
-$db = \Database::singleton();
+echo "\n*** Set Data_entry_completion_satus in flag table***
 
-$flagData = $db->pselectWithIndexKey(
+This script is to be run for one time use only to migrate and rename the 
+Data_entry_completion_status field. 
+
+If the instrument has a valid table in the database, Data_entry_completion_status 
+will be taken for back-population from the instrument table. Otherwise, it will be 
+taken from the data column of the flag table. An update on the flag table will set 
+the Required_elements_completed column to the Data_entry_completion_status. 
+
+After running this script, run Remove_Data_entry_completion_status_instr_column.php 
+to generate a DROP COLUMN patch to remove the old column from instrument tables.
+\n";
+$DB = \Database::singleton();
+
+$flagData = $DB->pselectWithIndexKey(
     "SELECT Data, CommentID, Test_name 
                 FROM flag",
     [],
@@ -32,32 +45,13 @@ foreach ($flagData as $cmid => $data) {
         $cmid,
         ''
     );
+    $dataArray = $instrument->getInstanceData();
+    $desc = $data['Data_entry_completion_status'];
 
-    $dataArray = json_decode($data['Data'], true);
-
-    $dataToUpdate = [];
-
-    if ($instrument->usesJSONData() === true) {
-        // If json instrument, take value from flag data
-        if (isset($dataArray['Data_entry_completion_status'])) {
-            $decs = $dataArray['Data_entry_completion_status'];
-        }
-    } else {
-        // Otherwise, take value from instrument table
-        $instrData = $instrument->getInstanceData();
-        if (isset($instrData['Data_entry_completion_status'])) {
-            $decs = $instrData['Data_entry_completion_status'];
-        }
-    }
-
-    if (isset($decs)) {
-        // change value from complete / incomplete to Y / N
-        if ($decs === 'Complete') {
-            $dataToUpdate['Required_elements_completed'] = 'Y';
-        } elseif ($decs === 'Incomplete') {
-            $dataToUpdate['Required_elements_completed'] = 'N';
-        }
-    }
+    // change value from complete / incomplete to Y / N
+    if ($decs === 'Complete') {
+        $dataToUpdate['Required_elements_completed'] = 'Y';
+    } 
 
     if (isset($dataArray['Data_entry_completion_status'])) {
         unset($dataArray['Data_entry_completion_status']);
@@ -65,7 +59,7 @@ foreach ($flagData as $cmid => $data) {
     }
 
     if (!empty($dataToUpdate)) {
-        $db->update(
+        $DB->update(
             'flag',
             $dataToUpdate,
             ['CommentID' => $cmid]
