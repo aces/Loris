a1352cf5d7dc8c39507c5dc5887b86b1344d0053
Rida Abou-Haidar <ridz1208@users.noreply.github.com>
2021-10-18 15:03:15 -0400

    Update webpack.config.js
    Co-authored-by: maltheism <16293415+maltheism@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -160,7 +160,7 @@ const config = [
     devtool: 'source-map',
     plugins: [
       new ESLintPlugin({
-        files: ['modules/', 'jsx/', 'htdocs/js/', 'webpack.config.js'],
+        files: ['modules/', 'jsx/', 'jslib/', 'htdocs/js/', 'webpack.config.js'],
         cache: true,
       }),
       new CopyPlugin({
