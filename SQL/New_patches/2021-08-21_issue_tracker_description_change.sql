5bc08da85c7babaa14e3c6785548f27ee9e6ac58
Rida Abou-Haidar <ridz1208@users.noreply.github.com>
2021-10-18 15:09:07 -0400

    [SQL] Raisinbread migration file update (#7654)

--- SQL/New_patches/2021-08-21_issue_tracker_description_change.sql
+++ SQL/New_patches/2021-08-21_issue_tracker_description_change.sql
@@ -1,1 +1,1 @@
-UPDATE ConfigSettings SET Description='URL of the prefered issue tracker for this study' WHERE Name='issue_tracker_url' AND Description='The *new* bug/issue tracker url';
+UPDATE ConfigSettings SET Description='URL of the preferred issue tracker for this study' WHERE Name='issue_tracker_url' AND Description='The *new* bug/issue tracker url';
