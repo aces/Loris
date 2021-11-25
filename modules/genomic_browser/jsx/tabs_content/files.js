99f6e5e3c6a4fb462e2fd6abe5855b3a39a7353d
maltheism <16293415+maltheism@users.noreply.github.com>
2021-11-25 11:41:07 -0500

    update

--- modules/genomic_browser/jsx/tabs_content/files.js
+++ modules/genomic_browser/jsx/tabs_content/files.js
@@ -35,6 +35,7 @@ class Files extends Component {
       },
     };
     this.fetchData = this.fetchData.bind(this);
+    this.formatColumn = this.formatColumn.bind(this);
     this.openFileUploadModal = this.openFileUploadModal.bind(this);
     this.closeFileUploadModal = this.closeFileUploadModal.bind(this);
     this.renderFileUploadForm = this.renderFileUploadForm.bind(this);
@@ -150,10 +151,12 @@ class Files extends Component {
    */
   formatColumn(column, cell, rowData, rowHeaders) {
     let reactElement;
+    console.log(rowData);
     switch (column) {
-      case 'PSCID':
-        const url = `${this.props.baseURL}/${rowData.DCCID}/`;
-        reactElement = <td><a href={url}>{rowData.PSCID}</a></td>;
+      case 'Name':
+        const url = `${this.props.baseURL}${rowData.Name}`;
+        const fileName = rowData.Name.split('/').pop();
+        reactElement = <td><a href={url}>{fileName}</a></td>;
         break;
       case 'Subproject':
         reactElement = <td>{this.state.data.subprojects[parseInt(cell)]}</td>;
@@ -258,12 +261,10 @@ class Files extends Component {
     );
   }
 }
-
 Files.defaultProps = {
   display: false,
   data: null,
 };
-
 Files.propTypes = {
   display: PropTypes.bool,
   data: PropTypes.object,
