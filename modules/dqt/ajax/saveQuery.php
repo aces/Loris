cba61f5b6d588ce00dfb3317d91708291c480c7e
maltheism <16293415+maltheism@users.noreply.github.com>
2022-01-10 15:10:30 -0500

    update

--- modules/dqt/ajax/saveQuery.php
+++ modules/dqt/ajax/saveQuery.php
@@ -68,8 +68,7 @@ if ($_REQUEST['SharedQuery'] === "true") {
                                     ': ' .
                                     $_REQUEST['QueryName'];
 }
-$fields = json_decode(json_encode($_REQUEST['Fields']));
-//$fields = json_decode($_REQUEST['Fields']);
+$fields = json_decode($_REQUEST['Fields']);
 $cond   = $_REQUEST['Filters'];
 $baseDocument['Conditions'] = $cond;
 $baseDocument['Fields']     = $fields;
@@ -81,8 +80,6 @@ if ($_REQUEST['OverwriteQuery'] === "true") {
     $query['id'] = $qid;
     print json_encode($query);
 } else {
-//    $send = $cdb->postDoc($baseDocument);
-//    print json_encode($send);
-    print $cdb->postDoc($baseDocument);
+    print json_encode($cdb->postDoc($baseDocument));
 }
 
