c07d432f4b346207a7318efbf67f90d52bbbe0d5
Dave MacFarlane <driusan@mcin.ca>
2022-02-10 12:29:37 -0500

    Fix loading of pages with PHP 81

--- php/libraries/CenterID.php
+++ php/libraries/CenterID.php
@@ -46,7 +46,7 @@ class CenterID extends ValidatableIdentifier implements \JsonSerializable
      *
      * @return mixed
      */
-    public function jsonSerialize()
+    public function jsonSerialize() : mixed
     {
         return $this->value;
     }
