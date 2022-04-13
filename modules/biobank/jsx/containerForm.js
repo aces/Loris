453c1763603573137590d5dbd38f453eddd8901d
Dave MacFarlane <driusan@mcin.ca>
2022-04-13 11:54:57 -0400

    eslint containerform.js

--- modules/biobank/jsx/containerForm.js
+++ modules/biobank/jsx/containerForm.js
@@ -15,7 +15,13 @@ const initialState = {
   errors: {list: {}},
 };
 
+/**
+ * A form for editing Containers in the Biobank
+ */
 class ContainerForm extends Component {
+  /**
+   * Constructor
+   */
   constructor() {
     super();
     this.state = initialState;
@@ -24,16 +30,32 @@ class ContainerForm extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  /**
+   * Set the current container being displayed
+   *
+   * @param {string} name - the name of the container
+   * @param {string} value - the value
+   */
   setCurrent(name, value) {
     const {current} = clone(this.state);
     current[name] = value;
     this.setState({current});
   }
 
+  /**
+   * Sets a list in the container state
+   *
+   * @param {object} list - the state to set
+   */
   setList(list) {
     this.setState({list});
   }
 
+  /**
+   * Handle submission of a form by calling onSubmit callback
+   *
+   * @return {Promise}
+   */
   handleSubmit() {
     const {list, current, errors} = this.state;
     return new Promise((resolve, reject) => {
@@ -42,6 +64,11 @@ class ContainerForm extends Component {
     });
   }
 
+  /**
+   * Render React component
+   *
+   * @return {JSX}
+   */
   render() {
     const {current, errors, list} = this.state;
     const {options, show} = this.props;
@@ -102,16 +129,30 @@ ContainerForm.propTypes = {
  * Acts a subform for ContainerForm
  **/
 class ContainerSubForm extends Component {
+  /**
+   * Constructor
+   */
   constructor() {
     super();
 
     this.setContainer = this.setContainer.bind(this);
   }
 
+  /**
+   * Set a value in the container
+   *
+   * @param {string} name - key name in list
+   * @param {string} value - value in list
+   */
   setContainer(name, value) {
     this.props.setListItem(name, value, this.props.itemKey);
   }
 
+  /**
+   * Render React component
+   *
+   * @return {JSX}
+   */
   render() {
     const {item, errors, options} = this.props;
 
