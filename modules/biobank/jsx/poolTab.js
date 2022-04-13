55320ec9f17d85262a4a1249cd433a6626a28cd9
Dave MacFarlane <driusan@mcin.ca>
2022-04-13 09:50:01 -0400

    eslint PoolTab.js

--- modules/biobank/jsx/poolTab.js
+++ modules/biobank/jsx/poolTab.js
@@ -6,7 +6,13 @@ import SpecimenForm from './specimenForm';
 
 import {mapFormOptions, clone} from './helpers.js';
 
+/**
+ * React component for the Pool tab of the Biobank module.
+ */
 class PoolTab extends Component {
+  /**
+   * Constructor
+   */
   constructor() {
     super();
 
@@ -18,20 +24,43 @@ class PoolTab extends Component {
     this.formatPoolColumns = this.formatPoolColumns.bind(this);
   }
 
+  /**
+   * Make the form editable
+   *
+   * @param {string} stateKey - ?
+   *
+   * @return {Promise}
+   */
   edit(stateKey) {
     const {editable} = clone(this.state);
     editable[stateKey] = true;
     return new Promise((res) => this.setState({editable}, res()));
   }
 
+  /**
+   * Clear the editable state of this form.
+   */
   clearEditable() {
     this.setState({editable: {}});
   }
 
+  /**
+   * Open the aliquot form for a pool
+   *
+   * @param {int} poolId - the pool id
+   */
   openAliquotForm(poolId) {
     this.setState({poolId}, () => this.edit('aliquotForm'));
   }
 
+  /**
+   * Map IDs in the pool columns to a string value.
+   *
+   * @param {string} column - the column name being mapped
+   * @param {string} value - the column value being mapped
+   *
+   * @return {string}
+   */
   mapPoolColumns(column, value) {
     const {data, options} = this.props;
     switch (column) {
@@ -48,6 +77,15 @@ class PoolTab extends Component {
     }
   }
 
+  /**
+   * Format a row of columns for the pooled specimen.
+   *
+   * @param {string} column - the column name
+   * @param {string} value - the column value
+   * @param {object} row - all the values from the row
+   *
+   * @return {JSX}
+   */
   formatPoolColumns(column, value, row) {
     const {options} = this.props;
     value = this.mapPoolColumns(column, value);
@@ -63,11 +101,17 @@ class PoolTab extends Component {
         return <td>{barcodes}</td>;
       case 'PSCID':
         const pscidURL = loris.BaseURL + '/'
-        + Object.values(options.candidates).find((cand) => cand.pscid == value).id;
+        + Object.values(options.candidates).find(
+             (cand) => cand.pscid == value
+          ).id;
         return <td><a href={pscidURL}>{value}</a></td>;
       case 'Visit Label':
-        const visitLabelURL = loris.BaseURL+'/instrument_list/?candID='+row['PSCID']+
-          '&sessionID='+Object.values(options.sessions).find((sess) => sess.label == value).id;
+        const visitLabelURL = loris.BaseURL +
+          '/instrument_list/?candID='+row['PSCID'] +
+          '&sessionID='+
+             Object.values(options.sessions).find(
+               (sess) => sess.label == value
+             ).id;
         return <td><a href={visitLabelURL}>{value}</a></td>;
       case 'Aliquot':
         const onClick = () => this.openAliquotForm(row['ID']);
@@ -78,18 +122,28 @@ class PoolTab extends Component {
   }
 
 
-  // TODO: This should be fixed. A lot of hacks are being used to initialize
-  // this form and there's definitely better ways to be doing it.
+  /**
+   * Render the aliquot form
+   *
+   * @return {JSX}
+   */
   renderAliquotForm() {
+    // TODO: This should be fixed. A lot of hacks are being used to initialize
+    // this form and there's definitely better ways to be doing it.
     const {data, options} = this.props;
-    if (!(loris.userHasPermission('biobank_specimen_create') && this.state.poolId)) {
+    if (!(loris.userHasPermission('biobank_specimen_create')
+        && this.state.poolId)
+    ) {
       return;
     }
     const specimens = Object.values(data.specimens)
       .filter((specimen) => specimen.poolId == this.state.poolId);
     const parents = specimens
       .map((specimen) => {
-        return {specimen: specimen, container: data.containers[specimen.containerId]};
+        return {
+            specimen: specimen,
+            container: data.containers[specimen.containerId],
+            };
       }
     );
 
@@ -107,6 +161,11 @@ class PoolTab extends Component {
     );
   }
 
+  /**
+   * Render the react component
+   *
+   * @return {JSX}
+   */
   render() {
     const {data, options} = this.props;
     const specimenTypes = mapFormOptions(
@@ -116,7 +175,9 @@ class PoolTab extends Component {
       return [
         pool.id,
         pool.label,
-        Math.round(pool.quantity*100)/100+' '+options.specimen.units[pool.unitId].label,
+        Math.round(pool.quantity*100)/100 +
+           ' ' +
+            options.specimen.units[pool.unitId].label,
         pool.specimenIds,
         options.candidates[pool.candidateId].pscid,
         options.sessions[pool.sessionId].label,
