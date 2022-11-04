69ee1e423bf0eb5be353bad1df0cf01d5b635b05
Dave MacFarlane <driusan@mcin.ca>
2022-11-04 13:45:09 -0400

    Add EndsWith

---
+++ src/Data/Query/Criteria/EndsWith.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace LORIS\Data\Query\Criteria;
+
+/**
+ * An EndsWith criteria represents a search for values that
+ * the string representation of ends with a certain value.
+ */
+class EndsWith implements \LORIS\Data\Query\Criteria {
+    /**
+     * Construct an EndsWith criteria
+     *
+     * @param mixed $suffix The suffix that values must end with
+     */
+    public function __construct(private $suffix) {
+    }
+
+    public function getValue() {
+        return $this->suffix;
+    }
+}
