83164327e52ccc10d9bc5f4cf65410685d45d2d6
Rida <rida.loris@gmail.com>
2022-11-07 10:27:44 -0500

    revert patch

--- SQL/New_patches/2022-12-01-subprojects_no_more.sql
+++ SQL/New_patches/2022-12-01-subprojects_no_more.sql
@@ -1,6 +1,6 @@
 ALTER TABLE subproject RENAME TO cohort;
 ALTER TABLE project_subproject_rel RENAME TO project_cohort_rel;
-ALTER TABLE visit_project_subproject_rel RENAME TO visit_project_cohort_rel; 
+ALTER TABLE visit_project_subproject_rel RENAME TO visit_project_cohort_rel;
 
 ALTER TABLE cohort CHANGE `SubprojectID` `CohortID` int(10) unsigned NOT NULL auto_increment;
 ALTER TABLE project_cohort_rel CHANGE `ProjectSubprojectRelID` `ProjectCohortRelID` int(10) unsigned NOT NULL AUTO_INCREMENT;
@@ -12,3 +12,21 @@ ALTER TABLE mri_protocol_group_target CHANGE `SubprojectID` `CohortID` int(10) u
 ALTER TABLE visit_project_cohort_rel CHANGE `VisitProjectSubprojectRelID` `VisitProjectCohortRelID` int(10) unsigned NOT NULL AUTO_INCREMENT;
 ALTER TABLE visit_project_cohort_rel CHANGE `ProjectSubprojectRelID` `ProjectCohortRelID` int(10) unsigned NOT NULL;
 
+
+/**
+REVERT PATCH
+
+ALTER TABLE cohort RENAME TO subproject;
+ALTER TABLE project_cohort_rel RENAME TO project_subproject_rel;
+ALTER TABLE visit_project_cohort_rel RENAME TO visit_project_subproject_rel;
+
+ALTER TABLE cohort CHANGE `CohortID` `SubprojectID` int(10) unsigned NOT NULL auto_increment;
+ALTER TABLE project_cohort_rel CHANGE `ProjectCohortRelID` `ProjectSubprojectRelID` int(10) unsigned NOT NULL AUTO_INCREMENT;
+ALTER TABLE project_cohort_rel CHANGE `CohortID` `SubprojectID` int(10) unsigned NOT NULL;
+ALTER TABLE session CHANGE `CohortID` `SubprojectID` int(10) unsigned DEFAULT NULL;
+ALTER TABLE test_battery CHANGE `CohortID` `SubprojectID` int(10) unsigned DEFAULT NULL;
+ALTER TABLE mri_protocol_checks_group_target CHANGE `CohortID` `SubprojectID` int(10) unsigned DEFAULT NULL;
+ALTER TABLE mri_protocol_group_target CHANGE `CohortID` `SubprojectID` int(10) unsigned DEFAULT NULL;
+ALTER TABLE visit_project_cohort_rel CHANGE `VisitProjectCohortRelID` `VisitProjectSubprojectRelID` int(10) unsigned NOT NULL AUTO_INCREMENT;
+ALTER TABLE visit_project_cohort_rel CHANGE `ProjectCohortRelID` `ProjectSubprojectRelID` int(10) unsigned NOT NULL;
+ */
