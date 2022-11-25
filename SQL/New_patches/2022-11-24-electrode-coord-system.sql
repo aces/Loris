4627b2ce1419e5e3433305b6c758480e6ceed883
regisoc <regis.ongaro-carcy@mcin.ca>
2022-11-25 11:09:50 -0500

    Base values for coord system name, type, unit

--- SQL/New_patches/2022-11-24-electrode-coord-system.sql
+++ SQL/New_patches/2022-11-24-electrode-coord-system.sql
@@ -3,6 +3,7 @@
 -- SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- ADDED
 -- Table `physiological_coord_system_type`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `physiological_coord_system_type` (
@@ -11,26 +12,75 @@ CREATE TABLE IF NOT EXISTS `physiological_coord_system_type` (
   PRIMARY KEY (`PhysiologicalCoordSystemTypeID`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
+-- insert
+INSERT INTO physiological_coord_system_type
+  (`Name`)
+  VALUES
+  ('MEG'),
+  ('EEG'),
+  ('iEEG'),
+  ('Fiducials'),
+  ('AnatomicalLandmark'),
+  ('HeadCoil'),
+  ('DigitizedHeadPoints');
+
+
 -- -----------------------------------------------------
+-- ADDED
 -- Table `physiological_coord_system_name`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `physiological_coord_system_name` (
   `PhysiologicalCoordSystemNameID`  INT(10)     UNSIGNED  NOT NULL AUTO_INCREMENT,
   `Name`                            VARCHAR(50)           NOT NULL UNIQUE,
+  `Orientation`                     VARCHAR(50)           NULL,
+  `Origin`                          VARCHAR(50)           NULL,
   PRIMARY KEY (`PhysiologicalCoordSystemNameID`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
+-- insert
+INSERT INTO physiological_coord_system_name
+  (`Name`, `Orientation`, `Origin`)
+  VALUES
+  ('ACPC', 'RAS', 'anterior commissure'),
+  ('Allen Institute', 'RAS', 'Bregma point'),
+  ('Analyze', 'LAS', ''),
+  ('BTi/4D', 'ALS', 'between the ears'),
+  ('CTF MRI', 'ALS', 'between the ears'),
+  ('CTF gradiometer', 'ALS', 'between the ears'),
+  ('CapTrak', 'RAS', 'approximately between the ears'),
+  ('Chieti ITAB', 'RAS', 'between the ears'),
+  ('DICOM', 'LPS', 'centre of MRI gradient coil'),
+  ('EEGLAB', 'ALS', 'between the ears'),
+  ('FreeSurfer', 'RAS',	'center of isotropic 1mm 256x256x256 volume'),
+  ('MNI', 'RAS', 'anterior commissure'),
+  ('NIfTI', 'RAS', ''),
+  ('Neuromag/Elekta/Megin', 'RAS', 'between the ears'),
+  ('Paxinos-Franklin', 'RSP', 'Bregma point'),
+  ('Scanner RAS (scanras)', 'RAS', 'scanner origin'),
+  ('Talairach-Tournoux', 'RAS', 'anterior commissure'),
+  ('Yokogawa', 'ALS', 'center of device');
+
 -- -----------------------------------------------------
+-- ADDED
 -- Table `physiological_coord_unit`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `physiological_coord_system_unit` (
   `PhysiologicalCoordSystemUnitID`  INT(10)     UNSIGNED  NOT NULL AUTO_INCREMENT,
   `Name`                            VARCHAR(50)           NOT NULL UNIQUE,
-  `Symbol`                          VARCHAR(10)           NOT NULL UNIQUE,
+  `Symbol`                          VARCHAR(50)           NOT NULL UNIQUE,
   PRIMARY KEY (`PhysiologicalCoordSystemUnitID`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
+INSERT INTO physiological_coord_system_unit
+  (`Name`, `Symbol`)
+  VALUES
+  ('none', ''),
+  ('millimeter', 'mm'),
+  ('centimeter', 'cm'),
+  ('meter', 'm');
+
 -- -----------------------------------------------------
+-- ADDED
 -- Table `physiological_coord_system`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `physiological_coord_system` (
@@ -60,16 +110,9 @@ CREATE TABLE IF NOT EXISTS `physiological_coord_system` (
     REFERENCES `physiological_coord_system_unit` (`PhysiologicalCoordSystemUnitID`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
--- -----------------------------------------------------
--- Table `physiological_electrode`
--- -----------------------------------------------------
-
--- need to move data before
-
-ALTER TABLE `physiological_electrode`
-  DROP COLUMN `PhysiologicalFileID`, `InsertTime`;
 
 -- -----------------------------------------------------
+-- ADDED
 -- Table `physiological_coord_system_electrode_rel`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `physiological_coord_system_electrode_rel` (
@@ -83,7 +126,7 @@ CREATE TABLE IF NOT EXISTS `physiological_coord_system_electrode_rel` (
   ),
   CONSTRAINT `FK_phys_coord_system_electrode_rel_coord_system`
     FOREIGN KEY (`PhysiologicalCoordSystemID`)
-    REFERENCES `physiological_coord_system` (`PhysiologicalCoordSystemID`) ,
+    REFERENCES `physiological_coord_system` (`PhysiologicalCoordSystemID`),
   CONSTRAINT `FK_phys_coord_system_electrode_rel_phys_file`
     FOREIGN KEY (`PhysiologicalFileID`)
     REFERENCES `physiological_file` (`PhysiologicalFileID`),
@@ -91,6 +134,22 @@ CREATE TABLE IF NOT EXISTS `physiological_coord_system_electrode_rel` (
     FOREIGN KEY (`PhysiologicalElectrodeID`)
     REFERENCES `physiological_electrode` (`PhysiologicalElectrodeID`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+
+
+-- -----------------------------------------------------
+-- ALTERED
+-- Table `physiological_electrode`
+-- -----------------------------------------------------
+
+-- need insert before altering `physiological_electrode`
+
+
+-- `InsertTime` and `PhysiologicalFileID` will be moved
+-- to the relation table `physiological_coord_system_electrode_rel`
+-- ALTER TABLE `physiological_electrode`
+--   DROP COLUMN `PhysiologicalFileID`, `InsertTime`;
+
 
 -- SET SQL_MODE=@OLD_SQL_MODE;
 -- SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
