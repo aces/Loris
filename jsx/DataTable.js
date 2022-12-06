5f027488472e480675cd7799e76b0916b86b046c
maltheism <16293415+maltheism@users.noreply.github.com>
2022-12-06 15:58:29 -0500

    [eslint] fix 'Modernizr' is not defined  no-undef (#7299)
    Solves eslint errors for no-undef and useful for removing the off found in  PR #7137
    
    Fixes 'Modernizr' is not defined no-undef

--- jsx/DataTable.js
+++ jsx/DataTable.js
@@ -473,10 +473,6 @@ class DataTable extends Component {
     let index = this.sortRows(filteredRowIndexes);
     let currentPageRow = (rowsPerPage * (this.state.page.number - 1));
 
-    if (this.props.filters.keyword) {
-      useKeyword = true;
-    }
-
     // Format each cell for the data table.
     for (let i = currentPageRow;
          (i < filteredCount) && (rows.length < rowsPerPage);
