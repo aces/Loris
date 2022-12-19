81b4c2690d42846ab30150799d0dfe97b421a918
AlizeeV <intralizee@icloud.com>
2022-12-19 12:35:39 -0500

    fix for jeffersoncasimir

--- modules/datadict/jsx/dataDictIndex.js
+++ modules/datadict/jsx/dataDictIndex.js
@@ -98,6 +98,7 @@ class DataDictIndex extends Component {
           const state = Object.assign({}, this.state);
           for (let i=0; i<state.data.length; i++) {
             if (arrayEquals(state.data[i], findData)) {
+              rowdata['Description Status'] = 'Modified';
               rowdata.Description = e.target.valueOf().innerText;
               state.data[i] = Object.values(rowdata);
               this.setState(state);
