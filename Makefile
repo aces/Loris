aces/Loris

commit 1d3a4d0d971c0fa99323421fa89cf9c97b747294

[npm] reproducible builds (#8224)
Prevent package-lock.json to be automatically regenerated with npm install to have more stable builds.

- enable package-lock.json tracking (recommended practice)
- replace npm install with npm ci to achieve fast and reproducible builds
- strict packages versions from package-lock.json are now used for install. To propagate a change made into package.json, run npm install and commit the new package-lock.json.
- any version disagrement between package.json and package-lock.json will trigger an error.

Makefile
@@ -10,20 +10,24 @@ VERSION: .
 phpdev:
 	composer install
 
-javascript:
+jsdev:
+	npm ci
+	npm run compile
+
+jslatest: clean
+	rm -rf package-lock.json
+	rm -rf modules/electrophysiology_browser/jsx/react-series-data-viewer/package-lock.json
 	npm install
 	npm run compile
 
-dev: VERSION phpdev javascript
+dev: VERSION phpdev jsdev
 
 clean:
 	rm -f smarty/templates_c/*
 	rm -f VERSION
 	rm -rf vendor
 	rm -rf node_modules
-	rm -f package-lock.json
 	rm -rf modules/electrophysiology_browser/jsx/react-series-data-viewer/node_modules
-	rm -f modules/electrophysiology_browser/jsx/react-series-data-viewer/package-lock.json
 
 # Perform static analysis checks
 checkstatic: phpdev

.gitignore
@@ -33,5 +33,4 @@ SQL/Archive/autogenerated/single_use/*.sql
 SQL/Archive/autogenerated/*.sql
 VERSION
 .DS_Store
-package-lock.json
 .eslintcache

commit 840448c707d44c2e8b897fe98625b88abc4aaab0

Make typo fix (#8291)
Fix a regression introduced by #8224

Makefile
@@ -1,6 +1,6 @@
 .PHONY: clean dev all check checkstatic unittests test phpdev javascript testdata
 
-all: VERSION javascript
+all: VERSION jsdev
 	composer install --no-dev
 
 # If anything changes, re-generate the VERSION file