aces/Loris

commit 77e403fbf0be51578fba32dc050cbaf3bc142aaa

Remove SWAL library (#7018)
Remove old swal library from htdocs and consistently use swal2 .

Resolves #6114

modules/help_editor/jsx/help_editor_helper.js
@@ -1,0 +1,83 @@
+/* eslint-disable */
+import swal from 'sweetalert2';
+
+document.addEventListener('DOMContentLoaded', () => {
+    $('input[name=preview]').on('click', function(e) {
+        if ($('div.help-content').length) {
+            $('div.help-content').remove();
+            e.preventDefault();
+        }
+        let title = document.getElementsByName('title')[0].value;
+        let content = document.getElementsByName('content')[0].value;
+        let div = document.createElement('div');
+        let btn = document.createElement('BUTTON');
+        let button = document.createTextNode('Close');
+        let wrap = document.createElement('div');
+
+        wrap.setAttribute('id', 'help-wrapper');
+        wrap.innerHTML = '<h1>' + title + '</h1>';
+        let markdownContent = document.createElement('div');
+        ReactDOM.render(
+            RMarkdown({content: content}),
+            markdownContent
+        );
+        wrap.appendChild(markdownContent);
+        btn.appendChild(button);
+        btn.className='btn btn-default';
+        btn.setAttribute('id', 'helpclose');
+        div.appendChild(btn);
+        div.appendChild(wrap);
+        document.getElementById('wrap').appendChild(div);
+        div.setAttribute('class', 'help-content');
+        btn.addEventListener('click', function(e) {
+            $(div).remove();
+            e.preventDefault();
+        });
+        e.preventDefault();
+    });
+
+    $('#save-help').on('click', function(e) {
+        e.preventDefault();
+        let title = document.getElementsByName('title')[0].value;
+        let content = document.getElementsByName('content')[0].value;
+        let section = document.getElementById('section').value;
+        let subsection = document.getElementById('subsection').value;
+        let helpID = document.getElementById('helpID').value;
+        let returnString = document.getElementById('return').value;
+
+        fetch(loris.BaseURL + '/help_editor/ajax/process.php', {
+            method: 'POST',
+            body: {
+                title: title ? title : '',
+                content: content ? content : '',
+                section: section ? section : '',
+                subsection: subsection ? subsection : '',
+                helpID: helpID ? helpID : '',
+            },
+        }).then((response) => {
+            if (response.status !== 200) {
+                console.error(response.status);
+                return;
+            }
+            swal.fire({
+                title: 'Content update successful!',
+                type: 'success',
+                showCancelButton: true,
+                confirmButtonText: returnString,
+                cancelButtonText: 'Close',
+            }).then((result) => {
+                if (result.value) {
+                    location.href = document.referrer;
+                }
+            });
+        }).catch((error) => {
+            console.error(error);
+            swal.fire({
+                title: 'Content update unsuccessful.',
+                text: 'Something went wrong',
+                type: 'error',
+                confirmButtonText: 'Try again',
+            });
+        });
+    });
+});

modules/help_editor/js/help_editor_helper.js
@@ -1,90 +1,0 @@
-$(document).ready(function(){
-$("input[name=preview]").click(function(e) {
-    if($('div.help-content').length) {
-        $('div.help-content').remove();
-        e.preventDefault();
-    }
-    var title   = $('input[name="title"]').val(),
-        content = $('textarea[name="content"]').val(),
-        myDate  = new Date(),
-        div     = document.createElement("div"),
-        btn     = document.createElement("BUTTON"),
-        button  = document.createTextNode("Close"),
-        wrap    = document.createElement("div");
-
-     // add leading zeros to date format
-     // Date.getMonth() returns the months 0 to 11, so add 1
-     var month  = ('0'+(myDate.getMonth()+1)).slice(-2);
-         date   = ('0'+myDate.getDate()).slice(-2);
-         hour   = ('0'+myDate.getHours()).slice(-2);
-         minute = ('0'+myDate.getMinutes()).slice(-2);
-         second = ('0'+myDate.getSeconds()).slice(-2);
-
-     var dateString = myDate.getFullYear() + "-" + month + "-" + date + " " +
-                      hour + ":" + minute + ":" + second;
-
-     wrap.setAttribute("id", "help-wrapper");
-     wrap.innerHTML  = "<h3>" + title + "</h3>";
-     markdownContent = document.createElement("div");
-     ReactDOM.render(RMarkdown({content: content}), markdownContent);
-     wrap.appendChild(markdownContent);
-     wrap.innerHTML  =  wrap.innerHTML + "<hr>Last updated: " + dateString;
-     btn.appendChild(button);
-     btn.className="btn btn-default";
-     btn.setAttribute("id","helpclose");
-     div.appendChild(btn);
-     div.appendChild(wrap);
-     document.getElementById('wrap').appendChild(div);
-     div.setAttribute("class", "help-content");
-     btn.addEventListener("click", function(e) {
-        $(div).remove();
-        e.preventDefault();
-        }) ;
-     e.preventDefault();
-
-});
-$("#save-help").click(function(e) {
-    e.preventDefault();
-    var title   = $('input[name="title"]').val(),
-        content = $('textarea[name="content"]').val(),
-        section = $("#section").val(),
-        subsection = $("#subsection").val(),
-        helpID = $("#helpID").val(),
-        returnString = $("#return").val();
-
-    $.ajax({
-        type: 'POST',
-        url: loris.BaseURL + '/help_editor/ajax/process.php',
-        data: {
-            title: title ? title : '',
-            content: content ? content : '',
-            section: section ? section : '',
-            subsection: subsection ? subsection : '',
-            helpID: helpID ? helpID : '',
-        },
-        success: function() {
-            swal.fire({
-                title: "Content update successful!",
-                type: "success",
-                showCancelButton: true,
-                confirmButtonText: returnString,
-                cancelButtonText: "Close",
-                closeOnConfirm: false,
-                closeOnCancel: true,
-            }).then(function(){
-                location.href = document.referrer;
-            });
-        },
-        error: function(xhr, errorCode, errorMsg) {
-            console.error(xhr);
-            swal.fire({
-                title: "Content update unsuccessful.",
-                text: errorCode + ": " + xhr.status + " " + errorMsg,
-                type: "error",
-                confirmButtonText: "Try again",
-                closeOnConfirm: true,
-            });
-        }
-    });
-});
-});

htdocs/bootstrap/css/custom-css.css
@@ -845,7 +845,7 @@ a.list-group-item:focus {
 }
 
 /* Necessary formatting for swal2 while still with bootstrap v3 */
-.swal2-popup {
+.swal2-container .swal2-popup {
   font-size: 1.6rem
 }
 

htdocs/js/helpHandler.js
@@ -94,7 +94,6 @@ $(document).ready(function() {
 
   $('.dynamictable').DynamicTable();
   $('.fileUpload').FileUpload();
-  swal.setDefaults({confirmButtonColor: '#064785'});
 });
 $(document).ajaxError(function(event, jqxhr, settings, thrownError) {
   if (jqxhr.status === 401) {

htdocs/js/instrument_controlpanel_swaldeletedata.js
@@ -1,22 +1,0 @@
-/**
- * Instrument selection confirmation message
- *
- * @param {object} event
- */
-function swalFunction(event) {
-  swal({
-    title: 'Please confirm deletion',
-    text: 'The instrument data will be deleted',
-    type: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Yes, delete the data',
-  }, function(result) {
-    if (result) {
-      document.getElementById('ConfirmDelete').submit();
-    }
-  });
-}
-
-document.getElementById('ConfirmDelete').onclick = swalFunction;

modules/help_editor/.gitignore
@@ -1,2 +1,4 @@
 js/columnFormatter.js
 js/help_editor.js
+js/help_editor_helper.js
+

commit 7d8cbefcccb8238488289b507908aa621ecd8065

[help_editor] Fix saving  (#8306)
Add formData to fetch.

Fixes #6940 regression in that broke in #7018

modules/help_editor/ajax/process.php
@@ -23,6 +23,11 @@ if (!$user->hasPermission('context_help')) {
     );
 }
 
+if (empty($_POST)) {
+    header("HTTP/1.1 400 Bad Request");
+    exit;
+}
+
 $DB = (\NDB_Factory::singleton())->database();
 
 if (!empty($_POST['helpID'])

modules/help_editor/jsx/help_editor_helper.js
@@ -38,22 +38,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     $('#save-help').on('click', function(e) {
         e.preventDefault();
-        let title = document.getElementsByName('title')[0].value;
-        let content = document.getElementsByName('content')[0].value;
-        let section = document.getElementById('section').value;
-        let subsection = document.getElementById('subsection').value;
-        let helpID = document.getElementById('helpID').value;
-        let returnString = document.getElementById('return').value;
+        const title = document.getElementsByName('title')[0].value ?? '';
+        const content = document.getElementsByName('content')[0].value ?? '';
+        const section = document.getElementById('section').value ?? '';
+        const subsection = document.getElementById('subsection').value ?? '';
+        const helpID = document.getElementById('helpID').value ?? '';
+        const returnString = document.getElementById('return').value;
+
+        const formData = new FormData();
+        formData.append('title', title);
+        formData.append('content', content);
+        formData.append('section', section);
+        formData.append('subsection', subsection);
+        formData.append('helpID', helpID);
 
         fetch(loris.BaseURL + '/help_editor/ajax/process.php', {
             method: 'POST',
-            body: {
-                title: title ? title : '',
-                content: content ? content : '',
-                section: section ? section : '',
-                subsection: subsection ? subsection : '',
-                helpID: helpID ? helpID : '',
-            },
+            body: formData,
         }).then((response) => {
             if (response.status !== 200) {
                 console.error(response.status);