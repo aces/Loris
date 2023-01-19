a0fcb51f7d147b27c4ae4adacefbcd72a6f0784b
xlecours <xavier.lecours@mcin.ca>
2023-01-19 11:57:10 -0500

    SQL

--- SQL/0000-00-00-schema.sql
+++ SQL/0000-00-00-schema.sql
@@ -978,7 +978,8 @@ CREATE TABLE `violations_resolved` (
   `User` varchar(255) DEFAULT NULL,
   `ChangeDate` datetime DEFAULT NULL,
   `Resolved` enum('unresolved', 'reran', 'emailed', 'inserted', 'rejected', 'inserted_flag', 'other') DEFAULT 'unresolved',
-  PRIMARY KEY (`ID`)
+  PRIMARY KEY (`ID`),
+  KEY `i_violations_resolved_extid_type` (`ExtID`,`TypeTable`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `mri_protocol_violated_scans` (
---
+++ SQL/New_patches/20230119_add_index_on_violations_resolved.sql
@@ -0,0 +1,1 @@
+CREATE INDEX `i_violations_resolved_extid_type` ON `violations_resolved` (`ExtID`, `TypeTable`);
