cba977c64e6231fd3a39ed818f225799a68da944
Georgemurad <george.mu00@gmail.com>
2023-01-24 09:25:17 -0500

    Update closed under the testing file in [candidate_list]

--- modules/candidate_list/test/candidate_listTest.php
+++ modules/candidate_list/test/candidate_listTest.php
@@ -283,7 +283,7 @@ class CandidateListTestIntegrationTest extends LorisIntegrationTestWithCandidate
             self::$feedback,
             self::$display,
             self::$clearFilter,
-            "closed",
+            "Closed",
             '11 rows'
         );
         $this->_filterTest(
