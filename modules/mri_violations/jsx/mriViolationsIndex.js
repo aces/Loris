58626fc3dcac641e7c89550ce52e31a515c5ad2b
Dave MacFarlane <driusan@mcin.ca>
2023-02-06 11:32:32 -0500

    Fix typo in message

--- modules/mri_violations/jsx/mriViolationsIndex.js
+++ modules/mri_violations/jsx/mriViolationsIndex.js
@@ -92,7 +92,7 @@ function ViolationsTable(props) {
   }, [props.name]);
 
   if (isError) {
-    return <h3>An error occured while loading the page.</h3>;
+    return <h3>An error occurred while loading the page.</h3>;
   }
 
   if (!isLoaded) {
