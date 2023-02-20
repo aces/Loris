2ad76f98558b0e7bd32cdb12afa37c45838a15f8
Dave MacFarlane <driusan@mcin.ca>
2023-02-20 15:58:23 -0500

    [mri_violations] Convert module to React and update UI (#7473)
    This converts the MRI Violations module to React. While doing so, is simplifies the subpages. They are no longer menu filters, but modal popup windows that display all details of errors for a scan when the link is clicked. All violations for the SeriesUID are shown to provide context. The maintenance should also be simplified by having converted most types of queries to DataProvisioners, which makes it easier to see the query and copy/paste into SQL. This should also result in more consistency with how CenterID/ProjectID matches are handled with the rest of LORIS.
    
    Fixes #7470
    Fixes #5714 (the pages no longer exist)
    Fixes #6576 (there is only one page with filters now)
    Fixes #6839 (the pages no longer exist, so don't have tabs..)

--- Makefile
+++ Makefile
@@ -55,3 +55,5 @@ testdata:
 login:
 	target=login npm run compile
 
+mri_violations:
+	target=mri_violations npm run compile
---
+++ modules/mri_violations/jsx/mriViolationsIndex.js
@@ -0,0 +1,152 @@
+import {Tabs, TabPane} from 'Tabs';
+import Loader from 'Loader';
+import FilterableDataTable from 'FilterableDataTable';
+import React, {useEffect, useState} from 'react';
+
+import {formatColumnUnresolved, unresolvedFilters}
+    from './unresolvedViolations.js';
+import {formatColumnResolved, resolvedFilters}
+    from './resolvedViolations.js';
+
+import ProtocolModal from './protocolModal.js';
+
+
+/**
+ * Entry point for the MRI Violatons module.
+ *
+ * @param {object} props - React Component properties
+ * @return {JSX}
+ */
+function MRIViolationsIndex(props) {
+  const [fieldOptions, setFieldOptions] = useState({});
+  const [violationModal, setViolationModal] = useState(false);
+  const tabs = [
+    {id: 'notresolved', label: 'Not Resolved'},
+    {id: 'resolved', label: 'Resolved'},
+  ];
+
+  const mapper = columnMapper(fieldOptions);
+
+  const violationsModal = (violationModal !== false) ?
+      <ProtocolModal onClose={() => setViolationModal(false)}
+        URL={props.ModuleURL}
+        SeriesUID={violationModal.SeriesUID}
+        Type={violationModal.ViolationType} /> : null;
+
+  return <div>
+      {violationsModal}
+      <Tabs tabs={tabs} defaultTab="notresolved" updateURL={true}>
+        <TabPane TabId={tabs[0].id}>
+          <ViolationsTable
+            URL={props.unresolvedURL}
+            name="notresolved"
+            mapper={mapper}
+            formatter={formatColumnUnresolved(
+              mapper,
+              setViolationModal,
+              props.ModuleURL + '/resolve'
+            )}
+            fields={unresolvedFilters(fieldOptions)}
+            setFieldOptions={setFieldOptions}
+            noDynamicTable={true}
+          />
+        </TabPane>
+        <TabPane TabId={tabs[1].id}>
+          <ViolationsTable
+            URL={props.resolvedURL}
+            name="resolved"
+            formatter={formatColumnResolved(mapper, setViolationModal)}
+            fields={resolvedFilters(fieldOptions)}
+            mapper={mapper}
+            noDynamicTable={true}
+          />
+        </TabPane>
+    </Tabs>
+  </div>;
+}
+
+/**
+ * Load a data table and render it
+ *
+ * @param {object} props - React Component properties
+ * @return {JSX}
+ */
+function ViolationsTable(props) {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(props.URL)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setData(result.Data);
+          if (props.setFieldOptions) {
+              props.setFieldOptions(result.fieldOptions);
+          }
+        },
+        (error) => {
+           setIsError(true);
+        }
+      );
+  }, [props.name]);
+
+  if (isError) {
+    return <h3>An error occurred while loading the page.</h3>;
+  }
+
+  if (!isLoaded) {
+    return <Loader/>;
+  }
+  return <FilterableDataTable
+          name={props.name}
+          data={data}
+          fields={props.fields}
+          getFormattedCell={props.formatter}
+          getMappedCell={props.mapper}
+          noDynamicTable={props.noDynamicTable}
+        />;
+}
+
+/**
+ * A helper to generate a column mapper callback which maps fieldOptions
+ * to values.
+ *
+ * @param {object} fieldOptions - the dynamic field options
+ * @return {function}
+ */
+function columnMapper(fieldOptions) {
+    return (column, value) => {
+        switch (column) {
+        case 'Project':
+            if (fieldOptions.projects) {
+                return fieldOptions.projects[value];
+            }
+        case 'Cohort':
+            if (fieldOptions.cohorts) {
+                return fieldOptions.cohorts[value];
+            }
+        case 'Site':
+            if (fieldOptions.sites) {
+                return fieldOptions.sites[value];
+            }
+        }
+        return value;
+    };
+}
+
+window.addEventListener('load', () => {
+  ReactDOM.render(
+    <MRIViolationsIndex
+      ModuleURL={`${loris.BaseURL}/mri_violations/`}
+      unresolvedURL={`${loris.BaseURL}/mri_violations/?format=json`}
+      resolvedURL={
+        `${loris.BaseURL}/mri_violations/resolved_violations?format=json`
+      }
+    />,
+    document.getElementById('lorisworkspace')
+  );
+});
+
