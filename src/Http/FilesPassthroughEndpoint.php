19862d8e2bb7026be80a00349d4072f34a6ad843
Dave MacFarlane <driusan@mcin.ca>
2023-03-08 14:33:49 -0500

    Improve docs

--- src/Http/FilesPassthroughEndpoint.php
+++ src/Http/FilesPassthroughEndpoint.php
@@ -5,16 +5,17 @@ use \Psr\Http\Message\ServerRequestInterface;
 use \Psr\Http\Message\ResponseInterface;
 
 /**
- * Creates the form for NDB_Form_media
+ * A FilesPassthroughEndpoint is a helper class for endpoints that
+ * need to handle accessing files from the filesystem thhrough a
+ * REST endpoint such as /modulename/files/abc.txt.
  *
- * @category Loris
- * @package  Media
- * @author   Alex Ilea <ailea.mcin@gmail.com>
- * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
- * @link     https://www.github.com/aces/CCNA/
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
  */
 abstract class FilesPassthroughEndpoint extends Endpoint
 {
+    /**
+     * Construct a FilesPassthroughEndpoint.
+     */
     public function __construct(
         protected \LORIS\LorisInstance $loris,
         protected \Module $Module
@@ -37,7 +38,7 @@ abstract class FilesPassthroughEndpoint extends Endpoint
         $file   = substr($url, $idx + strlen($prefix));
         switch ($request->getMethod()) {
             case 'GET':
-                $this->doNotification($file);
+                $this->doDownloadNotification($file);
                 $handler = new \LORIS\FilesDownloadHandler(
                     $this->getDownloadDirectory($this->loris->getConfiguration())
                 );
@@ -48,18 +49,49 @@ abstract class FilesPassthroughEndpoint extends Endpoint
         }
     }
 
-    //abstract public function _hasAccess(\User $user) : bool;
+    /**
+     * Check if a User has access to the endpoint hierarchy.
+     *
+     * @return bool
+     */
+    abstract public function _hasAccess(\User $user) : bool;
+
+    /**
+     * Return the download directory on the server's filesystem which the
+     * files are stored relative to. This is generally a configuration
+     * variable from LORIS.
+     *
+     * @param \NDB_Config $config The LORIS config object
+     * @return \SplFileInfo
+     */
     abstract protected function getDownloadDirectory(\NDB_Config $config): \SplFileInfo;
 
+    /**
+     * getEndpointPrefix returns the portion of the endpoint (relative to the module)
+     * that must be stripped from the URL to get the filename.
+     *
+     * @return string
+     */
     abstract protected function getEndpointPrefix() : string;
 
+    /**
+     * Define a stub loadResources so that we don't crash when the module
+     * handler calls it.
+     */
     public function loadResources(
         \User $user,
         ServerRequestInterface $request
     ) : void {
     }
 
-    protected function doNotification($file)
+    /**
+     * Send a notification for the download.
+     *
+     * @param string $file The filename being downloaded
+     *
+     * @return void
+     */
+    protected function doDownloadNotification($file)
     {
         $downloadNotifier = new \NDB_Notifier(
             $this->Module->getName(),
