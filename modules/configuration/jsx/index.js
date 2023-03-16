d0bb55e8bfc1bc8586023b951dd7460179992f59
Dave MacFarlane <driusan@mcin.ca>
2023-03-16 11:03:41 -0400

    PropTypes

--- modules/configuration/jsx/index.js
+++ modules/configuration/jsx/index.js
@@ -1,6 +1,7 @@
 import {createRoot} from 'react-dom/client';
 import {useEffect, useState} from 'react';
 import swal from 'sweetalert2';
+import PropTypes from 'prop-types';
 
 function IntroText(props) {
     return <div>
@@ -17,6 +18,9 @@ function IntroText(props) {
         </p>
     </div>;
 }
+IntroText.propTypes = {
+    BaseURL: PropTypes.string.isRequired,
+};
 
 function CategorySelection(props) {
     const catlist = props.categories.map((item) => {
@@ -37,11 +41,16 @@ function CategorySelection(props) {
         </ul>
     </div>;
 }
+CategorySelection.propTypes = {
+    categories: PropTypes.array.isRequired,
+    active: PropTypes.string,
+    setActive: PropTypes.func,
+};
 
 function ConfigurationSection(props) {
-    const [categoryItems, setCategoryItems] = useState(false);
+    const [categoryItems, setCategoryItems] = useState([]);
     useEffect(() => {
-        if (props.activeCategory === false) {
+        if (props.activeCategory === '') {
             // not yet loaded
             return;
         }
@@ -57,7 +66,6 @@ function ConfigurationSection(props) {
         })
         .then((data) => {
             setCategoryItems(data.category);
-            console.log(data);
         }).catch((error) => {
             swal.fire('Configuration Error!', error.toString(), 'error');
         });
@@ -77,6 +85,14 @@ function ConfigurationSection(props) {
     </div>;
 }
 
+ConfigurationSection.propTypes = {
+    BaseURL: PropTypes.string.isRequired,
+
+    activeCategory: PropTypes.string,
+    setActiveCategory: PropTypes.func,
+    categories: PropTypes.array,
+};
+
 function ItemDisplay(props) {
     return <div className="form-group" style={{display: 'flex'}}>
         <div style={{width: '25%'}} title={props.Description}>
@@ -84,9 +100,58 @@ function ItemDisplay(props) {
                 {props.Label}
             </label>
         </div>
-        <div style={{width: '75%'}}>Data stuff goes here.</div>
+        <div style={{width: '75%'}}>
+            <ItemValue DataType={props.DataType}
+                AllowMultiple={props.AllowMultiple}
+                Disabled={props.Disabled}
+                Value={props.Value}
+            />
+        </div>
     </div>;
 }
+ItemDisplay.propTypes = {
+    Name: PropTypes.string,
+    Description: PropTypes.string,
+    Label: PropTypes.string,
+
+    DataType: PropTypes.string,
+    AllowMultiple: PropTypes.bool,
+
+    Disabled: PropTypes.bool,
+    Value: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.array,
+    ]),
+};
+
+function ItemValue(props) {
+    if (props.AllowMultiple) {
+        console.error('Multiple not implemented');
+        return <div>Multiple not implemented</div>;
+    }
+    switch (props.DataType) {
+        case 'text':
+            return <TextElement label="" />;
+        case 'boolean':
+        case 'email':
+        case 'instrument':
+        case 'textarea':
+        case 'scan_type':
+        case 'date_format':
+        case 'lookup_center':
+        case 'path':
+        case 'web_path':
+        case 'log_level':
+        console.error(props.DataType, ' not implemented');
+        return <div>{props.DataType} not implemented</div>;
+        default:
+            throw new Error('Invalid DataType ' + props.DataType);
+    }
+}
+ItemValue.propTypes = {
+    DataType: PropTypes.string.isRequired,
+    AllowMultiple: PropTypes.string.isRequired,
+};
 function CategoryDisplay(props) {
     return <div>{props.items ? props.items.map((item) => {
                 return <ItemDisplay key={item.ID}
@@ -103,9 +168,13 @@ function CategoryDisplay(props) {
             }): ''}
             </div>;
 }
+CategoryDisplay.propTypes = {
+    items: PropTypes.array,
+};
+
 function ConfigurationIndex(props) {
     const [categories, setCategories] = useState([]);
-    const [activeCategory, setActiveCategory] = useState(false);
+    const [activeCategory, setActiveCategory] = useState('');
     useEffect(() => {
         fetch(props.BaseURL + '/configuration/categories')
         .then((resp) => {
@@ -134,11 +203,14 @@ function ConfigurationIndex(props) {
             />
     </div>;
 }
+ConfigurationIndex.propTypes = {
+    BaseURL: PropTypes.string.isRequired,
+};
+
 window.addEventListener('load', () => {
   const root = createRoot(document.getElementById('lorisworkspace'));
   root.render(
     <ConfigurationIndex
-      hasPermission={loris.userHasPermission}
       BaseURL={loris.BaseURL}
     />
   );
