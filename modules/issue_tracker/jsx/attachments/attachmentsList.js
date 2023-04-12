aces/Loris

commit 40f4d594d3f6d21b8d843b432f013755c70b759b

[issue_tracker] Display attachments (#8346)
Display image attachments inline in the imaging browser.

Resolves #8005

modules/issue_tracker/jsx/attachments/attachmentsList.js
@@ -152,6 +152,15 @@ class AttachmentsList extends Component {
   }
 
   /**
+   * Sets event target src to null
+   *
+   * @param {Object} event
+   */
+  displayNone(event) {
+    event.target.src = null;
+  }
+
+  /**
    * Renders the React component.
    *
    * @return {JSX} - React markup for the component
@@ -187,6 +196,7 @@ class AttachmentsList extends Component {
     );
 
     let attachmentsRows = [];
+    let regexImg = /image/;
     for (const key in this.state.attachments) {
       if (this.state.attachments.hasOwnProperty(key)) {
         const item = this.state.attachments[key];
@@ -205,7 +215,26 @@ class AttachmentsList extends Component {
               </div>
               <div className='col-md-8'>
                 <div className='col-md-1'><b>File: </b></div>
-                <div className='col-md-11'><i>{item.file_name}</i></div>
+                <div className='col-md-11'>
+                  <i>{item.file_name}</i>
+                  {regexImg.test(item.mime_type) ?
+                    (<img
+                      src={this.props.baseURL +
+                      '/issue_tracker/Attachment' +
+                      '?ID=' + item.ID +
+                      '&file_hash=' + item.file_hash +
+                      '&issue=' + this.props.issue +
+                      '&filename=' + item.file_name +
+                      '&mime_type=' + item.mime_type
+                      }
+                      width='100%'
+                      height='100%'
+                      onError={this.displayNone}
+                    >
+                    </img>) :
+                    null
+                  }
+                </div>
               </div>
             </div>
             <div className='row'>

commit ae59685f6f8cc3116f273443678484f37915783d

[issue_tracker] Fix warning
40f4d594d3f6d21b8d843b432f013755c70b759b introduced a regression in eslint warnings

where the warning:

```
modules/issue_tracker/jsx/attachments/attachmentsList.js
  157:1  warning  Invalid JSDoc @param "event" type "Object"; prefer: "object"  jsdoc/check-types
```

was introduced.

Fix the warning and upgrade the error type from warning to error so warnings of this
type don't get missed in the future, since there are no other instances.
get missed in the future.

modules/issue_tracker/jsx/attachments/attachmentsList.js
@@ -154,7 +154,7 @@ class AttachmentsList extends Component {
   /**
    * Sets event target src to null
    *
-   * @param {Object} event
+   * @param {object} event
    */
   displayNone(event) {
     event.target.src = null;