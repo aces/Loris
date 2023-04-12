19eebefe34a97e347a44fcdabc76c054ef07a9ff
Laetitia Fesselier <laetitia.fesselier@mail.mcgill.ca>
2023-04-12 14:50:42 -0400

    [RB] Clean deleted configs (#8594)
    #8397 deleted the css and rowsPerPage config settings. This PR clean RB with the deleted config entries.

--- SQL/Cleanup_patches/2023-02-21-unusedconfigs.sql
+++ SQL/Cleanup_patches/2023-02-21-unusedconfigs.sql
@@ -1,2 +1,5 @@
+DELETE FROM Config WHERE ConfigID IN (SELECT ID FROM ConfigSettings WHERE Name='css');
 DELETE FROM ConfigSettings WHERE Name='css';
+
+DELETE FROM Config WHERE ConfigID IN (SELECT ID FROM ConfigSettings WHERE Name='rowsPerPage');
 DELETE FROM ConfigSettings WHERE Name='rowsPerPage';
