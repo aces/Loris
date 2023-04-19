ea0bbc56519d07c906e00c94afe891edce52d7ca
Georgemurad <george.mu00@gmail.com>
2023-04-19 15:33:21 -0400

    [electrophysiology_uploader] inserting data to raisinbread

---
+++ raisinbread/RB_files/RB_electrophysiology_uploader.sql
@@ -0,0 +1,14 @@
+INSERT INTO `electrophysiology_uploader` (`UploadID`,`MetaData`,`UploadDate`,`SessionID`,`UploadLocation`,`Status`,`UploadedBy`) 
+values('1','Montreal','2023-04-19 14:43:22','02','archives/MTL001_300001_V1_bids_1681247806.tar.gz','Not Started','George Murad');
+
+INSERT INTO `electrophysiology_uploader` (`UploadID`,`MetaData`,`UploadDate`,`SessionID`,`UploadLocation`,`Status`,`UploadedBy`) 
+values('2','Data Coordinating Center','2023-04-19 13:53:55','1347','archives/MTL001_300001_V1_bids_1681247865.tar.gz','Not Started','Shen');
+
+INSERT INTO `electrophysiology_uploader` (`UploadID`,`MetaData`,`UploadDate`,`SessionID`,`UploadLocation`,`Status`,`UploadedBy`) 
+values('3','Ottawa','2023-04-19 13:33:19','1189','archives/MTL001_300001_V1_bids_1681247891.tar.gz','Not Started','Aarushi');
+
+INSERT INTO `electrophysiology_uploader` (`UploadID`,`MetaData`,`UploadDate`,`SessionID`,`UploadLocation`,`Status`,`UploadedBy`) 
+values('4','Rome','2023-04-19 14:43:22','2','archives/MTL001_300001_V1_bids_1681248351.tar.gz','Not Started','cecile');
+
+INSERT INTO `electrophysiology_uploader` (`UploadID`,`MetaData`,`UploadDate`,`SessionID`,`UploadLocation`,`Status`,`UploadedBy`) 
+values('5','Montreal','2023-04-19 16:02:25','3','archives/MTL001_300001_V1_bids_1681249251.tar.gz','Not Started','George Murad');
