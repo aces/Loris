e3a71b4ac7ebe8e32df8ba6e9bb0b4fe76735ba6
kongtiaowang <kongtiaowangshen@gmail.com>
2023-04-20 11:13:33 -0400

    test

---
+++ raisinbread/RB_files/RB_electrophysiology_uploader.sql
@@ -0,0 +1,10 @@
+INSERT INTO `electrophysiology_uploader` (`MetaData`,`UploadDate`,`SessionID`,`UploadLocation`,`Status`,`UploadedBy`) 
+values('Data Coordinating Center','2023-04-19 13:53:55','1347','archives/MTL001_300001_V1_bids_1681247865.tar.gz','Not Started','admin');
+NSERT INTO `electrophysiology_uploader` (`UploadID`,`MetaData`,`UploadDate`,`SessionID`,`UploadLocation`,`Status`,`UploadedBy`) 
+values('Data Coordinating Center','2023-04-19 13:53:55','1347','archives/MTL001_300001_V1_bids_1681247865.tar.gz','Not Started','admin');
+NSERT INTO `electrophysiology_uploader` (`UploadID`,`MetaData`,`UploadDate`,`SessionID`,`UploadLocation`,`Status`,`UploadedBy`) 
+values('Data Coordinating Center','2023-04-19 13:53:55','1347','archives/MTL001_300001_V1_bids_1681247865.tar.gz','Not Started','admin');
+NSERT INTO `electrophysiology_uploader` (`UploadID`,`MetaData`,`UploadDate`,`SessionID`,`UploadLocation`,`Status`,`UploadedBy`) 
+values('Data Coordinating Center','2023-04-19 13:53:55','1347','archives/MTL001_300001_V1_bids_1681247865.tar.gz','Not Started','admin');
+NSERT INTO `electrophysiology_uploader` (`UploadID`,`MetaData`,`UploadDate`,`SessionID`,`UploadLocation`,`Status`,`UploadedBy`) 
+values('Data Coordinating Center','2023-04-19 13:53:55','1347','archives/MTL001_300001_V1_bids_1681247865.tar.gz','Not Started','admin');
