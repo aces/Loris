aces/Loris

commit 77e403fbf0be51578fba32dc050cbaf3bc142aaa

Remove SWAL library (#7018)
Remove old swal library from htdocs and consistently use swal2 .

Resolves #6114

modules/instruments/jsx/ControlpanelDeleteInstrumentData.js
@@ -1,0 +1,69 @@
+/* exported RControlpanelDeleteInstrumentData */
+
+import React, {Component} from 'react';
+import swal from 'sweetalert2';
+
+/**
+ * Instrument data deletion confirmation message
+ */
+class ControlpanelDeleteInstrumentData extends Component {
+    /**
+     * @constructor
+     * @param {object} props - React Component properties
+     */
+     constructor(props) {
+         super(props);
+         this.confirmDeletion = this.confirmDeletion.bind(this);
+     }
+
+    /**
+     * Confirm instrument deletion
+     */
+    confirmDeletion() {
+        swal.fire({
+            title: 'Please confirm deletion',
+            text: 'The instrument data will be deleted',
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete the data',
+        }).then((result) => {
+            if (result.value) {
+              this.refs.ClearInstrument.dispatchEvent(
+                new Event('submit')
+              );
+            }
+        });
+    }
+
+    /**
+     * Renders the React component.
+     *
+     * @return {JSX} - React markup for the component
+     */
+    render() {
+        return <form ref="ClearInstrument" method="post">
+            <input
+                className="button"
+                type="button"
+                value="Delete instrument data"
+                onClick={this.confirmDeletion}
+            />
+            <input
+                type="hidden"
+                name="ClearInstrument"
+                value="1"
+            />
+        </form>;
+    }
+}
+
+let RControlpanelDeleteInstrumentData = React.createFactory(
+    ControlpanelDeleteInstrumentData
+);
+
+window.ControlpanelDeleteInstrumentData = ControlpanelDeleteInstrumentData;
+window.RControlpanelDeleteInstrumentData = RControlpanelDeleteInstrumentData;
+
+export default ControlpanelDeleteInstrumentData;

webpack.config.js
@@ -50,9 +50,9 @@ const resolve = {
   },
   extensions: ['*', '.js', '.jsx', '.json', '.ts', '.tsx'],
   fallback: {
-     fs: false,
-     path: false,
-   },
+    fs: false,
+    path: false,
+  },
 };
 
 const mod = {
@@ -138,7 +138,7 @@ let mode = 'production';
 try {
   const configFile = fs.readFileSync('project/config.xml', 'latin1');
   const res = /<[\s]*?sandbox[\s]*?>(.*)<\/[\s]*?sandbox[\s]*?>/
-              .exec(configFile);
+    .exec(configFile);
   if (res && parseInt(res[1]) == 1) mode = 'development';
 } catch (error) {
   console.error(
@@ -342,7 +342,7 @@ const lorisModules = {
   ],
   user_accounts: ['userAccountsIndex'],
   examiner: ['examinerIndex'],
-  help_editor: ['help_editor'],
+  help_editor: ['help_editor', 'help_editor_helper'],
   brainbrowser: ['Brainbrowser'],
   imaging_uploader: ['index'],
   acknowledgements: ['acknowledgementsIndex'],

htdocs/bootstrap/css/custom-css.css
@@ -845,7 +845,7 @@ a.list-group-item:focus {
 }
 
 /* Necessary formatting for swal2 while still with bootstrap v3 */
-.swal2-popup {
+.swal2-container .swal2-popup {
   font-size: 1.6rem
 }
 

htdocs/js/helpHandler.js
@@ -94,7 +94,6 @@ $(document).ready(function() {
 
   $('.dynamictable').DynamicTable();
   $('.fileUpload').FileUpload();
-  swal.setDefaults({confirmButtonColor: '#064785'});
 });
 $(document).ajaxError(function(event, jqxhr, settings, thrownError) {
   if (jqxhr.status === 401) {

htdocs/js/instrument_controlpanel_swaldeletedata.js
@@ -1,22 +1,0 @@
-/**
- * Instrument selection confirmation message
- *
- * @param {object} event
- */
-function swalFunction(event) {
-  swal({
-    title: 'Please confirm deletion',
-    text: 'The instrument data will be deleted',
-    type: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Yes, delete the data',
-  }, function(result) {
-    if (result) {
-      document.getElementById('ConfirmDelete').submit();
-    }
-  });
-}
-
-document.getElementById('ConfirmDelete').onclick = swalFunction;

modules/help_editor/.gitignore
@@ -1,2 +1,4 @@
 js/columnFormatter.js
 js/help_editor.js
+js/help_editor_helper.js
+

commit 06d27dc1415673644270f55c2c4e80e8f67945ff

[Instrument] ControlpanelDeleteInstrumentData error (#8681)
Fix clear instrument button regression introduced by #7018 and reported in PR #8673 

Fixes #7065, Fixes #8580

modules/instruments/jsx/ControlpanelDeleteInstrumentData.js
@@ -30,9 +30,25 @@ class ControlpanelDeleteInstrumentData extends Component {
             confirmButtonText: 'Yes, delete the data',
         }).then((result) => {
             if (result.value) {
-              this.refs.ClearInstrument.dispatchEvent(
-                new Event('submit')
-              );
+                const formData = new FormData();
+                formData.append('ClearInstrument', 1);
+                fetch(
+                    window.location.href,
+                    {
+                      method: 'POST',
+                      body: formData,
+                    }
+                ).then((response) => {
+                    if (!response.ok) {
+                      console.error(
+                        response.status + ': ' + response.statusText
+                      );
+                      return;
+                    }
+                    window.location.reload();
+                }).catch((error) => {
+                    console.error(error);
+                });
             }
         });
     }
@@ -43,19 +59,14 @@ class ControlpanelDeleteInstrumentData extends Component {
      * @return {JSX} - React markup for the component
      */
     render() {
-        return <form ref="ClearInstrument" method="post">
+        return (
             <input
                 className="button"
                 type="button"
                 value="Delete instrument data"
                 onClick={this.confirmDeletion}
             />
-            <input
-                type="hidden"
-                name="ClearInstrument"
-                value="1"
-            />
-        </form>;
+        );
     }
 }
 

php/libraries/NDB_BVL_InstrumentStatus_ControlPanel.class.inc
@@ -183,7 +183,7 @@ class NDB_BVL_InstrumentStatus_ControlPanel extends NDB_BVL_InstrumentStatus
                     $this->subtest ?? ''
                 );
                 $instrument->clearInstrument();
-                return false;
+                return true;
             } else {
                 return false;
             }

webpack.config.js
@@ -320,7 +320,7 @@ const lorisModules = {
   imaging_qc: ['imagingQCIndex'],
   server_processes_manager: ['server_processes_managerIndex'],
   statistics: ['WidgetIndex'],
-  instruments: ['CandidateInstrumentList'],
+  instruments: ['CandidateInstrumentList', 'ControlpanelDeleteInstrumentData'],
   candidate_profile: ['CandidateInfo'],
   api_docs: ['swagger-ui_custom'],
 };