9196eb383a97f47d91ccdc6b785ceebd23cdab25
kongtiaowang <kongtiaowangshen@gmail.com>
2023-06-27 12:45:33 -0400

    cs

--- modules/document_repository/jsx/uploadForm.js
+++ modules/document_repository/jsx/uploadForm.js
@@ -224,12 +224,12 @@ class DocUploadForm extends Component {
           } else {
             resp.json().then((data) => {
               console.error(resp);
-	           if (resp.status == 413) {
-                    swal.fire('Could not upload files, file too large', data.error, 'error');
-		   }
-                   if (resp.status == 403) {
-                    swal.fire('Could not upload files, permission denied ', data.error, 'error');
-                   }		    
+              if (resp.status == 413) {
+                swal.fire('File too large', data.error, 'error');
+              }
+              if (resp.status == 403) {
+                swal.fire('Permission denied', data.error, 'error');
+	      }		    
             }).catch((error) => {
               console.error(error);
               swal.fire(
