f10e47a910a6eb2300fa5e3acbef74cd8fb9da4f
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2023-08-22 14:17:29 -0400

    Update migration script

--- tools/update_issues_with_description.php
+++ tools/update_issues_with_description.php
@@ -1,9 +1,9 @@
 #!/usr/bin/php
 <?php
 /**
- * The script update_issues_with_description.php creates a new field
- * called "description" in the issues table and copies the first comment
- * by timestamp (dateAdded) in issues_comments table matching based on the issueID
+ * The script update_issues_with_description.php copies the first comment
+ * by timestamp (dateAdded) in issues_comments table to the description
+ * field of the issues table matching based on the issueID
  *
  * Usage: php update_issues_with_description.php
  *
@@ -18,86 +18,41 @@
 
 require_once __DIR__ . "/generic_includes.php";
 
-$tablename = "issues";
-$filename  = __DIR__ . "/../project/tables_sql/".$tablename.".sql";
-$output    = "ALTER TABLE $tablename ADD description longtext DEFAULT NULL;\n";
-$output   .= "SET FOREIGN_KEY_CHECKS=0;\n";
-$output   .= "UPDATE $tablename
-    SET description = (
-        SELECT issueComment
-        FROM issues_comments ic
-        WHERE ic.issueID = issues.issueID
-        ORDER BY ic.dateAdded ASC
-        LIMIT 1
-    )
-    WHERE issueID IN (
-        SELECT issueID
-        FROM issues_comments
-    );\n";
-$output   .= "ALTER TABLE issues_history MODIFY fieldChanged
-enum('assignee','status','comment','sessionID','centerID','title','category',
-'module','lastUpdatedBy','priority','candID', 'description') NOT NULL DEFAULT
-'comment';\n";
-$dirname   = dirname($filename);
-if (!is_dir($dirname)) {
-    mkdir($dirname, 0755, true);
-}
-$fp = fopen($filename, "w");
-fwrite($fp, $output);
-fclose($fp);
+set_include_path(
+    get_include_path().":".
+    __DIR__."/../project/libraries:".
+    __DIR__."/../php/libraries:"
+);
 
-$issueQueries = glob(__DIR__ . "/../project/tables_sql/issues.sql");
-array_walk($issueQueries, 'runPatch');
-echo "\n $tablename table updation complete\n";
+require_once __DIR__ . "/../vendor/autoload.php";
+require_once "NDB_Client.class.inc";
+$client = new NDB_Client();
+$client->makeCommandLine();
+$client->initialize();
 
-/**
- * Wrapper for runCommand() that pipes the content of an SQL file to an CLI
- * instance of MySQL.
- *
- * @param string $file The name of the file to run.
- *
- * @return void
- */
-function runPatch(string $file): void
-{
-    global $mysqlCommand;
-    $config       = \NDB_Factory::singleton()->config();
-    $dbInfo       = $config->getSettingFromXML('database');
-    $dbname       = $dbInfo['database'];
-    $host         = $dbInfo['host'];
-    $username     = $dbInfo['adminUser'];
-    $password     = $dbInfo['adminPassword'];
-    $mysqlCommand = sprintf(
-        "mysql -A %s -u %s -h %s -p%s",
-        escapeshellarg($dbname),
-        escapeshellarg($username),
-        escapeshellarg($host),
-        escapeshellarg($password)
+$DB = \NDB_Factory::singleton()->database();
+
+$allIssueIDsWithComments = $DB->pselectCol(
+    "SELECT issueID FROM issues_comments",
+    []
+);
+
+foreach($allIssueIDsWithComments as $issueID) {
+    $existingDescription = $DB->pselectOne(
+        "SELECT `description` FROM issues WHERE issueID=:issue_id",
+        ["issue_id" => $issueID]
     );
-    runCommand(
-        "cat $file | $mysqlCommand"
-    );
-}
-
-/**
- * A wrapper around `exec()` built-in function with basic error reporting.
- *
- * @param string $command Bash command to be executed by `exec()`
- *
- * @return void. Causes script to exit on non-successful status code.
- */
-function runCommand(string $command): void
-{
-    global $password;
-    // Hide password from output
-    $output = str_replace($password, str_repeat('*', 10), $command);
-    echo "Running update queries...";
-    exec($command, $output, $status);
-    // If a non-zero exit code is given, then an error has occurred.
-    // In this case, print the output.
-    if ($status) {
-        foreach ($output as $line) {
-            echo $line . PHP_EOL;
-        }
+    if (!$existingDescription) {
+        $description = $DB->pselectCol(
+            "SELECT issueComment
+            FROM issues_comments ic
+            WHERE ic.issueID = :issue_id
+            ORDER BY ic.dateAdded ASC
+            LIMIT 1",
+            ['issue_id' => $issueID]
+        );
+        $DB->update("issues", ["description" => $description[0]], ["issueID" => $issueID]);
     }
 }
+
+echo "\n Description field migration completed\n";
