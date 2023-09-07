1102e875f885fd91c71c6e091e926193b9af6496
zaliqarosli <zaliqa.rosli@mcin.ca>
2023-09-07 15:24:26 -0400

    fix

--- modules/imaging_uploader/jsx/UploadForm.js
+++ modules/imaging_uploader/jsx/UploadForm.js
@@ -70,9 +70,9 @@ class UploadForm extends Component {
         // join the remaining elements of patientName and pattern match
         // against each visit label. Use as visitLabel the best (longest) match
         ids.splice(0, 2);
-        suffix = ids.join('_');
-        visitLabels = form.visitLabel.options.keys;
-        bestMatch = '';
+        const suffix = ids.join('_');
+        const visitLabels = Object.keys(form.visitLabel.options);
+        let bestMatch = '';
         visitLabels.map((visitLabel) => {
           if (suffix.match(visitLabel) !== null) {
             // consider the first match only
@@ -94,9 +94,21 @@ class UploadForm extends Component {
         formData.candID = ids[1];
         formData.pSCID = ids[0];
         // visitLabel can contain underscores
-        // join the remaining elements of patientName and use as visitLabel
+        // join the remaining elements of patientName and pattern match
+        // against each visit label. Use as visitLabel the best (longest) match
         ids.splice(0, 2);
-        formData.visitLabel = ids.join('_');
+        const suffix = ids.join('_');
+        const visitLabels = Object.keys(form.visitLabel.options);
+        let bestMatch = '';
+        visitLabels.map((visitLabel) => {
+          if (suffix.match(visitLabel) !== null) {
+            // consider the first match only
+            if (suffix.match(visitLabel)[0].length > bestMatch.length) {
+              bestMatch = suffix.match(visitLabel)[0];
+            }
+          }
+        });
+        formData.visitLabel = bestMatch;
       }
     }
 
