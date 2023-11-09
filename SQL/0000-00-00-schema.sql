53e710878910c19e23e5fa135f4b327ecbabd101
zaliqarosli <zaliqa.rosli@mcin.ca>
2023-11-09 15:37:27 -0500

    fix rebase

--- SQL/0000-00-00-schema.sql
+++ SQL/0000-00-00-schema.sql
@@ -154,7 +154,9 @@ CREATE TABLE `diagnosis_evolution` (
   CONSTRAINT `PK_diagnosis_evolution` PRIMARY KEY (`DxEvolutionID`),
   CONSTRAINT `UK_diagnosis_evolution_Name` UNIQUE KEY `Name` (`Name`),
   CONSTRAINT `FK_diagnosis_evolution_ProjectID` FOREIGN KEY (`ProjectID`) REFERENCES `Project` (`ProjectID`) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;CREATE TABLE `candidate` (
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `candidate` (
   `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `CandID` int(6) NOT NULL DEFAULT '0',
   `PSCID` varchar(255) NOT NULL DEFAULT '',
@@ -189,17 +191,6 @@ CREATE TABLE `diagnosis_evolution` (
   CONSTRAINT `FK_candidate_1` FOREIGN KEY (`RegistrationCenterID`) REFERENCES `psc` (`CenterID`),
   CONSTRAINT `FK_candidate_2` FOREIGN KEY (`flagged_reason`) REFERENCES `caveat_options` (`ID`) ON DELETE RESTRICT ON UPDATE CASCADE,
   CONSTRAINT `FK_candidate_RegistrationProjectID` FOREIGN KEY (`RegistrationProjectID`) REFERENCES `Project` (`ProjectID`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `candidate_diagnosis_evolution_rel` (
-  `CandID` int(6) NOT NULL,
-  `DxEvolutionID` int(10) unsigned NOT NULL,
-  `Diagnosis` text DEFAULT NULL,
-  `Confirmed` enum('Y', 'N') DEFAULT NULL,
-  `LastUpdate` datetime NOT NULL DEFAULT NOW() ON UPDATE NOW(),
-  CONSTRAINT `PK_candidate_diagnosis_evolution_rel` PRIMARY KEY (`CandID`, `DxEvolutionID`),
-  CONSTRAINT `FK_candidate_diagnosis_evolution_rel_CandID` FOREIGN KEY (`CandID`) REFERENCES `candidate` (`CandID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `FK_candidate_diagnosis_evolution_rel_DxEvolutionID` FOREIGN KEY (`DxEvolutionID`) REFERENCES `diagnosis_evolution` (`DxEvolutionID`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `candidate_diagnosis_evolution_rel` (
