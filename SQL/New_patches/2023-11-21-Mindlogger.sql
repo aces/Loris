8bd28d04a498b728934e22b5c37e575b7bd07fee
Dzmitry Yahur <yagurdmitriy@gmail.com>
2023-11-30 15:59:51 +0400

    Removed local dev code.

--- SQL/New_patches/2023-11-21-Mindlogger.sql
+++ SQL/New_patches/2023-11-21-Mindlogger.sql
@@ -23,14 +23,3 @@ CREATE TABLE `instrument_mindlogger_schema`
 INSERT INTO test_subgroups (Subgroup_name)
 VALUES ('Mindlogger instruments');
 
-
--- Add mindlogger_schema_create perm to the first user
--- Todo: For local development only
-INSERT INTO `user_perm_rel` (userID, permID)
-VALUES (1, (SELECT ID FROM permissions WHERE code = 'mindlogger_schema_create'));
-
--- Update JWTKey setting value to more complex to fix "Unacceptable JWT key" error
--- Todo: For local development only
-UPDATE `Config`
-SET `Value` = 'T0AZOxGmoPE5S22jbzOicI9E2g768kTV5AyFiwEa2F2fSyYyQpsLYuFTWL8HB1uN#$%^'
-WHERE ConfigID = (SELECT ID FROM ConfigSettings WHERE Name = 'JWTKey');
