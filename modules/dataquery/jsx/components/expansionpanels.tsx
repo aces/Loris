8d12954a2dd8a7b69854375ba5c2d839baf54372
Dave MacFarlane <driusan@mcin.ca>
2023-12-01 10:53:21 -0500

    Use jsx/Panel instead of creating new Panel type for ExpansionPanels

--- modules/dataquery/jsx/components/expansionpanels.tsx
+++ modules/dataquery/jsx/components/expansionpanels.tsx
@@ -1,90 +1,5 @@
 import React, {useState} from 'react';
-
-/**
- * Component to render a single panel
- *
- * @param {object} props - The React Props
- * @param {boolean} props.defaultOpen - Whether the default should default open
- * @param {boolean} props.alwaysOpen - Whether the panel can not be collapsed
- * @param {string} props.title - The panel title
- * @param {React.ReactElement} props.content - The panel body
- * @returns {React.ReactElement} - The rendered panel
- */
-const Panel = (props: {
-    defaultOpen: boolean,
-    alwaysOpen: boolean,
-    title: string,
-    content: React.ReactElement,
-}) => {
-  const [active, setActive] = useState(props.defaultOpen);
-
-  const styles = {
-    accordion: {
-      default: {
-        width: '100%',
-        padding: '18px',
-        outline: 'none',
-        color: '#246EB6',
-        fontSize: '15px',
-        cursor: props.alwaysOpen ? 'default' : 'pointer',
-        textAlign: 'center' as const,
-        backgroundColor: '#fff',
-        border: '1px solid #246EB6',
-        transition: '0.4s',
-      },
-      active: {
-        color: '#fff',
-        textAlign: 'left' as const,
-        backgroundColor: '#246EB6',
-      },
-    },
-    panel: {
-      default: {
-        display: 'none',
-        padding: '20px 18px',
-        backgroundColor: '#fff',
-        border: '1px solid #246EB6',
-        overflow: 'hidden',
-      },
-      active: {
-        display: 'block',
-        margin: '0 0 10px 0',
-      },
-    },
-  };
-
-  /**
-   * Handle clicking on the header
-   *
-   * @returns {void}
-   */
-  const handleExpansionClick = () => {
-    if (props.alwaysOpen) return;
-    setActive((active) => !active);
-  };
-
-  const styleAccordion: React.CSSProperties = {
-    ...styles.accordion.default,
-    ...(active ? styles.accordion.active : {}),
-  };
-
-  const stylePanel: React.CSSProperties = {
-    ...styles.panel.default,
-    ...(active ? styles.panel.active : {}),
-  };
-
-  return (
-    <div>
-      <button onClick={() => handleExpansionClick()}
-              style={styleAccordion}>
-        {props.title}
-      </button>
-      <div style={stylePanel}>
-        {props.content}
-      </div>
-    </div>
-  );
-};
+import Panel from 'jsx/Panel';
 
 /**
  * Render a series of expansion panels
@@ -110,10 +25,10 @@ const ExpansionPanels = (props: {
         <Panel
           key={index}
           title={panel.title}
-          content={panel.content}
-          alwaysOpen={panel.alwaysOpen}
-          defaultOpen={panel.defaultOpen || props.alwaysOpen || true}
-        />
+          collapsed={panel.alwaysOpen}
+          initCollapsed={panel.defaultOpen || props.alwaysOpen || true}>
+          {panel.content}
+        </Panel>
       ))}
     </div>
   );
