5ff16c5409e9fa1d13653d39e4422b3008a1199c
Dave MacFarlane <driusan@mcin.ca>
2023-12-11 11:26:35 -0500

    [dataquery] New Data Query Module (#8907)
    This implements a completely new, CouchDB free data query module based on the LORIS data query framework and the API introduced in PR#8268.
    
    In the new module, all queries that are run by the user are saved into a history. The user can star or name a query in order to make it easier to find, rather than having to decide ahead of time that they want to save or share a query. Admins can pin queries (either to the top of the module or to the LORIS dashboard).
    
    The UI is also (generally) simplified and more context-sensitive. The "define filters" tab should be easier to understand. The view data tab has a new view where candidates are rows, fields are columns, and session-scoped variables are displayed inline in the cell in a table.
    
    Permissions are enforced for modules, candidates, and sessions.

---
+++ jsx/DataTable.d.ts
@@ -0,0 +1,70 @@
+import {ReactNode} from 'react';
+
+type TableRow = (string|null)[]
+
+type Field = {
+    show: boolean
+    label: string
+}
+
+type hideOptions = {
+    rowsPerPage: boolean
+    downloadCSV: boolean
+    defaultColumn: boolean
+}
+type DataTableProps = {
+    data: TableRow[]
+    rowNumLabel?: string
+    getFormattedCell: (label: string,
+        data: string,
+        row: TableRow,
+        headers: string[],
+        fieldNo: number) => ReactNode
+    onSort?: () => void
+    hide?: hideOptions
+    fields: Field[]
+    nullTableShow?: boolean
+    noDynamicTable?: boolean
+    getMappedCell?: (
+        label: string,
+        data: string|null,
+        row: TableRow,
+        headers: string[],
+        fieldNo: number) => string|(string|null)[]|null
+}
+
+/**
+ * The DataTable class. See DataTable.js
+ */
+class DataTable {
+    props: DataTableProps
+    state: any
+    context: object
+    refs: {[key: string]: ReactInstance}
+
+    /**
+     * Construct a new modal
+     */
+    constructor(props: DataTableProps)
+
+    /**
+     * React lifecycle method
+     *
+     * @returns {ReactNode}
+     */
+    render(): ReactNode
+
+    /**
+     * React lifecycle method
+     *
+     * @param {object} newstate - the state to overwrite
+     */
+    setState(newstate: object): void
+
+    /**
+     * React lifecycle method
+     */
+    forceUpdate(): void
+}
+
+export default DataTable;
