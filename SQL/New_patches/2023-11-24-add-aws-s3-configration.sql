58b371d25301c8582a643fc0526735c021c04429
Shen <kongtiaowangshen@gmail.com>
2024-01-26 09:49:51 -0500

    Update 2023-11-24-add-aws-s3-configration.sql

--- SQL/New_patches/2023-11-24-add-aws-s3-configration.sql
+++ SQL/New_patches/2023-11-24-add-aws-s3-configration.sql
@@ -1,7 +1,7 @@
 INSERT INTO `ConfigSettings` (`Name`, `Description`, `Visible`, `AllowMultiple`, `DataType`, `Parent`, `Label`, `OrderNumber`) VALUES 
 ('AWS_S3_Default_Bucket_Media','Default bucket for LORIS to use for accessing media files in S3.',1,0,'text',121,'AWS S3 Default Bucket for Media',3);
 
-INSERT INTO `ConfigSettings` ( `Name`, `Description`, `Visible`, `AllowMultiple`, `DataType`, `Parent`, `Label`, `OrderNumber`) VALUES ('AWS_S3_Default_Bucket_Document_Repository','Default bucket for LORIS to use for accessing Document_Repository files in S3.',1,0,'text',121,'AWS S3 Default Bucket for Media',3);
+INSERT INTO `ConfigSettings` ( `Name`, `Description`, `Visible`, `AllowMultiple`, `DataType`, `Parent`, `Label`, `OrderNumber`) VALUES ('AWS_S3_Default_Bucket_Document_Repository','Default bucket for LORIS to use for accessing Document_Repository files in S3.',1,0,'text',121,'AWS S3 Default Bucket for Document Repository',3);
 
 
 INSERT INTO `ConfigSettings` ( `Name`, `Description`, `Visible`, `AllowMultiple`, `DataType`, `Parent`, `Label`, `OrderNumber`) VALUES ('AWS_S3_Default_Bucket_Issue_Tracker','Default bucket for LORIS to use for accessing Issue Tracker files in S3.',1,0,'text',121,'AWS S3 Default Bucket for Issue Tracker',3);
