20dd92714eaf4e45cfd8e7adf3dee665840360e5
Shen <kongtiaowangshen@gmail.com>
2024-01-26 10:45:48 -0500

    Update 2023-11-24-add-aws-s3-configration.sql

--- SQL/New_patches/2023-11-24-add-aws-s3-configration.sql
+++ SQL/New_patches/2023-11-24-add-aws-s3-configration.sql
@@ -1,11 +1,6 @@
-INSERT INTO `ConfigSettings` (`Name`, `Description`, `Visible`, `AllowMultiple`, `DataType`, `Parent`, `Label`, `OrderNumber`) VALUES 
-('AWS_S3_Default_Bucket_Media','Default bucket for LORIS to use for accessing media files in S3.',1,0,'text',121,'AWS S3 Default Bucket for Media',3);
-
-INSERT INTO `ConfigSettings` ( `Name`, `Description`, `Visible`, `AllowMultiple`, `DataType`, `Parent`, `Label`, `OrderNumber`) VALUES ('AWS_S3_Default_Bucket_Document_Repository','Default bucket for LORIS to use for accessing Document_Repository files in S3.',1,0,'text',121,'AWS S3 Default Bucket for Document Repository',3);
-
-
-INSERT INTO `ConfigSettings` ( `Name`, `Description`, `Visible`, `AllowMultiple`, `DataType`, `Parent`, `Label`, `OrderNumber`) VALUES ('AWS_S3_Default_Bucket_Issue_Tracker','Default bucket for LORIS to use for accessing Issue Tracker files in S3.',1,0,'text',121,'AWS S3 Default Bucket for Issue Tracker',3);
-
+INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber) SELECT 'AWS_S3_Default_Bucket_Media', 'Default bucket for LORIS to use for accessing media files in S3.', 1, 0, 'text', ID, 'AWS S3 Default Bucket for Media', 3 FROM ConfigSettings WHERE Name='aws';
+INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber) SELECT 'AWS_S3_Default_Bucket_Document_Repository', 'Default bucket for LORIS to use for accessing Document_Repository files in S3.', 1, 0, 'text', ID, 'AWS S3 Default Bucket for Document Repository', 3 FROM ConfigSettings WHERE Name='aws';
+INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber) SELECT 'AWS_S3_Default_Bucket_Issue_Tracker', 'Default bucket for LORIS to use for accessing Issue Tracker files in S3.', 1, 0, 'text', ID, 'AWS S3 Default Bucket for Issue Tracker', 3 FROM ConfigSettings WHERE Name='aws';
 
 -- Insert record for ConfigID 141
 INSERT INTO `Config` (`ConfigID`, `Value`)
