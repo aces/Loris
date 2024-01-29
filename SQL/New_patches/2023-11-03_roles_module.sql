53fac251d6d33125269604107be3c17bb0b46ec4
regisoc <regis.ongaro-carcy@mcin.ca>
2024-01-29 18:46:12 -0500

    sql patch update

--- SQL/New_patches/2023-11-03_roles_module.sql
+++ SQL/New_patches/2023-11-03_roles_module.sql
@@ -42,11 +42,11 @@ INSERT INTO `roles` VALUES
 INSERT INTO modules (Name, Active) VALUES ('roles', 'Y');
 
 -- add new permissions (related to roles module)
-INSERT INTO `permissions` (`code`, `description`, `moduleID`) 
+INSERT INTO `permissions` (`code`, `description`, `action`, `moduleID`)
 VALUES
-  ('roles_view','Roles',(SELECT ID FROM modules WHERE Name='roles')),
-  ('roles_assign','Roles',(SELECT ID FROM modules WHERE Name='roles')),
-  ('roles_edit','Roles',(SELECT ID FROM modules WHERE Name='roles'));
+  ('roles_view','Roles Entries','View',(SELECT ID FROM modules WHERE Name='roles_manager')),
+  ('roles_edit','Roles Entries','Create/Edit',(SELECT ID FROM modules WHERE Name='roles_manager')),
+  ('roles_assign','Roles Entries','Edit',(SELECT ID FROM modules WHERE Name='roles_manager'));
 
 -- add role-permission rel table
 DROP TABLE IF EXISTS `role_permission_rel`;
@@ -72,3 +72,21 @@ INSERT INTO `role_permission_rel`(`permID`,`RoleID`)
     SELECT RoleID FROM roles WHERE Code = 'administrator'
   ) 
   FROM permissions;
+
+-- add role-user rel table
+DROP TABLE IF EXISTS `user_role_rel`;
+CREATE TABLE `user_role_rel` (
+  `RoleID` int(10) unsigned NOT NULL default '0',
+  `userID` int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`RoleID`,`userID`),
+  CONSTRAINT `FK_user_role_rel_1`
+  FOREIGN KEY (`userID`)
+    REFERENCES `users` (`ID`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `FK_user_role_rel_2`
+  FOREIGN KEY (`RoleID`)
+    REFERENCES `roles` (`RoleID`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET='utf8';
