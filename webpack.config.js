ec38d566728a821332407a9057b982c00ddb3f32
regisoc <regis.ongaro-carcy@mcin.ca>
2024-01-29 18:48:06 -0500

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -323,7 +323,7 @@ const lorisModules = {
   candidate_profile: ['CandidateInfo'],
   api_docs: ['swagger-ui_custom'],
   dashboard: ['welcome'],
-  roles: ['rolesIndex'],
+  roles_manager: ['rolesManagerIndex', 'rolesManagerForm'],
 };
 for (const [key] of Object.entries(lorisModules)) {
   const target = process.env.target;
