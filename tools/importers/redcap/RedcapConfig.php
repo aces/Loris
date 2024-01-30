ae8ed33b0957c7a9d07acdf81fbdfb4aeaae2423
zaliqarosli <zaliqa.rosli@mcin.ca>
2024-01-30 11:40:55 -0500

    move mapping functions to config class

--- tools/importers/redcap/RedcapConfig.php
+++ tools/importers/redcap/RedcapConfig.php
@@ -24,8 +24,228 @@ class RedcapConfig
         return json_decode($file_get_contents($this->configFilePath), true);
     }
 
+    /**
+     * Gets the importer configurations, with mappings of REDCap and LORIS fields
+     *
+     * @return array the mapping with loris field as key,
+     *               and redcap field as value(s)
+     */
     function getImportConfig(): array
     {
         return $this->import_config;
+    }
+
+    /**
+     * Gets the mapping of meta data field names for candidates and visit
+     * in REDCap and LORIS
+     *
+     * @return array the mapping with loris field as key,
+     *               and redcap field as value(s)
+     */
+    function getMetadataMapping() : array
+    {
+        return $this->getImporterConfig()['metadataMapping'];
+    }
+
+    /**
+     * Gets the mapping of cohort labels in REDCap and LORIS
+     *
+     * @return array the mapping with redcap site as key,
+     *               and loris site as value
+     */
+    function getCohortMapping() : array
+    {
+        $config  = $this->getImporterConfig()['cohortMapping'];
+        $mapping = [];
+
+        foreach ($config as $option) {
+            $mapping[$option['redcapValue']] = $option['lorisValue'];
+        }
+
+        return $mapping;
+    }
+
+    /**
+     * Gets the mapping of site names in REDCap and LORIS
+     *
+     * @return array the mapping with redcap site as key,
+     *               and loris site as value
+     */
+    function getSiteMapping() : array
+    {
+        $config  = $this->getImporterConfig()['siteMapping'];
+        $mapping = [];
+
+        foreach ($config as $option) {
+            $mapping[$option['redcapValue']] = $option['lorisValue'];
+        }
+
+        return $mapping;
+    }
+
+    /**
+     * Gets the mapping of gender/sex in REDCap and LORIS
+     *
+     * @return array the mapping with redcap gender as key,
+     *               and loris sex as value
+     */
+    function getSexMapping() : array
+    {
+        $config  = $this->getImporterConfig()['sexMapping'];
+        $mapping = [];
+
+        foreach ($config as $option) {
+            $mapping[$option['redcapValue']] = $option['lorisValue'];
+        }
+
+        return $mapping;
+    }
+
+    /**
+     * Get visit mapping for LORIS by REDCap visit in config file
+     *
+     * @return array An array of visit mapping
+     */
+    function getVisitMapping() : array
+    {
+        $config  = $this->getImporterConfig()['visitMapping'];
+        $mapping = [];
+
+        foreach ($config as $option) {
+            $mapping[$option['redcapValue']] = $option['lorisValue'];
+        }
+
+        return $mapping;
+    }
+
+    function getInstrumentMapping(): array
+    {
+        $config  = $this->getImporterConfig()['instrumentMapping'];
+        $mapping = [];
+
+        foreach ($config as $option) {
+            $mapping[$option['redcapValue']] = $option['lorisValue'];
+        }
+
+        return $mapping;
+    }
+
+    /**
+     * Get the REDCap date_taken field mappings for configured instrument
+     *
+     * return array The date_taken field names for configured instrument
+     */
+    function getDateTakenMapping() : array
+    {
+        $config  = $this->getImporterConfig()['dateTakenMapping'];
+        $mapping = [];
+
+        foreach ($config as $option) {
+            $mapping[$option['instrument']] = $option['dateTaken'];
+        }
+
+        return $mapping;
+    }
+
+    /**
+     * Gets Loris field names that are different than in REDCap because
+     * the string '_status' exists in the REDCap field name, a reserved
+     * key word in LORIS.
+     *
+     * return array
+     */
+    function getStatusFieldsMapping() : array
+    {
+        $config  = $this->getImporterConfig()['statusFieldsMapping'];
+        $mapping = [];
+
+        foreach ($config as $option) {
+            $mapping[$option['redcapValue']] = $option['lorisValue'];
+        }
+
+        return $mapping;
+    }
+
+    /**
+     * Get the REDCap examiner field mapping
+     *
+     * return array The examiner field names for configured instruments
+     */
+    function getExaminerMapping() : array
+    {
+        $config  = $this->getImporterConfig()['examinerMapping'];
+        $mapping = [];
+
+        foreach ($config as $option) {
+            $mapping[$option['instrument']] = $option['examiner'];
+        }
+
+        return $mapping;
+    }
+
+    function getSiteSpecificFields(): array
+    {
+        $config  = $this->getImporterConfig()['siteSpecific'];
+        $mapping = [];
+
+        foreach ($config as $option) {
+            $mapping[$option['id']] = $option['site'];
+        }
+
+        return $mapping;
+    }
+
+    function getDatesToScrub(): array
+    {
+        $config = $this->getImporterConfig()['datesToScrub'];
+        $dates  = [];
+
+        foreach ($config as $date_field) {
+            // Handle components to scrub
+            $components = [];
+            foreach ($date_field['component'] as $component) {
+                $components[$component['id']] = $component['value'];
+            }
+
+            $dates[$date_field['id']] = [
+                'format'    = $date_field['format'],
+                'component' = $components
+            ];
+        }
+
+        return $dates;
+    }
+
+    function getFieldsToIgnore(): array
+    {
+        return $this->getImporterConfig()['toIgnore'];
+    }
+
+    function getConsentMapping(): array
+    {
+        $config  = $this->getImporterConfig()['consentMapping'];
+        $mapping = [];
+
+        foreach ($config as $consent) {
+            $mapping[$consent['consentId']] = $consent;
+        }
+    }
+
+    function getInstrumentFlagsMapping(): array
+    {
+        $config  = $this->getImporterConfig()['instrumentFlagsMapping'];
+        $mapping = [];
+
+        foreach ($config as $instrument) {
+            $administration = $instrument['administration'] ?? null;
+            $validity       = $instrument['validity'] ?? null;
+            $dataEntry      = $instrument['dataEntry'] ?? null;
+
+            $mapping[$instrument['instrument']] = [
+                'administration' => $administration,
+                'validity'       => $validity,
+                'dataEntry'      => $dataEntry
+            ];
+        }
     }
 }
--- tools/importers/redcap/RedcapImporter.php
+++ tools/importers/redcap/RedcapImporter.php
@@ -410,7 +410,7 @@ abstract class RedcapImporter implements IRedcapImporter
                                    ? $row[$consenter_field] . ' (REDCap)'
                                    : '(REDCap)';
 
-                // Validate
+                // Validate consent values
                 if ($consent_status  === false
                     || $consent_date === ''
                     || strtotime($consent_date) === false
@@ -956,231 +956,6 @@ abstract class RedcapImporter implements IRedcapImporter
         print_r($this->errors);
     }
 
-    /**
-     * Gets the importer configurations, with mappings of REDCap and LORIS fields
-     *
-     * @return array the mapping with loris field as key,
-     *               and redcap field as value(s)
-     */
-    function getImporterConfig() : array
-    {
-       return $this->redcapConfig->getImportConfig();
-    }
-
-    /**
-     * Gets the mapping of meta data field names for candidates and visit
-     * in REDCap and LORIS
-     *
-     * @return array the mapping with loris field as key,
-     *               and redcap field as value(s)
-     */
-    function getMetadataMapping() : array
-    {
-        return getImporterConfig()['metadataMapping'];
-    }
-
-    /**
-     * Gets the mapping of cohort labels in REDCap and LORIS
-     *
-     * @return array the mapping with redcap site as key,
-     *               and loris site as value
-     */
-    function getCohortMapping() : array
-    {
-        $config  = getImporterConfig()['cohortMapping'];
-        $mapping = [];
-
-        foreach ($config as $option) {
-            $mapping[$option['redcapValue']] = $option['lorisValue'];
-        }
-
-        return $mapping;
-    }
-
-    /**
-     * Gets the mapping of site names in REDCap and LORIS
-     *
-     * @return array the mapping with redcap site as key,
-     *               and loris site as value
-     */
-    function getSiteMapping() : array
-    {
-        $config  = getImporterConfig()['siteMapping'];
-        $mapping = [];
-
-        foreach ($config as $option) {
-            $mapping[$option['redcapValue']] = $option['lorisValue'];
-        }
-
-        return $mapping;
-    }
-
-    /**
-     * Gets the mapping of gender/sex in REDCap and LORIS
-     *
-     * @return array the mapping with redcap gender as key,
-     *               and loris sex as value
-     */
-    function getSexMapping() : array
-    {
-        $config  = getImporterConfig()['sexMapping'];
-        $mapping = [];
-
-        foreach ($config as $option) {
-            $mapping[$option['redcapValue']] = $option['lorisValue'];
-        }
-
-        return $mapping;
-    }
-
-    /**
-     * Get visit mapping for LORIS by REDCap visit in config file
-     *
-     * @return array An array of visit mapping
-     */
-    function getVisitMapping() : array
-    {
-        $config  = getImporterConfig()['visitMapping'];
-        $mapping = [];
-
-        foreach ($config as $option) {
-            $mapping[$option['redcapValue']] = $option['lorisValue'];
-        }
-
-        return $mapping;
-    }
-
-    function getInstrumentMapping(): array
-    {
-        $config  = getImporterConfig()['instrumentMapping'];
-        $mapping = [];
-
-        foreach ($config as $option) {
-            $mapping[$option['redcapValue']] = $option['lorisValue'];
-        }
-
-        return $mapping;
-    }
-
-    /**
-     * Get the REDCap date_taken field mappings for configured instrument
-     *
-     * return array The date_taken field names for configured instrument
-     */
-    function getDateTakenMapping() : array
-    {
-        $config  = getImporterConfig()['dateTakenMapping'];
-        $mapping = [];
-
-        foreach ($config as $option) {
-            $mapping[$option['instrument']] = $option['dateTaken'];
-        }
-
-        return $mapping;
-    }
-
-    /**
-     * Gets Loris field names that are different than in REDCap because
-     * the string '_status' exists in the REDCap field name, a reserved
-     * key word in LORIS.
-     *
-     * return array
-     */
-    function getStatusFieldsMapping() : array
-    {
-        $config  = getImporterConfig()['statusFieldsMapping'];
-        $mapping = [];
-
-        foreach ($config as $option) {
-            $mapping[$option['redcapValue']] = $option['lorisValue'];
-        }
-
-        return $mapping;
-    }
-
-    /**
-     * Get the REDCap examiner field mapping
-     *
-     * return array The examiner field names for configured instruments
-     */
-    function getExaminerMapping() : array
-    {
-        $config  = getImporterConfig()['examinerMapping'];
-        $mapping = [];
-
-        foreach ($config as $option) {
-            $mapping[$option['instrument']] = $option['examiner'];
-        }
-
-        return $mapping;
-    }
-
-    function getSiteSpecificFields(): array
-    {
-        $config  = getImporterConfig()['siteSpecific'];
-        $mapping = [];
-
-        foreach ($config as $option) {
-            $mapping[$option['id']] = $option['site'];
-        }
-
-        return $mapping;
-    }
-
-    function getDatesToScrub(): array
-    {
-        $config = getImporterConfig()['datesToScrub'];
-        $dates  = [];
-
-        foreach ($config as $date_field) {
-            // Handle components to scrub
-            $components = [];
-            foreach ($date_field['component'] as $component) {
-                $components[$component['id']] = $component['value'];
-            }
-
-            $dates[$date_field['id']] = [
-                'format'    = $date_field['format'],
-                'component' = $components
-            ];
-        }
-
-        return $dates;
-    }
-
-    function getFieldsToIgnore(): array
-    {
-        return getImporterConfig()['toIgnore'];
-    }
-
-    function getConsentMapping(): array
-    {
-        $config  = getImporterConfig()['consentMapping'];
-        $mapping = [];
-
-        foreach ($config as $consent) {
-            $mapping[$consent['consentId']] = $consent;
-        }
-    }
-
-    function getInstrumentFlagsMapping(): array
-    {
-        $config  = getImporterConfig()['instrumentFlagsMapping'];
-        $mapping = [];
-
-        foreach ($config as $instrument) {
-            $administration = $instrument['administration'] ?? null;
-            $validity       = $instrument['validity'] ?? null;
-            $dataEntry      = $instrument['dataEntry'] ?? null;
-
-            $mapping[$instrument['instrument']] = [
-                'administration' => $administration,
-                'validity'       => $validity,
-                'dataEntry'      => $dataEntry
-            ];
-        }
-    }
-
     /*
      * Get candidate instrument administration flag from REDCap data.
      * In REDCap, 2=Complete, 1=Unverified, 0=Incomplete
@@ -1192,7 +967,7 @@ abstract class RedcapImporter implements IRedcapImporter
      */
     function getAdministrationFlag(array $data, string $instrument) : ?string
     {
-        $administrationMapping = $this->getInstrumentFlagsMapping()[$instrument]['administration'] ?? null;
+        $administrationMapping = $this->redcapConfig->getInstrumentFlagsMapping()[$instrument]['administration'] ?? null;
 
         if ($administrationMapping === null) {
             return null;
@@ -1214,7 +989,7 @@ abstract class RedcapImporter implements IRedcapImporter
      */
     function getValidityFlag(array $data, string $instrument) : ?string
     {
-        $validityMapping = $this->getInstrumentFlagsMapping()[$instrument]['validity'] ?? null;
+        $validityMapping = $this->redcapConfig->getInstrumentFlagsMapping()[$instrument]['validity'] ?? null;
 
         if ($validityMapping === null) {
             return null;
@@ -1236,7 +1011,7 @@ abstract class RedcapImporter implements IRedcapImporter
      */
     function getDataEntryFlag(array $data, string $instrument) : ?string
     {
-        $dataEntryMapping = $this->getInstrumentFlagsMapping()[$instrument]['dataEntry'] ?? null;
+        $dataEntryMapping = $this->redcapConfig->getInstrumentFlagsMapping()[$instrument]['dataEntry'] ?? null;
 
         if ($dataEntryMapping === null) {
             return null;
