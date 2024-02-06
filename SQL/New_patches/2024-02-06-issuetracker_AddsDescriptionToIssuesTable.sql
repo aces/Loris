7ddb1511f3ef64a97a75899ad168ddf5e20050f7
lorisadmin <rolando.acosta@mcin.ca>
2024-02-06 21:44:20 +0000

    code cleaning.

--- SQL/New_patches/2024-02-06-issuetracker_AddsDescriptionToIssuesTable.sql
+++ SQL/New_patches/2024-02-06-issuetracker_AddsDescriptionToIssuesTable.sql
@@ -3,5 +3,4 @@ ALTER TABLE `issues`
     AFTER `category`;
 
 ALTER TABLE `issues_history`
-    MODIFY `fieldChanged` enum('assignee','status','comment','sessionID','centerID','title','category','module','lastUpdatedBy','priority','candID', 'description') NOT NULL DEFAULT 'comment'
-    AFTER `dateAdded`;
+    MODIFY `fieldChanged` enum('assignee','status','comment','sessionID','centerID','title','category','module','lastUpdatedBy','priority','candID', 'description') NOT NULL DEFAULT 'comment';
