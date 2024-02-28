adcd61560d287be7326bcd409b478ca0b98ef8c0
root <root@cbrain.mcgill.ca>
2024-02-28 14:44:16 -0500

    Update the candidate Test

--- modules/candidate_profile/test/candidate_profileTest.php
+++ modules/candidate_profile/test/candidate_profileTest.php
@@ -98,12 +98,14 @@ class CandidateProfileIntegrationTest extends LorisIntegrationTestWithCandidate
      */
     function testCandidateProfileInstrumentLink()
     {
-	    $this->setupPermissions(['superuser']);
+	            $this->setupPermissions(['superuser']);
 
-		$this->safeGet($this->url . "/candidate_profile/115788/");
-		$this->safeClick( WebDriverBy::cssSelector( '#card0 > div > div > div > dl > div:nth-child(9) > dd > div > a:nth-child(1)' ) );
+		    $this->safeGet($this->url . "/candidate_profile/115788/");
+		    $this->safeClick( WebDriverBy::cssSelector( '#card0 > div > div > div > dl > div:nth-child(9) > dd > div > a:nth-child(1)' ) );
 
-		$bodyText  = $this->safeFindElement(WebDriverBy::cssSelector("body"))->getText();
+		            $bodyText
+            = $this->safeFindElement(WebDriverBy::cssSelector("body"))
+            ->getText();
         $this->assertStringContainsString(
             "Behavioural Battery of Instruments",
             $bodyText
