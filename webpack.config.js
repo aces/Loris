4e2977d27126ed863791006d9cd5ef7ad97629ba
Jefferson Casimir <jefferson.casimir@mcin.ca>
2024-03-14 11:32:20 -0400

    Restore webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -239,14 +239,13 @@ let config = [
 ];
 
 const lorisModules = {
-  /*
   media: ['CandidateMediaWidget', 'mediaIndex'],
   issue_tracker: [
     'issueTrackerIndex',
     'index',
     'CandidateIssuesWidget',
   ],
-  login: ['loginIndex'],/*
+  login: ['loginIndex'],
   publication: ['publicationIndex', 'viewProjectIndex'],
   document_repository: ['docIndex', 'editFormIndex'],
   candidate_parameters: [
@@ -291,7 +290,7 @@ const lorisModules = {
   electrophysiology_browser: [
     'electrophysiologyBrowserIndex',
     'electrophysiologySessionView',
-  ],/*
+  ],
   electrophysiology_uploader: [
     'ElectrophysiologyUploader',
     'UploadForm',
@@ -309,7 +308,7 @@ const lorisModules = {
   instrument_manager: ['instrumentManagerIndex'],
   survey_accounts: ['surveyAccountsIndex'],
   mri_violations: ['mriViolationsIndex'],
-  user_accounts: ['userAccountsIndex'],/*
+  user_accounts: ['userAccountsIndex'],
   examiner: ['examinerIndex'],
   help_editor: ['help_editor', 'helpEditorForm'],
   brainbrowser: ['Brainbrowser'],
@@ -323,7 +322,7 @@ const lorisModules = {
   instruments: ['CandidateInstrumentList', 'ControlpanelDeleteInstrumentData'],
   candidate_profile: ['CandidateInfo'],
   api_docs: ['swagger-ui_custom'],
-  dashboard: ['welcome'],*/
+  dashboard: ['welcome'],
 };
 for (const [key] of Object.entries(lorisModules)) {
   const target = process.env.target;
