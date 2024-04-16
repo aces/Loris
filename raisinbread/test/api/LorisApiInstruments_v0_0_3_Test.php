203d9781e9a07647451835992473f5e6a9457e29
kongtiaowang <kongtiaowangshen@gmail.com>
2024-04-16 10:30:36 -0400

    test

--- raisinbread/test/api/LorisApiInstruments_v0_0_3_Test.php
+++ raisinbread/test/api/LorisApiInstruments_v0_0_3_Test.php
@@ -19,7 +19,7 @@ require_once __DIR__ . "/LorisApiAuthenticated_v0_0_3_Test.php";
  */
 class LorisApiInstruments_v0_0_3_Test extends LorisApiAuthenticated_v0_0_3_Test
 {
-    protected $instrumentTest = "bmi";
+    protected $instrumentTest = "testtest";
     protected $candidTest     = "900000";
     protected $visitTest      = "V1";
 
