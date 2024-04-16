83f06d00889f0b3d5ee1e770277d46c5fbed4709
kongtiaowang <kongtiaowangshen@gmail.com>
2024-04-16 13:04:22 -0400

    phpcs

--- modules/candidate_profile/test/candidate_profileTest.php
+++ modules/candidate_profile/test/candidate_profileTest.php
@@ -101,7 +101,12 @@ class CandidateProfileIntegrationTest extends LorisIntegrationTestWithCandidate
                 $this->setupPermissions(['superuser']);
 
         $this->safeGet($this->url . "/candidate_profile/115788/");
-        $this->safeClick(WebDriverBy::cssSelector('#card0 > div > div > div > dl > div:nth-child(9) > dd > div > a:nth-child(1)'));
+        $this->safeClick(
+            WebDriverBy::cssSelector(
+                '#card0 > div > div > div >'.
+                ' dl > div:nth-child(9) > dd > div > a:nth-child(1)'
+            )
+        );
 
                     $bodyText
             = $this->safeFindElement(WebDriverBy::cssSelector("body"))
