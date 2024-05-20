c71a12331125d0da7280127adfac199c76d9b889
Saagar Arya <saagar.arya@mcin.ca>
2024-05-20 11:32:46 -0400

    Update rb instruments

--- raisinbread/instruments/instrument_sql/Meta/mri_parameter_form_meta.sql
+++ raisinbread/instruments/instrument_sql/Meta/mri_parameter_form_meta.sql
@@ -2,39 +2,39 @@ INSERT INTO test_names (Test_name, Full_name, Sub_group) SELECT 'mri_parameter_f
 
 INSERT INTO instrument_subtests (Test_name, Subtest_name, Description, Order_number) VALUES ('mri_parameter_form', 'mri_parameter_form_page1', 'Page 1', 1);
 
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label)
-  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V1' FROM cohort WHERE title = 'Stale';
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label)
-  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V1' FROM cohort WHERE title = 'Fresh';
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label)
-  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V1' FROM cohort WHERE title = 'Low Yeast';
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label)
-  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V1' FROM cohort WHERE title = 'High Yeast';
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label)
-  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V2' FROM cohort WHERE title = 'Stale';
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label)
-  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V2' FROM cohort WHERE title = 'Fresh';
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label)
-  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V2' FROM cohort WHERE title = 'Low Yeast';
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label)
-  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V2' FROM cohort WHERE title = 'High Yeast';
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label)
-  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V3' FROM cohort WHERE title = 'Stale';
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label)
-  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V3' FROM cohort WHERE title = 'Fresh';
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label)
-  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V3' FROM cohort WHERE title = 'Low Yeast';
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label)
-  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V3' FROM cohort WHERE title = 'High Yeast';
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label)
-  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V4' FROM cohort WHERE title = 'Low Yeast';
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label)
-  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V4' FROM cohort WHERE title = 'High Yeast';
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label)
-  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V5' FROM cohort WHERE title = 'Low Yeast';
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label)
-  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V5' FROM cohort WHERE title = 'High Yeast';
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label)
-  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V6' FROM cohort WHERE title = 'Low Yeast';
-INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label)
-  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V6' FROM cohort WHERE title = 'High Yeast';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label, DoubleDataEntryEnabled)
+  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V1', 1 FROM cohort WHERE title = 'Stale';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label, DoubleDataEntryEnabled)
+  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V1', 1 FROM cohort WHERE title = 'Fresh';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label, DoubleDataEntryEnabled)
+  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V1', 1 FROM cohort WHERE title = 'Low Yeast';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label, DoubleDataEntryEnabled)
+  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V1', 1 FROM cohort WHERE title = 'High Yeast';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label, DoubleDataEntryEnabled)
+  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V2', 1 FROM cohort WHERE title = 'Stale';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label, DoubleDataEntryEnabled)
+  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V2', 1 FROM cohort WHERE title = 'Fresh';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label, DoubleDataEntryEnabled)
+  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V2', 1 FROM cohort WHERE title = 'Low Yeast';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label, DoubleDataEntryEnabled)
+  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V2', 1 FROM cohort WHERE title = 'High Yeast';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label, DoubleDataEntryEnabled)
+  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V3', 1 FROM cohort WHERE title = 'Stale';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label, DoubleDataEntryEnabled)
+  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V3', 1 FROM cohort WHERE title = 'Fresh';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label, DoubleDataEntryEnabled)
+  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V3', 1 FROM cohort WHERE title = 'Low Yeast';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label, DoubleDataEntryEnabled)
+  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V3', 1 FROM cohort WHERE title = 'High Yeast';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label, DoubleDataEntryEnabled)
+  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V4', 1 FROM cohort WHERE title = 'Low Yeast';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label, DoubleDataEntryEnabled)
+  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V4', 1 FROM cohort WHERE title = 'High Yeast';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label, DoubleDataEntryEnabled)
+  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V5', 1 FROM cohort WHERE title = 'Low Yeast';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label, DoubleDataEntryEnabled)
+  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V5', 1 FROM cohort WHERE title = 'High Yeast';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label, DoubleDataEntryEnabled)
+  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V6', 1 FROM cohort WHERE title = 'Low Yeast';
+INSERT INTO test_battery (Test_name, AgeMinDays, AgeMaxDays, Active, Stage, CohortID, Visit_label, DoubleDataEntryEnabled)
+  SELECT 'mri_parameter_form', 1, 2147483647, 'Y', 'Visit', CohortID, 'V6', 1 FROM cohort WHERE title = 'High Yeast';
