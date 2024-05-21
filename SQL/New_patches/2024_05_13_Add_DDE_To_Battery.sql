e457ca7301ba26a40c8e69071c9dd18a6404ea3d
Saagar Arya <51128536+skarya22@users.noreply.github.com>
2024-05-21 10:01:23 -0400

    Ready for review

--- SQL/New_patches/2024_05_13_Add_DDE_To_Battery.sql
+++ SQL/New_patches/2024_05_13_Add_DDE_To_Battery.sql
@@ -2,7 +2,7 @@ ALTER TABLE test_battery ADD COLUMN DoubleDataEntryEnabled BOOLEAN DEFAULT FALSE
 
 UPDATE test_battery SET DoubleDataEntryEnabled = 1 WHERE Test_name IN (
     SELECT Value from Config WHERE ConfigID = (SELECT ID FROM ConfigSettings WHERE Name = 'DoubleDataEntryInstruments')
-)
+);
 
 DELETE FROM Config WHERE ConfigID IN (SELECT ID FROM ConfigSettings WHERE Name = 'DoubleDataEntryInstruments');
 
