be20fee5e07679a392efa6f0d72617fc71d7fd78
Saagar Arya <saagar.arya@mcin.ca>
2024-10-04 13:07:00 -0400

    [dictionary] Cohort Filter

--- modules/dictionary/jsx/dataDictIndex.js
+++ modules/dictionary/jsx/dataDictIndex.js
@@ -317,6 +317,15 @@ class DataDictIndex extends Component {
             show: false,
             filter: null,
         },
+        {
+            label: 'Cohorts',
+            show: false,
+            filter: {
+                name: 'Cohorts',
+                type: 'multiselect',
+                options: options.cohorts,
+            },
+        },
     ];
     return (
         <FilterableDataTable
