0c1b8811bb81ba7c86cf389e6d3c757d9eeb7f16
kongtiaowang <kongtiaowangshen@gmail.com>
2024-10-10 14:51:26 -0400

    tet

--- test/wait-for-services.sh
+++ test/wait-for-services.sh
@@ -14,7 +14,7 @@ done
 if [ -v SELENIUM_REQUIRED ]; 
 then
   echo "Waiting for Selenium..."
-  until $(curl --output /dev/null --silent --head --fail http://selenium:4444/wd/hub); do
+  until $(curl --output /dev/null --silent --head --fail http://localhost:4444/wd/hub); do
     sleep 1
   done
   echo "Selenium is alive"
