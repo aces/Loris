07499b66f78bec897411d2f7565a3c522b5aa694
Jefferson Casimir <jefferson.casimir@mcin.ca>
2024-10-28 16:44:24 -0400

    Instrument Data Form: Get header template

--- modules/instrument_manager/jsx/uploadForm.js
+++ modules/instrument_manager/jsx/uploadForm.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import swal from 'sweetalert2';
 import {FileElement} from 'jsx/Form';
+import {SelectElement} from '../../../jsx/Form';
 
 /**
  * Instrument Upload Form component
@@ -15,12 +16,13 @@ class InstrumentUploadForm extends Component {
     super(props);
 
     this.state = {
-      data: {},
       selectedFile: null,
+      selectedInstrument: '',
     };
 
-    this.fileSelected = this.fileSelected.bind(this);
-    this.upload = this.upload.bind(this);
+    this.instrumentFileSelected = this.instrumentFileSelected.bind(this);
+    this.uploadInstrument = this.uploadInstrument.bind(this);
+    this.getInstrumentOptions = this.getInstrumentOptions.bind(this);
   }
 
   /**
@@ -29,7 +31,7 @@ class InstrumentUploadForm extends Component {
    * @param {string} element - Element name
    * @param {string} file
    */
-  fileSelected(element, file) {
+  instrumentFileSelected(element, file) {
     this.setState({
       selectedFile: file,
     });
@@ -38,7 +40,7 @@ class InstrumentUploadForm extends Component {
   /**
    * Upload instrument
    */
-  upload() {
+  uploadInstrument() {
     const data = new FormData();
     data.append('install_file', this.state.selectedFile);
 
@@ -81,6 +83,17 @@ class InstrumentUploadForm extends Component {
       });
   }
 
+  getInstrumentOptions() {
+    const instruments = {};
+    if (this.props.data) {
+      this.props.data.Data.map((instrument) => {
+        const instrumentName = instrument[0];
+        instruments[instrumentName] = instrumentName;
+      });
+    }
+    return instruments;
+  }
+
   /**
    * Renders the React component.
    *
@@ -88,38 +101,112 @@ class InstrumentUploadForm extends Component {
    */
   render() {
     const disabled = () => this.state.selectedFile === null;
+    const instrumentSelected = this.state.selectedInstrument !== '';
 
     return (
-      <div className="row">
-        <div className="col-xs-4">
-          <div className="panel panel-primary">
-            <div className="panel-heading">Upload Instrument</div>
-            <div className="panel-body">
-              <div className="col-xs-12">
+      <>
+        <div className="row">
+          <div className="col-xs-4">
+            <UploadPanel
+              title={'Upload Instrument'}
+            >
+              <FileElement
+                name='install_file'
+                label='Instrument file'
+                onUserInput={this.instrumentFileSelected}
+                value={this.state.selectedFile}
+              />
+              <button
+                className="btn btn-default"
+                onClick={this.upload}
+                disabled={disabled()}
+              >
+                Install
+              </button>
+            </UploadPanel>
+          </div>
+        {/* </div> */}
+        {/* <div className="row"> */}
+          <div className="col-xs-4">
+            <UploadPanel
+              title={'Upload Instrument Data'}
+            >
+              <div className="row">
+                <SelectElement
+                  name={'select_instrument'}
+                  options={this.getInstrumentOptions()}
+                  label={'Select Instrument'}
+                  value={this.state.selectedInstrument}
+                  onUserInput={(e, value) => this.setState({
+                    ...this.state,
+                    selectedInstrument: value,
+                  })}
+                  emptyOption={true}
+                  sortByValue={true}
+                />
+              </div>
+              <div className="row">
                 <FileElement
-                  name='install_file'
-                  label='Instrument file'
-                  onUserInput={this.fileSelected}
+                  name='install_data_file'
+                  label='Instrument Data File (.csv)'
+                  onUserInput={this.instrumentFileSelected}
                   value={this.state.selectedFile}
                 />
+              </div>
+              <div style={{display: 'flex', justifyContent: 'space-between'}}>
                 <button
                   className="btn btn-default"
-                  onClick={this.upload}
-                  disabled={disabled()}
+                  onClick={() => {}}
+                  disabled={!instrumentSelected}
                 >
-                  Install
+                  Upload Data
                 </button>
+                <a
+                  className="btn btn-default"
+                  disabled={!instrumentSelected}
+                  href={`${this.props.action}?instrument=${this.state.selectedInstrument}`}
+                >
+                    Download Expected Template
+                </a>
               </div>
-            </div>
+            </UploadPanel>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 }
 
 InstrumentUploadForm.propTypes = {
   action: PropTypes.string.isRequired,
+  data: PropTypes.object.isRequired,
+};
+export default InstrumentUploadForm;
+
+/**
+ * Create a componenet to select permissions from a list of available
+ * permissions.
+ *
+ * @param {object} props - react props
+ * @return {JSX.Element}
+ */
+function UploadPanel(props) {
+  const children = React.Children.toArray(props.children);
+  return <div className="panel panel-primary">
+    <div className="panel-heading">
+      {props.title}
+    </div>
+    <div className="panel-body">
+      <div className="col-xs-12">
+        {children}
+      </div>
+    </div>
+  </div>;
+}
+
+UploadPanel.propTypes = {
+  children: PropTypes.array,
+  title: PropTypes.string,
 };
 
-export default InstrumentUploadForm;
+
