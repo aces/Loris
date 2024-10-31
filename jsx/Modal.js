39f704483e4ca70370ff2c6d27f091f1a25b996e
Rida Abou-Haidar <rida.abou-haidar@mcin.ca>
2024-10-31 10:10:59 -0400

    updated Modal component

--- jsx/Modal.js
+++ jsx/Modal.js
@@ -3,11 +3,13 @@
  *
  * @author Henri Rabalais
  * @version 1.1.0
+ *
  */
-import React, {Component} from 'react';
+// ########### CBIGR START ###########
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import swal from 'sweetalert2';
-import {ButtonElement} from 'jsx/Form';
+import Swal from 'sweetalert2';
+import Loader from 'Loader';
 
 /**
  * Modal Component.
@@ -26,22 +28,23 @@ import {ButtonElement} from 'jsx/Form';
  *   Modal Window.
  * =================================================
  *
+ * @return {JSX} - React markup for the component
  */
-class Modal extends Component {
-  /**
-   * @constructor
-   */
-  constructor() {
-    super();
-    this.handleClose = this.handleClose.bind(this);
-  }
+function Modal({
+                 throwWarning = false,
+                 show = false,
+                 onClose,
+                 onSubmit,
+                 onSuccess,
+                 title,
+                 children,
+               }) {
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
 
-  /**
-   * Display a warning message on close
-   */
-  handleClose() {
-    if (this.props.throwWarning) {
-      swal.fire({
+  const handleClose = () => {
+    if (throwWarning) {
+      Swal.fire({
         title: 'Are You Sure?',
         text: 'Leaving the form will result in the loss of any information ' +
           'entered.',
@@ -49,124 +52,163 @@ class Modal extends Component {
         showCancelButton: true,
         confirmButtonText: 'Proceed',
         cancelButtonText: 'Cancel',
-      }).then((result) => result.value && this.props.onClose());
+      }).then((result) => result.value && onClose());
     } else {
-      this.props.onClose();
+      onClose();
     }
-  }
+  };
 
-  /**
-   * Renders the React component.
-   *
-   * @return {JSX} - React markup for the component
-   */
-  render() {
-    const {show, children, onSubmit, title, width} = this.props;
+  const headerStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: '40px',
+    borderTopRightRadius: '10',
+    fontSize: 24,
+    padding: 35,
+    borderBottom: '1px solid #DDDDDD',
+  };
 
-    const headerStyle = {
-      display: 'flex',
-      flexDirection: 'row',
-      alignItems: 'center',
-      height: '40px',
-      borderTopRightRadius: '10',
-      fontSize: 24,
-      padding: 35,
-      borderBottom: '1px solid #DDDDDD',
-    };
+  const glyphStyle = {
+    marginLeft: 'auto',
+    cursor: 'pointer',
+  };
 
-    const glyphStyle = {
-      marginLeft: 'auto',
-      cursor: 'pointer',
-    };
+  const bodyStyle = {
+    padding: success ? 0 : '15px 15px',
+    maxHeight: success ? 0 : '75vh',
+    overflow: 'scroll',
+    opacity: success ? 0 : 1,
+    transition: '1s ease, opacity 0.3s',
+  };
 
-    const bodyStyle = {
-      padding: 15,
-      maxHeight: '75vh',
-      overflowY: 'scroll',
-    };
+  const modalContainer = {
+    display: 'block',
+    position: 'fixed',
+    zIndex: 9999,
+    paddingTop: '100px',
+    paddingBottom: '100px',
+    left: 0,
+    top: 0,
+    width: '100%',
+    height: '100%',
+    overflow: 'auto',
+    backgroundColor: 'rgba(0,0,0,0.7)',
+    visibility: show ? 'visible' : 'hidden',
+  };
 
-    const modalContainer = {
-      display: 'block',
-      position: 'fixed',
-      zIndex: 9999,
-      paddingTop: '65px',
-      left: 0,
-      top: 0,
-      width: '100%',
-      height: '100%',
-      overflow: 'auto',
-      backgroundColor: 'rgba(0,0,0,0.7)',
-      visibility: show ? 'visible' : 'hidden',
-    };
+  const modalContent = {
+    opacity: show ? 1 : 0,
+    top: show ? 0 : '-300px',
+    position: 'relative',
+    backgroundColor: '#fefefe',
+    borderRadius: '7px',
+    margin: 'auto',
+    padding: 0,
+    border: '1px solid #888',
+    width: '700px',
+    boxShadow: '0 4px 8px 0 rbga(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)',
+    transition: '0.4s ease',
+  };
 
-    const modalContent = {
-      opacity: show ? 1 : 0,
-      top: show ? 0 : '-300px',
-      position: 'relative',
-      backgroundColor: '#fefefe',
-      borderRadius: '7px',
-      margin: 'auto',
-      padding: 0,
-      border: '1px solid #888',
-      width: width || '700px',
-      boxShadow: '0 4px 8px 0 rbga(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)',
-      transition: 'top 0.4s, opacity 0.4s',
-    };
+  const renderChildren = () => show && children;
 
-    const renderChildren = () => show && children;
+  const footerStyle = {
+    borderTop: '1px solid #DDDDDD',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: '40px',
+    padding: '35px',
+    backgroundColor: success && '#e0ffec',
+  };
 
-    const footerStyle = {
-      borderTop: '1px solid #DDDDDD',
-      display: 'flex',
-      flexDirection: 'row',
-      alignItems: 'center',
-      height: '40px',
-      padding: '35px 35px 20px 35px',
-    };
+  const submitStyle = {
+    marginLeft: 'auto',
+    marginRight: '20px',
+  };
 
-    const submitStyle = {
-      marginLeft: 'auto',
-      marginRight: '20px',
-    };
+  const submitButton = () => {
+    if (onSubmit && !(loading || success)) {
+      return (
+        <div style={submitStyle}>
+          <ButtonElement/>
+        </div>
+      );
+    }
+  };
 
-    const submitButton = () => {
-      if (onSubmit) {
-        const submit = () => onSubmit().then(() => this.props.onClose())
-          .catch(() => {});
-        return (
-          <div style={submitStyle}>
-            <ButtonElement
-              label="Submit"
-              onUserInput={submit}
-            />
-          </div>
-        );
+  const processStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    margin: '0px auto',
+    width: '90px',
+  };
+  const loader = loading && (
+    <div style={processStyle}>
+      <Loader size={20}/>
+      <h5 className='animate-flicker'>Saving</h5>
+    </div>
+  );
+  const successDisplay = success && (
+    <div style={processStyle}>
+      <span
+        style={{color: 'green', marginBottom: '2px'}}
+        className='glyphicon glyphicon-ok-circle'
+      />
+      <h5>Success!</h5>
+    </div>
+  );
+
+    const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+    const submit = async (e) => {
+      console.log('form submit');
+      try {
+        setLoading(true);
+        const data = await onSubmit();
+        setLoading(false);
+        setSuccess(true);
+        await wait(2000);
+        setSuccess(false);
+        onClose();
+        onSuccess(data);
+      } catch {
+        setLoading(false);
       }
     };
 
-    return (
-      <div style={modalContainer} onClick={this.handleClose}>
-        <div
-          style={modalContent}
-          onClick={(e) => e.stopPropagation()}
+  return (
+    <div style={modalContainer} onClick={handleClose}>
+      <div
+        style={modalContent}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div style={headerStyle}>
+          {title}
+          <span style={glyphStyle} onClick={handleClose}>
+            ×
+          </span>
+        </div>
+        <FormElement
+          name="modalForm"
+          id="modalForm"
+          onSubmit={submit}
         >
-          <div style={headerStyle}>
-            {title}
-            <span style={glyphStyle} onClick={this.handleClose}>
-              ×
-            </span>
-          </div>
           <div style={bodyStyle}>
             {renderChildren()}
           </div>
           <div style={footerStyle}>
+            {loader}
+            {successDisplay}
             {submitButton()}
           </div>
-        </div>
+        </FormElement>
       </div>
-    );
-  }
+    </div>
+  );
 }
+// ###########  CBIGR END  ###########
 
 Modal.propTypes = {
   title: PropTypes.string,
@@ -174,12 +216,6 @@ Modal.propTypes = {
   onClose: PropTypes.func.isRequired,
   show: PropTypes.bool.isRequired,
   throwWarning: PropTypes.bool,
-  children: PropTypes.node,
-  width: PropTypes.string,
-};
-
-Modal.defaultProps = {
-  throwWarning: false,
 };
 
 export default Modal;
