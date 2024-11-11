cc6f1e89eddea6a666c49e751249c3a44a0ec6fa
shonibare <47093379+shonibare@users.noreply.github.com>
2024-11-11 13:30:52 -0500

    [candidate_list] Fix 'Access Profile' link on timepoint list page (#9455)
    Selection Filter boxes on 'Access Profile' page are
    empty when accessed via breadcrumb link.
    When the 'Access Profile' is reached via clicking on the breadcrumb at, the
    page will load, but the multi-select boxes in the 'Selection Filter'
    Menu appear empty. This is not the case if the page is accessed via the
    link in the 'Candidate' menu.

--- modules/candidate_list/jsx/candidateListIndex.js
+++ modules/candidate_list/jsx/candidateListIndex.js
@@ -65,7 +65,7 @@ class CandidateListIndex extends Component {
    * Called by React when the component has been rendered on the page.
    */
   componentDidMount() {
-    fetch('options',
+    fetch(loris.BaseURL+'/candidate_list/options',
         {credentials: 'same-origin'}).then(
             (resp) => resp.json()
         ).then(
