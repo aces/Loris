c3daa13d61c6c93a4422acb59acad3f5de5dfbb2
kongtiaowang <kongtiaowangshen@gmail.com>
2025-01-09 14:23:32 -0500

    fix NDB_Menu_Filter_Test.php

--- test/unittests/NDB_Menu_Filter_Test.php
+++ test/unittests/NDB_Menu_Filter_Test.php
@@ -62,7 +62,7 @@ class NDB_Menu_Filter_Test extends TestCase
     {
         $method          = ['_resetFilters'];
         $allOtherMethods = $this->_getAllMethodsExcept($method);
-        $allOtherMethods = array_values($allOtherMethods);  // Ensure this is an indexed array
+        $allOtherMethods = array_values($allOtherMethods);
         $stub            = $this->getMockBuilder('NDB_Menu_Filter')
             ->onlyMethods($allOtherMethods)
             ->disableOriginalConstructor()
