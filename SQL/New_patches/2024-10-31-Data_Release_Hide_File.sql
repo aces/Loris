5a637ef0e6a0b029782b5903d9bf24649008a517
Saagar Arya <saagar.arya@mcin.ca>
2025-01-28 17:20:35 -0500

    Rename hidden_by_id_ & remove subquery

--- SQL/New_patches/2024-10-31-Data_Release_Hide_File.sql
+++ SQL/New_patches/2024-10-31-Data_Release_Hide_File.sql
@@ -1,7 +1,7 @@
 ALTER TABLE data_release
-ADD COLUMN hidden_by_id int(10) unsigned NULL DEFAULT NULL,
-ADD CONSTRAINT FK_hidden_by_id
-FOREIGN KEY (hidden_by_id) REFERENCES users (ID);
+ADD COLUMN hidden_by_userid int(10) unsigned NULL DEFAULT NULL,
+ADD CONSTRAINT FK_hidden_by_userid
+FOREIGN KEY (hidden_by_userid) REFERENCES users (ID);
 
 INSERT INTO permissions (moduleID, code, action, description, categoryID)
     SELECT ID, 'data_release_hide', 'Edit', 'Hidden Releases', 2 FROM modules WHERE Name = 'data_release';
