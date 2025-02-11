b273f3b2603fa89f6c99ad4352181425b277d4fb
Kevin LaFleur <kevin.lafleur@mcin.ca>
2025-02-11 17:14:08 -0500

    updated all found instances of htmlspecialchars()

--- tools/generate_tables_sql_and_testNames.php
+++ tools/generate_tables_sql_and_testNames.php
@@ -54,7 +54,7 @@ foreach ($instruments as $instrument) {
 
         case "page":
             if (array_key_exists(2, $bits)) {
-                $pages[] = htmlspecialchars($bits[2]);
+                $pages[] = htmlspecialchars($bits[2],ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML5, null, false);
             }
             continue 2;
 
