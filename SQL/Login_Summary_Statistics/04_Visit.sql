80f8523b7d5dcb796b0f2cde9bdb8f0ff44ab403
Dave MacFarlane <dave.macfarlane@mcin.ca>
2025-02-12 16:49:47 -0500

    [Login Statistics] Fix query
    There was a semantic conflict between PR# and one of the queries
    in PR#9518, as the CandID now needs to come from the candidate table.
    
    This adds a JOIN from the session for the one query missing it, so
    that the update login statistics script doesn't fail with an SQL
    error.

--- SQL/Login_Summary_Statistics/04_Visit.sql
+++ SQL/Login_Summary_Statistics/04_Visit.sql
@@ -1,7 +1,8 @@
 SELECT 
     IFNULL(Project.Name, 'All Projects') as ProjectName, 
-    COUNT(CandID) AS count
-    FROM session s
+    COUNT(c.CandID) AS count
+    FROM candidate c
+    JOIN session s ON (c.ID=s.CandidateID)
     JOIN Project ON s.ProjectID = Project.ProjectID
 WHERE Project.showSummaryOnLogin = 1
 GROUP BY Project.Name WITH ROLLUP
