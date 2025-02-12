d00eea9816c477f878e072907116ccef9d73a22c
Sruthy Mathew <sruthy.mcin@gmail.com>
2025-02-12 13:54:35 -0500

    More Fixes and Clean Up

--- SQL/New_patches/2025-01-01-add_ParticipantID.sql
+++ SQL/New_patches/2025-01-01-add_ParticipantID.sql
@@ -1,21 +1,6 @@
 ALTER TABLE candidate ADD COLUMN ParticipantID VARCHAR(25) NULL, ADD UNIQUE INDEX `UK_candidate_ParticipantID` (`ParticipantID` ASC);
 INSERT INTO modules (Name, Active) VALUES ('participant_portal', 'Y');
 
-CREATE TABLE `instrument_completion_time` (
-                                              `ID` int(10) NOT NULL AUTO_INCREMENT,
-                                              `TestNameID` int(6) NOT NULL,
-                                              `completion_time` VARCHAR(10) DEFAULT NULL,
-                                              PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `partcipant_portal_emails` (
-                                              `ID` int(10) NOT NULL AUTO_INCREMENT,
-                                              `TestNameID` int(6) NOT NULL,
-                                              `completion_time` VARCHAR(10) DEFAULT NULL,
-                                              PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
 CREATE TABLE `participant_portal_emails` (
                                              `ID` INT(10) NOT NULL AUTO_INCREMENT,
                                              `ParticipantAccountID` INT(10) NOT NULL,
