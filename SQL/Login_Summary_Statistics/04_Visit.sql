b339900c2ea302647bdcf4f8892e9fabb048ff63
Dave MacFarlane <dave.macfarlane@mcin.ca>
2025-02-17 11:01:03 -0500

    Remove candidate join

--- SQL/Login_Summary_Statistics/04_Visit.sql
+++ SQL/Login_Summary_Statistics/04_Visit.sql
@@ -1,8 +1,7 @@
 SELECT 
     IFNULL(Project.Name, 'All Projects') as ProjectName, 
-    COUNT(c.CandID) AS count
-    FROM candidate c
-    JOIN session s ON (c.ID=s.CandidateID)
-    JOIN Project ON s.ProjectID = Project.ProjectID
+    COUNT(s.ID) AS count
+    FROM session s
+    JOIN Project ON (s.ProjectID = Project.ProjectID)
 WHERE Project.showSummaryOnLogin = 1
 GROUP BY Project.Name WITH ROLLUP
