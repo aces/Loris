d74cdacf18eaa9abf1d2f3d8b2591bcade26b5ae
root <root@cbrain.mcgill.ca>
2025-02-19 13:57:55 -0500

    [issue_tracker] Modify ['action','description'] in the permissions for issue tracker

--- SQL/New_patches/2025-02-07_update_add_issue_tracker_permissions.sql
+++ SQL/New_patches/2025-02-07_update_add_issue_tracker_permissions.sql
@@ -1,9 +1,9 @@
-UPDATE permissions SET code = 'issue_tracker_own_issue', description = 'See/Edit/Comment and Close on Own Issues' 
+UPDATE permissions SET code = 'issue_tracker_own_issue', description = 'Issues - Own', action = 'View/Edit/Comment/Close'
 WHERE code = 'issue_tracker_reporter';
-UPDATE permissions SET code = 'issue_tracker_all_issue', description = 'See/Edit/Comment on all Issues' 
+UPDATE permissions SET code = 'issue_tracker_all_issue', description = 'Issues - All Sites', action = 'View/Edit/Comment'
 WHERE code = 'issue_tracker_developer';
 
-INSERT INTO permissions (code, description, moduleID, categoryID) VALUES
-('issue_tracker_site_issue','See/Edit/Comment on Own Site Issues',(SELECT ID FROM modules WHERE Name = 'issue_tracker'),2),
-('issue_tracker_close_site_issue','Close Own Site Issues',(SELECT ID FROM modules WHERE Name = 'issue_tracker'),2),
-('issue_tracker_close_all_issue','Close all Issues',(SELECT ID FROM modules WHERE Name = 'issue_tracker'),2);
+INSERT INTO permissions (code, description, moduleID, action, categoryID) VALUES
+('issue_tracker_site_issue','Issues - Own Sites',(SELECT ID FROM modules WHERE Name = 'issue_tracker'),'View/Edit/Comment',2),
+('issue_tracker_close_site_issue','Issues - Own Sites',(SELECT ID FROM modules WHERE Name = 'issue_tracker'),'Close',2),
+('issue_tracker_close_all_issue','Issues - All Sites',(SELECT ID FROM modules WHERE Name = 'issue_tracker'),'Close',2);
