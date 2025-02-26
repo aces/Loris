57bc08452d48eec988cbd6914a68dd9c288bee66
root <root@cbrain.mcgill.ca>
2025-02-26 16:10:46 -0500

    alter issues_history tbl

---
+++ SQL/New_patches/2025-02-26_alter_issues_history_fieldChanged.sql
@@ -0,0 +1,6 @@
+ALTER TABLE issues_history CHANGE `fieldChanged` `fieldChanged` enum('assignee','status','comment','sessionID','centerID','title','category','module','lastUpdatedBy','priority','CandidateID','candID','description','watching','instrument') NOT NULL DEFAULT 'comment';
+
+UPDATE issues_history SET `fieldChanged` = 'CandidateID' WHERE `fieldChanged` = 'candID';
+
+ALTER TABLE issues_history CHANGE `fieldChanged` `fieldChanged` enum('assignee','status','comment','sessionID','centerID','title','category','module','lastUpdatedBy','priority','CandidateID', 'description','watching','instrument') NOT NULL DEFAULT 'comment';
+
