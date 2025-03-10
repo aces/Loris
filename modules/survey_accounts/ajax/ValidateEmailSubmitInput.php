de34479bb512795f8ea24c62cbc632448e3ee697
Shen <kongtiaowangshen@gmail.com>
2025-03-10 11:46:55 -0400

    [survey] - fix can't add or email a survey (#9639)
    Fix can't create a survey or email a survey. 
    Test : create a survey, make sure it works.
    
    Fixes #9637

--- modules/survey_accounts/ajax/ValidateEmailSubmitInput.php
+++ modules/survey_accounts/ajax/ValidateEmailSubmitInput.php
@@ -53,7 +53,7 @@ if ($numCandidates != 1) {
 $numSessions = $db->pselectOne(
     "SELECT COUNT(*) FROM session s
             JOIN candidate c ON (c.ID=s.CandidateID)
-            WHERE CandID=:v_CandID
+            WHERE c.CandID=:v_CandID
             AND UPPER(Visit_label)=UPPER(:v_VL)
             AND s.Active='Y'",
     [
@@ -82,7 +82,7 @@ if (empty($_REQUEST['TN'])) {
 $instrument_list = $db->pselect(
     "SELECT tn.Test_name FROM flag f
              JOIN session s on s.ID = f.SessionID
-             JOIN candidate ON c.ID = s.CandidateID
+             JOIN candidate c ON c.ID = s.CandidateID
              JOIN test_names tn ON tn.ID = f.TestID
              WHERE c.CandID=:v_CandID
              AND UPPER(s.Visit_label)=UPPER(:v_VL)
