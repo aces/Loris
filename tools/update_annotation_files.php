2d74f02dd418115623b107c3f0a0d2c71d9f8e00
Jefferson Casimir <jefferson.casimir@mcin.ca>
2025-03-12 18:15:28 -0400

    Replace annotations references with events

--- tools/update_annotation_files.php
+++ tools/update_annotation_files.php
@@ -1,35 +1,50 @@
 #!/usr/bin/env php
 <?php declare(strict_types=1);
 
-set_include_path(
-    get_include_path().":".
-    __DIR__."/../project/tools:".
-    __DIR__."/../php/tools:"
-);
+/**
+ * This script updates all the data associated with an EEG file
+ *
+ * Delete all table rows for a given EEG file
+ * "Usage: php delete_physiological_file.php PhysiologicalFileID";
+ * "Example: php delete_physiological_file.php 25";
+ *
+ * PHP Version 5
+ *
+ * @category Main
+ * @package  Loris
+ * @author   Various <example@example.com>
+ * @license  Loris license
+ * @link     https://www.github.com/aces/Loris/
+ */
 
-use LORIS\electrophysiology_browser\Models\ElectrophysioAnnotations;
-require_once __DIR__ . "/../vendor/autoload.php";
+use LORIS\electrophysiology_browser\Models\ElectrophysioEvents;
+
 require_once "generic_includes.php";
 require_once __DIR__
     . "/../modules/electrophysiology_browser/php/models/"
-    . "electrophysioannotations.class.inc";
+    . "electrophysioevents.class.inc";
 
 /**
  * Update any derivative files that are not up to date
  * with the data in the database
  */
 
+global $lorisInstance;
+
 $db = \NDB_Factory::singleton()->database();
 
 //Get all file IDs that must be updated
 $physioFileIDs = $db->pselect(
     "SELECT DISTINCT paf.PhysiologicalFileID
-    FROM physiological_annotation_file AS paf
+    FROM physiological_event_file AS paf
     WHERE paf.LastWritten <= paf.LastUpdate",
     []
 );
 
 foreach ($physioFileIDs as $id) {
-    (new ElectrophysioAnnotations(intval($id['PhysiologicalFileID'])))
-        ->updateFiles();
+    (new ElectrophysioEvents(
+        $lorisInstance,
+        intval($id['PhysiologicalFileID'])
+    )
+    )->updateFiles();
 }
