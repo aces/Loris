64ff22f979c664bd451a6721bb71231c4a67edbd
Jefferson Casimir <jefferson.casimir@mcin.ca>
2025-03-21 11:03:13 -0400

    Combine patches

--- SQL/New_patches/2025-01-08-add-server-processes-type-parse-instrument-data.sql
+++ SQL/New_patches/2025-01-08-add-server-processes-type-parse-instrument-data.sql
@@ -1,14 +1,23 @@
 -- Add parse_instrument_data to server_processes types
 ALTER TABLE server_processes MODIFY COLUMN type enum('mri_upload', 'parse_instrument_data') DEFAULT NULL;
 
-
 -- Add instrumentDataPath to ConfigSettings
 INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber)
-SELECT 'tempPath', 'Path for temporary files',
-       1, 0, 'text', ID, 'Temp File Path', 16
+SELECT 'instrumentDataPath', 'Path to directory for uploaded instrument data csv files',
+       1, 0, 'text', ID, 'Instrument Data Upload Path', 15
 FROM ConfigSettings WHERE Name="paths";
 
 -- Add instrumentDataPath Config
 INSERT INTO Config (ConfigID, Value)
-SELECT ID, "/data/tmp/"
-FROM ConfigSettings WHERE Name="tempPath";
+SELECT ID, "/data/uploads/instruments/"
+FROM ConfigSettings WHERE Name="instrumentDataPath";
+
+-- Rel table for instrument_data entry -> CSV file. DataID NULL if not successful
+CREATE TABLE `instrument_data_files` (
+    `ID` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `DataID` INT(10) unsigned default NULL,
+    `FilePath` VARCHAR(255) NOT NULL,
+    CONSTRAINT `FK_flag_instrument_data_files` FOREIGN KEY (`DataID`) REFERENCES `instrument_data` (`ID`),
+    PRIMARY KEY (`ID`)
+);
+
