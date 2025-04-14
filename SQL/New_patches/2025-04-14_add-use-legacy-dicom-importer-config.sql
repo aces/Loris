3221869b86693544a9cf173864c213d5ba0524a1
Maxime Mulder <maxime-mulder@outlook.com>
2025-04-14 09:11:11 +0000

    add_use_legacy_dicom_study_uploader_config

---
+++ SQL/New_patches/2025-04-14_add-use-legacy-dicom-importer-config.sql
@@ -0,0 +1,9 @@
+INSERT INTO ConfigSettings (Name, Description, Visible, AllowMultiple, DataType, Parent, Label, OrderNumber)
+  SELECT 'use_legacy_dicom_study_importer', 'Use the legacy DICOM study importer instead of the new one.', 1, 0, 'boolean', ID, 'Use legacy DICOM study importer', 11
+  FROM ConfigSettings
+  WHERE Name="imaging_pipeline";
+
+INSERT INTO Config (ConfigID, Value)
+  SELECT ID, 0
+  FROM ConfigSettings cs
+  WHERE cs.Name='use_legacy_dicom_study_importer';
