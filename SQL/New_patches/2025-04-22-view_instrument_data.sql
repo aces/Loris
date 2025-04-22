7beb99d67e06a38f3198d3ac6eb1c5d85f5ddc97
adamdaudrich <adam.daudrich@mcin.ca>
2025-04-22 10:51:30 -0400

    SQLFix

--- SQL/New_patches/2025-04-22-view_instrument_data.sql
+++ SQL/New_patches/2025-04-22-view_instrument_data.sql
@@ -1,3 +1,3 @@
-INSERT INTO permissions (code, description, ModuleID, action, category) 
-'view_instrument_data', 'Data Query Tool - View Instrument Data', 
-(SELECT ID FROM modules WHERE Name = 'instruments'), 'View', 2;
+INSERT INTO permissions (code, description, ModuleID, action, category)
+VALUES ('view_instrument_data', 'Data Query Tool - View Instrument Data', 
+(SELECT ID FROM modules WHERE Name = 'instruments'), 'View', 2);
