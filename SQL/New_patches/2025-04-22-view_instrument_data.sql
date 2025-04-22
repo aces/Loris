5d0f4067ab837ac3524f99e964cc99051b0abd36
adamdaudrich <adam.daudrich@mcin.ca>
2025-04-22 09:12:31 -0400

    viewInstrumentData

---
+++ SQL/New_patches/2025-04-22-view_instrument_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO permissions (code, description, ModuleID, action, category) 
+'view_instrument_data', 'Data Query Tool - View Instrument Data', 
+(SELECT ID FROM modules WHERE Name = 'instruments'), View, 2;
