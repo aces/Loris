c49657acc5cf2e5392a28bedbf977cd6abdc6c3b
regisoc <regis.ongaro-carcy@mcin.ca>
2025-05-23 17:19:40 -0400

    generic include path

--- tools/redcap/redcap_bulk_importer.php
+++ tools/redcap/redcap_bulk_importer.php
@@ -17,7 +17,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once __DIR__ . "/../../../tools/generic_includes.php";
+require_once __DIR__ . "/../../tools/generic_includes.php";
 
 // load redcap module
 try {
