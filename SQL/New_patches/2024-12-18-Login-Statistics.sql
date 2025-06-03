bfc6ba68f902db130ace0618fe09ddccfd092729
Saagar Arya <saagar.arya@mcin.ca>
2025-06-03 12:36:26 -0400

    Try updating old patch

--- SQL/New_patches/2024-12-18-Login-Statistics.sql
+++ SQL/New_patches/2024-12-18-Login-Statistics.sql
@@ -1,9 +1,11 @@
-CREATE TABLE Login_Summary_Statistics (
+DELETE TABLE IF EXISTS login_summary_statistics;
+DELETE TABLE IF EXISTS Login_Summary_Statistics;
+CREATE TABLE login_summary_statistics (
     Title VARCHAR(255),
     Project VARCHAR(255),
     Value INT,
     QueryOrder INT,
-     PRIMARY KEY (Title, Project)
+    PRIMARY KEY (Title, Project)
 );
 
 ALTER TABLE dataquery_study_queries_rel
