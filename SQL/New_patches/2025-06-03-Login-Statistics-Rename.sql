5be1fea392071cd9a154517017cb48a1278d8b6b
Saagar Arya <51128536+skarya22@users.noreply.github.com>
2025-06-03 13:17:40 -0400

    Delete new patch

--- SQL/New_patches/2025-06-03-Login-Statistics-Rename.sql
+++
@@ -1,9 +0,0 @@
-DELETE TABLE IF EXISTS login_summary_statistics;
-DELETE TABLE IF EXISTS Login_Summary_Statistics;
-CREATE TABLE login_summary_statistics (
-    Title VARCHAR(255),
-    Project VARCHAR(255),
-    Value INT,
-    QueryOrder INT,
-    PRIMARY KEY (Title, Project)
-);
