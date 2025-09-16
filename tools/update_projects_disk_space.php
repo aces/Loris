da79151b292ea9c2aa7ab31f6f360e03c38a7531
jeffersoncasimir <jefferson.casimir@mcin.ca>
2025-09-16 16:32:47 -0400

    Fint php lint

--- tools/update_projects_disk_space.php
+++ tools/update_projects_disk_space.php
@@ -1,89 +1,95 @@
-<?php
+<?php declare(strict_types=1);
 
 /**
- *
  * This script updates the cached DB record for disk space occupied by
  * projects. Intended to be run both periodically and via trigger
  *
  * Ex: 0 * * * * php /var/www/loris/project/tools/update_project_disk_space.php
- *
  */
 
 require_once __DIR__ . "/generic_includes.php";
 
-$db = \NDB_Factory::singleton()->database();
+$db     = \NDB_Factory::singleton()->database();
 $config = \NDB_Config::singleton();
 
-$dataDir = $config->getSetting('dataDirBasepath');
-$list_of_projects = Utility::getProjectList();
+$dataDir  = $config->getSetting('dataDirBasepath');
+$projects = Utility::getProjectList();
 
-$project_data = [];
-// NOTE: Below is EEGNet specific. Other projects may want to use 'files'
-// or a different query
-foreach ($list_of_projects as $pid => $project) {
-    $project_directory = $db->pselectOne(
+$projectData = [];
+
+foreach ($projects as $pid => $project) {
+    $projectDir = $db->pselectOne(
         "SELECT DISTINCT(
           SUBSTRING(SUBSTRING_INDEX(FilePath, '/', 2), 1)
         )
-        FROM physiological_file pf
-        LEFT JOIN session s ON s.ID = pf.SessionID
-        WHERE s.ProjectID=:PID LIMIT 1;",
+        FROM (
+            SELECT FilePath, SessionID
+            FROM physiological_file pf
+        UNION
+            SELECT File, SessionID as FilePath FROM files f
+        ) file_table
+        LEFT JOIN session s ON s.ID = file_table.SessionID
+        WHERE FilePath LIKE 'bids_imports%'
+        AND s.ProjectID=:PID LIMIT 1;",
         ['PID' => $pid]
     );
-    if (str_starts_with($project_directory, 'bids_imports')) {
-        $full_path = $dataDir . $project_directory;
-        $dir_size_gb = round(
-            get_dir_size($full_path) / pow(10, 9),
+    if (!is_null($projectDir)) {
+        $fullPath  = $dataDir . $projectDir;
+        $dirSizeGB = round(
+            getDirSize($fullPath) / pow(10, 9),
             1
         );
-        $project_data[$project]['total'] = $dir_size_gb;
+        $projectData[$project]['total'] = $dirSizeGB;
     }
 }
 
-$cached_data_type_id = $db->pselectOneInt(
+$cachedDataTypeID = $db->pselectOneInt(
     "SELECT `CachedDataTypeID`
         FROM `cached_data_type`
         WHERE `Name` = 'projects_disk_space'",
     []
 );
 
-$row_exists = $db->pselectOne(
+$rowExists = $db->pselectOne(
     "SELECT Value FROM cached_data
     WHERE CachedDataTypeID = :CDTID;",
-    ['CDTID' => $cached_data_type_id]
+    ['CDTID' => $cachedDataTypeID]
 );
 
-if ($row_exists) {
+if ($rowExists) {
     $db->update(
         'cached_data',
-        ['Value' => json_encode($project_data)],
-        ['CachedDataTypeID' => $cached_data_type_id]
+        ['Value' => json_encode($projectData)],
+        ['CachedDataTypeID' => $cachedDataTypeID]
     );
 } else {
     $db->insert(
         'cached_data',
         [
-            'CachedDataTypeID' => $cached_data_type_id,
-            'Value' => json_encode($project_data)
+            'CachedDataTypeID' => $cachedDataTypeID,
+            'Value'            => json_encode($projectData)
         ]
     );
 }
 
-echo "Disk space updated\r\n";
+echo "cached_data:projects_disk_space updated\r\n";
 
 
 /**
  * Calculate directory size, recursively, skipping .tgz files
  *
+ * @param string $directory Target directory
+ *
  * @return int
  */
-function get_dir_size($directory){
-    $size = 0;
+function getDirSize(string $directory): int
+{
+    $size  = 0;
     $files = glob($directory . '/*');
-    foreach($files as $path){
+    foreach ($files as $path) {
         if (!str_ends_with($path, '.tgz')) {
             is_file($path) && $size += filesize($path);
-            is_dir($path)  && $size += get_dir_size($path);
+            is_dir($path)  && $size += getDirSize($path);
         }
     }
     return $size;
