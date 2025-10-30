55401c3fba9a3441e466589fda6aeff5c494df98
Rida Abou-Haidar <rida.abou-haidar@mcin.ca>
2025-10-30 08:12:34 -0400

    fixing static tests

--- jslib/core/errors/HttpError.ts
+++ jslib/core/errors/HttpError.ts
@@ -4,12 +4,11 @@ import {BaseError} from './BaseError';
  * Base class for HTTP-related errors.
  */
 export class HttpError extends BaseError {
-
   /**
    * @param request The Request object that generated the error.
    * @param response The raw HTTP Response object (guaranteed to be 2xx, e.g., 204).
    * @param message The error message.
-   */  
+   */
   constructor(
     public readonly request: Request,
     public readonly response?: Response,
