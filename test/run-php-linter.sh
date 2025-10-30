36a34b3277c24e54f7a94595c1913236260b9527
Saagar Arya <51128536+skarya22@users.noreply.github.com>
2025-10-30 11:15:58 -0400

    [publication] Fix collaborator behaviour (#9908)
    - Publication collaborator behaviour was a bit confusing and not future
    proof. Issues such as:
    - Two publications could not have a collaborator with the same name and
    different emails
    - Same issue with Lead Investigator and a collaborator in another
    publication
    - Resolved by separating the publication_collaborator table to have one
    entry per collaborator per publication. There is no reason for two
    publications to point to the same collaborator since they are editable
    in each publication.
    
    * Resolves #9590

--- test/run-php-linter.sh
+++ test/run-php-linter.sh
@@ -41,19 +41,8 @@ find docs modules htdocs php src tools \
 
 # except:
 declare -a ignored_files=(
-    tools/single_use/Cleanup_Consent_Data.php
-    tools/single_use/Engine_Change_MyISAM_to_INNODB.php
-    tools/single_use/normalize_mri_protocol_range_data.php
-    tools/single_use/Update_scan_type_of_mri_violations_log_when_manual_caveat.php
-    tools/single_use/instrument_double_escape_report.php
-    tools/single_use/cleanup_mri_tables_for_19-0_release.php
-    tools/single_use/Normalize_protocol_split_rows.php
-    tools/single_use/migrate_sql_to_json.php
-    tools/single_use/data_dictionary_cleaner.php
-    tools/single_use/Normalize_Consent_Data.php
-    tools/single_use/remove_logged_passwords.php
-    tools/deprecated/create_candidates.php
-    tools/deprecated/excelDump.php
+    tools/single_use/*
+    tools/deprecated/*
 )
 
 declare -a params=(
