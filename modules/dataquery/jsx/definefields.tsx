a3a6fd4993d45c71f40f091733667d092934389b
jeffersoncasimir <15801528+jeffersoncasimir@users.noreply.github.com>
2025-11-21 16:00:42 -0500

    Fix line lengths for linter

--- modules/dataquery/jsx/definefields.tsx
+++ modules/dataquery/jsx/definefields.tsx
@@ -376,7 +376,9 @@ function DefineFields(props: {
     fieldList = (<div>
       <div style={{display: 'flex', flexWrap: 'wrap',
         justifyContent: 'space-between'}}>
-        <h2>{t('{{columnName}} fields', {ns: 'dataquery', columnName: cname})}</h2>
+        <h2>
+          {t('{{columnName}} fields', {ns: 'dataquery', columnName: cname})}
+        </h2>
         <div style={{marginTop: '1em',
           display: 'flex',
           flexWrap: 'nowrap',
@@ -445,7 +447,10 @@ function DefineFields(props: {
               <div>
                 <button type="button" className="btn btn-primary"
                   style={{marginBottom: 7}}
-                  onClick={props.onClearAll}>{t('Clear', {ns: 'loris'})}</button>
+                  onClick={
+                    props.onClearAll}>{t('Clear', {ns: 'loris'})
+                  }
+                </button>
               </div>
             </div>
             <SelectedFieldList
