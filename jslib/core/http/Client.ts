158439fe05673fe36ff78ad9fc04bc4c6bb38446
Henri Rabalais <henri.j.rabalais@gmail.com>
2025-11-24 09:20:33 -0500

    trying to fix errors

--- jslib/core/http/Client.ts
+++ jslib/core/http/Client.ts
@@ -154,14 +154,14 @@ export class Client<T> {
       }
 
       // Handle responses with no content or non-JSON content
-      const contentType = response.headers.get('content-type');
-      if (!contentType || !contentType.includes('application/json')) {
-        throw new Errors.NoContent(
-          request,
-          response,
-          this.getErrorMessage('NoContentError', request, response)
-        );
-      }
+      // const contentType = response.headers.get('content-type');
+      // if (!contentType || !contentType.includes('application/json')) {
+      //   throw new Errors.NoContent(
+      //     request,
+      //     response,
+      //     this.getErrorMessage('NoContentError', request, response)
+      //   );
+      // }
 
       // 2. Handle JSON parsing errors
       try {
