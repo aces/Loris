780a7ee57f935a11fe2ebaa08a3491f8387a4451
Dave MacFarlane <driusan@mcin.ca>
2025-12-01 10:40:07 -0500

    [Multilingual] Move "Are you sure?" prompt to loris namespace (#10179)
    This string is shared between many modules. Move it to the LORIS
    namespace for easier maintenance.

--- modules/schedule_module/jsx/scheduleIndex.js
+++ modules/schedule_module/jsx/scheduleIndex.js
@@ -238,15 +238,15 @@ mapColumn(column, cell) {
 deleteConfirm(id) {
   const {t} = this.props;
   swal.fire({
-    title: t('Are you sure?', {ns: 'schedule_module'}),
+    title: t('Are you sure?', {ns: 'loris'}),
     text: t('You won\'t be able to revert this!',
       {ns: 'schedule_module'}),
     type: 'warning',
     showCancelButton: true,
     confirmButtonText: t('Yes, delete it!',
-      {ns: 'schedule_module'}),
+      {ns: 'loris'}),
     cancelButtonText: t('No, cancel it!',
-      {ns: 'schedule_module'}),
+      {ns: 'loris'}),
   }).then((result) => {
     if (result.value) {
       swal.fire(
