05985ca96129d6429749e895de369ebc641519be
Dave MacFarlane <dave.macfarlane@mcin.ca>
2025-12-08 15:05:58 -0500

    [schedule_module] Fix "Yes, delete it!" translation in schedule module
    The string was looking up in the wrong namespace.

--- modules/schedule_module/jsx/scheduleIndex.js
+++ modules/schedule_module/jsx/scheduleIndex.js
@@ -244,7 +244,7 @@ deleteConfirm(id) {
     type: 'warning',
     showCancelButton: true,
     confirmButtonText: t('Yes, delete it!',
-      {ns: 'loris'}),
+      {ns: 'schedule_module'}),
     cancelButtonText: t('No, cancel it!',
       {ns: 'loris'}),
   }).then((result) => {
