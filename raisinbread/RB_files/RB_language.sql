0c4b21a1e14672985a4cfa97be99c47d145b325f
Dave MacFarlane <dave.macfarlane@mcin.ca>
2025-12-10 13:09:20 -0500

    Add French and Japanese to RB

--- raisinbread/RB_files/RB_language.sql
+++ raisinbread/RB_files/RB_language.sql
@@ -1,6 +1,9 @@
+/*M!999999\- enable the sandbox mode */ 
 SET FOREIGN_KEY_CHECKS=0;
 TRUNCATE TABLE `language`;
 LOCK TABLES `language` WRITE;
-INSERT INTO `language` (`language_id`, `language_code`, `language_label`) VALUES (1,'en-CA','English');
+INSERT INTO `language` (`language_id`, `language_code`, `language_label`) VALUES (1,'en_CA','English');
+INSERT INTO `language` (`language_id`, `language_code`, `language_label`) VALUES (2,'fr_CA','Français');
+INSERT INTO `language` (`language_id`, `language_code`, `language_label`) VALUES (3,'ja_JP','日本語');
 UNLOCK TABLES;
 SET FOREIGN_KEY_CHECKS=1;
