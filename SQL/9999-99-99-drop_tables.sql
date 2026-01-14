0d44a659454dcf53f5b830e606dc0dcb4d398a93
kongtiaowang <kongtiaowangshen@gmail.com>
2026-01-14 13:01:11 -0500

    test2

--- SQL/9999-99-99-drop_tables.sql
+++ SQL/9999-99-99-drop_tables.sql
@@ -240,6 +240,10 @@ DROP TABLE IF EXISTS `appointment_type`;
 
 DROP TABLE IF EXISTS `openid_connect_csrf`;
 DROP TABLE IF EXISTS `openid_connect_providers`;
+DROP TABLE IF EXISTS `physiological_task_event_history`;
+DROP TABLE IF EXISTS `hed_tag_history`;
+DROP TABLE IF EXISTS `hed_tag_endorsement_history`;
+DROP TABLE IF EXISTS `hed_tag_endorsement`;
 
 
 DROP TABLE IF EXISTS `candidate_diagnosis_evolution_rel`;
