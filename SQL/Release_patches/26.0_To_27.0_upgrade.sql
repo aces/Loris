cb41adcba064ec5860be1cc02597e89932fec90b
kongtiaowang <kongtiaowangshen@gmail.com>
2026-01-19 10:56:36 -0500

    Subquery returns more than 1 row or null

--- SQL/Release_patches/26.0_To_27.0_upgrade.sql
+++ SQL/Release_patches/26.0_To_27.0_upgrade.sql
@@ -418,57 +418,82 @@ SELECT 'dicom_archive_view_ownsites', 'DICOMs - Own Sites', ID, 'View', 2 FROM m
     WHERE code = 'issue_tracker_reporter';
 -- Change FKs from CandID to CandidateID which is now candidate.ID
 ALTER TABLE CNV DROP constraint CNV_ibfk_3;
-UPDATE CNV SET CandID=(SELECT ID from candidate c WHERE c.CandID=CNV.CandID);
+UPDATE CNV
+JOIN candidate c ON c.CandID = CNV.CandID
+SET CNV.CandID = c.ID;
 ALTER TABLE CNV CHANGE CandID CandidateID int(10) unsigned DEFAULT NULL;
 ALTER TABLE CNV ADD CONSTRAINT CNV_ibfk_3 FOREIGN KEY (CandidateID) REFERENCES candidate(ID);
 
 ALTER TABLE candidate_consent_rel DROP CONSTRAINT `FK_candidate_consent_rel_CandidateID`;
-UPDATE candidate_consent_rel SET CandidateID=(SELECT ID from candidate c WHERE c.CandID=candidate_consent_rel.CandidateID);
+UPDATE candidate_consent_rel rel
+JOIN candidate c
+  ON c.CandID = rel.CandidateID
+SET rel.CandidateID = c.ID;
 ALTER TABLE candidate_consent_rel CHANGE CandidateID CandidateID int(10) unsigned NOT NULL;
 ALTER TABLE candidate_consent_rel ADD CONSTRAINT FK_candidate_consent_rel_CandidateID FOREIGN KEY (CandidateID) REFERENCES candidate(ID) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 ALTER TABLE participant_status DROP CONSTRAINT `fk_participant_status_3`;
-UPDATE participant_status SET CandID=(SELECT ID from candidate c WHERE c.CandID=participant_status.CandID);
+UPDATE participant_status ps
+JOIN candidate c ON c.CandID = ps.CandID
+SET ps.CandID = c.ID;
 ALTER TABLE participant_status CHANGE CandID CandidateID int(10) unsigned NOT NULL;
 ALTER TABLE participant_status ADD CONSTRAINT FK_participant_status_3 FOREIGN KEY (CandidateID) REFERENCES candidate(ID) ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE dataquery_run_results DROP CONSTRAINT `dataquery_run_results_ibfk_1`;
-UPDATE dataquery_run_results SET CandID=(SELECT ID from candidate c WHERE c.CandID=dataquery_run_results.CandID);
+UPDATE dataquery_run_results d
+JOIN candidate c ON c.CandID = d.CandID
+SET d.CandID = c.ID;
 ALTER TABLE dataquery_run_results CHANGE CandID CandidateID int(10) unsigned NOT NULL;
 ALTER TABLE dataquery_run_results ADD CONSTRAINT dataquery_run_results_ibfk_1 FOREIGN KEY (CandidateID) REFERENCES candidate(ID);
 
 ALTER TABLE issues DROP CONSTRAINT `fk_issues_3`;
-UPDATE issues SET CandID=(SELECT ID from candidate c WHERE c.CandID=issues.CandID);
+UPDATE issues i
+JOIN candidate c ON c.CandID = i.CandID
+SET i.CandID = c.ID;
 ALTER TABLE issues CHANGE CandID CandidateID int(10) unsigned DEFAULT NULL;
 ALTER TABLE issues ADD CONSTRAINT fk_issues_3 FOREIGN KEY (CandidateID) REFERENCES candidate(ID);
 
 ALTER TABLE session DROP CONSTRAINT `fk_session_1`;
-UPDATE session SET CandID=(SELECT ID from candidate c WHERE c.CandID=session.CandID);
+UPDATE session s
+JOIN candidate c ON c.CandID = s.CandID
+SET s.CandID = c.ID;
 ALTER TABLE session CHANGE CandID CandidateID int(10) unsigned NOT NULL;
 ALTER TABLE session ADD CONSTRAINT fk_session_1 FOREIGN KEY (CandidateID) REFERENCES candidate(ID);
 
 ALTER TABLE genomic_candidate_files_rel DROP CONSTRAINT `genomic_candidate_files_rel_ibfk_1`;
-UPDATE genomic_candidate_files_rel SET CandID=(SELECT ID from candidate c WHERE c.CandID=genomic_candidate_files_rel.CandID);
+UPDATE genomic_candidate_files_rel g
+JOIN candidate c ON c.CandID = g.CandID
+SET g.CandID = c.ID;
 ALTER TABLE genomic_candidate_files_rel CHANGE CandID CandidateID int(10) unsigned NOT NULL;
 ALTER TABLE genomic_candidate_files_rel ADD CONSTRAINT genomic_candidate_files_rel_ibfk_1 FOREIGN KEY (CandidateID) REFERENCES candidate(ID);
 
 ALTER TABLE mri_scanner DROP CONSTRAINT `FK_mri_scanner_1`;
-UPDATE mri_scanner SET CandID=(SELECT ID from candidate c WHERE c.CandID=mri_scanner.CandID);
+UPDATE mri_scanner m
+JOIN candidate c ON c.CandID = m.CandID
+SET m.CandID = c.ID;
 ALTER TABLE mri_scanner CHANGE CandID CandidateID int(10) unsigned DEFAULT NULL;
 ALTER TABLE mri_scanner ADD CONSTRAINT FK_mri_scanner_1 FOREIGN KEY (CandidateID) REFERENCES candidate(ID);
 
 ALTER TABLE genomic_sample_candidate_rel DROP CONSTRAINT `genomic_sample_candidate_rel_ibfk_1`;
-UPDATE genomic_sample_candidate_rel SET CandID=(SELECT ID from candidate c WHERE c.CandID=genomic_sample_candidate_rel.CandID);
+UPDATE genomic_sample_candidate_rel g
+JOIN candidate c ON c.CandID = g.CandID
+SET g.CandID = c.ID;
 ALTER TABLE genomic_sample_candidate_rel CHANGE CandID CandidateID int(10) unsigned NOT NULL;
 ALTER TABLE genomic_sample_candidate_rel ADD CONSTRAINT `genomic_sample_candidate_rel_ibfk_1` FOREIGN KEY (CandidateID) REFERENCES `candidate`(`ID`);
 
 ALTER TABLE SNP_candidate_rel DROP CONSTRAINT `fk_SNP_candidate_rel_2`;
-UPDATE SNP_candidate_rel SET CandID=(SELECT ID from candidate c WHERE c.CandID=SNP_candidate_rel.CandID);
+UPDATE SNP_candidate_rel s
+JOIN candidate c
+  ON c.CandID = s.CandID
+SET s.CandID = c.ID;
 ALTER TABLE SNP_candidate_rel CHANGE CandID CandidateID int(10) unsigned NOT NULL;
 ALTER TABLE SNP_candidate_rel ADD CONSTRAINT `fk_SNP_candidate_rel_2` FOREIGN KEY (CandidateID) REFERENCES candidate(ID) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 ALTER TABLE parameter_candidate DROP CONSTRAINT `FK_parameter_candidate_2`;
-UPDATE parameter_candidate SET CandID=(SELECT ID from candidate c WHERE c.CandID=parameter_candidate.CandID);
+UPDATE parameter_candidate p
+JOIN candidate c
+  ON c.CandID = p.CandID
+SET p.CandID = c.ID;
 ALTER TABLE parameter_candidate CHANGE CandID CandidateID int(10) unsigned NOT NULL;
 ALTER TABLE parameter_candidate ADD CONSTRAINT FK_parameter_candidate_2 FOREIGN KEY (CandidateID) REFERENCES candidate(ID);
 
@@ -489,23 +514,36 @@ ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 
 -- Changes references to candidate.CandID that were NOT FK. Add FK
-UPDATE feedback_bvl_thread SET CandID=(SELECT ID from candidate c WHERE c.CandID=feedback_bvl_thread.CandID);
+UPDATE feedback_bvl_thread f
+JOIN candidate c ON c.CandID = f.CandID
+SET f.CandID = c.ID;
 ALTER TABLE feedback_bvl_thread CHANGE CandID CandidateID int(10) unsigned DEFAULT NULL;
 ALTER TABLE feedback_bvl_thread ADD CONSTRAINT FK_feedback_bvl_thread_candidate_1 FOREIGN KEY (CandidateID) REFERENCES candidate(ID);
 
-UPDATE mri_violations_log SET CandID=(SELECT ID from candidate c WHERE c.CandID=mri_violations_log.CandID);
+UPDATE mri_violations_log m
+JOIN candidate c ON c.CandID = m.CandID
+SET m.CandID = c.ID;
 ALTER TABLE mri_violations_log CHANGE CandID CandidateID int(10) unsigned DEFAULT NULL;
 ALTER TABLE mri_violations_log ADD CONSTRAINT FK_mri_violations_log_candidate_1 FOREIGN KEY (CandidateID) REFERENCES candidate(ID);
 
-UPDATE mri_protocol_violated_scans SET CandID=(SELECT ID from candidate c WHERE c.CandID=mri_protocol_violated_scans.CandID);
+UPDATE mri_protocol_violated_scans m
+JOIN candidate c
+  ON c.CandID = m.CandID
+SET m.CandID = c.ID;
 ALTER TABLE mri_protocol_violated_scans CHANGE CandID CandidateID int(10) unsigned;
 ALTER TABLE mri_protocol_violated_scans ADD CONSTRAINT FK_mri_protocol_violated_scans_candidate_1 FOREIGN KEY (CandidateID) REFERENCES candidate(ID);
 
-UPDATE participant_status_history SET CandID=(SELECT ID from candidate c WHERE c.CandID=participant_status_history.CandID);
+UPDATE participant_status_history p
+JOIN candidate c
+  ON c.CandID = p.CandID
+SET p.CandID = c.ID;
 ALTER TABLE participant_status_history CHANGE CandID CandidateID int(10) unsigned NOT NULL;
 ALTER TABLE participant_status_history ADD CONSTRAINT FK_participant_status_history_candidate_1 FOREIGN KEY (CandidateID) REFERENCES candidate(ID);
 
-UPDATE family SET CandID=(SELECT ID from candidate c WHERE c.CandID=family.CandID);
+UPDATE family f
+JOIN candidate c
+  ON c.CandID = f.CandID
+SET f.CandID = c.ID;
 ALTER TABLE family CHANGE CandID CandidateID int(10) unsigned NOT NULL;
 ALTER TABLE family ADD CONSTRAINT FK_family_candidate_1 FOREIGN KEY (CandidateID) REFERENCES candidate(ID);
 
