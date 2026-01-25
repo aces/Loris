8bcb18aa806afec618e8a1594df92820b26177f3
Arnav Makkar <142909930+arnav-makkar@users.noreply.github.com>
2026-01-25 17:26:40 +0530

    fix max line length

--- modules/dataquery/jsx/welcome.tsx
+++ modules/dataquery/jsx/welcome.tsx
@@ -808,9 +808,12 @@ function SingleQueryDisplay(props: {
     swal.fire({
       type: 'success',
       title: t('Query Loaded', {ns: 'dataquery'}),
-      text: t('Return to the previous page to run or modify your query.', {ns: 'dataquery'}),
-	  confirmButtonText: t('Return', {ns: 'dataquery'}),
-    });
+      text: t(
+        'Return to the previous page to run or modify your query.',
+        {ns: 'dataquery'}
+      ),
+      confirmButtonText: t('Return', {ns: 'dataquery'}),
+});
   };
 
   const loadIcon = <LoadIcon onClick={loadQuery} />;
