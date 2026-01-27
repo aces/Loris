0d6b4b6931078825e6370b1df44790f9ef883e51
Henri Rabalais <henri.j.rabalais@gmail.com>
2026-01-27 07:02:13 -0500

    removed imports

--- jsx/FilterableDataTable.tsx
+++ jsx/FilterableDataTable.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useCallback, useMemo, ReactNode, ReactElement} from 'react';
+import React, {useState, useEffect, useCallback, useMemo} from 'react';
 import {useTranslation} from 'react-i18next';
 import Panel from 'jsx/Panel';
 import DataTable from 'jsx/DataTable';
