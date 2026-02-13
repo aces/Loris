9ff650325143375c0dca446014e9864acdf799ef
kongtiaowang <kongtiaowangshen@gmail.com>
2026-02-12 20:42:14 -0500

    test

--- modules/battery_manager/test/BatteryManagerTest.php
+++ modules/battery_manager/test/BatteryManagerTest.php
@@ -323,7 +323,7 @@ class BatteryManagerTest extends LorisIntegrationTest
             self::$display,
             self::$clearFilter,
             'BMI Calculator',
-            '4 rows',
+            '6 rows',
             1
         );
         $this->_filterTest(
