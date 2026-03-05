a70bf7c2960f6e4a3b6d4be4a1358616ac898adb
arnav-makkar <arnavmakkar080505@gmail.com>
2026-03-06 02:05:16 +0530

    terms of use translation added

--- jsx/PolicyButton.js
+++ jsx/PolicyButton.js
@@ -8,6 +8,76 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Swal from 'sweetalert2';
+import 'I18nSetup';
+import {withTranslation} from 'react-i18next';
+
+const DATAQUERY_DEFAULT_CONTENT =
+  'By using this Data Query Tool, you acknowledge that you know it is ' +
+  'in beta and may not work as expected. You also agree to use it ' +
+  'responsibly and not to misuse the data.';
+const LOGIN_DEFAULT_CONTENT =
+  'By using this LORIS instance you acknowledge that you know it is ' +
+  'filled with test data, and not real user data.';
+
+const POLICY_TRANSLATION_MSGIDS = Object.freeze({
+  dataquery_example: Object.freeze({
+    HeaderButtonText: 'Terms of Use',
+    SwalTitle: 'Terms of Use',
+    AcceptButtonText: 'Yes, I accept',
+    DeclineButtonText: 'Decline',
+    Content: DATAQUERY_DEFAULT_CONTENT,
+  }),
+  login_example: Object.freeze({
+    HeaderButtonText: 'Terms of Use',
+    SwalTitle: 'Terms of Use',
+    AcceptButtonText: 'Accept',
+    Content: LOGIN_DEFAULT_CONTENT,
+  }),
+});
+
+/**
+ * Return the canonical literal msgid for a known policy field.
+ *
+ * @param {object} policy - Policy object from backend.
+ * @param {string} field - Backend policy field name.
+ * @return {string|null} canonical msgid or null if unknown.
+ */
+const getPolicyMsgid = (policy, field) => {
+  if (!policy
+    || typeof policy.PolicyName !== 'string'
+    || policy.PolicyName === ''
+  ) {
+    return null;
+  }
+  const policyTranslations = POLICY_TRANSLATION_MSGIDS[policy.PolicyName];
+  if (!policyTranslations || !policyTranslations[field]) {
+    return null;
+  }
+  return policyTranslations[field];
+};
+
+/**
+ * Translate known policy text while preserving literal fallback.
+ *
+ * @param {object} policy - Policy object from backend.
+ * @param {string} field - Backend policy field name.
+ * @param {string|undefined|null} fallbackText - Raw backend value.
+ * @param {function|undefined} t - i18n translate function from withTranslation.
+ * @return {string|undefined|null} Translated or fallback text.
+ */
+const translatePolicyField = (policy, field, fallbackText, t) => {
+  if (typeof fallbackText !== 'string' || fallbackText === '') {
+    return fallbackText;
+  }
+  const msgid = getPolicyMsgid(policy, field);
+  if (msgid === null || typeof t !== 'function') {
+    return fallbackText;
+  }
+  return t(
+    msgid,
+    {ns: 'loris', defaultValue: fallbackText}
+  );
+};
 
 /**
  * PolicyButton Component
@@ -21,39 +91,57 @@ import Swal from 'sweetalert2';
  * @param {string} [props.buttonText] - Optional text for the button.
  * @param {boolean} [props.anon] - Optional flag to indicate if the user is anonymous.
  * @param {function} [props.callback] - Optional callback function to execute after the policy decision.
+ * @param {function} [props.t] - Translate function from withTranslation.
  */
-const PolicyButton = ({
+const PolicyButtonComponent = ({
   onClickPolicy,
   popUpPolicy,
   buttonStyle,
   buttonText,
   anon=false,
   callback=() => {},
+  t,
 }) => {
   if (popUpPolicy && popUpPolicy.needsRenewal) {
-    fireSwal(popUpPolicy);
+    fireSwal(popUpPolicy, undefined, undefined, t);
   }
   if (onClickPolicy) {
+    const headerButtonText = translatePolicyField(
+      onClickPolicy,
+      'HeaderButtonText',
+      onClickPolicy.HeaderButtonText,
+      t
+    );
     return <a
       className="hidden-xs hidden-sm"
       id="on-click-policy-button"
       style={{...buttonStyle}}
       href="#"
       onClick={() => {
-        fireSwal(onClickPolicy, anon, callback);
+        fireSwal(onClickPolicy, anon, callback, t);
       }}
     >
-      {buttonText || onClickPolicy.HeaderButtonText}
+      {buttonText || headerButtonText}
     </a>;
   }
 };
 
-const fireSwal = (policy, anon, callback) => {
+const fireSwal = (policy, anon=false, callback=() => {}, t) => {
   Swal.fire({
-    title: policy.SwalTitle,
-    html: policy.Content,
-    confirmButtonText: policy.AcceptButtonText,
-    cancelButtonText: policy.DeclineButtonText,
+    title: translatePolicyField(policy, 'SwalTitle', policy.SwalTitle, t),
+    html: translatePolicyField(policy, 'Content', policy.Content, t),
+    confirmButtonText: translatePolicyField(
+      policy,
+      'AcceptButtonText',
+      policy.AcceptButtonText,
+      t
+    ),
+    cancelButtonText: translatePolicyField(
+      policy,
+      'DeclineButtonText',
+      policy.DeclineButtonText,
+      t
+    ),
     showCancelButton: policy.DeclineButtonText,
     allowOutsideClick: false,
   }).then((decision) => {
@@ -82,15 +170,18 @@ const fireSwal = (policy, anon, callback) => {
   });
 };
 
-PolicyButton.propTypes = {
+PolicyButtonComponent.propTypes = {
   onClickPolicy: PropTypes.shape({
+    PolicyName: PropTypes.string,
+    HeaderButtonText: PropTypes.string,
     SwalTitle: PropTypes.string.isRequired,
     Content: PropTypes.string.isRequired,
     AcceptButtonText: PropTypes.string.isRequired,
     DeclineButtonText: PropTypes.string.isRequired,
-  }).isRequired,
-  onClickPolicy: PropTypes.object.isRequired,
+  }),
   popUpPolicy: PropTypes.shape({
+    PolicyName: PropTypes.string,
+    HeaderButtonText: PropTypes.string,
     needsRenewal: PropTypes.bool,
     SwalTitle: PropTypes.string,
     Content: PropTypes.string,
@@ -101,8 +192,11 @@ PolicyButton.propTypes = {
   buttonText: PropTypes.string,
   anon: PropTypes.bool,
   callback: PropTypes.func,
+  t: PropTypes.func,
 };
+
+const PolicyButton = withTranslation('loris')(PolicyButtonComponent);
 
 window.PolicyButton = PolicyButton;
 
-export {PolicyButton, fireSwal};
+export {PolicyButton, PolicyButtonComponent, fireSwal, translatePolicyField};
