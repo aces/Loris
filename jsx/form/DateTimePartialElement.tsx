ca9b22f0f5255a2c8956bf329ebe22dc24271427
Henri Rabalais <henri.j.rabalais@gmail.com>
2026-03-13 12:50:57 -0400

    fixing lint

--- jsx/form/DateTimePartialElement.tsx
+++ jsx/form/DateTimePartialElement.tsx
@@ -101,8 +101,8 @@ type MaskProps = {
  * React component for an input datetime mask.
  *
  * @param props The props of the component
- * @param props.value
- * @param props.children
+ * @param props.value The value
+ * @param props.children The component children
  * @returns The corresponding React element
  */
 const Mask: React.FC<MaskProps> = ({value, children}) => (
