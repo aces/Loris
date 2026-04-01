8978dd60cf0bafcc2dcf51781db5d36d0109b810
racostas <37309344+racostas@users.noreply.github.com>
2026-04-01 12:34:50 -0400

    Merge commit from fork
    * [security - publication] Takes loris URL from server side, not from the form POST.
    
    * Takes out the reference to the direct link from templates.
    
    ---------
    
    Co-authored-by: lorisadmin <rolando.acosta@mcin.ca>

--- modules/publication/ajax/FileUpload.php
+++ modules/publication/ajax/FileUpload.php
@@ -140,7 +140,7 @@ function uploadPublication() : void
         showPublicationError($e->getMessage(), 500);
     }
 
-    notify($pubID, 'submission', $_POST['baseURL']);
+    notify($pubID, 'submission');
 }
 
 /**
@@ -428,13 +428,12 @@ function cleanup(int $pubID) : void
 /**
  * Send out email notifications for project submission
  *
- * @param int    $pubID   publication ID
- * @param string $type    The notification type i.e., submission|edit|review
- * @param string $baseURL the base URL of the loris site
+ * @param int    $pubID publication ID
+ * @param string $type  The notification type i.e., submission|edit|review
  *
  * @return void
  */
-function notify($pubID, $type, $baseURL) : void
+function notify($pubID, $type) : void
 {
     $acceptedTypes = [
         'submission',
@@ -471,14 +470,12 @@ function notify($pubID, $type, $baseURL) : void
     $emailData['Title']       = $data['Title'];
     $emailData['Date']        = $data['DateProposed'];
     $emailData['User']        = $user->getFullname();
-    $emailData['URL']         = $baseURL . '/publication/view_project/?id='.$pubID;
     $emailData['ProjectName'] = $config->getSetting('prefix');
     $Notifier = new \NDB_Notifier(
         "publication",
         $type
     );
     $msg_data = [
-        'URL'         => $emailData['URL'],
         'Title'       => $emailData['Title'],
         'User'        => $emailData['User'],
         'ProjectName' => $emailData['ProjectName'],
@@ -650,10 +647,10 @@ function editProject() : void
     processFiles($id);
     // if publication status is changed, send review email
     if (isset($toUpdate['PublicationStatusID'])) {
-        notify($id, 'review', $_POST['baseURL']);
+        notify($id, 'review');
     } else {
         // otherwise send edit email
-        notify($id, 'edit', $_POST['baseURL']);
+        notify($id, 'edit');
     }
     if (!empty($toUpdate)) {
         $db->update(
--- smarty/templates/email/notifier_publication_edit.tpl
+++ smarty/templates/email/notifier_publication_edit.tpl
@@ -3,7 +3,7 @@ Subject: [LORIS] A project proposal has been edited
 Hello,
 
 The project proposal "{$Title}" has been edited by {$User}.
-You can view the changes by clicking the following link {$URL}.
+You can view the changes in the loris website.
 
 Thank you,
 The {$ProjectName} Team
