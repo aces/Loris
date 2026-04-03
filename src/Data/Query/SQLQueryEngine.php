564f63580eec9d39acfc99b040b0acf394361f1c
Henri Rabalais <henri.j.rabalais@gmail.com>
2026-04-03 06:47:50 -0400

    [DQT] Fix visit list filtering in candidate matching

--- src/Data/Query/SQLQueryEngine.php
+++ src/Data/Query/SQLQueryEngine.php
@@ -116,7 +116,7 @@ abstract class SQLQueryEngine implements QueryEngine
         $this->addWhereClause("c.Active='Y'");
         $prepbindings = [];
 
-        $this->buildQueryFromCriteria($term, $prepbindings);
+        $this->buildQueryFromCriteria($term, $prepbindings, $visitList);
 
         $query = 'SELECT DISTINCT c.CandID FROM';
 
