9735890d994e151257f6932123536d0c851adf59
Nicolas Brossard <nicolasbrossard.mni@gmail.com>
2026-04-09 09:34:16 -0400

    Fix for bvl_feedback security vulnerability

--- modules/bvl_feedback/ajax/bvl_panel_ajax.php
+++ modules/bvl_feedback/ajax/bvl_panel_ajax.php
@@ -19,7 +19,21 @@
 namespace LORIS\bvl_feedback;
 use \LORIS\StudyEntities\Candidate\CandID;
 
-$username = \User::singleton()->getUsername();
+//----------------------------------------//
+// Ensure user is allowed to issue any of //
+// the bvl_feedback AJAX calls            //
+//----------------------------------------//
+$user = \User::singleton();
+if (!$user->hasPermission('bvl_feedback')) {
+    header("HTTP/1.1 403 Forbidden");
+    header("Content-Type: text/plain");
+    exit(
+        "You do not have valid permissions for this
+         operation."
+    );
+}
+
+$username = $user->getUsername();
 $data     = \Utility::parseFormData($_POST);
 
 if (isset($data['candID']) && !empty($data['candID'])) {
