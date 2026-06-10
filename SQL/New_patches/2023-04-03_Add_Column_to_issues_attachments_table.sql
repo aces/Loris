82460cf7649c47bdbb8d41c13d61327b5049b128
George Murad <george.murad@mcgill.ca>
2026-06-10 14:36:37 -0400

    Address review comments for attachment deletion

--- SQL/New_patches/2023-04-03_Add_Column_to_issues_attachments_table.sql
+++ SQL/New_patches/2023-04-03_Add_Column_to_issues_attachments_table.sql
@@ -1,1 +1,7 @@
-ALTER TABLE issues_attachments ADD COLUMN `date_deleted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE issues_attachments ADD COLUMN `date_deleted` timestamp NULL DEFAULT NULL;
+
+UPDATE issues_attachments
+SET date_deleted = '1999-12-31 00:00:00'
+WHERE deleted = 1;
+
+ALTER TABLE issues_attachments DROP COLUMN deleted;
--- SQL/Release_patches/2023-04-03_Add_Column_to_issues_attachments_table.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE issues_attachments ADD COLUMN `date_deleted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP;
