7bc8c938956eac63838aa9b693e2d7ca7cb479f1
Rida Abou-Haidar <ridz1208@users.noreply.github.com>
2026-06-15 09:52:14 -0400

    Apply suggestion from @ridz1208

--- SQL/New_patches/2026-05-25_add_family_label.sql
+++ SQL/New_patches/2026-05-25_add_family_label.sql
@@ -1,4 +1,6 @@
 ALTER TABLE family ADD COLUMN RelationshipLabel VARCHAR(255);
+-- handles all cases for existing projects
+UPDATE family SET RelationshipLabel = REPLACE(Relationship_type, '_', ' ');
 
 -- LORIS specific labels (can be modified for projects)
 UPDATE family SET RelationshipLabel = 'Full Sibling' WHERE Relationship_type = 'full_sibling';
