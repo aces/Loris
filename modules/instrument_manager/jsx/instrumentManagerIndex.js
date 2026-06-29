7f9796b74054b2a48f99cc81b7be55ac1892297a
Saif Shaikh <saif.shaikh@mcin.ca>
2026-06-29 16:26:17 -0400

    docker fix 1

--- modules/instrument_manager/jsx/instrumentManagerIndex.js
+++ modules/instrument_manager/jsx/instrumentManagerIndex.js
@@ -13,7 +13,7 @@ import InfoPanel from 'jsx/InfoPanel';
 
 import Select from 'react-select';
 import swal from 'sweetalert2';
-import i18n from 'I18nSetup'; 
+import 'I18nSetup';
 
 /**
  * Instrument Manager Index component
