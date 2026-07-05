037969c0d89553e7f42c043675a5303658047436
HachemJ <jad.elhachem.pro@gmail.com>
2026-07-05 12:56:46 -0400

    added missing translations and fixed failing tests

--- modules/candidate_list/jsx/candidateListIndex.js
+++ modules/candidate_list/jsx/candidateListIndex.js
@@ -163,15 +163,15 @@ class CandidateListIndex extends Component {
     }
     if (column === this.props.t('Feedback', {ns: 'loris'})) {
       switch (cell) {
-        case '1': return <td style={{background: '#E4A09E'}}>
-          {this.props.t('Opened', {ns: 'candidate_list'})}</td>;
-        case '2': return <td style={{background: '#EEEEAA'}}>
-          {this.props.t('Answered', {ns: 'candidate_list'})}</td>;
-        case '3': return <td style={{background: '#99CC99'}}>
-          {this.props.t('Closed', {ns: 'candidate_list'})}</td>;
-        case '4': return <td style={{background: '#99CCFF'}}>
-          {this.props.t('Comment', {ns: 'candidate_list'})}</td>;
-        default: return <td>{this.props.t('None', {ns: 'loris'})}</td>;
+      case '1': return <td style={{background: '#E4A09E'}}>
+        {this.props.t('Opened', {ns: 'candidate_list'})}</td>;
+      case '2': return <td style={{background: '#EEEEAA'}}>
+        {this.props.t('Answered', {ns: 'candidate_list'})}</td>;
+      case '3': return <td style={{background: '#99CC99'}}>
+        {this.props.t('Closed', {ns: 'candidate_list'})}</td>;
+      case '4': return <td style={{background: '#99CCFF'}}>
+        {this.props.t('Comment', {ns: 'candidate_list'})}</td>;
+      default: return <td>{this.props.t('None', {ns: 'loris'})}</td>;
       }
     }
     if (column === this.props.t('Scan Done', {ns: 'loris'})) {
