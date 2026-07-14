d959d1dbb1b729b7b8b6cb9ff4020a30284edc92
Michael Lapadula <michael.lapadula@mcin.ca>
2026-07-14 14:13:03 -0400

    Change patch to set charset for table instead

--- SQL/New_patches/2026-07-13-Add-utf8mb4-to-history-table.sql
+++ SQL/New_patches/2026-07-13-Add-utf8mb4-to-history-table.sql
@@ -1,15 +1,35 @@
-ALTER TABLE history 
-    MODIFY tbl varchar(255) CHARACTER SET utf8mb4 NOT NULL DEFAULT '',
-    MODIFY col varchar(255) CHARACTER SET utf8mb4 NOT NULL DEFAULT '',
-    MODIFY old text CHARACTER SET utf8mb4 NULL DEFAULT NULL,
-    MODIFY new text CHARACTER SET utf8mb4 NULL DEFAULT NULL,
-    MODIFY primaryCols text CHARACTER SET utf8mb4 NULL DEFAULT NULL,
-    MODIFY primaryVals text CHARACTER SET utf8mb4 NULL DEFAULT NULL,
-    MODIFY userID varchar(255) CHARACTER SET utf8mb4 NOT NULL DEFAULT '',
-    MODIFY type char(1) CHARACTER SET utf8mb4 NULL DEFAULT NULL;
+SET FOREIGN_KEY_CHECKS = 0;
 
-ALTER TABLE users 
-    MODIFY Real_name varchar(255) CHARACTER SET utf8mb4,
-    MODIFY First_name varchar(255) CHARACTER SET utf8mb4,
-    MODIFY Last_name varchar(255) CHARACTER SET utf8mb4,
-    MODIFY Email varchar(255) CHARACTER SET utf8mb4 NOT NULL DEFAULT '';
+ALTER TABLE electrophysiology_uploader
+    DROP FOREIGN KEY FK_eegupload_UploadedBy;
+ALTER TABLE server_processes
+    DROP FOREIGN KEY FK_task_1;
+ALTER TABLE issues
+    DROP FOREIGN KEY fk_issues_1,
+    DROP FOREIGN KEY fk_issues_2,
+    DROP FOREIGN KEY fk_issues_6,
+    DROP FOREIGN KEY fk_issues_8;
+ALTER TABLE issues_watching
+    DROP FOREIGN KEY fk_issues_watching_1;
+
+ALTER TABLE history CONVERT TO CHARACTER SET utf8mb4;
+ALTER TABLE users CONVERT TO CHARACTER SET utf8mb4;
+ALTER TABLE electrophysiology_uploader CONVERT TO CHARACTER SET utf8mb4;
+ALTER TABLE server_processes CONVERT TO CHARACTER SET utf8mb4;
+ALTER TABLE issues CONVERT TO CHARACTER SET utf8mb4;
+ALTER TABLE issues_watching CONVERT TO CHARACTER SET utf8mb4;
+ALTER TABLE issues_categories CONVERT TO CHARACTER SET utf8mb4;
+
+ALTER TABLE electrophysiology_uploader
+    ADD CONSTRAINT `FK_eegupload_UploadedBy` FOREIGN KEY (`UploadedBy`) REFERENCES `users` (`UserID`);
+ALTER TABLE server_processes
+    ADD CONSTRAINT `FK_task_1` FOREIGN KEY (`userid`) REFERENCES `users` (`UserID`);
+ALTER TABLE issues
+    ADD CONSTRAINT `fk_issues_1` FOREIGN KEY (`reporter`) REFERENCES `users` (`UserID`),
+    ADD CONSTRAINT `fk_issues_2` FOREIGN KEY (`assignee`) REFERENCES `users` (`UserID`),
+    ADD CONSTRAINT `fk_issues_6` FOREIGN KEY (`lastUpdatedBy`) REFERENCES `users` (`UserID`),
+    ADD CONSTRAINT `fk_issues_8` FOREIGN KEY (`category`) REFERENCES `issues_categories` (`categoryName`);
+ALTER TABLE issues_watching
+    ADD CONSTRAINT `fk_issues_watching_1` FOREIGN KEY (`userID`) REFERENCES `users` (`UserID`);
+
+SET FOREIGN_KEY_CHECKS = 1;
