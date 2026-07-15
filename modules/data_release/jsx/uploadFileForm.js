95c435948061ad283ce98f866a7a19a80f0d359c
adamdaudrich <adam.daudrich@mcin.ca>
2026-07-15 09:41:32 -0400

    add trailing space

--- modules/data_release/jsx/uploadFileForm.js
+++ modules/data_release/jsx/uploadFileForm.js
@@ -197,7 +197,7 @@ class UploadFileForm extends Component {
             {ns: 'data_release'}) + '\n ' +
                 t('Would you like to overwrite existing file?',
                   {ns: 'data_release'}) + '\n ' +
-                t('Note that the version associated with the' +
+                t('Note that the version associated with the ' +
                   'file will also be overwritten.',
                 {ns: 'data_release'}),
           type: 'warning',
