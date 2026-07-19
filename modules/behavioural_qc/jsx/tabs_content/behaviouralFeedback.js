f26fdc7c6bdbe3f529370682b74357d8cd5bad03
Montek <montekkundan@gmail.com>
2026-07-18 20:52:41 -0400

    [behavioural_qc] simplify feedback URL parameter filtering

--- modules/behavioural_qc/jsx/tabs_content/behaviouralFeedback.js
+++ modules/behavioural_qc/jsx/tabs_content/behaviouralFeedback.js
@@ -16,7 +16,7 @@ const buildURL = (baseURL, path, params) => {
   const query = new URLSearchParams();
 
   Object.entries(params).forEach(([key, value]) => {
-    if (value !== undefined && value !== null && value !== '') {
+    if (value) {
       query.append(key, value);
     }
   });
