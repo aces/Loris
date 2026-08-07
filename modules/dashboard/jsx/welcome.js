4b550f527e81e360aa027b54c4fa92c26d3b6c35
HachemJ <jad.elhachem.pro@gmail.com>
2026-08-07 12:59:24 -0400

    display last login timestamp in local timezone

--- modules/dashboard/jsx/welcome.js
+++ modules/dashboard/jsx/welcome.js
@@ -1,6 +1,21 @@
 import DOMPurify from 'dompurify';
+import i18n from 'I18nSetup';
 
 window.addEventListener('load', () => {
+  const lastLogin = document.getElementById('last-login');
+
+  if (lastLogin?.dataset.timestamp) {
+    const timestamp = new Date(lastLogin.dataset.timestamp);
+    if (!isNaN(timestamp.getTime())) {
+      lastLogin.textContent = new Intl.DateTimeFormat(
+        i18n.language.replace('_', '-'),
+        {
+          dateStyle: 'full',
+          timeStyle: 'short',
+        }
+      ).format(timestamp);
+    }
+  }
   fetch(loris.BaseURL + '/dashboard/projectdescription').then( (resp) => {
     if (!resp.ok) {
       throw new Error('Could not get project description');
--- modules/dashboard/templates/welcomebody.tpl
+++ modules/dashboard/templates/welcomebody.tpl
@@ -2,6 +2,6 @@
    that they can be translated in the .po files before the
    variable interpolation *}
 <h3 class="welcome">{sprintf(dgettext("dashboard", "Welcome, %s!"), $username)}</h3>
-<p class="pull-right small login-time">{dgettext("dashboard", "Last login:")} {$last_login}</p>
+<p class="pull-right small login-time">{dgettext("dashboard", "Last login: ")}<span id="last-login" data-timestamp="{$last_login}"</span></p>
 <p id="project-description" class="project-description"></p>
 <script src="/dashboard/js/welcome.js"></script>
